45f0f631651978ebb9a7a7aba6426a1d483cf99c
Pratul Kalia <pratul@uncommon.is>
2018-05-09 17:52:37 +0530

    Remove unneeded ParcelCreator suppressions

--- app/src/main/java/org/resolvetosavelives/red/newentry/mobile/PatientMobileEntryScreenKey.kt
+++ app/src/main/java/org/resolvetosavelives/red/newentry/mobile/PatientMobileEntryScreenKey.kt
@@ -1,12 +1,10 @@
 package org.resolvetosavelives.red.newentry.mobile
 
-import android.annotation.SuppressLint
 import kotlinx.android.parcel.Parcelize
 import org.resolvetosavelives.red.R
 import org.resolvetosavelives.red.router.screen.FullScreenKey
 
 @Parcelize
-@SuppressLint("ParcelCreator")
 class PatientMobileEntryScreenKey : FullScreenKey {
 
   override fun layoutRes(): Int {

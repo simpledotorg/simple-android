939ec0ab6a3226ed1727f49722f182d81f4a5c0e
Pratul Kalia <pratul@uncommon.is>
2018-05-09 17:52:37 +0530

    Remove unneeded ParcelCreator suppressions

--- app/src/main/java/org/resolvetosavelives/red/home/HomeScreenKey.kt
+++ app/src/main/java/org/resolvetosavelives/red/home/HomeScreenKey.kt
@@ -1,12 +1,10 @@
 package org.resolvetosavelives.red.home
 
-import android.annotation.SuppressLint
 import kotlinx.android.parcel.Parcelize
 import org.resolvetosavelives.red.R
 import org.resolvetosavelives.red.router.screen.FullScreenKey
 
 @Parcelize
-@SuppressLint("ParcelCreator")
 class HomeScreenKey : FullScreenKey {
 
   override fun layoutRes(): Int {

8acd14b2963ede4d54d80d79e8f41b3dbc602558
Saket Narayan <saket@saket.me>
2018-05-10 15:30:08 +0530

    Add PatientRepository, Patient and OngoingPatientEntry classes

---
+++ app/src/main/java/org/resolvetosavelives/red/newentry/search/Patient.kt
@@ -0,0 +1,7 @@
+package org.resolvetosavelives.red.newentry.search
+
+// TODO: find a better package for Patient and its related classes.
+data class Patient(
+    val fullName: String,
+    val mobileNumber: String
+)

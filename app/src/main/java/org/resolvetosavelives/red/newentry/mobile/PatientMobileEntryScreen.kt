1f2fb7c17dea0ec101feeda96d3f3cb6a77d8c52
Saket Narayan <saket@saket.me>
2018-05-10 11:42:45 +0530

    Use Kotterknife instead of Butterknife

--- app/src/main/java/org/resolvetosavelives/red/newentry/mobile/PatientMobileEntryScreen.kt
+++ app/src/main/java/org/resolvetosavelives/red/newentry/mobile/PatientMobileEntryScreen.kt
@@ -2,9 +2,10 @@ package org.resolvetosavelives.red.newentry.mobile
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.Button
 import android.widget.RelativeLayout
-import kotlinx.android.synthetic.main.screen_patient_mobile_entry.view.*
-import org.resolvetosavelives.red.R.id.newPatientButton
+import kotterknife.bindView
+import org.resolvetosavelives.red.R
 import org.resolvetosavelives.red.TheActivity
 import org.resolvetosavelives.red.newentry.personal.PatientPersonalDetailsEntryScreen
 
@@ -13,6 +14,8 @@ class PatientMobileEntryScreen(context: Context, attrs: AttributeSet) : Relative
   companion object {
     val KEY = PatientMobileEntryScreenKey()
   }
+
+  private val newPatientButton: Button by bindView(R.id.home_new_patient)
 
   override fun onFinishInflate() {
     super.onFinishInflate()

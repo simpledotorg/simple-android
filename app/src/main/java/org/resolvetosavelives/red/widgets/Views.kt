44e7bb0583128221562c0246ec2be908e0dacac8
Saket Narayan <saket@saket.me>
2018-05-11 17:59:44 +0530

    Add EditText#showKeyboard() extension function

---
+++ app/src/main/java/org/resolvetosavelives/red/widgets/Views.kt
@@ -0,0 +1,13 @@
+package org.resolvetosavelives.red.widgets
+
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
+fun EditText.showKeyboard() {
+  post({
+    this.requestFocus()
+    val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+  })
+}

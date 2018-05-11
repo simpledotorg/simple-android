4c1d11327f25c3a2a47cfa6ae04f492f0a6cd36a
Saket Narayan <saket@saket.me>
2018-05-11 11:35:38 +0530

    Persist saved patients to database

--- app/src/main/java/org/resolvetosavelives/red/newentry/search/Patient.kt
+++ app/src/main/java/org/resolvetosavelives/red/newentry/search/Patient.kt
@@ -1,7 +1,18 @@
 package org.resolvetosavelives.red.newentry.search
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
 // TODO: find a better package for Patient and its related classes.
+@Entity
 data class Patient(
+    @PrimaryKey
+    val uuid: String,
+
+    @ColumnInfo(name = "full_name")
     val fullName: String,
+
+    @ColumnInfo(name = "mobile_number")
     val mobileNumber: String
 )
---
+++ app/src/main/java/org/resolvetosavelives/red/newentry/search/PatientDao.kt
@@ -0,0 +1,20 @@
+package org.resolvetosavelives.red.newentry.search
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import io.reactivex.Flowable
+
+@Dao
+interface PatientDao {
+
+  @Query("SELECT * FROM patient WHERE full_name LIKE :query")
+  fun search(query: String): Flowable<List<Patient>>
+
+  @Query("SELECT * FROM patient")
+  fun allPatients(): Flowable<List<Patient>>
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun save(patient: Patient)
+}

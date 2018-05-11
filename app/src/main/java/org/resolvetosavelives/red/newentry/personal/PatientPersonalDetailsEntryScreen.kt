944e29ffafad7c80e8d5e32c26f6df67d933d680
Saket Narayan <saket@saket.me>
2018-05-11 16:29:00 +0530

    Add missing background thread schedulers to all database operations

--- app/src/main/java/org/resolvetosavelives/red/newentry/personal/PatientPersonalDetailsEntryScreen.kt
+++ app/src/main/java/org/resolvetosavelives/red/newentry/personal/PatientPersonalDetailsEntryScreen.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.RelativeLayout
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotterknife.bindView
 import org.resolvetosavelives.red.R
 import org.resolvetosavelives.red.TheActivity
@@ -30,10 +31,11 @@ class PatientPersonalDetailsEntryScreen(context: Context, attrs: AttributeSet) :
     nextButton.setOnClickListener({
       val repository = TheActivity.patientRepository()
 
-      // TODO: threading.
       repository.ongoingEntry()
           .map { entry -> entry.copy(personalDetails = OngoingPatientEntry.PersonalDetails(fullNameEditText.text.toString())) }
           .flatMapCompletable { entry: OngoingPatientEntry -> repository.save(entry) }
+          .subscribeOn(Schedulers.io())
+          .observeOn(AndroidSchedulers.mainThread())
           .subscribe({
             TheActivity.screenRouter().push(PatientAddressEntryScreen.KEY)
           })

05f46e28b5fcf67fd0711da66c2ca72c6bc1f6a5
Saket Narayan <saket@saket.me>
2018-05-15 16:15:56 +0530

    Use MVI in NewBpScreen

---
+++ app/src/test/java/org/resolvetosavelives/red/home/bp/NewBpScreenControllerTest.kt
@@ -0,0 +1,43 @@
+package org.resolvetosavelives.red.home.bp
+
+import com.nhaarman.mockito_kotlin.mock
+import io.reactivex.subjects.PublishSubject
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyNoMoreInteractions
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import org.resolvetosavelives.red.widgets.UiEvent
+
+class NewBpScreenControllerTest {
+
+  @Rule
+  @JvmField
+  var mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+  private val screen: NewBpScreen = mock()
+  private val uiEvents: PublishSubject<UiEvent> = PublishSubject.create()
+  private val controller: NewBpScreenController = NewBpScreenController()
+
+  @Before
+  fun setUp() {
+    uiEvents
+        .compose(controller)
+        .subscribe { uiChange -> uiChange(screen) }
+  }
+
+  @Test
+  fun `when new patient is clicked then patient search screen should open`() {
+    uiEvents.onNext(NewPatientClicked())
+
+    verify(screen).openNewPatientScreen()
+  }
+
+  @After
+  fun tearDown() {
+    verifyNoMoreInteractions(screen)
+  }
+}

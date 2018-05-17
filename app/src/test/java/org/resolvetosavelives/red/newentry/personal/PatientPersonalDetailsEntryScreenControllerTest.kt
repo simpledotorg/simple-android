194d96d9f4939e1dd68129e420224bb763a868ff
Saket Narayan <saket@saket.me>
2018-05-17 12:45:37 +0530

    Pre-fill existing patient details when personal details entry screen starts

--- app/src/test/java/org/resolvetosavelives/red/newentry/personal/PatientPersonalDetailsEntryScreenControllerTest.kt
+++ app/src/test/java/org/resolvetosavelives/red/newentry/personal/PatientPersonalDetailsEntryScreenControllerTest.kt
@@ -2,9 +2,10 @@ package org.resolvetosavelives.red.newentry.personal
 
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.argumentCaptor
-import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.subjects.PublishSubject
@@ -14,23 +15,19 @@ import org.junit.Test
 import org.resolvetosavelives.red.newentry.search.Gender
 import org.resolvetosavelives.red.newentry.search.OngoingPatientEntry
 import org.resolvetosavelives.red.newentry.search.PatientRepository
+import org.resolvetosavelives.red.widgets.ScreenCreated
 import org.resolvetosavelives.red.widgets.UiEvent
 
 class PatientPersonalDetailsEntryScreenControllerTest {
 
   private val screen: PatientPersonalDetailsEntryScreen = mock()
-  private lateinit var repository: PatientRepository
+  private var repository: PatientRepository = mock()
 
   private val uiEvents: PublishSubject<UiEvent> = PublishSubject.create()
   private lateinit var controller: PatientPersonalDetailsEntryScreenController
 
   @Before
   fun setUp() {
-    repository = mock {
-      on { ongoingEntry() }.doReturn(Single.just(OngoingPatientEntry(mobileNumber = "9999999999")))
-      on { save(any()) }.doReturn(Completable.complete())
-    }
-
     controller = PatientPersonalDetailsEntryScreenController(repository)
 
     uiEvents
@@ -40,18 +37,45 @@ class PatientPersonalDetailsEntryScreenControllerTest {
 
   @Test
   fun `when screen starts then the keyboard should be shown on full name field`() {
+    // This is unrelated to this test, this suppresses an
+    // error that happens after this test has passed.
+    whenever(repository.ongoingEntry()).thenReturn(Single.never())
+
+    uiEvents.onNext(ScreenCreated())
+
     verify(screen).showKeyboardOnFullnameField()
+  }
+
+  @Test
+  fun `when screen starts and existing personal details are present then existing details should be pre-filled`() {
+    val existingPersonalDetails = OngoingPatientEntry.PersonalDetails("Ashok kumar", "01/01/1900", 25, Gender.TRANS)
+    val existingEntry = OngoingPatientEntry(mobileNumber = "9999999999", personalDetails = existingPersonalDetails)
+    whenever(repository.ongoingEntry()).thenReturn(Single.just(existingEntry))
+
+    uiEvents.onNext(ScreenCreated())
+
+    verify(screen).preFill(existingPersonalDetails)
+  }
+
+  @Test
+  fun `when screen starts and existing personal details are not present then existing details should not be pre-filled`() {
+    val existingEntry = OngoingPatientEntry(mobileNumber = "9999999999", personalDetails = null)
+    whenever(repository.ongoingEntry()).thenReturn(Single.just(existingEntry))
+
+    verify(screen, never()).preFill(any())
   }
 
   @Test
   fun `when proceed event is clicked then personal details should be saved and the next screen should be opened`() {
     val details = OngoingPatientEntry.PersonalDetails("Ashok kumar", "01/01/1900", 25, Gender.TRANS)
 
+    whenever(repository.ongoingEntry()).thenReturn(Single.just(OngoingPatientEntry(mobileNumber = "9999999999")))
+    whenever(repository.save(any())).thenReturn(Completable.complete())
+
     uiEvents.onNext(PatientFullNameTextChanged(details.fullName))
     uiEvents.onNext(PatientDateOfBirthTextChanged(details.dateOfBirth))
     uiEvents.onNext(PatientAgeTextChanged(details.ageWhenCreated))
     uiEvents.onNext(PatientGenderChanged(details.gender))
-
     uiEvents.onNext(PatientPersonalDetailsProceedClicked())
 
     argumentCaptor<OngoingPatientEntry>().apply {

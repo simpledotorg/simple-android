42efe99cd6238be8cff18e7909ca7eadd86d9606
Pratul Kalia <pratul@uncommon.is>
2018-05-24 14:46:31 +0530

    Add Sentry libraries and configuration

--- app/src/main/java/org/resolvetosavelives/red/RedApp.kt
+++ app/src/main/java/org/resolvetosavelives/red/RedApp.kt
@@ -4,10 +4,14 @@ import android.app.Application
 import com.facebook.stetho.Stetho
 import com.gabrielittner.threetenbp.LazyThreeTen
 import com.tspoon.traceur.Traceur
+import io.sentry.Sentry
+import io.sentry.android.AndroidSentryClientFactory
 import org.resolvetosavelives.red.di.AppComponent
 import timber.log.Timber
 
 abstract class RedApp : Application() {
+
+  val dsn = "https://e8606e0cd2f7470e834e446905612ff2@sentry.io/1212614"
 
   companion object {
     lateinit var appComponent: AppComponent
@@ -25,6 +29,8 @@ abstract class RedApp : Application() {
     LazyThreeTen.init(this)
 
     appComponent = buildDaggerGraph()
+
+    Sentry.init(dsn, AndroidSentryClientFactory(this))
   }
 
   abstract fun buildDaggerGraph(): AppComponent

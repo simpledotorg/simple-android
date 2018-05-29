a17f8a6bc5cd6d5acf4380ff7debba9e5a31e6d8
Saket Narayan <saket@saket.me>
2018-05-29 17:47:30 +0530

    Implement pulling of patients from server along with Android tests

--- app/src/androidTest/java/org/resolvetosavelives/red/sync/PatientSyncAndroidTest.kt
+++ app/src/androidTest/java/org/resolvetosavelives/red/sync/PatientSyncAndroidTest.kt
@@ -5,7 +5,11 @@ import android.arch.persistence.room.Room
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.f2prateek.rx.preferences2.Preference
+import com.gojuno.koptional.Optional
+import com.gojuno.koptional.Some
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Single
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -16,6 +20,8 @@ import org.resolvetosavelives.red.di.StorageModule
 import org.resolvetosavelives.red.newentry.search.Gender
 import org.resolvetosavelives.red.newentry.search.OngoingPatientEntry
 import org.resolvetosavelives.red.newentry.search.PatientRepository
+import org.threeten.bp.Duration
+import org.threeten.bp.Instant
 
 @RunWith(AndroidJUnit4::class)
 class PatientSyncAndroidTest {
@@ -23,8 +29,9 @@ class PatientSyncAndroidTest {
   private lateinit var patientSync: PatientSync
   private lateinit var repository: PatientRepository
   private lateinit var database: AppDatabase
-  private lateinit var firstSyncDone: Preference<Boolean>
+  private lateinit var lastPullTimestamp: Preference<Optional<Instant>>
   private lateinit var api: PatientSyncApiV1
+  private val config = PatientSyncConfig(frequency = Duration.ofHours(1), batchSize = 10)
 
   @Before
   fun setUp() {
@@ -36,29 +43,26 @@ class PatientSyncAndroidTest {
     val rxSharedPrefs = StorageModule().rxSharedPreferences(appContext)
 
     api = patientSyncModule.patientSyncApi(appContext, networkModule.retrofitBuilder(moshi))
-    firstSyncDone = patientSyncModule.firstSyncFlag(rxSharedPrefs)
+    lastPullTimestamp = patientSyncModule.lastPullTimestamp(rxSharedPrefs)
     database = Room.inMemoryDatabaseBuilder(appContext, AppDatabase::class.java).build()
     repository = PatientRepository(database)
-    patientSync = PatientSync(api, repository, firstSyncDone)
+    patientSync = PatientSync(api, repository, Single.just(config), lastPullTimestamp)
   }
 
-  @After
-  fun tearDown() {
-    database.clearAllTables()
-    database.close()
+  private fun insertDummyPatients(count: Int): Completable {
+    return Observable.range(0, count)
+        .flatMapCompletable({ index ->
+          repository
+              .saveOngoingEntry(OngoingPatientEntry(
+                  personalDetails = OngoingPatientEntry.PersonalDetails("Ashok Kumar #$index", "12-04-1993", null, Gender.TRANSGENDER),
+                  address = OngoingPatientEntry.Address("colony-or-village", "district", "state")))
+              .andThen(repository.saveOngoingEntryAsPatient())
+        })
   }
 
   @Test
   fun when_pending_sync_patients_are_present_then_they_should_be_pushed_to_the_server_and_marked_as_synced_on_success() {
-    Observable.range(0, 5)
-        .flatMapCompletable({ count ->
-          repository
-              .saveOngoingEntry(OngoingPatientEntry(
-                  personalDetails = OngoingPatientEntry.PersonalDetails("Ashok Kumar #$count", "12-04-1993", null, Gender.TRANSGENDER),
-                  address = OngoingPatientEntry.Address("colony-or-village", "district", "state")))
-              .andThen(repository.saveOngoingEntryAsPatient())
-        })
-        .subscribe()
+    insertDummyPatients(count = 5).blockingAwait()
 
     patientSync.push().blockingAwait()
 
@@ -71,10 +75,31 @@ class PatientSyncAndroidTest {
   }
 
   @Test
-  fun when_pulling_patients_then_verify_that_they_correct_get_deserialized() {
-    api.pull(isFirstSync = true, recordsToRetrieve = 20)
+  fun when_pulling_patients_then_paginate_till_the_server_does_not_have_anymore_patients() {
+    // Set lastPullTimestamp to a bit in the past.
+    lastPullTimestamp.set(Some(Instant.now().minusSeconds(1)))
+
+    val patientsToInsert = 2 * config.batchSize + 7
+
+    insertDummyPatients(count = patientsToInsert)
+        .andThen(patientSync.push())
+        .andThen(Completable.fromAction({ database.clearAllTables() }))
+        .blockingAwait()
+
+    patientSync.pull().blockingAwait()
+
+    repository.patientCount()
+        .take(1)
         .test()
         .await()
-        .assertValue({ response -> response.patients.isNotEmpty() })
+        .assertValue(patientsToInsert)
+        .assertComplete()
+        .assertNoErrors()
+  }
+
+  @After
+  fun tearDown() {
+    database.clearAllTables()
+    database.close()
   }
 }
--- app/src/test/java/org/resolvetosavelives/red/sync/PatientSyncTest.kt
+++ app/src/test/java/org/resolvetosavelives/red/sync/PatientSyncTest.kt
@@ -1,33 +1,43 @@
 package org.resolvetosavelives.red.sync
 
 import com.f2prateek.rx.preferences2.Preference
+import com.gojuno.koptional.None
+import com.gojuno.koptional.Optional
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Completable
+import io.reactivex.Observable
 import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 import org.resolvetosavelives.red.newentry.search.PatientRepository
+import org.threeten.bp.Instant
 
 class PatientSyncTest {
 
   private val api: PatientSyncApiV1 = mock()
   private val repository: PatientRepository = mock()
-  private val firstSyncDone = mock<Preference<Boolean>>()
+  private val lastSyncTimestamp: Preference<Optional<Instant>> = mock()
+
+  private lateinit var syncConfigProvider: () -> PatientSyncConfig
+
   private lateinit var patientSync: PatientSync
 
   @Before
   fun setup() {
-    patientSync = PatientSync(api, repository, firstSyncDone)
+    // Do not move this to property initialization. It's important to reset this on every test.
+    syncConfigProvider = mock()
+    patientSync = PatientSync(api, repository, Single.fromCallable { syncConfigProvider() }, lastSyncTimestamp)
   }
 
   @Test
   fun `when pending-sync patients are empty then the server should not get called`() {
     whenever(repository.pendingSyncPatients()).thenReturn(Single.just(listOf()))
 
-    patientSync.sync()
+    patientSync.push().blockingAwait()
 
     verify(api, never()).push(any())
   }
@@ -39,7 +49,15 @@ class PatientSyncTest {
 
   @Test
   fun `when pulling patients for the time then 'first_time' query-param should be set`() {
+    val config = PatientSyncConfig(frequency = mock(), batchSize = 10)
+    whenever(lastSyncTimestamp.asObservable()).thenReturn(Observable.just(None))
+    whenever(syncConfigProvider()).thenReturn(config)
+    whenever(api.pull(isFirstSync = true, recordsToRetrieve = config.batchSize)).thenReturn(Single.just(PatientPullResponse(listOf(), Instant.now())))
+    whenever(repository.mergeWithLocalDatabase(any())).thenReturn(Completable.complete())
 
+    patientSync.pull().blockingAwait()
+
+    verify(api).pull(isFirstSync = true, recordsToRetrieve = config.batchSize)
   }
 
   @Test

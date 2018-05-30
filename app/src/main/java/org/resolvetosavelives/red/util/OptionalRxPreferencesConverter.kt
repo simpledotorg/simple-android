36f9eceb2f4b3a5e5e5549e8c14b4cfdc747c540
Saket Narayan <saket@saket.me>
2018-05-30 16:15:05 +0530

    Remove KOptional library and introduce our own Optional with Just and None types

--- app/src/main/java/org/resolvetosavelives/red/util/OptionalRxPreferencesConverter.kt
+++ app/src/main/java/org/resolvetosavelives/red/util/OptionalRxPreferencesConverter.kt
@@ -1,22 +1,19 @@
 package org.resolvetosavelives.red.util
 
 import com.f2prateek.rx.preferences2.Preference
-import com.gojuno.koptional.None
-import com.gojuno.koptional.Optional
-import com.gojuno.koptional.Some
 
 class OptionalRxPreferencesConverter<T : Any>(private val valueConverter: Preference.Converter<T>) : Preference.Converter<Optional<T>> {
 
   override fun deserialize(serialized: String): Optional<T> {
     return when {
       serialized.isEmpty() -> None
-      else -> Some(valueConverter.deserialize(serialized))
+      else -> Just(valueConverter.deserialize(serialized))
     }
   }
 
   override fun serialize(optional: Optional<T>): String {
     return when (optional) {
-      is Some -> valueConverter.serialize(optional.value)
+      is Just -> valueConverter.serialize(optional.value)
       is None -> ""
     }
   }

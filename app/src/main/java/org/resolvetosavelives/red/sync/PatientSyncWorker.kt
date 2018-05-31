14bf92a1700d43bcc37b9931e15e9d6d5024298c
Saket Narayan <saket@saket.me>
2018-05-31 12:10:20 +0530

    Avoid re-scheduling of PatientSyncWorker when syncing fails

--- app/src/main/java/org/resolvetosavelives/red/sync/PatientSyncWorker.kt
+++ app/src/main/java/org/resolvetosavelives/red/sync/PatientSyncWorker.kt
@@ -3,9 +3,6 @@ package org.resolvetosavelives.red.sync
 import androidx.work.Worker
 import io.reactivex.Single
 import org.resolvetosavelives.red.RedApp
-import timber.log.Timber
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
 import javax.inject.Inject
 
 class PatientSyncWorker : Worker() {
@@ -21,24 +18,8 @@ class PatientSyncWorker : Worker() {
     RedApp.appComponent.inject(this)
 
     return patientSync.sync()
+        .onErrorComplete()
         .andThen(Single.just(WorkerResult.SUCCESS))
-        .onErrorReturn({ error ->
-          when {
-            isRecoverable(error) -> WorkerResult.RETRY
-            else -> {
-              Timber.e(error, "Couldn't sync patients")
-              WorkerResult.FAILURE
-            }
-          }
-        })
         .blockingGet()
-  }
-
-  private fun isRecoverable(error: Throwable): Boolean {
-    return when (error) {
-      is SocketTimeoutException -> true
-      is UnknownHostException -> true
-      else -> false
-    }
   }
 }

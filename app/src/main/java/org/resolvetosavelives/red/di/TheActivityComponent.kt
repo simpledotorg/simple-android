1200d4a8a15ef12f9bb1bdf732da92e0cc724c57
Saket Narayan <saket@saket.me>
2018-06-06 13:28:18 +0530

    Remove all classes related to old patient entry

--- app/src/main/java/org/resolvetosavelives/red/di/TheActivityComponent.kt
+++ app/src/main/java/org/resolvetosavelives/red/di/TheActivityComponent.kt
@@ -4,22 +4,16 @@ import dagger.BindsInstance
 import dagger.Subcomponent
 import org.resolvetosavelives.red.TheActivity
 import org.resolvetosavelives.red.home.bp.NewBpScreen
-import org.resolvetosavelives.red.newentry.address.PatientAddressEntryScreen
-import org.resolvetosavelives.red.newentry.personal.PatientPersonalDetailsEntryScreen
-import org.resolvetosavelives.red.newentry.phone.PatientPhoneEntryScreen
-import org.resolvetosavelives.red.search.PatientSearchByPhoneScreen
 import org.resolvetosavelives.red.newentry.success.PatientSavedScreen
 import org.resolvetosavelives.red.qrscan.AadhaarScanScreen
 import org.resolvetosavelives.red.router.screen.ScreenRouter
+import org.resolvetosavelives.red.search.PatientSearchByPhoneScreen
 
 @Subcomponent
 interface TheActivityComponent {
 
   fun inject(target: NewBpScreen)
   fun inject(target: TheActivity)
-  fun inject(target: PatientPersonalDetailsEntryScreen)
-  fun inject(target: PatientAddressEntryScreen)
-  fun inject(target: PatientPhoneEntryScreen)
   fun inject(target: PatientSavedScreen)
   fun inject(target: PatientSearchByPhoneScreen)
   fun inject(target: AadhaarScanScreen)

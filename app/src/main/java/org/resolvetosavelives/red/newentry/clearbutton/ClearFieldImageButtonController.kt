1348287f26b2a8882cb306e26cdcd31b0f224418
Saket Narayan <saket@saket.me>
2018-06-12 14:15:05 +0530

    PatientEntry: keep 'X' buttons visible only while their fields have focus and are non-empty

--- app/src/main/java/org/resolvetosavelives/red/newentry/clearbutton/ClearFieldImageButtonController.kt
+++ app/src/main/java/org/resolvetosavelives/red/newentry/clearbutton/ClearFieldImageButtonController.kt
@@ -3,6 +3,7 @@ package org.resolvetosavelives.red.newentry.clearbutton
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
+import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.ofType
 import org.resolvetosavelives.red.widgets.UiEvent
 import javax.inject.Inject
@@ -15,10 +16,17 @@ class ClearFieldImageButtonController @Inject constructor() : ObservableTransfor
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
     val replayedEvents = events.replay(1).refCount()
 
-    return replayedEvents
+    val textChanges = replayedEvents
         .ofType<CleareableFieldTextChanged>()
         .map { it.text.isNotBlank() }
+
+    val focusChanges = replayedEvents
+        .ofType<CleareableFieldFocusChanged>()
+        .map { it.hasFocus }
+
+    return Observables.combineLatest(textChanges, focusChanges)
+        .map { (hasText, hasFocus) -> hasText.and(hasFocus) }
         .distinctUntilChanged()
-        .map { hasText -> { ui: Ui -> ui.setVisible(hasText) } }
+        .map { textCanBeCleared -> { ui: Ui -> ui.setVisible(textCanBeCleared) } }
   }
 }

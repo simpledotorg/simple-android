dca9fb09e4afb7fd7b285c9bebf0fd4a604d27b7
Saket Narayan <saket@saket.me>
2018-06-12 14:15:05 +0530

    PatientEntry: keep 'X' buttons visible only while their fields have focus and are non-empty

--- app/src/main/java/org/resolvetosavelives/red/newentry/clearbutton/ClearFieldImageButtonEvents.kt
+++ app/src/main/java/org/resolvetosavelives/red/newentry/clearbutton/ClearFieldImageButtonEvents.kt
@@ -3,3 +3,5 @@ package org.resolvetosavelives.red.newentry.clearbutton
 import org.resolvetosavelives.red.widgets.UiEvent
 
 data class CleareableFieldTextChanged(val text: String) : UiEvent
+
+data class CleareableFieldFocusChanged(val hasFocus: Boolean) : UiEvent
--- app/src/test/java/org/resolvetosavelives/red/newentry/clearbutton/ClearFieldImageButtonControllerTest.kt
+++ app/src/test/java/org/resolvetosavelives/red/newentry/clearbutton/ClearFieldImageButtonControllerTest.kt
@@ -25,12 +25,15 @@ class ClearFieldImageButtonControllerTest {
   }
 
   @Test
-  fun `clear button should remain visible only while the associated field is not blank`() {
+  fun `clear button should remain visible only while the associated field is focused and is not blank`() {
+    uiEvents.onNext(CleareableFieldFocusChanged(hasFocus = false))
     uiEvents.onNext(CleareableFieldTextChanged(""))
+    uiEvents.onNext(CleareableFieldFocusChanged(hasFocus = true))
     uiEvents.onNext(CleareableFieldTextChanged("A"))
     uiEvents.onNext(CleareableFieldTextChanged("AB"))
+    uiEvents.onNext(CleareableFieldFocusChanged(hasFocus = false))
 
-    verify(button).setVisible(false)
+    verify(button, times(2)).setVisible(false)
     verify(button, times(1)).setVisible(true)
   }
 }

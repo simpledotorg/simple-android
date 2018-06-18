32395dbdc48ce21dc00020a60092b74a5e69d804
Saket Narayan <saket@saket.me>
2018-06-18 13:05:38 +0530

    Hold an array of dosages in ProtocolDrug instead of a single dosage

--- app/src/main/java/org/simple/clinic/protocol/ProtocolRepository.kt
+++ app/src/main/java/org/simple/clinic/protocol/ProtocolRepository.kt
@@ -7,46 +7,28 @@ import javax.inject.Inject
 class ProtocolRepository @Inject constructor() {
 
   fun currentProtocol(): Observable<Protocol> {
-    val protocolUuid = UUID.fromString("3a4aadc1-3322-40be-95b4-a960e6cff9e3")
+    val protocolUuid = UUID.fromString("d486b15b-ac2a-4d2f-b87c-e1905d5ee793")
     return Observable.just(Protocol(
         protocolUuid,
         name = "Dummy protocol",
         drugs = listOf(
             ProtocolDrug(
-                UUID.fromString("80976570-0747-4555-9f71-ccba053aab27"),
+                UUID.fromString("feab6950-86fe-4b70-95c9-f21620140068"),
                 name = "Amlodipine",
                 rxNormCode = "rxnormcode-1",
-                dosage = "5mg",
+                dosages = listOf("5mg", "10mg"),
                 protocolUUID = protocolUuid),
             ProtocolDrug(
-                UUID.fromString("045b3a7e-ccaa-49ac-b274-e1c6845d621c"),
-                name = "Amlodipine",
-                rxNormCode = "rxnormcode-1",
-                dosage = "10mg",
-                protocolUUID = protocolUuid),
-            ProtocolDrug(
-                UUID.fromString("8a0d86b2-9cfa-49cf-8152-956d6ab0545e"),
+                UUID.fromString("f951b82d-2198-4fed-a55c-e2be67894009"),
                 name = "Telmisartan",
                 rxNormCode = "rxnormcode-2",
-                dosage = "40mg",
+                dosages = listOf("40mg", "80mg"),
                 protocolUUID = protocolUuid),
             ProtocolDrug(
-                UUID.fromString("25c12696-2994-440b-875f-be85b5bf39f0"),
-                name = "Telmisartan",
-                rxNormCode = "rxnormcode-2",
-                dosage = "5mg",
-                protocolUUID = protocolUuid),
-            ProtocolDrug(
-                UUID.fromString("ff32ca01-ba99-40b7-bc48-18b2aa87486d"),
+                UUID.fromString("43f7ea6d-4002-4aa8-b440-c5b4798fb78c"),
                 name = "Chlorthalidone",
                 rxNormCode = "rxnormcode-3",
-                dosage = "5mg",
-                protocolUUID = protocolUuid),
-            ProtocolDrug(
-                UUID.fromString("cc891917-ae67-4d80-91f1-fcc1ac5a54f6"),
-                name = "Chlorthalidone",
-                rxNormCode = "rxnormcode-3",
-                dosage = "5mg",
+                dosages = listOf("12.5mg", "25mg"),
                 protocolUUID = protocolUuid)
         )
     ))

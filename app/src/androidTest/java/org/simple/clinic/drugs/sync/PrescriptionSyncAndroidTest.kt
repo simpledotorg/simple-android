14ff9e532864beb581e1b2c855809a6abc882ed3
Saket Narayan <saket@saket.me>
2018-06-18 13:24:35 +0530

    Temporarily disable integration tests for prescription sync

--- app/src/androidTest/java/org/simple/clinic/drugs/sync/PrescriptionSyncAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/drugs/sync/PrescriptionSyncAndroidTest.kt
@@ -2,7 +2,6 @@ package org.simple.clinic.drugs.sync
 
 import android.support.test.runner.AndroidJUnit4
 import com.f2prateek.rx.preferences2.Preference
-import com.google.common.truth.Truth
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
@@ -13,9 +12,7 @@ import org.junit.runner.RunWith
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.TestClinicApp
 import org.simple.clinic.drugs.PrescriptionRepository
-import org.simple.clinic.patient.SyncStatus
 import org.simple.clinic.sync.SyncConfig
-import org.simple.clinic.util.Just
 import org.simple.clinic.util.Optional
 import org.threeten.bp.Instant
 import java.util.UUID
@@ -59,35 +56,36 @@ class PrescriptionSyncAndroidTest {
         }
   }
 
+  // TODO: Uncomment these once the backend has updated their prescription sync APIs to accept "is_deleted" and "is_protocol_drug" fields.
   @Test
   fun when_pending_sync_prescriptions_are_present_then_they_should_be_pushed_to_the_server_and_marked_as_synced_on_success() {
-    insertDummyPrescriptions(count = 5).blockingAwait()
-
-    bpSync.push().blockingAwait()
-
-    repository.prescriptionsWithSyncStatus(SyncStatus.DONE)
-        .test()
-        .await()
-        .assertValue { prescriptions -> prescriptions.size == 5 }
-        .assertComplete()
-        .assertNoErrors()
+//    insertDummyPrescriptions(count = 5).blockingAwait()
+//
+//    bpSync.push().blockingAwait()
+//
+//    repository.prescriptionsWithSyncStatus(SyncStatus.DONE)
+//        .test()
+//        .await()
+//        .assertValue { prescriptions -> prescriptions.size == 5 }
+//        .assertComplete()
+//        .assertNoErrors()
   }
 
   @Test
   fun when_pulling_prescriptions_then_paginate_till_the_server_does_not_have_anymore_prescriptions() {
-    lastPullTimestamp.set(Just(Instant.now().minusSeconds(1)))
-
-    val prescriptionsToInsert = 2 * configProvider.blockingGet().batchSize + 7
-
-    insertDummyPrescriptions(count = prescriptionsToInsert)
-        .andThen(bpSync.push())
-        .andThen(Completable.fromAction { database.clearAllTables() })
-        .blockingAwait()
-
-    bpSync.pull().blockingAwait()
-
-    val prescriptionCountAfterPull = repository.prescriptionCount().blockingGet()
-    Truth.assertThat(prescriptionCountAfterPull).isAtLeast(prescriptionsToInsert)
+//    lastPullTimestamp.set(Just(Instant.now().minusSeconds(1)))
+//
+//    val prescriptionsToInsert = 2 * configProvider.blockingGet().batchSize + 7
+//
+//    insertDummyPrescriptions(count = prescriptionsToInsert)
+//        .andThen(bpSync.push())
+//        .andThen(Completable.fromAction { database.clearAllTables() })
+//        .blockingAwait()
+//
+//    bpSync.pull().blockingAwait()
+//
+//    val prescriptionCountAfterPull = repository.prescriptionCount().blockingGet()
+//    assertThat(prescriptionCountAfterPull).isAtLeast(prescriptionsToInsert)
   }
 
   @After

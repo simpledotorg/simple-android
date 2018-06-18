a133aea20efe37d14e0d19f1604f1b12b89e658d
Srihari Sriraman <srihari@nilenso.com>
2018-06-18 13:47:40 +0530

    Ensure release build works
    - Disable proguard for release build
    - Add a DebugClinic application, configure debug build to use that
    - Only use Stetho in debug builds

---
+++ app/src/debug/java/org/simple/clinic/DebugClinic.kt
@@ -0,0 +1,33 @@
+package org.simple.clinic
+
+import android.annotation.SuppressLint
+import com.facebook.stetho.Stetho
+import com.tspoon.traceur.Traceur
+import org.simple.clinic.di.AppComponent
+import org.simple.clinic.di.AppModule
+import org.simple.clinic.di.DaggerAppComponent
+import org.simple.clinic.sync.SyncScheduler
+import timber.log.Timber
+import javax.inject.Inject
+
+@SuppressLint("Registered")
+class DebugClinic : Clinic() {
+
+  @Inject
+  lateinit var syncScheduler: SyncScheduler
+
+  override fun onCreate() {
+    super.onCreate()
+
+    Timber.plant(Timber.DebugTree())
+    Traceur.enableLogging()
+    Stetho.initializeWithDefaults(this)
+    syncScheduler.schedule().subscribe()
+  }
+
+  override fun buildDaggerGraph(): AppComponent {
+    return DaggerAppComponent.builder()
+        .appModule(AppModule(this))
+        .build()
+  }
+}

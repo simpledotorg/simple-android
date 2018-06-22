e88908d9095b766b21fc66174cf804f41a7df7bc
Pratul Kalia <pratul@uncommon.is>
2018-06-22 21:29:56 +0530

    Make LoginPinScreenController tests pass

--- app/src/test/java/org/simple/clinic/login/pin/LoginPinScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/login/pin/LoginPinScreenControllerTest.kt
@@ -20,14 +20,25 @@ class LoginPinScreenControllerTest {
 
   private val screen = mock<LoginPinScreen>()
   private val uiEvents = PublishSubject.create<UiEvent>()
-  private val controller = LoginPinScreenController()
   private val userSession = mock<UserSession>()
+
+  lateinit var controller: LoginPinScreenController
 
   @Before
   fun setUp() {
-    uiEvents
-        .compose(controller)
-        .subscribe { uiChange -> uiChange(screen) }
+    controller = LoginPinScreenController(userSession)
+
+    uiEvents.compose(controller).subscribe { uiChange -> uiChange(screen) }
+  }
+
+  @Test
+  fun `when screen starts, show phone number`() {
+    whenever(userSession.ongoingLoginEntry()).thenReturn(Single.just(OngoingLoginEntry(otp = "3444", phoneNumber = "123")))
+
+    uiEvents.onNext(PinScreenCreated())
+
+    verify(userSession).ongoingLoginEntry()
+    verify(screen).showPhoneNumber("123")
   }
 
   @Test
@@ -44,24 +55,26 @@ class LoginPinScreenControllerTest {
   fun `if pin is not empty, and submit is clicked, make login api call, and open home screen`() {
     val ongoingEntry = OngoingLoginEntry(otp = "123", phoneNumber = "99999")
     whenever(userSession.ongoingLoginEntry()).thenReturn(Single.just(ongoingEntry))
+    whenever(userSession.saveOngoingLoginEntry(any())).thenReturn(Completable.complete())
     whenever(userSession.login()).thenReturn(Single.just(LoginResult.Success()))
 
     uiEvents.onNext(PinTextChanged("0000"))
     uiEvents.onNext(PinSubmitClicked())
 
     val inOrder = inOrder(userSession, screen)
-    inOrder.verify(userSession).ongoingLoginEntry()
-    inOrder.verify(screen).showProgressBar()
-    inOrder.verify(userSession).saveOngoingLoginEntry(OngoingLoginEntry(otp = "123", phoneNumber = "99999", pin = "0000"))
     inOrder.verify(userSession).login()
-    inOrder.verify(screen).hideProgressBar()
+    inOrder.verify(userSession).ongoingLoginEntry()
+    inOrder.verify(userSession).saveOngoingLoginEntry(OngoingLoginEntry(otp = "123", phoneNumber = "99999", pin = "0000"))
     inOrder.verify(screen).openHomeScreen()
+    inOrder.verify(screen).showProgressBar()
+    inOrder.verify(screen).hideProgressBar()
   }
 
   @Test
   fun `if login api call throws any errors, show errors`() {
+    val ongoingEntry = OngoingLoginEntry(otp = "123", phoneNumber = "99999")
+    whenever(userSession.ongoingLoginEntry()).thenReturn(Single.just(ongoingEntry))
     whenever(userSession.saveOngoingLoginEntry(any())).thenReturn(Completable.complete())
-
     whenever(userSession.login())
         .thenReturn(Single.just(LoginResult.NetworkError()))
         .thenReturn(Single.just(LoginResult.ServerError("Earth dead")))
@@ -69,19 +82,11 @@ class LoginPinScreenControllerTest {
 
     uiEvents.onNext(PinTextChanged("0000"))
     uiEvents.onNext(PinSubmitClicked())
+    uiEvents.onNext(PinSubmitClicked())
+    uiEvents.onNext(PinSubmitClicked())
 
     verify(screen).showNetworkError()
     verify(screen).showServerError("Earth dead")
     verify(screen).showUnexpectedError()
-  }
-
-  @Test
-  fun `when screen starts, show phone number`() {
-    whenever(userSession.ongoingLoginEntry()).thenReturn(Single.just(OngoingLoginEntry(otp = "3444", phoneNumber = "123")))
-
-    uiEvents.onNext(PinScreenCreated())
-
-    verify(userSession).ongoingLoginEntry()
-    verify(screen).showPhoneNumber("123")
   }
 }

41fbd9dd7495ad62b9b8b766e747cb2db3cc8399
Pratul Kalia <pratul@uncommon.is>
2018-06-26 15:01:54 +0530

    Add ability to pull facilities from API

--- app/src/main/java/org/simple/clinic/drugs/sync/PrescriptionSyncApiV1.kt
+++ app/src/main/java/org/simple/clinic/drugs/sync/PrescriptionSyncApiV1.kt
@@ -19,6 +19,7 @@ interface PrescriptionSyncApiV1 {
       @Body body: PrescriptionPushRequest
   ): Single<DataPushResponse>
 
+  // TODO: Remove this function, and reuse other pull function with nullable lastPullTimestamp
   @GET("$version/prescription_drugs/sync")
   fun pull(
       @Query("limit") recordsToPull: Int

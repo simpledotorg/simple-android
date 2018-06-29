71d810192892683729cf4592f0e7c2958585ee00
Saket Narayan <saket@saket.me>
2018-06-29 16:46:20 +0200

    Add tests for RelativeTimestampGenerator

--- app/src/main/java/org/simple/clinic/summary/RelativeTimestampGenerator.kt
+++ app/src/main/java/org/simple/clinic/summary/RelativeTimestampGenerator.kt
@@ -3,29 +3,31 @@ package org.simple.clinic.summary
 import android.content.Context
 import org.simple.clinic.R
 import org.threeten.bp.Instant
-import org.threeten.bp.LocalDate
-import org.threeten.bp.Period
+import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneOffset
 import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.temporal.ChronoUnit
 import java.util.Locale
 import javax.inject.Inject
 
-// TODO: Tests.
 class RelativeTimestampGenerator @Inject constructor() {
 
-  private fun generate(today: LocalDate, time: Instant): RelativeTimestamp {
-    val timeDate = time.atZone(ZoneOffset.UTC).toLocalDate()
+  fun generate(today: LocalDateTime, time: Instant): RelativeTimestamp {
+    val dateTime = time.atZone(ZoneOffset.UTC).toLocalDateTime()
+
+    val todayAtMidnight = today.truncatedTo(ChronoUnit.DAYS)
+    val yesterdayAtMidnight = todayAtMidnight.minusDays(1)
 
     return when {
-      timeDate.dayOfMonth == today.dayOfMonth -> Today()
-      timeDate.dayOfMonth == today.minusDays(1).dayOfMonth -> Yesterday()
-      timeDate > timeDate.minusMonths(6) -> WithinSixMonths(Period.between(today, timeDate))
+      dateTime > todayAtMidnight -> Today()
+      dateTime > yesterdayAtMidnight -> Yesterday()
+      dateTime > today.minusMonths(6) -> WithinSixMonths(ChronoUnit.DAYS.between(dateTime, today))
       else -> OlderThanSixMonths(time)
     }
   }
 
   fun generate(time: Instant): RelativeTimestamp {
-    val today = LocalDate.now(ZoneOffset.UTC)
+    val today = LocalDateTime.now(ZoneOffset.UTC)
     return generate(today, time)
   }
 }
@@ -46,9 +48,9 @@ class Yesterday : RelativeTimestamp() {
   }
 }
 
-class WithinSixMonths(private val period: Period) : RelativeTimestamp() {
+data class WithinSixMonths(private val daysBetween: Long) : RelativeTimestamp() {
   override fun displayText(context: Context): String {
-    return context.getString(R.string.timestamp_days, period.days)
+    return context.getString(R.string.timestamp_days, daysBetween)
   }
 }
 

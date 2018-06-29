dfb0adc5360daec5195538c7a433de4ddd2c8737
Saket Narayan <saket@saket.me>
2018-06-29 06:41:52 +0200

    Avoid syncing of any data while the user is logged out

--- app/src/main/java/org/simple/clinic/sync/SyncWorker.kt
+++ app/src/main/java/org/simple/clinic/sync/SyncWorker.kt
@@ -1,12 +1,17 @@
 package org.simple.clinic.sync
 
 import androidx.work.Worker
+import com.f2prateek.rx.preferences2.Preference
 import io.reactivex.Completable
 import io.reactivex.Single
 import org.simple.clinic.ClinicApp
 import org.simple.clinic.bp.sync.BloodPressureSync
 import org.simple.clinic.drugs.sync.PrescriptionSync
 import org.simple.clinic.patient.sync.PatientSync
+import org.simple.clinic.user.LoggedInUser
+import org.simple.clinic.util.None
+import org.simple.clinic.util.Optional
+import timber.log.Timber
 import javax.inject.Inject
 
 class SyncWorker : Worker() {
@@ -24,8 +29,16 @@ class SyncWorker : Worker() {
   @Inject
   lateinit var prescriptionSync: PrescriptionSync
 
+  @Inject
+  lateinit var loggedInUserPref: Preference<Optional<LoggedInUser>>
+
   override fun doWork(): WorkerResult {
     ClinicApp.appComponent.inject(this)
+
+    if (loggedInUserPref.get() === None) {
+      Timber.i("User isn't logged in yet. Skipping sync.")
+      return WorkerResult.SUCCESS
+    }
 
     return Completable.mergeArrayDelayError(patientSync.sync(), bloodPressureSync.sync(), prescriptionSync.sync())
         .onErrorComplete()

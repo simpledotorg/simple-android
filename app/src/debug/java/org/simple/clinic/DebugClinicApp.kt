7c9f46c13be5fca7d4d17a47f3ac852d393bc608
Saket Narayan <saket@saket.me>
2018-06-29 11:11:25 +0200

    Add a debug notification for force syncing data

--- app/src/debug/java/org/simple/clinic/DebugClinicApp.kt
+++ app/src/debug/java/org/simple/clinic/DebugClinicApp.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import com.facebook.stetho.Stetho
 import com.tspoon.traceur.Traceur
 import org.simple.clinic.di.AppComponent
@@ -8,6 +9,7 @@ import org.simple.clinic.di.AppModule
 import org.simple.clinic.di.DaggerDebugAppComponent
 import org.simple.clinic.di.DebugAppComponent
 import org.simple.clinic.sync.SyncScheduler
+import org.simple.clinic.widgets.SimpleActivityLifecycleCallbacks
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -17,16 +19,35 @@ class DebugClinicApp : ClinicApp() {
   @Inject
   lateinit var syncScheduler: SyncScheduler
 
+  companion object {
+    fun appComponent(): DebugAppComponent {
+      return ClinicApp.appComponent as DebugAppComponent
+    }
+  }
+
   override fun onCreate() {
     super.onCreate()
-
-    (appComponent as DebugAppComponent).inject(this)
+    appComponent().inject(this)
 
     Timber.plant(Timber.DebugTree())
     Traceur.enableLogging()
     Stetho.initializeWithDefaults(this)
 
     syncScheduler.schedule().subscribe()
+
+    registerActivityLifecycleCallbacks(object : SimpleActivityLifecycleCallbacks() {
+      override fun onActivityStarted(activity: Activity) {
+        if (activity is TheActivity) {
+          DebugNotificationService.show(activity)
+        }
+      }
+
+      override fun onActivityStopped(activity: Activity) {
+        if (activity is TheActivity) {
+          DebugNotificationService.stop(activity)
+        }
+      }
+    })
   }
 
   override fun buildDaggerGraph(): AppComponent {
--- app/src/debug/java/org/simple/clinic/di/DebugAppComponent.kt
+++ app/src/debug/java/org/simple/clinic/di/DebugAppComponent.kt
@@ -2,11 +2,12 @@ package org.simple.clinic.di
 
 import dagger.Component
 import org.simple.clinic.DebugClinicApp
+import org.simple.clinic.DebugNotificationActionReceiver
 
 @AppScope
 @Component(modules = [AppModule::class])
 interface DebugAppComponent : AppComponent {
 
   fun inject(target: DebugClinicApp)
-
+  fun inject(target: DebugNotificationActionReceiver)
 }

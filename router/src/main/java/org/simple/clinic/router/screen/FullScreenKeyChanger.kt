e3449baecd018f2cf855e75f7d740f7640aab035
Saket Narayan <saket@saket.me>
2018-07-19 14:26:08 +0530

    Add basic screen change animations

--- router/src/main/java/org/simple/clinic/router/screen/FullScreenKeyChanger.kt
+++ router/src/main/java/org/simple/clinic/router/screen/FullScreenKeyChanger.kt
@@ -2,8 +2,10 @@ package org.simple.clinic.router.screen
 
 import android.app.Activity
 import android.support.annotation.IdRes
+import android.support.v4.view.animation.FastOutSlowInInterpolator
+import android.view.View
 import android.view.ViewGroup
-
+import flow.Direction
 import flow.KeyChanger
 
 /**
@@ -26,5 +28,73 @@ class FullScreenKeyChanger(
 
   override fun screenLayoutContainer(): ViewGroup {
     return activity.findViewById(screenLayoutContainerRes)
+  }
+
+  override fun animate(
+      outgoingKey: FullScreenKey?,
+      outgoingView: View?,
+      incomingKey: FullScreenKey,
+      incomingView: View,
+      direction: flow.Direction,
+      onCompleteListener: () -> Unit
+  ) {
+    if (outgoingView == null || direction == flow.Direction.REPLACE) {
+      onCompleteListener.invoke()
+      return
+    }
+
+    val scaleChange = 0.05f
+    val duration = 200L
+    val interpolator = FastOutSlowInInterpolator()
+
+    if (direction == Direction.FORWARD || direction == Direction.REPLACE) {
+      outgoingView.animate()
+          .scaleX(1f - scaleChange)
+          .scaleY(1f - scaleChange)
+          .alpha(0f)
+          .setDuration(duration)
+          .setInterpolator(interpolator)
+          .start()
+
+    } else {
+      outgoingView.animate()
+          .scaleX(1f + scaleChange)
+          .scaleY(1f + scaleChange)
+          .alpha(0f)
+          .setDuration(duration)
+          .setInterpolator(interpolator)
+          .start()
+    }
+
+    if (direction == Direction.FORWARD || direction == Direction.REPLACE) {
+      incomingView.scaleX = 1f + scaleChange
+      incomingView.scaleY = 1f + scaleChange
+      incomingView.alpha = 0f
+      incomingView.animate()
+          .scaleX(1f)
+          .scaleY(1f)
+          .alpha(1f)
+          .setDuration(duration)
+          .setInterpolator(interpolator)
+          .withEndAction {
+            onCompleteListener.invoke()
+          }
+          .start()
+
+    } else {
+      incomingView.scaleX = 1f - scaleChange
+      incomingView.scaleY = 1f - scaleChange
+      incomingView.alpha = 0f
+      incomingView.animate()
+          .scaleX(1f)
+          .scaleY(1f)
+          .alpha(1f)
+          .setDuration(duration)
+          .setInterpolator(interpolator)
+          .withEndAction {
+            onCompleteListener.invoke()
+          }
+          .start()
+    }
   }
 }

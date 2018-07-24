6a7a6d00fc9191fbf60391ef2135f4f2a216f3fb
Saket Narayan <saket@saket.me>
2018-07-24 15:47:19 +0530

    Throw an explicit error if back-press interceptor is not set in LinearLayoutWithPreImeKeyEventListener

--- app/src/main/java/org/simple/clinic/bp/entry/LinearLayoutWithPreImeKeyEventListener.kt
+++ app/src/main/java/org/simple/clinic/bp/entry/LinearLayoutWithPreImeKeyEventListener.kt
@@ -7,7 +7,9 @@ import android.widget.LinearLayout
 
 class LinearLayoutWithPreImeKeyEventListener(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
-  lateinit var backKeyPressInterceptor: () -> Unit
+  var backKeyPressInterceptor: () -> Unit = {
+    throw AssertionError("Back-press interceptor not set")
+  }
 
   override fun dispatchKeyEventPreIme(event: KeyEvent): Boolean {
     return if (event.action == KeyEvent.ACTION_UP && event.keyCode == KeyEvent.KEYCODE_BACK) {

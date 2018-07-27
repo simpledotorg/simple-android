35481206d4d6e4823779b97ba90ba07551bbbe0c
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-07-27 15:54:18 +0530

    Set onboarding complete flag when the user clicks get started and move to the registration screen

--- app/src/main/java/org/simple/clinic/onboarding/OnboardingScreen.kt
+++ app/src/main/java/org/simple/clinic/onboarding/OnboardingScreen.kt
@@ -6,13 +6,17 @@ import android.util.AttributeSet
 import android.widget.Button
 import android.widget.RelativeLayout
 import android.widget.TextView
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotterknife.bindView
 import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.registration.phone.RegistrationPhoneScreen
+import org.simple.clinic.router.screen.RouterDirection
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.util.clamp
 import javax.inject.Inject
-
 
 class OnboardingScreen(context: Context, attributeSet: AttributeSet) : RelativeLayout(context, attributeSet) {
 
@@ -24,25 +28,44 @@ class OnboardingScreen(context: Context, attributeSet: AttributeSet) : RelativeL
   @Inject
   lateinit var router: ScreenRouter
 
-  private val appLogo by bindView<TextView>(R.id.onboarding_logo)
-  private val scrollingContainer by bindView<NestedScrollView>(R.id.onboarding_scrolling_container)
+  @Inject
+  lateinit var controller: OnboardingScreenController
+
+  private val appLogoTextView by bindView<TextView>(R.id.onboarding_logo)
+  private val scrollView by bindView<NestedScrollView>(R.id.onboarding_scrolling_container)
   private val getStartedButton by bindView<Button>(R.id.onboarding_get_started)
 
   override fun onFinishInflate() {
     super.onFinishInflate()
 
-    if (isInEditMode) return
+    if (isInEditMode) {
+      return
+    }
 
     TheActivity.component.inject(this)
 
-    fadeLogoWithContainerScroll()
+    fadeLogoWithContentScroll()
+
+    getStartedClicks()
+        .observeOn(Schedulers.io())
+        .compose(controller)
+        .observeOn(AndroidSchedulers.mainThread())
+        .takeUntil(RxView.detaches(this))
+        .subscribe { uiChange -> uiChange(this) }
   }
 
-  private fun fadeLogoWithContainerScroll() {
-    scrollingContainer.setOnScrollChangeListener { _: NestedScrollView, _: Int, scrollY: Int, _: Int, _: Int ->
-      val distanceBetweenLogoAndScrollTop = appLogo.top - scrollY.toFloat()
-      val opacity = (distanceBetweenLogoAndScrollTop / appLogo.top).clamp(0F, 1F)
-      appLogo.alpha = opacity
+  private fun fadeLogoWithContentScroll() {
+    scrollView.setOnScrollChangeListener { _: NestedScrollView, _: Int, scrollY: Int, _: Int, _: Int ->
+      val distanceBetweenLogoAndScrollTop = appLogoTextView.top - scrollY.toFloat()
+      val opacity = (distanceBetweenLogoAndScrollTop / appLogoTextView.top).clamp(0F, 1F)
+      appLogoTextView.alpha = opacity
     }
+  }
+
+  private fun getStartedClicks() =
+      RxView.clicks(getStartedButton).map { OnboardingGetStartedClicked() }
+
+  fun moveToRegistrationScreen() {
+    router.clearHistoryAndPush(RegistrationPhoneScreen.KEY, RouterDirection.FORWARD)
   }
 }
---
+++ app/src/test/java/org/simple/clinic/onboarding/OnboardingScreenControllerTest.kt
@@ -0,0 +1,35 @@
+package org.simple.clinic.onboarding
+
+import com.f2prateek.rx.preferences2.Preference
+import com.nhaarman.mockito_kotlin.eq
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import io.reactivex.subjects.PublishSubject
+import org.junit.Before
+import org.junit.Test
+import org.simple.clinic.widgets.UiEvent
+
+class OnboardingScreenControllerTest {
+
+  private val screen = mock<OnboardingScreen>()
+  private val hasUserCompletedOnboarding = mock<Preference<Boolean>>()
+
+  private val uiEvents = PublishSubject.create<UiEvent>()
+  lateinit var controller: OnboardingScreenController
+
+  @Before
+  fun setUp() {
+    controller = OnboardingScreenController(hasUserCompletedOnboarding)
+
+    uiEvents.compose(controller).subscribe { uiChange -> uiChange(screen) }
+  }
+
+  @Test
+  fun `when the onboarding action is done, it should set the preference and open the registrartion screen`() {
+    uiEvents.onNext(OnboardingGetStartedClicked())
+
+    verify(hasUserCompletedOnboarding).set(eq(true))
+    verify(screen).moveToRegistrationScreen()
+  }
+
+}

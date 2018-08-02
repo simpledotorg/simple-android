9d7b6fc526c23fc8446712cdbda258793d132907
Saket Narayan <saket@saket.me>
2018-08-02 17:33:05 +0530

    Pre-fill existing user details in registration-name screen

--- app/src/main/java/org/simple/clinic/registration/name/RegistrationNameScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/registration/name/RegistrationNameScreenEvents.kt
@@ -2,6 +2,8 @@ package org.simple.clinic.registration.name
 
 import org.simple.clinic.widgets.UiEvent
 
+class RegistrationFullNameScreenCreated : UiEvent
+
 data class RegistrationFullNameTextChanged(val fullName: String) : UiEvent
 
 class RegistrationFullNameDoneClicked : UiEvent

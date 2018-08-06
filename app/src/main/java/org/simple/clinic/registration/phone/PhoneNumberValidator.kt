17d83f8711d8c8678fde1d5b4c18ff8dae98bb88
Saket Narayan <saket@saket.me>
2018-08-06 16:16:51 +0530

    Replace 'pin' with 'PIN' in strings.xml

--- app/src/main/java/org/simple/clinic/registration/phone/PhoneNumberValidator.kt
+++ app/src/main/java/org/simple/clinic/registration/phone/PhoneNumberValidator.kt
@@ -2,12 +2,11 @@ package org.simple.clinic.registration.phone
 
 import javax.inject.Inject
 
+private const val LENGTH_OF_INDIAN_PHONE_NUMBER = 10
+
 class PhoneNumberValidator @Inject constructor() {
 
-  val LENGTH_OF_INDIAN_PHONE_NUMBER = 10
-
   fun isValid(number: String): Boolean {
-    // Only Indian phone numbers are accepted for now.
     return number.length == LENGTH_OF_INDIAN_PHONE_NUMBER
   }
 }

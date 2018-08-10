0b989037ff9791a45a96c467e7d53db9b45d65d2
Saket Narayan <saket@saket.me>
2018-08-10 15:18:48 +0530

    Avoid checking for location permission on screen start during registration

--- app/src/main/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreenEvents.kt
@@ -4,5 +4,3 @@ import org.simple.clinic.util.RuntimePermissionResult
 import org.simple.clinic.widgets.UiEvent
 
 data class LocationPermissionChanged(val result: RuntimePermissionResult) : UiEvent
-
-class RegistrationLocationPermissionScreenCreated : UiEvent

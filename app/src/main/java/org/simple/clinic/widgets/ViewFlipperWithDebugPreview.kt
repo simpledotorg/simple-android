cb722ccbbe4d3ce9245a6fdbf0d417ab77f30479
Saket Narayan <saket@saket.me>
2018-08-16 15:19:43 +0530

    Add ViewFlipperWithDebugPreview

---
+++ app/src/main/java/org/simple/clinic/widgets/ViewFlipperWithDebugPreview.kt
@@ -0,0 +1,32 @@
+package org.simple.clinic.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ViewFlipper
+
+import org.simple.clinic.R
+
+/** Exposes a way to set the displayed child in layout preview. */
+class ViewFlipperWithDebugPreview(context: Context, attrs: AttributeSet) : ViewFlipper(context, attrs) {
+
+  private var childToDisplayPostInflate: Int = 0
+
+  init {
+    if (isInEditMode) {
+      val attributes = context.obtainStyledAttributes(attrs, R.styleable.ViewFlipperWithDebugPreview)
+      childToDisplayPostInflate = attributes.getInt(R.styleable.ViewFlipperWithDebugPreview_debug_displayedChild, 0)
+      attributes.recycle()
+    }
+  }
+
+  override fun onFinishInflate() {
+    super.onFinishInflate()
+
+    if (isInEditMode) {
+      if (childToDisplayPostInflate > childCount) {
+        throw IllegalStateException("childToDisplayPostInflate is greater than child count")
+      }
+      displayedChild = childToDisplayPostInflate
+    }
+  }
+}

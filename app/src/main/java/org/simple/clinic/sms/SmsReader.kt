e0bc208647bf216f5569a9157b48ee6213341efa
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-08-16 14:11:14 +0530

    Add initial implementation of SmsReaderImpl

--- app/src/main/java/org/simple/clinic/sms/SmsReader.kt
+++ app/src/main/java/org/simple/clinic/sms/SmsReader.kt
@@ -1,15 +1,19 @@
 package org.simple.clinic.sms
 
-import io.reactivex.Single
+import io.reactivex.Observable
 
 sealed class SmsReadResult {
 
   data class Success(val string: String) : SmsReadResult()
 
+<<<<<<< HEAD
   data class Error(val cause: Throwable?) : SmsReadResult()
+=======
+  data class SmsReadError(val cause: Throwable? = null) : SmsReadResult()
+>>>>>>> Add initial implementation of SmsReaderImpl
 }
 
 interface SmsReader {
 
-  fun waitForSms(): Single<SmsReadResult>
+  fun waitForSms(): Observable<SmsReadResult>
 }

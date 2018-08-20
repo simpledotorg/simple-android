53f1c943a0f768eb9f30f4ae44715e954a726124
Saket Narayan <saket@saket.me>
2018-08-20 12:26:25 +0530

    Hide user's approval status after 24h of getting approved

---
+++ app/src/main/java/org/simple/clinic/home/patients/PatientsModule.kt
@@ -0,0 +1,19 @@
+package org.simple.clinic.home.patients
+
+import com.f2prateek.rx.preferences2.Preference
+import com.f2prateek.rx.preferences2.RxSharedPreferences
+import dagger.Module
+import dagger.Provides
+import org.simple.clinic.util.InstantRxPreferencesConverter
+import org.threeten.bp.Instant
+import javax.inject.Named
+
+@Module
+class PatientsModule {
+
+  @Provides
+  @Named("approval_status_changed_at")
+  fun approvalStatusChangeTimestamp(rxSharedPrefs: RxSharedPreferences): Preference<Instant> {
+    return rxSharedPrefs.getObject("approval_status_changed_at", Instant.EPOCH, InstantRxPreferencesConverter())
+  }
+}

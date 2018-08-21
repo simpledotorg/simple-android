5e07588f8743fbd66328c4d0539331424ba36603
Saket Narayan <saket@saket.me>
2018-08-21 15:05:51 +0530

    Allow 4-digit phone numbers for registration in debug build

--- app/src/test/java/org/simple/clinic/registration/phone/PhoneNumberValidatorTest.kt
+++ app/src/test/java/org/simple/clinic/registration/phone/PhoneNumberValidatorTest.kt
@@ -7,7 +7,7 @@ class PhoneNumberValidatorTest {
 
   @Test
   fun `validate Indian phone numbers`() {
-    val validator = PhoneNumberValidator()
+    val validator = IndianPhoneNumberValidator()
     assertThat(validator.isValid("123456789")).isFalse()
     assertThat(validator.isValid("1234567890")).isTrue()
   }

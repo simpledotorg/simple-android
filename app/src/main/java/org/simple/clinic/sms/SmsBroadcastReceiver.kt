aded125432a86682b59f514a16fa421167defe9b
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-08-21 15:03:03 +0530

    Change SmsBroadcastReceiver to handle new OTP length

--- app/src/main/java/org/simple/clinic/sms/SmsBroadcastReceiver.kt
+++ app/src/main/java/org/simple/clinic/sms/SmsBroadcastReceiver.kt
@@ -9,9 +9,12 @@ import com.google.android.gms.common.api.Status
 import org.simple.clinic.ClinicApp
 import timber.log.Timber
 
+private const val OTP_START_INDEX = 4
+private const val OTP_LENGTH = 6
+
 class SmsBroadcastReceiver : BroadcastReceiver() {
 
-  val userSession = ClinicApp.appComponent.userSession()
+  private val userSession = ClinicApp.appComponent.userSession()
 
   override fun onReceive(context: Context, intent: Intent) {
     val extras = intent.extras
@@ -22,7 +25,7 @@ class SmsBroadcastReceiver : BroadcastReceiver() {
         val message = extras[SmsRetriever.EXTRA_SMS_MESSAGE] as String
 
         // I don't like magic numbers, but I am not sure of the best place to put this since the message is hard coded
-        val otp = message.substring(4, 8)
+        val otp = message.substring(OTP_START_INDEX, OTP_START_INDEX + OTP_LENGTH)
 
         // TODO: Remove this log once we actually schedule the call to
         Timber.d("Received OTP: $otp, User Session: $userSession")

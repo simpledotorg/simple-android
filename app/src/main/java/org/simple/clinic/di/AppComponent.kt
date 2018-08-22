ea6ca80a740c6a1fbf3d7e073f77fe6b2cd6a1fe
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-08-22 12:48:58 +0530

    Rename SmsBroadcastReceiver -> OtpSmsReceiver

--- app/src/main/java/org/simple/clinic/di/AppComponent.kt
+++ app/src/main/java/org/simple/clinic/di/AppComponent.kt
@@ -6,7 +6,7 @@ import org.simple.clinic.activity.TheActivityComponent
 import org.simple.clinic.facility.FacilitySync
 import org.simple.clinic.newentry.clearbutton.ClearFieldImageButton
 import org.simple.clinic.registration.RegistrationWorker
-import org.simple.clinic.sms.SmsBroadcastReceiver
+import org.simple.clinic.sms.OtpSmsReceiver
 import org.simple.clinic.sync.SyncWorker
 import org.simple.clinic.user.UserSession
 import javax.inject.Scope
@@ -19,7 +19,7 @@ interface AppComponent {
   fun inject(target: SyncWorker)
   fun inject(target: ClearFieldImageButton)
   fun inject(target: RegistrationWorker)
-  fun inject(target: SmsBroadcastReceiver)
+  fun inject(target: OtpSmsReceiver)
 
   fun activityComponentBuilder(): TheActivityComponent.Builder
 

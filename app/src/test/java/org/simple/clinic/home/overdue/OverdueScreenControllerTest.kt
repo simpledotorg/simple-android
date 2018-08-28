e663975c57bf3a0609c712c1decf26918b23b947
Sanchita Agarwal <sanchita@uncommon.is>
2018-08-28 18:18:30 +0530

    Add empty state to the overdue list

---
+++ app/src/test/java/org/simple/clinic/home/overdue/OverdueScreenControllerTest.kt
@@ -0,0 +1,35 @@
+package org.simple.clinic.home.overdue
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import io.reactivex.subjects.PublishSubject
+import org.junit.Before
+import org.junit.Test
+import org.simple.clinic.widgets.UiEvent
+
+class OverdueScreenControllerTest {
+
+  private val screen = mock<OverdueScreen>()
+  private val uiEvents = PublishSubject.create<UiEvent>()
+
+  lateinit var controller: OverdueScreenController
+
+  @Before
+  fun setUp() {
+    controller = OverdueScreenController()
+
+    uiEvents.compose(controller).subscribe { uiChange -> uiChange(screen) }
+  }
+
+  @Test
+  fun `when screen is created, show list of overdue patients`() {
+    uiEvents.onNext(OverdueScreenCreated())
+
+    verify(screen).setupOverdueList()
+  }
+
+  @Test
+  fun `when overdue list is empty, show message to user`() {
+
+  }
+}

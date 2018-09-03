c1ce762a5d8991522574f960aa2b067bfd4265ba
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-09-03 09:43:52 +0530

    Associate user ID with Analytics on cold start

--- app/src/main/java/org/simple/clinic/ClinicApp.kt
+++ app/src/main/java/org/simple/clinic/ClinicApp.kt
@@ -2,9 +2,14 @@ package org.simple.clinic
 
 import android.support.multidex.MultiDexApplication
 import com.gabrielittner.threetenbp.LazyThreeTen
+import io.reactivex.schedulers.Schedulers
 import io.sentry.Sentry
 import io.sentry.android.AndroidSentryClientFactory
+import org.simple.clinic.analytics.Analytics
 import org.simple.clinic.di.AppComponent
+import org.simple.clinic.user.User
+import org.simple.clinic.util.Just
+import timber.log.Timber
 
 abstract class ClinicApp : MultiDexApplication() {
 
@@ -28,4 +33,18 @@ abstract class ClinicApp : MultiDexApplication() {
   }
 
   abstract fun buildDaggerGraph(): AppComponent
+
+  protected fun keepUserIdUpdatedInAnalytics() {
+    appComponent.userSession()
+        .loggedInUser()
+        .subscribeOn(Schedulers.io())
+        .filter { it is Just<User> }
+        .map { (user) -> user!! }
+        .filter { it.loggedInStatus == User.LoggedInStatus.LOGGED_IN }
+        .subscribe({
+          Analytics.setUserId(it.uuid)
+        }, {
+          Timber.e(it, "Could not update user ID in analytics")
+        })
+  }
 }

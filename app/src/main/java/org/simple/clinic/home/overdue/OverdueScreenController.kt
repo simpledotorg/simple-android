6186d55e8d18ac36dc022d8e008560b5a03f4e8c
Sanchita Agarwal <sanchita@uncommon.is>
2018-09-04 17:36:41 +0530

    Schedule an appointment every time a BP is saved

--- app/src/main/java/org/simple/clinic/home/overdue/OverdueScreenController.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/OverdueScreenController.kt
@@ -4,10 +4,12 @@ import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 import io.reactivex.rxkotlin.ofType
 import org.simple.clinic.overdue.AppointmentRepository
+import org.simple.clinic.patient.Age
 import org.simple.clinic.widgets.UiEvent
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
+import org.threeten.bp.Period
 import org.threeten.bp.ZoneOffset
 import org.threeten.bp.temporal.ChronoUnit.DAYS
 import javax.inject.Inject
@@ -37,7 +39,7 @@ class OverdueScreenController @Inject constructor(
                 appointmentUuid = it.appointment.uuid,
                 name = it.fullName,
                 gender = it.gender,
-                age = 22,
+                age = getAge(it.dateOfBirth, it.age),
                 bpSystolic = it.bloodPressure.systolic,
                 bpDiastolic = it.bloodPressure.diastolic,
                 bpDaysAgo = calculateDaysAgoFromInstant(it.bloodPressure.updatedAt),
@@ -51,6 +53,20 @@ class OverdueScreenController @Inject constructor(
         .map { { ui: Ui -> ui.handleEmptyList(it) } }
 
     return updateListStream.mergeWith(emptyStateStream)
+  }
+
+  private fun getAge(dateOfBirth: LocalDate?, age: Age?): Int {
+    return if (age == null) {
+      Period.between(dateOfBirth!!, LocalDate.now()).years
+
+    } else {
+      val ageUpdatedAt = LocalDateTime.ofInstant(age.updatedAt, ZoneOffset.UTC)
+      val updatedAtLocalDate = LocalDate.of(ageUpdatedAt.year, ageUpdatedAt.month, ageUpdatedAt.dayOfMonth)
+      val yearsSinceThen = Period.between(updatedAtLocalDate, LocalDate.now()).years
+      val ageWhenRecorded = age.value
+
+      ageWhenRecorded + yearsSinceThen
+    }
   }
 
   private fun calculateDaysAgoFromInstant(instant: Instant): Int {

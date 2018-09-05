f1e6a0570e9831918e8e27fb97d6ee317dcab55e
Pratul Kalia <pratul@uncommon.is>
2018-09-05 15:43:41 +0530

    Add analytics strings to overdue screen events

--- app/src/main/java/org/simple/clinic/home/overdue/OverdueScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/OverdueScreenEvents.kt
@@ -1,5 +1,14 @@
 package org.simple.clinic.home.overdue
 
+import org.simple.clinic.util.RuntimePermissionResult
 import org.simple.clinic.widgets.UiEvent
 
 class OverdueScreenCreated : UiEvent
+
+data class CallPatientClicked(val phoneNumber: String) : UiEvent {
+  override val analyticsName = "Overdue Screen:Call Patient Clicked"
+}
+
+data class CallPhonePermissionChanged(val result: RuntimePermissionResult) : UiEvent {
+  override val analyticsName = "Overdue Screen:Call Phone Permission:$result"
+}

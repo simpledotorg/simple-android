f02fbd65173b204c998d9097943f11e9e637fbf7
Sanchita Agarwal <sanchita@uncommon.is>
2018-09-05 14:44:31 +0530

    Clean up overdue list and update UI

--- app/src/main/java/org/simple/clinic/home/overdue/OverdueListAdapter.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/OverdueListAdapter.kt
@@ -1,8 +1,11 @@
 package org.simple.clinic.home.overdue
 
+import android.support.v4.content.ContextCompat
 import android.support.v7.recyclerview.extensions.ListAdapter
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +13,7 @@ import android.widget.TextView
 import kotterknife.bindView
 import org.simple.clinic.R
 import org.simple.clinic.patient.Gender
+import org.simple.clinic.util.Truss
 import java.util.UUID
 
 class OverdueListAdapter : ListAdapter<OverdueListItem, OverdueListViewHolder>(OverdueListDiffer()) {
@@ -37,17 +41,24 @@ data class OverdueListItem(
 
 class OverdueListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-  private val patientName by bindView<TextView>(R.id.overdue_patient_name)
+  private val patientName by bindView<TextView>(R.id.overdue_patient_name_gender_age)
   private val patientBP by bindView<TextView>(R.id.overdue_patient_bp)
   private val overdueDays by bindView<TextView>(R.id.overdue_days)
-  private val patientGenderAge by bindView<TextView>(R.id.overdue_gender_age)
 
   fun render(item: OverdueListItem) {
     val context = itemView.context
-    patientName.text = item.name
+    val spanBuilder = Truss()
+        .append(item.name)
+        .append(" ")
+        .pushSpan(ForegroundColorSpan(ContextCompat.getColor(context, R.color.black_opacity_50)
+        ))
+        .pushSpan(AbsoluteSizeSpan(14, true))
+        .append(context.getString(R.string.overdue_list_item_patient_gender_age, context.getString(item.gender.displayTextRes), item.age))
+        .popSpan()
+
+    patientName.text = spanBuilder.build()
     patientBP.text = context.resources.getQuantityString(R.plurals.overdue_list_item_patient_bp, item.bpDaysAgo, item.bpDaysAgo, item.bpSystolic, item.bpDiastolic)
     overdueDays.text = context.resources.getQuantityString(R.plurals.overdue_list_item_overdue_days, item.overdueDays, item.overdueDays)
-    patientGenderAge.text = context.getString(R.string.overdue_list_item_patient_gender_age, context.getString(item.gender.displayTextRes), item.age)
   }
 }
 

db259f8052f3f72146f4aefb6f1fe32fe24b5001
Saket Narayan <saket@uncommon.is>
2018-09-07 18:14:36 +0530

    Use DataSyncAndroidTester for simplifying tests for syncing appointments and communications

--- app/src/androidTest/java/org/simple/clinic/overdue/communication/CommunicationSyncAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/overdue/communication/CommunicationSyncAndroidTest.kt
@@ -2,10 +2,6 @@ package org.simple.clinic.overdue.communication
 
 import android.support.test.runner.AndroidJUnit4
 import com.f2prateek.rx.preferences2.Preference
-import com.google.common.truth.Truth.assertThat
-import io.reactivex.Completable
-import io.reactivex.Observable
-import io.reactivex.Single
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -13,10 +9,7 @@ import org.junit.runner.RunWith
 import org.simple.clinic.AuthenticationRule
 import org.simple.clinic.TestClinicApp
 import org.simple.clinic.TestData
-import org.simple.clinic.patient.SyncStatus
-import org.simple.clinic.sync.SyncConfig
-import org.simple.clinic.user.UserSession
-import org.simple.clinic.util.Just
+import org.simple.clinic.sync.DataSyncAndroidTester
 import org.simple.clinic.util.Optional
 import org.threeten.bp.Instant
 import javax.inject.Inject
@@ -26,16 +19,7 @@ import javax.inject.Named
 class CommunicationSyncAndroidTest {
 
   @Inject
-  lateinit var configProvider: Single<SyncConfig>
-
-  @Inject
   lateinit var repository: CommunicationRepository
-
-  @Inject
-  lateinit var dao: Communication.RoomDao
-
-  @Inject
-  lateinit var userSession: UserSession
 
   @Inject
   @field:Named("last_communication_pull_timestamp")
@@ -50,6 +34,9 @@ class CommunicationSyncAndroidTest {
   @Inject
   lateinit var testData: TestData
 
+  @Inject
+  lateinit var tester: DataSyncAndroidTester
+
   @get:Rule
   val authenticationRule = AuthenticationRule()
 
@@ -58,37 +45,24 @@ class CommunicationSyncAndroidTest {
     TestClinicApp.appComponent().inject(this)
   }
 
-  private fun insertDummyRecords(count: Int): Completable {
-    return Observable.range(0, count)
-        .map { testData.communication(syncStatus = SyncStatus.PENDING) }
-        .toList()
-        .flatMapCompletable { repository.save(it) }
-  }
-
   @Test
   fun when_pending_sync_records_are_present_then_they_should_be_pushed_to_the_server_and_marked_as_synced_on_success() {
-    val count = 5
-    insertDummyRecords(count).blockingAwait()
-
-    sync.push().blockingAwait()
-
-    val updatedRecords = dao.recordsWithSyncStatus(SyncStatus.DONE).blockingFirst()
-    assertThat(updatedRecords).hasSize(count)
+    tester.test_push(
+        push = { sync.push() },
+        repository = repository,
+        generateRecord = { testData.communication(it) })
   }
 
   @Test
   fun when_pulling_records_then_paginate_till_the_server_does_not_have_anymore_records() {
-    lastPullTimestamp.set(Just(Instant.EPOCH))
-
-    val recordsToInsert = 2 * configProvider.blockingGet().batchSize + 7
-    val dummyPayloads = (0 until recordsToInsert).map { testData.communicationPayload() }
-
-    syncApi.push(CommunicationPushRequest(dummyPayloads))
-        .toCompletable()
-        .andThen(sync.pull())
-        .blockingGet()
-
-    val recordCountAfterPull = repository.recordCount().blockingGet()
-    assertThat(recordCountAfterPull).isAtLeast(recordsToInsert)
+    tester.test_pull(
+        repository = repository,
+        lastPullTimestamp = lastPullTimestamp,
+        pull = { sync.pull() },
+        pushNetworkCall = { count ->
+          val payloads = (0 until count).map { testData.communicationPayload() }
+          val request = CommunicationPushRequest(payloads)
+          syncApi.push(request)
+        })
   }
 }

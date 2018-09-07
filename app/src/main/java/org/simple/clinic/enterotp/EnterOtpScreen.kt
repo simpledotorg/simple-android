9fceedd6fbec081cdc786dc2f6163416b58c1257
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-09-07 16:09:11 +0530

    Add basic implementation of the EnterOtpScreenController

--- app/src/main/java/org/simple/clinic/enterotp/EnterOtpScreen.kt
+++ app/src/main/java/org/simple/clinic/enterotp/EnterOtpScreen.kt
@@ -3,7 +3,52 @@ package org.simple.clinic.enterotp
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.RelativeLayout
+import android.widget.TextView
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
+import io.reactivex.schedulers.Schedulers
+import kotterknife.bindView
+import org.simple.clinic.R
+import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.widgets.PinEditText
+import org.simple.clinic.widgets.showKeyboard
+import javax.inject.Inject
 
 class EnterOtpScreen(context: Context, attributeSet: AttributeSet) : RelativeLayout(context, attributeSet) {
 
+  @Inject
+  lateinit var controller: EnterOtpScreenController
+
+  private val userPhoneNumberTextView by bindView<TextView>(R.id.enterotp_phonenumber)
+  private val otpEntryEditText by bindView<PinEditText>(R.id.enterotp_otp)
+
+  override fun onFinishInflate() {
+    super.onFinishInflate()
+    if (isInEditMode) {
+      return
+    }
+    TheActivity.component.inject(this)
+
+    screenCreates()
+        .observeOn(Schedulers.io())
+        .compose(controller)
+        .observeOn(mainThread())
+        .takeUntil(RxView.detaches(this))
+        .subscribe { uiChange -> uiChange(this) }
+
+    otpEntryEditText.showKeyboard()
+  }
+
+  private fun screenCreates() = Observable.just(EnterOtpScreenCreated())
+
+  fun showUserPhoneNumber(phoneNumber: String) {
+    val phoneNumberWithCountryCode = resources.getString(
+        R.string.enterotp_phonenumber,
+        resources.getString(R.string.country_dialing_code),
+        phoneNumber
+    )
+
+    userPhoneNumberTextView.text = phoneNumberWithCountryCode
+  }
 }
---
+++ app/src/test/java/org/simple/clinic/enterotp/EnterOtpScreenControllerTest.kt
@@ -0,0 +1,42 @@
+package org.simple.clinic.enterotp
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import org.junit.Before
+import org.junit.Test
+import org.simple.clinic.patient.PatientMocker
+import org.simple.clinic.user.UserSession
+import org.simple.clinic.widgets.UiEvent
+
+class EnterOtpScreenControllerTest {
+
+  private lateinit var controller: EnterOtpScreenController
+  private lateinit var screen: EnterOtpScreen
+
+  private val uiEvents: PublishSubject<UiEvent> = PublishSubject.create()
+  private val userSession = mock<UserSession>()
+
+  @Before
+  fun setUp() {
+    screen = mock()
+
+    controller = EnterOtpScreenController(userSession)
+
+    uiEvents
+        .compose(controller)
+        .subscribe { uiChange -> uiChange(screen) }
+  }
+
+  @Test
+  fun `when the screen is created, the logged in users phone number must be shown`() {
+    val user = PatientMocker.loggedInUser(phone = "1111111111")
+    whenever(userSession.requireLoggedInUser()).thenReturn(Observable.just(user))
+
+    uiEvents.onNext(EnterOtpScreenCreated())
+
+    verify(screen).showUserPhoneNumber("1111111111")
+  }
+}

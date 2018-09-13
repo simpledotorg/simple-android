8e0e7798d3295a0ab76cc832267f982e68745b41
Saket Narayan <saket@uncommon.is>
2018-09-13 15:40:11 +0530

    Replace composition with inheritance for running common Android tests for syncing data
    This way, all subclasses can inherit the tests automatically.

--- app/src/androidTest/java/org/simple/clinic/overdue/AppointmentSyncAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/overdue/AppointmentSyncAndroidTest.kt
@@ -2,21 +2,21 @@ package org.simple.clinic.overdue
 
 import android.support.test.runner.AndroidJUnit4
 import com.f2prateek.rx.preferences2.Preference
+import io.reactivex.Single
 import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
-import org.simple.clinic.AuthenticationRule
 import org.simple.clinic.TestClinicApp
 import org.simple.clinic.TestData
-import org.simple.clinic.sync.DataSyncAndroidTester
+import org.simple.clinic.patient.SyncStatus
+import org.simple.clinic.sync.BaseDataSyncAndroidTest
+import org.simple.clinic.sync.DataPushResponse
 import org.simple.clinic.util.Optional
 import org.threeten.bp.Instant
 import javax.inject.Inject
 import javax.inject.Named
 
 @RunWith(AndroidJUnit4::class)
-class AppointmentSyncAndroidTest {
+class AppointmentSyncAndroidTest : BaseDataSyncAndroidTest<Appointment, AppointmentPayload>() {
 
   @Inject
   lateinit var repository: AppointmentRepository
@@ -34,35 +34,25 @@ class AppointmentSyncAndroidTest {
   @Inject
   lateinit var testData: TestData
 
-  @Inject
-  lateinit var tester: DataSyncAndroidTester
-
-  @get:Rule
-  val authenticationRule = AuthenticationRule()
-
   @Before
   fun setUp() {
     TestClinicApp.appComponent().inject(this)
   }
 
-  @Test
-  fun when_pending_sync_records_are_present_then_they_should_be_pushed_to_the_server_and_marked_as_synced_on_success() {
-    tester.test_push(
-        push = { sync.push() },
-        repository = repository,
-        generateRecord = { testData.appointment(it) })
-  }
+  override fun push() = sync.push()
 
-  @Test
-  fun when_pulling_records_then_paginate_till_the_server_does_not_have_anymore_records() {
-    tester.test_pull(
-        repository = repository,
-        lastPullTimestamp = lastPullTimestamp,
-        pull = { sync.pull() },
-        pushNetworkCall = { count ->
-          val payloads = (0 until count).map { testData.appointmentPayload() }
-          val request = AppointmentPushRequest(payloads)
-          syncApi.push(request)
-        })
+  override fun pull() = sync.pull()
+
+  override fun repository() = repository
+
+  override fun generateRecord(syncStatus: SyncStatus) = testData.appointment(syncStatus)
+
+  override fun generatePayload() = testData.appointmentPayload()
+
+  override fun lastPullTimestamp() = lastPullTimestamp
+
+  override fun pushNetworkCall(payloads: List<AppointmentPayload>): Single<DataPushResponse> {
+    val request = AppointmentPushRequest(payloads)
+    return syncApi.push(request)
   }
 }
--- app/src/androidTest/java/org/simple/clinic/overdue/communication/CommunicationSyncAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/overdue/communication/CommunicationSyncAndroidTest.kt
@@ -2,21 +2,21 @@ package org.simple.clinic.overdue.communication
 
 import android.support.test.runner.AndroidJUnit4
 import com.f2prateek.rx.preferences2.Preference
+import io.reactivex.Single
 import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
-import org.simple.clinic.AuthenticationRule
 import org.simple.clinic.TestClinicApp
 import org.simple.clinic.TestData
-import org.simple.clinic.sync.DataSyncAndroidTester
+import org.simple.clinic.patient.SyncStatus
+import org.simple.clinic.sync.BaseDataSyncAndroidTest
+import org.simple.clinic.sync.DataPushResponse
 import org.simple.clinic.util.Optional
 import org.threeten.bp.Instant
 import javax.inject.Inject
 import javax.inject.Named
 
 @RunWith(AndroidJUnit4::class)
-class CommunicationSyncAndroidTest {
+class CommunicationSyncAndroidTest: BaseDataSyncAndroidTest<Communication, CommunicationPayload>() {
 
   @Inject
   lateinit var repository: CommunicationRepository
@@ -34,35 +34,25 @@ class CommunicationSyncAndroidTest {
   @Inject
   lateinit var testData: TestData
 
-  @Inject
-  lateinit var tester: DataSyncAndroidTester
-
-  @get:Rule
-  val authenticationRule = AuthenticationRule()
-
   @Before
   fun setUp() {
     TestClinicApp.appComponent().inject(this)
   }
 
-  @Test
-  fun when_pending_sync_records_are_present_then_they_should_be_pushed_to_the_server_and_marked_as_synced_on_success() {
-    tester.test_push(
-        push = { sync.push() },
-        repository = repository,
-        generateRecord = { testData.communication(it) })
-  }
+  override fun push() = sync.push()
 
-  @Test
-  fun when_pulling_records_then_paginate_till_the_server_does_not_have_anymore_records() {
-    tester.test_pull(
-        repository = repository,
-        lastPullTimestamp = lastPullTimestamp,
-        pull = { sync.pull() },
-        pushNetworkCall = { count ->
-          val payloads = (0 until count).map { testData.communicationPayload() }
-          val request = CommunicationPushRequest(payloads)
-          syncApi.push(request)
-        })
+  override fun pull() = sync.pull()
+
+  override fun repository() = repository
+
+  override fun generateRecord(syncStatus: SyncStatus) = testData.communication(syncStatus)
+
+  override fun generatePayload() = testData.communicationPayload()
+
+  override fun lastPullTimestamp() = lastPullTimestamp
+
+  override fun pushNetworkCall(payloads: List<CommunicationPayload>): Single<DataPushResponse> {
+    val request = CommunicationPushRequest(payloads)
+    return syncApi.push(request)
   }
 }

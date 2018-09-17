c5c1b44ef0e35b11e1f0e8ae00c3909017fd7fbf
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-09-17 14:44:30 +0530

    Start forgot PIN flow on confirmation of reset PIN

--- app/src/main/java/org/simple/clinic/login/applock/ConfirmResetPinDialog.kt
+++ app/src/main/java/org/simple/clinic/login/applock/ConfirmResetPinDialog.kt
@@ -1,15 +1,23 @@
 package org.simple.clinic.login.applock
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatDialogFragment
 import android.view.View
+import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 import org.simple.clinic.R
+import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.user.UserSession
+import javax.inject.Inject
 
 class ConfirmResetPinDialog : AppCompatDialogFragment() {
+
+  @Inject
+  lateinit var userSession: UserSession
 
   companion object {
     fun show(fragmentManager: FragmentManager) {
@@ -22,6 +30,12 @@ class ConfirmResetPinDialog : AppCompatDialogFragment() {
     }
   }
 
+  override fun onAttach(context: Context) {
+    super.onAttach(context)
+    TheActivity.component.inject(this)
+  }
+
+
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
     return AlertDialog.Builder(context!!)
         .setTitle(R.string.applock_reset_pin_alert_title)
@@ -30,6 +44,13 @@ class ConfirmResetPinDialog : AppCompatDialogFragment() {
           val view = dialog.ownerActivity.findViewById<View>(android.R.id.content)
           val snackbar = Snackbar.make(view, getString(R.string.applock_reset_pin_waiting), Snackbar.LENGTH_INDEFINITE)
           snackbar.show()
+
+          userSession.startForgotPinFlow(1)
+              .observeOn(mainThread())
+              .doOnTerminate { snackbar.dismiss() }
+              .subscribe {
+                // TODO: Open new PIN entry screen
+              }
         }
         .setNegativeButton(R.string.applock_reset_pin_alert_cancel, null)
         .create()

c9104dac8478da2b7cff3074a219f49b5ee4a421
Saket Narayan <saket@uncommon.is>
2018-09-17 15:40:15 +0530

    Fix a bug where DateOfBirthEditText ends up formatting its hint

--- app/src/main/java/org/simple/clinic/newentry/DateOfBirthEditText.kt
+++ app/src/main/java/org/simple/clinic/newentry/DateOfBirthEditText.kt
@@ -13,11 +13,21 @@ class DateOfBirthEditText(context: Context, attrs: AttributeSet) : MaskedEditTex
     focusChanges = Observable
         .create<Boolean> { emitter ->
           emitter.setCancellable { super.setOnFocusChangeListener(null) }
-          super.setOnFocusChangeListener({ _, hasFocus -> emitter.onNext(hasFocus) })
+          super.setOnFocusChangeListener { _, hasFocus -> emitter.onNext(hasFocus) }
           emitter.onNext(hasFocus())
         }
         .replay(1)
         .refCount()
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+
+    // MaskedEditText has a bug where it ends up showing the
+    // hint as the formatted text even when used inside a
+    // TextInputLayout. So a hint of "Date of birth" shows up
+    // as "Da/te/ of". This seems to fix it.
+    text = text
   }
 
   override fun setOnFocusChangeListener(listener: OnFocusChangeListener) {

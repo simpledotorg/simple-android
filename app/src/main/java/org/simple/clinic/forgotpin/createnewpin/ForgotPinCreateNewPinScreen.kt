8dad1bf5b15c1030a24f6f2cdd239c4687b3743b
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-09-17 16:48:25 +0530

    Handle PIN entry events when creating new PIN

--- app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
@@ -2,9 +2,14 @@ package org.simple.clinic.forgotpin.createnewpin
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -12,7 +17,11 @@ import kotterknife.bindView
 import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
 import org.simple.clinic.widgets.ScreenCreated
+import org.simple.clinic.widgets.StaggeredEditText
 import org.simple.clinic.widgets.UiEvent
+import org.simple.clinic.widgets.hideKeyboard
+import org.simple.clinic.widgets.showKeyboard
+import timber.log.Timber
 import javax.inject.Inject
 
 class ForgotPinCreateNewPinScreen(context: Context, attributeSet: AttributeSet?) : RelativeLayout(context, attributeSet) {
@@ -22,6 +31,8 @@ class ForgotPinCreateNewPinScreen(context: Context, attributeSet: AttributeSet?)
 
   private val userNameTextView by bindView<TextView>(R.id.forgotpin_user_fullname)
   private val facilityNameTextView by bindView<TextView>(R.id.forgotpin_facility_name)
+  private val pinEntryEditText by bindView<StaggeredEditText>(R.id.forgotpin_pin)
+  private val pinErrorTextView by bindView<TextView>(R.id.forgotpin_error)
 
   override fun onFinishInflate() {
     super.onFinishInflate()
@@ -31,15 +42,28 @@ class ForgotPinCreateNewPinScreen(context: Context, attributeSet: AttributeSet?)
 
     TheActivity.component.inject(this)
 
-    screenCreates()
+    Observable.merge(screenCreates(), pinTextChanges(), pinSubmitClicked())
         .observeOn(Schedulers.io())
         .compose(controller)
         .observeOn(AndroidSchedulers.mainThread())
         .takeUntil(RxView.detaches(this))
         .subscribe { it.invoke(this) }
+
+    pinEntryEditText.showKeyboard()
   }
 
   private fun screenCreates(): Observable<UiEvent> = Observable.just(ScreenCreated())
+
+  private fun pinTextChanges(): Observable<UiEvent> {
+    return RxTextView.textChanges(pinEntryEditText)
+        .map { ForgotPinCreateNewPinTextChanged(it.toString()) }
+  }
+
+  private fun pinSubmitClicked(): Observable<UiEvent> {
+   return RxTextView.editorActions(pinEntryEditText)
+       .filter { it == EditorInfo.IME_ACTION_DONE }
+       .map { ForgotPinCreateNewPinSubmitClicked }
+  }
 
   fun showUserName(name: String) {
     userNameTextView.text = name
@@ -47,5 +71,18 @@ class ForgotPinCreateNewPinScreen(context: Context, attributeSet: AttributeSet?)
 
   fun showFacility(name: String) {
     facilityNameTextView.text = name
+  }
+
+  fun showInvalidPinError() {
+    pinErrorTextView.visibility = View.VISIBLE
+  }
+
+  fun showConfirmPinScreen(pin: String) {
+    Toast.makeText(context, "Under Construction", Toast.LENGTH_SHORT).show()
+    hideKeyboard()
+  }
+
+  fun hideInvalidPinError() {
+    pinErrorTextView.visibility = View.GONE
   }
 }

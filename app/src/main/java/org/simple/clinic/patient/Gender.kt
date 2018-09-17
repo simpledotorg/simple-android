445e1ebf0a1217529faf2d4192e7abec2340baba
Sanchita Agarwal <sanchita@uncommon.is>
2018-09-17 15:26:06 +0530

    Update overdue list UI as per new design

--- app/src/main/java/org/simple/clinic/patient/Gender.kt
+++ app/src/main/java/org/simple/clinic/patient/Gender.kt
@@ -5,16 +5,16 @@ import com.squareup.moshi.Json
 import org.simple.clinic.R
 import org.simple.clinic.util.RoomEnumTypeConverter
 
-enum class Gender(@StringRes val displayTextRes: Int) {
+enum class Gender(@StringRes val displayTextRes: Int, val displayLetterRes: Int) {
 
   @Json(name = "male")
-  MALE(R.string.gender_male),
+  MALE(R.string.gender_male, R.string.gender_male_letter),
 
   @Json(name = "female")
-  FEMALE(R.string.gender_female),
+  FEMALE(R.string.gender_female, R.string.gender_female_letter),
 
   @Json(name = "transgender")
-  TRANSGENDER(R.string.gender_transgender);
+  TRANSGENDER(R.string.gender_transgender, R.string.gender_trans_letter);
 
   class RoomTypeConverter : RoomEnumTypeConverter<Gender>(Gender::class.java)
 }

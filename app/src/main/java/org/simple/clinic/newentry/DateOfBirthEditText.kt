9d1ac64e5372c54b7ed8a277d5b33d4bb57b7507
Saket Narayan <saket@uncommon.is>
2018-09-18 13:27:26 +0530

    Improve how setting of formatted hint is ignored in DateOfBirthEditTetxt

--- app/src/main/java/org/simple/clinic/newentry/DateOfBirthEditText.kt
+++ app/src/main/java/org/simple/clinic/newentry/DateOfBirthEditText.kt
@@ -1,11 +1,23 @@
 package org.simple.clinic.newentry
 
 import android.content.Context
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
 import android.util.AttributeSet
 import br.com.sapereaude.maskedEditText.MaskedEditText
 import io.reactivex.Observable
+import timber.log.Timber
+import java.lang.reflect.Field
 
 class DateOfBirthEditText(context: Context, attrs: AttributeSet) : MaskedEditText(context, attrs), MultipleFocusChangeListeners {
+
+  companion object {
+    val keepHintField: Field by lazy {
+      val keepHintField = MaskedEditText::class.java.getDeclaredField("keepHint")
+      keepHintField.isAccessible = true
+      keepHintField
+    }
+  }
 
   override val focusChanges: Observable<Boolean>
 
@@ -20,14 +32,27 @@ class DateOfBirthEditText(context: Context, attrs: AttributeSet) : MaskedEditTex
         .refCount()
   }
 
-  override fun onAttachedToWindow() {
-    super.onAttachedToWindow()
+  override fun setText(text: CharSequence?, type: BufferType?) {
+    // MaskedEditText has a bug where it ends up showing the hint as
+    // the formatted text even when used inside a TextInputLayout. So
+    // a hint of "Date of birth" shows up as "Da/te/ of". From my
+    // limited research, MaskedEditText calls makeMaskedTextWithHint()
+    // from init(), even when keepHint attribute is set to false.
+    try {
+      val keepHint = keepHintField.get(this) as Boolean
+      if (keepHint.not() && text is Spannable) {
+        // makeMaskedTextWithHint() creates a
+        // ForegroundColorSpan with the current hint color.
+        val spans = text.getSpans(0, text.length, ForegroundColorSpan::class.java)
+        if (spans.isNotEmpty() && spans.first().foregroundColor == currentHintTextColor) {
+          return
+        }
+      }
+    } catch (e: Throwable) {
+      Timber.e(e)
+    }
 
-    // MaskedEditText has a bug where it ends up showing the
-    // hint as the formatted text even when used inside a
-    // TextInputLayout. So a hint of "Date of birth" shows up
-    // as "Da/te/ of". This seems to fix it.
-    text = text
+    super.setText(text, type)
   }
 
   override fun setOnFocusChangeListener(listener: OnFocusChangeListener) {

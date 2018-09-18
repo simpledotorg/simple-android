bedcbd7e9bef5f04893a432303f92469dbbb9f7f
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-09-18 11:35:28 +0530

    Handle facility clicks in Forgot Pin create new PIN screen

--- app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinEvents.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinEvents.kt
@@ -2,10 +2,14 @@ package org.simple.clinic.forgotpin.createnewpin
 
 import org.simple.clinic.widgets.UiEvent
 
-data class ForgotPinCreateNewPinTextChanged(val pin: String): UiEvent {
+data class ForgotPinCreateNewPinTextChanged(val pin: String) : UiEvent {
   override val analyticsName = "Forgot PIN:Create new PIN:Text Changed"
 }
 
-object ForgotPinCreateNewPinSubmitClicked: UiEvent {
+object ForgotPinCreateNewPinSubmitClicked : UiEvent {
   override val analyticsName = "Forgot PIN:Create new PIN:Submit Clicked"
+}
+
+object ForgotPinCreateNewPinFacilityClicked : UiEvent {
+  override val analyticsName = "Forgot PIN:Create new PIN:Facility Clicked"
 }

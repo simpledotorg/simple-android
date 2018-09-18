2a0b6c2271f5a330596db4b05f0b62de2986d1a0
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-09-18 17:03:29 +0530

    Follow convention for creating screen keys

--- app/src/main/java/org/simple/clinic/forgotpin/confirmpin/ForgotPinConfirmPinScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/confirmpin/ForgotPinConfirmPinScreenEvents.kt
@@ -16,10 +16,10 @@ data class ForgotPinConfirmPinScreenCreated(val pin: String) : UiEvent {
   }
 }
 
-data class ForgotPinConfirmPinSubmitClicked(val pin: String): UiEvent {
+data class ForgotPinConfirmPinSubmitClicked(val pin: String) : UiEvent {
   override val analyticsName = "Forgot PIN:Confirm PIN:Submit Clicked"
 }
 
-data class ForgotPinConfirmPinTextChanged(val text: String): UiEvent {
+data class ForgotPinConfirmPinTextChanged(val text: String) : UiEvent {
   override val analyticsName = "Forgot PIN:Confirm PIN:Text Changed"
 }

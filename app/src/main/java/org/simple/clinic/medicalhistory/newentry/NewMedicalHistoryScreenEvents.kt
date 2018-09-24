15e796a328aaadfe5a7c94f2198ffbc247176b3e
Saket Narayan <saket@uncommon.is>
2018-09-24 15:40:13 +0530

    Save medical history answers

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
@@ -8,10 +8,6 @@ data class NewMedicalHistoryScreenCreated(val patientUuid: PatientUuid) : UiEven
   override val analyticsName = "New Medical History:Screen Created for $patientUuid"
 }
 
-data class MedicalHistoryAnswerToggled(val question: MedicalHistoryQuestion, val selected: Boolean) : UiEvent {
-  override val analyticsName = "New Medical History:Answer Toggled"
-}
-
 class SaveMedicalHistoryClicked : UiEvent {
   override val analyticsName = "New Medical History:Save Clicked"
 }

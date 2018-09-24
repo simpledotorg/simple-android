6b43c22da33c3147f684cadd3ea19837ac0b4340
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-09-24 09:45:36 +0530

    Handle back clicks directly in the UI in ForgotPinConfirmNewPinScreen

--- app/src/main/java/org/simple/clinic/forgotpin/confirmpin/ForgotPinConfirmPinScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/confirmpin/ForgotPinConfirmPinScreenEvents.kt
@@ -6,10 +6,6 @@ object ForgotPinConfirmPinScreenFacilityClicked : UiEvent {
   override val analyticsName = "Forgot PIN:Confirm PIN:Facility Clicked"
 }
 
-object ForgotPinConfirmPinScreenBackClicked : UiEvent {
-  override val analyticsName = "Forgot PIN:Confirm PIN:Back Clicked"
-}
-
 data class ForgotPinConfirmPinScreenCreated(val pin: String) : UiEvent {
   init {
     if (pin.isBlank()) throw AssertionError("PIN cannot be blank!")

1cedfa3c7b121e81bf0e13bc974a3eb9ff37ac13
Saket Narayan <saket@uncommon.is>
2018-09-24 17:09:56 +0530

    Save patient only after medical history is saved

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
@@ -1,12 +1,6 @@
 package org.simple.clinic.medicalhistory.newentry
 
-import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
-import org.simple.clinic.patient.PatientUuid
 import org.simple.clinic.widgets.UiEvent
-
-data class NewMedicalHistoryScreenCreated(val patientUuid: PatientUuid) : UiEvent {
-  override val analyticsName = "New Medical History:Screen Created for $patientUuid"
-}
 
 class SaveMedicalHistoryClicked : UiEvent {
   override val analyticsName = "New Medical History:Save Clicked"

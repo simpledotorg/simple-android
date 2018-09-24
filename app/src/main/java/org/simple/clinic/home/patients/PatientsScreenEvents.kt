d73fda0264d9c4299f6676f3896ef34d4ac784c9
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-09-24 16:57:21 +0530

    Remove aadhar scan resources and event

--- app/src/main/java/org/simple/clinic/home/patients/PatientsScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/home/patients/PatientsScreenEvents.kt
@@ -6,10 +6,6 @@ class NewPatientClicked : UiEvent {
   override val analyticsName = "Patients:Search For Patient Clicked"
 }
 
-class ScanAadhaarClicked : UiEvent {
-  override val analyticsName = "Patients:Scan For Aadhaar Clicked"
-}
-
 class UserApprovedStatusDismissed : UiEvent {
   override val analyticsName = "Patients:Dismissed User Approved Status"
 }

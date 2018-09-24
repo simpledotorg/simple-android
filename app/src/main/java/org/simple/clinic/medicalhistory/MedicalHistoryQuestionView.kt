bc996fc68a72e64e65a9153442772bade8a476ba
Saket Narayan <saket@uncommon.is>
2018-09-24 15:40:13 +0530

    Save medical history answers

--- app/src/main/java/org/simple/clinic/medicalhistory/MedicalHistoryQuestionView.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/MedicalHistoryQuestionView.kt
@@ -11,17 +11,29 @@ import kotterknife.bindView
 import org.simple.clinic.R
 import org.simple.clinic.widgets.UiEvent
 
+data class MedicalHistoryAnswerToggled(val question: MedicalHistoryQuestion, val selected: Boolean) : UiEvent {
+  override val analyticsName = "New Medical History:Answer Toggled"
+}
+
 @SuppressLint("ClickableViewAccessibility")
 class MedicalHistoryQuestionView(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
 
   val checkBox by bindView<CheckBox>(R.id.newmedicalhistory_item_checkbox)
+  val toggles = PublishSubject.create<UiEvent>()!!
+
+  lateinit var question: MedicalHistoryQuestion
 
   init {
     LayoutInflater.from(context).inflate(R.layout.list_medical_history_question, this, true)
-    checkBox.setOnTouchListener { _, event -> this.onTouchEvent(event) }
+    setOnTouchListener { _, event -> checkBox.onTouchEvent(event) }
+
+    checkBox.setOnCheckedChangeListener { _, isChecked ->
+      toggles.onNext(MedicalHistoryAnswerToggled(question, isChecked))
+    }
   }
 
   fun render(question: MedicalHistoryQuestion) {
+    this.question = question
     checkBox.setText(question.questionRes)
   }
 }

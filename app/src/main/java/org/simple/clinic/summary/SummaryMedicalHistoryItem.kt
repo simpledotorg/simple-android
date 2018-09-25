22bf1c7a1236a039de3df2fab0c37f31376a067f
Saket Narayan <saket@uncommon.is>
2018-09-25 17:47:36 +0530

    Show answers for medical history in summary screen

--- app/src/main/java/org/simple/clinic/summary/SummaryMedicalHistoryItem.kt
+++ app/src/main/java/org/simple/clinic/summary/SummaryMedicalHistoryItem.kt
@@ -7,6 +7,12 @@ import io.reactivex.subjects.Subject
 import kotterknife.bindView
 import org.simple.clinic.R
 import org.simple.clinic.medicalhistory.MedicalHistory
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_DIABETES
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_HEART_ATTACK
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_KIDNEY_DISEASE
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_STROKE
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IS_ON_TREATMENT_FOR_HYPERTENSION
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestionView
 import org.simple.clinic.widgets.UiEvent
 import java.util.Locale.ENGLISH
 
@@ -28,9 +34,43 @@ data class SummaryMedicalHistoryItem(
     holder.lastUpdatedAtTextView.text = context.getString(
         R.string.patientsummary_medicalhistory_last_updated,
         lastUpdatedAt.displayText(context).toLowerCase(ENGLISH))
+
+    holder.heartAttackQuestionView.apply {
+      render(HAS_HAD_A_HEART_ATTACK)
+      checkBox.isChecked = medicalHistory.hasHadHeartAttack
+    }
+
+    holder.strokeQuestionView.apply {
+      render(HAS_HAD_A_STROKE)
+      checkBox.isChecked = medicalHistory.hasHadStroke
+    }
+
+    holder.kidneyDiseaseQuestionView.apply {
+      render(HAS_HAD_A_KIDNEY_DISEASE)
+      checkBox.isChecked = medicalHistory.hasHadKidneyDisease
+    }
+
+    holder.hypertensionQuestionView.apply {
+      render(IS_ON_TREATMENT_FOR_HYPERTENSION)
+      checkBox.isChecked = medicalHistory.isOnTreatmentForHypertension
+    }
+
+    holder.diabetesQuestionView.apply {
+      render(HAS_DIABETES)
+      checkBox.isChecked = medicalHistory.hasDiabetes
+    }
   }
 
   class HistoryViewHolder(rootView: View) : ViewHolder(rootView) {
     val lastUpdatedAtTextView by bindView<TextView>(R.id.patientsummary_medicalhistory_last_update_timestamp)
+    val heartAttackQuestionView by bindView<MedicalHistoryQuestionView>(R.id.patientsummary_medicalhistory_question_heartattack)
+    val strokeQuestionView by bindView<MedicalHistoryQuestionView>(R.id.patientsummary_medicalhistory_question_stroke)
+    val kidneyDiseaseQuestionView by bindView<MedicalHistoryQuestionView>(R.id.patientsummary_medicalhistory_question_kidney)
+    val hypertensionQuestionView by bindView<MedicalHistoryQuestionView>(R.id.patientsummary_medicalhistory_question_hypertension)
+    val diabetesQuestionView by bindView<MedicalHistoryQuestionView>(R.id.patientsummary_medicalhistory_question_diabetes)
+
+    init {
+      diabetesQuestionView.divider.visibility = View.GONE
+    }
   }
 }

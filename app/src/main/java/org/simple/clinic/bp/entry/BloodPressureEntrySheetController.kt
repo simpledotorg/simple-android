83e84848ae3e9050dbc38998123ec23f31b72596
Pratul Kalia <pratul@uncommon.is>
2018-10-04 16:46:32 +0530

    Add validations for acceptable blood pressure values

--- app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntrySheetController.kt
+++ app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntrySheetController.kt
@@ -7,6 +7,13 @@ import io.reactivex.rxkotlin.ofType
 import io.reactivex.rxkotlin.withLatestFrom
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.bp.BloodPressureRepository
+import org.simple.clinic.bp.entry.BloodPressureEntrySheetController.Validation.ERROR_DIASTOLIC_TOO_HIGH
+import org.simple.clinic.bp.entry.BloodPressureEntrySheetController.Validation.ERROR_DIASTOLIC_TOO_LOW
+import org.simple.clinic.bp.entry.BloodPressureEntrySheetController.Validation.ERROR_SYSTOLIC_LESS_THAN_DIASTOLIC
+import org.simple.clinic.bp.entry.BloodPressureEntrySheetController.Validation.ERROR_SYSTOLIC_TOO_HIGH
+import org.simple.clinic.bp.entry.BloodPressureEntrySheetController.Validation.ERROR_SYSTOLIC_TOO_LOW
+import org.simple.clinic.bp.entry.BloodPressureEntrySheetController.Validation.SUCCESS
+import org.simple.clinic.util.exhaustive
 import org.simple.clinic.widgets.UiEvent
 import javax.inject.Inject
 
@@ -18,11 +25,12 @@ class BloodPressureEntrySheetController @Inject constructor(
 ) : ObservableTransformer<UiEvent, UiChange> {
 
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
-    val replayedEvents = events.compose(ReportAnalyticsEvents()).replay(1).refCount()
+    val replayedEvents = events.compose(ReportAnalyticsEvents()).replay().refCount()
 
     return Observable.merge(
-        handleImeOptionClicks(replayedEvents),
-        systolicTextChanges(replayedEvents))
+        systolicTextChanges(replayedEvents),
+        validationErrorResets(replayedEvents),
+        bpValidationsAndSaves(replayedEvents))
   }
 
   private fun systolicTextChanges(events: Observable<UiEvent>): Observable<UiChange> {
@@ -39,7 +47,19 @@ class BloodPressureEntrySheetController @Inject constructor(
         || (systolicText.length == 2 && systolicText.matches("^[3-9].*$".toRegex()))
   }
 
-  private fun handleImeOptionClicks(events: Observable<UiEvent>): Observable<UiChange> {
+  private fun validationErrorResets(events: Observable<UiEvent>): Observable<UiChange> {
+    val systolicChanges = events.ofType<BloodPressureSystolicTextChanged>()
+        .distinctUntilChanged()
+        .map { { ui: Ui -> ui.hideErrorMessage() } }
+
+    val diastolicChanges = events.ofType<BloodPressureDiastolicTextChanged>()
+        .distinctUntilChanged()
+        .map { { ui: Ui -> ui.hideErrorMessage() } }
+
+    return Observable.merge(systolicChanges, diastolicChanges)
+  }
+
+  private fun bpValidationsAndSaves(events: Observable<UiEvent>): Observable<UiChange> {
     val imeDoneClicks = events.ofType<BloodPressureSaveClicked>()
 
     val patientUuids = events
@@ -54,19 +74,57 @@ class BloodPressureEntrySheetController @Inject constructor(
         .ofType<BloodPressureDiastolicTextChanged>()
         .map { it.diastolic }
 
-    return imeDoneClicks
+    val triples = imeDoneClicks
         .withLatestFrom(patientUuids, systolicChanges, diastolicChanges) { _, uuid, systolic, diastolic -> Triple(uuid, systolic, diastolic) }
-        .filter { (_, systolic, diastolic) -> isInputValid(systolic, diastolic) }
-        .take(1)
+
+    val errors = triples
+        .map { (_, systolic, diastolic) ->
+          { ui: Ui ->
+            when (validateInput(systolic, diastolic)) {
+              ERROR_SYSTOLIC_LESS_THAN_DIASTOLIC -> ui.showSystolicLessThanDiastolicError()
+              ERROR_SYSTOLIC_TOO_HIGH -> ui.showSystolicHighError()
+              ERROR_SYSTOLIC_TOO_LOW -> ui.showSystolicLowError()
+              ERROR_DIASTOLIC_TOO_HIGH -> ui.showDiastolicHighError()
+              ERROR_DIASTOLIC_TOO_LOW -> ui.showDiastolicLowError()
+              SUCCESS -> { // Nothing to do here, SUCCESS handled below separately!
+              }
+            }.exhaustive()
+          }
+        }
+
+    val saves = triples
+        .filter { (_, systolic, diastolic) -> validateInput(systolic, diastolic) == SUCCESS }
+        .distinctUntilChanged()
         .flatMap { (uuid, systolic, diastolic) ->
           bloodPressureRepository
               .saveMeasurement(uuid, systolic.toInt(), diastolic.toInt())
               .toCompletable()
               .andThen(Observable.just({ ui: Ui -> ui.setBPSavedResultAndFinish() }))
         }
+
+    return Observable.merge(errors, saves)
   }
 
-  private fun isInputValid(systolic: String, diastolic: String): Boolean {
-    return systolic.length in 2..3 && diastolic.length in 2..3
+  private fun validateInput(systolic: String, diastolic: String): Validation {
+    val systolicNumber = systolic.toInt()
+    val diastolicNumber = diastolic.toInt()
+
+    return when {
+      systolicNumber < 70 -> ERROR_SYSTOLIC_TOO_LOW
+      systolicNumber > 300 -> ERROR_SYSTOLIC_TOO_HIGH
+      diastolicNumber < 40 -> ERROR_DIASTOLIC_TOO_LOW
+      diastolicNumber > 180 -> ERROR_DIASTOLIC_TOO_HIGH
+      systolicNumber < diastolicNumber -> ERROR_SYSTOLIC_LESS_THAN_DIASTOLIC
+      else -> SUCCESS
+    }
+  }
+
+  enum class Validation {
+    SUCCESS,
+    ERROR_SYSTOLIC_TOO_HIGH,
+    ERROR_SYSTOLIC_TOO_LOW,
+    ERROR_DIASTOLIC_TOO_HIGH,
+    ERROR_DIASTOLIC_TOO_LOW,
+    ERROR_SYSTOLIC_LESS_THAN_DIASTOLIC
   }
 }
--- app/src/test/java/org/simple/clinic/bp/entry/BloodPressureEntrySheetControllerTest.kt
+++ app/src/test/java/org/simple/clinic/bp/entry/BloodPressureEntrySheetControllerTest.kt
@@ -34,6 +34,72 @@ class BloodPressureEntrySheetControllerTest {
   }
 
   @Test
+  fun `when systolic is less than diastolic, show error`() {
+    uiEvents.onNext(BloodPressureEntrySheetCreated(patientUuid))
+    uiEvents.onNext(BloodPressureSystolicTextChanged("90"))
+    uiEvents.onNext(BloodPressureDiastolicTextChanged("140"))
+    uiEvents.onNext(BloodPressureSaveClicked())
+
+    verify(bloodPressureRepository, never()).saveMeasurement(any(), any(), any())
+    verify(sheet).showSystolicLessThanDiastolicError()
+  }
+
+  @Test
+  fun `when systolic is less than minimum possible, show error`() {
+    uiEvents.onNext(BloodPressureEntrySheetCreated(patientUuid))
+    uiEvents.onNext(BloodPressureSystolicTextChanged("55"))
+    uiEvents.onNext(BloodPressureDiastolicTextChanged("55"))
+    uiEvents.onNext(BloodPressureSaveClicked())
+
+    verify(bloodPressureRepository, never()).saveMeasurement(any(), any(), any())
+    verify(sheet).showSystolicLowError()
+  }
+
+  @Test
+  fun `when systolic is more than maximum possible, show error`() {
+    uiEvents.onNext(BloodPressureEntrySheetCreated(patientUuid))
+    uiEvents.onNext(BloodPressureSystolicTextChanged("333"))
+    uiEvents.onNext(BloodPressureDiastolicTextChanged("88"))
+    uiEvents.onNext(BloodPressureSaveClicked())
+
+    verify(bloodPressureRepository, never()).saveMeasurement(any(), any(), any())
+    verify(sheet).showSystolicHighError()
+  }
+
+  @Test
+  fun `when diastolic is less than minimum possible, show error`() {
+    uiEvents.onNext(BloodPressureEntrySheetCreated(patientUuid))
+    uiEvents.onNext(BloodPressureSystolicTextChanged("110"))
+    uiEvents.onNext(BloodPressureDiastolicTextChanged("33"))
+    uiEvents.onNext(BloodPressureSaveClicked())
+
+    verify(bloodPressureRepository, never()).saveMeasurement(any(), any(), any())
+    verify(sheet).showDiastolicLowError()
+  }
+
+  @Test
+  fun `when diastolic is more than maximum possible, show error`() {
+    uiEvents.onNext(BloodPressureEntrySheetCreated(patientUuid))
+    uiEvents.onNext(BloodPressureSystolicTextChanged("233"))
+    uiEvents.onNext(BloodPressureDiastolicTextChanged("190"))
+    uiEvents.onNext(BloodPressureSaveClicked())
+
+    verify(bloodPressureRepository, never()).saveMeasurement(any(), any(), any())
+    verify(sheet).showDiastolicHighError()
+  }
+
+  @Test
+  fun `when systolic or diastolic values change, hide the error message`() {
+    uiEvents.onNext(BloodPressureSystolicTextChanged("12"))
+    uiEvents.onNext(BloodPressureSystolicTextChanged("120"))
+    uiEvents.onNext(BloodPressureSystolicTextChanged("130"))
+    uiEvents.onNext(BloodPressureDiastolicTextChanged("90"))
+    uiEvents.onNext(BloodPressureDiastolicTextChanged("99"))
+
+    verify(sheet, times(5)).hideErrorMessage()
+  }
+
+  @Test
   fun `when save is clicked but input is invalid then blood pressure measurement should not be saved`() {
     uiEvents.onNext(BloodPressureEntrySheetCreated(patientUuid))
     uiEvents.onNext(BloodPressureSystolicTextChanged("1"))

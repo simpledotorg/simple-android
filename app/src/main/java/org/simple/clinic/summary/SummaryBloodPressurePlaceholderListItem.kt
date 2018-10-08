11d652f3cc36ae7f421652da96e49b1db3753b83
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-10-08 15:23:08 +0530

    Extract summary view item IDs into a separate object

--- app/src/main/java/org/simple/clinic/summary/SummaryBloodPressurePlaceholderListItem.kt
+++ app/src/main/java/org/simple/clinic/summary/SummaryBloodPressurePlaceholderListItem.kt
@@ -7,18 +7,13 @@ import com.xwray.groupie.ViewHolder
 import io.reactivex.subjects.Subject
 import kotterknife.bindView
 import org.simple.clinic.R
+import org.simple.clinic.summary.SummaryListAdapterIds.BP_PLACEHOLDER
 import org.simple.clinic.widgets.UiEvent
 
 data class SummaryBloodPressurePlaceholderListItem(
     private val placeholderNumber: Int,
     private val showHint: Boolean = false
-    // We have three placeholders (might change in the future) so we can't hardcode an id. At the same time, we
-    // we have to choose IDs that will stop it from clashing with other IDs in the adapter, so we multiply the
-    // placeholder number with -20.
-
-    // FIXME: Figure out an automatic solution which will work for all IDs. Also see why we can't use Groupie's
-    // auto generated IDs.
-) : GroupieItemWithUiEvents<SummaryBloodPressurePlaceholderListItem.BpPlaceholderViewHolder>(adapterId = (placeholderNumber * -20).toLong()) {
+) : GroupieItemWithUiEvents<SummaryBloodPressurePlaceholderListItem.BpPlaceholderViewHolder>(adapterId = BP_PLACEHOLDER(placeholderNumber)) {
 
   override lateinit var uiEvents: Subject<UiEvent>
 

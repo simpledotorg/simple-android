fee2417b8c730f181ced17fc1c76a990f00ccb82
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-10-10 16:10:00 +0530

    Change PatientRepository to implement SynceableRepository.

--- app/src/main/java/org/simple/clinic/patient/PatientSaveModel.kt
+++ app/src/main/java/org/simple/clinic/patient/PatientSaveModel.kt
@@ -1,7 +1,3 @@
 package org.simple.clinic.patient
 
-data class PatientSaveModel(
-    val patient: Patient,
-    val address: PatientAddress,
-    val phoneNumbers: List<PatientPhoneNumber>
-)
+data class PatientSaveModel(val patient: Patient, val address: PatientAddress, val phoneNumbers: List<PatientPhoneNumber>)

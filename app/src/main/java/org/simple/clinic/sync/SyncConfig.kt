89ad3355d40509858bc5bcfdba86d609772efefd
Pratul Kalia <pratul@uncommon.is>
2018-10-11 17:45:16 +0530

    Update WorkManager, Play Services Auth, and Play Services Vision

--- app/src/main/java/org/simple/clinic/sync/SyncConfig.kt
+++ app/src/main/java/org/simple/clinic/sync/SyncConfig.kt
@@ -2,11 +2,9 @@ package org.simple.clinic.sync
 
 import org.threeten.bp.Duration
 
-// TODO: We should think about limiting the number of records to push/pull.
 data class SyncConfig(
 
     val frequency: Duration,
 
-    /** Number of patients to sync in one network call */
-    val batchSize: Int
+    val batchSize: Int // Number of records to sync in per network call
 )

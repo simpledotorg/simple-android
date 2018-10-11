8bf383387574cf55c7add7f5db68665573d96b0c
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-10-11 15:08:04 +0530

    Introduce DataSyncTest to verify some of the behaviour around Syncing.

---
+++ app/src/test/java/org/simple/clinic/sync/DataSyncTest.kt
@@ -0,0 +1,72 @@
+package org.simple.clinic.sync
+
+import com.f2prateek.rx.preferences2.Preference
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Completable
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.simple.clinic.patient.SyncStatus
+import org.simple.clinic.util.Optional
+import org.threeten.bp.Instant
+import java.util.UUID
+
+class DataSyncTest {
+
+  private lateinit var dataSync: DataSync
+  private lateinit var syncConfig: SyncConfig
+  private lateinit var repository: SynceableRepository<Any, Any>
+  private lateinit var lastSyncTimestamp: Preference<Optional<Instant>>
+
+  @Before
+  fun setup() {
+    repository = mock()
+    lastSyncTimestamp = mock()
+
+    dataSync = DataSync(Single.fromCallable { syncConfig })
+  }
+
+  @Test
+  fun `when pending sync records are empty, then the push network call should not be made`() {
+    whenever(repository.recordsWithSyncStatus(SyncStatus.PENDING)).thenReturn(Single.just(emptyList()))
+
+    var networkCallMade = false
+
+    dataSync.push(repository) {
+      networkCallMade = true
+      Single.just(DataPushResponse(emptyList()))
+    }.blockingAwait()
+
+    assertThat(networkCallMade).isFalse()
+  }
+
+  @Test
+  fun `when a push succeeds then any records that were created or updated during the push started should not get marked as synced`() {
+    // TODO: This is tricky to test since it involves concurrency. See if we can change how
+    // the sync itself happens so that this case does not get triggered.
+  }
+
+  @Test
+  fun `if there are validation errors in push, then the failing records should be marked as invalid`() {
+    whenever(repository.recordsWithSyncStatus(SyncStatus.PENDING)).thenReturn(Single.just(listOf(1, 2, 3)))
+    whenever(repository.setSyncStatus(any<SyncStatus>(), any())).thenReturn(Completable.complete())
+    whenever(repository.setSyncStatus(any<List<UUID>>(), any())).thenReturn(Completable.complete())
+
+    val validationErrors = listOf(
+        ValidationErrors(uuid = UUID.randomUUID(), schemaErrorMessages = listOf("error-1")),
+        ValidationErrors(uuid = UUID.randomUUID(), schemaErrorMessages = listOf("error-2"))
+    )
+
+    dataSync.push(repository) {
+      Single.just(DataPushResponse(validationErrors))
+    }.blockingAwait()
+
+    verify(repository).setSyncStatus(SyncStatus.PENDING, SyncStatus.IN_FLIGHT)
+    verify(repository).setSyncStatus(validationErrors.map { it.uuid }, SyncStatus.INVALID)
+  }
+
+}

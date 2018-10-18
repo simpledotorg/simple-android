19dbea811951ebb5380cbf0c27758f583e49ebba
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-10-18 11:50:01 +0530

    Add FacilitySync to the ModelSyncTest parameters
    This needed changes to the ModelSyncTest class -
    1. Stop testing for an exact error on the failing operation because FacilitySync will never call it anyway.
    2. Let ModelSync return from tests early if a particular ModelSync implementation does not perform any of the sync operations.

--- app/src/test/java/org/simple/clinic/sync/ModelSyncTest.kt
+++ app/src/test/java/org/simple/clinic/sync/ModelSyncTest.kt
@@ -9,23 +9,38 @@ import junitparams.JUnitParamsRunner
 import junitparams.Parameters
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.simple.clinic.facility.FacilitySync
 import org.simple.clinic.patient.sync.PatientSync
+import org.simple.clinic.sync.ModelSyncTest.SyncOperation.PULL
+import org.simple.clinic.sync.ModelSyncTest.SyncOperation.PUSH
 
 @RunWith(JUnitParamsRunner::class)
 class ModelSyncTest {
 
   @Suppress("Unused")
-  private fun `sync models that both push and pull`(): List<(SyncCoordinator) -> ModelSync> {
+  private fun `sync models that both push and pull`(): List<List<Any>> {
     return listOf(
-        { syncCoordinator: SyncCoordinator -> PatientSync(syncCoordinator, mock(), mock(), mock()) }
+        listOf<Any>(
+            { syncCoordinator: SyncCoordinator -> PatientSync(syncCoordinator, mock(), mock(), mock()) },
+            setOf(PUSH, PULL)
+        ),
+        listOf<Any>(
+            { syncCoordinator: SyncCoordinator -> FacilitySync(syncCoordinator, mock(), mock(), mock()) },
+            setOf(PULL)
+        )
     )
   }
 
   @Test
   @Parameters(method = "sync models that both push and pull")
   fun <T : Any, P : Any> `errors during push should not affect pull`(
-      modelSyncProvider: (SyncCoordinator) -> ModelSync
+      modelSyncProvider: (SyncCoordinator) -> ModelSync,
+      supportedSyncOperations: Set<SyncOperation>
   ) {
+    if ((PULL in supportedSyncOperations).not()) {
+      return
+    }
+
     val syncCoordinator = mock<SyncCoordinator>()
     var pullCompleted = false
 
@@ -36,9 +51,8 @@ class ModelSyncTest {
     val modelSync = modelSyncProvider(syncCoordinator)
 
     modelSync.sync()
-        .test()
-        .await()
-        .assertError(RuntimeException::class.java)
+        .onErrorComplete()
+        .blockingAwait()
 
     assertThat(pullCompleted).isTrue()
   }
@@ -46,22 +60,30 @@ class ModelSyncTest {
   @Test
   @Parameters(method = "sync models that both push and pull")
   fun <T : Any, P : Any> `errors during pull should not affect push`(
-      modelSyncProvider: (SyncCoordinator) -> ModelSync
+      modelSyncProvider: (SyncCoordinator) -> ModelSync,
+      supportedSyncOperations: Set<SyncOperation>
   ) {
-    val dataSync = mock<SyncCoordinator>()
+    if ((PUSH in supportedSyncOperations).not()) {
+      return
+    }
+
+    val syncCoordinator = mock<SyncCoordinator>()
     var pushCompleted = false
 
-    whenever(dataSync.pull(any<SynceableRepository<T, P>>(), any(), any())).thenReturn(Completable.error(RuntimeException()))
-    whenever(dataSync.push(any<SynceableRepository<T, P>>(), any()))
+    whenever(syncCoordinator.pull(any<SynceableRepository<T, P>>(), any(), any())).thenReturn(Completable.error(RuntimeException()))
+    whenever(syncCoordinator.push(any<SynceableRepository<T, P>>(), any()))
         .thenReturn(Completable.complete().doOnComplete { pushCompleted = true })
 
-    val modelSync = modelSyncProvider(dataSync)
+    val modelSync = modelSyncProvider(syncCoordinator)
 
     modelSync.sync()
-        .test()
-        .await()
-        .assertError(RuntimeException::class.java)
+        .onErrorComplete()
+        .blockingAwait()
 
     assertThat(pushCompleted).isTrue()
+  }
+
+  enum class SyncOperation {
+    PUSH, PULL
   }
 }

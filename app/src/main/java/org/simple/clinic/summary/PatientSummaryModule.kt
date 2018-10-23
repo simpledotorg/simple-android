0b166160cc3ad8d7085b4b4dcf3f2a546eb53a6f
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-10-23 14:17:52 +0530

    Provide the config as a Single to PatientSummaryScreenController
    Earlier, it was passed in directly to the constructor, which made testing the class a little trick since we force to a single instace of Config for all tests.  This makes tests a lot more independent and configurable since we can provide it separately in every test if required.

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryModule.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryModule.kt
@@ -2,11 +2,15 @@ package org.simple.clinic.summary
 
 import dagger.Module
 import dagger.Provides
+import io.reactivex.Single
 import org.threeten.bp.Duration
 
 @Module
 class PatientSummaryModule {
 
   @Provides
-  fun providesSummaryConfig() = PatientSummaryConfig(numberOfBpPlaceholders = 3, bpEditableFor = Duration.ofMinutes(1L))
+  fun providesSummaryConfig(): Single<PatientSummaryConfig> = Single.just(PatientSummaryConfig(
+      numberOfBpPlaceholders = 3,
+      bpEditableFor = Duration.ofMinutes(1L)
+  ))
 }

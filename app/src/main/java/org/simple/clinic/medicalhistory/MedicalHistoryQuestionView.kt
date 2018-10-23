4a39b14895ffdd91f0fed9713435cf426fa3efb3
Sanchita Agarwal <sanchita@uncommon.is>
2018-10-23 18:18:42 +0530

    Use answer enums instead of booleans in MedicalHistory and add DB migration

--- app/src/main/java/org/simple/clinic/medicalhistory/MedicalHistoryQuestionView.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/MedicalHistoryQuestionView.kt
@@ -2,62 +2,81 @@ package org.simple.clinic.medicalhistory
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.CompoundButton
 import android.widget.FrameLayout
 import android.widget.TextView
-import com.jakewharton.rxbinding2.InitialValueObservable
-import com.jakewharton.rxbinding2.widget.RxCompoundButton
+import io.reactivex.Observable
 import kotterknife.bindView
 import org.simple.clinic.R
-import org.simple.clinic.widgets.CheckBoxWithSuppressibleListener
+import org.simple.clinic.widgets.setCompoundDrawableStart
+import org.simple.clinic.widgets.setHorizontalPadding
 
 @SuppressLint("ClickableViewAccessibility")
 class MedicalHistoryQuestionView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
 
   private val labelTextView by bindView<TextView>(R.id.newmedicalhistory_item_label)
-  private val containerFrame by bindView<ViewGroup>(R.id.newmedicalhistory_item_frame)
-  private val checkBox by bindView<CheckBoxWithSuppressibleListener>(R.id.newmedicalhistory_item_checkbox)
   private val divider by bindView<View>(R.id.newmedicalhistory_item_divider)
+  private val checkYes by bindView<CompoundButton>(R.id.newmedicalhistory_item_yes)
+  private val checkNo by bindView<CompoundButton>(R.id.newmedicalhistory_item_no)
 
   lateinit var question: MedicalHistoryQuestion
 
-  var isChecked: Boolean
-    get() = checkBox.isChecked
-    set(value) {
-      checkBox.isChecked = value
-    }
+//  var isChecked: Boolean
+//    get() = checkYes.isChecked
+//    set(value) {
+//        checkYes.isChecked = value
+//        checkNo.isChecked = value.not()
+//    }
 
-  fun setCheckedWithoutListener(checked: Boolean) {
-    checkBox.runWithoutListener {
-      checkBox.isChecked = checked
+  var isChecked: Boolean
+    get() = true
+    set(value) {
+//        checkYes.isChecked = value
+//        checkNo.isChecked = value.not()
     }
-  }
 
   init {
-    LayoutInflater.from(context).inflate(R.layout.list_medical_history_question, this, true)
+    LayoutInflater.from(context).inflate(R.layout.list_medical_history_question2, this, true)
 
-    // The entire View should show a touch feedback instead of just the CheckBox.
-    setOnClickListener {
-      checkBox.isChecked = checkBox.isChecked.not()
+    val color: (Int) -> Int = { colorRes -> ContextCompat.getColor(context, colorRes) }
+    val setCheckedState: (CompoundButton, Boolean) -> Unit = { button, isChecked ->
+      button.apply {
+        when {
+          isChecked -> {
+            setTextColor(color(R.color.white100))
+            setCompoundDrawableStart(R.drawable.ic_done_16dp)
+            setHorizontalPadding(R.dimen.medicalhistory_unselected_padding)
+          }
+          else -> {
+            setTextColor(color(R.color.blue1))
+            setCompoundDrawableStart(null)
+            setHorizontalPadding(R.dimen.medicalhistory_selected_padding)
+          }
+        }
+      }
     }
-    checkBox.setOnTouchListener { _, event -> this.onTouchEvent(event) }
 
-    // Transferring padding so that external usages can set a padding on the internal frame
-    // directly that contains a touch selector. <merge> would have been a better option, but
-    // attributes on <merge> tag don't get merged.
-    containerFrame.setPaddingRelative(paddingStart, paddingTop, paddingEnd, paddingBottom)
-    setPaddingRelative(0, 0, 0, 0)
+    val listener = { buttonView: CompoundButton, isChecked: Boolean ->
+      setCheckedState(buttonView,isChecked)
+    }
+    checkYes.setOnCheckedChangeListener(listener)
+    checkNo.setOnCheckedChangeListener(listener)
+
+    listener(checkYes, checkYes.isChecked)
+    listener(checkNo, checkYes.isChecked)
   }
 
   fun setOnCheckedChangeListener(listener: (View, Boolean) -> Unit) {
-    checkBox.setOnCheckedChangeListener(listener)
+   // checkBox.setOnCheckedChangeListener(listener)
   }
 
-  fun checkedChanges(): InitialValueObservable<Boolean> {
-    return RxCompoundButton.checkedChanges(checkBox)
+  fun checkedChanges(): Observable<Boolean> {
+  //  return RxCompoundButton.checkedChanges(checkYes)
+    return Observable.empty<Boolean>()
   }
 
   fun hideDivider() {
--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.medicalhistory.newentry
 
+import org.simple.clinic.medicalhistory.MedicalHistory
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
 import org.simple.clinic.widgets.UiEvent
 
@@ -7,6 +8,6 @@ class SaveMedicalHistoryClicked : UiEvent {
   override val analyticsName = "New Medical History:Save Clicked"
 }
 
-data class NewMedicalHistoryAnswerToggled(val question: MedicalHistoryQuestion, val selected: Boolean) : UiEvent {
-  override val analyticsName = "New Medical History:Answer for $question set to $selected"
+data class NewMedicalHistoryAnswerToggled(val question: MedicalHistoryQuestion, val answer: MedicalHistory.Answer) : UiEvent {
+  override val analyticsName = "New Medical History:Answer for $question set to $answer"
 }

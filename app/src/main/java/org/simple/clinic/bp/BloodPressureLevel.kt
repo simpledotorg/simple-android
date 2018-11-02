52f21bbf55abfe3d2e4616084c9fe8c04bbe9523
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-11-02 14:02:20 +0530

    Change label for all high BP levels to 'High'
    Previously, we had two more levels 'Very High' and 'Extremely High' which have now been removed.

--- app/src/main/java/org/simple/clinic/bp/BloodPressureLevel.kt
+++ app/src/main/java/org/simple/clinic/bp/BloodPressureLevel.kt
@@ -8,10 +8,10 @@ import org.simple.clinic.util.Optional
 enum class BloodPressureLevel(private val urgency: Int, val displayTextRes: Optional<Int>) {
 
   EXTREMELY_HIGH
-      (4, Just(R.string.bloodpressure_level_extremely_high)),
+      (4, Just(R.string.bloodpressure_level_moderately_high)),
 
   VERY_HIGH
-      (3, Just(R.string.bloodpressure_level_very_high)),
+      (3, Just(R.string.bloodpressure_level_moderately_high)),
 
   MODERATELY_HIGH
       (2, Just(R.string.bloodpressure_level_moderately_high)),

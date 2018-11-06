2e039221f1679fdd4cfebc610aad7cbae4476ed9
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-11-06 10:29:09 +0530

    Validate entries when editing patient data

--- app/src/test/java/org/simple/clinic/editpatient/PatientEditScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/editpatient/PatientEditScreenControllerTest.kt
@@ -12,27 +12,47 @@ import junitparams.Parameters
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.simple.clinic.editpatient.PatientEditValidationError.COLONY_OR_VILLAGE_EMPTY
+import org.simple.clinic.editpatient.PatientEditValidationError.DISTRICT_EMPTY
+import org.simple.clinic.editpatient.PatientEditValidationError.FULL_NAME_EMPTY
+import org.simple.clinic.editpatient.PatientEditValidationError.PHONE_NUMBER_EMPTY
+import org.simple.clinic.editpatient.PatientEditValidationError.PHONE_NUMBER_LENGTH_TOO_LONG
+import org.simple.clinic.editpatient.PatientEditValidationError.PHONE_NUMBER_LENGTH_TOO_SHORT
+import org.simple.clinic.editpatient.PatientEditValidationError.STATE_EMPTY
+import org.simple.clinic.patient.Gender
 import org.simple.clinic.patient.Patient
 import org.simple.clinic.patient.PatientAddress
 import org.simple.clinic.patient.PatientMocker
 import org.simple.clinic.patient.PatientPhoneNumber
 import org.simple.clinic.patient.PatientRepository
+import org.simple.clinic.registration.phone.PhoneNumberValidator
+import org.simple.clinic.registration.phone.PhoneNumberValidator.Result.BLANK
+import org.simple.clinic.registration.phone.PhoneNumberValidator.Result.LENGTH_TOO_LONG
+import org.simple.clinic.registration.phone.PhoneNumberValidator.Result.LENGTH_TOO_SHORT
+import org.simple.clinic.registration.phone.PhoneNumberValidator.Result.VALID
 import org.simple.clinic.util.Just
-import org.simple.clinic.util.Optional
+import org.simple.clinic.util.None
 import org.simple.clinic.util.toOptional
 import org.simple.clinic.widgets.UiEvent
+import java.util.UUID
 
 @RunWith(JUnitParamsRunner::class)
 class PatientEditScreenControllerTest {
 
-  private val screen = mock<PatientEditScreen>()
-  private val patientRepository = mock<PatientRepository>()
-
   private val uiEvents = PublishSubject.create<UiEvent>()
-  private val controller = PatientEditScreenController(patientRepository)
+
+  private lateinit var screen: PatientEditScreen
+  private lateinit var patientRepository: PatientRepository
+  private lateinit var numberValidator: PhoneNumberValidator
+  private lateinit var controller: PatientEditScreenController
 
   @Before
   fun setUp() {
+    screen = mock()
+    patientRepository = mock()
+    numberValidator = mock()
+
+    controller = PatientEditScreenController(patientRepository, numberValidator)
 
     uiEvents
         .compose(controller)
@@ -95,6 +115,287 @@ class PatientEditScreenControllerTest {
         generateTestData(null, phoneNumber = "1111111111"),
         generateTestData("", phoneNumber = "1111111111"),
         generateTestData(colonyOrVillage = "Colony", phoneNumber = null)
+    )
+  }
+
+  @Test
+  fun `when save is clicked, patient name should be validated`() {
+    whenever(numberValidator.validate(any(), any())).thenReturn(VALID)
+    whenever(patientRepository.patient(any())).thenReturn(Observable.just(PatientMocker.patient().toOptional()))
+    whenever(patientRepository.address(any())).thenReturn(Observable.just(PatientMocker.address().toOptional()))
+    whenever(patientRepository.phoneNumbers(any())).thenReturn(Observable.just(None))
+
+    uiEvents.onNext(PatientEditScreenCreated(UUID.randomUUID()))
+
+    uiEvents.onNext(PatientEditPhoneNumberTextChanged(""))
+    uiEvents.onNext(PatientEditGenderChanged(Gender.MALE))
+    uiEvents.onNext(PatientEditColonyOrVillageChanged("Colony"))
+    uiEvents.onNext(PatientEditDistrictTextChanged("District"))
+    uiEvents.onNext(PatientEditStateTextChanged("State"))
+
+    uiEvents.onNext(PatientEditPatientNameTextChanged(""))
+    uiEvents.onNext(PatientEditSaveClicked())
+
+    verify(screen).showValidationErrors(setOf(FULL_NAME_EMPTY))
+  }
+
+  @Test
+  @Parameters(method = "params for validating phone numbers")
+  fun `when save is clicked, phone number should be validated`(
+      alreadyPresentPhoneNumber: PatientPhoneNumber?,
+      numberValidationResult: PhoneNumberValidator.Result,
+      expectedError: PatientEditValidationError?
+  ) {
+    whenever(patientRepository.phoneNumbers(any())).thenReturn(Observable.just(alreadyPresentPhoneNumber.toOptional()))
+    whenever(patientRepository.patient(any())).thenReturn(Observable.just(PatientMocker.patient().toOptional()))
+    whenever(patientRepository.address(any())).thenReturn(Observable.just(PatientMocker.address().toOptional()))
+
+    whenever(numberValidator.validate(any(), any())).thenReturn(numberValidationResult)
+
+    uiEvents.onNext(PatientEditScreenCreated(UUID.randomUUID()))
+
+    uiEvents.onNext(PatientEditGenderChanged(Gender.MALE))
+    uiEvents.onNext(PatientEditColonyOrVillageChanged("Colony"))
+    uiEvents.onNext(PatientEditDistrictTextChanged("District"))
+    uiEvents.onNext(PatientEditStateTextChanged("State"))
+    uiEvents.onNext(PatientEditPatientNameTextChanged("Name"))
+
+    uiEvents.onNext(PatientEditPhoneNumberTextChanged(""))
+    uiEvents.onNext(PatientEditSaveClicked())
+
+    if (expectedError == null) {
+      verify(screen, never()).showValidationErrors(any())
+    } else {
+      verify(screen).showValidationErrors(setOf(expectedError))
+    }
+  }
+
+  @Suppress("Unused")
+  private fun `params for validating phone numbers`(): List<List<Any?>> {
+    return listOf(
+        listOf(null, BLANK, null),
+        listOf(null, LENGTH_TOO_LONG, PHONE_NUMBER_LENGTH_TOO_LONG),
+        listOf(null, LENGTH_TOO_SHORT, PHONE_NUMBER_LENGTH_TOO_SHORT),
+        listOf(PatientMocker.phoneNumber(), BLANK, PHONE_NUMBER_EMPTY),
+        listOf(PatientMocker.phoneNumber(), LENGTH_TOO_SHORT, PHONE_NUMBER_LENGTH_TOO_SHORT),
+        listOf(PatientMocker.phoneNumber(), LENGTH_TOO_LONG, PHONE_NUMBER_LENGTH_TOO_LONG)
+    )
+  }
+
+  @Test
+  fun `when save is clicked, the colony should be validated`() {
+    whenever(numberValidator.validate(any(), any())).thenReturn(VALID)
+    whenever(patientRepository.patient(any())).thenReturn(Observable.just(PatientMocker.patient().toOptional()))
+    whenever(patientRepository.address(any())).thenReturn(Observable.just(PatientMocker.address().toOptional()))
+    whenever(patientRepository.phoneNumbers(any())).thenReturn(Observable.just(None))
+
+    uiEvents.onNext(PatientEditScreenCreated(UUID.randomUUID()))
+
+    uiEvents.onNext(PatientEditPhoneNumberTextChanged(""))
+    uiEvents.onNext(PatientEditGenderChanged(Gender.MALE))
+    uiEvents.onNext(PatientEditDistrictTextChanged("District"))
+    uiEvents.onNext(PatientEditStateTextChanged("State"))
+    uiEvents.onNext(PatientEditPatientNameTextChanged("Name"))
+
+    uiEvents.onNext(PatientEditColonyOrVillageChanged(""))
+    uiEvents.onNext(PatientEditSaveClicked())
+
+    verify(screen).showValidationErrors(setOf(COLONY_OR_VILLAGE_EMPTY))
+  }
+
+  @Test
+  fun `when save is clicked, the district should be validated`() {
+    whenever(numberValidator.validate(any(), any())).thenReturn(VALID)
+    whenever(patientRepository.patient(any())).thenReturn(Observable.just(PatientMocker.patient().toOptional()))
+    whenever(patientRepository.address(any())).thenReturn(Observable.just(PatientMocker.address().toOptional()))
+    whenever(patientRepository.phoneNumbers(any())).thenReturn(Observable.just(None))
+
+    uiEvents.onNext(PatientEditScreenCreated(UUID.randomUUID()))
+
+    uiEvents.onNext(PatientEditPhoneNumberTextChanged(""))
+    uiEvents.onNext(PatientEditGenderChanged(Gender.MALE))
+    uiEvents.onNext(PatientEditColonyOrVillageChanged("Colony"))
+    uiEvents.onNext(PatientEditStateTextChanged("State"))
+    uiEvents.onNext(PatientEditPatientNameTextChanged("Name"))
+
+    uiEvents.onNext(PatientEditDistrictTextChanged(""))
+    uiEvents.onNext(PatientEditSaveClicked())
+
+    verify(screen).showValidationErrors(setOf(DISTRICT_EMPTY))
+  }
+
+  @Test
+  fun `when save is clicked, the state should be validated`() {
+    whenever(numberValidator.validate(any(), any())).thenReturn(VALID)
+    whenever(patientRepository.patient(any())).thenReturn(Observable.just(PatientMocker.patient().toOptional()))
+    whenever(patientRepository.address(any())).thenReturn(Observable.just(PatientMocker.address().toOptional()))
+    whenever(patientRepository.phoneNumbers(any())).thenReturn(Observable.just(None))
+
+    uiEvents.onNext(PatientEditScreenCreated(UUID.randomUUID()))
+
+    uiEvents.onNext(PatientEditPhoneNumberTextChanged(""))
+    uiEvents.onNext(PatientEditGenderChanged(Gender.MALE))
+    uiEvents.onNext(PatientEditColonyOrVillageChanged("Colony"))
+    uiEvents.onNext(PatientEditDistrictTextChanged("District"))
+    uiEvents.onNext(PatientEditPatientNameTextChanged("Name"))
+
+    uiEvents.onNext(PatientEditStateTextChanged(""))
+    uiEvents.onNext(PatientEditSaveClicked())
+
+    verify(screen).showValidationErrors(setOf(STATE_EMPTY))
+  }
+
+  @Test
+  @Parameters(method = "params for validating all fields on save clicks")
+  fun `when save is clicked, all fields should be validated`(
+      alreadyPresentPhoneNumber: PatientPhoneNumber?,
+      name: String,
+      numberValidationResult: PhoneNumberValidator.Result,
+      colonyOrVillage: String,
+      district: String,
+      state: String,
+      expectedErrors: Set<PatientEditValidationError>
+  ) {
+    whenever(patientRepository.phoneNumbers(any())).thenReturn(Observable.just(alreadyPresentPhoneNumber.toOptional()))
+    whenever(patientRepository.patient(any())).thenReturn(Observable.just(PatientMocker.patient().toOptional()))
+    whenever(patientRepository.address(any())).thenReturn(Observable.just(PatientMocker.address().toOptional()))
+
+    whenever(numberValidator.validate(any(), any())).thenReturn(numberValidationResult)
+
+    uiEvents.onNext(PatientEditScreenCreated(UUID.randomUUID()))
+
+    uiEvents.onNext(PatientEditPatientNameTextChanged(name))
+    uiEvents.onNext(PatientEditPhoneNumberTextChanged(""))
+    uiEvents.onNext(PatientEditColonyOrVillageChanged(colonyOrVillage))
+    uiEvents.onNext(PatientEditDistrictTextChanged(district))
+    uiEvents.onNext(PatientEditStateTextChanged(state))
+    uiEvents.onNext(PatientEditGenderChanged(Gender.MALE))
+
+    uiEvents.onNext(PatientEditSaveClicked())
+
+    if (expectedErrors.isNotEmpty()) {
+      verify(screen).showValidationErrors(expectedErrors)
+    } else {
+      verify(screen, never()).showValidationErrors(any())
+    }
+  }
+
+  @Suppress("Unused")
+  private fun `params for validating all fields on save clicks`(): List<List<Any?>> {
+    return listOf(
+        listOf(
+            PatientMocker.phoneNumber(),
+            "",
+            BLANK,
+            "",
+            "",
+            "",
+            setOf(FULL_NAME_EMPTY, PHONE_NUMBER_EMPTY, COLONY_OR_VILLAGE_EMPTY, DISTRICT_EMPTY, STATE_EMPTY)
+        ),
+        listOf(
+            null,
+            "",
+            BLANK,
+            "",
+            "",
+            "",
+            setOf(FULL_NAME_EMPTY, COLONY_OR_VILLAGE_EMPTY, DISTRICT_EMPTY, STATE_EMPTY)
+        ),
+
+        listOf(
+            PatientMocker.phoneNumber(),
+            "",
+            LENGTH_TOO_SHORT,
+            "Colony",
+            "",
+            "",
+            setOf(FULL_NAME_EMPTY, PHONE_NUMBER_LENGTH_TOO_SHORT, DISTRICT_EMPTY, STATE_EMPTY)
+        ),
+        listOf(
+            null,
+            "",
+            LENGTH_TOO_SHORT,
+            "Colony",
+            "",
+            "",
+            setOf(FULL_NAME_EMPTY, PHONE_NUMBER_LENGTH_TOO_SHORT, DISTRICT_EMPTY, STATE_EMPTY)
+        ),
+
+        listOf(
+            PatientMocker.phoneNumber(),
+            "Name",
+            LENGTH_TOO_LONG,
+            "",
+            "District",
+            "",
+            setOf(PHONE_NUMBER_LENGTH_TOO_LONG, COLONY_OR_VILLAGE_EMPTY, STATE_EMPTY)
+        ),
+        listOf(
+            null,
+            "Name",
+            LENGTH_TOO_LONG,
+            "",
+            "District",
+            "",
+            setOf(PHONE_NUMBER_LENGTH_TOO_LONG, COLONY_OR_VILLAGE_EMPTY, STATE_EMPTY)
+        ),
+
+        listOf(
+            PatientMocker.phoneNumber(),
+            "",
+            VALID,
+            "Colony",
+            "District",
+            "",
+            setOf(FULL_NAME_EMPTY, STATE_EMPTY)
+        ),
+        listOf(
+            null,
+            "",
+            VALID,
+            "Colony",
+            "District",
+            "",
+            setOf(FULL_NAME_EMPTY, STATE_EMPTY)
+        ),
+
+        listOf(
+            PatientMocker.phoneNumber(),
+            "",
+            BLANK,
+            "Colony",
+            "District",
+            "State",
+            setOf(FULL_NAME_EMPTY, PHONE_NUMBER_EMPTY)
+        ),
+        listOf(
+            null,
+            "",
+            BLANK,
+            "Colony",
+            "District",
+            "State",
+            setOf(FULL_NAME_EMPTY)
+        ),
+
+        listOf(
+            PatientMocker.phoneNumber(),
+            "Name",
+            VALID,
+            "Colony",
+            "District",
+            "State",
+            emptySet<PatientEditValidationError>()
+        ),
+        listOf(
+            null,
+            "Name",
+            VALID,
+            "Colony",
+            "District",
+            "State",
+            emptySet<PatientEditValidationError>()
+        )
     )
   }
 }

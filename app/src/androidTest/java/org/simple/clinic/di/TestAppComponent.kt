57defb46e91edf3b3cfd1605708cb4e35bdf5a4c
Saket Narayan <saket@uncommon.is>
2018-11-06 17:34:12 +0530

    Add BruteForceProtection for tracking failed PIN authentications

--- app/src/androidTest/java/org/simple/clinic/di/TestAppComponent.kt
+++ app/src/androidTest/java/org/simple/clinic/di/TestAppComponent.kt
@@ -18,6 +18,7 @@ import org.simple.clinic.overdue.communication.CommunicationRepositoryAndroidTes
 import org.simple.clinic.overdue.communication.CommunicationSyncAndroidTest
 import org.simple.clinic.patient.PatientRepositoryAndroidTest
 import org.simple.clinic.patient.PatientSyncAndroidTest
+import org.simple.clinic.pin.BruteForceProtectionAndroidTest
 import org.simple.clinic.summary.RelativeTimestampGeneratorAndroidTest
 import org.simple.clinic.user.OngoingLoginEntryRepositoryTest
 import org.simple.clinic.user.UserDaoAndroidTest
@@ -47,4 +48,5 @@ interface TestAppComponent : AppComponent {
   fun inject(target: PatientSyncAndroidTest)
   fun inject(target: BloodPressureRepositoryAndroidTest)
   fun inject(target: OngoingLoginEntryRepositoryTest)
+  fun inject(target: BruteForceProtectionAndroidTest)
 }

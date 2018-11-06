fb6b4522630899ec68b82ba0398c8633723f742e
Saket Narayan <saket@uncommon.is>
2018-11-06 15:07:09 +0530

    Migrate BloodPressureSync to use SyncCoordinator

--- app/src/androidTest/java/org/simple/clinic/bp/sync/BloodPressureSyncAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/bp/sync/BloodPressureSyncAndroidTest.kt
@@ -2,155 +2,54 @@ package org.simple.clinic.bp.sync
 
 import android.support.test.runner.AndroidJUnit4
 import com.f2prateek.rx.preferences2.Preference
-import com.google.common.truth.Truth.assertThat
-import io.bloco.faker.Faker
-import io.reactivex.Completable
-import io.reactivex.Observable
-import io.reactivex.Single
 import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
-import org.simple.clinic.AppDatabase
-import org.simple.clinic.AuthenticationRule
 import org.simple.clinic.TestClinicApp
 import org.simple.clinic.TestData
+import org.simple.clinic.bp.BloodPressureMeasurement
 import org.simple.clinic.bp.BloodPressureRepository
-import org.simple.clinic.facility.Facility
-import org.simple.clinic.patient.Gender
-import org.simple.clinic.patient.Patient
-import org.simple.clinic.patient.PatientAddress
-import org.simple.clinic.patient.PatientStatus
 import org.simple.clinic.patient.SyncStatus
-import org.simple.clinic.sync.SyncConfig
-import org.simple.clinic.user.UserSession
-import org.simple.clinic.util.Just
+import org.simple.clinic.sync.BaseSyncCoordinatorAndroidTest
 import org.simple.clinic.util.Optional
 import org.threeten.bp.Instant
-import org.threeten.bp.LocalDate
-import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Named
 
 @RunWith(AndroidJUnit4::class)
-class BloodPressureSyncAndroidTest {
-
-  @Inject
-  lateinit var configProvider: Single<SyncConfig>
+class BloodPressureSyncAndroidTest : BaseSyncCoordinatorAndroidTest<BloodPressureMeasurement, BloodPressureMeasurementPayload>() {
 
   @Inject
   lateinit var repository: BloodPressureRepository
-
-  @Inject
-  lateinit var userSession: UserSession
-
-  @Inject
-  lateinit var database: AppDatabase
 
   @Inject
   @field:Named("last_bp_pull_timestamp")
   lateinit var lastPullTimestamp: Preference<Optional<Instant>>
 
   @Inject
+  lateinit var syncApi: BloodPressureSyncApiV1
+
+  @Inject
   lateinit var bpSync: BloodPressureSync
 
   @Inject
-  lateinit var faker: Faker
-
-  @Inject
   lateinit var testData: TestData
-
-  @get:Rule
-  val authenticationRule = AuthenticationRule()
 
   @Before
   fun setUp() {
     TestClinicApp.appComponent().inject(this)
   }
 
-  private fun insertDummyMeasurements(count: Int): Completable {
-    val facilityUUID = testData.qaUserFacilityUuid()
-    database.facilityDao().save(listOf(
-        Facility(
-            facilityUUID,
-            faker.company.name(),
-            null,
-            null,
-            null,
-            faker.address.city(),
-            faker.address.state(),
-            "India",
-            null,
-            Instant.now(),
-            Instant.now(),
-            SyncStatus.DONE
-        )
-    ))
+  override fun push() = bpSync.push()
 
-    val addressUuid = UUID.randomUUID()
-    database.addressDao().save(
-        PatientAddress(
-            addressUuid,
-            null,
-            faker.address.city(),
-            faker.address.state(),
-            "India",
-            Instant.now(),
-            Instant.now()
-        )
-    )
+  override fun pull() = bpSync.pull()
 
-    val patientUuid = UUID.randomUUID()
-    database.patientDao().save(
-        Patient(
-            patientUuid,
-            addressUuid,
-            faker.name.name(),
-            faker.name.name(),
-            Gender.FEMALE,
-            LocalDate.parse("1947-08-15"),
-            null,
-            PatientStatus.ACTIVE,
-            Instant.now(),
-            Instant.now(),
-            SyncStatus.DONE
-        ))
+  override fun repository() = repository
 
-    return Observable.range(0, count)
-        .flatMapSingle { index ->
-          repository.saveMeasurement(patientUuid, systolic = 100 + index, diastolic = 50 + index)
-        }
-        .ignoreElements()
-  }
+  override fun generateRecord(syncStatus: SyncStatus) = testData.bloodPressureMeasurement(syncStatus = syncStatus)
 
-  @Test
-  fun when_pending_sync_measurements_are_present_then_they_should_be_pushed_to_the_server_and_marked_as_synced_on_success() {
-    insertDummyMeasurements(count = 5)
-        .andThen(bpSync.push())
-        .blockingAwait()
+  override fun generatePayload() = testData.bpPayload()
 
-    repository.measurementsWithSyncStatus(SyncStatus.DONE)
-        .test()
-        .await()
-        .assertValue { measurements -> measurements.size == 5 }
-        .assertComplete()
-        .assertNoErrors()
-  }
+  override fun lastPullTimestamp() = lastPullTimestamp
 
-  @Test
-  fun when_pulling_measurements_then_paginate_till_the_server_does_not_have_anymore_measurements() {
-    lastPullTimestamp.set(Just(Instant.EPOCH))
-
-    val measurementsToInsert = 2 * configProvider.blockingGet().batchSize + 7
-
-    insertDummyMeasurements(count = measurementsToInsert)
-        .andThen(bpSync.push())
-        .andThen(Completable.fromAction { database.bloodPressureDao().clearData() })
-        .blockingAwait()
-
-    bpSync.pull().blockingAwait()
-
-    val measurementCountAfterPull = repository.measurementCount().blockingGet()
-    assertThat(measurementCountAfterPull).isAtLeast(measurementsToInsert)
-  }
+  override fun pushNetworkCall(payloads: List<BloodPressureMeasurementPayload>) = syncApi.push(BloodPressurePushRequest(payloads))
 }
--- app/src/main/java/org/simple/clinic/bp/sync/BloodPressureSync.kt
+++ app/src/main/java/org/simple/clinic/bp/sync/BloodPressureSync.kt
@@ -2,97 +2,29 @@ package org.simple.clinic.bp.sync
 
 import com.f2prateek.rx.preferences2.Preference
 import io.reactivex.Completable
-import io.reactivex.Observable
-import io.reactivex.Single
-import io.reactivex.rxkotlin.toCompletable
-import io.reactivex.schedulers.Schedulers
+import org.simple.clinic.bp.BloodPressureMeasurement
 import org.simple.clinic.bp.BloodPressureRepository
-import org.simple.clinic.patient.SyncStatus
-import org.simple.clinic.sync.DataPushResponse
-import org.simple.clinic.sync.SyncConfig
-import org.simple.clinic.util.Just
-import org.simple.clinic.util.None
+import org.simple.clinic.sync.SyncCoordinator
 import org.simple.clinic.util.Optional
 import org.threeten.bp.Instant
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Named
 
 class BloodPressureSync @Inject constructor(
+    private val syncCoordinator: SyncCoordinator,
     private val api: BloodPressureSyncApiV1,
     private val repository: BloodPressureRepository,
-    private val configProvider: Single<SyncConfig>,
     @Named("last_bp_pull_timestamp") private val lastPullTimestamp: Preference<Optional<Instant>>
 ) {
 
-  fun sync(): Completable {
-    return Completable.mergeArrayDelayError(push(), pull())
-  }
+  fun sync(): Completable = Completable.mergeArrayDelayError(push(), pull())
 
-  fun push(): Completable {
-    val recoverStuckRecordsFromPreviousSync = repository.updateMeasurementsSyncStatus(SyncStatus.IN_FLIGHT, SyncStatus.PENDING)
+  fun push() = syncCoordinator.push(repository) { api.push(toRequest(it)) }
 
-    val cachedPendingSyncMeasurements = repository.measurementsWithSyncStatus(SyncStatus.PENDING)
-        // Converting to an Observable because Single#filter() returns a Maybe.
-        // And Maybe#flatMapSingle() throws a NoSuchElementException on completion.
-        .toObservable()
-        .filter { it.isNotEmpty() }
-        .cache()
+  fun pull() = syncCoordinator.pull(repository, lastPullTimestamp, api::pull)
 
-    val pendingToInFlight = cachedPendingSyncMeasurements
-        .flatMapCompletable {
-          repository.updateMeasurementsSyncStatus(oldStatus = SyncStatus.PENDING, newStatus = SyncStatus.IN_FLIGHT)
-        }
-
-    val networkCall = cachedPendingSyncMeasurements
-        .map { patients -> patients.map { it.toPayload() } }
-        .map(::BloodPressurePushRequest)
-        .flatMapSingle { request -> api.push(request) }
-        .doOnNext { response -> logValidationErrorsIfAny(response) }
-        .map { it.validationErrors }
-        .map { errors -> errors.map { it.uuid } }
-        .flatMapCompletable { patientUuidsWithErrors ->
-          repository
-              .updateMeasurementsSyncStatus(oldStatus = SyncStatus.IN_FLIGHT, newStatus = SyncStatus.DONE)
-              .andThen(patientUuidsWithErrors.let {
-                when {
-                  it.isEmpty() -> Completable.complete()
-                  else -> repository.updateMeasurementsSyncStatus(patientUuidsWithErrors, SyncStatus.INVALID)
-                }
-              })
-        }
-
-    return recoverStuckRecordsFromPreviousSync
-        .andThen(pendingToInFlight)
-        .andThen(networkCall)
-  }
-
-  private fun logValidationErrorsIfAny(response: DataPushResponse) {
-    if (response.validationErrors.isNotEmpty()) {
-      Timber.e("Server sent validation errors for BP measurements: ${response.validationErrors}")
-    }
-  }
-
-  fun pull(): Completable {
-    return configProvider
-        .flatMapCompletable { config ->
-          lastPullTimestamp.asObservable()
-              .take(1)
-              .flatMapSingle { lastPullTime ->
-                when (lastPullTime) {
-                  is Just -> api.pull(recordsToPull = config.batchSize, lastPullTimestamp = lastPullTime.value)
-                  is None -> api.pull(recordsToPull = config.batchSize)
-                }
-              }
-              .flatMap { response ->
-                repository.mergeWithLocalData(response.measurements)
-                    .observeOn(Schedulers.single())
-                    .andThen({ lastPullTimestamp.set(Just(response.processedSinceTimestamp)) }.toCompletable())
-                    .andThen(Observable.just(response))
-              }
-              .repeat()
-              .takeWhile { response -> response.measurements.size >= config.batchSize }
-              .ignoreElements()
-        }
+  private fun toRequest(measurements: List<BloodPressureMeasurement>): BloodPressurePushRequest {
+    val payloads = measurements.map { it.toPayload() }
+    return BloodPressurePushRequest(payloads)
   }
 }

faef51702617d4f732e8f8c3d7be74c017b96285
Sanchita Agarwal <sanchita@uncommon.is>
2018-11-14 18:18:41 +0530

    Add CheckBoxWithSuppressibleListener to fix Medical history issue in PatientSummaryScreen

---
+++ app/src/main/java/org/simple/clinic/widgets/CheckboxWithSuppressibleListener.kt
@@ -0,0 +1,22 @@
+package org.simple.clinic.widgets
+
+import android.content.Context
+import android.support.v7.widget.AppCompatCheckBox
+import android.util.AttributeSet
+
+class CheckBoxWithSuppressibleListener(context: Context, attrs: AttributeSet) : AppCompatCheckBox(context, attrs) {
+
+  private var listener: OnCheckedChangeListener? = null
+
+  override fun setOnCheckedChangeListener(listener: OnCheckedChangeListener?) {
+    super.setOnCheckedChangeListener(listener)
+    this.listener = listener
+  }
+
+  fun runWithoutListener(runner: () -> Unit) {
+    val copy = listener
+    setOnCheckedChangeListener(null)
+    runner()
+    setOnCheckedChangeListener(copy)
+  }
+}

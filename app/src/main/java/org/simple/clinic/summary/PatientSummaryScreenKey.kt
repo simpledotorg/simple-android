8f6894d2a979b7e154769378031d215d1764670b
Sanchita Agarwal <sanchita@uncommon.is>
2018-11-16 20:19:51 +0530

    Add screen created time to Patient summary

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryScreenKey.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryScreenKey.kt
@@ -5,10 +5,15 @@ import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.R
 import org.simple.clinic.router.screen.FullScreenKey
+import org.threeten.bp.Instant
 import java.util.UUID
 
 @Parcelize
-data class PatientSummaryScreenKey(val patientUuid: UUID, val caller: PatientSummaryCaller) : FullScreenKey, Parcelable {
+data class PatientSummaryScreenKey(
+    val patientUuid: UUID,
+    val caller: PatientSummaryCaller,
+    val screenCreatedTimestamp: Instant
+) : FullScreenKey, Parcelable {
 
   @IgnoredOnParcel
   override val analyticsName = "Patient Summary"

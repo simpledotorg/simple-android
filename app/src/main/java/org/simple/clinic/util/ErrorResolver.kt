13b2eec904a73b40e7ae5fe89579949812049d1d
Saket Narayan <saket@uncommon.is>
2018-11-16 18:00:34 +0530

    Ignore reporting network errors to crash reporter

---
+++ app/src/main/java/org/simple/clinic/util/ErrorResolver.kt
@@ -0,0 +1,49 @@
+package org.simple.clinic.util
+
+import io.reactivex.exceptions.CompositeException
+import okhttp3.internal.http2.ConnectionShutdownException
+import okhttp3.internal.http2.StreamResetException
+import java.io.IOException
+import java.net.SocketException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import kotlin.reflect.KClass
+
+private val KNOWN_NETWORK_ERRORS: Set<KClass<out IOException>> = setOf(
+    SocketException::class,
+    SocketTimeoutException::class,
+    UnknownHostException::class,
+    StreamResetException::class,
+    ConnectionShutdownException::class)
+
+object ErrorResolver {
+
+  fun resolve(error: Throwable): ResolvedError {
+    val actualCause = findActualCause(error)
+
+    return if (error::class in KNOWN_NETWORK_ERRORS) {
+      ResolvedError.NetworkRelated(actualCause)
+    } else {
+      ResolvedError.Unexpected(actualCause)
+    }
+  }
+
+  private fun findActualCause(error: Throwable): Throwable {
+    var actualError = error
+    if (actualError is CompositeException) {
+      // CompositeException wraps the cause inside another
+      // exception called CompositeExceptionCausalChain.
+      actualError = findActualCause(actualError.cause.cause!!)
+    }
+    // This place may identify more wrapped errors
+    // like UndeliverableException, etc. in the future.
+    return actualError
+  }
+}
+
+sealed class ResolvedError(val actualCause: Throwable) {
+
+  class NetworkRelated(actualCause: Throwable) : ResolvedError(actualCause)
+
+  class Unexpected(actualCause: Throwable) : ResolvedError(actualCause)
+}

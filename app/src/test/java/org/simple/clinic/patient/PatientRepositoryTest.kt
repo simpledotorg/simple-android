da8f61c771dd8c35811f8ee48d8bf78cc24f92f8
Vinay Shenoy <vinay.sh@uncommon.is>
2018-11-22 13:04:44 +0530

    Inject SearchPatientByName into PatientRepository

--- app/src/test/java/org/simple/clinic/patient/PatientRepositoryTest.kt
+++ app/src/test/java/org/simple/clinic/patient/PatientRepositoryTest.kt
@@ -18,6 +18,7 @@ import org.simple.clinic.AppDatabase
 import org.simple.clinic.bp.BloodPressureMeasurement
 import org.simple.clinic.facility.FacilityRepository
 import org.simple.clinic.newentry.DateOfBirthFormatValidator
+import org.simple.clinic.patient.filter.SearchPatientByName
 import org.simple.clinic.patient.fuzzy.AgeFuzzer
 import org.simple.clinic.patient.fuzzy.BoundedAge
 import org.simple.clinic.patient.sync.PatientPayload
@@ -33,6 +34,7 @@ class PatientRepositoryTest {
 
   private lateinit var repository: PatientRepository
   private lateinit var ageFuzzer: AgeFuzzer
+  private lateinit var searchPatientByName: SearchPatientByName
 
   private val database = mock<AppDatabase>()
   private val clock = TestClock()
@@ -50,7 +52,17 @@ class PatientRepositoryTest {
   fun setUp() {
     ageFuzzer = mock()
     whenever(ageFuzzer.bounded(any())).thenReturn(BoundedAge(LocalDate.now(clock), LocalDate.now(clock)))
-    repository = PatientRepository(database, dobValidator, facilityRepository, userSession, numberValidator, clock, ageFuzzer)
+    searchPatientByName = mock()
+
+    repository = PatientRepository(
+        database,
+        dobValidator,
+        facilityRepository,
+        userSession,
+        numberValidator,
+        clock,
+        ageFuzzer,
+        searchPatientByName)
 
     val user = PatientMocker.loggedInUser()
     whenever(userSession.requireLoggedInUser()).thenReturn(Observable.just(user))

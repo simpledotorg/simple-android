e36154d438dbe7a4a70fd12cc5cf279d1620c3a9
Vinay Shenoy <vinay.sh@uncommon.is>
2018-11-24 08:21:18 +0530

    Add method for estimating current age based on current age and timestamp at which age was recorded

---
+++ app/src/main/java/org/simple/clinic/util/TimeUtil.kt
@@ -0,0 +1,15 @@
+package org.simple.clinic.util
+
+import org.threeten.bp.Clock
+import org.threeten.bp.Instant
+import org.threeten.bp.LocalDate
+import org.threeten.bp.Period
+import org.threeten.bp.ZoneOffset.UTC
+
+fun estimateCurrentAge(recordedAge: Int, ageRecordedAtTimestamp: Instant, clock: Clock): Int {
+  val ageRecordedAtDate = ageRecordedAtTimestamp.atZone(UTC).toLocalDate()
+  val today = LocalDate.now(clock)
+
+  val yearsPassedSinceAgeRecorded = Period.between(ageRecordedAtDate, today).years
+  return yearsPassedSinceAgeRecorded + recordedAge
+}

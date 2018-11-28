2b98a82a9b0832f903f52cff942b06b452a7fd42
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-11-28 16:56:47 +0530

    Add return type to SortByTotalSumOfDistances

--- app/src/main/java/org/simple/clinic/patient/filter/SortByTotalSumOfDistances.kt
+++ app/src/main/java/org/simple/clinic/patient/filter/SortByTotalSumOfDistances.kt
@@ -2,5 +2,6 @@ package org.simple.clinic.patient.filter
 
 class SortByTotalSumOfDistances : Comparator<PatientSearchContext> {
 
-  override fun compare(first: PatientSearchContext, second: PatientSearchContext) = first.totalEditDistance.compareTo(second.totalEditDistance)
+  override fun compare(first: PatientSearchContext, second: PatientSearchContext): Int =
+      first.totalEditDistance.compareTo(second.totalEditDistance)
 }

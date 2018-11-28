3e87282326f84fec1db7edb27a9b1c87029e6ed6
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-11-28 16:52:25 +0530

    Rename EditDistanceRecord -> EditDistance
    This commit also makes it a nested class of SearchContext.

--- app/src/test/java/org/simple/clinic/patient/filter/SortByTotalSumOfDistancesTest.kt
+++ app/src/test/java/org/simple/clinic/patient/filter/SortByTotalSumOfDistancesTest.kt
@@ -11,62 +11,6 @@ import java.util.UUID
 @RunWith(JUnitParamsRunner::class)
 class SortByTotalSumOfDistancesTest {
 
-//  @Test
-//  @Parameters(method = "params for sorting values")
-//  fun `it should sort the values based on the total sum of the distances`(
-//      originalList: List<SearchInfo>,
-//      expected: List<SearchInfo>
-//  ) {
-//    val sorted = originalList.sortedWith(SortByTotalSumOfDistances())
-//
-//    assertThat(sorted).isEqualTo(expected)
-//  }
-//
-//  @Suppress("Unused")
-//  private fun `params for sorting values`(): List<List<Any>> {
-//    fun generateTestData(distances: List<List<Double>>, results: List<Int>): List<Any> {
-//      val originalList = distances.map {
-//        SearchInfo(
-//            patient = PatientNameAndId(fullName = "Name", uuid = UUID.randomUUID()),
-//            nameParts = emptyList(),
-//            searchParts = emptyList(),
-//            distances = it
-//        )
-//      }
-//      val expected = results.map { originalList[it] }
-//
-//      return listOf(originalList, expected)
-//    }
-//
-//    return listOf(
-//        generateTestData(
-//            distances = listOf(
-//                listOf(1.0),
-//                listOf(2.0),
-//                listOf(3.0)
-//            ),
-//            results = listOf(0, 1, 2)
-//        ),
-//        generateTestData(
-//            distances = listOf(
-//                listOf(1.0),
-//                listOf(0.2, 0.3, 0.4),
-//                listOf(2.0),
-//                listOf(0.5)),
-//            results = listOf(3, 1, 0, 2)
-//        ),
-//        generateTestData(
-//            distances = listOf(
-//                listOf(-0.1, 0.1),
-//                listOf(-0.5),
-//                listOf(-0.5, 0.25, -0.3),
-//                listOf(2.0, -4.0, 3.0),
-//                listOf(4.0)),
-//            results = listOf(2, 1, 0, 3, 4)
-//        )
-//    )
-//  }
-
   @Test
   @Parameters(method = "params for sorting values")
   fun `it should sort the values based on the total sum of the distances`(
@@ -80,13 +24,13 @@ class SortByTotalSumOfDistancesTest {
 
   @Suppress("Unused")
   private fun `params for sorting values`(): List<List<Any>> {
-    fun generateTestData(distances: List<List<EditDistanceRecord>>, results: List<Int>): List<Any> {
+    fun generateTestData(distances: List<List<SearchContext.EditDistance>>, results: List<Int>): List<Any> {
       val originalList = distances.map {
         SearchContext(
             patient = PatientNameAndId(fullName = "Name", uuid = UUID.randomUUID()),
             nameParts = emptyList(),
             searchParts = emptyList(),
-            editDistanceRecords = it
+            editDistances = it
         )
       }
       val expected = results.map { originalList[it] }
@@ -94,7 +38,7 @@ class SortByTotalSumOfDistancesTest {
       return listOf(originalList, expected)
     }
 
-    val template = EditDistanceRecord(namePart = "", searchPart = "", editDistance = 0.0)
+    val template = SearchContext.EditDistance(namePart = "", searchPart = "", editDistance = 0.0)
 
     return listOf(
         generateTestData(

87a3641f6b4283ee65a000af5b6f2f9b6406a96c
Sanchita Agarwal <sanchita@uncommon.is>
2018-12-05 16:15:10 +0530

    Add HomeScreenController and show current facility on HomeScreen

--- app/src/main/java/org/simple/clinic/home/HomeScreen.kt
+++ app/src/main/java/org/simple/clinic/home/HomeScreen.kt
@@ -1,13 +1,22 @@
 package org.simple.clinic.home
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.RelativeLayout
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
+import io.reactivex.schedulers.Schedulers.io
 import kotterknife.bindView
 import org.simple.clinic.R
+import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.hideKeyboard
+import javax.inject.Inject
 
 class HomeScreen(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
 
@@ -17,16 +26,38 @@ class HomeScreen(context: Context, attrs: AttributeSet) : RelativeLayout(context
 
   private val rootLayout by bindView<ViewGroup>(R.id.home_root)
   private val viewPager by bindView<ViewPager>(R.id.home_viewpager)
+  private val facilitySelectButton by bindView<Button>(R.id.home_facility_change_button)
 
+  @Inject
+  lateinit var controller: HomeScreenController
+
+  @SuppressLint("CheckResult")
   override fun onFinishInflate() {
     super.onFinishInflate()
     if (isInEditMode) {
       return
     }
 
+    TheActivity.component.inject(this)
+
+    screenCreates()
+        .observeOn(io())
+        .compose(controller)
+        .observeOn(mainThread())
+        .takeUntil(RxView.detaches(this))
+        .subscribe { uiChange -> uiChange(this) }
+
     // Keyboard stays open after login finishes, not sure why.
     rootLayout.hideKeyboard()
 
     viewPager.adapter = HomePagerAdapter(context)
   }
+
+  private fun screenCreates() = Observable.just(ScreenCreated())
+
+
+  fun setFacility(currentFacility: String) {
+    facilitySelectButton.text = currentFacility
+  }
+
 }

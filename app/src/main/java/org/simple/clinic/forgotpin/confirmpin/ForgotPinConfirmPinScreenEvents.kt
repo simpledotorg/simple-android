873e730c7b90a353d7ca4962af53421ab2b80f68
Sanchita Agarwal <sanchita@uncommon.is>
2018-12-06 15:33:14 +0530

    Remove facility change option from ForgotConfirmPinScreen

--- app/src/main/java/org/simple/clinic/forgotpin/confirmpin/ForgotPinConfirmPinScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/confirmpin/ForgotPinConfirmPinScreenEvents.kt
@@ -2,10 +2,6 @@ package org.simple.clinic.forgotpin.confirmpin
 
 import org.simple.clinic.widgets.UiEvent
 
-object ForgotPinConfirmPinScreenFacilityClicked : UiEvent {
-  override val analyticsName = "Forgot PIN:Confirm PIN:Facility Clicked"
-}
-
 data class ForgotPinConfirmPinScreenCreated(val pin: String) : UiEvent {
   init {
     if (pin.isBlank()) throw AssertionError("PIN cannot be blank!")

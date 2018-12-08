04e527d8135c802aedc614610d521a48fd1b0add
Saket Narayan <saket@uncommon.is>
2018-12-08 21:14:18 +0530

    Use requireContext() in all dialogs

--- app/src/main/java/org/simple/clinic/editpatient/ConfirmDiscardChangesDialog.kt
+++ app/src/main/java/org/simple/clinic/editpatient/ConfirmDiscardChangesDialog.kt
@@ -28,7 +28,7 @@ class ConfirmDiscardChangesDialog : AppCompatDialogFragment() {
   }
 
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-    return AlertDialog.Builder(context!!, R.style.Clinic_V2_DialogStyle_Destructive)
+    return AlertDialog.Builder(requireContext(), R.style.Clinic_V2_DialogStyle_Destructive)
         .setMessage(R.string.patientedit_confirm_discard_title)
         .setPositiveButton(R.string.patientedit_confirm_discard_ok) { _, _ ->
           screenRouter.pop()

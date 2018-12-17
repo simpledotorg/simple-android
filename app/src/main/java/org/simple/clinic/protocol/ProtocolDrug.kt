834aad2fc56c2e76e676b93dc7e0f4c88f835546
Sanchita Agarwal <sanchita@uncommon.is>
2018-12-17 19:24:43 +0530

    Update PrescribedDrugsScreenController to use synced protocols

--- app/src/main/java/org/simple/clinic/protocol/ProtocolDrug.kt
+++ app/src/main/java/org/simple/clinic/protocol/ProtocolDrug.kt
@@ -3,6 +3,7 @@ package org.simple.clinic.protocol
 /**
  * Drugs recommended in a [Protocol].
  */
+@Deprecated(message = "Use v2")
 data class ProtocolDrug(
     val name: String,
     val rxNormCode: String?,
--- app/src/test/java/org/simple/clinic/drugs/selection/PrescribedDrugsScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/drugs/selection/PrescribedDrugsScreenControllerTest.kt
@@ -10,15 +10,21 @@ import io.reactivex.subjects.PublishSubject
 import org.junit.Before
 import org.junit.Test
 import org.simple.clinic.drugs.PrescriptionRepository
+import org.simple.clinic.drugs.selection.ProtocolDrugSelectionListItem.*
+import org.simple.clinic.facility.FacilityRepository
 import org.simple.clinic.patient.PatientMocker
-import org.simple.clinic.protocol.Protocol
-import org.simple.clinic.protocol.ProtocolRepository
+import org.simple.clinic.protocolv2.ProtocolDrugAndDosages
+import org.simple.clinic.protocolv2.ProtocolRepository
+import org.simple.clinic.user.User
+import org.simple.clinic.user.UserSession
 import org.simple.clinic.widgets.UiEvent
 import java.util.UUID
 
 class PrescribedDrugsScreenControllerTest {
 
   private val screen = mock<PrescribedDrugsScreen>()
+  private val userSession = mock<UserSession>()
+  private val facilityRepository = mock<FacilityRepository>()
   private val protocolRepository = mock<ProtocolRepository>()
   private val prescriptionRepository = mock<PrescriptionRepository>()
   private val patientUuid = UUID.randomUUID()
@@ -28,7 +34,7 @@ class PrescribedDrugsScreenControllerTest {
 
   @Before
   fun setUp() {
-    controller = PrescribedDrugsScreenController(protocolRepository, prescriptionRepository)
+    controller = PrescribedDrugsScreenController(userSession, facilityRepository, protocolRepository, prescriptionRepository)
 
     uiEvents
         .compose(controller)
@@ -37,30 +43,35 @@ class PrescribedDrugsScreenControllerTest {
 
   @Test
   fun `should correctly construct RecyclerView models from protocol drugs and prescribed drugs`() {
-    val amlodipine = PatientMocker.protocolDrug(name = "Amlodipine", dosages = listOf("5mg", "10mg"))
-    val telmisartan = PatientMocker.protocolDrug(name = "Telmisartan", dosages = listOf("40mg", "80mg"))
-    val chlorthalidone = PatientMocker.protocolDrug(name = "Chlorthalidone", dosages = listOf("12.5mg", "25mg"))
+    val amlodipine5mg = PatientMocker.protocolDrug(name = "Amlodipine", dosage = "5mg")
+    val amlodipine10mg = PatientMocker.protocolDrug(name = "Amlodipine", dosage = "10mg")
+    val telmisartan40mg = PatientMocker.protocolDrug(name = "Telmisartan", dosage = "40mg")
+    val telmisartan80mg = PatientMocker.protocolDrug(name = "Telmisartan", dosage = "80mg")
 
-    val protocol = Protocol(
-        mock(),
-        name = "Dummy protocol",
-        drugs = listOf(amlodipine, telmisartan, chlorthalidone))
-    whenever(protocolRepository.currentProtocol()).thenReturn(Observable.just(protocol))
+    val protocolUuid = UUID.randomUUID()
+    val currentFacility = PatientMocker.facility(protocolUuid = protocolUuid)
+    whenever(userSession.requireLoggedInUser()).thenReturn(Observable.just(PatientMocker.loggedInUser()))
+    whenever(facilityRepository.currentFacility(any<User>())).thenReturn(Observable.just(currentFacility))
+
+    whenever(protocolRepository.drugsForProtocolOrDefault(protocolUuid)).thenReturn(Observable.just(listOf(
+        ProtocolDrugAndDosages(amlodipine10mg.name, listOf(amlodipine5mg, amlodipine10mg)),
+        ProtocolDrugAndDosages(telmisartan40mg.name, listOf(telmisartan40mg, telmisartan80mg))
+    )))
 
     val prescriptionUuid1 = UUID.randomUUID()
     val prescriptionUuid2 = UUID.randomUUID()
     val prescriptionUuid3 = UUID.randomUUID()
     val prescriptionUuid4 = UUID.randomUUID()
 
-    val amlodipinePrescription = PatientMocker.prescription(name = "Amlodipine", dosage = "10mg", isProtocolDrug = true)
-    val telmisartanPrescription = PatientMocker.prescription(uuid = prescriptionUuid1, name = "Telmisartan", dosage = "9000mg", isProtocolDrug = false)
+    val amlodipine10mgPrescription = PatientMocker.prescription(name = "Amlodipine", dosage = "10mg", isProtocolDrug = true)
+    val telmisartan9000mgPrescription = PatientMocker.prescription(uuid = prescriptionUuid1, name = "Telmisartan", dosage = "9000mg", isProtocolDrug = false)
     val ReesesPrescription = PatientMocker.prescription(uuid = prescriptionUuid2, name = "Reese's", dosage = "5 packets", isProtocolDrug = false)
     val fooPrescription = PatientMocker.prescription(uuid = prescriptionUuid3, name = "Foo", dosage = "2 pills", isProtocolDrug = false)
     val barPrescription = PatientMocker.prescription(uuid = prescriptionUuid4, name = "Bar", dosage = null, isProtocolDrug = false)
 
     val prescriptions = listOf(
-        amlodipinePrescription,
-        telmisartanPrescription,
+        amlodipine10mgPrescription,
+        telmisartan9000mgPrescription,
         ReesesPrescription,
         fooPrescription,
         barPrescription)
@@ -68,69 +79,68 @@ class PrescribedDrugsScreenControllerTest {
 
     uiEvents.onNext(PrescribedDrugsScreenCreated(patientUuid))
 
-    val expectedProtocolDrugUiModels = listOf(
+    val expectedUiModels = listOf(
         ProtocolDrugSelectionListItem(
-            0,
-            drug = amlodipine,
-            option1 = ProtocolDrugSelectionListItem.DosageOption.Unselected(amlodipine.dosages[0]),
-            option2 = ProtocolDrugSelectionListItem.DosageOption.Selected(amlodipine.dosages[1], prescription = amlodipinePrescription)),
+            id = 0,
+            drugName = amlodipine10mg.name,
+            option1 = DosageOption.Unselected(amlodipine5mg),
+            option2 = DosageOption.Selected(amlodipine10mg, prescription = amlodipine10mgPrescription)),
         ProtocolDrugSelectionListItem(
-            1,
-            drug = telmisartan,
-            option1 = ProtocolDrugSelectionListItem.DosageOption.Unselected(telmisartan.dosages[0]),
-            option2 = ProtocolDrugSelectionListItem.DosageOption.Unselected(telmisartan.dosages[1])),
-        ProtocolDrugSelectionListItem(
-            2,
-            drug = chlorthalidone,
-            option1 = ProtocolDrugSelectionListItem.DosageOption.Unselected(chlorthalidone.dosages[0]),
-            option2 = ProtocolDrugSelectionListItem.DosageOption.Unselected(chlorthalidone.dosages[1])),
-        CustomPrescribedDrugListItem(telmisartanPrescription),
+            id = 1,
+            drugName = telmisartan40mg.name,
+            option1 = DosageOption.Unselected(telmisartan40mg),
+            option2 = DosageOption.Unselected(telmisartan80mg)),
+        CustomPrescribedDrugListItem(telmisartan9000mgPrescription),
         CustomPrescribedDrugListItem(ReesesPrescription),
         CustomPrescribedDrugListItem(fooPrescription),
         CustomPrescribedDrugListItem(barPrescription))
-    verify(screen).populateDrugsList(expectedProtocolDrugUiModels)
+    verify(screen).populateDrugsList(expectedUiModels)
   }
 
   @Test
   fun `when a protocol drug is selected then a prescription should be saved for it`() {
-    whenever(prescriptionRepository.savePrescription(any(), any(), any())).thenReturn(Completable.complete())
-    whenever(protocolRepository.currentProtocol()).thenReturn(Observable.never())
+    whenever(userSession.requireLoggedInUser()).thenReturn(Observable.never())
+    whenever(prescriptionRepository.savePrescription(any(), any())).thenReturn(Completable.complete())
+    whenever(protocolRepository.drugsForProtocolOrDefault(any())).thenReturn(Observable.never())
     whenever(prescriptionRepository.newestPrescriptionsForPatient(patientUuid)).thenReturn(Observable.empty())
 
-    val amlodipine = PatientMocker.protocolDrug(name = "Amlodipine", dosages = listOf("5mg", "10mg"))
+    val amlodipine10mg = PatientMocker.protocolDrug(name = "Amlodipine", dosage = "10mg")
 
     uiEvents.onNext(PrescribedDrugsScreenCreated(patientUuid))
-    uiEvents.onNext(ProtocolDrugDosageSelected(amlodipine, "10mg"))
+    uiEvents.onNext(ProtocolDrugDosageSelected(amlodipine10mg))
 
-    verify(prescriptionRepository).savePrescription(patientUuid, drug = amlodipine, dosage = "10mg")
+    verify(prescriptionRepository).savePrescription(patientUuid, drug = amlodipine10mg)
   }
 
   @Test
   fun `when a protocol drug is unselected then its prescription should be soft deleted`() {
-    val amlodipine = PatientMocker.protocolDrug(name = "Amlodipine", dosages = listOf("5mg", "10mg"))
+    val amlodipine5mg = PatientMocker.protocolDrug(name = "Amlodipine", dosage = "5mg")
+    val amlodipine10mg = PatientMocker.protocolDrug(name = "Amlodipine", dosage = "10mg")
     val unselectedPrescriptionId = UUID.randomUUID()
 
-    whenever(prescriptionRepository.savePrescription(any(), any(), any())).thenReturn(Completable.complete())
+    whenever(userSession.requireLoggedInUser()).thenReturn(Observable.never())
+    whenever(prescriptionRepository.savePrescription(any(), any())).thenReturn(Completable.complete())
     whenever(prescriptionRepository.softDeletePrescription(unselectedPrescriptionId)).thenReturn(Completable.complete())
 
-    whenever(protocolRepository.currentProtocol()).thenReturn(Observable.never())
+    whenever(protocolRepository.drugsForProtocolOrDefault(any())).thenReturn(Observable.never())
     whenever(prescriptionRepository.newestPrescriptionsForPatient(patientUuid)).thenReturn(Observable.empty())
 
     uiEvents.onNext(PrescribedDrugsScreenCreated(patientUuid))
-    uiEvents.onNext(ProtocolDrugDosageSelected(amlodipine, "10mg"))
-    uiEvents.onNext(ProtocolDrugDosageSelected(amlodipine, "5mg"))
+    uiEvents.onNext(ProtocolDrugDosageSelected(amlodipine10mg))
+    uiEvents.onNext(ProtocolDrugDosageSelected(amlodipine5mg))
     uiEvents.onNext(ProtocolDrugDosageUnselected(
-        drug = amlodipine,
-        prescription = PatientMocker.prescription(unselectedPrescriptionId, amlodipine.name, "10mg")))
+        drug = amlodipine10mg,
+        prescription = PatientMocker.prescription(uuid = unselectedPrescriptionId, name = amlodipine10mg.name, dosage = amlodipine10mg.dosage)))
 
-    verify(prescriptionRepository).savePrescription(patientUuid, drug = amlodipine, dosage = "10mg")
-    verify(prescriptionRepository).savePrescription(patientUuid, drug = amlodipine, dosage = "5mg")
+    verify(prescriptionRepository).savePrescription(patientUuid, drug = amlodipine10mg)
+    verify(prescriptionRepository).savePrescription(patientUuid, drug = amlodipine5mg)
     verify(prescriptionRepository).softDeletePrescription(unselectedPrescriptionId)
   }
 
   @Test
   fun `when new prescription button is clicked then prescription entry sheet should be shown`() {
-    whenever(protocolRepository.currentProtocol()).thenReturn(Observable.never())
+    whenever(userSession.requireLoggedInUser()).thenReturn(Observable.never())
+    whenever(protocolRepository.drugsForProtocolOrDefault(any())).thenReturn(Observable.never())
     whenever(prescriptionRepository.newestPrescriptionsForPatient(patientUuid)).thenReturn(Observable.empty())
 
     uiEvents.onNext(PrescribedDrugsScreenCreated(patientUuid))

a6b71fe0affb067fa0e5010f25b3bd57ec202fc2
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-12-21 15:38:29 +0530

    Register appointment in CommunicationSyncAndroidTest

--- app/src/androidTest/java/org/simple/clinic/overdue/communication/CommunicationSyncAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/overdue/communication/CommunicationSyncAndroidTest.kt
@@ -9,16 +9,20 @@ import org.junit.runner.RunWith
 import org.simple.clinic.AuthenticationRule
 import org.simple.clinic.TestClinicApp
 import org.simple.clinic.TestData
+import org.simple.clinic.facility.FacilityRepository
+import org.simple.clinic.overdue.AppointmentPushRequest
+import org.simple.clinic.overdue.AppointmentSyncApiV2
 import org.simple.clinic.patient.SyncStatus
 import org.simple.clinic.sync.BaseSyncCoordinatorAndroidTest
 import org.simple.clinic.sync.DataPushResponse
+import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.Optional
 import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Named
 
 @RunWith(AndroidJUnit4::class)
-class CommunicationSyncAndroidTest: BaseSyncCoordinatorAndroidTest<Communication, CommunicationPayload>() {
+class CommunicationSyncAndroidTest : BaseSyncCoordinatorAndroidTest<Communication, CommunicationPayload>() {
 
   @Inject
   lateinit var repository: CommunicationRepository
@@ -36,12 +40,36 @@ class CommunicationSyncAndroidTest: BaseSyncCoordinatorAndroidTest<Communication
   @Inject
   lateinit var testData: TestData
 
+  @Inject
+  lateinit var facilityRepository: FacilityRepository
+
+  @Inject
+  lateinit var userSession: UserSession
+
+  @Inject
+  lateinit var appointmentSyncApi: AppointmentSyncApiV2
+
   @get:Rule
-  val authenticationRule = AuthenticationRule(registerAppointmentWithUuid = UUID.randomUUID())
+  val authenticationRule = AuthenticationRule()
+
+  val appointmentUuid = UUID.randomUUID()
 
   @Before
   fun setUp() {
     TestClinicApp.appComponent().inject(this)
+    registerAppointment()
+  }
+
+  private fun registerAppointment() {
+    val registeredFacilityUuid = facilityRepository
+        .currentFacilityUuid(userSession.loggedInUserImmediate()!!)!!
+
+    val appointmentPayload = testData.appointmentPayload(uuid = appointmentUuid, apiV2Enabled = true, facilityUuid = registeredFacilityUuid)
+    val pushRequest = AppointmentPushRequest(listOf(appointmentPayload))
+
+    appointmentSyncApi
+        .push(pushRequest)
+        .blockingGet()
   }
 
   override fun push() = sync.push()
@@ -53,11 +81,11 @@ class CommunicationSyncAndroidTest: BaseSyncCoordinatorAndroidTest<Communication
   override fun generateRecord(syncStatus: SyncStatus): Communication {
     return testData.communication(
         syncStatus = syncStatus,
-        appointmentUuid = authenticationRule.registerAppointmentWithUuid!!)
+        appointmentUuid = appointmentUuid)
   }
 
   override fun generatePayload(): CommunicationPayload {
-    return testData.communicationPayload(appointmentUuid = authenticationRule.registerAppointmentWithUuid!!)
+    return testData.communicationPayload(appointmentUuid = appointmentUuid)
   }
 
   override fun lastPullToken(): Preference<Optional<String>> = lastPullToken

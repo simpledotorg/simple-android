ac2a3872a1e641c090b48db8240040d3d94e6519
Vinay S Shenoy <vinay.sh@uncommon.is>
2018-12-26 15:48:33 +0530

    Added BloodPressureConfig with feature flag for enabling delete BP feature

--- app/src/main/java/org/simple/clinic/bp/BloodPressureModule.kt
+++ app/src/main/java/org/simple/clinic/bp/BloodPressureModule.kt
@@ -4,6 +4,7 @@ import com.f2prateek.rx.preferences2.Preference
 import com.f2prateek.rx.preferences2.RxSharedPreferences
 import dagger.Module
 import dagger.Provides
+import io.reactivex.Single
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.bp.sync.BloodPressureSyncApiV2
 import org.simple.clinic.util.None
@@ -30,5 +31,10 @@ open class BloodPressureModule {
   @Named("last_bp_pull_token")
   fun lastPullToken(rxSharedPrefs: RxSharedPreferences): Preference<Optional<String>> {
     return rxSharedPrefs.getObject("last_bp_pull_token_v2", None, OptionalRxPreferencesConverter(StringPreferenceConverter()))
+  }
+
+  @Provides
+  open fun provideBloodPressureEntryConfig(): Single<BloodPressureConfig> {
+    return Single.just(BloodPressureConfig(deleteBloodPressureFeatureEnabled = false))
   }
 }

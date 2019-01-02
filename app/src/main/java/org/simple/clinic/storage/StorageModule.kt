969fb3911c62849dbed57aee701928ec7235eb7d
Saket Narayan <saket@uncommon.is>
2019-01-02 13:12:48 +0530

    Provide list of migrations separately in DI module

--- app/src/main/java/org/simple/clinic/storage/StorageModule.kt
+++ app/src/main/java/org/simple/clinic/storage/StorageModule.kt
@@ -3,6 +3,7 @@ package org.simple.clinic.storage
 import android.app.Application
 import android.arch.persistence.db.SupportSQLiteOpenHelper
 import android.arch.persistence.room.Room
+import android.arch.persistence.room.migration.Migration
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import com.f2prateek.rx.preferences2.RxSharedPreferences
@@ -19,8 +20,41 @@ open class StorageModule(
 ) {
 
   @Provides
+  fun databaseMigrations(): MutableList<Migration> {
+    return mutableListOf(
+        Migration_3_4(),
+        Migration_4_5(),
+        Migration_5_6(),
+        Migration_6_7(),
+        Migration_7_8(),
+        Migration_8_9(),
+        Migration_9_10(),
+        Migration_10_11(),
+        Migration_11_12(),
+        Migration_12_13(),
+        Migration_13_14(),
+        Migration_14_15(),
+        Migration_15_16(),
+        Migration_16_17(),
+        Migration_17_18(),
+        Migration_18_19(),
+        Migration_19_20(),
+        Migration_20_21(),
+        Migration_21_22(),
+        Migration_22_23(),
+        Migration_23_24(),
+        Migration_24_25(),
+        Migration_25_26(),
+        Migration_26_27())
+  }
+
+  @Provides
   @AppScope
-  fun appDatabase(appContext: Application, factory: SupportSQLiteOpenHelper.Factory): AppDatabase {
+  fun appDatabase(
+      appContext: Application,
+      factory: SupportSQLiteOpenHelper.Factory,
+      migrations: MutableList<Migration>
+  ): AppDatabase {
     return Room.databaseBuilder(appContext, AppDatabase::class.java, databaseName)
         .openHelperFactory(factory)
         .apply {
@@ -28,31 +62,7 @@ open class StorageModule(
             allowMainThreadQueries()
           }
         }
-        .addMigrations(
-            Migration_3_4(),
-            Migration_4_5(),
-            Migration_5_6(),
-            Migration_6_7(),
-            Migration_7_8(),
-            Migration_8_9(),
-            Migration_9_10(),
-            Migration_10_11(),
-            Migration_11_12(),
-            Migration_12_13(),
-            Migration_13_14(),
-            Migration_14_15(),
-            Migration_15_16(),
-            Migration_16_17(),
-            Migration_17_18(),
-            Migration_18_19(),
-            Migration_19_20(),
-            Migration_20_21(),
-            Migration_21_22(),
-            Migration_22_23(),
-            Migration_23_24(),
-            Migration_24_25(),
-            Migration_25_26(),
-            Migration_26_27())
+        .addMigrations(*migrations.toTypedArray())
         .build()
   }
 

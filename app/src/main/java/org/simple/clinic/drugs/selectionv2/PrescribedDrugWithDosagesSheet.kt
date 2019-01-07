1140e19c3597a3e011b58f92d5e3466d4e18f8c1
Sanchita Agarwal <sanchita@uncommon.is>
2019-01-07 16:49:05 +0530

    Add PrescibedDrugWithDosageSheet and adapter to show the dosages

---
+++ app/src/main/java/org/simple/clinic/drugs/selectionv2/PrescribedDrugWithDosagesSheet.kt
@@ -0,0 +1,28 @@
+package org.simple.clinic.drugs.selectionv2
+
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotterknife.bindView
+import org.simple.clinic.R
+import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.widgets.BottomSheetActivity
+import javax.inject.Inject
+
+class PrescribedDrugWithDosagesSheet : BottomSheetActivity() {
+
+  @Inject
+  lateinit var adapter: PrescribedDosageAdapter
+
+  private val recyclerView by bindView<RecyclerView>(R.id.prescribed_drugs_with_dosages_list)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.prescribed_drug_with_dosages_sheet)
+    TheActivity.component.inject(this)
+
+    recyclerView.adapter = adapter
+    recyclerView.layoutManager = LinearLayoutManager(this)
+
+  }
+}

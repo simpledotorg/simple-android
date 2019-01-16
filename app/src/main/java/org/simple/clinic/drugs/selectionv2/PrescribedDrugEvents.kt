fc69fc80a45939d17f8d1d06c7ec16b72143f7b6
Sanchita Agarwal <sanchita@uncommon.is>
2019-01-16 18:55:30 +0530

    Send prescription for the selected drug to DosagePickerSheet

--- app/src/main/java/org/simple/clinic/drugs/selectionv2/PrescribedDrugEvents.kt
+++ app/src/main/java/org/simple/clinic/drugs/selectionv2/PrescribedDrugEvents.kt
@@ -1,8 +1,9 @@
 package org.simple.clinic.drugs.selectionv2
 
+import org.simple.clinic.drugs.PrescribedDrug
 import org.simple.clinic.widgets.UiEvent
 import java.util.UUID
 
-data class ProtocolDrugSelected(val drugName: String) : UiEvent
+data class ProtocolDrugSelected(val drugName: String, val prescribedDrug: PrescribedDrug?) : UiEvent
 
 data class PrescribedDrugsScreenCreated(val patientUuid: UUID) : UiEvent

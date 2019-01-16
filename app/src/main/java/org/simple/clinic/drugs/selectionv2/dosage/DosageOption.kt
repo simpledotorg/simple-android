e98225d66df72d36def5fcd22c0995c81f7fe84d
Sanchita Agarwal <sanchita@uncommon.is>
2019-01-16 17:35:48 +0530

    Modify DosageOption#Dosage params to take ProtocolDrug

--- app/src/main/java/org/simple/clinic/drugs/selectionv2/dosage/DosageOption.kt
+++ app/src/main/java/org/simple/clinic/drugs/selectionv2/dosage/DosageOption.kt
@@ -1,6 +1,8 @@
 package org.simple.clinic.drugs.selectionv2.dosage
 
+import org.simple.clinic.protocol.ProtocolDrug
+
 sealed class DosageOption {
-  data class Dosage(val dosage: String) : DosageOption()
+  data class Dosage(val protocolDrug: ProtocolDrug) : DosageOption()
   object None: DosageOption()
 }

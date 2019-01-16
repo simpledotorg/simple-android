eb52da3abfc08690457dc7e1e1683f615505ddc9
Sanchita Agarwal <sanchita@uncommon.is>
2019-01-16 18:05:48 +0530

    Save prescription when a dosage is selected

--- app/src/main/java/org/simple/clinic/drugs/selectionv2/dosage/DosagePickerEvents.kt
+++ app/src/main/java/org/simple/clinic/drugs/selectionv2/dosage/DosagePickerEvents.kt
@@ -1,8 +1,11 @@
 package org.simple.clinic.drugs.selectionv2.dosage
 
+import org.simple.clinic.protocol.ProtocolDrug
 import org.simple.clinic.widgets.UiEvent
 import java.util.UUID
 
 data class DosagePickerSheetCreated(val drugName: String, val patientUuid: UUID) : UiEvent
 
 data class DosageItemClicked(val dosage: DosageOption) : UiEvent
+
+data class DosageSelected(val protocolDrug: ProtocolDrug) : UiEvent

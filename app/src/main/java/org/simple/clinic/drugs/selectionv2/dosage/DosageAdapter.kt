e2fc7509ac53625a61c6867b08f14427c109dcc9
Sanchita Agarwal <sanchita@uncommon.is>
2019-01-16 17:41:50 +0530

    Add item clicks stream for dosage item

--- app/src/main/java/org/simple/clinic/drugs/selectionv2/dosage/DosageAdapter.kt
+++ app/src/main/java/org/simple/clinic/drugs/selectionv2/dosage/DosageAdapter.kt
@@ -7,16 +7,21 @@ import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
 import kotterknife.bindView
 import org.simple.clinic.R
 import org.simple.clinic.util.exhaustive
+import org.simple.clinic.widgets.UiEvent
 import javax.inject.Inject
 
-class DosageAdapter @Inject constructor() : ListAdapter<DosageListItem, DosageViewHolder>(PrescribedDosageDiffer()) {
+class DosageAdapter @Inject constructor() : ListAdapter<DosageListItem, DosageViewHolder>(DosageDiffer()) {
+
+  val itemClicks: PublishSubject<UiEvent> = PublishSubject.create()
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DosageViewHolder {
     val layout = LayoutInflater.from(parent.context).inflate(R.layout.prescribed_drug_with_dosage_list_item, parent, false)
-    return DosageViewHolder(layout)
+    return DosageViewHolder(layout, itemClicks)
   }
 
   override fun onBindViewHolder(holder: DosageViewHolder, position: Int) {
@@ -25,12 +30,18 @@ class DosageAdapter @Inject constructor() : ListAdapter<DosageListItem, DosageVi
   }
 }
 
-class DosageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class DosageViewHolder(itemView: View, itemClicks: Subject<UiEvent>) : RecyclerView.ViewHolder(itemView) {
 
   private val dosageTextView by bindView<TextView>(R.id.prescribed_drug_list_item_dosage_name)
   private val dividerView by bindView<View>(R.id.prescribed_drug_list_item_divider)
 
   lateinit var dosageItem: DosageListItem
+
+  init {
+    itemView.setOnClickListener {
+      itemClicks.onNext(DosageItemClicked(dosageItem.dosageOption))
+    }
+  }
 
   fun render() {
     val dosageType = dosageItem.dosageOption
@@ -49,7 +60,7 @@ class DosageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
 data class DosageListItem(val dosageOption: DosageOption)
 
-class PrescribedDosageDiffer : DiffUtil.ItemCallback<DosageListItem>() {
+class DosageDiffer : DiffUtil.ItemCallback<DosageListItem>() {
   override fun areItemsTheSame(oldItem: DosageListItem, newItem: DosageListItem): Boolean {
     return if (oldItem.dosageOption is DosageOption.Dosage && newItem.dosageOption is DosageOption.Dosage) {
       oldItem.dosageOption.protocolDrug.uuid == newItem.dosageOption.protocolDrug.uuid

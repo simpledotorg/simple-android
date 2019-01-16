4253de514d49ab90eaca99d61a309bf442d9ea16
Saket Narayan <saket@uncommon.is>
2019-01-16 13:11:37 +0530

    Introduce DateOfBirthFormatValidator#validate2() to work with sealed classes

--- app/src/main/java/org/simple/clinic/widgets/ageanddateofbirth/DateOfBirthFormatValidator.kt
+++ app/src/main/java/org/simple/clinic/widgets/ageanddateofbirth/DateOfBirthFormatValidator.kt
@@ -17,27 +17,42 @@ class DateOfBirthFormatValidator @Inject constructor(
     DATE_IS_IN_FUTURE,
   }
 
-  fun validate(dateText: String, nowDate: LocalDate = LocalDate.now(UTC)): Result {
-    return try {
-      if (dateText.isBlank()) {
-        Result.INVALID_PATTERN
-      }
-
-      val parsedDate = dateOfBirthFormat.parse(dateText, LocalDate::from)
-      when {
-        parsedDate > nowDate -> Result.DATE_IS_IN_FUTURE
-        else -> Result.VALID
-      }
-
-    } catch (e: Exception) {
-      when (e) {
-        is DateTimeParseException -> Result.INVALID_PATTERN
-        else -> throw e
-      }
+  sealed class Result2 {
+    data class Valid(val parsedDate: LocalDate) : Result2()
+    sealed class Invalid : Result2() {
+      object InvalidPattern : Invalid()
+      object DateIsInFuture : Invalid()
     }
   }
 
-  fun validate(date: String): Result {
-    TODO()
+  @Deprecated(
+      message = "use validate2 instead which uses sealed classes instead of enums",
+      replaceWith = ReplaceWith("validate2(dateText, nowDate)"))
+  fun validate(dateText: String, nowDate: LocalDate = LocalDate.now(UTC)): Result {
+    return when (validate2(dateText, nowDate)) {
+      is Result2.Valid -> Result.VALID
+      is Result2.Invalid.InvalidPattern -> Result.INVALID_PATTERN
+      is Result2.Invalid.DateIsInFuture -> Result.DATE_IS_IN_FUTURE
+    }
+  }
+
+  fun validate2(dateText: String, nowDate: LocalDate = LocalDate.now(UTC)): Result2 {
+    try {
+      if (dateText.isBlank()) {
+        return Result2.Invalid.InvalidPattern
+      }
+
+      val parsedDate = dateOfBirthFormat.parse(dateText, LocalDate::from)
+      return when {
+        parsedDate > nowDate -> Result2.Invalid.DateIsInFuture
+        else -> Result2.Valid(parsedDate)
+      }
+
+    } catch (e: Exception) {
+      return when (e) {
+        is DateTimeParseException -> Result2.Invalid.InvalidPattern
+        else -> throw e
+      }
+    }
   }
 }
--- app/src/test/java/org/simple/clinic/newentry/DateOfBirthFormatValidatorTest.kt
+++ app/src/test/java/org/simple/clinic/newentry/DateOfBirthFormatValidatorTest.kt
@@ -7,6 +7,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.simple.clinic.widgets.ageanddateofbirth.DateOfBirthFormatValidator
 import org.simple.clinic.widgets.ageanddateofbirth.DateOfBirthFormatValidator.Result
+import org.simple.clinic.widgets.ageanddateofbirth.DateOfBirthFormatValidator.Result2.Invalid
+import org.simple.clinic.widgets.ageanddateofbirth.DateOfBirthFormatValidator.Result2.Valid
 import org.threeten.bp.LocalDate
 import org.threeten.bp.format.DateTimeFormatter
 import java.util.Locale
@@ -23,18 +25,46 @@ class DateOfBirthFormatValidatorTest {
     "24/04, INVALID_PATTERN",
     " , INVALID_PATTERN"
   ])
-  fun validate(date: String, expectedResult: Result) {
+  fun `validate (v1)`(date: String, expectedResult: Result) {
     val format = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH)
 
     assertThat(DateOfBirthFormatValidator(format).validate(date)).isEqualTo(expectedResult)
   }
 
   @Test
-  fun `validate future date`() {
+  @Parameters(method = "params for v2 validation")
+  fun `validate (v2)`(date: String, expectedResult: DateOfBirthFormatValidator.Result2) {
+    val format = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH)
+
+    assertThat(DateOfBirthFormatValidator(format).validate2(date)).isEqualTo(expectedResult)
+  }
+
+  @Suppress("unused")
+  fun `params for v2 validation`(): List<Any> {
+    return listOf(
+        listOf("24/04/1971", Valid(LocalDate.of(1971, 4, 24))),
+        listOf("24-04-1971", Invalid.InvalidPattern),
+        listOf("1971-04-24", Invalid.InvalidPattern),
+        listOf("1971-24-04", Invalid.InvalidPattern),
+        listOf("24/04", Invalid.InvalidPattern),
+        listOf(" ", Invalid.InvalidPattern))
+  }
+
+  @Test
+  fun `validate future date (v1)`() {
     val format = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH)
     val dobValidator = DateOfBirthFormatValidator(format)
     val futureDateResult = dobValidator.validate("01/01/3000", nowDate = LocalDate.parse("2018-07-16"))
 
     assertThat(futureDateResult).isEqualTo(Result.DATE_IS_IN_FUTURE)
+  }
+
+  @Test
+  fun `validate future date (v2)`() {
+    val format = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH)
+    val dobValidator = DateOfBirthFormatValidator(format)
+    val futureDateResult = dobValidator.validate2("01/01/3000", nowDate = LocalDate.parse("2018-07-16"))
+
+    assertThat(futureDateResult).isEqualTo(Invalid.DateIsInFuture)
   }
 }

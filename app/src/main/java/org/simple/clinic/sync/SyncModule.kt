ad8a14a4fe10f34ce26cb6f76b3d2ca5147c2189
Vinay S Shenoy <vinay.sh@uncommon.is>
2019-01-17 15:15:13 +0530

    Inject syncs as a list of 'ModelSync' instances into DataSync
    This enforces all Sync classes to conform to a standard interface. This commit
    also adds a couple of simple tests for DataSync.

--- app/src/main/java/org/simple/clinic/sync/SyncModule.kt
+++ app/src/main/java/org/simple/clinic/sync/SyncModule.kt
@@ -4,13 +4,21 @@ import dagger.Module
 import dagger.Provides
 import io.reactivex.Single
 import org.simple.clinic.bp.BloodPressureModule
+import org.simple.clinic.bp.sync.BloodPressureSync
 import org.simple.clinic.drugs.PrescriptionModule
+import org.simple.clinic.drugs.sync.PrescriptionSync
 import org.simple.clinic.facility.FacilityModule
+import org.simple.clinic.facility.FacilitySync
 import org.simple.clinic.medicalhistory.MedicalHistoryModule
+import org.simple.clinic.medicalhistory.sync.MedicalHistorySync
 import org.simple.clinic.overdue.AppointmentModule
+import org.simple.clinic.overdue.AppointmentSync
 import org.simple.clinic.overdue.communication.CommunicationModule
+import org.simple.clinic.overdue.communication.CommunicationSync
+import org.simple.clinic.patient.sync.PatientSync
 import org.simple.clinic.patient.sync.PatientSyncModule
 import org.simple.clinic.protocol.ProtocolModule
+import org.simple.clinic.protocol.sync.ProtocolSync
 import javax.inject.Named
 
 @Module(includes = [
@@ -23,12 +31,6 @@ import javax.inject.Named
   MedicalHistoryModule::class,
   ProtocolModule::class])
 class SyncModule {
-
-  fun syncConfig(): Single<SyncConfig> {
-    return Single.just(SyncConfig(
-        syncInterval = SyncInterval.FREQUENT,
-        batchSizeEnum = BatchSize.SMALL))
-  }
 
   @Provides
   @Named("sync_config_frequent")
@@ -45,5 +47,23 @@ class SyncModule {
         syncInterval = SyncInterval.DAILY,
         batchSizeEnum = BatchSize.MEDIUM
     ))
+  }
+
+  @Provides
+  fun syncs(
+      facilitySync: FacilitySync,
+      protocolSync: ProtocolSync,
+      patientSync: PatientSync,
+      bloodPressureSync: BloodPressureSync,
+      medicalHistorySync: MedicalHistorySync,
+      appointmentSync: AppointmentSync,
+      communicationSync: CommunicationSync,
+      prescriptionSync: PrescriptionSync
+  ): ArrayList<ModelSync> {
+    return arrayListOf(
+        facilitySync, protocolSync, patientSync,
+        bloodPressureSync, medicalHistorySync, appointmentSync,
+        communicationSync, prescriptionSync
+    )
   }
 }

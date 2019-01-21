720594e0233efb03f58b5c10d2a3e22cc67b403f
Vinay S Shenoy <vinay.sh@uncommon.is>
2019-01-21 10:47:26 +0530

    Remove the 'frequency' and 'backOffDelay' properties from 'SyncConfig'
    These were temporary properties added to ease transition to the 'SyncInterval'
    configuration object.

--- app/src/main/java/org/simple/clinic/sync/SyncConfig.kt
+++ app/src/main/java/org/simple/clinic/sync/SyncConfig.kt
@@ -4,8 +4,4 @@ data class SyncConfig(
     val syncInterval: SyncInterval,
     val batchSize: BatchSize,
     val syncGroupId: String
-) {
-  val frequency = syncInterval.frequency
-
-  val backOffDelay = syncInterval.backOffDelay
-}
+)

22b38551f1df2fec09f0555557e79f357ea7f12a
Vinay S Shenoy <vinay.sh@uncommon.is>
2019-01-21 11:16:09 +0530

    Add 'FileStorage' to abstract platform-specific file I/O
    This commit also adds an implementation for the Android platform with the
    'getFile' operation.

---
+++ app/src/androidTest/java/org/simple/clinic/storage/files/AndroidFileStorageAndroidTest.kt
@@ -0,0 +1,79 @@
+package org.simple.clinic.storage.files
+
+import android.app.Application
+import androidx.test.runner.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.simple.clinic.TestClinicApp
+import java.io.File
+import javax.inject.Inject
+
+@RunWith(AndroidJUnit4::class)
+class AndroidFileStorageAndroidTest {
+
+  @Inject
+  lateinit var application: Application
+
+  @Inject
+  lateinit var fileStorage: FileStorage
+
+  lateinit var testDirectory: File
+
+  @Before
+  fun setUp() {
+    TestClinicApp.appComponent().inject(this)
+    testDirectory = application.filesDir.resolve("test_dir")
+    testDirectory.deleteRecursively()
+    assertThat(testDirectory.mkdirs()).isTrue()
+  }
+
+  @Test
+  fun getting_a_file_at_non_existent_path_must_create_it() {
+    val path = testDirectory.resolve("foo/bar/file_1.txt").path
+
+    val result = fileStorage.getFile(path) as GetFileResult.Success
+
+    result.file.let { file ->
+      assertThat(file.exists()).isTrue()
+      assertThat(file.isFile).isTrue()
+      assertThat(file.parentFile.name).isEqualTo("bar")
+      assertThat(file.parentFile.parentFile.name).isEqualTo("foo")
+      assertThat(file.parentFile.parentFile.parentFile).isEqualTo(testDirectory)
+    }
+  }
+
+  @Test
+  fun getting_an_already_existing_file_must_not_overwrite_it() {
+    val text = "Some text that is already present"
+
+    val alreadyExistingFile = testDirectory.resolve("file.txt")
+    alreadyExistingFile.writeText(text)
+    assertThat(alreadyExistingFile.readText()).isEqualTo(text)
+
+    val result = fileStorage.getFile(alreadyExistingFile.path) as GetFileResult.Success
+    assertThat(result.file.readText()).isEqualTo(text)
+  }
+
+  @Test
+  fun getting_a_non_existing_file_at_an_existing_path_must_create_it() {
+    val path = testDirectory.resolve("file_1.txt").path
+
+    val result = fileStorage.getFile(path) as GetFileResult.Success
+
+    result.file.let { file ->
+      assertThat(file.exists()).isTrue()
+      assertThat(file.isFile).isTrue()
+      assertThat(file.parentFile.name).isEqualTo(testDirectory.name)
+    }
+  }
+
+  @Test
+  fun getting_a_writeable_file_that_points_to_a_directory_should_return_the_appropriate_result() {
+    val path = testDirectory.name
+    val result = fileStorage.getFile(path)
+
+    assertThat(result).isEqualTo(GetFileResult.NotAFile(path))
+  }
+}

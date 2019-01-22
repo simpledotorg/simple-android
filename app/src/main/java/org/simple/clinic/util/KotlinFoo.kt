1673303c0f5acc31cbce284c1b68b884c7aea954
Vinay S Shenoy <vinay.sh@uncommon.is>
2019-01-22 14:57:56 +0530

    Add ReportsRepository which manages the reports file
    Two methods have been added to the Repository:
    
    1. reportsFile() - Which exposes a stream of file events whenever the underlying
    file is updated.
    2. updateReports() - Which can be used to save reports into the file.

--- app/src/main/java/org/simple/clinic/util/KotlinFoo.kt
+++ app/src/main/java/org/simple/clinic/util/KotlinFoo.kt
@@ -1,4 +1,10 @@
 package org.simple.clinic.util
 
+import io.reactivex.Maybe
+import io.reactivex.Single
+
 // Forces when blocks to be exhaustive.
 fun Unit.exhaustive() {}
+
+inline fun <reified U> Single<*>.ofType(): Maybe<U> =
+    filter { it is U }.cast(U::class.java)

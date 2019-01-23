749b8124deb6a7ab15b03280014b177208bee25a
Rakshak R.Hegde <rakshakhegde@gmail.com>
2019-01-23 17:01:48 +0530

    Add ReportsScreenController

--- app/src/main/java/org/simple/clinic/home/report/ReportsScreen.kt
+++ app/src/main/java/org/simple/clinic/home/report/ReportsScreen.kt
@@ -3,6 +3,7 @@ package org.simple.clinic.home.report
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import java.net.URI
 
 class ReportsScreen(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
@@ -16,5 +17,13 @@ class ReportsScreen(context: Context, attrs: AttributeSet) : LinearLayout(contex
     if (isInEditMode) {
       return
     }
+  }
+
+  fun showReport(uri: URI) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  fun showNoReportsAvailable() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
   }
 }

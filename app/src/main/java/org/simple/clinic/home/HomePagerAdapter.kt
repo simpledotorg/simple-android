f6d1d0fe8f46fbd71644407d561b0a6d8f875bd6
Sanchita Agarwal <sanchita@uncommon.is>
2019-02-04 17:52:45 +0530

    Replace KEY with class in Home screens

--- app/src/main/java/org/simple/clinic/home/HomePagerAdapter.kt
+++ app/src/main/java/org/simple/clinic/home/HomePagerAdapter.kt
@@ -8,9 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.simple.clinic.R
-import org.simple.clinic.home.overdue.OverdueScreen
-import org.simple.clinic.home.patients.PatientsScreen
-import org.simple.clinic.home.report.ReportsScreen
+import org.simple.clinic.home.overdue.OverdueScreenKey
+import org.simple.clinic.home.patients.PatientsScreenKey
+import org.simple.clinic.home.report.ReportsScreenKey
 
 class HomePagerAdapter(private val context: Context) : PagerAdapter() {
 
@@ -36,9 +36,9 @@ class HomePagerAdapter(private val context: Context) : PagerAdapter() {
 
 private enum class HomeTabs(@LayoutRes val key: Int, @StringRes val title: Int) {
 
-  PATIENT(PatientsScreen.KEY.layoutRes(), R.string.tab_patient),
+  PATIENT(PatientsScreenKey().layoutRes(), R.string.tab_patient),
 
-  OVERDUE(OverdueScreen.KEY.layoutRes(), R.string.tab_overdue),
+  OVERDUE(OverdueScreenKey().layoutRes(), R.string.tab_overdue),
 
-  REPORTS(ReportsScreen.KEY.layoutRes(), R.string.tab_reports)
+  REPORTS(ReportsScreenKey().layoutRes(), R.string.tab_reports)
 }

195845bd36f3db667736e70470cc7c45cc0acb5e
Saket Narayan <saket@uncommon.is>
2019-02-11 13:15:08 +0530

    Facility selection: order facilities by proximity to user
    The proximity has a threshold, after which all facilities are treated as "far".

--- app/src/main/java/org/simple/clinic/registration/RegistrationConfig.kt
+++ app/src/main/java/org/simple/clinic/registration/RegistrationConfig.kt
@@ -1,9 +1,11 @@
 package org.simple.clinic.registration
 
+import org.simple.clinic.util.Distance
 import org.threeten.bp.Duration
 
 data class RegistrationConfig(
     val retryBackOffDelayInMinutes: Long, // TODO: convert to Duration
     val locationListenerExpiry: Duration,
-    val locationUpdateInterval: Duration
+    val locationUpdateInterval: Duration,
+    val proximityThresholdForNearbyFacilities: Distance
 )

bd2c042ff1ea2529be89c1100e5758ab1c21cd6f
Sanchita Agarwal <sanchita@uncommon.is>
2019-02-12 15:46:23 +0530

    Add SyncIndicatorStatusCalculator to set sync results

---
+++ app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorStatusCalculator.kt
@@ -0,0 +1,38 @@
+package org.simple.clinic.sync.indicator
+
+import com.f2prateek.rx.preferences2.Preference
+import io.reactivex.schedulers.Schedulers.io
+import org.simple.clinic.di.AppScope
+import org.simple.clinic.sync.DataSync
+import org.simple.clinic.sync.SyncGroup
+import org.simple.clinic.sync.SyncGroupResult
+import org.simple.clinic.util.Just
+import org.simple.clinic.util.Optional
+import org.threeten.bp.Clock
+import org.threeten.bp.Instant
+import javax.inject.Inject
+import javax.inject.Named
+
+@AppScope
+class SyncIndicatorStatusCalculator @Inject constructor(
+    dataSync: DataSync,
+    clock: Clock,
+    @Named("last_frequent_sync_succeeded_timestamp") private val lastSyncSuccessTimestamp: Preference<Optional<Instant>>,
+    @Named("last_frequent_sync_result") private val lastSyncResult: Preference<Optional<SyncGroupResult>>
+) {
+  init {
+    saveSyncResults(dataSync, clock)
+  }
+
+  private fun saveSyncResults(dataSync: DataSync, clock: Clock) {
+    dataSync
+        .streamSyncResults()
+        .distinctUntilChanged()
+        .subscribeOn(io())
+        .filter { (syncGroup, _) -> syncGroup == SyncGroup.FREQUENT }
+        .doOnNext { (_, result) -> lastSyncResult.set(Just(result)) }
+        .filter { (_, result) -> result == SyncGroupResult.SUCCESS }
+        .map { lastSyncSuccessTimestamp.set(Just(Instant.now(clock))) }
+        .subscribe()
+  }
+}

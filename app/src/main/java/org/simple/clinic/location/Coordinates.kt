51664c252001a98e6bad6c3e2c2d6c1ce3f154a6
Saket Narayan <saket@uncommon.is>
2019-02-19 07:32:49 +0000

    Move Haversine.distance() to Coordinates.haversineDistance()

--- app/src/main/java/org/simple/clinic/location/Coordinates.kt
+++ app/src/main/java/org/simple/clinic/location/Coordinates.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.location
 
 import androidx.room.ColumnInfo
+import org.simple.clinic.util.Distance
 
 data class Coordinates(
 
@@ -9,4 +10,34 @@ data class Coordinates(
 
     @ColumnInfo(name = "longitude")
     val longitude: Double
-)
+) {
+
+  companion object {
+
+    // Earth is not a perfect sphere so this is an approximate value.
+    private const val EARTH_RADIUS = 6371.0088
+
+    /**
+     * Haversine formula determines the great-circle distance between two points
+     * on a sphere given their longitudes and latitudes.
+     *
+     * Source: https://github.com/jasonwinn/haversine
+     * Modified to use with Kotlin.
+     */
+    fun haversineDistance(start: Coordinates, end: Coordinates): Distance {
+      val latitudeDelta = Math.toRadians(end.latitude - start.latitude)
+      val longitudeDelta = Math.toRadians(end.longitude - start.longitude)
+
+      val startLatitudeRadians = Math.toRadians(start.latitude)
+      val endLatitudeRadians = Math.toRadians(end.latitude)
+
+      val a = hav(latitudeDelta) + Math.cos(startLatitudeRadians) * Math.cos(endLatitudeRadians) * hav(longitudeDelta)
+      val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+      return Distance.ofKilometers(EARTH_RADIUS * c)
+    }
+
+    private fun hav(value: Double): Double {
+      return Math.pow(Math.sin(value / 2), 2.0)
+    }
+  }
+}
--- app/src/test/java/org/simple/clinic/util/CoordinatesTest.kt
+++ app/src/test/java/org/simple/clinic/util/CoordinatesTest.kt
@@ -4,14 +4,14 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.simple.clinic.location.Coordinates
 
-class HaversineTest {
+class CoordinatesTest {
 
   @Test
   fun `haversine formula should work correctly`() {
     val start = Coordinates(latitude = 1.908537, longitude = 73.537524)
     val end = Coordinates(latitude = 59.299800, longitude = 18.209118)
-    val maldivesToStockholm = Haversine.distance(start, end)
-    val stockholmToMaldives = Haversine.distance(start, end)
+    val maldivesToStockholm = Coordinates.haversineDistance(start, end)
+    val stockholmToMaldives = Coordinates.haversineDistance(start, end)
 
     assertThat(maldivesToStockholm).isEqualTo(Distance.ofKilometers(7939.655950402867))
     assertThat(stockholmToMaldives).isEqualTo(Distance.ofKilometers(7939.655950402867))

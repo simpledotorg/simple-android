0dc0dc442ab65a04bbfdbf3e390335385e9a8624
Rakshak R.Hegde <rakshakhegde@gmail.com>
2019-02-20 08:51:47 +0000

    Add RecentPatientItem.Data and bind data

--- app/src/main/java/org/simple/clinic/patient/recent/RecentPatientItem.kt
+++ app/src/main/java/org/simple/clinic/patient/recent/RecentPatientItem.kt
@@ -2,11 +2,25 @@ package org.simple.clinic.patient.recent
 
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.recent_patient_item_view.view.*
 import org.simple.clinic.R
+import org.simple.clinic.patient.Gender
 
-class RecentPatientItem : Item<ViewHolder>() {
+class RecentPatientItem(private val data: Data) : Item<ViewHolder>() {
+
   override fun getLayout(): Int = R.layout.recent_patient_item_view
 
   override fun bind(viewHolder: ViewHolder, position: Int) {
+    viewHolder.itemView.apply {
+      recentpatient_item_title.text = data.title
+      recentpatient_item_last_bp.text = data.lastBp
+      recentpatient_item_gender.setImageResource(data.gender.displayIconRes)
+    }
   }
+
+  data class Data(
+      val title: String,
+      val lastBp: String,
+      val gender: Gender
+  )
 }
--- app/src/main/java/org/simple/clinic/patient/recent/RecentPatientsView.kt
+++ app/src/main/java/org/simple/clinic/patient/recent/RecentPatientsView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.xwray.groupie.GroupAdapter
+import org.simple.clinic.patient.Gender
 
 class RecentPatientsView(context: Context, attrs: AttributeSet) : RecyclerView(context, attrs) {
 
@@ -15,12 +16,40 @@ class RecentPatientsView(context: Context, attrs: AttributeSet) : RecyclerView(c
     val groupAdapter = GroupAdapter<com.xwray.groupie.ViewHolder>()
     adapter = groupAdapter
 
-    groupAdapter.add(RecentPatientItem())
-    groupAdapter.add(RecentPatientItem())
-    groupAdapter.add(RecentPatientItem())
-    groupAdapter.add(RecentPatientItem())
-    groupAdapter.add(RecentPatientItem())
-    groupAdapter.add(RecentPatientItem())
-    groupAdapter.add(RecentPatientItem())
+    groupAdapter.add(RecentPatientItem(RecentPatientItem.Data(
+        title = "Anish Acharya, 43",
+        lastBp = "140/90",
+        gender = Gender.TRANSGENDER
+    )))
+    groupAdapter.add(RecentPatientItem(RecentPatientItem.Data(
+        title = "Anish24 Acharya, 43",
+        lastBp = "141/90",
+        gender = Gender.MALE
+    )))
+    groupAdapter.add(RecentPatientItem(RecentPatientItem.Data(
+        title = "Anish3 Acharya, 43",
+        lastBp = "142/90",
+        gender = Gender.FEMALE
+    )))
+    groupAdapter.add(RecentPatientItem(RecentPatientItem.Data(
+        title = "Anish4 Acharya, 43",
+        lastBp = "145/90",
+        gender = Gender.TRANSGENDER
+    )))
+    groupAdapter.add(RecentPatientItem(RecentPatientItem.Data(
+        title = "Anish55 Acharya, 43",
+        lastBp = "149/90",
+        gender = Gender.MALE
+    )))
+    groupAdapter.add(RecentPatientItem(RecentPatientItem.Data(
+        title = "Anish6 Acharya, 43",
+        lastBp = "141/90",
+        gender = Gender.FEMALE
+    )))
+    groupAdapter.add(RecentPatientItem(RecentPatientItem.Data(
+        title = "Anish7 Acharya, 43",
+        lastBp = "142/90",
+        gender = Gender.TRANSGENDER
+    )))
   }
 }

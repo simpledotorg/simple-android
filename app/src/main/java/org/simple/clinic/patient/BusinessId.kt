b238480da5a035a11714ffb571a4c1b26dd683cf
Vinay S Shenoy <vinay.sh@uncommon.is>
2019-02-27 11:39:24 +0000

    Add moshi adapters for BusinessId#IdentifierType and MetaVersion
    This commit also changes these two enums to sealed classes. The reason
    being that this gives us forward compatibity where we can add new enums
    in APIs while not breaking older app installations.

--- app/src/main/java/org/simple/clinic/patient/BusinessId.kt
+++ app/src/main/java/org/simple/clinic/patient/BusinessId.kt
@@ -5,6 +5,9 @@ import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverter
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import org.simple.clinic.util.SafeEnumTypeAdapter
 import org.threeten.bp.Instant
 import java.util.UUID
 
@@ -35,39 +38,70 @@ data class BusinessId(
     val deletedAt: Instant?
 ) {
 
-  enum class IdentifierType {
-    BP_PASSPORT,
-    UNKNOWN;
+  sealed class IdentifierType {
+
+    object BpPassport : IdentifierType()
+
+    data class Unknown(val actual: String) : IdentifierType()
+
+    object TypeAdapter: SafeEnumTypeAdapter<IdentifierType>(
+        knownMappings = mapOf(
+            BpPassport to "simple_bp_passport"
+        ),
+        unknownStringToEnumConverter = { Unknown(it) },
+        unknownEnumToStringConverter = { (it as Unknown).actual }
+    )
 
     class RoomTypeConverter {
 
       @TypeConverter
-      fun fromStringValue(stringValue: String) = try {
-        IdentifierType.valueOf(stringValue)
-      } catch (e: IllegalArgumentException) {
-        UNKNOWN
-      }
+      fun toEnum(value: String?) = TypeAdapter.toEnum(value)
 
       @TypeConverter
-      fun toStringValue(identifierType: IdentifierType) = identifierType.name
+      fun fromEnum(enum: IdentifierType?) = TypeAdapter.fromEnum(enum)
+    }
+
+    class MoshiTypeAdapter {
+
+      @FromJson
+      fun toEnum(value: String?) = TypeAdapter.toEnum(value)
+
+      @ToJson
+      fun fromEnum(enum: IdentifierType?) = TypeAdapter.fromEnum(enum)
     }
   }
 
-  enum class MetaVersion {
-    BP_PASSPORT_META_V1,
-    UNKNOWN;
+  sealed class MetaVersion {
+
+    object BpPassportMetaV1 : MetaVersion()
+
+    data class Unknown(val actual: String): MetaVersion()
+
+    object TypeAdapter: SafeEnumTypeAdapter<MetaVersion>(
+        knownMappings = mapOf(
+            BpPassportMetaV1 to "org.simple.bppassport.meta.v1"
+        ),
+        unknownStringToEnumConverter = { Unknown(it) },
+        unknownEnumToStringConverter = { (it as Unknown).actual }
+    )
 
     class RoomTypeConverter {
 
       @TypeConverter
-      fun fromStringValue(stringValue: String) = try {
-        MetaVersion.valueOf(stringValue)
-      } catch (e: IllegalArgumentException) {
-        UNKNOWN
-      }
+      fun toEnum(value: String?) = TypeAdapter.toEnum(value)
 
       @TypeConverter
-      fun toStringValue(metaVersion: MetaVersion) = metaVersion.name
+      fun fromEnum(enum: MetaVersion?) = TypeAdapter.fromEnum(enum)
+    }
+
+    class MoshiTypeAdapter {
+
+      @FromJson
+      fun toEnum(value: String?) = TypeAdapter.toEnum(value)
+
+      @ToJson
+      fun fromEnum(enum: MetaVersion?) = TypeAdapter.fromEnum(enum)
     }
   }
 }
+

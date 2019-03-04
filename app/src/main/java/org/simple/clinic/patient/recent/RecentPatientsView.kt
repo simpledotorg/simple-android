f50cc3e5e6cbb96f6b1ba3a86d9454f2f2a01c10
Rakshak R.Hegde <rakshakhegde@gmail.com>
2019-03-04 09:41:16 +0000

    Add no recent patients view

--- app/src/main/java/org/simple/clinic/patient/recent/RecentPatientsView.kt
+++ app/src/main/java/org/simple/clinic/patient/recent/RecentPatientsView.kt
@@ -2,17 +2,20 @@ package org.simple.clinic.patient.recent
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.FrameLayout
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.jakewharton.rxbinding2.view.RxView
 import com.xwray.groupie.GroupAdapter
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.recent_patients.view.*
+import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
 import org.simple.clinic.widgets.ScreenCreated
+import org.simple.clinic.widgets.visibleOrGone
 import javax.inject.Inject
 
-class RecentPatientsView(context: Context, attrs: AttributeSet) : RecyclerView(context, attrs) {
+class RecentPatientsView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
 
   @Inject
   lateinit var controller: RecentPatientsViewController
@@ -23,8 +26,12 @@ class RecentPatientsView(context: Context, attrs: AttributeSet) : RecyclerView(c
     super.onFinishInflate()
     TheActivity.component.inject(this)
 
-    layoutManager = LinearLayoutManager(context)
-    adapter = groupAdapter
+    inflate(context, R.layout.recent_patients, this)
+
+    recent_patient_recyclerview.apply {
+      layoutManager = LinearLayoutManager(context)
+      adapter = groupAdapter
+    }
 
     Observable.mergeArray(screenCreates())
         .compose(controller)

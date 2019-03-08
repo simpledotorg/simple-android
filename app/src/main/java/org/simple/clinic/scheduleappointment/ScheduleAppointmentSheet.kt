8575706080557b1ba56d5c05ac93bde13ffbb2e3
Rakshak R.Hegde <rakshakhegde@gmail.com>
2019-03-08 06:15:12 +0000

    Fix: Set the schedule appointment default date index to point to 1 month

--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentSheet.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentSheet.kt
@@ -17,7 +17,8 @@ import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
 import org.simple.clinic.widgets.BottomSheetActivity
 import org.threeten.bp.LocalDate
-import org.threeten.bp.temporal.ChronoUnit
+import org.threeten.bp.temporal.ChronoUnit.DAYS
+import org.threeten.bp.temporal.ChronoUnit.MONTHS
 import java.util.UUID
 import javax.inject.Inject
 
@@ -36,30 +37,32 @@ class ScheduleAppointmentSheet : BottomSheetActivity() {
     }
   }
 
+  private val oneMonth = ScheduleAppointment("1 month", 1, MONTHS)
+
   private val possibleDates = listOf(
-      ScheduleAppointment("1 day", 1, ChronoUnit.DAYS),
-      ScheduleAppointment("2 days", 2, ChronoUnit.DAYS),
-      ScheduleAppointment("3 days", 3, ChronoUnit.DAYS),
-      ScheduleAppointment("4 days", 4, ChronoUnit.DAYS),
-      ScheduleAppointment("5 days", 5, ChronoUnit.DAYS),
-      ScheduleAppointment("6 days", 6, ChronoUnit.DAYS),
-      ScheduleAppointment("7 days", 7, ChronoUnit.DAYS),
-      ScheduleAppointment("8 days", 8, ChronoUnit.DAYS),
-      ScheduleAppointment("9 days", 9, ChronoUnit.DAYS),
-      ScheduleAppointment("10 days", 10, ChronoUnit.DAYS),
-      ScheduleAppointment("20 days", 20, ChronoUnit.DAYS),
-      ScheduleAppointment("1 month", 1, ChronoUnit.MONTHS),
-      ScheduleAppointment("2 months", 2, ChronoUnit.MONTHS),
-      ScheduleAppointment("3 months", 3, ChronoUnit.MONTHS),
-      ScheduleAppointment("4 months", 4, ChronoUnit.MONTHS),
-      ScheduleAppointment("5 months", 5, ChronoUnit.MONTHS),
-      ScheduleAppointment("6 months", 6, ChronoUnit.MONTHS),
-      ScheduleAppointment("7 months", 7, ChronoUnit.MONTHS),
-      ScheduleAppointment("8 months", 8, ChronoUnit.MONTHS),
-      ScheduleAppointment("9 months", 9, ChronoUnit.MONTHS),
-      ScheduleAppointment("10 months", 10, ChronoUnit.MONTHS),
-      ScheduleAppointment("11 months", 11, ChronoUnit.MONTHS),
-      ScheduleAppointment("12 months", 12, ChronoUnit.MONTHS)
+      ScheduleAppointment("1 day", 1, DAYS),
+      ScheduleAppointment("2 days", 2, DAYS),
+      ScheduleAppointment("3 days", 3, DAYS),
+      ScheduleAppointment("4 days", 4, DAYS),
+      ScheduleAppointment("5 days", 5, DAYS),
+      ScheduleAppointment("6 days", 6, DAYS),
+      ScheduleAppointment("7 days", 7, DAYS),
+      ScheduleAppointment("8 days", 8, DAYS),
+      ScheduleAppointment("9 days", 9, DAYS),
+      ScheduleAppointment("10 days", 10, DAYS),
+      ScheduleAppointment("20 days", 20, DAYS),
+      oneMonth,
+      ScheduleAppointment("2 months", 2, MONTHS),
+      ScheduleAppointment("3 months", 3, MONTHS),
+      ScheduleAppointment("4 months", 4, MONTHS),
+      ScheduleAppointment("5 months", 5, MONTHS),
+      ScheduleAppointment("6 months", 6, MONTHS),
+      ScheduleAppointment("7 months", 7, MONTHS),
+      ScheduleAppointment("8 months", 8, MONTHS),
+      ScheduleAppointment("9 months", 9, MONTHS),
+      ScheduleAppointment("10 months", 10, MONTHS),
+      ScheduleAppointment("11 months", 11, MONTHS),
+      ScheduleAppointment("12 months", 12, MONTHS)
   )
 
   @Inject
@@ -98,7 +101,7 @@ class ScheduleAppointmentSheet : BottomSheetActivity() {
   private val initialState = {
     val uuid = intent.extras.getSerializable(KEY_PATIENT_UUID) as UUID
     ScheduleAppointmentSheetCreated(
-        defaultDateIndex = possibleDates.lastIndex,
+        defaultDateIndex = possibleDates.indexOf(oneMonth),
         patientUuid = uuid,
         numberOfDates = possibleDates.size
     )

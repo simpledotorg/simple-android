08fe1066621cf98b67a82e8623fe0241859958c1
Saket Narayan <saket@uncommon.is>
2019-03-12 12:51:11 +0530

    Format NewlyVerifiedUser

--- app/src/main/java/org/simple/clinic/user/NewlyVerifiedUser.kt
+++ app/src/main/java/org/simple/clinic/user/NewlyVerifiedUser.kt
@@ -3,18 +3,20 @@ package org.simple.clinic.user
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
-import org.simple.clinic.util.Just
+import org.simple.clinic.user.User.LoggedInStatus.LOGGED_IN
+import org.simple.clinic.user.User.LoggedInStatus.OTP_REQUESTED
 import org.simple.clinic.util.Optional
+import org.simple.clinic.util.filterAndUnwrapJust
 
 class NewlyVerifiedUser : ObservableTransformer<Optional<User>, User> {
 
   override fun apply(upstream: Observable<Optional<User>>): ObservableSource<User> {
     return upstream
-        .filter { it is Just<User> }
-        .map { (user) -> UserAndStatus(user!!, user.loggedInStatus) }
+        .filterAndUnwrapJust()
+        .map { user -> UserAndStatus(user, user.loggedInStatus) }
         .buffer(2, 1)
         .filter { it.size == 2 }
-        .filter { it[0].loggedInStatus == User.LoggedInStatus.OTP_REQUESTED && it[1].loggedInStatus == User.LoggedInStatus.LOGGED_IN }
+        .filter { it[0].loggedInStatus == OTP_REQUESTED && it[1].loggedInStatus == LOGGED_IN }
         .map { it[1].user }
   }
 

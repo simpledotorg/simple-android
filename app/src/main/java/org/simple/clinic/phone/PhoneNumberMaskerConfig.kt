634dad1d58090eb0469336157ec41c82c5756366
Saket Narayan <saket@uncommon.is>
2019-03-22 19:29:58 +0530

    Read PhoneNumberMaskerConfig through Firebase remote config

--- app/src/main/java/org/simple/clinic/phone/PhoneNumberMaskerConfig.kt
+++ app/src/main/java/org/simple/clinic/phone/PhoneNumberMaskerConfig.kt
@@ -1,6 +1,31 @@
 package org.simple.clinic.phone
 
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import io.reactivex.Observable
+import org.simple.clinic.BuildConfig
+
 data class PhoneNumberMaskerConfig(
     val maskingEnabled: Boolean,
     val proxyPhoneNumber: String
-)
+) {
+
+  companion object {
+    private const val KEY_MASKING_ENABLED = "phonenumbermasker_masking_enabled"
+    private const val KEY_PROXY_PHONE_NUM = "phonenumbermasker_proxy_phone_number"
+
+    fun read(source: FirebaseRemoteConfig): Observable<PhoneNumberMaskerConfig> {
+      return Observable.fromCallable {
+        PhoneNumberMaskerConfig(
+            maskingEnabled = source.getBoolean(KEY_MASKING_ENABLED),
+            proxyPhoneNumber = source.getString(KEY_PROXY_PHONE_NUM))
+      }
+    }
+
+    fun defaultValues(): Map<String, Any> {
+      return mapOf(
+          KEY_MASKING_ENABLED to false,
+          KEY_PROXY_PHONE_NUM to BuildConfig.MASKED_PHONE_NUMBER
+      )
+    }
+  }
+}

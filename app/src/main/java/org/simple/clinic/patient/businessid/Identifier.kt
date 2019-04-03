1f60172b51e060782494a70bf83db5d1c4d54985
Vinay S Shenoy <vinay@obvious.in>
2019-04-03 11:11:57 +0530

    Make Identifier a Parcelable class
    This model will be instantiated and passed in via Screen keys in an
    upcoming feature, so it needs to be Parcelable.

--- app/src/main/java/org/simple/clinic/patient/businessid/Identifier.kt
+++ app/src/main/java/org/simple/clinic/patient/businessid/Identifier.kt
@@ -1,12 +1,15 @@
 package org.simple.clinic.patient.businessid
 
+import android.os.Parcelable
 import androidx.annotation.VisibleForTesting
 import androidx.room.ColumnInfo
 import androidx.room.TypeConverter
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
+import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.util.SafeEnumTypeAdapter
 
+@Parcelize
 data class Identifier(
 
     @ColumnInfo(name = "identifier")
@@ -14,11 +17,14 @@ data class Identifier(
 
     @ColumnInfo(name = "identifierType")
     val type: IdentifierType
-) {
-  sealed class IdentifierType {
+): Parcelable {
 
+  sealed class IdentifierType: Parcelable {
+
+    @Parcelize
     object BpPassport : IdentifierType()
 
+    @Parcelize
     data class Unknown(val actual: String) : IdentifierType()
 
     object TypeAdapter : SafeEnumTypeAdapter<IdentifierType>(

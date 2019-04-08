e947e4b94c85e8a3aa6bce82f269382c8dcc56a1
Vinay S Shenoy <code@vinaysshenoy.com>
2019-04-08 10:00:05 +0530

    Statically import IdentifierType in some classes
    - PatientRepository
    - PatientRepositoryAndroidTest
    - PatientSummaryScreenControllerTest

--- app/src/test/java/org/simple/clinic/summary/PatientSummaryScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/summary/PatientSummaryScreenControllerTest.kt
@@ -50,6 +50,7 @@ import org.simple.clinic.patient.PatientRepository
 import org.simple.clinic.patient.PatientSummaryResult
 import org.simple.clinic.patient.SyncStatus
 import org.simple.clinic.patient.businessid.Identifier
+import org.simple.clinic.patient.businessid.Identifier.IdentifierType.BpPassport
 import org.simple.clinic.summary.addphone.MissingPhoneReminderRepository
 import org.simple.clinic.util.Just
 import org.simple.clinic.util.None
@@ -961,18 +962,18 @@ class PatientSummaryScreenControllerTest {
   private fun `patient summary open intentions`() = listOf(
       OpenIntention.ViewExistingPatient,
       OpenIntention.ViewNewPatient,
-      OpenIntention.LinkIdWithPatient(Identifier(UUID.randomUUID().toString(), Identifier.IdentifierType.BpPassport))
+      OpenIntention.LinkIdWithPatient(Identifier(UUID.randomUUID().toString(), BpPassport))
   )
 
   @Suppress("Unused")
   private fun `patient summary open intentions except new patient`() = listOf(
       OpenIntention.ViewExistingPatient,
-      OpenIntention.LinkIdWithPatient(Identifier(UUID.randomUUID().toString(), Identifier.IdentifierType.BpPassport))
+      OpenIntention.LinkIdWithPatient(Identifier(UUID.randomUUID().toString(), BpPassport))
   )
 
   @Suppress("Unused")
   private fun `patient summary open intentions and summary item changed`(): List<List<Any>> {
-    val identifier = Identifier(UUID.randomUUID().toString(), Identifier.IdentifierType.BpPassport)
+    val identifier = Identifier(UUID.randomUUID().toString(), BpPassport)
 
     return listOf(
         listOf(OpenIntention.ViewExistingPatient, true),

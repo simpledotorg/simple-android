ad961f48b2070ba8b821a60de76f45a7281b01aa
Vinay S Shenoy <code@vinaysshenoy.com>
2019-04-11 11:51:41 +0530

    Change IdentifierDisplayAdapterAndroidTest to verify registered types

--- app/src/androidTest/java/org/simple/clinic/util/identifierdisplay/IdentifierDisplayAdapterAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/util/identifierdisplay/IdentifierDisplayAdapterAndroidTest.kt
@@ -7,8 +7,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.simple.clinic.TestClinicApp
 import org.simple.clinic.patient.businessid.Identifier
-import org.simple.clinic.patient.businessid.Identifier.IdentifierType.BpPassport
-import org.simple.clinic.patient.businessid.Identifier.IdentifierType.Unknown
 import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
@@ -23,22 +21,13 @@ class IdentifierDisplayAdapterAndroidTest {
   }
 
   @Test
-  fun all_types_of_identifiers_should_be_converted_for_display_correctly() {
-    val identifiersAndExpectedDisplayCharSequence = Identifier
-        .IdentifierType
-        .values()
-        .plus(Unknown(actual = "bppassport_short_code"))
-        .map {
-          when (it) {
-            BpPassport -> Identifier(value = "1e8e3b37-cadd-4010-a4f8-14522bdf64a3", type = BpPassport) to "183 3740"
-            is Unknown -> Identifier(value = "1234567", type = Unknown(actual = "bppassport_short_code")) to "1234567"
-          }
-        }
+  fun all_known_types_of_adapters_must_be_registered_in_the_adapter() {
+    val registeredIdentifierTypes = identifierDisplayAdapter.converters.keys
 
-    identifiersAndExpectedDisplayCharSequence
-        .forEach { (identifier, expected) ->
-          val charSequence = identifierDisplayAdapter.toCharSequence(identifier)
-          assertThat(charSequence).isEqualTo(expected)
-        }
+    val expected = Identifier.IdentifierType.values().toSet()
+
+    // If this fails, you probably forgot to register an adapter for a newly added IdentifierType in
+    // IdentifierDisplayAdapterModule
+    assertThat(registeredIdentifierTypes).isEqualTo(expected)
   }
 }

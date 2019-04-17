0165921bc061858d9e76923a2a26ddc48e17d9df
Vinay S Shenoy <code@vinaysshenoy.com>
2019-04-17 04:56:49 +0000

    Add factory method PatientConfig#read(ConfigReader)
    This creates an instance of the config using the remote config reader

--- app/src/main/java/org/simple/clinic/patient/PatientConfig.kt
+++ app/src/main/java/org/simple/clinic/patient/PatientConfig.kt
@@ -1,20 +1,47 @@
 package org.simple.clinic.patient
 
-/**
- * See the [SQLite Limits](https://www.sqlite.org/limits.html) doc for more information.
- *
- * See the section "maximum number of host parameters in a single SQL statement".
- * */
-const val MAXIMUM_SQLITE_QUERY_LIMIT = 1000
+import androidx.annotation.VisibleForTesting
+import io.reactivex.Observable
+import org.simple.clinic.remoteconfig.ConfigReader
 
 data class PatientConfig(
     val limitOfSearchResults: Int,
     val scanSimpleCardFeatureEnabled: Boolean,
     val recentPatientLimit: Int
 ) {
-  init {
-    if (limitOfSearchResults !in 1 until MAXIMUM_SQLITE_QUERY_LIMIT) {
-      throw AssertionError("limit of search results must be within [1, 999]")
+
+  companion object {
+
+    /**
+     * See the [SQLite Limits](https://www.sqlite.org/limits.html) doc for more information.
+     *
+     * See the section "maximum number of host parameters in a single SQL statement".
+     * */
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    const val MAXIMUM_SQLITE_QUERY_LIMIT = 1000L
+
+    fun read(configReader: ConfigReader): Observable<PatientConfig> {
+      return Observable.fromCallable {
+        val numberOfSearchResults = configReader
+            .long("patients_searchresults_limit", 100L)
+            .coerceAtLeast(1L)
+            .coerceAtMost(MAXIMUM_SQLITE_QUERY_LIMIT)
+            .toInt()
+
+        val bpPassportFeatureEnabled = configReader
+            .boolean("patients_bppassportfeature_enabled", false)
+
+        val numberOfRecentPatients = configReader
+            .long("patients_recentpatients_limit", 10L)
+            .coerceAtLeast(1L)
+            .toInt()
+
+        PatientConfig(
+            limitOfSearchResults = numberOfSearchResults,
+            scanSimpleCardFeatureEnabled = bpPassportFeatureEnabled,
+            recentPatientLimit = numberOfRecentPatients
+        )
+      }
     }
   }
 }

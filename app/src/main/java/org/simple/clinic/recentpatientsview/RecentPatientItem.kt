1d9a8ec9103506ede733e352c42d4c29ccfa135e
Vinay S Shenoy <vinay@obvious.in>
2019-04-22 09:57:43 +0000

    Remove synthetic accessors from Recent Patients view
    This commit also introduces a dedicated ViewHolder type for the Recent
    Patient item type.

--- app/src/main/java/org/simple/clinic/recentpatientsview/RecentPatientItem.kt
+++ app/src/main/java/org/simple/clinic/recentpatientsview/RecentPatientItem.kt
@@ -5,7 +5,8 @@ import android.content.Context
 import android.view.View
 import com.xwray.groupie.ViewHolder
 import io.reactivex.subjects.Subject
-import kotlinx.android.synthetic.main.recent_patient_item_view.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import kotterknife.bindView
 import org.simple.clinic.R
 import org.simple.clinic.patient.Gender
@@ -26,34 +27,53 @@ data class RecentPatientItem(
     val age: Int,
     val lastBp: LastBp?,
     val gender: Gender
-) : RecentPatientItemType<ViewHolder>(uuid.hashCode().toLong()) {
+) : RecentPatientItemType<RecentPatientItem.RecentPatientViewHolder>(uuid.hashCode().toLong()) {
 
   override fun getLayout(): Int = R.layout.recent_patient_item_view
 
-  @SuppressLint("SetTextI18n")
-  override fun bind(viewHolder: ViewHolder, position: Int) {
-    viewHolder.itemView.apply {
-      recentpatient_item_title.text = "$name, $age"
-
-      val lastBpText = lastBpText(context)
-      recentpatient_item_last_bp.text = lastBpText
-      recentpatient_item_last_bp_label.visibleOrGone(lastBpText.isNotBlank())
-      recentpatient_item_gender.setImageResource(gender.displayIconRes)
-
-      setOnClickListener {
-        uiEvents.onNext(RecentPatientItemClicked(patientUuid = uuid))
-      }
-    }
+  override fun createViewHolder(itemView: View): RecentPatientViewHolder {
+    return RecentPatientViewHolder(itemView)
   }
 
-  private fun lastBpText(context: Context): String =
-      lastBp?.run { "$systolic/$diastolic, ${updatedAtRelativeTimestamp.displayText(context)}" } ?: ""
+  @SuppressLint("SetTextI18n")
+  override fun bind(viewHolder: RecentPatientViewHolder, position: Int) {
+    viewHolder.itemView.setOnClickListener {
+      uiEvents.onNext(RecentPatientItemClicked(patientUuid = uuid))
+    }
+
+    viewHolder.render(name, age, lastBp, gender)
+  }
 
   data class LastBp(
       val systolic: Int,
       val diastolic: Int,
       val updatedAtRelativeTimestamp: RelativeTimestamp
   )
+
+  class RecentPatientViewHolder(rootView: View) : ViewHolder(rootView) {
+    private val nameAgeTextView by bindView<TextView>(R.id.recentpatient_item_title)
+    private val lastBpTextView by bindView<TextView>(R.id.recentpatient_item_last_bp)
+    private val lastBpLabel by bindView<View>(R.id.recentpatient_item_last_bp_label)
+    private val genderImageView by bindView<ImageView>(R.id.recentpatient_item_gender)
+
+    fun render(
+        name: String,
+        age: Int,
+        lastBp: LastBp?,
+        gender: Gender
+    ) {
+      nameAgeTextView.text = itemView.resources.getString(R.string.patients_recentpatients_nameage, name, age)
+
+      val lastBpText = lastBpText(itemView.context, lastBp)
+      lastBpTextView.text = lastBpText
+      lastBpLabel.visibleOrGone(lastBpText.isNotBlank())
+      genderImageView.setImageResource(gender.displayIconRes)
+    }
+
+    private fun lastBpText(context: Context, lastBp: LastBp?): String {
+      return lastBp?.run { "$systolic/$diastolic, ${updatedAtRelativeTimestamp.displayText(context)}" } ?: ""
+    }
+  }
 }
 
 object SeeAllItem : RecentPatientItemType<SeeAllItemViewHolder>(0) {

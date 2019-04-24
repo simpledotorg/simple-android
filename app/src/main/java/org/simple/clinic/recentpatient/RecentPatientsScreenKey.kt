bddeb740d5c2be3ae52f42ebbf270286087c5d67
Rakshak R.Hegde <rakshak@obvious.in>
2019-04-24 10:23:02 +0000

    Add recent patients screen

---
+++ app/src/main/java/org/simple/clinic/recentpatient/RecentPatientsScreenKey.kt
@@ -0,0 +1,15 @@
+package org.simple.clinic.recentpatient
+
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.R
+import org.simple.clinic.router.screen.FullScreenKey
+
+@Parcelize
+class RecentPatientsScreenKey : FullScreenKey {
+
+  @IgnoredOnParcel
+  override val analyticsName = "Recent Patient Screen"
+
+  override fun layoutRes() = R.layout.recent_patients_screen
+}

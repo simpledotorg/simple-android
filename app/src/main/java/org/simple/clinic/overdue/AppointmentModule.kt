dfa28a5b0e59bffd5d6ad8c15f73601677b3ca19
Vinay S Shenoy <vinay@obvious.in>
2019-05-06 05:01:23 +0000

    Enable AppointmentSync v3

--- app/src/main/java/org/simple/clinic/overdue/AppointmentModule.kt
+++ app/src/main/java/org/simple/clinic/overdue/AppointmentModule.kt
@@ -23,7 +23,7 @@ class AppointmentModule {
     return Single.just(AppointmentConfig(
         minimumOverduePeriodForHighRisk = Period.ofDays(30),
         overduePeriodForLowestRiskLevel = Period.ofDays(365),
-        isApiV3Enabled = false,
+        isApiV3Enabled = true,
         appointmentDuePeriodForDefaulters = Period.ofDays(30)
     ))
   }

23c9181fad28b5c99bb5e97f58980350dddf4e25
Vinay S Shenoy <code@vinaysshenoy.com>
2019-05-14 11:10:48 +0000

    Delay screen change actions for all screens
    This is done because in some cases we need to change the backstack as
    soon as a screen opens. Since we set up the UI event streams in
    onFinishInflate(), this can lead to cases where the backstack is
    modified while a screen change animation is ongoing. This causes
    problems with Flow (memory leaks where screens are retained in the view
    hierarchy but not in the backstack).
    
    This commit delays all screen change events that are emitted from
    controllers by the screen change animation duration so that we are
    guaranteed that any changes to the backstack will happen only after
    all screen change animations are run.

--- app/src/main/java/org/simple/clinic/ViewControllerBinding.kt
+++ app/src/main/java/org/simple/clinic/ViewControllerBinding.kt
@@ -7,9 +7,29 @@ import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 import io.reactivex.schedulers.Schedulers.io
 import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
+import org.threeten.bp.Duration
+import java.util.concurrent.TimeUnit
 
 @SuppressLint("CheckResult")
 fun <T> bindUiToController(
+    ui: T,
+    events: Observable<UiEvent>,
+    controller: ObservableTransformer<UiEvent, (T) -> Unit>,
+    screenDestroys: Observable<ScreenDestroyed>,
+    uiChangeDelay: Duration = SCREEN_CHANGE_ANIMATION_DURATION
+) {
+  events
+      .mergeWith(screenDestroys)
+      .observeOn(io())
+      .compose(controller)
+      .delaySubscription(uiChangeDelay.toMillis(), TimeUnit.MILLISECONDS)
+      .observeOn(mainThread())
+      .takeUntil(screenDestroys)
+      .subscribe { uiChange -> uiChange(ui) }
+}
+
+@SuppressLint("CheckResult")
+fun <T> bindUiToControllerWithoutDelay(
     ui: T,
     events: Observable<UiEvent>,
     controller: ObservableTransformer<UiEvent, (T) -> Unit>,

9b71039a3662fd0d7e2b1c695d62067762a45615
Vinay S Shenoy <vinay@obvious.in>
2019-05-17 16:45:30 +0530

    Provide loggedInUser externally in PatientRepository

--- app/src/test/java/org/simple/clinic/summary/linkId/LinkIdWithPatientViewControllerTest.kt
+++ app/src/test/java/org/simple/clinic/summary/linkId/LinkIdWithPatientViewControllerTest.kt
@@ -5,14 +5,17 @@ import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.subjects.PublishSubject
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.simple.clinic.facility.FacilityRepository
 import org.simple.clinic.patient.PatientMocker
 import org.simple.clinic.patient.PatientRepository
 import org.simple.clinic.patient.businessid.Identifier
+import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.RxErrorsRule
 import org.simple.clinic.widgets.UiEvent
 import java.util.UUID
@@ -26,9 +29,9 @@ class LinkIdWithPatientViewControllerTest {
 
   private val view = mock<LinkIdWithPatientView>()
 
-  private val uiEvents = PublishSubject.create<UiEvent>()
+  private val userSession = mock<UserSession>()
 
-  private val controller = LinkIdWithPatientViewController(patientRepository)
+  private val uiEvents = PublishSubject.create<UiEvent>()
 
   private val patientUuid = UUID.randomUUID()
 
@@ -37,8 +40,18 @@ class LinkIdWithPatientViewControllerTest {
       type = Identifier.IdentifierType.random()
   )
 
+  private val user = PatientMocker.loggedInUser()
+
+  private val facility = PatientMocker.facility()
+
+  private val facilityRepository = mock<FacilityRepository>()
+
+  private val controller = LinkIdWithPatientViewController(patientRepository, userSession, facilityRepository)
+
   @Before
   fun setUp() {
+    whenever(userSession.requireLoggedInUser()).thenReturn(Observable.just(user))
+    whenever(facilityRepository.currentFacility(user)).thenReturn(Observable.just(facility))
     uiEvents
         .compose(controller)
         .subscribe { uiChange -> uiChange(view) }
@@ -55,14 +68,13 @@ class LinkIdWithPatientViewControllerTest {
   fun `when add is clicked, id should be added to patient and sheet should close`() {
     val businessId = PatientMocker.businessId(patientUuid = patientUuid, identifier = identifier)
 
-    whenever(patientRepository.addIdentifierToPatient(patientUuid, identifier)).thenReturn(Single.just(businessId))
+    whenever(patientRepository.addIdentifierToPatient(patientUuid, identifier, user, facility)).thenReturn(Single.just(businessId))
 
     uiEvents.onNext(LinkIdWithPatientViewShown(patientUuid, identifier))
     uiEvents.onNext(LinkIdWithPatientAddClicked)
 
-    verify(patientRepository).addIdentifierToPatient(patientUuid, identifier)
+    verify(patientRepository).addIdentifierToPatient(patientUuid, identifier, user, facility)
     verify(view).closeSheetWithIdLinked()
-
   }
 
   @Test
@@ -71,6 +83,6 @@ class LinkIdWithPatientViewControllerTest {
     uiEvents.onNext(LinkIdWithPatientCancelClicked)
 
     verify(view).closeSheetWithoutIdLinked()
-    verify(patientRepository, never()).addIdentifierToPatient(any(), any())
+    verify(patientRepository, never()).addIdentifierToPatient(any(), any(), any(), any())
   }
 }

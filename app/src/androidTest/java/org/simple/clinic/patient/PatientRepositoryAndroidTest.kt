30331af319dab2a39986ce0dafeb101a2730fb32
Sanchita Agarwal <sanchita@obvious.in>
2019-05-21 10:57:03 +0000

    Add method PatientRepository#updatePatientRecordedAt to check and update recordedAt

--- app/src/androidTest/java/org/simple/clinic/patient/PatientRepositoryAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/patient/PatientRepositoryAndroidTest.kt
@@ -1735,4 +1735,46 @@ class PatientRepositoryAndroidTest {
     assertThat(latestBpPassport).isEqualTo(currentBpPassport)
   }
 
+  @Test
+  fun when_updating_patient_recordedAt_then_it_should_compare_and_update_the_date() {
+
+    fun createPatientProfile(patientUuid: UUID, recordedAt: Instant): PatientProfile {
+      return testData.patientProfile(patientUuid = patientUuid)
+          .run {
+            copy(patient = patient.copy(
+                recordedAt = recordedAt,
+                updatedAt = Instant.now(testClock),
+                syncStatus = DONE)
+            )
+          }
+    }
+
+    val patientUuid1 = UUID.randomUUID()
+    val recordedAtForPatient1 = Instant.now(testClock)
+    val patientProfile1 = createPatientProfile(patientUuid1, recordedAtForPatient1)
+    testClock.advanceBy(Duration.ofMinutes(1))
+    val instantToCompare1 = Instant.now(testClock)
+
+    val patientUuid2 = UUID.randomUUID()
+    val instantToCompare2 = Instant.now(testClock)
+    testClock.advanceBy(Duration.ofMinutes(1))
+    val recordedAtForPatient2 = Instant.now(testClock)
+    val patientProfile2 = createPatientProfile(patientUuid2, recordedAtForPatient2)
+
+    patientRepository.save(listOf(patientProfile1, patientProfile2)).blockingAwait()
+
+    patientRepository.compareAndUpdateRecordedAt(patientUuid1, instantToCompare1).blockingAwait()
+    patientRepository.compareAndUpdateRecordedAt(patientUuid2, instantToCompare2).blockingAwait()
+
+    val patient1 = patientRepository.patient(patientUuid1).blockingFirst().toNullable()!!
+    val patient2 = patientRepository.patient(patientUuid2).blockingFirst().toNullable()!!
+
+    assertThat(patient1.recordedAt).isEqualTo(recordedAtForPatient1)
+    assertThat(patient1.syncStatus).isEqualTo(DONE)
+
+    assertThat(patient2.recordedAt).isEqualTo(instantToCompare2)
+    assertThat(patient2.syncStatus).isEqualTo(PENDING)
+    assertThat(patient2.updatedAt).isEqualTo(Instant.now(testClock))
+  }
 }
+

005fb0303115bf09fc4f1df9923b831a1516bba1
Vinay S Shenoy <vinay@obvious.in>
2019-05-28 14:33:11 +0530

    Change PrescriptionSyncApiV2 to use global api version
    This commit also renames the interface to PrescriptionSyncApi since the
    verison is now controlled externally.

--- app/src/main/java/org/simple/clinic/drugs/sync/PrescriptionSyncApi.kt
+++ app/src/main/java/org/simple/clinic/drugs/sync/PrescriptionSyncApi.kt
@@ -1,24 +1,21 @@
 package org.simple.clinic.drugs.sync
 
 import io.reactivex.Single
+import org.simple.clinic.CURRENT_API_VERSION
 import org.simple.clinic.sync.DataPushResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 
-interface PrescriptionSyncApiV2 {
+interface PrescriptionSyncApi {
 
-  companion object {
-    const val version = "v2"
-  }
-
-  @POST("$version/prescription_drugs/sync")
+  @POST("$CURRENT_API_VERSION/prescription_drugs/sync")
   fun push(
       @Body body: PrescriptionPushRequest
   ): Single<DataPushResponse>
 
-  @GET("$version/prescription_drugs/sync")
+  @GET("$CURRENT_API_VERSION/prescription_drugs/sync")
   fun pull(
       @Query("limit") recordsToPull: Int,
       @Query("process_token") lastPullToken: String? = null

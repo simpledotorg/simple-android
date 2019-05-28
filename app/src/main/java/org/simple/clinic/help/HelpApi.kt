e7479cdbc9a6d4550483630b62437a196f316061
Vinay S Shenoy <vinay@obvious.in>
2019-05-28 14:43:54 +0530

    Change HelpApi to use global api version

--- app/src/main/java/org/simple/clinic/help/HelpApi.kt
+++ app/src/main/java/org/simple/clinic/help/HelpApi.kt
@@ -1,10 +1,11 @@
 package org.simple.clinic.help
 
 import io.reactivex.Single
+import org.simple.clinic.CURRENT_API_VERSION
 import retrofit2.http.GET
 
 interface HelpApi {
 
-  @GET("v2/help.html")
+  @GET("$CURRENT_API_VERSION/help.html")
   fun help(): Single<String>
 }

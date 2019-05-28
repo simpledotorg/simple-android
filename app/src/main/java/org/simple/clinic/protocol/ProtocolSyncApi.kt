2215b6f34be060a94ebc2ca324650eb45702472b
Vinay S Shenoy <vinay@obvious.in>
2019-05-28 16:09:04 +0530

    Change ProtocolSynncApiV2 to use the global api version
    This commit also renames the interface to ProtocolSyncApi since the
    version is now controlled externally.

--- app/src/main/java/org/simple/clinic/protocol/ProtocolSyncApi.kt
+++ app/src/main/java/org/simple/clinic/protocol/ProtocolSyncApi.kt
@@ -1,19 +1,15 @@
 package org.simple.clinic.protocol
 
 import io.reactivex.Single
+import org.simple.clinic.CURRENT_API_VERSION
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface ProtocolSyncApiV2 {
+interface ProtocolSyncApi {
 
-  companion object {
-    const val version = "v2"
-  }
-
-  @GET("$version/protocols/sync")
+  @GET("$CURRENT_API_VERSION/protocols/sync")
   fun pull(
       @Query("limit") recordsToPull: Int,
       @Query("process_token") lastPullToken: String? = null
   ): Single<ProtocolPullResponse>
-
 }

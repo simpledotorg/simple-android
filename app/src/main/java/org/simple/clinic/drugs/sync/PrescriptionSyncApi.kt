503a5a6bcdd29358e8e2a56b47efe5052ab79c15
Vinay S Shenoy <code@vinaysshenoy.com>
2019-05-29 06:56:04 +0000

    Add static "X-RESYNC-TOKEN" headers to all sync models (GET methods)

--- app/src/main/java/org/simple/clinic/drugs/sync/PrescriptionSyncApi.kt
+++ app/src/main/java/org/simple/clinic/drugs/sync/PrescriptionSyncApi.kt
@@ -4,6 +4,7 @@ import io.reactivex.Single
 import org.simple.clinic.sync.DataPushResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Query
 
@@ -14,6 +15,7 @@ interface PrescriptionSyncApi {
       @Body body: PrescriptionPushRequest
   ): Single<DataPushResponse>
 
+  @Headers(value = ["X-RESYNC-TOKEN: 1"])
   @GET("prescription_drugs/sync")
   fun pull(
       @Query("limit") recordsToPull: Int,
--- app/src/main/java/org/simple/clinic/medicalhistory/sync/MedicalHistorySyncApi.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/sync/MedicalHistorySyncApi.kt
@@ -4,6 +4,7 @@ import io.reactivex.Single
 import org.simple.clinic.sync.DataPushResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Query
 
@@ -14,6 +15,7 @@ interface MedicalHistorySyncApi {
       @Body body: MedicalHistoryPushRequest
   ): Single<DataPushResponse>
 
+  @Headers(value = ["X-RESYNC-TOKEN: 1"])
   @GET("medical_histories/sync")
   fun pull(
       @Query("limit") recordsToPull: Int,

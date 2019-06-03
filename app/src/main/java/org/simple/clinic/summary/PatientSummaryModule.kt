7aa680571034fffb0932c443dc8ee578d249062d
Vinay S Shenoy <vinay@obvious.in>
2019-06-03 13:45:49 +0530

    Remove PatientSummaryResult class

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryModule.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryModule.kt
@@ -1,14 +1,9 @@
 package org.simple.clinic.summary
 
-import com.f2prateek.rx.preferences2.Preference
-import com.f2prateek.rx.preferences2.RxSharedPreferences
-import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import io.reactivex.Single
 import org.simple.clinic.AppDatabase
-import org.simple.clinic.patient.PatientSummaryResult
-import javax.inject.Named
 
 @Module
 open class PatientSummaryModule {
@@ -18,13 +13,6 @@ open class PatientSummaryModule {
       numberOfBpPlaceholders = 3,
       numberOfBpsToDisplay = 100
   ))
-
-  @Provides
-  @Named("patient_summary_result")
-  fun patientSummaryResult(rxSharedPreferences: RxSharedPreferences, moshi: Moshi): Preference<PatientSummaryResult> {
-    val typeConverter = PatientSummaryResult.RxPreferencesConverter(moshi)
-    return rxSharedPreferences.getObject("patient_summary_result_v1", PatientSummaryResult.NotSaved, typeConverter)
-  }
 
   @Provides
   fun missingPhoneReminderDao(appDatabase: AppDatabase) = appDatabase.missingPhoneReminderDao()

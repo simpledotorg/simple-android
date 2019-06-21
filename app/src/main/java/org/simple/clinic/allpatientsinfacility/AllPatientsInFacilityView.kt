916aad738e3d651af5d7d687bee243f85589e79f
Vinay S Shenoy <vinay@obvious.in>
2019-06-21 06:35:37 +0000

    Handle UI changes in all patients in facility view

--- app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityView.kt
+++ app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityView.kt
@@ -2,7 +2,12 @@ package org.simple.clinic.allpatientsinfacility
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.view_allpatientsinfacility.view.*
+import org.simple.clinic.R
 import org.simple.clinic.facility.Facility
 import org.simple.clinic.patient.PatientSearchResult
 
@@ -11,11 +16,37 @@ class AllPatientsInFacilityView(
     attributeSet: AttributeSet
 ) : FrameLayout(context, attributeSet), AllPatientsInFacilityUi {
 
+  private val searchResultsAdapter = AllPatientsInFacilityListAdapter()
+
+  override fun onFinishInflate() {
+    super.onFinishInflate()
+    setupAllPatientsList()
+    setupInitialViewVisibility()
+  }
+
+  private fun setupInitialViewVisibility() {
+    patientsList.visibility = View.GONE
+    noPatientsContainer.visibility = View.GONE
+  }
+
+  private fun setupAllPatientsList() {
+    patientsList.apply {
+      setHasFixedSize(true)
+      layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+      adapter = searchResultsAdapter
+    }
+  }
+
   override fun showNoPatientsFound(facilityName: String) {
-    TODO("not implemented")
+    patientsList.visibility = View.GONE
+    noPatientsContainer.visibility = View.VISIBLE
+    noPatientsLabel.text = resources.getString(R.string.allpatientsinfacility_nopatients_title, facilityName)
   }
 
   override fun showPatients(facility: Facility, patientSearchResults: List<PatientSearchResult>) {
-    TODO("not implemented")
+    patientsList.visibility = View.VISIBLE
+    noPatientsContainer.visibility = View.GONE
+    val listItems = AllPatientsInFacilityListItem.mapSearchResultsToListItems(facility, patientSearchResults)
+    searchResultsAdapter.submitList(listItems)
   }
 }

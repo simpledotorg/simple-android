7f4846ba029116280dcd73ccd4336ebc4c77e8a8
Vinay S Shenoy <vinay@obvious.in>
2019-06-26 10:51:21 +0530

    Add UI events from All Patients In Facility View

--- app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityListAdapter.kt
+++ app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityListAdapter.kt
@@ -2,19 +2,26 @@ package org.simple.clinic.allpatientsinfacility
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 import org.simple.clinic.widgets.recyclerview.ViewHolderX
 
-class AllPatientsInFacilityListAdapter : ListAdapter<AllPatientsInFacilityListItem, ViewHolderX>(
-    AllPatientsInFacilityListItem.AllPatientsInFacilityListItemCallback()
-) {
+class AllPatientsInFacilityListAdapter(
+    diffCallback: DiffUtil.ItemCallback<AllPatientsInFacilityListItem>
+) : ListAdapter<AllPatientsInFacilityListItem, ViewHolderX>(diffCallback) {
+
+  private val eventSubject = PublishSubject.create<AllPatientsInFacilityListItem.Event>()
+
+  val listItemEvents: Observable<AllPatientsInFacilityListItem.Event> = eventSubject.hide()
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderX {
     return ViewHolderX(LayoutInflater.from(parent.context).inflate(viewType, parent, false))
   }
 
   override fun onBindViewHolder(holder: ViewHolderX, position: Int) {
-    getItem(position).render(holder)
+    getItem(position).render(holder, eventSubject)
   }
 
   override fun getItemViewType(position: Int): Int {

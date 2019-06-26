9be738390678c5e84610b56489096f7d7a47bd3f
Sanchita Agarwal <sanchita@obvious.in>
2019-06-26 07:21:18 +0000

    Add remote config for showing app update

--- app/src/main/java/org/simple/clinic/appupdate/AppUpdateConfig.kt
+++ app/src/main/java/org/simple/clinic/appupdate/AppUpdateConfig.kt
@@ -1,6 +1,17 @@
 package org.simple.clinic.appupdate
 
+import org.simple.clinic.remoteconfig.ConfigReader
+
 data class AppUpdateConfig(
     val inAppUpdateEnabled: Boolean,
     val differenceBetweenVersionsToNudge: Long
-)
+) {
+
+  companion object {
+    fun read(reader: ConfigReader): AppUpdateConfig {
+      return AppUpdateConfig(
+          inAppUpdateEnabled = reader.boolean(name = "appupdate_enabled", default = false),
+          differenceBetweenVersionsToNudge = reader.long(name = "appupdate_version_diff_to_nudge", default = 1000))
+    }
+  }
+}

93eeea7f594f688a715c8be56f06650b04127b74
Vinay S Shenoy <code@vinaysshenoy.com>
2019-07-04 09:40:54 +0000

    Hook all patients in facility events to add id to patient search screen

--- app/src/main/java/org/simple/clinic/addidtopatient/searchforpatient/AddIdToPatientSearchScreen.kt
+++ app/src/main/java/org/simple/clinic/addidtopatient/searchforpatient/AddIdToPatientSearchScreen.kt
@@ -2,23 +2,32 @@ package org.simple.clinic.addidtopatient.searchforpatient
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.RelativeLayout
 import com.jakewharton.rxbinding2.view.RxView
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.ofType
 import kotlinx.android.synthetic.main.screen_addidtopatientsearch.view.*
 import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
 import org.simple.clinic.addidtopatient.searchresults.AddIdToPatientSearchResultsScreenKey
+import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilitySearchResultClicked
+import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityView
 import org.simple.clinic.bindUiToController
 import org.simple.clinic.router.screen.ScreenRouter
+import org.simple.clinic.summary.OpenIntention
+import org.simple.clinic.summary.PatientSummaryScreenKey
 import org.simple.clinic.text.style.TextAppearanceWithLetterSpacingSpan
 import org.simple.clinic.util.Truss
+import org.simple.clinic.util.UtcClock
 import org.simple.clinic.util.identifierdisplay.IdentifierDisplayAdapter
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ScreenDestroyed
-import org.simple.clinic.widgets.showKeyboard
+import org.simple.clinic.widgets.UiEvent
+import org.threeten.bp.Instant
+import java.util.UUID
 import javax.inject.Inject
 
 class AddIdToPatientSearchScreen(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
@@ -35,6 +44,9 @@ class AddIdToPatientSearchScreen(context: Context, attrs: AttributeSet) : Relati
   @Inject
   lateinit var identifierDisplayAdapter: IdentifierDisplayAdapter
 
+  @Inject
+  lateinit var utcClock: UtcClock
+
   private val screenKey by unsafeLazy {
     screenRouter.key<AddIdToPatientSearchScreenKey>(this)
   }
@@ -46,7 +58,6 @@ class AddIdToPatientSearchScreen(context: Context, attrs: AttributeSet) : Relati
     }
     TheActivity.component.inject(this)
 
-    fullNameEditText.showKeyboard()
     toolBar.setNavigationOnClickListener {
       screenRouter.pop()
     }
@@ -54,7 +65,11 @@ class AddIdToPatientSearchScreen(context: Context, attrs: AttributeSet) : Relati
 
     bindUiToController(
         ui = this,
-        events = Observable.merge(nameChanges(), searchClicks()),
+        events = Observable.merge(
+            nameChanges(),
+            searchClicks(),
+            patientClickEvents()
+        ),
         controller = controller,
         screenDestroys = RxView.detaches(this).map { ScreenDestroyed() }
     )
@@ -95,12 +110,12 @@ class AddIdToPatientSearchScreen(context: Context, attrs: AttributeSet) : Relati
         .map { SearchClicked }
   }
 
-  fun showSearchButtonAsEnabled() {
-    searchButtonFrame.isEnabled = true
-  }
-
-  fun showSearchButtonAsDisabled() {
-    searchButtonFrame.isEnabled = false
+  private fun patientClickEvents(): Observable<UiEvent> {
+    return (allPatientsView as AllPatientsInFacilityView)
+        .uiEvents
+        .ofType<AllPatientsInFacilitySearchResultClicked>()
+        .map { it.patientUuid }
+        .map(::PatientItemClicked)
   }
 
   fun openAddIdToPatientSearchResultsScreen(name: String) {
@@ -117,5 +132,29 @@ class AddIdToPatientSearchScreen(context: Context, attrs: AttributeSet) : Relati
       null
     }
     fullNameInputLayout.isErrorEnabled = visible
+  }
+
+  fun openPatientSummary(patientUuid: UUID) {
+    screenRouter.push(PatientSummaryScreenKey(
+        patientUuid = patientUuid,
+        intention = OpenIntention.LinkIdWithPatient(screenKey.identifier),
+        screenCreatedTimestamp = Instant.now(utcClock)
+    ))
+  }
+
+  fun showAllPatientsInFacility() {
+    allPatientsView.visibility = View.VISIBLE
+  }
+
+  fun hideAllPatientsInFacility() {
+    allPatientsView.visibility = View.GONE
+  }
+
+  fun showSearchButton() {
+    searchButtonFrame.visibility = View.VISIBLE
+  }
+
+  fun hideSearchButton() {
+    searchButtonFrame.visibility = View.GONE
   }
 }

a7bdf9d6df0d244912663846c247fde6433667a2
Vinay S Shenoy <code@vinaysshenoy.com>
2019-07-15 06:50:44 +0000

    Hide keyboard on scroll in patient search screen

--- app/src/main/java/org/simple/clinic/search/PatientSearchScreen.kt
+++ app/src/main/java/org/simple/clinic/search/PatientSearchScreen.kt
@@ -13,6 +13,7 @@ import kotlinx.android.synthetic.main.screen_patient_search.view.*
 import kotlinx.android.synthetic.main.view_findorregisterpatient_toolbar.view.*
 import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityListScrolled
 import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilitySearchResultClicked
 import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityView
 import org.simple.clinic.bindUiToController
@@ -21,8 +22,11 @@ import org.simple.clinic.search.results.PatientSearchResultsScreenKey
 import org.simple.clinic.summary.OpenIntention
 import org.simple.clinic.summary.PatientSummaryScreenKey
 import org.simple.clinic.util.UtcClock
+import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
+import org.simple.clinic.widgets.hideKeyboard
+import org.simple.clinic.widgets.showKeyboard
 import org.threeten.bp.Instant
 import java.util.UUID
 import javax.inject.Inject
@@ -41,6 +45,10 @@ class PatientSearchScreen(context: Context, attrs: AttributeSet) : RelativeLayou
   @Inject
   lateinit var utcClock: UtcClock
 
+  private val allPatientsInFacilityView by unsafeLazy {
+    allPatientsView as AllPatientsInFacilityView
+  }
+
   override fun onFinishInflate() {
     super.onFinishInflate()
     if (isInEditMode) {
@@ -51,6 +59,10 @@ class PatientSearchScreen(context: Context, attrs: AttributeSet) : RelativeLayou
     toolbar.backButton.setOnClickListener {
       screenRouter.pop()
     }
+    toolbar.patientNameEditText.showKeyboard()
+
+    val screenDestroys = RxView.detaches(this).map { ScreenDestroyed() }
+    hideKeyboardWhenAllPatientsListIsScrolled(screenDestroys)
 
     bindUiToController(
         ui = this,
@@ -60,7 +72,7 @@ class PatientSearchScreen(context: Context, attrs: AttributeSet) : RelativeLayou
             patientClickEvents()
         ),
         controller = controller,
-        screenDestroys = RxView.detaches(this).map { ScreenDestroyed() }
+        screenDestroys = screenDestroys
     )
   }
 
@@ -82,10 +94,19 @@ class PatientSearchScreen(context: Context, attrs: AttributeSet) : RelativeLayou
   }
 
   private fun patientClickEvents(): Observable<UiEvent> {
-    return (allPatientsView as AllPatientsInFacilityView)
+    return allPatientsInFacilityView
         .uiEvents
         .ofType<AllPatientsInFacilitySearchResultClicked>()
         .map { PatientItemClicked(it.patientUuid) }
+  }
+
+  @Suppress("CheckResult")
+  private fun hideKeyboardWhenAllPatientsListIsScrolled(screenDestroys: Observable<ScreenDestroyed>) {
+    allPatientsInFacilityView
+        .uiEvents
+        .ofType<AllPatientsInFacilityListScrolled>()
+        .takeUntil(screenDestroys)
+        .subscribe { toolbar.hideKeyboard() }
   }
 
   fun openPatientSearchResultsScreen(name: String) {

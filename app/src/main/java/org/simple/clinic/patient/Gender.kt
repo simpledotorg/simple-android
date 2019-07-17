be8b639aad722aed5b814e1560b3556c66987a21
Ragunath Jawahar <ragunath@obvious.in>
2019-07-17 12:41:58 +0530

    Remove unused property from Gender

--- app/src/main/java/org/simple/clinic/patient/Gender.kt
+++ app/src/main/java/org/simple/clinic/patient/Gender.kt
@@ -7,19 +7,18 @@ import org.simple.clinic.R
 import org.simple.clinic.util.RoomEnumTypeConverter
 
 enum class Gender(
-    @StringRes val displayTextRes: Int,
     @StringRes val displayLetterRes: Int,
     @DrawableRes val displayIconRes: Int
 ) {
 
   @Json(name = "male")
-  MALE(R.string.gender_male, R.string.gender_male_letter, R.drawable.ic_patient_male),
+  MALE(R.string.gender_male_letter, R.drawable.ic_patient_male),
 
   @Json(name = "female")
-  FEMALE(R.string.gender_female, R.string.gender_female_letter, R.drawable.ic_patient_female),
+  FEMALE(R.string.gender_female_letter, R.drawable.ic_patient_female),
 
   @Json(name = "transgender")
-  TRANSGENDER(R.string.gender_transgender, R.string.gender_trans_letter, R.drawable.ic_patient_transgender);
+  TRANSGENDER(R.string.gender_trans_letter, R.drawable.ic_patient_transgender);
 
   class RoomTypeConverter : RoomEnumTypeConverter<Gender>(Gender::class.java)
 }

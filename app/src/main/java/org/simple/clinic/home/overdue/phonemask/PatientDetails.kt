98d8387ab9cd2ac837e1ea69ee7203460ca2e4e7
Vinay S Shenoy <code@vinaysshenoy.com>
2019-07-18 10:10:38 +0530

    Remove presentation logic from PatientDetails in phone mask bottom sheet

--- app/src/main/java/org/simple/clinic/home/overdue/phonemask/PatientDetails.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/phonemask/PatientDetails.kt
@@ -1,8 +1,10 @@
 package org.simple.clinic.home.overdue.phonemask
 
+import org.simple.clinic.patient.Gender
+
 data class PatientDetails(
-    val phoneNumber: String?,
+    val phoneNumber: String,
     val name: String,
-    val genderLetterRes: Int,
+    val gender: Gender,
     val age: Int
 )

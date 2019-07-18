7e6cbdf87c11a036e821d00c8a01d81ef598387e
Sanchita Agarwal <sanchita@obvious.in>
2019-07-18 13:49:28 +0000

    Add loading screen to make blocking call for registering user

---
+++ app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingScreen.kt
@@ -0,0 +1,40 @@
+package org.simple.clinic.registration.register
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.Observable
+import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.bindUiToController
+import org.simple.clinic.widgets.ScreenCreated
+import org.simple.clinic.widgets.ScreenDestroyed
+import org.simple.clinic.widgets.UiEvent
+import javax.inject.Inject
+
+class RegistrationLoadingScreen(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+  @Inject
+  lateinit var controller: RegistrationLoadingScreenController
+
+  override fun onFinishInflate() {
+    super.onFinishInflate()
+
+    TheActivity.component.inject(this)
+
+    bindUiToController(
+        ui = this,
+        events = Observable.just<UiEvent>(ScreenCreated()),
+        controller = controller,
+        screenDestroys = RxView.detaches(this).map { ScreenDestroyed() }
+    )
+  }
+
+  fun openHomeScreen() {
+    TODO("not implemented")
+  }
+
+  fun showError() {
+    TODO("not implemented")
+  }
+}
---
+++ app/src/test/java/org/simple/clinic/registration/register/RegistrationLoadingScreenControllerTest.kt
@@ -0,0 +1,58 @@
+package org.simple.clinic.registration.register
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Single
+import io.reactivex.subjects.PublishSubject
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.simple.clinic.registration.RegistrationResult
+import org.simple.clinic.user.UserSession
+import org.simple.clinic.widgets.ScreenCreated
+import org.simple.clinic.widgets.UiEvent
+
+@RunWith(JUnitParamsRunner::class)
+class RegistrationLoadingScreenControllerTest {
+
+  private val userSession = mock<UserSession>()
+  private val screen = mock<RegistrationLoadingScreen>()
+  lateinit var controller: RegistrationLoadingScreenController
+
+  private val uiEvents = PublishSubject.create<UiEvent>()
+
+  @Before
+  fun setUp() {
+    controller = RegistrationLoadingScreenController(userSession)
+
+    uiEvents
+        .compose(controller)
+        .subscribe { uiChange -> uiChange(screen) }
+  }
+
+  @Parameters(method = "params for testing user registration")
+  @Test
+  fun `when screen is created then user registration api should be called`(results: RegistrationResult) {
+    whenever(userSession.register()).thenReturn(Single.just(results))
+
+    uiEvents.onNext(ScreenCreated())
+
+    verify(userSession).register()
+    when (results) {
+      is RegistrationResult.Success -> verify(screen).openHomeScreen()
+      is RegistrationResult.Error -> verify(screen).showError()
+    }
+    verifyNoMoreInteractions(screen)
+  }
+
+  @Suppress("unused")
+  fun `params for testing user registration`() =
+      listOf<RegistrationResult>(
+          RegistrationResult.Success(),
+          RegistrationResult.Error()
+      )
+}

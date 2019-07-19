6e26f151bcfe1c8123de1799ae61de283a6fe5b9
Vinay S Shenoy <code@vinaysshenoy.com>
2019-07-19 11:04:13 +0000

    Inline the FindOrRegisterPatientToolbar XML into the PatientSearchScreen
    Earlier, the same toolbar was supposed to be used in two different
    screens, the PatientSearchScreen and the AddIdToPatientSearchScreen. So,
    it was implemented as a reusable widget. However, after adding it to the
    AddIdToPatientSearchScreen, it was apparent that the design does not
    really go well with that screen and the design was changed.
    
    Because of this, there is no longer a need to have a separate widget.

--- app/src/main/java/org/simple/clinic/search/PatientSearchScreen.kt
+++ app/src/main/java/org/simple/clinic/search/PatientSearchScreen.kt
@@ -10,7 +10,6 @@ import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
 import kotlinx.android.synthetic.main.screen_patient_search.view.*
-import kotlinx.android.synthetic.main.view_findorregisterpatient_toolbar.view.*
 import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
 import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityListScrolled

530f9d3a21344f5e45859c58e98f19ab83d97d71
Sanchita Agarwal <sanchita@obvious.in>
2019-07-22 13:21:57 +0000

    Remove user registration scheduler

--- app/src/main/java/org/simple/clinic/registration/RegistrationConfig.kt
+++ app/src/main/java/org/simple/clinic/registration/RegistrationConfig.kt
@@ -4,7 +4,6 @@ import org.simple.clinic.util.Distance
 import org.threeten.bp.Duration
 
 data class RegistrationConfig(
-    val retryBackOffDelayInMinutes: Long, // TODO: convert to Duration
     val locationListenerExpiry: Duration,
     val locationUpdateInterval: Duration,
     val proximityThresholdForNearbyFacilities: Distance,

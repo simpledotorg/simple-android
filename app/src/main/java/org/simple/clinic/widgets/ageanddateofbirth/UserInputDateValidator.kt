7b4b335be8b6fd36ccb5dede732246dcf3e9c2ee
Ragunath Jawahar <ragunath@obvious.in>
2019-07-23 11:35:52 +0000

    Remove deprecated validate function and enum from UserInputDateValidator

--- app/src/main/java/org/simple/clinic/widgets/ageanddateofbirth/UserInputDateValidator.kt
+++ app/src/main/java/org/simple/clinic/widgets/ageanddateofbirth/UserInputDateValidator.kt
@@ -13,29 +13,11 @@ class UserInputDateValidator @Inject constructor(
     @Named("date_for_user_input") private val dateOfBirthFormat: DateTimeFormatter
 ) {
 
-  @Deprecated(message = "deprecated in favor of Result2")
-  enum class Result {
-    VALID,
-    INVALID_PATTERN,
-    DATE_IS_IN_FUTURE,
-  }
-
   sealed class Result2 {
     data class Valid(val parsedDate: LocalDate) : Result2()
     sealed class Invalid : Result2() {
       object InvalidPattern : Invalid()
       object DateIsInFuture : Invalid()
-    }
-  }
-
-  @Deprecated(
-      message = "use validate2 instead which uses sealed classes instead of enums",
-      replaceWith = ReplaceWith("validate2(dateText, nowDate)"))
-  fun validate(dateText: String, nowDate: LocalDate = dateInUserTimeZone()): Result {
-    return when (validate2(dateText, nowDate)) {
-      is Result2.Valid -> Result.VALID
-      is Result2.Invalid.InvalidPattern -> Result.INVALID_PATTERN
-      is Result2.Invalid.DateIsInFuture -> Result.DATE_IS_IN_FUTURE
     }
   }
 

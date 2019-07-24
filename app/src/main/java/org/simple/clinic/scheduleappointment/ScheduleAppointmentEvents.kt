1ab1764d739b97d0029f89c35e3f4ce100714bdb
Rakshak R.Hegde <rakshak@obvious.in>
2019-07-24 07:00:47 +0000

    Show calendar date in schedule appointment dialog

--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentEvents.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentEvents.kt
@@ -5,16 +5,37 @@ import java.util.UUID
 
 data class ScheduleAppointmentSheetCreated(val defaultDateIndex: Int, val patientUuid: UUID, val numberOfDates: Int) : UiEvent
 
+data class ScheduleAppointmentSheetCreated2(
+    val possibleAppointments: List<ScheduleAppointment>,
+    val defaultAppointment: ScheduleAppointment
+) : UiEvent
+
 data class AppointmentDateIncremented(val currentIndex: Int, val size: Int) : UiEvent {
   override val analyticsName = "Schedule Appointment:Increment appointment due date"
+}
+
+object AppointmentDateIncremented2 : UiEvent {
+  override val analyticsName = "Schedule Appointment:Increment appointment due date 2"
 }
 
 data class AppointmentDateDecremented(val currentIndex: Int, val size: Int) : UiEvent {
   override val analyticsName = "Schedule Appointment:Decrement appointment due date"
 }
 
+object AppointmentDateDecremented2 : UiEvent {
+  override val analyticsName = "Schedule Appointment:Decrement appointment due date 2"
+}
+
 data class AppointmentScheduled(val selectedDateState: ScheduleAppointment) : UiEvent {
   override val analyticsName = "Schedule Appointment:Appointment scheduled"
+}
+
+data class AppointmentCalendarDateSelected(
+    val year: Int,
+    val month: Int,
+    val dayOfMonth: Int
+) : UiEvent {
+  override val analyticsName = "Schedule Appointment:Appointment calendar date selected"
 }
 
 class SchedulingSkipped : UiEvent {

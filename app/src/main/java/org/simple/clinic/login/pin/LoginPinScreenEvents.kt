cd8f1598d3f6beb75cd13ec1076764a8e60e1dea
Vinay S Shenoy <vinay@obvious.in>
2019-07-29 06:06:18 +0000

    Save logged in user after request OTP call is successful

--- app/src/main/java/org/simple/clinic/login/pin/LoginPinScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/login/pin/LoginPinScreenEvents.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.login.pin
 
+import org.simple.clinic.user.OngoingLoginEntry
 import org.simple.clinic.widgets.UiEvent
 
 class PinScreenCreated : UiEvent
@@ -15,3 +16,7 @@ data class LoginPinAuthenticated(val pin: String) : UiEvent {
 // This event does not need an analytics name because it is
 // part of the old, deprecated login flow and will be removed
 data class LoginPinOtpReceived(val otp: String) : UiEvent
+
+data class LoginPinScreenUpdatedLoginEntry(val ongoingLoginEntry: OngoingLoginEntry) : UiEvent {
+  override val analyticsName: String = "Login:Pin Entry:Updated Login Entry"
+}

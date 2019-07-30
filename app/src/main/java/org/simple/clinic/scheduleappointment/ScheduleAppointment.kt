00fcd438a1ff5babaa5282e3313b0a716ae88270
Rakshak R.Hegde <rakshak@obvious.in>
2019-07-30 09:08:01 +0000

    Handle increment decrement clicks after calendar date is selected

--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointment.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointment.kt
@@ -2,4 +2,17 @@ package org.simple.clinic.scheduleappointment
 
 import org.threeten.bp.temporal.ChronoUnit
 
-data class ScheduleAppointment(val displayText: String, val timeAmount: Int, val chronoUnit: ChronoUnit)
+data class ScheduleAppointment(
+    val displayText: String,
+    val timeAmount: Int,
+    val chronoUnit: ChronoUnit
+) {
+
+  companion object {
+    val DEFAULT = ScheduleAppointment(
+        displayText = "1 month",
+        timeAmount = 1,
+        chronoUnit = ChronoUnit.MONTHS
+    )
+  }
+}

2caeba7e11c40ffaf36611912e3a89f0c8216208
Vinay S Shenoy <code@vinaysshenoy.com>
2019-08-01 06:47:24 +0000

    Change PatientSearchResultRegisterNewPatient to accept search criteria
    This commit also introduce a temporary constructor and a property to
    maintain compatibility with current code

--- app/src/main/java/org/simple/clinic/search/results/PatientSearchResultsScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/search/results/PatientSearchResultsScreenEvents.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.search.results
 
+import org.simple.clinic.patient.PatientSearchCriteria
 import org.simple.clinic.patient.PatientSearchResult
 import org.simple.clinic.widgets.UiEvent
 
@@ -9,6 +10,19 @@ data class PatientSearchResultClicked(val searchResult: PatientSearchResult) : U
   override val analyticsName = "Patient Search Results:Search Result Clicked"
 }
 
-data class PatientSearchResultRegisterNewPatient(val patientName: String) : UiEvent {
-  override val analyticsName = "Patient Search Results:Register New Patient"
+data class PatientSearchResultRegisterNewPatient(val searchCriteria: PatientSearchCriteria) : UiEvent {
+
+  constructor(patientName: String) : this(PatientSearchCriteria.Name(patientName))
+
+  val patientName: String
+    get() = (searchCriteria as PatientSearchCriteria.Name).patientName
+
+  override val analyticsName: String
+    get() {
+      val criteriaAnalyticsName = when (searchCriteria) {
+        is PatientSearchCriteria.Name -> "Name"
+        is PatientSearchCriteria.PhoneNumber -> "Phone Number"
+      }
+      return "Patient Search Results:Register New Patient:$criteriaAnalyticsName"
+    }
 }

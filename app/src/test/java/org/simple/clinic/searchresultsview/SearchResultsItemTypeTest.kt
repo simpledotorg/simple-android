5048fe4c0272d99d69b8044e3396b7edeee66129
Vinay S Shenoy <vinay@obvious.in>
2019-08-01 05:50:04 +0000

    Move ignored tests to search results item type test

---
+++ app/src/test/java/org/simple/clinic/searchresultsview/SearchResultsItemTypeTest.kt
@@ -0,0 +1,128 @@
+package org.simple.clinic.searchresultsview
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.simple.clinic.patient.PatientMocker
+import java.util.UUID
+
+class SearchResultsItemTypeTest {
+
+  private val currentFacility = PatientMocker.facility(UUID.fromString("69cf85c8-6788-4071-b985-0536ae606b70"))
+
+  @Test
+  fun `list items must be generated from the search results`() {
+    // given
+    val patientUuid1 = UUID.fromString("1d5f18d9-43f7-4e7f-92d3-a4f641709470")
+    val patientUuid2 = UUID.fromString("139bfac5-1adc-43fa-9406-d1000fb67a88")
+    val patientSearchResult1 = PatientMocker.patientSearchResult(patientUuid1)
+    val patientSearchResult2 = PatientMocker.patientSearchResult(patientUuid2)
+    val searchResults = PatientSearchResults(
+        visitedCurrentFacility = listOf(patientSearchResult1),
+        notVisitedCurrentFacility = listOf(patientSearchResult2)
+    )
+
+    // when
+    val listItems = SearchResultsItemType.generateListItems(
+        results = searchResults,
+        currentFacility = currentFacility
+    )
+
+    // then
+    val expected = listOf(
+        SearchResultsItemType.InCurrentFacilityHeader(facilityName = currentFacility.name),
+        SearchResultsItemType.SearchResultRow(
+            searchResult = patientSearchResult1,
+            currentFacility = currentFacility
+        ),
+        SearchResultsItemType.NotInCurrentFacilityHeader,
+        SearchResultsItemType.SearchResultRow(
+            searchResult = patientSearchResult2,
+            currentFacility = currentFacility
+        )
+    )
+    assertThat(listItems).isEqualTo(expected)
+  }
+
+  @Test
+  fun `when there are no search results in other facilities, the other results header must not be generated`() {
+    // given
+    val patientUuid1 = UUID.fromString("1d5f18d9-43f7-4e7f-92d3-a4f641709470")
+    val patientUuid2 = UUID.fromString("139bfac5-1adc-43fa-9406-d1000fb67a88")
+    val patientSearchResult1 = PatientMocker.patientSearchResult(patientUuid1)
+    val patientSearchResult2 = PatientMocker.patientSearchResult(patientUuid2)
+    val searchResults = PatientSearchResults(
+        visitedCurrentFacility = listOf(patientSearchResult1, patientSearchResult2),
+        notVisitedCurrentFacility = emptyList()
+    )
+
+    // when
+    val listItems = SearchResultsItemType.generateListItems(
+        results = searchResults,
+        currentFacility = currentFacility
+    )
+
+    // then
+    val expected = listOf(
+        SearchResultsItemType.InCurrentFacilityHeader(facilityName = currentFacility.name),
+        SearchResultsItemType.SearchResultRow(
+            searchResult = patientSearchResult1,
+            currentFacility = currentFacility
+        ),
+        SearchResultsItemType.SearchResultRow(
+            searchResult = patientSearchResult2,
+            currentFacility = currentFacility
+        )
+    )
+    assertThat(listItems).isEqualTo(expected)
+  }
+
+  @Test
+  fun `when there are no search results in the current facility, then the current facility header with no results must be shown`() {
+    // given
+    val patientUuid1 = UUID.fromString("1d5f18d9-43f7-4e7f-92d3-a4f641709470")
+    val patientUuid2 = UUID.fromString("139bfac5-1adc-43fa-9406-d1000fb67a88")
+    val patientSearchResult1 = PatientMocker.patientSearchResult(patientUuid1)
+    val patientSearchResult2 = PatientMocker.patientSearchResult(patientUuid2)
+    val searchResults = PatientSearchResults(
+        visitedCurrentFacility = emptyList(),
+        notVisitedCurrentFacility = listOf(patientSearchResult1, patientSearchResult2)
+    )
+
+    // when
+    val listItems = SearchResultsItemType.generateListItems(
+        results = searchResults,
+        currentFacility = currentFacility
+    )
+
+    // then
+    val expected = listOf(
+        SearchResultsItemType.InCurrentFacilityHeader(facilityName = currentFacility.name),
+        SearchResultsItemType.NoPatientsInCurrentFacility,
+        SearchResultsItemType.NotInCurrentFacilityHeader,
+        SearchResultsItemType.SearchResultRow(
+            searchResult = patientSearchResult1,
+            currentFacility = currentFacility
+        ),
+        SearchResultsItemType.SearchResultRow(
+            searchResult = patientSearchResult2,
+            currentFacility = currentFacility
+        )
+    )
+    assertThat(listItems).isEqualTo(expected)
+  }
+
+  @Test
+  fun `when there are no search results, then no list items must be generated`() {
+    // given
+    val searchResults = PatientSearchResults(
+        visitedCurrentFacility = emptyList(),
+        notVisitedCurrentFacility = emptyList()
+    )
+
+    // when
+    val listItems = SearchResultsItemType.generateListItems(searchResults, currentFacility)
+
+    // then
+    assertThat(listItems).isEmpty()
+  }
+}

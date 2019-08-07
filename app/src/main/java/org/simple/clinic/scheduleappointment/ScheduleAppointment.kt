fda22e79f4af51384f3c1812a9ecb5b343dd484b
Vinay S Shenoy <vinay@obvious.in>
2019-08-07 14:06:15 +0000

    Remove static display text from ScheduleAppointment

--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointment.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointment.kt
@@ -3,7 +3,6 @@ package org.simple.clinic.scheduleappointment
 import org.threeten.bp.temporal.ChronoUnit
 
 data class ScheduleAppointment(
-    val displayText: String,
     val timeAmount: Int,
     val chronoUnit: ChronoUnit
 )

7a7fca7d928b14021c0d34176c49c864946ea2c3
Vinay S Shenoy <vinay@obvious.in>
2019-08-07 10:47:32 +0530

    Extract generic adapter that can support multiple view types

--- app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityListAdapter.kt
+++ app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityListAdapter.kt
@@ -1,37 +1,16 @@
 package org.simple.clinic.allpatientsinfacility
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
-import io.reactivex.Observable
-import io.reactivex.subjects.PublishSubject
 import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityListItem.SectionTitle.None
 import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityListItem.SectionTitle.Text
-import org.simple.clinic.widgets.recyclerview.ViewHolderX
+import org.simple.clinic.widgets.ItemAdapter
 import java.util.Locale
 
 class AllPatientsInFacilityListAdapter(
     diffCallback: DiffUtil.ItemCallback<AllPatientsInFacilityListItem>,
     private val locale: Locale
-) : ListAdapter<AllPatientsInFacilityListItem, ViewHolderX>(diffCallback), FastScrollRecyclerView.SectionedAdapter {
-
-  private val eventSubject = PublishSubject.create<AllPatientsInFacilityListItem.Event>()
-
-  val listItemEvents: Observable<AllPatientsInFacilityListItem.Event> = eventSubject.hide()
-
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderX {
-    return ViewHolderX(LayoutInflater.from(parent.context).inflate(viewType, parent, false))
-  }
-
-  override fun onBindViewHolder(holder: ViewHolderX, position: Int) {
-    getItem(position).render(holder, eventSubject)
-  }
-
-  override fun getItemViewType(position: Int): Int {
-    return getItem(position).layoutResId
-  }
+) : ItemAdapter<AllPatientsInFacilityListItem, AllPatientsInFacilityListItem.Event>(diffCallback), FastScrollRecyclerView.SectionedAdapter {
 
   override fun getSectionName(position: Int): String {
     return when (val sectionTitle = getItem(position).sectionTitle(locale)) {

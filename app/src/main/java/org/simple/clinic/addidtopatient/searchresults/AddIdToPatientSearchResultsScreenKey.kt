ac2d4fc928ae0c6051e09342be61d79fdcaefcc4
Vinay S Shenoy <vinay@obvious.in>
2019-08-08 15:52:03 +0530

    Forward criteria to add ID to patient search results screen

--- app/src/main/java/org/simple/clinic/addidtopatient/searchresults/AddIdToPatientSearchResultsScreenKey.kt
+++ app/src/main/java/org/simple/clinic/addidtopatient/searchresults/AddIdToPatientSearchResultsScreenKey.kt
@@ -3,12 +3,13 @@ package org.simple.clinic.addidtopatient.searchresults
 import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.R
+import org.simple.clinic.patient.PatientSearchCriteria
 import org.simple.clinic.patient.businessid.Identifier
 import org.simple.clinic.router.screen.FullScreenKey
 
 @Parcelize
 data class AddIdToPatientSearchResultsScreenKey(
-    val fullName: String,
+    val criteria: PatientSearchCriteria,
     val identifier: Identifier
 ) : FullScreenKey {
 
@@ -16,4 +17,10 @@ data class AddIdToPatientSearchResultsScreenKey(
   override val analyticsName = "Add ID to Patient: Search Results"
 
   override fun layoutRes() = R.layout.screen_addidtopatientsearchresults
+
+  val fullName: String
+    get() = when (criteria) {
+      is PatientSearchCriteria.Name -> criteria.patientName
+      is PatientSearchCriteria.PhoneNumber -> ""
+    }
 }

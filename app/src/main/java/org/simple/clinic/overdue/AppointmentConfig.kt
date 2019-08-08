51565b93044cb5fa10850605e892c03e76b63163
Vinay S Shenoy <vinay@obvious.in>
2019-08-08 06:53:30 +0000

    Add ScheduleAppointmentConfig as a dependency of AppointmentConfig

--- app/src/main/java/org/simple/clinic/overdue/AppointmentConfig.kt
+++ app/src/main/java/org/simple/clinic/overdue/AppointmentConfig.kt
@@ -1,9 +1,12 @@
 package org.simple.clinic.overdue
 
+import io.reactivex.Observable
+import org.simple.clinic.scheduleappointment.ScheduleAppointmentConfig
 import org.threeten.bp.Period
 
 data class AppointmentConfig(
     val minimumOverduePeriodForHighRisk: Period,
     val overduePeriodForLowestRiskLevel: Period,
-    val appointmentDuePeriodForDefaulters: Period
+    val appointmentDuePeriodForDefaulters: Period,
+    val scheduleAppointmentConfigProvider: Observable<ScheduleAppointmentConfig>
 )

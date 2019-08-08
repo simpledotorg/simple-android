84a2257fef381e6f8be9a8038b4d7220875fd560
Vinay S Shenoy <vinay@obvious.in>
2019-08-08 16:16:47 +0530

    Add criteria as a parameter to add Id to patient results screen created

--- app/src/main/java/org/simple/clinic/addidtopatient/searchresults/AddIdPatientSearchResultsScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/addidtopatient/searchresults/AddIdPatientSearchResultsScreenEvents.kt
@@ -1,10 +1,21 @@
 package org.simple.clinic.addidtopatient.searchresults
 
+import org.simple.clinic.patient.PatientSearchCriteria
 import org.simple.clinic.patient.PatientSearchResult
 import org.simple.clinic.patient.businessid.Identifier
 import org.simple.clinic.widgets.UiEvent
 
-data class AddIdToPatientSearchResultsScreenCreated(val patientName: String, val identifier: Identifier) : UiEvent
+data class AddIdToPatientSearchResultsScreenCreated(
+    val criteria: PatientSearchCriteria,
+    val identifier: Identifier
+) : UiEvent {
+
+  val patientName: String
+    get() = when (criteria) {
+      is PatientSearchCriteria.Name -> criteria.patientName
+      is PatientSearchCriteria.PhoneNumber -> ""
+    }
+}
 
 data class AddIdToPatientSearchResultClicked(val searchResult: PatientSearchResult) : UiEvent {
   override val analyticsName = "Add ID to Patient Search Results:Search Result Clicked"

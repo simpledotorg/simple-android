1b501912eb689ddf1f34d8c142d63ba21a2a1696
Vinay S Shenoy <code@vinaysshenoy.com>
2019-08-08 16:32:41 +0530

    Prefill registration entry from add ID to patient search results

--- app/src/main/java/org/simple/clinic/addidtopatient/searchresults/AddIdToPatientSearchResultsController.kt
+++ app/src/main/java/org/simple/clinic/addidtopatient/searchresults/AddIdToPatientSearchResultsController.kt
@@ -8,8 +8,11 @@ import io.reactivex.rxkotlin.withLatestFrom
 import org.simple.clinic.ReplayUntilScreenIsDestroyed
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.patient.OngoingNewPatientEntry
-import org.simple.clinic.patient.OngoingNewPatientEntry.PersonalDetails
 import org.simple.clinic.patient.PatientRepository
+import org.simple.clinic.patient.PatientSearchCriteria
+import org.simple.clinic.patient.PatientSearchCriteria.Name
+import org.simple.clinic.patient.PatientSearchCriteria.PhoneNumber
+import org.simple.clinic.patient.businessid.Identifier
 import org.simple.clinic.widgets.UiEvent
 import javax.inject.Inject
 
@@ -42,20 +45,26 @@ class AddIdToPatientSearchResultsController @Inject constructor(
     val screenCreates = events.ofType<AddIdToPatientSearchResultsScreenCreated>()
 
     return events.ofType<AddIdToPatientSearchResultRegisterNewPatientClicked>()
-        .withLatestFrom(screenCreates) { _, screenCreated -> screenCreated.patientName to screenCreated.identifier }
-        .map { (patientName, identifier) ->
-          OngoingNewPatientEntry(
-              personalDetails = PersonalDetails(
-                  fullName = patientName,
-                  dateOfBirth = null,
-                  age = null,
-                  gender = null),
-              identifier = identifier)
-        }
-        .flatMap {
-          patientRepository
-              .saveOngoingEntry(it)
-              .andThen(Observable.just({ ui: Ui -> ui.openPatientEntryScreen() }))
-        }
+        .withLatestFrom(screenCreates) { _, screenCreated -> screenCreated.criteria to screenCreated.identifier }
+        .map { (criteria, identifier) -> entryFromSearchCriteria(criteria, identifier) }
+        .flatMap(this::saveEntryAndGoToEntryScreen)
+  }
+
+  private fun entryFromSearchCriteria(
+      criteria: PatientSearchCriteria,
+      identifier: Identifier
+  ): OngoingNewPatientEntry {
+    val ongoingNewPatientEntry = when (criteria) {
+      is Name -> OngoingNewPatientEntry.fromFullName(criteria.patientName)
+      is PhoneNumber -> OngoingNewPatientEntry.fromPhoneNumber(criteria.phoneNumber)
+    }
+
+    return ongoingNewPatientEntry.withIdentifier(identifier)
+  }
+
+  private fun saveEntryAndGoToEntryScreen(entry: OngoingNewPatientEntry): Observable<UiChange> {
+    return patientRepository
+        .saveOngoingEntry(entry)
+        .andThen(Observable.just { ui: Ui -> ui.openPatientEntryScreen() })
   }
 }

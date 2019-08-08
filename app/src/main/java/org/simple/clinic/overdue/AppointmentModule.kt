9d40c970eade309348a9e13fedf455de60f3c91a
Vinay S Shenoy <vinay@obvious.in>
2019-08-08 08:23:59 +0000

    Move properties of ScheduleAppointmentConfig to AppointmentConfig

--- app/src/main/java/org/simple/clinic/overdue/AppointmentModule.kt
+++ app/src/main/java/org/simple/clinic/overdue/AppointmentModule.kt
@@ -7,8 +7,7 @@ import dagger.Provides
 import io.reactivex.Observable
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.home.overdue.OverdueAppointment
-import org.simple.clinic.scheduleappointment.ScheduleAppointmentConfig
-import org.simple.clinic.scheduleappointment.ScheduleAppointmentModule
+import org.simple.clinic.scheduleappointment.ScheduleAppointmentIn
 import org.simple.clinic.util.None
 import org.simple.clinic.util.Optional
 import org.simple.clinic.util.OptionalRxPreferencesConverter
@@ -17,16 +16,42 @@ import org.threeten.bp.Period
 import retrofit2.Retrofit
 import javax.inject.Named
 
-@Module(includes = [ScheduleAppointmentModule::class])
+@Module
 class AppointmentModule {
 
   @Provides
-  fun config(scheduleAppointmentConfigProvider: Observable<ScheduleAppointmentConfig>): Observable<AppointmentConfig> {
+  fun config(): Observable<AppointmentConfig> {
     return Observable.just(AppointmentConfig(
         minimumOverduePeriodForHighRisk = Period.ofDays(30),
         overduePeriodForLowestRiskLevel = Period.ofDays(365),
         appointmentDuePeriodForDefaulters = Period.ofDays(30),
-        scheduleAppointmentConfigProvider = scheduleAppointmentConfigProvider
+        periodsToScheduleAppointmentsIn = listOf(
+            ScheduleAppointmentIn.days(1),
+            ScheduleAppointmentIn.days(2),
+            ScheduleAppointmentIn.days(3),
+            ScheduleAppointmentIn.days(4),
+            ScheduleAppointmentIn.days(5),
+            ScheduleAppointmentIn.days(6),
+            ScheduleAppointmentIn.days(7),
+            ScheduleAppointmentIn.days(8),
+            ScheduleAppointmentIn.days(9),
+            ScheduleAppointmentIn.days(10),
+            ScheduleAppointmentIn.weeks(2),
+            ScheduleAppointmentIn.weeks(3),
+            ScheduleAppointmentIn.months(1),
+            ScheduleAppointmentIn.months(2),
+            ScheduleAppointmentIn.months(3),
+            ScheduleAppointmentIn.months(4),
+            ScheduleAppointmentIn.months(5),
+            ScheduleAppointmentIn.months(6),
+            ScheduleAppointmentIn.months(7),
+            ScheduleAppointmentIn.months(8),
+            ScheduleAppointmentIn.months(9),
+            ScheduleAppointmentIn.months(10),
+            ScheduleAppointmentIn.months(11),
+            ScheduleAppointmentIn.months(12)
+        ),
+        scheduleAppointmentInByDefault = ScheduleAppointmentIn.months(1)
     ))
   }
 

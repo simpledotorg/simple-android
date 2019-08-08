193c8d3890b239cda4b78091f1e8a9b3838c354f
Vinay S Shenoy <vinay@obvious.in>
2019-08-08 08:23:59 +0000

    Move properties of ScheduleAppointmentConfig to AppointmentConfig

--- app/src/main/java/org/simple/clinic/overdue/AppointmentConfig.kt
+++ app/src/main/java/org/simple/clinic/overdue/AppointmentConfig.kt
@@ -1,7 +1,5 @@
 package org.simple.clinic.overdue
 
-import io.reactivex.Observable
-import org.simple.clinic.scheduleappointment.ScheduleAppointmentConfig
 import org.simple.clinic.scheduleappointment.ScheduleAppointmentIn
 import org.threeten.bp.Period
 
@@ -9,11 +7,6 @@ data class AppointmentConfig(
     val minimumOverduePeriodForHighRisk: Period,
     val overduePeriodForLowestRiskLevel: Period,
     val appointmentDuePeriodForDefaulters: Period,
-    val scheduleAppointmentConfigProvider: Observable<ScheduleAppointmentConfig>
-) {
-  val periodsToScheduleAppointmentsIn: List<ScheduleAppointmentIn>
-    get() = scheduleAppointmentConfigProvider.blockingFirst().periodsToScheduleAppointmentsIn
-
-  val scheduleAppointmentInByDefault: ScheduleAppointmentIn
-    get() = scheduleAppointmentConfigProvider.blockingFirst().scheduleAppointmentInByDefault
-}
+    val periodsToScheduleAppointmentsIn: List<ScheduleAppointmentIn>,
+    val scheduleAppointmentInByDefault: ScheduleAppointmentIn
+)
--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentConfig.kt
+++
@@ -1,7 +0,0 @@
-package org.simple.clinic.scheduleappointment
-
-// TODO: Merge with appointment config
-data class ScheduleAppointmentConfig(
-    val periodsToScheduleAppointmentsIn: List<ScheduleAppointmentIn>,
-    val scheduleAppointmentInByDefault: ScheduleAppointmentIn
-)

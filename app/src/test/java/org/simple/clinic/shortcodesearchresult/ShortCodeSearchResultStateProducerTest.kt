6f00e7c90e305b196a4e9077e138725a990b8a4f
Sanchita Agarwal <sanchita@obvious.in>
2019-08-21 07:45:51 +0000

    Open patient summary when search result is clicked

--- app/src/test/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultStateProducerTest.kt
+++ app/src/test/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultStateProducerTest.kt
@@ -1,6 +1,8 @@
 package org.simple.clinic.shortcodesearchresult
 
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
@@ -10,13 +12,15 @@ import org.simple.clinic.patient.PatientRepository
 import org.simple.clinic.patient.PatientSearchResult
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
+import java.util.UUID
 
 class ShortCodeSearchResultStateProducerTest {
   private val uiEventsSubject = PublishSubject.create<UiEvent>()
   private val patientRepository = mock<PatientRepository>()
   private val shortCode = "1234567"
   private val fetchingPatientsState = ShortCodeSearchResultState.fetchingPatients(shortCode)
-  private val uiStateProducer = ShortCodeSearchResultStateProducer(fetchingPatientsState, patientRepository)
+  private val ui = mock<ShortCodeSearchResultUi>()
+  private val uiStateProducer = ShortCodeSearchResultStateProducer(fetchingPatientsState, patientRepository, ui)
   private val uiStates = uiEventsSubject
       .compose(uiStateProducer)
       .doOnNext { uiStateProducer.states.onNext(it) }
@@ -59,5 +63,31 @@ class ShortCodeSearchResultStateProducerTest {
       assertValues(fetchingPatientsState, fetchingPatientsState.noMatchingPatients())
       assertNotTerminated()
     }
+  }
+
+  @Test
+  fun `when the user clicks on a patient search result, then open the patient summary screen`() {
+    // given
+    val patientUuid = UUID.fromString("d18fa4dc-3b47-4a88-826f-342401527d65")
+    val patientSearchResults = listOf(PatientMocker.patientSearchResult(uuid = patientUuid))
+    val patientsFetched = ShortCodeSearchResultState
+        .fetchingPatients("1234567")
+        .patientsFetched(patientSearchResults)
+    uiStateProducer.states.onNext(patientsFetched) // TODO Fix `setState` in tests
+
+    val testObserver = uiStates.test()
+
+    // when
+    uiEventsSubject.onNext(ViewPatient(patientUuid))
+
+    // then
+    with(testObserver) {
+      assertNoErrors()
+      assertNoValues()
+      assertNotTerminated()
+    }
+
+    verify(ui).openPatientSummary(patientUuid)
+    verifyNoMoreInteractions(ui)
   }
 }

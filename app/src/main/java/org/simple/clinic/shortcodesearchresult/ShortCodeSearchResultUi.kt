ac31865b215e764fb9652e469c89187225fb760f
Sanchita Agarwal <sanchita@obvious.in>
2019-08-21 09:28:24 +0000

    Rendering logic for showing patients

--- app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultUi.kt
+++ app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultUi.kt
@@ -1,9 +1,13 @@
 package org.simple.clinic.shortcodesearchresult
 
+import org.simple.clinic.patient.PatientSearchResult
 import java.util.UUID
 
 interface ShortCodeSearchResultUi {
   fun openPatientSummary(patientUuid: UUID)
   fun openPatientSearch()
   fun showLoading()
+  fun hideLoading()
+  fun showSearchResults(foundPatients: List<PatientSearchResult>)
+  fun showSearchPatientButton()
 }
--- app/src/test/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultUiChangeProducerTest.kt
+++ app/src/test/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultUiChangeProducerTest.kt
@@ -5,25 +5,53 @@ import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
+import org.junit.Before
 import org.junit.Test
+import org.simple.clinic.patient.PatientMocker
+import java.util.UUID
 
 class ShortCodeSearchResultUiChangeProducerTest {
+  private val ui = mock<ShortCodeSearchResultUi>()
+  private val states = PublishSubject.create<ShortCodeSearchResultState>()
+
+  @Before
+  fun setup() {
+    states
+        .compose(ShortCodeSearchResultUiChangeProducer(Schedulers.trampoline()))
+        .subscribe { uiChange -> uiChange(ui) }
+  }
+
   @Test
   fun `it shows the loading screen when fetching patients`() {
     // given
     val fetchingPatients = ShortCodeSearchResultState.fetchingPatients("1234567")
-    val ui = mock<ShortCodeSearchResultUi>()
-
-    val states = PublishSubject.create<ShortCodeSearchResultState>()
-    states
-        .compose(ShortCodeSearchResultUiChangeProducer(Schedulers.trampoline()))
-        .subscribe { uiChange -> uiChange(ui) }
 
     // when
     states.onNext(fetchingPatients)
 
     // then
     verify(ui).showLoading()
+    verifyNoMoreInteractions(ui)
+  }
+
+  @Test
+  fun `it shows a list of patients if found`() {
+    // given
+    val foundPatients = listOf(
+        PatientMocker.patientSearchResult(uuid = UUID.fromString("be021029-4935-4400-8e5b-e57176a45517"))
+    )
+
+    val patientsFetched = ShortCodeSearchResultState
+        .fetchingPatients("1234567")
+        .patientsFetched(foundPatients)
+
+    // when
+    states.onNext(patientsFetched)
+
+    // then
+    verify(ui).hideLoading()
+    verify(ui).showSearchResults(foundPatients)
+    verify(ui).showSearchPatientButton()
     verifyNoMoreInteractions(ui)
   }
 }

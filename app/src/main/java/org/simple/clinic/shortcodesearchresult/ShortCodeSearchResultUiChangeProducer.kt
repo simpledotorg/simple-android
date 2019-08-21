67af17e228e7ff2a26bc4217800ec217ac642f26
Sanchita Agarwal <sanchita@obvious.in>
2019-08-21 09:28:24 +0000

    Rendering logic for showing patients

--- app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultUiChangeProducer.kt
+++ app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultUiChangeProducer.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.shortcodesearchresult
 
+import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 import io.reactivex.Scheduler
 import org.simple.clinic.plumbing.AsyncOp
@@ -12,9 +13,22 @@ class ShortCodeSearchResultUiChangeProducer(
 ) : BaseUiChangeProducer<ShortCodeSearchResultState, ShortCodeSearchResultUi>(uiScheduler) {
   override fun uiChanges(): ObservableTransformer<ShortCodeSearchResultState, ShortCodeSearchResultUiChange> {
     return ObservableTransformer<ShortCodeSearchResultState, ShortCodeSearchResultUiChange> { states ->
-      states
+      val showLoading = states
           .filter { it.fetchPatientsAsyncOp == AsyncOp.IN_FLIGHT }
           .map { { ui: ShortCodeSearchResultUi -> ui.showLoading() } }
+
+      val showPatients = states
+          .filter { it.fetchPatientsAsyncOp == AsyncOp.SUCCEEDED && it.patients.isNotEmpty() }
+          .map { state ->
+            { ui: ShortCodeSearchResultUi ->
+              ui.hideLoading()
+              ui.showSearchResults(state.patients)
+              ui.showSearchPatientButton()
+            }
+          }
+
+      Observable
+          .merge(showLoading, showPatients)
     }
   }
 }

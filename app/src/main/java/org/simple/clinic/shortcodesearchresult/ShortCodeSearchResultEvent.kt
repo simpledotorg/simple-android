53191802ec222e2c630cc3cb09639ec15eea7090
Sanchita Agarwal <sanchita@obvious.in>
2019-08-21 07:45:51 +0000

    Open patient summary when search result is clicked

--- app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultEvent.kt
+++ app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultEvent.kt
@@ -1,5 +1,8 @@
 package org.simple.clinic.shortcodesearchresult
 
 import org.simple.clinic.widgets.UiEvent
+import java.util.UUID
 
 sealed class ShortCodeSearchResultEvent : UiEvent
+
+data class ViewPatient(val patientUuid: UUID) : ShortCodeSearchResultEvent()

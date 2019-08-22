1487d1540bde714714b832a281f09e6e87f5f8a3
Ragunath Jawahar <ragunath@obvious.in>
2019-08-22 13:20:52 +0530

    Extract hard-coded required short code search to Globals

--- app/src/main/java/org/simple/clinic/scanid/ShortCodeInput.kt
+++ app/src/main/java/org/simple/clinic/scanid/ShortCodeInput.kt
@@ -1,7 +1,9 @@
 package org.simple.clinic.scanid
 
+import org.simple.clinic.SHORT_CODE_REQUIRED_LENGTH
+
 data class ShortCodeInput(val shortCodeText: String) {
   fun isValid(): Boolean {
-    return shortCodeText.length == 7
+    return shortCodeText.length == SHORT_CODE_REQUIRED_LENGTH
   }
 }

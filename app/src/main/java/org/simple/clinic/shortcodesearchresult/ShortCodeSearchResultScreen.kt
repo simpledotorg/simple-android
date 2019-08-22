65064afa9a6f47754b404161339832702c60bd7d
Sanchita Agarwal <sanchita@obvious.in>
2019-08-22 08:52:15 +0000

    Add ShortCodeSearchResultScreen

---
+++ app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultScreen.kt
@@ -0,0 +1,9 @@
+package org.simple.clinic.shortcodesearchresult
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class ShortCodeSearchResultScreen(context: Context, attributes: AttributeSet) : RelativeLayout(context, attributes) {
+
+}

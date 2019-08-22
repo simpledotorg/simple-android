38d3c75537e0ca486f676a0642ba7ab762a41695
Ragunath Jawahar <ragunath@obvious.in>
2019-08-22 15:12:49 +0530

    Group failures under a new sealed class for short code validation errors

--- app/src/main/java/org/simple/clinic/scanid/ShortCodeInput.kt
+++ app/src/main/java/org/simple/clinic/scanid/ShortCodeInput.kt
@@ -1,14 +1,16 @@
 package org.simple.clinic.scanid
 
 import org.simple.clinic.SHORT_CODE_REQUIRED_LENGTH
-import org.simple.clinic.scanid.ShortCodeValidationResult.Empty
+import org.simple.clinic.scanid.ShortCodeValidationResult.Failure.Empty
+import org.simple.clinic.scanid.ShortCodeValidationResult.Failure.NotEqualToRequiredLength
+import org.simple.clinic.scanid.ShortCodeValidationResult.Success
 
 data class ShortCodeInput(val shortCodeText: String) {
   fun validate(): ShortCodeValidationResult {
     return when {
       shortCodeText.isEmpty() -> Empty
-      shortCodeText.length !=  SHORT_CODE_REQUIRED_LENGTH -> ShortCodeValidationResult.NotEqualToRequiredLength
-      shortCodeText.length == SHORT_CODE_REQUIRED_LENGTH -> ShortCodeValidationResult.Success
+      shortCodeText.length !=  SHORT_CODE_REQUIRED_LENGTH -> NotEqualToRequiredLength
+      shortCodeText.length == SHORT_CODE_REQUIRED_LENGTH -> Success
       else -> throw UnsupportedOperationException("Unknown situation for short code input: $shortCodeText")
     }
   }
@@ -17,6 +19,8 @@ data class ShortCodeInput(val shortCodeText: String) {
 sealed class ShortCodeValidationResult {
   object Success : ShortCodeValidationResult()
 
-  object Empty : ShortCodeValidationResult()
-  object NotEqualToRequiredLength : ShortCodeValidationResult()
+  sealed class Failure : ShortCodeValidationResult() {
+    object Empty : Failure()
+    object NotEqualToRequiredLength : Failure()
+  }
 }

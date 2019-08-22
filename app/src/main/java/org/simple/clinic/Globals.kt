18b9700e0acbe2830e837a51949a6a77a255befe
Ragunath Jawahar <ragunath@obvious.in>
2019-08-22 13:20:52 +0530

    Extract hard-coded required short code search to Globals

--- app/src/main/java/org/simple/clinic/Globals.kt
+++ app/src/main/java/org/simple/clinic/Globals.kt
@@ -3,3 +3,4 @@ package org.simple.clinic
 import org.threeten.bp.Duration
 
 val SCREEN_CHANGE_ANIMATION_DURATION: Duration = Duration.ofMillis(200L)
+val SHORT_CODE_REQUIRED_LENGTH: Int = 7
--- app/src/main/java/org/simple/clinic/patient/shortcode/UuidShortCodeCreatorModule.kt
+++ app/src/main/java/org/simple/clinic/patient/shortcode/UuidShortCodeCreatorModule.kt
@@ -2,13 +2,14 @@ package org.simple.clinic.patient.shortcode
 
 import dagger.Module
 import dagger.Provides
+import org.simple.clinic.SHORT_CODE_REQUIRED_LENGTH
 
 @Module
 class UuidShortCodeCreatorModule {
 
   @Provides
   fun provideUuidShortCodeCreator(): UuidShortCodeCreator {
-    val requiredShortCodeLength = 7
+    val requiredShortCodeLength = SHORT_CODE_REQUIRED_LENGTH
 
     return UuidShortCodeCreator(
         requiredShortCodeLength = requiredShortCodeLength,

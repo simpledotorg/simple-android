79030556638191557c8b32fe37dd8b33925fc6e0
Sanchita Agarwal <sanchita@obvious.in>
2019-08-22 10:06:52 +0000

    Add ViewControllerBinding to ShortCodeSearchResultScreen

--- app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultScreen.kt
+++ app/src/main/java/org/simple/clinic/shortcodesearchresult/ShortCodeSearchResultScreen.kt
@@ -5,20 +5,38 @@ import android.util.AttributeSet
 import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.screen_shortcode_search_result.view.*
 import org.simple.clinic.R
+import org.simple.clinic.ViewControllerBinding
+import org.simple.clinic.patient.PatientSearchResult
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.text.style.TextAppearanceWithLetterSpacingSpan
 import org.simple.clinic.util.Truss
 import org.simple.clinic.util.Unicode
+import org.simple.clinic.util.UtcClock
+import org.simple.clinic.widgets.UiEvent
+import java.util.UUID
 import javax.inject.Inject
 
-class ShortCodeSearchResultScreen(context: Context, attributes: AttributeSet) : RelativeLayout(context, attributes) {
+class ShortCodeSearchResultScreen(context: Context, attributes: AttributeSet) : RelativeLayout(context, attributes), ShortCodeSearchResultUi {
+
+  @Inject
+  lateinit var uiStateProducer: ShortCodeSearchResultStateProducer
+
+  @Inject
+  lateinit var uiChangeProducer: ShortCodeSearchResultUiChangeProducer
+
+  @Inject
+  lateinit var utcClock: UtcClock
 
   @Inject
   lateinit var screenRouter: ScreenRouter
 
+  private lateinit var binding: ViewControllerBinding<UiEvent, ShortCodeSearchResultState, ShortCodeSearchResultUi>
+
   override fun onFinishInflate() {
     super.onFinishInflate()
     setupToolBar()
+
+    binding = ViewControllerBinding.bindToView(this, uiStateProducer, uiChangeProducer)
   }
 
   private fun setupToolBar() {
@@ -38,5 +56,33 @@ class ShortCodeSearchResultScreen(context: Context, attributes: AttributeSet) : 
         .append(formattedShortCode)
         .popSpan()
         .build()
+  }
+
+  override fun openPatientSummary(patientUuid: UUID) {
+    TODO("not implemented")
+  }
+
+  override fun openPatientSearch() {
+    TODO("not implemented")
+  }
+
+  override fun showLoading() {
+    TODO("not implemented")
+  }
+
+  override fun hideLoading() {
+    TODO("not implemented")
+  }
+
+  override fun showSearchResults(foundPatients: List<PatientSearchResult>) {
+    TODO("not implemented")
+  }
+
+  override fun showSearchPatientButton() {
+    TODO("not implemented")
+  }
+
+  override fun showNoPatientsMatched() {
+    TODO("not implemented")
   }
 }

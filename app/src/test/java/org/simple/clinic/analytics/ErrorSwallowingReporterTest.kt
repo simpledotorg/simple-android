b48a55b98d38bc69f61aa090961b0bb9ca1e5362
Vinay S Shenoy <code@vinaysshenoy.com>
2019-08-26 10:29:40 +0530

    Add a decorator analytics reporter for swallowing errors

---
+++ app/src/test/java/org/simple/clinic/analytics/ErrorSwallowingReporterTest.kt
@@ -0,0 +1,55 @@
+package org.simple.clinic.analytics
+
+import org.junit.Test
+import org.simple.clinic.patient.PatientMocker
+import org.simple.clinic.user.User
+
+class ErrorSwallowingReporterTest {
+
+  private val reporter = ErrorSwallowingReporter(FailingAnalyticsReporter())
+
+  @Test(expected = Test.None::class)
+  fun `no error should be thrown when reporting a new user registration`() {
+    // given
+    val user = PatientMocker.loggedInUser()
+
+    // when
+    reporter.setLoggedInUser(user, isANewRegistration = true)
+  }
+
+  @Test(expected = Test.None::class)
+  fun `no error should be thrown when reporting a user login`() {
+    // given
+    val user = PatientMocker.loggedInUser()
+
+    // when
+    reporter.setLoggedInUser(user, isANewRegistration = false)
+  }
+
+  @Test(expected = Test.None::class)
+  fun `no error should be thrown when clearing the user`() {
+    // when
+    reporter.resetUser()
+  }
+
+  @Test(expected = Test.None::class)
+  fun `no error should be thrown when creating an event`() {
+    // when
+    reporter.createEvent("Event", emptyMap())
+  }
+
+  private class FailingAnalyticsReporter : AnalyticsReporter {
+
+    override fun setLoggedInUser(user: User, isANewRegistration: Boolean) {
+      throw RuntimeException()
+    }
+
+    override fun resetUser() {
+      throw RuntimeException()
+    }
+
+    override fun createEvent(event: String, props: Map<String, Any>) {
+      throw RuntimeException()
+    }
+  }
+}

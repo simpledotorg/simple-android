c3e97f5866dce39023f84974d504a754ddde91c1
Vinay S Shenoy <vinay@obvious.in>
2019-08-26 10:29:40 +0530

    Add a decorator analytics reporter for swallowing errors

--- app/src/main/java/org/simple/clinic/analytics/AnalyticsReporter.kt
+++ app/src/main/java/org/simple/clinic/analytics/AnalyticsReporter.kt
@@ -10,16 +10,4 @@ interface AnalyticsReporter {
   fun resetUser()
 
   fun createEvent(event: String, props: Map<String, Any>)
-
-  /**
-   * Safely report events so that the app does not crash if any of the
-   * reporters fail.
-   **/
-  fun safely(message: String = "", block: AnalyticsReporter.() -> Unit) {
-    try {
-      block()
-    } catch (e: Exception) {
-      Timber.e(e, if (message.isBlank()) "Could not report event!" else message)
-    }
-  }
 }

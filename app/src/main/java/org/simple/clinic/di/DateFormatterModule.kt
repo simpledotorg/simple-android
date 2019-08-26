0cd868de934ff611679b6417ad7f7a580a1ce392
Vinay S Shenoy <vinay@obvious.in>
2019-08-26 13:09:28 +0000

    Stop making date formatters singleton objects
    Since the locale can change dynamically, we should instantiate the
    formatters when needed.

--- app/src/main/java/org/simple/clinic/di/DateFormatterModule.kt
+++ app/src/main/java/org/simple/clinic/di/DateFormatterModule.kt
@@ -10,22 +10,18 @@ import javax.inject.Named
 class DateFormatterModule {
 
   @Provides
-  @AppScope
   @Named("date_for_search_results")
   fun provideDateFormatterForSearchResults(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("d-MMM-yyyy", locale)
 
   @Provides
-  @AppScope
   @Named("date_for_user_input")
   fun provideDateFormatterForUserInput(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", locale)
 
   @Provides
-  @AppScope
   @Named("time_for_bps_recorded")
   fun providesTimeFormatterForBPRecorded(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("h:mm a", locale)
 
   @Provides
-  @AppScope
   @Named("recent_patients_header")
   fun providesTimeFormatterForRecentPatientsHeader(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM, yyyy", locale)
 }

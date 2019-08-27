ea13337d0caf1df6b7bdb7f05b7c5cb0605c3bdc
Ragunath Jawahar <ragunath@obvious.in>
2019-08-27 11:12:08 +0000

    Format short code in the same format as its hint

---
+++ app/src/main/java/org/simple/clinic/scanid/ui/ShortCodeSpanWatcher.kt
@@ -0,0 +1,23 @@
+package org.simple.clinic.scanid.ui
+
+import android.text.Editable
+import android.text.Spannable
+import org.simple.clinic.text.style.TrailingSpaceForCharacterSpan
+import org.simple.clinic.util.TextWatcherAdapter
+
+private const val CHARS_REQUIRED_TO_INSERT_SPACE = 4
+private const val INSERT_SPACE_AT_INDEX = 2
+
+class ShortCodeSpanWatcher : TextWatcherAdapter() {
+    private val trailingSpaceSpan = TrailingSpaceForCharacterSpan()
+
+    override fun afterTextChanged(editable: Editable) {
+        with(editable) {
+            removeSpan(trailingSpaceSpan)
+
+            if (length >= CHARS_REQUIRED_TO_INSERT_SPACE) {
+                setSpan(trailingSpaceSpan, INSERT_SPACE_AT_INDEX, INSERT_SPACE_AT_INDEX + 1, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+            }
+        }
+    }
+}

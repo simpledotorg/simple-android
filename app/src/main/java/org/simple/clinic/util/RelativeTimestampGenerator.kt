25df0eb506eaea5f88b0974b18fa1040c5dbaa58
Vinay S Shenoy <vinay@obvious.in>
2019-08-27 13:15:33 +0000

    Remove unused methods from RelativeTimestampGenerator

--- app/src/main/java/org/simple/clinic/util/RelativeTimestampGenerator.kt
+++ app/src/main/java/org/simple/clinic/util/RelativeTimestampGenerator.kt
@@ -1,7 +1,6 @@
 package org.simple.clinic.util
 
 import android.content.Context
-import androidx.annotation.VisibleForTesting
 import org.simple.clinic.R
 import org.simple.clinic.util.RelativeTimestamp.ExactDate
 import org.simple.clinic.util.RelativeTimestamp.Today
@@ -9,42 +8,12 @@ import org.simple.clinic.util.RelativeTimestamp.WithinSixMonths
 import org.simple.clinic.util.RelativeTimestamp.Yesterday
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
-import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneOffset
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.temporal.ChronoUnit.DAYS
 import javax.inject.Inject
 
 class RelativeTimestampGenerator @Inject constructor() {
-
-  @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-  fun generate(today: LocalDateTime, time: Instant): RelativeTimestamp {
-    val dateTime = time.atZone(ZoneOffset.UTC).toLocalDateTime()
-
-    val todayAtMidnight = today.truncatedTo(DAYS)
-    val yesterdayAtMidnight = todayAtMidnight.minusDays(1)
-    val tomorrowAtMidnight = todayAtMidnight.plusDays(1)
-
-    return when {
-      dateTime > tomorrowAtMidnight -> ExactDate(time)
-      dateTime > todayAtMidnight -> Today
-      dateTime > yesterdayAtMidnight -> Yesterday
-      dateTime > today.minusMonths(6) -> WithinSixMonths(daysBetween(dateTime, today).toInt())
-      else -> ExactDate(time)
-    }
-  }
-
-  private fun daysBetween(dateTime: LocalDateTime, today: LocalDateTime) =
-      DAYS.between(dateTime.toLocalDate(), today.toLocalDate())
-
-  @Deprecated(
-      message = "",
-      replaceWith = ReplaceWith("generate(time, userClock)")
-  )
-  fun generate(time: Instant): RelativeTimestamp {
-    val today = LocalDateTime.now(ZoneOffset.UTC)
-    return generate(today, time)
-  }
 
   fun generate(instant: Instant, userClock: UserClock): RelativeTimestamp {
     val then = instant.toLocalDateAtZone(userClock.zone)

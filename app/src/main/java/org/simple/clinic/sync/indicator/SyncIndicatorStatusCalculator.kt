17dd5764f93cb56c7d2604c6eee126fcf219c9ba
Vinay S Shenoy <vinay@obvious.in>
2019-08-28 12:46:15 +0000

    Clean up SyncIndicatorStatusCalculator
    - Improve the chains
    - Hide copy functions of LastSyncedState

--- app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorStatusCalculator.kt
+++ app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorStatusCalculator.kt
@@ -2,23 +2,24 @@ package org.simple.clinic.sync.indicator
 
 import android.annotation.SuppressLint
 import com.f2prateek.rx.preferences2.Preference
-import io.reactivex.schedulers.Schedulers.io
 import org.simple.clinic.di.AppScope
 import org.simple.clinic.sync.DataSync
 import org.simple.clinic.sync.LastSyncedState
-import org.simple.clinic.sync.SyncGroup
+import org.simple.clinic.sync.SyncGroup.FREQUENT
+import org.simple.clinic.sync.SyncProgress
 import org.simple.clinic.sync.SyncProgress.FAILURE
 import org.simple.clinic.sync.SyncProgress.SUCCESS
 import org.simple.clinic.sync.SyncProgress.SYNCING
 import org.simple.clinic.util.UtcClock
-import org.threeten.bp.Instant
+import org.simple.clinic.util.scheduler.SchedulersProvider
 import javax.inject.Inject
 
 @AppScope
 class SyncIndicatorStatusCalculator @Inject constructor(
     private val dataSync: DataSync,
     private val utcClock: UtcClock,
-    private val lastSyncProgress: Preference<LastSyncedState>
+    private val lastSyncedStatePreference: Preference<LastSyncedState>,
+    private val schedulersProvider: SchedulersProvider
 ) {
 
   @SuppressLint("CheckResult")
@@ -28,15 +29,19 @@ class SyncIndicatorStatusCalculator @Inject constructor(
         .distinctUntilChanged()
 
     syncResultsStream
-        .subscribeOn(io())
-        .filter { (syncGroup, _) -> syncGroup == SyncGroup.FREQUENT }
-        .map { (_, progress) ->
-          val updatedState = lastSyncProgress.get().copy(lastSyncProgress = progress)
-          when (progress) {
-            SUCCESS -> updatedState.copy(lastSyncSucceededAt = Instant.now(utcClock))
-            FAILURE, SYNCING -> updatedState
-          }
-        }
-        .subscribe { lastSyncProgress.set(it) }
+        .subscribeOn(schedulersProvider.io())
+        .filter { it.syncGroup == FREQUENT }
+        .map { it.syncProgress }
+        .map(this::updateLastSyncedStateWithProgress)
+        .subscribe(lastSyncedStatePreference::set)
+  }
+
+  private fun updateLastSyncedStateWithProgress(progress: SyncProgress): LastSyncedState {
+    return with(lastSyncedStatePreference.get()) {
+      when (progress) {
+        SUCCESS -> success(utcClock)
+        FAILURE, SYNCING -> withProgress(progress)
+      }
+    }
   }
 }

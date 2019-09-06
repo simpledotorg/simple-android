3e3c9833dcdd7a7709128c3e72d9c25b83d889e8
Rakshak R.Hegde <rakshak@obvious.in>
2019-09-06 20:00:39 +0530

    Add DayOfMonthTest

---
+++ app/src/test/java/org/simple/clinic/home/patients/illustration/DayOfMonthMoshiTypeAdapterTest.kt
@@ -0,0 +1,22 @@
+package org.simple.clinic.home.patients.illustration
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.threeten.bp.Month
+
+class DayOfMonthMoshiTypeAdapterTest {
+
+  private val typeAdapter = DayOfMonth.MoshiTypeAdapter
+
+  @Test
+  fun `DayOfMonth must get serialized correctly`() {
+    val serialized = typeAdapter.fromModel(DayOfMonth(6, Month.JANUARY))
+    assertThat(serialized).isEqualTo("6 JANUARY")
+  }
+
+  @Test
+  fun `DayOfMonth must get deserialized correctly`() {
+    val deserialized = typeAdapter.toModel("6 January")
+    assertThat(deserialized).isEqualTo(DayOfMonth(6, Month.JANUARY))
+  }
+}

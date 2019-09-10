3cbf56232ae69650e2337a4cc5d294dbfde7f2aa
Vinay S Shenoy <vinay@obvious.in>
2019-09-10 12:35:32 +0530

    Add query to load patient name and phone number in experiment

--- app/src/main/java/experiments/instantsearch/InstantPatientSearchExperimentsDao.kt
+++ app/src/main/java/experiments/instantsearch/InstantPatientSearchExperimentsDao.kt
@@ -1,13 +1,16 @@
 package experiments.instantsearch
 
+import android.database.Cursor
+import androidx.sqlite.db.SupportSQLiteDatabase
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.Single
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.patient.PatientSearchResult
 import org.simple.clinic.patient.PatientStatus
 import java.util.UUID
 
-class InstantPatientSearchExperimentsDao(private val appDatabase: AppDatabase): PatientSearchResult.RoomDao {
+class InstantPatientSearchExperimentsDao(private val appDatabase: AppDatabase) : PatientSearchResult.RoomDao {
 
   private val wrappedDao = appDatabase.patientSearchDao()
 
@@ -25,5 +28,35 @@ class InstantPatientSearchExperimentsDao(private val appDatabase: AppDatabase): 
 
   override fun searchByPhoneNumber(phoneNumber: String): Flowable<List<PatientSearchResult>> {
     return wrappedDao.searchByPhoneNumber(phoneNumber)
+  }
+
+  fun namePhoneNumber(): Observable<List<PatientNamePhoneNumber>> {
+    return Observable.fromCallable { appDatabase.openHelper.readableDatabase }
+        .map(this::loadPatientNamePhoneNumberFromDb)
+  }
+
+  private fun loadPatientNamePhoneNumberFromDb(database: SupportSQLiteDatabase): List<PatientNamePhoneNumber> {
+    val sql = """
+      |SELECT P."uuid", P."fullName", PPN."number" 
+      |FROM "Patient" P
+      |LEFT JOIN "PatientPhoneNumber" PPN ON (P."uuid" = PPN."patientUuid" AND PPN."deletedAt" IS NULL)
+      |WHERE P."status" = 'active' AND P."deletedAt" IS NULL
+    """.trimMargin()
+
+    return database.query(sql)
+        .use { cursor ->
+          generateSequence { cursor.takeIf { it.moveToNext() } }
+              .map(::PatientNamePhoneNumber)
+              .toList()
+        }
+  }
+
+  data class PatientNamePhoneNumber(val patientUuid: UUID, val patientName: String, val patientPhoneNumber: String?) {
+
+    constructor(cursor: Cursor) : this(
+        patientUuid = UUID.fromString(cursor.getString(0)),
+        patientName = cursor.getString(1),
+        patientPhoneNumber = cursor.getString(2)
+    )
   }
 }

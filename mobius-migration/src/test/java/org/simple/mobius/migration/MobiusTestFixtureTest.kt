e6bf8090a7c9214149afc9ddc98f2710b2a81b71
Ragunath Jawahar <ragunath@obvious.in>
2019-09-12 03:02:35 +0000

    Handle effects in the fixture

--- mobius-migration/src/test/java/org/simple/mobius/migration/MobiusTestFixtureTest.kt
+++ mobius-migration/src/test/java/org/simple/mobius/migration/MobiusTestFixtureTest.kt
@@ -2,9 +2,12 @@ package org.simple.mobius.migration
 
 import com.google.common.truth.Truth.assertThat
 import com.google.common.util.concurrent.MoreExecutors
+import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.subjects.PublishSubject
 import org.junit.Test
+import org.simple.mobius.migration.fix.CounterEffect
 import org.simple.mobius.migration.fix.CounterEvent
+import org.simple.mobius.migration.fix.CounterEvent.Decrement
 import org.simple.mobius.migration.fix.CounterEvent.Increment
 import org.simple.mobius.migration.fix.CounterModel
 import org.simple.mobius.migration.fix.update
@@ -15,8 +18,12 @@ class MobiusTestFixtureTest {
   private val defaultModel: CounterModel = 0
   private val view = View()
   private val renderFunction = { model: CounterModel -> view.render(model) }
+  private val effectHandler = RxMobius
+      .subtypeEffectHandler<CounterEffect, CounterEvent>()
+      .addAction(CounterEffect.NegativeNumberEffect::class.java) { view.notifyNegativeNumber() }
+      .build()
   private val executorService = MoreExecutors.newDirectExecutorService()
-  private val fixture = MobiusTestFixture(events, ::update, defaultModel, renderFunction, executorService)
+  private val fixture = MobiusTestFixture(events, ::update, defaultModel, renderFunction, effectHandler, executorService)
 
   @Test
   fun `it can dispatch events to update the model`() {
@@ -40,12 +47,29 @@ class MobiusTestFixtureTest {
     assertThat(view.model)
         .isEqualTo(1)
   }
+
+  @Test
+  fun `it can realize effects`() {
+    // when
+    events.onNext(Decrement)
+
+    // then
+    assertThat(fixture.model)
+        .isEqualTo(-1)
+    assertThat(view.notifyNegativeNumberInvoked)
+        .isTrue()
+  }
 }
 
 class View {
   var model by Delegates.notNull<CounterModel>()
+  var notifyNegativeNumberInvoked: Boolean = false
 
   fun render(model: CounterModel) {
     this.model = model
+  }
+
+  fun notifyNegativeNumber() {
+    this.notifyNegativeNumberInvoked = true
   }
 }

d3d8d012a2dd6656807e28cd67e54e180935d424
Ragunath Jawahar <ragunath@obvious.in>
2019-09-12 02:45:24 +0000

    Invoke render functions to test view

--- mobius-migration/src/test/java/org/simple/mobius/migration/MobiusTestFixtureTest.kt
+++ mobius-migration/src/test/java/org/simple/mobius/migration/MobiusTestFixtureTest.kt
@@ -6,15 +6,19 @@ import io.reactivex.subjects.PublishSubject
 import org.junit.Test
 import org.simple.mobius.migration.fix.CounterEvent
 import org.simple.mobius.migration.fix.CounterEvent.Increment
+import org.simple.mobius.migration.fix.CounterModel
 import org.simple.mobius.migration.fix.update
+import kotlin.properties.Delegates
 
 class MobiusTestFixtureTest {
+  private val executorService = MoreExecutors.newDirectExecutorService()
+  private val events = PublishSubject.create<CounterEvent>()
+  private val noopRenderFunction: (CounterModel) -> Unit = { /* deliberately no-op */ }
+
   @Test
   fun `it can dispatch events to update the model`() {
     // given
-    val executorService = MoreExecutors.newDirectExecutorService()
-    val events = PublishSubject.create<CounterEvent>()
-    val fixture = MobiusTestFixture(0, ::update, events, executorService)
+    val fixture = MobiusTestFixture(events, ::update, 0, noopRenderFunction, executorService)
 
     // when
     with(events) {
@@ -25,5 +29,28 @@ class MobiusTestFixtureTest {
     // then
     assertThat(fixture.model)
         .isEqualTo(2)
+  }
+
+  @Test
+  fun `it can invoke the render function after a model update`() {
+    // given
+    val view = object {
+      var model by Delegates.notNull<Int>()
+
+      fun render(model: CounterModel) {
+        this.model = model
+      }
+    }
+    val renderFunction = { model: CounterModel -> view.render(model) }
+    MobiusTestFixture(events, ::update, 0, renderFunction, executorService)
+
+    // when
+    with(events) {
+      onNext(Increment)
+    }
+
+    // then
+    assertThat(view.model)
+        .isEqualTo(1)
   }
 }

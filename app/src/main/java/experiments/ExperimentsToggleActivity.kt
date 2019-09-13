3013d83b8610be081b130eb290370ecc2fb5c246
Vinay S Shenoy <vinay@obvious.in>
2019-09-13 16:46:15 +0530

    Add function to clear seed data

--- app/src/main/java/experiments/ExperimentsToggleActivity.kt
+++ app/src/main/java/experiments/ExperimentsToggleActivity.kt
@@ -1,16 +1,24 @@
 package experiments
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
-import com.f2prateek.rx.preferences2.Preference
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.android.schedulers.AndroidSchedulers.*
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.Schedulers.*
 import kotlinx.android.synthetic.main.experiments_activitytoggle.*
 import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.patient.PatientRepository
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.*
 import javax.inject.Inject
-import javax.inject.Named
 
 class ExperimentsToggleActivity : AppCompatActivity() {
 
@@ -28,8 +36,8 @@ class ExperimentsToggleActivity : AppCompatActivity() {
 
   class ExperimentsToggleFragment : PreferenceFragmentCompat() {
 
-    @field:[Inject Named("experiment_instantsearch_v1_toggle")]
-    lateinit var instantSearchV1ExperimentToggle: Preference<Boolean>
+    @Inject
+    lateinit var patientRepository: PatientRepository
 
     override fun onAttach(context: Context) {
       super.onAttach(context)
@@ -41,14 +49,43 @@ class ExperimentsToggleActivity : AppCompatActivity() {
       val screen = preferenceManager.createPreferenceScreen(context)
 
       val toggleInstantSearchV1Experiment = SwitchPreferenceCompat(context).apply {
-        key = instantSearchV1ExperimentToggle.key()
+        key = "experiment_instantsearch_v1_toggle"
         title = "Instant Search (v1)"
         summary = "Toggle the instant search experiment (v1) flow"
       }
 
+      val resetSeedData = Preference(context).apply {
+        key = "reset_seed_data"
+        title = "Reset Seed Data"
+        isPersistent = false
+        summary = "Clears the patient data already saved and resets the data to the default seed data"
+        setOnPreferenceClickListener {
+          resetSeedData()
+          true
+        }
+      }
+
       screen.addPreference(toggleInstantSearchV1Experiment)
+      screen.addPreference(resetSeedData)
 
       preferenceScreen = screen
+    }
+
+    @SuppressLint("CheckResult")
+    private fun resetSeedData() {
+      val dialog = AlertDialog.Builder(requireContext())
+          .setTitle("Resetting Seed Data")
+          .setMessage("Please wait\u2026")
+          .setCancelable(false)
+          .create()
+
+      patientRepository
+          .clearPatientData()
+          .doOnSubscribe { requireActivity().runOnUiThread { dialog.show() } }
+          .subscribeOn(io())
+          .delay(3L, SECONDS)
+          .observeOn(mainThread())
+          .subscribe { dialog.dismiss() }
     }
   }
 }

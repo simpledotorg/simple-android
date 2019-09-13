0242fb3992e5e912d4aeec2daa3062f8633960f8
Ragunath Jawahar <ragunath@obvious.in>
2019-09-13 11:59:09 +0000

    Enable AllPatientsInFacilityView to use Mobius

---
+++ app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityListAdapter.kt
@@ -0,0 +1,21 @@
+package org.simple.clinic.allpatientsinfacility
+
+import androidx.recyclerview.widget.DiffUtil
+import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
+import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityListItem.SectionTitle.None
+import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityListItem.SectionTitle.Text
+import org.simple.clinic.widgets.ItemAdapter
+import java.util.Locale
+
+class AllPatientsInFacilityListAdapter(
+    diffCallback: DiffUtil.ItemCallback<AllPatientsInFacilityListItem>,
+    private val locale: Locale
+) : ItemAdapter<AllPatientsInFacilityListItem, AllPatientsInFacilityListItem.Event>(diffCallback), FastScrollRecyclerView.SectionedAdapter {
+
+  override fun getSectionName(position: Int): String {
+    return when (val sectionTitle = getItem(position).sectionTitle(locale)) {
+      is None -> ""
+      is Text -> sectionTitle.title
+    }
+  }
+}

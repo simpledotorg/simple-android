5d0acfdb7f20a965548795163513ea1420ee4b11
Ragunath Jawahar <ragunath@obvious.in>
2019-09-13 17:06:40 +0530

    Migrate view rendering logic to Mobius for all patients in facility

--- app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityUi.kt
+++ app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityUi.kt
@@ -3,5 +3,8 @@ package org.simple.clinic.allpatientsinfacility
 interface AllPatientsInFacilityUi {
   fun showNoPatientsFound(facilityName: String)
 
-  fun showPatients(facilityUiState: FacilityUiState, patientSearchResults: List<PatientSearchResultUiState>)
+  fun showPatients(
+      facilityUiState: FacilityUiState,
+      patientSearchResults: List<PatientSearchResultUiState>
+  )
 }

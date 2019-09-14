d3a9e7d93c653d0c52419c48540b7e745d4a5f19
Vinay S Shenoy <vinay@obvious.in>
2019-09-14 12:10:50 +0530

    Disable analytics

--- app/src/main/java/org/simple/clinic/ReleaseClinicApp.kt
+++ app/src/main/java/org/simple/clinic/ReleaseClinicApp.kt
@@ -1,15 +1,11 @@
 package org.simple.clinic
 
 import android.annotation.SuppressLint
-import org.simple.clinic.analytics.HeapAnalyticsReporter
-import org.simple.clinic.analytics.MixpanelAnalyticsReporter
-import org.simple.clinic.analytics.swallowErrors
 import org.simple.clinic.di.AppComponent
 import org.simple.clinic.di.AppModule
 import org.simple.clinic.di.DaggerAppComponent
 import org.simple.clinic.sync.SyncScheduler
 import org.simple.clinic.sync.indicator.SyncIndicatorStatusCalculator
-import org.simple.clinic.util.unsafeLazy
 import javax.inject.Inject
 
 @SuppressLint("Registered")
@@ -20,13 +16,6 @@ class ReleaseClinicApp : ClinicApp() {
 
   @Inject
   lateinit var syncIndicatorStatusCalculator: SyncIndicatorStatusCalculator
-
-  override val analyticsReporters by unsafeLazy {
-    listOf(
-        HeapAnalyticsReporter(this).swallowErrors(),
-        MixpanelAnalyticsReporter(this).swallowErrors()
-    )
-  }
 
   override fun onCreate() {
     super.onCreate()

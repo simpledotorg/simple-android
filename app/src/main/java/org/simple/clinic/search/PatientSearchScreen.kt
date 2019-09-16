64c7770191a56eb0d25afecdbeac9af1d6ecc02c
Ragunath Jawahar <ragunath@obvious.in>
2019-09-16 06:29:41 +0000

    Make PatientSearchScreen to use AllPatientsInFacilityView (Mobius)

--- app/src/main/java/org/simple/clinic/search/PatientSearchScreen.kt
+++ app/src/main/java/org/simple/clinic/search/PatientSearchScreen.kt
@@ -12,10 +12,13 @@ import io.reactivex.rxkotlin.ofType
 import kotlinx.android.synthetic.main.screen_patient_search.view.*
 import org.simple.clinic.R
 import org.simple.clinic.activity.TheActivity
+import org.simple.clinic.allpatientsinfacility.migration.AllPatientsInFacilityViewMigration
+import org.simple.clinic.allpatientsinfacility.migration.ExposesUiEvents
 import org.simple.clinic.allpatientsinfacility_old.AllPatientsInFacilityListScrolled
 import org.simple.clinic.allpatientsinfacility_old.AllPatientsInFacilitySearchResultClicked
-import org.simple.clinic.allpatientsinfacility_old.AllPatientsInFacilityView
 import org.simple.clinic.bindUiToController
+import org.simple.clinic.mobius.migration.MobiusMigrationConfig
+import org.simple.clinic.mobius.migration.RevertToCommit
 import org.simple.clinic.patient.PatientSearchCriteria
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.search.results.PatientSearchResultsScreenKey
@@ -28,9 +31,17 @@ import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.hideKeyboard
 import org.simple.clinic.widgets.showKeyboard
 import org.threeten.bp.Instant
+import timber.log.Timber
 import java.util.UUID
 import javax.inject.Inject
 
+@AllPatientsInFacilityViewMigration(
+    notes = "Removes toggling mechanism, just make sure the reverted layouts do not contain layouts suffixed with `_old`."
+)
+@RevertToCommit(
+    withMessage = "Make PatientSearchScreen to use AllPatientsInFacilityView (Mobius)",
+    afterCommitId = "4bd5c87cf567ec64184622a0a3699ce93675e28d"
+)
 class PatientSearchScreen(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
 
   @Inject
@@ -45,8 +56,18 @@ class PatientSearchScreen(context: Context, attrs: AttributeSet) : RelativeLayou
   @Inject
   lateinit var utcClock: UtcClock
 
+  @Inject
+  lateinit var mobiusMigrationConfig: MobiusMigrationConfig
+
   private val allPatientsInFacilityView by unsafeLazy {
-    allPatientsView as AllPatientsInFacilityView
+    allPatientsViewStub.layoutResource = if (mobiusMigrationConfig.useAllPatientsInFacilityView) {
+      Timber.d("Using AllPatientsInFacilityView (Mobius)")
+      R.layout.view_allpatientsinfacility
+    } else {
+      Timber.d("Using AllPatientsInFacilityView (OG Architecture)")
+      R.layout.view_allpatientsinfacility_old
+    }
+    allPatientsViewStub.inflate() as ExposesUiEvents
   }
 
   override fun onFinishInflate() {
@@ -129,11 +150,11 @@ class PatientSearchScreen(context: Context, attrs: AttributeSet) : RelativeLayou
   }
 
   fun showAllPatientsInFacility() {
-    allPatientsView.visibility = View.VISIBLE
+    (allPatientsInFacilityView as View).visibility = View.VISIBLE
   }
 
   fun hideAllPatientsInFacility() {
-    allPatientsView.visibility = View.GONE
+    (allPatientsInFacilityView as View).visibility = View.GONE
   }
 
   fun showSearchButton() {

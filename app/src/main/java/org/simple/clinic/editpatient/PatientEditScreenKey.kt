be03b052d686cd56bb062c750f75e7daa4d8901e
Ragunath Jawahar <ragunath@obvious.in>
2019-09-19 10:56:50 +0000

    PatientEditScreenKey remove UUID key and collapse hierarchy

--- app/src/main/java/org/simple/clinic/editpatient/PatientEditScreenKey.kt
+++ app/src/main/java/org/simple/clinic/editpatient/PatientEditScreenKey.kt
@@ -7,20 +7,20 @@ import org.simple.clinic.patient.Patient
 import org.simple.clinic.patient.PatientAddress
 import org.simple.clinic.patient.PatientPhoneNumber
 import org.simple.clinic.router.screen.FullScreenKey
-import java.util.UUID
 
-sealed class PatientEditScreenKey(open val patientUuid: UUID) : FullScreenKey {
+@Parcelize
+data class PatientEditScreenKey(
+    val patient: Patient,
+    val address: PatientAddress,
+    val phoneNumber: PatientPhoneNumber?
+) : FullScreenKey {
   companion object {
-    @Deprecated("Use `fromPatientData` instead.")
-    fun fromPatientUuid(uuid: UUID): PatientEditScreenKey =
-        PatientEditScreenKeyWithUuid(uuid)
-
     fun fromPatientData(
         patient: Patient,
         address: PatientAddress,
         phoneNumber: PatientPhoneNumber?
     ): PatientEditScreenKey {
-      return PatientEditScreenKeyWithData(patient, address, phoneNumber)
+      return PatientEditScreenKey(patient, address, phoneNumber)
     }
   }
 
@@ -29,15 +29,3 @@ sealed class PatientEditScreenKey(open val patientUuid: UUID) : FullScreenKey {
 
   override fun layoutRes() = R.layout.screen_patient_edit
 }
-
-@Parcelize
-data class PatientEditScreenKeyWithUuid(
-    override val patientUuid: UUID
-) : PatientEditScreenKey(patientUuid)
-
-@Parcelize
-data class PatientEditScreenKeyWithData(
-    val patient: Patient,
-    val address: PatientAddress,
-    val phoneNumber: PatientPhoneNumber?
-) : PatientEditScreenKey(patient.uuid)

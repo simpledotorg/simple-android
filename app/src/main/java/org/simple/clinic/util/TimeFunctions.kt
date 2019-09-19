075aa175fc55c2b10baf5dc6c63c296c9ab561e2
Vinay S Shenoy <vinay@obvious.in>
2019-09-19 11:05:57 +0000

    Remove unused age and DOB estimation functions

--- app/src/main/java/org/simple/clinic/util/TimeFunctions.kt
+++ app/src/main/java/org/simple/clinic/util/TimeFunctions.kt
@@ -1,6 +1,5 @@
 package org.simple.clinic.util
 
-import org.threeten.bp.Clock
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalTime
@@ -8,24 +7,12 @@ import org.threeten.bp.Period
 import org.threeten.bp.ZoneId
 import org.threeten.bp.ZoneOffset.UTC
 
-fun estimateCurrentAge(recordedAge: Int, ageRecordedAtTimestamp: Instant, clock: Clock): Int {
-  val ageRecordedAtDate = ageRecordedAtTimestamp.atZone(UTC).toLocalDate()
-  val today = LocalDate.now(clock)
-
-  val yearsPassedSinceAgeRecorded = Period.between(ageRecordedAtDate, today).years
-  return yearsPassedSinceAgeRecorded + recordedAge
-}
-
 fun estimateCurrentAge(recordedAge: Int, ageRecordedAtTimestamp: Instant, clock: UserClock): Int {
   val ageRecordedAtDate = ageRecordedAtTimestamp.toLocalDateAtZone(clock.zone)
   val today = LocalDate.now(clock)
 
   val yearsPassedSinceAgeRecorded = Period.between(ageRecordedAtDate, today).years
   return recordedAge + yearsPassedSinceAgeRecorded
-}
-
-fun estimateCurrentAge(recordedDateOfBirth: LocalDate, clock: Clock): Int {
-  return Period.between(recordedDateOfBirth, LocalDate.now(clock)).years
 }
 
 fun estimateCurrentAge(recordedDateOfBirth: LocalDate, clock: UserClock): Int {

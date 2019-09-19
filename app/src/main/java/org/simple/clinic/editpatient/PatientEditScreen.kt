a10dca4ea517a6b02d6c8682f38edc5e8c90598b
Ragunath Jawahar <ragunath@obvious.in>
2019-09-19 09:22:23 +0000

    Use a factory function to create PatientEditScreenCreated instances

--- app/src/main/java/org/simple/clinic/editpatient/PatientEditScreen.kt
+++ app/src/main/java/org/simple/clinic/editpatient/PatientEditScreen.kt
@@ -136,7 +136,7 @@ class PatientEditScreen(context: Context, attributeSet: AttributeSet) : Relative
   private fun screenCreates(): Observable<UiEvent> {
     val key = screenRouter.key<PatientEditScreenKey>(this)
 
-    return Observable.just(PatientEditScreenCreated(key.patientUuid))
+    return Observable.just(PatientEditScreenCreated.fromPatientUuid(key.patientUuid))
   }
 
   private fun saveClicks(): Observable<UiEvent> {

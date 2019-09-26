da52652f452db0ce765c0e4eacdebaa5ece1ea71
Rakshak R.Hegde <rakshak@obvious.in>
2019-09-26 11:31:32 +0000

    Download illustrations

---
+++ app/src/main/java/org/simple/clinic/illustration/FileDownloadService.kt
@@ -0,0 +1,14 @@
+package org.simple.clinic.illustration
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Streaming
+import retrofit2.http.Url
+
+interface FileDownloadService {
+
+  @Streaming
+  @GET
+  fun downloadFile(@Url fileUrl: String): Call<ResponseBody>
+}

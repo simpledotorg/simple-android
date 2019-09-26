c13cdab0a61d360356671672d96a525d1f65f128
Rakshak R.Hegde <rakshak@obvious.in>
2019-09-26 11:31:32 +0000

    Download illustrations

--- app/src/main/java/org/simple/clinic/illustration/HomescreenIllustrationModule.kt
+++ app/src/main/java/org/simple/clinic/illustration/HomescreenIllustrationModule.kt
@@ -1,11 +1,15 @@
 package org.simple.clinic.illustration
 
+import android.app.Application
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.remoteconfig.ConfigReader
+import org.threeten.bp.Month
+import retrofit2.Retrofit
+import java.io.File
+import javax.inject.Named
 
 @Module
 class HomescreenIllustrationModule {
@@ -18,9 +22,20 @@ class HomescreenIllustrationModule {
       configReader: ConfigReader,
       moshi: Moshi
   ): List<HomescreenIllustration> {
-    val parameterizedType = Types.newParameterizedType(List::class.java, HomescreenIllustration::class.java)
-    val adapter = moshi.adapter<List<HomescreenIllustration>>(parameterizedType)
-    val illustrations = adapter.fromJson(configReader.string("home_screen_illustration", "[]"))
-    return checkNotNull(illustrations)
+    return listOf(
+        HomescreenIllustration(
+            eventId = "valmiki-jayanti.png",
+            illustrationUrl = "https://firebasestorage.googleapis.com/v0/b/simple-org.appspot.com/o/image%20(4).png?alt=media&token=1b123ed1-e644-46d4-b4cd-f0ed54a90420",
+            from = DayOfMonth(20, Month.SEPTEMBER),
+            to = DayOfMonth(30, Month.SEPTEMBER)
+        )
+    )
   }
+
+  @Provides
+  @Named("homescreen-illustration-folder")
+  fun illustrationsFolder(context: Application) = File(context.filesDir, "homescreen-illustrations")
+
+  @Provides
+  fun fileDownloadService(retrofit: Retrofit): FileDownloadService = retrofit.create(FileDownloadService::class.java)
 }

8face4bec7a4f4c46e2c5d6e4d224ed4b821b1af
Rakshak R.Hegde <rakshak@obvious.in>
2019-09-30 07:45:55 +0000

    Parse home screen illustration config string using Moshi adapter

--- app/src/main/java/org/simple/clinic/illustration/HomescreenIllustrationModule.kt
+++ app/src/main/java/org/simple/clinic/illustration/HomescreenIllustrationModule.kt
@@ -1,8 +1,11 @@
 package org.simple.clinic.illustration
 
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import dagger.Module
 import dagger.Provides
-import org.threeten.bp.Month
+import org.simple.clinic.remoteconfig.ConfigReader
 import retrofit2.Retrofit
 import javax.inject.Named
 
@@ -17,13 +20,15 @@ class HomescreenIllustrationModule {
   fun fileDownloadService(retrofit: Retrofit): FileDownloadService = retrofit.create(FileDownloadService::class.java)
 
   @Provides
-  fun illustrations(): List<HomescreenIllustration> =
-      listOf(
-          HomescreenIllustration(
-              eventId = "valmiki-jayanti.png",
-              illustrationUrl = "https://firebasestorage.googleapis.com/v0/b/simple-org.appspot.com/o/valmiki.png?alt=media&token=15a1f9da-3712-403b-aa13-e51cba37ef88",
-              from = DayOfMonth(20, Month.SEPTEMBER),
-              to = DayOfMonth(30, Month.SEPTEMBER)
-          )
-      )
+  fun illustrations(
+      moshi: Moshi,
+      configReader: ConfigReader
+  ): List<HomescreenIllustration> {
+    val illustrationsType = Types.newParameterizedType(List::class.java, HomescreenIllustration::class.java)
+    val mapType = Types.newParameterizedType(Map::class.java, String::class.java, illustrationsType)
+    val adapter: JsonAdapter<Map<String, List<HomescreenIllustration>>> = moshi.adapter(mapType)
+
+    val map = adapter.fromJson(configReader.string("home_screen_illustration", "{}"))
+    return map?.get("IN") ?: emptyList()
+  }
 }

663a3471937eab623ecc961282bdec36d82be623
Vinay S Shenoy <vinay@obvious.in>
2019-10-01 10:37:00 +0530

    Clean up IllustrationConfigParser
    Changes:
    - Extract the json adapter into a property
    - Improve how the value is being extracted from the map

--- app/src/main/java/org/simple/clinic/illustration/IllustrationConfigParser.kt
+++ app/src/main/java/org/simple/clinic/illustration/IllustrationConfigParser.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.illustration
 
+import androidx.annotation.VisibleForTesting
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
@@ -7,15 +8,28 @@ import org.simple.clinic.remoteconfig.ConfigReader
 import javax.inject.Inject
 
 class IllustrationConfigParser @Inject constructor(
-    private val moshi: Moshi,
+    moshi: Moshi,
     private val configReader: ConfigReader
 ) {
-  fun illustrations(): List<HomescreenIllustration> {
+
+  private val illustrationMapJsonAdapter: JsonAdapter<Map<String, List<HomescreenIllustration>>>
+
+  init {
     val illustrationsType = Types.newParameterizedType(List::class.java, HomescreenIllustration::class.java)
     val mapType = Types.newParameterizedType(Map::class.java, String::class.java, illustrationsType)
-    val adapter: JsonAdapter<Map<String, List<HomescreenIllustration>>> = moshi.adapter(mapType)
 
-    val map = adapter.fromJson(configReader.string("home_screen_illustration", "{}"))
-    return map?.get("IN") ?: emptyList()
+    illustrationMapJsonAdapter = moshi.adapter(mapType)
+  }
+
+  companion object {
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    const val EMPTY_JSON = "{}"
+  }
+
+  fun illustrations(): List<HomescreenIllustration> {
+    val illustrationConfigJson = configReader.string("home_screen_illustration", EMPTY_JSON)
+
+    val map: Map<String, List<HomescreenIllustration>> = illustrationMapJsonAdapter.fromJson(illustrationConfigJson)!!
+    return map.getOrElse("IN") { emptyList() }
   }
 }

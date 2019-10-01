e2fa5ec7d7be734d3057b7f1613219d3bfb8c1eb
Ragunath Jawahar <ragunath@obvious.in>
2019-10-01 10:40:22 +0000

    Create Mobius components

---
+++ app/src/main/java/org/simple/clinic/onboarding/OnboardingModel.kt
@@ -0,0 +1,7 @@
+package org.simple.clinic.onboarding
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+object OnboardingModel : Parcelable
--- app/src/test/java/org/simple/clinic/onboarding/OnboardingScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/onboarding/OnboardingScreenControllerTest.kt
@@ -4,12 +4,13 @@ import com.f2prateek.rx.preferences2.Preference
 import com.nhaarman.mockito_kotlin.eq
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import com.spotify.mobius.Next.noChange
 import io.reactivex.subjects.PublishSubject
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.simple.clinic.util.RxErrorsRule
-import org.simple.clinic.widgets.UiEvent
+import org.simple.mobius.migration.MobiusTestFixture
 
 class OnboardingScreenControllerTest {
 
@@ -19,16 +20,27 @@ class OnboardingScreenControllerTest {
   private val screen = mock<OnboardingUi>()
   private val hasUserCompletedOnboarding = mock<Preference<Boolean>>()
 
-  private val uiEvents = PublishSubject.create<UiEvent>()
+  private val uiEvents = PublishSubject.create<OnboardingEvent>()
   lateinit var controller: OnboardingScreenController
 
   @Before
   fun setUp() {
     controller = OnboardingScreenController(hasUserCompletedOnboarding)
 
-    uiEvents
+    val sharedOnboardingEvents = uiEvents.hide().share()
+
+    sharedOnboardingEvents
         .compose(controller)
         .subscribe { uiChange -> uiChange(screen) }
+
+    MobiusTestFixture<OnboardingModel, OnboardingEvent, OnboardingEffect>(
+        sharedOnboardingEvents,
+        OnboardingModel,
+        null,
+        { model: OnboardingModel, event: OnboardingEvent -> noChange<OnboardingModel, OnboardingEffect>() },
+        OnboardingEffectHandler.createEffectHandler(),
+        { /* No-op */ }
+    ).start()
   }
 
   @Test

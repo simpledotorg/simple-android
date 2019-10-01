f0e9d855a148a5b37984562c279045d8bfa30585
Ragunath Jawahar <ragunath@obvious.in>
2019-10-01 11:10:21 +0000

    Add schedulers to effect handler

--- app/src/main/java/org/simple/clinic/onboarding/OnboardingEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/onboarding/OnboardingEffectHandler.kt
@@ -3,20 +3,28 @@ package org.simple.clinic.onboarding
 import com.f2prateek.rx.preferences2.Preference
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.util.scheduler.SchedulersProvider
 
 object OnboardingEffectHandler {
   fun createEffectHandler(
       hasUserCompletedOnboarding: Preference<Boolean>,
-      ui: OnboardingUi
+      ui: OnboardingUi,
+      schedulersProvider: SchedulersProvider
   ): ObservableTransformer<OnboardingEffect, OnboardingEvent> {
     return RxMobius
         .subtypeEffectHandler<OnboardingEffect, OnboardingEvent>()
-        .addTransformer(CompleteOnboardingEffect::class.java) { completeOnboardingEffect ->
-          completeOnboardingEffect
-              .doOnNext { hasUserCompletedOnboarding.set(true) }
-              .map { OnboardingCompleted }
-        }
-        .addAction(MoveToRegistrationEffect::class.java) { ui.moveToRegistrationScreen() }
+        .addTransformer(CompleteOnboardingEffect::class.java, completeOnboardingTransformer(hasUserCompletedOnboarding))
+        .addAction(MoveToRegistrationEffect::class.java, ui::moveToRegistrationScreen, schedulersProvider.ui())
         .build()
+  }
+
+  private fun completeOnboardingTransformer(
+      hasUserCompletedOnboarding: Preference<Boolean>
+  ): ObservableTransformer<CompleteOnboardingEffect, OnboardingEvent> {
+    return ObservableTransformer { completeOnboardingEffect ->
+      completeOnboardingEffect
+          .doOnNext { hasUserCompletedOnboarding.set(true) }
+          .map { OnboardingCompleted }
+    }
   }
 }

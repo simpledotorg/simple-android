ff9b70c6fbca3cafc0f6498cf3c6870a9ee79283
Vinay S Shenoy <vinay@obvious.in>
2019-10-01 06:54:21 +0000

    Add Settings update logic
    - Update the user details when they haven't already been loaded

---
+++ app/src/test/java/org/simple/clinic/settings/SettingsUpdateTest.kt
@@ -0,0 +1,27 @@
+package org.simple.clinic.settings
+
+import com.spotify.mobius.test.NextMatchers.hasModel
+import com.spotify.mobius.test.NextMatchers.hasNoEffects
+import com.spotify.mobius.test.UpdateSpec
+import com.spotify.mobius.test.UpdateSpec.assertThatNext
+import org.junit.Test
+
+class SettingsUpdateTest {
+
+  private val defaultModel = SettingsModel.FETCHING_USER_DETAILS
+
+  @Test
+  fun `the user details loaded event must update the model`() {
+    val spec = UpdateSpec<SettingsModel, SettingsEvent, SettingsEffect>(SettingsLogic::update)
+    val userName = "Anish Acharya"
+    val userPhoneNumber = "1234567890"
+
+    spec
+        .given(defaultModel)
+        .whenEvent(UserDetailsLoaded(userName, userPhoneNumber))
+        .then(assertThatNext(
+            hasModel(defaultModel.userDetailsFetched(userName, userPhoneNumber)),
+            hasNoEffects()
+        ))
+  }
+}

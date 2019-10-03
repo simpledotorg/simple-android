cf14cae37590d81256c1694976d5d4fd5ac0aeb0
Ragunath Jawahar <ragunath@obvious.in>
2019-10-03 09:11:51 +0000

    Move screen created logic to Mobius

--- app/src/main/java/org/simple/clinic/newentry/PatientEntryLogic.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryLogic.kt
@@ -3,17 +3,24 @@ package org.simple.clinic.newentry
 import com.spotify.mobius.First
 import com.spotify.mobius.First.first
 import com.spotify.mobius.Next
+import com.spotify.mobius.Next.next
 import com.spotify.mobius.Next.noChange
 
 fun patientEntryInit(
     model: PatientEntryModel
-): First<PatientEntryModel, PatientEntryEffect> {
-  return first(model)
-}
+): First<PatientEntryModel, PatientEntryEffect> =
+    first(model, setOf(FetchPatientEntry))
 
 fun patientEntryUpdate(
     model: PatientEntryModel,
     event: PatientEntryEvent
 ): Next<PatientEntryModel, PatientEntryEffect> {
+  if (event is OngoingEntryFetched) {
+    return next(
+        model.patientEntryFetched(event.patientEntry),
+        setOf(PrefillFields(event.patientEntry))
+    )
+  }
+
   return noChange()
 }
--- app/src/main/java/org/simple/clinic/newentry/PatientEntryModel.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryModel.kt
@@ -1,3 +1,14 @@
 package org.simple.clinic.newentry
 
-object PatientEntryModel
+import org.simple.clinic.patient.OngoingNewPatientEntry
+
+data class PatientEntryModel(
+    val patientEntry: OngoingNewPatientEntry? = null
+) {
+  companion object {
+    val DEFAULT = PatientEntryModel()
+  }
+
+  fun patientEntryFetched(patientEntry: OngoingNewPatientEntry): PatientEntryModel =
+      copy(patientEntry = patientEntry)
+}

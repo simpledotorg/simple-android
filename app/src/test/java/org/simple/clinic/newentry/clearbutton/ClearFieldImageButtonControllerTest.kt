7ec3e3ac9c8b6c38a3daeae9ae48efb3d5ebd70f
Ragunath Jawahar <ragunath@obvious.in>
2019-10-03 05:13:09 +0000

    Wrap function calls on the same object with a `with` block

--- app/src/test/java/org/simple/clinic/newentry/clearbutton/ClearFieldImageButtonControllerTest.kt
+++ app/src/test/java/org/simple/clinic/newentry/clearbutton/ClearFieldImageButtonControllerTest.kt
@@ -31,12 +31,14 @@ class ClearFieldImageButtonControllerTest {
 
   @Test
   fun `clear button should remain visible only while the associated field is focused and is not blank`() {
-    uiEvents.onNext(CleareableFieldFocusChanged(hasFocus = false))
-    uiEvents.onNext(CleareableFieldTextChanged(""))
-    uiEvents.onNext(CleareableFieldFocusChanged(hasFocus = true))
-    uiEvents.onNext(CleareableFieldTextChanged("A"))
-    uiEvents.onNext(CleareableFieldTextChanged("AB"))
-    uiEvents.onNext(CleareableFieldFocusChanged(hasFocus = false))
+    with(uiEvents) {
+      onNext(CleareableFieldFocusChanged(hasFocus = false))
+      onNext(CleareableFieldTextChanged(""))
+      onNext(CleareableFieldFocusChanged(hasFocus = true))
+      onNext(CleareableFieldTextChanged("A"))
+      onNext(CleareableFieldTextChanged("AB"))
+      onNext(CleareableFieldFocusChanged(hasFocus = false))
+    }
 
     verify(button, times(2)).setVisible(false)
     verify(button, times(1)).setVisible(true)

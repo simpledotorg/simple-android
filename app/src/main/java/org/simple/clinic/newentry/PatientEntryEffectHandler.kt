eef189ef7feedfbb75d417b21ae272c8f0b87866
Ragunath Jawahar <ragunath@obvious.in>
2019-10-03 09:11:51 +0000

    Move screen created logic to Mobius

--- app/src/main/java/org/simple/clinic/newentry/PatientEntryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryEffectHandler.kt
@@ -2,14 +2,50 @@ package org.simple.clinic.newentry
 
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
+import io.reactivex.rxkotlin.Singles
+import org.simple.clinic.facility.FacilityRepository
+import org.simple.clinic.patient.OngoingNewPatientEntry
+import org.simple.clinic.patient.PatientRepository
+import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 object PatientEntryEffectHandler {
   fun createEffectHandler(
+      userSession: UserSession,
+      facilityRepository: FacilityRepository,
+      patientRepository: PatientRepository,
+      ui: PatientEntryUi,
       schedulersProvider: SchedulersProvider
   ): ObservableTransformer<PatientEntryEffect, PatientEntryEvent> {
     return RxMobius
         .subtypeEffectHandler<PatientEntryEffect, PatientEntryEvent>()
+        .addTransformer(FetchPatientEntry::class.java, fetchOngoingEntryEffectHandler(userSession, facilityRepository, patientRepository, schedulersProvider.io()))
+        .addConsumer(PrefillFields::class.java, { ui.preFillFields(it.patientEntry) }, schedulersProvider.ui())
         .build()
+  }
+
+  private fun fetchOngoingEntryEffectHandler(
+      userSession: UserSession,
+      facilityRepository: FacilityRepository,
+      patientRepository: PatientRepository,
+      scheduler: Scheduler
+  ): ObservableTransformer<FetchPatientEntry, PatientEntryEvent> {
+    return ObservableTransformer { fetchPatientEntries ->
+      val getPatientEntryAndFacility = Singles.zip(
+          patientRepository.ongoingEntry(),
+          facilityRepository.currentFacility(userSession).firstOrError()
+      )
+
+      fetchPatientEntries
+          .flatMapSingle { getPatientEntryAndFacility }
+          .subscribeOn(scheduler)
+          .map { (entry, facility) ->
+            // TODO(rj): 2019-10-03 Extract as function!
+            entry.takeIf { it.address != null }
+                ?: entry.copy(address = OngoingNewPatientEntry.Address(colonyOrVillage = "", district = facility.district, state = facility.state))
+          }
+          .map { OngoingEntryFetched(it) }
+    }
   }
 }

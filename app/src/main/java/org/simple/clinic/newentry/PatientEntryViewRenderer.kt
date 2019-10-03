9b09d38842e256d446a2a25aa157543e6d3bb5a9
Ragunath Jawahar <ragunath@obvious.in>
2019-10-03 11:38:51 +0000

    Move toggling identifier section to Mobius

--- app/src/main/java/org/simple/clinic/newentry/PatientEntryViewRenderer.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryViewRenderer.kt
@@ -4,6 +4,12 @@ import org.simple.clinic.mobius.ViewRenderer
 
 class PatientEntryViewRenderer(val ui: PatientEntryUi) : ViewRenderer<PatientEntryModel> {
   override fun render(model: PatientEntryModel) {
-    // **crickets**
+    if (model.patientEntry == null) return
+
+    if (model.patientEntry.identifier != null) {
+      ui.showIdentifierSection()
+    } else {
+      ui.hideIdentifierSection()
+    }
   }
 }

f91518c79562258cbcdc33bc3d2828bec10f6c6a
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-04 13:01:40 +0000

    Change LoginOtpSmsListenerImpl to use the convenience method

--- app/src/main/java/org/simple/clinic/login/LoginOtpSmsListenerImpl.kt
+++ app/src/main/java/org/simple/clinic/login/LoginOtpSmsListenerImpl.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.google.android.gms.auth.api.phone.SmsRetriever
 import io.reactivex.Completable
 import org.simple.clinic.util.scheduler.SchedulersProvider
+import org.simple.clinic.util.toCompletable
 import javax.inject.Inject
 
 class LoginOtpSmsListenerImpl @Inject constructor(
@@ -12,15 +13,10 @@ class LoginOtpSmsListenerImpl @Inject constructor(
 ) : LoginOtpSmsListener {
 
   override fun listenForLoginOtp(): Completable {
-    return Completable
-        .create { emitter ->
-          val client = SmsRetriever.getClient(application)
-
-          val task = client.startSmsRetriever()
-
-          task.addOnSuccessListener { emitter.onComplete() }
-          task.addOnFailureListener { emitter.onError(it) }
-        }
+    return SmsRetriever
+        .getClient(application)
+        .startSmsRetriever()
+        .toCompletable()
         .observeOn(schedulersProvider.io())
   }
 }

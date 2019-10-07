a0ab8ce7cfd135595f434b2b5ac43a7ac7e616c0
Vinay S Shenoy <vinay@obvious.in>
2019-10-07 05:47:18 +0000

    Add ChangeLanguage update logic

---
+++ app/src/test/java/org/simple/clinic/settings/changelanguage/ChangeLanguageUpdateTest.kt
@@ -0,0 +1,74 @@
+package org.simple.clinic.settings.changelanguage
+
+import com.spotify.mobius.test.NextMatchers.hasEffects
+import com.spotify.mobius.test.NextMatchers.hasModel
+import com.spotify.mobius.test.NextMatchers.hasNoEffects
+import com.spotify.mobius.test.NextMatchers.hasNoModel
+import com.spotify.mobius.test.UpdateSpec
+import com.spotify.mobius.test.UpdateSpec.assertThatNext
+import org.junit.Test
+import org.simple.clinic.settings.Language
+
+class ChangeLanguageUpdateTest {
+
+  private val defaultModel = ChangeLanguageModel.FETCHING_LANGUAGES
+
+  private val englishIndia = Language(displayName = "English", languageCode = "en_IN")
+  private val hindiIndia = Language(displayName = "हिंदी", languageCode = "hi_IN")
+
+  private val spec = UpdateSpec<ChangeLanguageModel, ChangeLanguageEvent, ChangeLanguageEffect>(ChangeLanguageLogic::update)
+
+  @Test
+  fun `when the current selected language is loaded, the ui must be updated`() {
+    spec
+        .given(defaultModel)
+        .whenEvent(CurrentSelectedLanguageLoadedEvent(englishIndia))
+        .then(assertThatNext(
+            hasModel(defaultModel.withCurrentLanguage(englishIndia)),
+            hasNoEffects()
+        ))
+  }
+
+  @Test
+  fun `when the supported languages are loaded, the ui must be upda`() {
+    val supportedLanguages = listOf(englishIndia, hindiIndia)
+
+    spec
+        .given(defaultModel)
+        .whenEvent(SupportedLanguagesLoadedEvent(supportedLanguages))
+        .then(assertThatNext(
+            hasModel(defaultModel.withSupportedLanguages(supportedLanguages)),
+            hasNoEffects()
+        ))
+  }
+
+  @Test
+  fun `when the user selects a language, the currently selected language must be changed to the selected language`() {
+    val model = defaultModel
+        .withCurrentLanguage(englishIndia)
+        .withSupportedLanguages(listOf(englishIndia, hindiIndia))
+
+    spec
+        .given(model)
+        .whenEvent(SelectLanguageEvent(hindiIndia))
+        .then(assertThatNext(
+            hasNoModel(),
+            hasEffects(UpdateSelectedLanguageEffect(hindiIndia) as ChangeLanguageEffect)
+        ))
+  }
+
+  @Test
+  fun `when the currently selected language is changed, then apply changes and restart the activity`() {
+    val model = defaultModel
+        .withCurrentLanguage(englishIndia)
+        .withSupportedLanguages(listOf(englishIndia, hindiIndia))
+
+    spec
+        .given(model)
+        .whenEvent(SelectedLanguageChangedEvent(hindiIndia))
+        .then(assertThatNext(
+            hasModel(model.withCurrentLanguage(hindiIndia)),
+            hasEffects(RestartActivityEffect as ChangeLanguageEffect)
+        ))
+  }
+}

fa3c5c5bec3d724b528e7727234b5f1d2225dd19
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-08 10:05:19 +0530

    Handle load supported languages effect

--- app/src/test/java/org/simple/clinic/settings/changelanguage/ChangeLanguageEffectHandlerTest.kt
+++ app/src/test/java/org/simple/clinic/settings/changelanguage/ChangeLanguageEffectHandlerTest.kt
@@ -8,6 +8,7 @@ import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
 import org.junit.After
 import org.junit.Test
+import org.simple.clinic.settings.ProvidedLanguage
 import org.simple.clinic.settings.SettingsRepository
 import org.simple.clinic.settings.SystemDefaultLanguage
 import org.simple.clinic.util.scheduler.TrampolineSchedulersProvider
@@ -42,6 +43,26 @@ class ChangeLanguageEffectHandlerTest {
     // then
     testObserver
         .assertValue(CurrentSelectedLanguageLoadedEvent(selectedLanguage))
+        .assertNotComplete()
+        .assertNotTerminated()
+  }
+
+  @Test
+  fun `the list of supported languages must be fetched when the load supported languages effect is received`() {
+    // given
+    val supportedLanguages = listOf(
+        SystemDefaultLanguage,
+        ProvidedLanguage(displayName = "English", languageCode = "en_IN"),
+        ProvidedLanguage(displayName = "हिंदी", languageCode = "hi_IN")
+    )
+    whenever(settingsRepository.getSupportedLanguages()).thenReturn(Single.just(supportedLanguages))
+
+    // when
+    effectsSubject.onNext(LoadSupportedLanguagesEffect)
+
+    // then
+    testObserver
+        .assertValue(SupportedLanguagesLoadedEvent(supportedLanguages))
         .assertNotComplete()
         .assertNotTerminated()
   }

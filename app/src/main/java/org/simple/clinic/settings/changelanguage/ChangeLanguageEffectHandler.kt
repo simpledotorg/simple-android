633ac13e2f53f552582599579db3e8f662f8f071
Vinay S Shenoy <vinay@obvious.in>
2019-10-08 16:04:35 +0530

    Clean up ChangeLanguageEffectHandler
    - Extract functions for individual effect handlers

--- app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageEffectHandler.kt
@@ -2,6 +2,7 @@ package org.simple.clinic.settings.changelanguage
 
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
 import org.simple.clinic.settings.SettingsRepository
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
@@ -14,37 +15,58 @@ object ChangeLanguageEffectHandler {
   ): ObservableTransformer<ChangeLanguageEffect, ChangeLanguageEvent> {
     return RxMobius
         .subtypeEffectHandler<ChangeLanguageEffect, ChangeLanguageEvent>()
-        .addTransformer(LoadCurrentSelectedLanguageEffect::class.java) { effectStream ->
-          effectStream
-              .flatMapSingle {
-                settingsRepository
-                    .getCurrentSelectedLanguage()
-                    .subscribeOn(schedulersProvider.io())
-              }
-              .map(::CurrentSelectedLanguageLoadedEvent)
-        }
-        .addTransformer(LoadSupportedLanguagesEffect::class.java) { effectStream ->
-          effectStream
-              .flatMapSingle {
-                settingsRepository
-                    .getSupportedLanguages()
-                    .subscribeOn(schedulersProvider.io())
-              }
-              .map(::SupportedLanguagesLoadedEvent)
-        }
-        .addTransformer(UpdateSelectedLanguageEffect::class.java) { effectStream ->
-          effectStream
-              .map { it.newLanguage }
-              .flatMapSingle { newLanguage ->
-                settingsRepository
-                    .setCurrentSelectedLanguage(newLanguage)
-                    .subscribeOn(schedulersProvider.io())
-                    .toSingleDefault(newLanguage)
-              }
-              .map(::SelectedLanguageChangedEvent)
-        }
+        .addTransformer(LoadCurrentSelectedLanguageEffect::class.java, loadCurrentSelectedLanguage(settingsRepository, schedulersProvider.io()))
+        .addTransformer(LoadSupportedLanguagesEffect::class.java, loadSupportedLanguages(settingsRepository, schedulersProvider.io()))
+        .addTransformer(UpdateSelectedLanguageEffect::class.java, updateSelectedLanguage(settingsRepository, schedulersProvider.io()))
         .addAction(RestartActivityEffect::class.java, activityRestarter::restart, schedulersProvider.ui())
         .build()
+  }
+
+  private fun loadCurrentSelectedLanguage(
+      settingsRepository: SettingsRepository,
+      scheduler: Scheduler
+  ): ObservableTransformer<LoadCurrentSelectedLanguageEffect, ChangeLanguageEvent> {
+    return ObservableTransformer { effectStream ->
+      effectStream
+          .flatMapSingle {
+            settingsRepository
+                .getCurrentSelectedLanguage()
+                .subscribeOn(scheduler)
+          }
+          .map(::CurrentSelectedLanguageLoadedEvent)
+    }
+  }
+
+  private fun loadSupportedLanguages(
+      settingsRepository: SettingsRepository,
+      scheduler: Scheduler
+  ): ObservableTransformer<LoadSupportedLanguagesEffect, ChangeLanguageEvent> {
+    return ObservableTransformer { effectStream ->
+      effectStream
+          .flatMapSingle {
+            settingsRepository
+                .getSupportedLanguages()
+                .subscribeOn(scheduler)
+          }
+          .map(::SupportedLanguagesLoadedEvent)
+    }
+  }
+
+  private fun updateSelectedLanguage(
+      settingsRepository: SettingsRepository,
+      scheduler: Scheduler
+  ): ObservableTransformer<UpdateSelectedLanguageEffect, ChangeLanguageEvent> {
+    return ObservableTransformer { effectStream ->
+      effectStream
+          .map { it.newLanguage }
+          .flatMapSingle { newLanguage ->
+            settingsRepository
+                .setCurrentSelectedLanguage(newLanguage)
+                .subscribeOn(scheduler)
+                .toSingleDefault(newLanguage)
+          }
+          .map(::SelectedLanguageChangedEvent)
+    }
   }
 
   interface ActivityRestarter {

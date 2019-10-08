cd5c69d23d5f16012a8b67bfcbb8c987c541c5a3
Ragunath Jawahar <ragunath@obvious.in>
2019-10-08 09:06:39 +0000

    Move `justEffect` out of `EditPatientUpdate`

--- app/src/main/java/org/simple/clinic/mobius/MobiusFoo.kt
+++ app/src/main/java/org/simple/clinic/mobius/MobiusFoo.kt
@@ -2,6 +2,11 @@ package org.simple.clinic.mobius
 
 import com.spotify.mobius.First
 import com.spotify.mobius.Init
+import com.spotify.mobius.Next
+import com.spotify.mobius.Next.dispatch
 
 fun <M, F> ((M) -> First<M, F>).toInit(): Init<M, F> =
     Init { this.invoke(it) }
+
+fun <M, F> justEffect(effect: F): Next<M, F> =
+    dispatch<M, F>(setOf(effect))

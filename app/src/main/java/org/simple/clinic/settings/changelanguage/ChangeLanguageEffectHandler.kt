8307ee8686a52467c16163664e003066341e84d9
Vinay S Shenoy <vinay@obvious.in>
2019-10-08 09:42:54 +0530

    Handle load current selected language effect

--- app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageEffectHandler.kt
@@ -2,13 +2,26 @@ package org.simple.clinic.settings.changelanguage
 
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.settings.SettingsRepository
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 object ChangeLanguageEffectHandler {
 
-  fun create(schedulersProvider: SchedulersProvider): ObservableTransformer<ChangeLanguageEffect, ChangeLanguageEvent> {
+  fun create(
+      schedulersProvider: SchedulersProvider,
+      settingsRepository: SettingsRepository
+  ): ObservableTransformer<ChangeLanguageEffect, ChangeLanguageEvent> {
     return RxMobius
         .subtypeEffectHandler<ChangeLanguageEffect, ChangeLanguageEvent>()
+        .addTransformer(LoadCurrentSelectedLanguageEffect::class.java) { effectStream ->
+          effectStream
+              .flatMapSingle {
+                settingsRepository
+                    .getCurrentSelectedLanguage()
+                    .subscribeOn(schedulersProvider.io())
+              }
+              .map(::CurrentSelectedLanguageLoadedEvent)
+        }
         .build()
   }
 }

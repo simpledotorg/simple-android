c06c415c57e8138ba426ee78f7cd330052f5bf43
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-08 09:42:54 +0530

    Handle load current selected language effect

--- app/src/test/java/org/simple/clinic/settings/changelanguage/ChangeLanguageEffectHandlerTest.kt
+++ app/src/test/java/org/simple/clinic/settings/changelanguage/ChangeLanguageEffectHandlerTest.kt
@@ -1,21 +1,48 @@
 package org.simple.clinic.settings.changelanguage
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Single
 import io.reactivex.observers.TestObserver
 import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
 import org.junit.After
+import org.junit.Test
+import org.simple.clinic.settings.SettingsRepository
+import org.simple.clinic.settings.SystemDefaultLanguage
 import org.simple.clinic.util.scheduler.TrampolineSchedulersProvider
 
 class ChangeLanguageEffectHandlerTest {
 
+  private val settingsRepository = mock<SettingsRepository>()
+
   private val effectsSubject: Subject<ChangeLanguageEffect> = PublishSubject.create<ChangeLanguageEffect>()
 
   private val testObserver: TestObserver<ChangeLanguageEvent> = effectsSubject
-      .compose(ChangeLanguageEffectHandler.create(TrampolineSchedulersProvider()))
+      .compose(ChangeLanguageEffectHandler.create(
+          schedulersProvider = TrampolineSchedulersProvider(),
+          settingsRepository = settingsRepository
+      ))
       .test()
 
   @After
   fun tearDown() {
     testObserver.dispose()
+  }
+
+  @Test
+  fun `the current selected language must be fetched when the load current selected effect is received`() {
+    // given
+    val selectedLanguage = SystemDefaultLanguage
+    whenever(settingsRepository.getCurrentSelectedLanguage()).thenReturn(Single.just(selectedLanguage))
+
+    // when
+    effectsSubject.onNext(LoadCurrentSelectedLanguageEffect)
+
+    // then
+    testObserver
+        .assertValue(CurrentSelectedLanguageLoadedEvent(selectedLanguage))
+        .assertNotComplete()
+        .assertNotTerminated()
   }
 }

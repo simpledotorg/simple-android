d5da6e8d18d0a892484cd4618252824027336ec8
Vinay S Shenoy <vinay@obvious.in>
2019-10-10 05:13:37 +0000

    Render the UI once the model has been initialized

--- app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageUi.kt
+++ app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageUi.kt
@@ -1,4 +1,12 @@
 package org.simple.clinic.settings.changelanguage
 
+import org.simple.clinic.settings.Language
+
 interface ChangeLanguageUi {
+
+  fun displayLanguages(supportedLanguages: List<Language>, selectedLanguage: Language?)
+
+  fun setDoneButtonDisabled()
+
+  fun setDoneButtonEnabled()
 }
--- app/src/test/java/org/simple/clinic/settings/changelanguage/ChangeLanguageUiRendererTest.kt
+++ app/src/test/java/org/simple/clinic/settings/changelanguage/ChangeLanguageUiRendererTest.kt
@@ -1,13 +1,21 @@
 package org.simple.clinic.settings.changelanguage
 
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
 import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import org.junit.Test
+import org.simple.clinic.settings.ProvidedLanguage
+import org.simple.clinic.settings.SystemDefaultLanguage
 
 class ChangeLanguageUiRendererTest {
 
   private val ui = mock<ChangeLanguageUi>()
   private val defaultModel = ChangeLanguageModel.FETCHING_LANGUAGES
+
+  private val englishIndia = ProvidedLanguage(displayName = "English", languageCode = "en_IN")
+  private val hindiIndia = ProvidedLanguage(displayName = "हिंदी", languageCode = "hi_IN")
+  private val supportedLanguages = listOf(englishIndia, hindiIndia)
 
   private val renderer = ChangeLanguageUiRenderer(ui)
 
@@ -18,5 +26,63 @@ class ChangeLanguageUiRendererTest {
 
     // then
     verifyZeroInteractions(ui)
+  }
+
+  @Test
+  fun `when the current language has not been fetched, do nothing`() {
+    // given
+    val model = defaultModel
+        .withSupportedLanguages(listOf(englishIndia, hindiIndia))
+
+    // when
+    renderer.render(model)
+
+    // then
+    verifyZeroInteractions(ui)
+  }
+
+  @Test
+  fun `when the supported languages have not been fetched, do nothing`() {
+    // given
+    val model = defaultModel
+        .withCurrentLanguage(englishIndia)
+
+    // when
+    renderer.render(model)
+
+    // then
+    verifyZeroInteractions(ui)
+  }
+
+  @Test
+  fun `if the user has selected a language, render the ui with the selected language`() {
+    // given
+    val model = defaultModel
+        .withSupportedLanguages(supportedLanguages)
+        .withCurrentLanguage(englishIndia)
+
+    // when
+    renderer.render(model)
+
+    // then
+    verify(ui).displayLanguages(supportedLanguages, englishIndia)
+    verify(ui).setDoneButtonEnabled()
+    verifyNoMoreInteractions(ui)
+  }
+
+  @Test
+  fun `if the user is using the system default language, render the ui without any selected language`() {
+    // given
+    val model = defaultModel
+        .withCurrentLanguage(SystemDefaultLanguage)
+        .withSupportedLanguages(supportedLanguages)
+
+    // when
+    renderer.render(model)
+
+    // then
+    verify(ui).displayLanguages(supportedLanguages, null)
+    verify(ui).setDoneButtonDisabled()
+    verifyNoMoreInteractions(ui)
   }
 }

5743dcdc727e7a4e35f4e670fe12dc4772360eb8
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-11 05:40:57 +0000

    Add ChangeLanguageListItem

---
+++ app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageListItem.kt
@@ -0,0 +1,42 @@
+package org.simple.clinic.settings.changelanguage
+
+import androidx.recyclerview.widget.DiffUtil
+import io.reactivex.subjects.Subject
+import kotlinx.android.synthetic.main.list_change_language_view.*
+import org.simple.clinic.R
+import org.simple.clinic.settings.Language
+import org.simple.clinic.settings.ProvidedLanguage
+import org.simple.clinic.widgets.ItemAdapter
+import org.simple.clinic.widgets.recyclerview.ViewHolderX
+
+data class ChangeLanguageListItem(
+    private val language: ProvidedLanguage,
+    private val isSelected: Boolean
+) : ItemAdapter.Item<ChangeLanguageListItem.Event> {
+
+  override fun layoutResId(): Int {
+    return R.layout.list_change_language_view
+  }
+
+  override fun render(holder: ViewHolderX, subject: Subject<Event>) {
+    holder.languageButton.apply {
+      text = language.displayName
+      isChecked = isSelected
+      setOnClickListener { subject.onNext(Event.ListItemClicked(language)) }
+    }
+  }
+
+  sealed class Event {
+    data class ListItemClicked(val language: Language) : Event()
+  }
+
+  class DiffCallback : DiffUtil.ItemCallback<ChangeLanguageListItem>() {
+    override fun areItemsTheSame(oldItem: ChangeLanguageListItem, newItem: ChangeLanguageListItem): Boolean {
+      return oldItem.language.languageCode == newItem.language.languageCode
+    }
+
+    override fun areContentsTheSame(oldItem: ChangeLanguageListItem, newItem: ChangeLanguageListItem): Boolean {
+      return oldItem == newItem
+    }
+  }
+}

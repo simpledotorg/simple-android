4270bac22aa639475922fc8369de8a185543c9d0
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-14 10:55:26 +0000

    Add effect to load current language in SettingsEffectHandler

--- app/src/main/java/org/simple/clinic/settings/SettingsUpdate.kt
+++ app/src/main/java/org/simple/clinic/settings/SettingsUpdate.kt
@@ -1,14 +1,16 @@
 package org.simple.clinic.settings
 
 import com.spotify.mobius.Next
-import com.spotify.mobius.Next.next
+import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
+import org.simple.clinic.mobius.next
 
 class SettingsUpdate : Update<SettingsModel, SettingsEvent, SettingsEffect> {
 
   override fun update(model: SettingsModel, event: SettingsEvent): Next<SettingsModel, SettingsEffect> {
     return when (event) {
       is UserDetailsLoaded -> next(model.userDetailsFetched(name = event.name, phoneNumber = event.phoneNumber))
+      else -> noChange()
     }
   }
 }

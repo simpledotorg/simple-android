e59db0173a56e95fe327f4e0e37f60d91ca52b99
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-14 04:04:43 +0000

    Implement fetching of current language

--- app/src/main/java/org/simple/clinic/settings/Language.kt
+++ app/src/main/java/org/simple/clinic/settings/Language.kt
@@ -2,11 +2,19 @@ package org.simple.clinic.settings
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import java.util.Locale
 
 sealed class Language : Parcelable
 
 @Parcelize
-data class ProvidedLanguage(val displayName: String, val languageCode: String) : Language()
+data class ProvidedLanguage(val displayName: String, val languageCode: String) : Language() {
+
+  fun matchesLocale(locale: Locale): Boolean {
+    val languageTag = locale.toLanguageTag()
+
+    return languageCode.equals(languageTag, ignoreCase = true)
+  }
+}
 
 @Parcelize
 object SystemDefaultLanguage : Language()
--- app/src/main/java/org/simple/clinic/settings/SettingsRepositoryImpl.kt
+++ app/src/main/java/org/simple/clinic/settings/SettingsRepositoryImpl.kt
@@ -1,12 +1,31 @@
 package org.simple.clinic.settings
 
+import com.f2prateek.rx.preferences2.Preference
 import io.reactivex.Completable
+import io.reactivex.Maybe
 import io.reactivex.Single
+import org.simple.clinic.util.Optional
+import org.simple.clinic.util.filterAndUnwrapJust
+import java.util.Locale
 
-class SettingsRepositoryImpl : SettingsRepository {
+class SettingsRepositoryImpl(
+    private val userSelectedLocalePreference: Preference<Optional<Locale>>,
+    private val supportedLanguages: List<Language>
+) : SettingsRepository {
 
   override fun getCurrentLanguage(): Single<Language> {
-    return Single.never()
+    return Single.just(userSelectedLocalePreference.get())
+        .filterAndUnwrapJust()
+        .flatMap(::findSupportedLanguageForLocale)
+        .toSingle(SystemDefaultLanguage)
+  }
+
+  private fun findSupportedLanguageForLocale(locale: Locale): Maybe<Language> {
+    return Maybe.fromCallable {
+      supportedLanguages
+          .filterIsInstance<ProvidedLanguage>()
+          .find { it.matchesLocale(locale) }
+    }
   }
 
   override fun getSupportedLanguages(): Single<List<Language>> {

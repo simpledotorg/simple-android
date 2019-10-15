ca2b80766f71051770edf4ce18f289f6dcfbeca9
Ragunath Jawahar <ragunath@obvious.in>
2019-10-15 09:26:12 +0000

    Create `PrefillDate` effect

--- app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryEffectHandler.kt
@@ -1,12 +1,32 @@
 package org.simple.clinic.bp.entry
 
-import io.reactivex.Observable
+import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.util.UserInputDatePaddingCharacter
+import org.simple.clinic.util.scheduler.SchedulersProvider
+import org.threeten.bp.LocalDate
 
 object BloodPressureEntryEffectHandler {
-  fun create(): ObservableTransformer<BloodPressureEntryEffect, BloodPressureEntryEvent> {
-    return ObservableTransformer {
-      Observable.never<BloodPressureEntryEvent>()
-    }
+  fun create(
+      ui: BloodPressureEntryUi,
+      inputDatePaddingCharacter: UserInputDatePaddingCharacter,
+      schedulersProvider: SchedulersProvider
+  ): ObservableTransformer<BloodPressureEntryEffect, BloodPressureEntryEvent> {
+    return RxMobius
+        .subtypeEffectHandler<BloodPressureEntryEffect, BloodPressureEntryEvent>()
+        .addConsumer(PrefillDate::class.java, { prefillDate(ui, it.date, inputDatePaddingCharacter) }, schedulersProvider.ui())
+        .build()
+  }
+
+  fun prefillDate(
+      ui: BloodPressureEntryUi,
+      date: LocalDate,
+      inputDatePaddingCharacter: UserInputDatePaddingCharacter
+  ) {
+    val dayString = date.dayOfMonth.toString().padStart(length = 2, padChar = inputDatePaddingCharacter.value)
+    val monthString = date.monthValue.toString().padStart(length = 2, padChar = inputDatePaddingCharacter.value)
+    val yearString = date.year.toString().substring(startIndex = 2, endIndex = 4)
+    ui.setDateOnInputFields(dayString, monthString, yearString)
+    ui.showDateOnDateButton(date)
   }
 }

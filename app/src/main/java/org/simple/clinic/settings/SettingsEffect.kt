9916c452471e9ccce95ea45573008e774e367757
Vinay S Shenoy <vinay@obvious.in>
2019-10-15 05:09:14 +0000

    Open change language screen from Settings

--- app/src/main/java/org/simple/clinic/settings/SettingsEffect.kt
+++ app/src/main/java/org/simple/clinic/settings/SettingsEffect.kt
@@ -5,3 +5,5 @@ sealed class SettingsEffect
 object LoadUserDetailsEffect : SettingsEffect()
 
 object LoadCurrentLanguageEffect : SettingsEffect()
+
+object OpenLanguageSelectionScreenEffect: SettingsEffect()

31e27b388a471e42b4a4c4564d9d9a83140e60b8
Vinay S Shenoy <vinay@obvious.in>
2019-10-15 04:08:06 +0000

    Render the current language on the Settings screen

--- app/src/main/java/org/simple/clinic/settings/SettingsUi.kt
+++ app/src/main/java/org/simple/clinic/settings/SettingsUi.kt
@@ -1,5 +1,10 @@
 package org.simple.clinic.settings
 
 interface SettingsUi {
+
   fun displayUserDetails(name: String, phoneNumber: String)
+
+  fun displayCurrentLanguage(language: String)
+
+  fun setChangeLanguageButtonVisible()
 }

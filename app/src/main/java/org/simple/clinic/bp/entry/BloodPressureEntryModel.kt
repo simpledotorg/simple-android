c46c8720f2f945d5f91f82680b1d9ee0c9bd2b13
Ragunath Jawahar <ragunath@obvious.in>
2019-10-15 11:36:08 +0530

    Add a factory function to `BloodPressureEntryModel`

--- app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryModel.kt
+++ app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryModel.kt
@@ -1,4 +1,13 @@
 package org.simple.clinic.bp.entry
 
-class BloodPressureEntryModel {
+import org.simple.clinic.bp.entry.OpenAs.New
+import java.util.UUID
+
+data class BloodPressureEntryModel(
+    val openAs: OpenAs
+) {
+  companion object {
+    fun newBloodPressureEntry(patientUuid: UUID): BloodPressureEntryModel =
+        BloodPressureEntryModel(New(patientUuid))
+  }
 }

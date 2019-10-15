ec2583b21f041f0843fa1a13e5069f3793b1a58f
Vinay S Shenoy <vinay@obvious.in>
2019-10-15 05:39:39 +0000

    Wire settings and language selection screens

---
+++ app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageScreenKey.kt
@@ -0,0 +1,15 @@
+package org.simple.clinic.settings.changelanguage
+
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.R
+import org.simple.clinic.router.screen.FullScreenKey
+
+@Parcelize
+class ChangeLanguageScreenKey : FullScreenKey {
+
+  @IgnoredOnParcel
+  override val analyticsName: String = "Language Selection"
+
+  override fun layoutRes(): Int = R.layout.screen_change_language
+}

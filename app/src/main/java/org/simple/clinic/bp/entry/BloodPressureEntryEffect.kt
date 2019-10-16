7ee63d88d667d234f2a9efa169bbf4e827eb2df2
Ragunath Jawahar <ragunath@obvious.in>
2019-10-16 07:33:08 +0000

    Move `DiastolicBackspaceClicked` logic to Mobius

--- app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryEffect.kt
+++ app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryEffect.kt
@@ -7,3 +7,7 @@ object PrefillDateForNewEntry : BloodPressureEntryEffect()
 object HideBpErrorMessage : BloodPressureEntryEffect()
 
 object ChangeFocusToDiastolic : BloodPressureEntryEffect()
+
+object ChangeFocusToSystolic : BloodPressureEntryEffect()
+
+data class SetSystolic(val systolic: String) : BloodPressureEntryEffect()
--- app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryModel.kt
+++ app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryModel.kt
@@ -7,7 +7,9 @@ import org.simple.clinic.bp.entry.OpenAs.Update
 
 @Parcelize
 data class BloodPressureEntryModel(
-    val openAs: OpenAs
+    val openAs: OpenAs,
+    val systolic: String = "",
+    val diastolic: String = ""
 ) : Parcelable {
   companion object {
     fun newBloodPressureEntry(openAsNew: New): BloodPressureEntryModel =
@@ -16,4 +18,20 @@ data class BloodPressureEntryModel(
     fun updateBloodPressureEntry(openAsUpdate: Update): BloodPressureEntryModel =
         BloodPressureEntryModel(openAsUpdate)
   }
+
+  fun withSystolic(systolic: String): BloodPressureEntryModel =
+      copy(systolic = systolic)
+
+  fun withDiastolic(diastolic: String): BloodPressureEntryModel =
+      copy(diastolic = diastolic)
+
+  fun deleteDiastolicLastDigit(): BloodPressureEntryModel = if (diastolic.isNotEmpty())
+    copy(diastolic = diastolic.substring(0, diastolic.length - 1))
+  else
+    this
+
+  fun deleteSystolicLastDigit(): BloodPressureEntryModel = if (systolic.isNotEmpty())
+    copy(systolic = systolic.substring(0, systolic.length - 1))
+  else
+    this
 }

af582108e8b9c9a1127589934ee429d4384a5541
Ragunath Jawahar <ragunath@obvious.in>
2019-10-16 07:42:51 +0530

    Deprecate classes in Mobius migration package and remove unused classes

--- app/src/main/java/org/simple/clinic/mobius/migration/Architecture.kt
+++ app/src/main/java/org/simple/clinic/mobius/migration/Architecture.kt
@@ -1,5 +1,8 @@
 package org.simple.clinic.mobius.migration
 
+@Deprecated("""
+  We are no longer using this approach to migrate towards Mobius.
+  This package will be deleted as soon as Edit Patient feature becomes stable.""")
 enum class Architecture(val analyticsName: String) {
   ORIGINAL("original"),
   MOBIUS("mobius")
--- app/src/main/java/org/simple/clinic/mobius/migration/RevertToCommit.kt
+++
@@ -1,7 +0,0 @@
-package org.simple.clinic.mobius.migration
-
-@Retention(AnnotationRetention.SOURCE)
-annotation class RevertToCommit(
-    val withMessage: String,
-    val afterCommitId: String
-)

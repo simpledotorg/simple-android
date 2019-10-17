9cdf35b368678d2e4e55aa94953beec2ebc8c72c
Ragunath Jawahar <ragunath@obvious.in>
2019-10-17 15:47:51 +0530

    Unify `justEffects` and `justEffect` and call it `dispatch`

--- app/src/main/java/org/simple/clinic/onboarding/OnboardingUpdate.kt
+++ app/src/main/java/org/simple/clinic/onboarding/OnboardingUpdate.kt
@@ -2,13 +2,13 @@ package org.simple.clinic.onboarding
 
 import com.spotify.mobius.Next
 import com.spotify.mobius.Update
-import org.simple.clinic.mobius.justEffect
+import org.simple.clinic.mobius.dispatch
 
 class OnboardingUpdate : Update<OnboardingModel, OnboardingEvent, OnboardingEffect> {
   override fun update(model: OnboardingModel, event: OnboardingEvent): Next<OnboardingModel, OnboardingEffect> {
     return when (event) {
-      GetStartedClicked -> justEffect(CompleteOnboardingEffect)
-      OnboardingCompleted -> justEffect(MoveToRegistrationEffect)
+      GetStartedClicked -> dispatch(CompleteOnboardingEffect)
+      OnboardingCompleted -> dispatch(MoveToRegistrationEffect)
     }
   }
 }

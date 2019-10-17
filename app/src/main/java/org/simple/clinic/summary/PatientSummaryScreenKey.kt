a13c0331a26fed0e26fadb8975f67295ddf6ee59
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-17 10:08:22 +0530

    Exclude screen created timestamp from comparison in summary screen key
    Flow short circuits the changing of the view if the incoming key is
    equal to the key of the current view. However, this particular screen
    includes a screen created timestamp, which is an `Instant`, as a
    property.
    
    Ideally, this should be a property of the summary screen, and saved and
    restored as part of the view restoration mechanism, but the older
    architecture style sets up the screen before it gets attached to the
    window, so the state restoration cannot happen before the screen is
    setup.
    
    This property causes issues with the current architecture which
    sometimes pushes multiple keys simultaneously to Flow. The correct
    solution is to move to Mobius and move this property to the UI model,
    but as a stop gap solution, commit excludes the screen created timestamp
    from being used for the equals() and hashcode() methods so that it does
    not break Flow's short-circuiting behaviour.

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryScreenKey.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryScreenKey.kt
@@ -6,12 +6,14 @@ import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.R
 import org.simple.clinic.router.screen.FullScreenKey
 import org.threeten.bp.Instant
+import java.util.Objects
 import java.util.UUID
 
 @Parcelize
 data class PatientSummaryScreenKey(
     val patientUuid: UUID,
     val intention: OpenIntention,
+    // TODO(vs): 2019-10-18 Move this to the UI model when migrating to Mobius
     val screenCreatedTimestamp: Instant
 ) : FullScreenKey, Parcelable {
 
@@ -20,5 +22,21 @@ data class PatientSummaryScreenKey(
 
   override fun layoutRes(): Int {
     return R.layout.screen_patient_summary
+  }
+
+  override fun equals(other: Any?): Boolean {
+    return when {
+      this === other -> true
+      other == null || this.javaClass != other.javaClass -> false
+      else -> {
+        val that = other as PatientSummaryScreenKey
+
+        patientUuid == that.patientUuid && intention == that.intention
+      }
+    }
+  }
+
+  override fun hashCode(): Int {
+    return Objects.hash(patientUuid, intention)
   }
 }

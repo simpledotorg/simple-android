99595f7e4bde0cd70d2038749f970c36da1db29b
Ragunath Jawahar <ragunath@obvious.in>
2019-10-17 15:27:56 +0530

    Make `justEffect` internally call `justEffects`

--- mobius-base/src/main/java/org/simple/clinic/mobius/MobiusFoo.kt
+++ mobius-base/src/main/java/org/simple/clinic/mobius/MobiusFoo.kt
@@ -4,7 +4,7 @@ import com.spotify.mobius.Next
 import com.spotify.mobius.Next.dispatch
 
 fun <M, F> justEffect(effect: F): Next<M, F> =
-    dispatch<M, F>(setOf(effect))
+    justEffects(effect)
 
 fun <M, F> next(model: M, vararg effects: F): Next<M, F> = if (effects.isEmpty()) {
   Next.next(model)

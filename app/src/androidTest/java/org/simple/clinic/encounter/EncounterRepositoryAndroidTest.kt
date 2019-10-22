087c42b893f8479d0cb921183812f408678ada8d
Sanchita Agarwal <sanchita@obvious.in>
2019-10-22 18:22:18 +0530

    Use ObservationsForEncounter instead of Encounter to sync
    We cannot use Encounter entity directly to sync encounters.
    This is because we also need to fetch corresponding BPs for each encounter.
    Using @Relation in ObservationsForEncounter we were able to fetch the list
    of BPs for every encounter.

--- app/src/androidTest/java/org/simple/clinic/encounter/EncounterRepositoryAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/encounter/EncounterRepositoryAndroidTest.kt
@@ -44,11 +44,10 @@ class EncounterRepositoryAndroidTest {
   @Test
   fun record_count_should_return_the_count_of_no_of_encounters() {
     //given
-    repository.save(listOf(testData.encounter(
-        uuid = UUID.fromString("306b0298-a04d-4680-a55f-9c8696512aa1"),
-        patientUuid = UUID.fromString("2ffd2254-4a22-4b1a-9abf-841f5ae5bea3"),
-        facilityUuid = UUID.fromString("05e105d8-8a62-4b85-ba3a-319ad742ceb0")
-    ))).blockingAwait()
+    repository.save(listOf(testData.observationsForEncounter(testData.encounter(
+        uuid = UUID.fromString("6f70d92d-340e-42ea-8274-ccbf3b174d7c"),
+        patientUuid = UUID.fromString("c25805f2-033b-45ad-bd75-45a2c3d29e87")
+    )))).blockingAwait()
 
     //when
     val count = repository.recordCount().blockingFirst()
@@ -63,22 +62,39 @@ class EncounterRepositoryAndroidTest {
     val patientUuid = UUID.fromString("c2273c6c-036b-41f0-a128-84d5551be3ce")
     val encounterUuid1 = UUID.fromString("0b144e4a-ce13-431d-93f1-1d10d0639c09")
     val encounterUuid2 = UUID.fromString("0799e067-6b14-4e45-8171-e6a9d84626fb")
+    val bpUuid1 = UUID.fromString("0347a91f-8776-418d-a117-7cf6a4ae0713")
+    val bpUuid2 = UUID.fromString("cd90ca58-b8ea-413c-bdb5-c5941b943f96")
 
-    val encountersPayload1 = testData.encounterPayload(uuid = encounterUuid1, patientUuid = patientUuid, updatedAt = Instant.parse("2018-02-11T00:00:00Z"))
-    val encountersPayload2 = testData.encounterPayload(uuid = encounterUuid2, patientUuid = patientUuid)
+    val bpPayloads1 = listOf(testData.bpPayload(uuid = bpUuid1, patientUuid = patientUuid))
+    val bpPayloads2 = listOf(testData.bpPayload(uuid = bpUuid2, patientUuid = patientUuid))
 
-    repository.save(listOf(testData.encounter(uuid = encounterUuid1, patientUuid = patientUuid, syncStatus = SyncStatus.PENDING, updatedAt = Instant.parse("2018-02-13T00:00:00Z")))).blockingAwait()
+    val encountersPayload1 = testData.encounterPayload(uuid = encounterUuid1, patientUuid = patientUuid, updatedAt = Instant.parse("2018-02-11T00:00:00Z"), bpPayloads = bpPayloads1)
+    val encountersPayload2 = testData.encounterPayload(uuid = encounterUuid2, patientUuid = patientUuid, bpPayloads = bpPayloads2)
+
+    val encounter1 = testData.encounter(
+        uuid = encounterUuid1,
+        patientUuid = patientUuid,
+        syncStatus = SyncStatus.PENDING,
+        updatedAt = Instant.parse("2018-02-13T00:00:00Z")
+    )
+    repository.save(listOf(testData.observationsForEncounter(
+        encounter = encounter1
+    ))).blockingAwait()
 
     //when
     repository.mergeWithLocalData(listOf(encountersPayload1, encountersPayload2)).blockingAwait()
-    val encounter = repository.recordsWithSyncStatus(SyncStatus.DONE).blockingGet()
-    val encounterPending = repository.recordsWithSyncStatus(SyncStatus.PENDING).blockingGet()
+    val observationsForEncounters = repository.recordsWithSyncStatus(SyncStatus.DONE).blockingGet()
+    val pendingObservationsForEncounters = repository.recordsWithSyncStatus(SyncStatus.PENDING).blockingGet()
 
     //then
-    assertThat(encounter.size).isEqualTo(1)
-    assertThat(encounter.first().uuid).isEqualTo(encounterUuid2)
-
-    with(encounterPending.first()) {
+    assertThat(observationsForEncounters.size).isEqualTo(1)
+    with(observationsForEncounters.first()) {
+      assertThat(encounter.uuid).isEqualTo(encounterUuid2)
+      assertThat(bloodPressures.size).isEqualTo(1)
+      assertThat(bloodPressures.first().uuid).isEqualTo(bpUuid2)
+    }
+    assertThat(pendingObservationsForEncounters.size).isEqualTo(1)
+    with(pendingObservationsForEncounters.first().encounter) {
       assertThat(uuid).isEqualTo(encounterUuid1)
       assertThat(updatedAt).isEqualTo(Instant.parse("2018-02-13T00:00:00Z"))
     }
--- app/src/main/java/org/simple/clinic/storage/Databases.kt
+++ app/src/main/java/org/simple/clinic/storage/Databases.kt
@@ -2,6 +2,10 @@ package org.simple.clinic.storage
 
 import androidx.sqlite.db.SupportSQLiteDatabase
 
+/**
+ * This extension function is to be used in Room Migrations only.
+ * For running database queries in a transaction otherwise, use [RoomDatabase#runInTransaction(Runnable)]
+ **/
 fun SupportSQLiteDatabase.inTransaction(block: SupportSQLiteDatabase.() -> Unit) {
   beginTransaction()
   try {

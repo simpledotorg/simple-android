5b22c1aad5668043ce7bf7d3f1568aac5b1611ae
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-22 15:05:57 +0530

    Load OnboardingScreen in SetupActivity
    (cherry picked from commit c09ce62b5c11390434ac4c480773b070bb6cedcb)

--- app/src/main/java/org/simple/clinic/setup/SetupActivityComponent.kt
+++ app/src/main/java/org/simple/clinic/setup/SetupActivityComponent.kt
@@ -3,9 +3,10 @@ package org.simple.clinic.setup
 import dagger.Subcomponent
 import org.simple.clinic.activity.BindsActivity
 import org.simple.clinic.activity.BindsScreenRouter
+import org.simple.clinic.onboarding.OnboardingScreenInjector
 
 @Subcomponent
-interface SetupActivityComponent {
+interface SetupActivityComponent: OnboardingScreenInjector {
 
   fun inject(target: SetupActivity)
 

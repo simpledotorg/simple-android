0b04482880d1b665c4e083788e8d4d5282561a00
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-22 15:59:08 +0530

    Improve injecting of screens

--- app/src/main/java/org/simple/clinic/onboarding/OnboardingScreenInjector.kt
+++ app/src/main/java/org/simple/clinic/onboarding/OnboardingScreenInjector.kt
@@ -1,9 +1,5 @@
 package org.simple.clinic.onboarding
 
 interface OnboardingScreenInjector {
-  companion object {
-    const val INJECTOR_KEY = "OnboardingScreenInjector"
-  }
-
   fun inject(target: OnboardingScreen)
 }

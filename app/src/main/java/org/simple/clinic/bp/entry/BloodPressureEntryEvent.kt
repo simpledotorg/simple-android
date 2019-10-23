b3ce980c31ebd0ecf70c2faed3f5ad851ba92b96
Ragunath Jawahar <ragunath@obvious.in>
2019-10-23 11:36:11 +0530

    Remove unused events

--- app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryEvent.kt
+++ app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryEvent.kt
@@ -3,8 +3,6 @@ package org.simple.clinic.bp.entry
 import org.simple.clinic.bp.BloodPressureMeasurement
 import org.simple.clinic.bp.entry.BloodPressureEntrySheet.ScreenType
 import org.simple.clinic.widgets.UiEvent
-import org.simple.clinic.widgets.ageanddateofbirth.UserInputDateValidator
-import org.threeten.bp.LocalDate
 
 sealed class BloodPressureEntryEvent : UiEvent
 
@@ -28,10 +26,6 @@ object RemoveClicked : BloodPressureEntryEvent() {
 
 object DiastolicBackspaceClicked : BloodPressureEntryEvent()
 
-data class BloodPressureReadingsValidated(val result: BpValidator.Validation) : BloodPressureEntryEvent()
-
-data class DateValidated(val date: String, val result: UserInputDateValidator.Result) : BloodPressureEntryEvent()
-
 data class ScreenChanged(val type: ScreenType) : BloodPressureEntryEvent()
 
 data class DayChanged(val day: String) : BloodPressureEntryEvent()
@@ -39,10 +33,6 @@ data class DayChanged(val day: String) : BloodPressureEntryEvent()
 data class MonthChanged(val month: String) : BloodPressureEntryEvent()
 
 data class YearChanged(val twoDigitYear: String) : BloodPressureEntryEvent()
-
-data class DateChanged(val date: String) : BloodPressureEntryEvent()
-
-data class DateToPrefillCalculated(val date: LocalDate) : BloodPressureEntryEvent()
 
 data class BloodPressureSaved(val wasDateChanged: Boolean) : BloodPressureEntryEvent() {
   override val analyticsName = when {

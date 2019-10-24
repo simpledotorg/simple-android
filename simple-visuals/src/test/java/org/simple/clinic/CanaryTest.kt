3b3898fb733584bb2c1db636bb8d02470e6bda23
Ragunath Jawahar <ragunath@obvious.in>
2019-10-24 12:19:40 +0530

    Add a `visuals` module

---
+++ simple-visuals/src/test/java/org/simple/clinic/CanaryTest.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class CanaryTest {
+  @Test
+  fun `test environment is setup`() {
+    assertThat(true)
+        .isTrue()
+  }
+}

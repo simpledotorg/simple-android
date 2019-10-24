3f030f9d25a132caba214dd0b21ff4d404489442
Ragunath Jawahar <ragunath@obvious.in>
2019-10-24 14:03:44 +0530

    Terminate after all events are emitted
    (cherry picked from commit 8d030bab21a888e2eb0ff4192bf3d088335947ec)

--- simple-visuals/src/main/java/org/simple/clinic/await/Await.kt
+++ simple-visuals/src/main/java/org/simple/clinic/await/Await.kt
@@ -5,15 +5,25 @@ import io.reactivex.Scheduler
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
+private typealias Elapsed = Int
+private typealias Delay = Int
+
 class Await(
     private val delayMillis: List<Int>,
     private val scheduler: Scheduler = Schedulers.computation()
 ) {
   fun events(): Observable<Unit> {
+    val initialElapsedDelay: Pair<Elapsed, Delay> = 0 to 0
+
     return Observable
         .fromIterable(delayMillis)
-        .scan(0, { delaySoFar, delay -> delay - delaySoFar })
+        .scan(initialElapsedDelay, { elapsedDelayPair, itemDelay ->
+          val (elapsed, _) = elapsedDelayPair
+          itemDelay to itemDelay - elapsed
+        })
+        .map { (_, delay) -> delay }
         .skip(1)
-        .concatMap { Observable.timer(it.toLong(), TimeUnit.MILLISECONDS, scheduler).map { Unit } }
+        .map { it.toLong() }
+        .concatMap { delay -> Observable.timer(delay, TimeUnit.MILLISECONDS, scheduler).map { Unit } }
   }
 }

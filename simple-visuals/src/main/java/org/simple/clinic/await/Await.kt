ad27c7a9d1043ce5c23fc2544a38395902717c8d
Ragunath Jawahar <ragunath@obvious.in>
2019-10-24 16:06:00 +0530

    Change `Timing` to `TimingInMillis` to be explicit about the unit
    (cherry picked from commit 84eec2ee74cfe2d1d066843d8a6e52e1b6881088)

--- simple-visuals/src/main/java/org/simple/clinic/await/Await.kt
+++ simple-visuals/src/main/java/org/simple/clinic/await/Await.kt
@@ -3,15 +3,15 @@ package org.simple.clinic.await
 import io.reactivex.Observable
 import io.reactivex.Scheduler
 import io.reactivex.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 private typealias Delay = Int
-typealias Timing = Int
+typealias TimingInMillis = Int
 
-data class Checkpoint<T>(val item: T, val timing: Timing) {
+data class Checkpoint<T>(val item: T, val timingInMillis: TimingInMillis) {
   companion object {
-    fun unit(timing: Timing): Checkpoint<Unit> =
-        Checkpoint(Unit, timing)
+    fun unit(timingInMillis: TimingInMillis): Checkpoint<Unit> =
+        Checkpoint(Unit, timingInMillis)
   }
 }
 
@@ -21,26 +21,26 @@ class Await<T>(
 ) {
   fun items(): Observable<T> {
     val initialValue: Pair<Checkpoint<T?>, Delay> = Checkpoint(null as T?, 0) to 0
-    val sortedCheckpoints = checkpoints.sortedBy { it.timing }
+    val sortedCheckpoints = checkpoints.sortedBy { it.timingInMillis }
     assertNoDuplicateTimings(sortedCheckpoints)
 
     return Observable
         .fromIterable(sortedCheckpoints)
         .scan(initialValue, { elapsedDelayPair, checkpoint ->
           val (previousCheckpoint, _) = elapsedDelayPair
-          checkpoint as Checkpoint<T?> to checkpoint.timing - previousCheckpoint.timing
+          checkpoint as Checkpoint<T?> to checkpoint.timingInMillis - previousCheckpoint.timingInMillis
         })
         .skip(1)
         .concatMap { (checkpoint, delay) ->
           Observable
-              .timer(delay.toLong(), TimeUnit.MILLISECONDS, scheduler)
+              .timer(delay.toLong(), MILLISECONDS, scheduler)
               .map { checkpoint.item }
         }
   }
 
   private fun assertNoDuplicateTimings(checkpoints: List<Checkpoint<T>>) {
     checkpoints
-        .map { it.timing }
+        .map { it.timingInMillis }
         .foldRightIndexed(0) { index, timing, previousTiming ->
           if (timing == previousTiming && index != 0) {
             throw IllegalArgumentException("Found duplicate timing '$timing'. Duplicate timing values are not allowed.")

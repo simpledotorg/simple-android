68a80ba2d4ea9bf336825bd1df81e2217595a9a8
Vinay S Shenoy <vinay@obvious.in>
2019-10-24 18:24:58 +0530

    Use prepared statement to insert into Encounter

--- app/src/main/java/org/simple/clinic/storage/Migration_49_50.kt
+++ app/src/main/java/org/simple/clinic/storage/Migration_49_50.kt
@@ -30,67 +30,74 @@ class Migration_49_50 : Migration(49, 50) {
     val tableName = "BloodPressureMeasurement"
 
     database.inTransaction {
+
       //Using a random UUID to give a default value to non-null encounterUuid temporarily.
       //The encounterUuid will be replaced with the correct value when the query executes in next line.
       database.execSQL(""" ALTER TABLE $tableName ADD COLUMN "encounterUuid" TEXT NOT NULL DEFAULT '05f9c798-1701-4379-b14a-b1b18d937a33'  """)
-      database.compileStatement("""UPDATE $tableName SET "encounterUuid" = ? WHERE "uuid" = ?  """)
-          .use { statement ->
-            query(""" SELECT * FROM $tableName """)
-                .use { cursor ->
 
-                  generateSequence { cursor.takeIf { it.moveToNext() } }
-                      .map { it.string("uuid") to it }
-                      .forEach { (uuid, cursorRow) ->
-                        val patientUuid = cursor.string("patientUuid")
+      database.compileStatement("""INSERT OR IGNORE INTO "Encounter" VALUES (?, ?, ?, ?, ?, NULL)""")
+          .use { insertIntoEncounter ->
 
-                        val bpRecordedAt = instantConverter.toInstant(cursorRow.string("recordedAt"))!!
-                        val encounteredOn = bpRecordedAt.toLocalDateAtZone(userClock.zone)
+            database.compileStatement("""UPDATE $tableName SET "encounterUuid" = ? WHERE "uuid" = ?  """)
+                .use { updateBpWithEncounter ->
 
-                        val encounterId = generateEncounterUuid(
-                            facilityUuid = UUID.fromString(cursorRow.string("facilityUuid")),
-                            patientUuid = UUID.fromString(patientUuid),
-                            encounteredDate = encounteredOn
-                        ).toString()
+                  query(""" SELECT * FROM $tableName """)
+                      .use { cursor ->
 
-                        // This migration was edited after it went to production. The first version
-                        // of the migration had a bug where BPs which were deleted would end up
-                        // generating migrations where the deletedAt property was set to the string
-                        // "null" instead of SQL NULL value.
-                        //
-                        // We fixed this migration, but some users did end up running this migration
-                        // and would have encounters which should technically be deleted, but still
-                        // show up in the UI because the deletedAt field is non-null.
-                        //
-                        // We need to add another migration to delete those encounters.
-                        // TODO(vs): 2019-10-24 Add a migration for deleting the encounters with "null" deletedAt
+                        generateSequence { cursor.takeIf { it.moveToNext() } }
+                            .map { it.string("uuid") to it }
+                            .forEach { (uuid, cursorRow) ->
+                              val patientUuid = cursor.string("patientUuid")
 
-                        val bpDeletedAt = cursorRow.string("deletedAt")
+                              val bpRecordedAt = instantConverter.toInstant(cursorRow.string("recordedAt"))!!
+                              val encounteredOn = bpRecordedAt.toLocalDateAtZone(userClock.zone)
 
-                        // Technically, we should generate an encounter with deletedAt != null for a
-                        // deleted BP. However, these will never be show in the UI and the server
-                        // will eventually send those encounters to us when we sync.
-                        //
-                        // For convenience, we can skip generating these encounters for us.
-                        if (bpDeletedAt == null) {
-                          execSQL("""
-                            INSERT OR IGNORE INTO "Encounter" 
-                            VALUES (
-                              '$encounterId',
-                              '$patientUuid',
-                              '${localDateConverter.fromLocalDate(encounteredOn)}',
-                              '${cursorRow.string("createdAt")}',
-                              '${cursorRow.string("updatedAt")}',
-                              NULL
-                            )
-                            """
-                          )
-                        }
+                              val encounterId = generateEncounterUuid(
+                                  facilityUuid = UUID.fromString(cursorRow.string("facilityUuid")),
+                                  patientUuid = UUID.fromString(patientUuid),
+                                  encounteredDate = encounteredOn
+                              ).toString()
 
-                        statement.bindString(1, encounterId)
-                        statement.bindString(2, uuid)
-                        statement.executeUpdateDelete()
+                              // This migration was edited after it went to production. The first version
+                              // of the migration had a bug where BPs which were deleted would end up
+                              // generating migrations where the deletedAt property was set to the string
+                              // "null" instead of SQL NULL value.
+                              //
+                              // We fixed this migration, but some users did end up running this migration
+                              // and would have encounters which should technically be deleted, but still
+                              // show up in the UI because the deletedAt field is non-null.
+                              //
+                              // We need to add another migration to delete those encounters.
+                              // TODO(vs): 2019-10-24 Add a migration for deleting the encounters with "null" deletedAt
+
+                              val bpDeletedAt = cursorRow.string("deletedAt")
+
+                              // Technically, we should generate an encounter with deletedAt != null for a
+                              // deleted BP. However, these will never be show in the UI and the server
+                              // will eventually send those encounters to us when we sync.
+                              //
+                              // For convenience, we can skip generating these encounters for us.
+                              if (bpDeletedAt == null) {
+                                with(insertIntoEncounter) {
+                                  bindString(1, encounterId)
+                                  bindString(2, patientUuid)
+                                  bindString(3, localDateConverter.fromLocalDate(encounteredOn))
+                                  bindString(4, cursorRow.string("createdAt"))
+                                  bindString(5, cursorRow.string("updatedAt"))
+
+                                  executeInsert()
+                                }
+                              }
+
+                              with(updateBpWithEncounter) {
+                                bindString(1, encounterId)
+                                bindString(2, uuid)
+                                executeUpdateDelete()
+                              }
+                            }
                       }
                 }
+
           }
     }
   }

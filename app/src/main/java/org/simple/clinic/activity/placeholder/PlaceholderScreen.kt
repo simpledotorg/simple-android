69d9445c1c7ec7fe3174d8303028d727dd0e88f4
Ragunath Jawahar <ragunath@obvious.in>
2019-10-25 18:34:26 +0530

    Show progress UI after a delay of 3 seconds
    (cherry picked from commit d099c338dbad2ab82531ea9139f9f19a2b2f22bc)

--- app/src/main/java/org/simple/clinic/activity/placeholder/PlaceholderScreen.kt
+++ app/src/main/java/org/simple/clinic/activity/placeholder/PlaceholderScreen.kt
@@ -3,12 +3,39 @@ package org.simple.clinic.activity.placeholder
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.RelativeLayout
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.screen_placeholder.view.*
+import org.simple.clinic.await.Await
+import org.simple.clinic.await.Checkpoint
+import java.util.concurrent.TimeUnit.SECONDS
 
 class PlaceholderScreen(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
+  private val delayToShowMessage = SECONDS.toMillis(3).toInt()
+  private val await = Await(listOf(Checkpoint.unit(delayToShowMessage)))
+  private var awaitDisposable: Disposable? = null
+
   override fun onFinishInflate() {
     super.onFinishInflate()
     if (isInEditMode) {
       return
     }
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    awaitDisposable = await.items()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe { showLoadingUi() }
+  }
+
+  override fun onDetachedFromWindow() {
+    awaitDisposable?.dispose()
+    super.onDetachedFromWindow()
+  }
+
+  private fun showLoadingUi() {
+    loadingTextLayout.visibility = VISIBLE
+    loadingProgressBar.visibility = VISIBLE
   }
 }

20911776d91c441408920fc17525c38e662ad7b7
Vinay S Shenoy <code@vinaysshenoy.com>
2019-10-26 09:02:07 +0530

    Handle initialize database effect in SetupActivity

--- app/src/main/java/org/simple/clinic/setup/SetupActivityEvent.kt
+++ app/src/main/java/org/simple/clinic/setup/SetupActivityEvent.kt
@@ -3,3 +3,5 @@ package org.simple.clinic.setup
 sealed class SetupActivityEvent
 
 data class UserDetailsFetched(val hasUserCompletedOnboarding: Boolean) : SetupActivityEvent()
+
+object DatabaseInitialized: SetupActivityEvent()

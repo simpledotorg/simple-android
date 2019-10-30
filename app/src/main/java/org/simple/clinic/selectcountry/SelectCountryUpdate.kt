2ee8fce90e55e20cb3d1bd897daf2be1e9efb685
Rakshak R.Hegde <rakshak@obvious.in>
2019-10-30 13:06:41 +0530

    Handle ManifestFetched in SelectCountryUpdate

---
+++ app/src/main/java/org/simple/clinic/selectcountry/SelectCountryUpdate.kt
@@ -0,0 +1,14 @@
+package org.simple.clinic.selectcountry
+
+import com.spotify.mobius.Next
+import com.spotify.mobius.Next.next
+import com.spotify.mobius.Update
+
+class SelectCountryUpdate : Update<SelectCountryModel, SelectCountryEvent, SelectCountryEffect> {
+
+  override fun update(model: SelectCountryModel, event: SelectCountryEvent): Next<SelectCountryModel, SelectCountryEffect> {
+    return when (event) {
+      is ManifestFetched -> next(model.withSupportedCountries(event.countries))
+    }
+  }
+}

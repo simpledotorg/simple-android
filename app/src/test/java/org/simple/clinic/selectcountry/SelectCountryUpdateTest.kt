acf6598ffcd7d23b0e200f5735c682081dd79ccf
Rakshak R.Hegde <rakshak@obvious.in>
2019-10-30 13:06:41 +0530

    Handle ManifestFetched in SelectCountryUpdate

---
+++ app/src/test/java/org/simple/clinic/selectcountry/SelectCountryUpdateTest.kt
@@ -0,0 +1,41 @@
+package org.simple.clinic.selectcountry
+
+import com.spotify.mobius.test.NextMatchers.hasModel
+import com.spotify.mobius.test.NextMatchers.hasNoEffects
+import com.spotify.mobius.test.UpdateSpec
+import com.spotify.mobius.test.UpdateSpec.assertThatNext
+import org.junit.Test
+import java.net.URI
+
+class SelectCountryUpdateTest {
+
+  private val defaultModel = SelectCountryModel.FETCHING
+
+  private val spec = UpdateSpec(SelectCountryUpdate())
+
+  @Test
+  fun `when the manifest is fetched, then update the countries list`() {
+    val countries = listOf(
+        Country(
+            code = "IN",
+            endpoint = URI("https://in.simple.org"),
+            displayName = "India",
+            isdCode = "91"
+        ),
+        Country(
+            code = "BD",
+            endpoint = URI("https://bd.simple.org"),
+            displayName = "Bangladesh",
+            isdCode = "880"
+        )
+    )
+
+    spec
+        .given(defaultModel)
+        .whenEvent(ManifestFetched(countries))
+        .then(assertThatNext(
+            hasModel(defaultModel.withSupportedCountries(countries)),
+            hasNoEffects()
+        ))
+  }
+}

e12f8ef15cf94140efe0f07fb1d03a52850ab829
Vinay S Shenoy <code@vinaysshenoy.com>
2019-11-04 12:03:33 +0530

    Add method to log all sizes of saved view states

--- router/src/main/java/flow/StateSizeLogger.kt
+++ router/src/main/java/flow/StateSizeLogger.kt
@@ -1,8 +1,31 @@
 package flow
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.os.Parcel
 import android.os.Parcelable
+import android.util.SparseArray
+import org.simple.clinic.router.screen.FullScreenKey
+import timber.log.Timber
 import java.lang.reflect.Modifier
+
+fun logSavedStateSizes(flow: Flow, keyParceler: KeyParceler, resources: Resources) {
+  val bundlesToBeSaved = parcelables(getKeyManagerUnsafe(flow), keyParceler, flow.filteredHistory)
+
+  bundlesToBeSaved.forEach { logBundleSize(it, resources) }
+}
+
+/**
+ * Repurposed from [InternalLifecycleIntegration.save]
+ **/
+private fun parcelables(keyManager: KeyManager, parceler: KeyParceler, history: History): List<Bundle> {
+  return history
+      .reverseIterator<Any>()
+      .asSequence()
+      .filterNot { it.javaClass.isAnnotationPresent(NotPersistent::class.java) }
+      .map { keyManager.getState(it).toBundle(parceler) }
+      .toList()
+}
 
 /**
  * This method retrieves the current [KeyManager] from [Flow].
@@ -24,14 +47,38 @@ private fun getKeyManagerUnsafe(flow: Flow): KeyManager {
   return keyManagerField.get(flow) as KeyManager
 }
 
-/**
- * Repurposed from [InternalLifecycleIntegration.save]
- **/
-private fun parcelables(keyManager: KeyManager, parceler: KeyParceler, history: History): List<Bundle> {
-  return history
-      .reverseIterator<Any>()
-      .asSequence()
-      .filterNot { it.javaClass.isAnnotationPresent(NotPersistent::class.java) }
-      .map { keyManager.getState(it).toBundle(parceler) }
-      .toList()
+private fun logBundleSize(bundle: Bundle, resources: Resources) {
+  val screenKey = bundle["KEY"] as FullScreenKey
+
+  val viewIds = bundle.getIntArray("VIEW_STATE_IDS")
+  val viewStateSizeMap = viewIds!!
+      .map { viewId -> bundle.getSparseParcelableArray<Parcelable>("VIEW_STATE_$viewId")!! }
+      .map { viewAndChildStates -> viewAndChildStates.sizesOfViewStates(resources) }
+
+  Timber.tag("SaveState").i("Key: ${screenKey.javaClass.name}; Size: ${screenKey.parceledSize()}")
+  Timber.tag("SaveState").i("Key Value: $screenKey")
+  Timber.tag("SaveState").i("View States: $viewStateSizeMap")
+}
+
+private fun SparseArray<Parcelable>.sizesOfViewStates(resources: Resources): Map<String, Int> {
+  return (0 until size())
+      .map {
+        val viewId = keyAt(it)
+        val viewName = try {
+          resources.getResourceEntryName(viewId)
+        } catch (e: Resources.NotFoundException) {
+          viewId.toString()
+        }
+        val viewState = valueAt(it)
+
+        viewName to viewState.parceledSize()
+      }
+      .toMap()
+}
+
+private fun Parcelable.parceledSize(): Int {
+  return with(Parcel.obtain()) {
+    writeToParcel(this, 0)
+    marshall().size
+  }
 }

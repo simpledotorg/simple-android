6b698cd701530398498112ddaff7632d37187500
Vinay S Shenoy <vinay@obvious.in>
2019-11-05 09:54:48 +0530

    Handle errors when fetching the app manifest

--- app/src/main/java/org/simple/clinic/appconfig/ManifestFetchResult.kt
+++ app/src/main/java/org/simple/clinic/appconfig/ManifestFetchResult.kt
@@ -1,5 +1,9 @@
 package org.simple.clinic.appconfig
 
+import org.simple.clinic.util.ResolvedError
+
 sealed class ManifestFetchResult
 
-data class ManifestFetchSucceeded(val countries: List<Country>) : ManifestFetchResult()
+data class FetchSucceeded(val countries: List<Country>) : ManifestFetchResult()
+
+data class FetchError(val error: ResolvedError): ManifestFetchResult()

dd32d90b8fb51f065fcf38e8c36d13f6846dfa99
Vinay S Shenoy <code@vinaysshenoy.com>
2019-11-06 10:12:28 +0530

    Add JSON adapter for the `Country` class

--- app/src/main/java/org/simple/clinic/appconfig/Country.kt
+++ app/src/main/java/org/simple/clinic/appconfig/Country.kt
@@ -1,10 +1,21 @@
 package org.simple.clinic.appconfig
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.net.URI
 
+@JsonClass(generateAdapter = true)
 data class Country(
+
+    @Json(name = "country_code")
     val code: String,
+
+    @Json(name = "endpoint")
     val endpoint: URI,
+
+    @Json(name = "display_name")
     val displayName: String,
+
+    @Json(name = "isd_code")
     val isdCode: String
 )

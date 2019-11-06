e3590e1a2830c495a0fb5514a8993290a1f1c41d
Vinay S Shenoy <code@vinaysshenoy.com>
2019-11-06 10:57:21 +0530

    Add DI provider for the selected country preference

--- app/src/main/java/org/simple/clinic/appconfig/AppConfigModule.kt
+++ app/src/main/java/org/simple/clinic/appconfig/AppConfigModule.kt
@@ -1,8 +1,15 @@
 package org.simple.clinic.appconfig
 
+import com.f2prateek.rx.preferences2.Preference
+import com.f2prateek.rx.preferences2.RxSharedPreferences
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.appconfig.api.ManifestFetchApi
+import org.simple.clinic.util.None
+import org.simple.clinic.util.Optional
+import org.simple.clinic.util.preference.MoshiPreferenceConverter
+import org.simple.clinic.util.preference.OptionalRxPreferencesConverter
 import retrofit2.Retrofit
 import javax.inject.Named
 
@@ -12,5 +19,12 @@ class AppConfigModule {
   @Provides
   fun provideManifestFetchApi(@Named("for_config") retrofit: Retrofit): ManifestFetchApi {
     return retrofit.create(ManifestFetchApi::class.java)
+  }
+
+  @Provides
+  fun provideSelectedCountryPreference(rxSharedPreferences: RxSharedPreferences, moshi: Moshi): Preference<Optional<Country>> {
+    val countryPreferenceConverter = MoshiPreferenceConverter(moshi, Country::class.java)
+    return rxSharedPreferences
+        .getObject("preference_selected_country_v1", None, OptionalRxPreferencesConverter(countryPreferenceConverter))
   }
 }

e425f872127bd560ecd657e62fdb1cdc3f8e032e
Vinay S Shenoy <vinay@obvious.in>
2019-11-06 12:18:13 +0530

    Show error messages when the manifest fails to fetch

--- app/src/main/java/org/simple/clinic/selectcountry/SelectCountryUiRenderer.kt
+++ app/src/main/java/org/simple/clinic/selectcountry/SelectCountryUiRenderer.kt
@@ -5,10 +5,21 @@ import org.simple.clinic.mobius.ViewRenderer
 class SelectCountryUiRenderer(private val ui: SelectCountryUi) : ViewRenderer<SelectCountryModel> {
 
   override fun render(model: SelectCountryModel) {
-    if (model.hasFetchedCountries()) {
-      ui.displaySupportedCountries(model.countries!!)
-    } else {
-      ui.showProgress()
+    when {
+      model.hasFetchedCountries() -> {
+        ui.hideProgress()
+        ui.displaySupportedCountries(model.countries!!)
+      }
+      model.isFetching() -> ui.showProgress()
+      model.hasFailedToFetchCountries() -> {
+        ui.hideProgress()
+        when(model.manifestFetchError!!) {
+          NetworkError -> ui.displayNetworkErrorMessage()
+          ServerError -> ui.displayServerErrorMessage()
+          UnexpectedError -> ui.displayGenericErrorMessage()
+        }
+        ui.showRetryButton()
+      }
     }
   }
 }

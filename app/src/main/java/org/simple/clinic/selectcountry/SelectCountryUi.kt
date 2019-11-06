ce2d0558af6a7d903efd43371a67afe0cf429b7a
Vinay S Shenoy <vinay@obvious.in>
2019-11-06 12:18:13 +0530

    Show error messages when the manifest fails to fetch

--- app/src/main/java/org/simple/clinic/selectcountry/SelectCountryUi.kt
+++ app/src/main/java/org/simple/clinic/selectcountry/SelectCountryUi.kt
@@ -4,5 +4,10 @@ import org.simple.clinic.appconfig.Country
 
 interface SelectCountryUi {
   fun showProgress()
+  fun hideProgress()
   fun displaySupportedCountries(countries: List<Country>)
+  fun displayNetworkErrorMessage()
+  fun displayServerErrorMessage()
+  fun displayGenericErrorMessage()
+  fun showRetryButton()
 }

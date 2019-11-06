6ddb7680c9ab7b6550c400dae196c785965802a0
Vinay S Shenoy <vinay@obvious.in>
2019-11-06 12:02:22 +0530

    Render the list of supported countries

--- app/src/main/java/org/simple/clinic/selectcountry/SelectCountryUi.kt
+++ app/src/main/java/org/simple/clinic/selectcountry/SelectCountryUi.kt
@@ -1,5 +1,8 @@
 package org.simple.clinic.selectcountry
 
+import org.simple.clinic.appconfig.Country
+
 interface SelectCountryUi {
   fun showProgress()
+  fun displaySupportedCountries(countries: List<Country>)
 }

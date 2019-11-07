60bbcebc9195c60d5d74902a770f511fbc5a8965
Vinay S Shenoy <vinay@obvious.in>
2019-11-07 13:56:59 +0530

    Display localised country names in SelectCountryScreen

--- app/src/main/java/org/simple/clinic/selectcountry/adapter/SelectableCountryItem.kt
+++ app/src/main/java/org/simple/clinic/selectcountry/adapter/SelectableCountryItem.kt
@@ -5,24 +5,31 @@ import io.reactivex.subjects.Subject
 import kotlinx.android.synthetic.main.list_selectcountry_country_view.*
 import org.simple.clinic.R
 import org.simple.clinic.appconfig.Country
+import org.simple.clinic.appconfig.displayname.CountryDisplayNameFetcher
 import org.simple.clinic.selectcountry.adapter.Event.CountryClicked
 import org.simple.clinic.widgets.ItemAdapter
 import org.simple.clinic.widgets.recyclerview.ViewHolderX
 
 data class SelectableCountryItem(
     val country: Country,
-    val isSelected: Boolean,
-    val showDivider: Boolean
+    private val isCountryChosenByUser: Boolean,
+    private val showDivider: Boolean,
+    private val countryDisplayNameFetcher: CountryDisplayNameFetcher
 ) : ItemAdapter.Item<Event> {
 
   companion object {
-    fun from(countries: List<Country>, chosenCountry: Country?): List<SelectableCountryItem> {
+    fun from(countries: List<Country>, chosenCountry: Country?, countryDisplayNameFetcher: CountryDisplayNameFetcher): List<SelectableCountryItem> {
       return countries
           .mapIndexed { index, country ->
             val isLastCountryInList = index == countries.lastIndex
             val hasUserChosenCountry = country == chosenCountry
 
-            SelectableCountryItem(country = country, isSelected = hasUserChosenCountry, showDivider = !isLastCountryInList)
+            SelectableCountryItem(
+                country = country,
+                isCountryChosenByUser = hasUserChosenCountry,
+                showDivider = !isLastCountryInList,
+                countryDisplayNameFetcher = countryDisplayNameFetcher
+            )
           }
     }
   }
@@ -31,9 +38,8 @@ data class SelectableCountryItem(
 
   override fun render(holder: ViewHolderX, subject: Subject<Event>) {
     holder.countryButton.apply {
-      // TODO(vs): 2019-11-06 Read the country name from string resources
-      text = country.displayName
-      isChecked = isSelected
+      text = countryDisplayNameFetcher.displayNameForCountry(country)
+      isChecked = isCountryChosenByUser
       holder.divider.visibility = if (showDivider) View.VISIBLE else View.INVISIBLE
       setOnClickListener { subject.onNext(CountryClicked(country)) }
     }

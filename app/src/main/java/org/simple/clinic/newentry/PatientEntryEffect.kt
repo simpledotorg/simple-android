eccae9b9bc99a0f05427ad804536d1f0173c11b2
Ragunath Jawahar <ragunath@obvious.in>
2019-11-11 16:09:30 +0530

    Order effects in the logical order in which they usually happen

--- app/src/main/java/org/simple/clinic/newentry/PatientEntryEffect.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryEffect.kt
@@ -7,18 +7,16 @@ sealed class PatientEntryEffect
 
 object FetchPatientEntry : PatientEntryEffect()
 
-data class PrefillFields(
-    val patientEntry: OngoingNewPatientEntry
-) : PatientEntryEffect()
+data class PrefillFields(val patientEntry: OngoingNewPatientEntry) : PatientEntryEffect()
 
 object ScrollFormOnGenderSelection : PatientEntryEffect()
 
 data class ShowDatePatternInDateOfBirthLabel(val show: Boolean) : PatientEntryEffect()
 
-data class SavePatient(val entry: OngoingNewPatientEntry) : PatientEntryEffect()
-
 data class ShowValidationErrors(val errors: List<PatientEntryValidationError>) : PatientEntryEffect()
 
-object OpenMedicalHistoryEntryScreen : PatientEntryEffect()
-
 data class HideValidationError(val field: Field) : PatientEntryEffect()
+
+data class SavePatient(val entry: OngoingNewPatientEntry) : PatientEntryEffect()
+
+object OpenMedicalHistoryEntryScreen : PatientEntryEffect()

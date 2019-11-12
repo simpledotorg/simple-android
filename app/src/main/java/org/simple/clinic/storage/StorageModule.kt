34ab20efacdb04d4df3461d968bd5698447f3298
Vinay S Shenoy <vinay@obvious.in>
2019-11-12 12:20:33 +0530

    Extract Room migrations provider method into separate module

--- app/src/main/java/org/simple/clinic/storage/StorageModule.kt
+++ app/src/main/java/org/simple/clinic/storage/StorageModule.kt
@@ -16,76 +16,18 @@ import org.simple.clinic.storage.files.AndroidFileStorage
 import org.simple.clinic.storage.files.FileOperations
 import org.simple.clinic.storage.files.FileStorage
 
-@Module
+@Module(includes = [RoomMigrationsModule::class])
 open class StorageModule(
     private val databaseName: String = "red-db",
     private val runDatabaseQueriesOnMainThread: Boolean = false
 ) {
 
   @Provides
-  fun databaseMigrations(): ArrayList<Migration> {
-    // kotlin.collections.List doesn't work when injecting collections using
-    // Dagger. Using @JvmSuppressWildcards is an option, but that looks verbose.
-    return arrayListOf(
-        Migration_3_4(),
-        Migration_4_5(),
-        Migration_5_6(),
-        Migration_6_7(),
-        Migration_7_8(),
-        Migration_8_9(),
-        Migration_9_10(),
-        Migration_10_11(),
-        Migration_11_12(),
-        Migration_12_13(),
-        Migration_13_14(),
-        Migration_14_15(),
-        Migration_15_16(),
-        Migration_16_17(),
-        Migration_17_18(),
-        Migration_18_19(),
-        Migration_19_20(),
-        Migration_20_21(),
-        Migration_21_22(),
-        Migration_22_23(),
-        Migration_23_24(),
-        Migration_24_25(),
-        Migration_25_26(),
-        Migration_26_27(),
-        Migration_27_28(),
-        Migration_28_29(),
-        Migration_29_30(),
-        Migration_30_31(),
-        Migration_31_32(),
-        Migration_32_33(),
-        Migration_33_34(),
-        Migration_34_35(),
-        Migration_35_36(),
-        Migration_36_37(),
-        Migration_37_38(),
-        Migration_38_39(),
-        Migration_39_40(),
-        Migration_40_41(),
-        Migration_41_42(),
-        Migration_42_43(),
-        Migration_43_44(),
-        Migration_44_45(),
-        Migration_45_46(),
-        Migration_46_47(),
-        Migration_47_48(),
-        Migration_48_49(),
-        Migration_49_50(),
-        Migration_50_51(),
-        Migration_51_52(),
-        Migration_52_53()
-    )
-  }
-
-  @Provides
   @AppScope
   open fun appDatabase(
       appContext: Application,
       factory: SupportSQLiteOpenHelper.Factory,
-      migrations: ArrayList<Migration>
+      migrations: List<@JvmSuppressWildcards Migration>
   ): AppDatabase {
     return Room.databaseBuilder(appContext, AppDatabase::class.java, databaseName)
         .openHelperFactory(factory)

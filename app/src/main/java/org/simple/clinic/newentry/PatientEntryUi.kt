441e8b439ae4016c68f378578c25fd79d6a89a9d
Ragunath Jawahar <ragunath@obvious.in>
2019-11-12 16:23:08 +0530

    Split validation error function calls to a separate interface

--- app/src/main/java/org/simple/clinic/newentry/PatientEntryUi.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryUi.kt
@@ -3,21 +3,11 @@ package org.simple.clinic.newentry
 import org.simple.clinic.patient.OngoingNewPatientEntry
 import org.simple.clinic.widgets.ageanddateofbirth.DateOfBirthAndAgeVisibility
 
-interface PatientEntryUi {
+interface PatientEntryUi : PatientEntryValidationActions {
   fun prefillFields(entry: OngoingNewPatientEntry)
   fun openMedicalHistoryEntryScreen()
   fun setDateOfBirthAndAgeVisibility(visibility: DateOfBirthAndAgeVisibility)
   fun setShowDatePatternInDateOfBirthLabel(showPattern: Boolean)
-  fun showEmptyFullNameError(show: Boolean)
-  fun showLengthTooShortPhoneNumberError(show: Boolean)
-  fun showLengthTooLongPhoneNumberError(show: Boolean)
-  fun showMissingGenderError(show: Boolean)
-  fun showEmptyColonyOrVillageError(show: Boolean)
-  fun showEmptyDistrictError(show: Boolean)
-  fun showEmptyStateError(show: Boolean)
-  fun showEmptyDateOfBirthAndAgeError(show: Boolean)
-  fun showInvalidDateOfBirthError(show: Boolean)
-  fun showDateOfBirthIsInFutureError(show: Boolean)
   fun scrollToFirstFieldWithError()
   fun scrollFormOnGenderSelection()
   fun showIdentifierSection()

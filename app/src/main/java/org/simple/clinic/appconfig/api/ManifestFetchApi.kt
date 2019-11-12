7e18df7175c559bb6318bcbe9a9527d44f078325
Vinay S Shenoy <vinay@obvious.in>
2019-11-12 11:10:58 +0530

    Update the manifest fetch api result as per API spec

--- app/src/main/java/org/simple/clinic/appconfig/api/ManifestFetchApi.kt
+++ app/src/main/java/org/simple/clinic/appconfig/api/ManifestFetchApi.kt
@@ -1,11 +1,11 @@
 package org.simple.clinic.appconfig.api
 
 import io.reactivex.Single
-import org.simple.clinic.appconfig.Country
+import org.simple.clinic.appconfig.Manifest
 import retrofit2.http.GET
 
 interface ManifestFetchApi {
 
   @GET("manifest.json")
-  fun fetchManifest(): Single<List<Country>>
+  fun fetchManifest(): Single<Manifest>
 }

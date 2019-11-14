f5fffa31935388facac9437c6429fc0cd247aab8
Vinay S Shenoy <vinay@obvious.in>
2019-11-14 10:27:55 +0530

    Add LoginUserWithOtp class to abstract login flow

--- app/src/main/java/org/simple/clinic/setup/SetupActivityModule.kt
+++ app/src/main/java/org/simple/clinic/setup/SetupActivityModule.kt
@@ -2,18 +2,13 @@ package org.simple.clinic.setup
 
 import dagger.Module
 import dagger.Provides
-import org.simple.clinic.AppDatabase
 import org.simple.clinic.BuildConfig
 import org.simple.clinic.appconfig.Country
-import org.simple.clinic.user.User
 import java.net.URI
 import javax.inject.Named
 
 @Module
 class SetupActivityModule {
-
-  @Provides
-  fun provideUserDao(appDatabase: AppDatabase): User.RoomDao = appDatabase.userDao()
 
   @Provides
   @Named("fallback")

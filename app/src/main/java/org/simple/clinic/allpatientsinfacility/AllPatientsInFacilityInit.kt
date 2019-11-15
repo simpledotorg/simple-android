61b2ea790717f01c77664ff6284d9f0aa9764e92
Ragunath Jawahar <ragunath@obvious.in>
2019-11-15 12:18:13 +0530

    Requery `List<PatientSearchResultUiState>` from the database
    (cherry picked from commit 764440bacde0ba14343935f732324e1e25538759)

--- app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityInit.kt
+++ app/src/main/java/org/simple/clinic/allpatientsinfacility/AllPatientsInFacilityInit.kt
@@ -3,9 +3,9 @@ package org.simple.clinic.allpatientsinfacility
 import com.spotify.mobius.First
 import com.spotify.mobius.First.first
 import com.spotify.mobius.Init
+import org.simple.clinic.allpatientsinfacility.AllPatientsInFacilityModel.Companion.FETCHING_PATIENTS
 
 class AllPatientsInFacilityInit : Init<AllPatientsInFacilityModel, AllPatientsInFacilityEffect> {
-  override fun init(model: AllPatientsInFacilityModel): First<AllPatientsInFacilityModel, AllPatientsInFacilityEffect> {
-    return first(model, setOf(FetchFacilityEffect))
-  }
+  override fun init(model: AllPatientsInFacilityModel): First<AllPatientsInFacilityModel, AllPatientsInFacilityEffect> =
+      first(FETCHING_PATIENTS, setOf(FetchFacilityEffect))
 }

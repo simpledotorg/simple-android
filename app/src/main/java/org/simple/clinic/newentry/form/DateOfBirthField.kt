3c84ec9fb47349c557be2f83edce49233c0224ba
Ragunath Jawahar <ragunath@obvious.in>
2019-11-20 15:58:33 +0530

    Check for date in the future in `DateOfBirth` field

--- app/src/main/java/org/simple/clinic/newentry/form/DateOfBirthField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/DateOfBirthField.kt
@@ -1,16 +1,22 @@
 package org.simple.clinic.newentry.form
 
+import org.simple.clinic.newentry.form.ValidationError.DateIsInFuture
 import org.simple.clinic.newentry.form.ValidationError.InvalidDateFormat
 import org.simple.clinic.newentry.form.ValidationError.MissingValue
 import org.threeten.bp.LocalDate
+import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.format.DateTimeParseException
 
-class DateOfBirthField : InputField<String>() {
+class DateOfBirthField(
+    private val dateTimeFormatter: DateTimeFormatter,
+    private val today: LocalDate
+) : InputField<String>() {
   override fun validate(value: String): List<ValidationError> {
     if (value.isBlank()) return listOf(MissingValue)
 
     try {
-      LocalDate.parse(value)
+      val parsedDate = LocalDate.parse(value, dateTimeFormatter)
+      if (parsedDate > today) return listOf(DateIsInFuture)
     } catch (e: DateTimeParseException) {
       return listOf(InvalidDateFormat)
     }

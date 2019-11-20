8b150ab007eb94de4ed5778267597b41b4691464
Vinay S Shenoy <code@vinaysshenoy.com>
2019-11-20 09:39:44 +0530

    Use `RegisterUser` in `RegistrationLoadingScreenController`

--- app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingScreenController.kt
+++ app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingScreenController.kt
@@ -3,11 +3,15 @@ package org.simple.clinic.registration.register
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
+import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.ofType
+import org.simple.clinic.facility.FacilityRepository
+import org.simple.clinic.user.UserSession
+import org.simple.clinic.user.registeruser.RegisterUser
 import org.simple.clinic.user.registeruser.RegistrationResult.NetworkError
 import org.simple.clinic.user.registeruser.RegistrationResult.Success
 import org.simple.clinic.user.registeruser.RegistrationResult.UnexpectedError
-import org.simple.clinic.user.UserSession
+import org.simple.clinic.util.filterAndUnwrapJust
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
 import javax.inject.Inject
@@ -16,7 +20,9 @@ typealias Ui = RegistrationLoadingScreen
 typealias UiChange = (Ui) -> Unit
 
 class RegistrationLoadingScreenController @Inject constructor(
-    private val userSession: UserSession
+    private val userSession: UserSession,
+    private val facilityRepository: FacilityRepository,
+    private val registerUser: RegisterUser
 ) : ObservableTransformer<UiEvent, UiChange> {
 
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
@@ -31,9 +37,17 @@ class RegistrationLoadingScreenController @Inject constructor(
     val register = Observable
         .merge(creates, retryClicks)
         .flatMap {
-          userSession
-              .register()
-              .toObservable()
+          val savedUser = userSession
+              .loggedInUser()
+              .filterAndUnwrapJust()
+              .take(1)
+
+          val facilityToRegisterAt = savedUser
+              .flatMap { facilityRepository.currentFacility(it) }
+              .take(1)
+
+          Observables.combineLatest(savedUser, facilityToRegisterAt)
+              .flatMapSingle { (user, facility) -> registerUser.registerUserAtFacility(user, facility) }
         }
         .replay()
         .refCount()
--- app/src/test/java/org/simple/clinic/registration/register/RegistrationLoadingScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/registration/register/RegistrationLoadingScreenControllerTest.kt
@@ -7,25 +7,44 @@ import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
 import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Completable
+import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.subjects.PublishSubject
 import org.junit.Before
 import org.junit.Test
+import org.simple.clinic.facility.FacilityRepository
+import org.simple.clinic.patient.PatientMocker
+import org.simple.clinic.user.User
+import org.simple.clinic.user.User.LoggedInStatus.NOT_LOGGED_IN
 import org.simple.clinic.user.UserSession
+import org.simple.clinic.user.UserStatus.WaitingForApproval
+import org.simple.clinic.user.registeruser.RegisterUser
 import org.simple.clinic.user.registeruser.RegistrationResult
 import org.simple.clinic.user.registeruser.RegistrationResult.NetworkError
 import org.simple.clinic.user.registeruser.RegistrationResult.Success
 import org.simple.clinic.user.registeruser.RegistrationResult.UnexpectedError
+import org.simple.clinic.util.Just
+import org.simple.clinic.util.Optional
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
+import java.util.UUID
 
 class RegistrationLoadingScreenControllerTest {
 
   private val userSession = mock<UserSession>()
   private val screen = mock<RegistrationLoadingScreen>()
+  private val registerUser = mock<RegisterUser>()
+  private val facilityRepository = mock<FacilityRepository>()
   private val uiEvents = PublishSubject.create<UiEvent>()
 
-  private val controller = RegistrationLoadingScreenController(userSession)
+  private val user = PatientMocker.loggedInUser(
+      uuid = UUID.fromString("fe1786be-5725-45b5-a6aa-e9ce0f99f794"),
+      loggedInStatus = NOT_LOGGED_IN,
+      status = WaitingForApproval
+  )
+  private val facility = PatientMocker.facility(UUID.fromString("37e253a9-8a8a-4c60-8aac-34338dc47e8b"))
+
+  private val controller = RegistrationLoadingScreenController(userSession, facilityRepository, registerUser)
 
   @Before
   fun setUp() {
@@ -36,12 +55,14 @@ class RegistrationLoadingScreenControllerTest {
 
   @Test
   fun `when retry button is clicked, then loader should show and register api should be called`() {
-    whenever(userSession.register()).doReturn(Single.just<RegistrationResult>(Success))
+    whenever(userSession.loggedInUser()) doReturn Observable.just<Optional<User>>(Just(user))
+    whenever(facilityRepository.currentFacility(user)) doReturn Observable.just(facility)
+    whenever(registerUser.registerUserAtFacility(user, facility)).doReturn(Single.just<RegistrationResult>(Success))
     whenever(userSession.clearOngoingRegistrationEntry()).doReturn(Completable.complete())
 
     uiEvents.onNext(RegisterErrorRetryClicked)
 
-    verify(userSession).register()
+    verify(registerUser).registerUserAtFacility(user, facility)
     verify(userSession).clearOngoingRegistrationEntry()
     verify(screen).openHomeScreen()
   }
@@ -49,21 +70,24 @@ class RegistrationLoadingScreenControllerTest {
   @Test
   fun `when screen is created, then the user registration api should be called`() {
     // given
-    whenever(userSession.register()) doReturn Single.never()
+    whenever(userSession.loggedInUser()) doReturn Observable.just<Optional<User>>(Just(user))
+    whenever(facilityRepository.currentFacility(user)) doReturn Observable.just(facility)
+    whenever(registerUser.registerUserAtFacility(user, facility)) doReturn Single.never()
 
     // when
     uiEvents.onNext(ScreenCreated())
 
     // then
-    verify(userSession).register()
-    verifyNoMoreInteractions(userSession)
+    verify(registerUser).registerUserAtFacility(user, facility)
     verifyZeroInteractions(screen)
   }
 
   @Test
   fun `when the register user call succeeds, then clear registration entry and go to home screen`() {
     // given
-    whenever(userSession.register()) doReturn Single.just<RegistrationResult>(Success)
+    whenever(userSession.loggedInUser()) doReturn Observable.just<Optional<User>>(Just(user))
+    whenever(facilityRepository.currentFacility(user)) doReturn Observable.just(facility)
+    whenever(registerUser.registerUserAtFacility(user, facility)) doReturn Single.just<RegistrationResult>(Success)
     whenever(userSession.clearOngoingRegistrationEntry()) doReturn Completable.complete()
 
     // when
@@ -77,7 +101,9 @@ class RegistrationLoadingScreenControllerTest {
   @Test
   fun `when the register call fails with a network error, show the network error message`() {
     // given
-    whenever(userSession.register()) doReturn Single.just<RegistrationResult>(NetworkError)
+    whenever(userSession.loggedInUser()) doReturn Observable.just<Optional<User>>(Just(user))
+    whenever(facilityRepository.currentFacility(user)) doReturn Observable.just(facility)
+    whenever(registerUser.registerUserAtFacility(user, facility)) doReturn Single.just<RegistrationResult>(NetworkError)
 
     // when
     uiEvents.onNext(ScreenCreated())
@@ -90,7 +116,9 @@ class RegistrationLoadingScreenControllerTest {
   @Test
   fun `when the register call fails with any other error, show the generic error message`() {
     // given
-    whenever(userSession.register()) doReturn Single.just<RegistrationResult>(UnexpectedError)
+    whenever(userSession.loggedInUser()) doReturn Observable.just<Optional<User>>(Just(user))
+    whenever(facilityRepository.currentFacility(user)) doReturn Observable.just(facility)
+    whenever(registerUser.registerUserAtFacility(user, facility)) doReturn Single.just<RegistrationResult>(UnexpectedError)
 
     // when
     uiEvents.onNext(ScreenCreated())

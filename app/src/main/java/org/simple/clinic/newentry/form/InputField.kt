ee107c0687748130d01445af6ef8cdbc9547f5ad
Ragunath Jawahar <ragunath@obvious.in>
2019-11-20 12:36:23 +0530

    Add `GenderField`

--- app/src/main/java/org/simple/clinic/newentry/form/InputField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/InputField.kt
@@ -1,3 +1,5 @@
 package org.simple.clinic.newentry.form
 
-abstract class InputField
+abstract class InputField<T> {
+  abstract fun validate(value: T): List<ValidationError>
+}
--- app/src/main/java/org/simple/clinic/newentry/form/PatientNameField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/PatientNameField.kt
@@ -2,8 +2,8 @@ package org.simple.clinic.newentry.form
 
 import org.simple.clinic.newentry.form.ValidationError.FieldIsBlankEmpty
 
-class PatientNameField : InputField() {
-  fun validate(value: String): List<ValidationError> {
+class PatientNameField : InputField<String>() {
+  override fun validate(value: String): List<ValidationError> {
     if (value.isNotBlank()) {
       return emptyList()
     }

b415bf0c8b7aa8529617ba0b2e60b0f6cb255bd9
Ragunath Jawahar <ragunath@obvious.in>
2019-11-20 12:26:46 +0530

    Add `PatientNameField`

---
+++ app/src/test/java/org/simple/clinic/newentry/form/PatientNameFieldTest.kt
@@ -0,0 +1,30 @@
+package org.simple.clinic.newentry.form
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.simple.clinic.newentry.form.ValidationError.FieldIsBlankEmpty
+
+class PatientNameFieldTest {
+  @Test
+  fun `it returns an empty list when the field is non-empty`() {
+    val validationErrors = PatientNameField().validate("Ajay")
+    assertThat(validationErrors)
+        .isEmpty()
+  }
+
+  @Test
+  fun `it returns an error when the field is empty`() {
+    val validationErrors = PatientNameField().validate("")
+    assertThat(validationErrors)
+        .containsExactly(FieldIsBlankEmpty)
+        .inOrder()
+  }
+
+  @Test
+  fun `it returns an error when the field is blank`() {
+    val validationErrors = PatientNameField().validate("    ")
+    assertThat(validationErrors)
+        .containsExactly(FieldIsBlankEmpty)
+        .inOrder()
+  }
+}

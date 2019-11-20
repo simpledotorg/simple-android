4ea2a7e9da9af9fe51f3b7e4e20c0eb2af6ac97e
Ragunath Jawahar <ragunath@obvious.in>
2019-11-20 15:58:33 +0530

    Check for date in the future in `DateOfBirth` field

--- app/src/main/java/org/simple/clinic/newentry/form/ValidationError.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/ValidationError.kt
@@ -3,4 +3,5 @@ package org.simple.clinic.newentry.form
 sealed class ValidationError {
   object MissingValue : ValidationError()
   object InvalidDateFormat : ValidationError()
+  object DateIsInFuture : ValidationError()
 }

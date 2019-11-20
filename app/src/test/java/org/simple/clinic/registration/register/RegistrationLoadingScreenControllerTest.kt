68804cfd62a29584ade5e680617960a1589f5f9d
Vinay S Shenoy <code@vinaysshenoy.com>
2019-11-20 09:18:53 +0530

    Clean up tests in `RegistrationLoadingScreenControllerTest`
    - Make more specific tests
    - Remove parameterized tests

--- app/src/test/java/org/simple/clinic/registration/register/RegistrationLoadingScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/registration/register/RegistrationLoadingScreenControllerTest.kt
@@ -4,15 +4,13 @@ import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
+import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.subjects.PublishSubject
-import junitparams.JUnitParamsRunner
-import junitparams.Parameters
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.simple.clinic.user.UserSession
 import org.simple.clinic.user.registeruser.RegistrationResult
 import org.simple.clinic.user.registeruser.RegistrationResult.NetworkError
@@ -21,61 +19,84 @@ import org.simple.clinic.user.registeruser.RegistrationResult.UnexpectedError
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
 
-@RunWith(JUnitParamsRunner::class)
 class RegistrationLoadingScreenControllerTest {
 
   private val userSession = mock<UserSession>()
   private val screen = mock<RegistrationLoadingScreen>()
-  lateinit var controller: RegistrationLoadingScreenController
-
   private val uiEvents = PublishSubject.create<UiEvent>()
+
+  private val controller = RegistrationLoadingScreenController(userSession)
 
   @Before
   fun setUp() {
-    controller = RegistrationLoadingScreenController(userSession)
-
     uiEvents
         .compose(controller)
         .subscribe { uiChange -> uiChange(screen) }
   }
 
-  @Parameters(method = "params for testing user registration")
   @Test
-  fun `when screen is created then user registration api should be called`(results: RegistrationResult) {
-    whenever(userSession.register()).doReturn(Single.just(results))
-    whenever(userSession.clearOngoingRegistrationEntry()).doReturn(Completable.complete())
-
-    uiEvents.onNext(ScreenCreated())
-
-    verify(userSession).register()
-    when (results) {
-      Success -> {
-        verify(userSession).clearOngoingRegistrationEntry()
-        verify(screen).openHomeScreen()
-      }
-      NetworkError -> verify(screen).showNetworkError()
-      UnexpectedError -> verify(screen).showUnexpectedError()
-    }
-    verifyNoMoreInteractions(screen)
-  }
-
-  @Suppress("unused")
-  fun `params for testing user registration`() =
-      listOf<RegistrationResult>(
-          Success,
-          NetworkError,
-          UnexpectedError
-      )
-
-  @Test
-  fun `when retry button is clicked in error state then loader should show and register api should be called`() {
+  fun `when retry button is clicked, then loader should show and register api should be called`() {
     whenever(userSession.register()).doReturn(Single.just<RegistrationResult>(Success))
-    whenever(userSession.clearOngoingRegistrationEntry()).doReturn(Completable.never())
+    whenever(userSession.clearOngoingRegistrationEntry()).doReturn(Completable.complete())
 
     uiEvents.onNext(RegisterErrorRetryClicked)
 
     verify(userSession).register()
     verify(userSession).clearOngoingRegistrationEntry()
     verify(screen).openHomeScreen()
+  }
+
+  @Test
+  fun `when screen is created, then the user registration api should be called`() {
+    // given
+    whenever(userSession.register()) doReturn Single.never()
+
+    // when
+    uiEvents.onNext(ScreenCreated())
+
+    // then
+    verify(userSession).register()
+    verifyNoMoreInteractions(userSession)
+    verifyZeroInteractions(screen)
+  }
+
+  @Test
+  fun `when the register user call succeeds, then clear registration entry and go to home screen`() {
+    // given
+    whenever(userSession.register()) doReturn Single.just<RegistrationResult>(Success)
+    whenever(userSession.clearOngoingRegistrationEntry()) doReturn Completable.complete()
+
+    // when
+    uiEvents.onNext(ScreenCreated())
+
+    // then
+    verify(screen).openHomeScreen()
+    verifyNoMoreInteractions(screen)
+  }
+
+  @Test
+  fun `when the register call fails with a network error, show the network error message`() {
+    // given
+    whenever(userSession.register()) doReturn Single.just<RegistrationResult>(NetworkError)
+
+    // when
+    uiEvents.onNext(ScreenCreated())
+
+    // then
+    verify(screen).showNetworkError()
+    verifyNoMoreInteractions(screen)
+  }
+
+  @Test
+  fun `when the register call fails with any other error, show the generic error message`() {
+    // given
+    whenever(userSession.register()) doReturn Single.just<RegistrationResult>(UnexpectedError)
+
+    // when
+    uiEvents.onNext(ScreenCreated())
+
+    // then
+    verify(screen).showUnexpectedError()
+    verifyNoMoreInteractions(screen)
   }
 }

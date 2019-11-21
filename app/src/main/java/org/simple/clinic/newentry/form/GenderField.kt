43dfc537b95c49ddf23cad467370a2ee0607b91c
Ragunath Jawahar <ragunath@obvious.in>
2019-11-21 14:46:24 +0530

    Change return type from `List` to `Set`

--- app/src/main/java/org/simple/clinic/newentry/form/GenderField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/GenderField.kt
@@ -4,7 +4,7 @@ import org.simple.clinic.newentry.form.ValidationError.MissingValue
 import org.simple.clinic.patient.Gender
 
 class GenderField : InputField<Gender?>() {
-  override fun validate(value: Gender?): List<ValidationError> {
-    return if (value == null) listOf(MissingValue) else emptyList()
+  override fun validate(value: Gender?): Set<ValidationError> {
+    return if (value == null) setOf(MissingValue) else emptySet()
   }
 }

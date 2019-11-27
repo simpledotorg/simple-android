563fa89431c08f301ac182e3b76fb7de3e66e18e
Ragunath Jawahar <ragunath@obvious.in>
2019-11-27 16:21:54 +0530

    Add form fields for India to `FormFieldFactory`

---
+++ app/src/test/java/org/simple/clinic/newentry/country/FormFieldFactoryTest.kt
@@ -0,0 +1,46 @@
+package org.simple.clinic.newentry.country
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.simple.clinic.BuildConfig
+import org.simple.clinic.appconfig.Country
+import org.simple.clinic.newentry.form.AgeField
+import org.simple.clinic.newentry.form.DateOfBirthField
+import org.simple.clinic.newentry.form.DistrictField
+import org.simple.clinic.newentry.form.GenderField
+import org.simple.clinic.newentry.form.LandlineOrMobileField
+import org.simple.clinic.newentry.form.PatientNameField
+import org.simple.clinic.newentry.form.StateField
+import org.simple.clinic.newentry.form.VillageOrColonyField
+import org.threeten.bp.LocalDate
+import org.threeten.bp.format.DateTimeFormatter
+import java.net.URI
+
+class FormFieldFactoryTest {
+  private val dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+  private val today = LocalDate.parse("20/11/2019", dateTimeFormatter)
+
+  @Test
+  fun `it returns fields that are specific to India`() {
+    val india = Country(
+        isoCountryCode = "IN",
+        endpoint = URI.create("https://in.simple.org/api/"),
+        displayName = "India",
+        isdCode = "91"
+    )
+
+    val fields = FormFieldFactory.fields(dateTimeFormatter, today, india)
+    assertThat(fields.map { it::class })
+        .containsExactly(
+            PatientNameField::class,
+            AgeField::class,
+            DateOfBirthField::class,
+            LandlineOrMobileField::class,
+            GenderField::class,
+            VillageOrColonyField::class,
+            DistrictField::class,
+            StateField::class
+        )
+        .inOrder()
+  }
+}

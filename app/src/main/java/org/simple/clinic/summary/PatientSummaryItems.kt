b2df68b5400f12813ef861f6b8d4be51565e8d8a
Vinay S Shenoy <vinay@obvious.in>
2019-11-27 11:54:39 +0530

    Generate prescribed drugs list items in the UI

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryItems.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryItems.kt
@@ -1,7 +1,9 @@
 package org.simple.clinic.summary
 
+import org.simple.clinic.drugs.PrescribedDrug
+
 data class PatientSummaryItems(
-    val prescriptionItems: SummaryPrescribedDrugsItem,
+    val prescription: List<PrescribedDrug>,
     val bloodPressureListItems: List<SummaryBloodPressureListItem>,
     val medicalHistoryItems: SummaryMedicalHistoryItem
 )

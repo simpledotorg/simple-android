6a8de0bb3b302686decf07ea08a9c9152c3bc1a5
Ragunath Jawahar <ragunath@obvious.in>
2019-11-27 16:41:56 +0530

    Add form fields for Bangladesh

--- app/src/test/java/org/simple/clinic/newentry/country/FormFieldFactoryTest.kt
+++ app/src/test/java/org/simple/clinic/newentry/country/FormFieldFactoryTest.kt
@@ -2,16 +2,18 @@ package org.simple.clinic.newentry.country
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
-import org.simple.clinic.BuildConfig
 import org.simple.clinic.appconfig.Country
 import org.simple.clinic.newentry.form.AgeField
+import org.simple.clinic.newentry.form.BusinessIdentifierField
 import org.simple.clinic.newentry.form.DateOfBirthField
 import org.simple.clinic.newentry.form.DistrictField
 import org.simple.clinic.newentry.form.GenderField
 import org.simple.clinic.newentry.form.LandlineOrMobileField
 import org.simple.clinic.newentry.form.PatientNameField
 import org.simple.clinic.newentry.form.StateField
+import org.simple.clinic.newentry.form.StreetAddressField
 import org.simple.clinic.newentry.form.VillageOrColonyField
+import org.simple.clinic.newentry.form.ZoneField
 import org.threeten.bp.LocalDate
 import org.threeten.bp.format.DateTimeFormatter
 import java.net.URI
@@ -38,6 +40,33 @@ class FormFieldFactoryTest {
             LandlineOrMobileField::class,
             GenderField::class,
             VillageOrColonyField::class,
+            DistrictField::class,
+            StateField::class
+        )
+        .inOrder()
+  }
+
+  @Test
+  fun `it returns fields that are specific to Bangladesh`() {
+    val bangladesh = Country(
+        isoCountryCode = "BD",
+        endpoint = URI.create("https://bd.simple.org/api/"),
+        displayName = "Bangladesh",
+        isdCode = "880"
+    )
+
+    val fields = FormFieldFactory.fields(dateTimeFormatter, today, bangladesh)
+    assertThat(fields.map { it::class })
+        .containsExactly(
+            PatientNameField::class,
+            AgeField::class,
+            DateOfBirthField::class,
+            LandlineOrMobileField::class,
+            GenderField::class,
+            BusinessIdentifierField::class,
+            StreetAddressField::class,
+            VillageOrColonyField::class,
+            ZoneField::class,
             DistrictField::class,
             StateField::class
         )

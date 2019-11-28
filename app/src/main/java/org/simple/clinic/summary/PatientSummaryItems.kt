23aca79a05f04a9211cb2a11954d1e84c3b7af1f
Vinay S Shenoy <vinay@obvious.in>
2019-11-28 10:43:18 +0530

    Generate BP list item in the UI

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryItems.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryItems.kt
@@ -1,10 +1,11 @@
 package org.simple.clinic.summary
 
+import org.simple.clinic.bp.BloodPressureMeasurement
 import org.simple.clinic.drugs.PrescribedDrug
 import org.simple.clinic.medicalhistory.MedicalHistory
 
 data class PatientSummaryItems(
     val prescription: List<PrescribedDrug>,
-    val bloodPressureListItems: List<SummaryBloodPressureListItem>,
+    val bloodPressures: List<BloodPressureMeasurement>,
     val medicalHistory: MedicalHistory
 )

dd6fa902b6771c9c49589b3da176189f30d8c689
Vinay S Shenoy <vinay@obvious.in>
2019-12-02 12:54:58 +0530

    Introduce discrete view for displaying the list of prescribed drugs

--- app/src/main/java/org/simple/clinic/summary/prescribeddrugs/DrugSummaryView.kt
+++ app/src/main/java/org/simple/clinic/summary/prescribeddrugs/DrugSummaryView.kt
@@ -1,9 +1,9 @@
 package org.simple.clinic.summary.prescribeddrugs
 
-import android.view.View
-import com.xwray.groupie.ViewHolder
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.list_patientsummary_prescriptions.*
+import android.content.Context
+import android.util.AttributeSet
+import androidx.cardview.widget.CardView
+import kotlinx.android.synthetic.main.list_patientsummary_prescriptions.view.*
 import org.simple.clinic.R
 import org.simple.clinic.drugs.PrescribedDrug
 import org.simple.clinic.util.RelativeTimestampGenerator
@@ -11,21 +11,18 @@ import org.simple.clinic.util.UserClock
 import org.simple.clinic.widgets.visibleOrGone
 import org.threeten.bp.format.DateTimeFormatter
 
-class DrugsSummaryViewHolder(
-    override val containerView: View,
-    private val onUpdateClicked: () -> Unit
-) : ViewHolder(containerView), LayoutContainer {
+class DrugSummaryView(context: Context, attributeSet: AttributeSet) : CardView(context, attributeSet) {
 
-  init {
-    updateButton.setOnClickListener { onUpdateClicked() }
-  }
+  private val timestampGenerator = RelativeTimestampGenerator()
 
   fun bind(
       prescriptions: List<PrescribedDrug>,
       dateFormatter: DateTimeFormatter,
-      timestampGenerator: RelativeTimestampGenerator,
-      userClock: UserClock
+      userClock: UserClock,
+      updateClicked: () -> Unit
   ) {
+    updateButton.setOnClickListener { updateClicked() }
+
     summaryViewGroup.visibleOrGone(prescriptions.isNotEmpty())
 
     setButtonText(prescriptions)
@@ -41,9 +38,9 @@ class DrugsSummaryViewHolder(
 
       val lastUpdatedTimestamp = timestampGenerator.generate(lastUpdatedPrescription.updatedAt, userClock)
 
-      lastUpdatedTimestampTextView.text = itemView.resources.getString(
+      lastUpdatedTimestampTextView.text = resources.getString(
           R.string.patientsummary_prescriptions_last_updated,
-          lastUpdatedTimestamp.displayText(itemView.context, dateFormatter)
+          lastUpdatedTimestamp.displayText(context, dateFormatter)
       )
     }
   }
@@ -54,9 +51,9 @@ class DrugsSummaryViewHolder(
 
   private fun setButtonText(prescriptions: List<PrescribedDrug>) {
     updateButton.text = if (prescriptions.isEmpty()) {
-      itemView.context.getString(R.string.patientsummary_prescriptions_add)
+      context.getString(R.string.patientsummary_prescriptions_add)
     } else {
-      itemView.context.getString(R.string.patientsummary_prescriptions_update)
+      context.getString(R.string.patientsummary_prescriptions_update)
     }
   }
 }

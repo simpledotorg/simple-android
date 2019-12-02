1cfd551d5ccc7e7efd515e34a6773d5448cd5337
Vinay S Shenoy <code@vinaysshenoy.com>
2019-12-02 12:54:58 +0530

    Introduce discrete view for displaying the list of prescribed drugs

--- app/src/main/java/org/simple/clinic/summary/SummaryPrescribedDrugsItem.kt
+++ app/src/main/java/org/simple/clinic/summary/SummaryPrescribedDrugsItem.kt
@@ -1,12 +1,12 @@
 package org.simple.clinic.summary
 
 import android.view.View
+import com.xwray.groupie.ViewHolder
 import io.reactivex.subjects.Subject
 import org.simple.clinic.R
 import org.simple.clinic.drugs.PrescribedDrug
 import org.simple.clinic.summary.SummaryListAdapterIds.PRESCRIBED_DRUGS
-import org.simple.clinic.summary.prescribeddrugs.DrugsSummaryViewHolder
-import org.simple.clinic.util.RelativeTimestampGenerator
+import org.simple.clinic.summary.prescribeddrugs.DrugSummaryView
 import org.simple.clinic.util.UserClock
 import org.simple.clinic.widgets.UiEvent
 import org.threeten.bp.format.DateTimeFormatter
@@ -15,19 +15,18 @@ data class SummaryPrescribedDrugsItem(
     val prescriptions: List<PrescribedDrug>,
     val dateFormatter: DateTimeFormatter,
     val userClock: UserClock
-) : GroupieItemWithUiEvents<DrugsSummaryViewHolder>(adapterId = PRESCRIBED_DRUGS) {
-
-  private val relativeTimestampGenerator = RelativeTimestampGenerator()
+) : GroupieItemWithUiEvents<ViewHolder>(adapterId = PRESCRIBED_DRUGS) {
 
   override lateinit var uiEvents: Subject<UiEvent>
 
   override fun getLayout() = R.layout.list_patientsummary_prescriptions
 
-  override fun createViewHolder(itemView: View): DrugsSummaryViewHolder {
-    return DrugsSummaryViewHolder(itemView) { uiEvents.onNext(PatientSummaryUpdateDrugsClicked()) }
+  override fun createViewHolder(itemView: View): ViewHolder {
+    return ViewHolder(itemView)
   }
 
-  override fun bind(holder: DrugsSummaryViewHolder, position: Int) {
-    holder.bind(prescriptions, dateFormatter, relativeTimestampGenerator, userClock)
+  override fun bind(holder: ViewHolder, position: Int) {
+    val drugSummaryView = holder.itemView as DrugSummaryView
+    drugSummaryView.bind(prescriptions, dateFormatter, userClock) { uiEvents.onNext(PatientSummaryUpdateDrugsClicked()) }
   }
 }

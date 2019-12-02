b8d1ef6bc2aecc610aea5f889175b7e59fe85dec
Vinay Shenoy <vinay@obvious.in>
2019-12-02 20:56:43 +0530

    Toggle CV history answer via a callback instead of event subject

--- app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryView.kt
+++ app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryView.kt
@@ -4,16 +4,13 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.cardview.widget.CardView
-import io.reactivex.subjects.Subject
 import kotlinx.android.synthetic.main.patientsummary_medicalhistoryview_content.view.*
 import org.simple.clinic.R
 import org.simple.clinic.medicalhistory.Answer
 import org.simple.clinic.medicalhistory.MedicalHistory
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestionView
-import org.simple.clinic.summary.SummaryMedicalHistoryAnswerToggled
 import org.simple.clinic.util.RelativeTimestamp
-import org.simple.clinic.widgets.UiEvent
 import org.threeten.bp.format.DateTimeFormatter
 
 class MedicalHistorySummaryView(
@@ -30,7 +27,7 @@ class MedicalHistorySummaryView(
       medicalHistory: MedicalHistory,
       lastUpdatedAt: RelativeTimestamp,
       dateFormatter: DateTimeFormatter,
-      uiEvents: Subject<UiEvent>
+      answerToggled: (MedicalHistoryQuestion, Answer) -> Unit
   ) {
     val updatedAtDisplayText = lastUpdatedAt.displayText(context, dateFormatter)
 
@@ -41,18 +38,14 @@ class MedicalHistorySummaryView(
 
     val renderQuestionView = { view: MedicalHistoryQuestionView, question: MedicalHistoryQuestion, answer: Answer ->
       view.render(question, answer)
-      view.answerChangeListener = { newAnswer ->
-        uiEvents.onNext(SummaryMedicalHistoryAnswerToggled(question, newAnswer))
-      }
+      view.answerChangeListener = { newAnswer -> answerToggled(question, newAnswer) }
     }
 
-    run {
-      renderQuestionView(diagnosedForHypertensionQuestionView, MedicalHistoryQuestion.DIAGNOSED_WITH_HYPERTENSION, medicalHistory.diagnosedWithHypertension)
-      renderQuestionView(treatmentForHypertensionQuestionView, MedicalHistoryQuestion.IS_ON_TREATMENT_FOR_HYPERTENSION, medicalHistory.isOnTreatmentForHypertension)
-      renderQuestionView(heartAttackQuestionView, MedicalHistoryQuestion.HAS_HAD_A_HEART_ATTACK, medicalHistory.hasHadHeartAttack)
-      renderQuestionView(strokeQuestionView, MedicalHistoryQuestion.HAS_HAD_A_STROKE, medicalHistory.hasHadStroke)
-      renderQuestionView(kidneyDiseaseQuestionView, MedicalHistoryQuestion.HAS_HAD_A_KIDNEY_DISEASE, medicalHistory.hasHadKidneyDisease)
-      renderQuestionView(diabetesQuestionView, MedicalHistoryQuestion.HAS_DIABETES, medicalHistory.hasDiabetes)
-    }
+    renderQuestionView(diagnosedForHypertensionQuestionView, MedicalHistoryQuestion.DIAGNOSED_WITH_HYPERTENSION, medicalHistory.diagnosedWithHypertension)
+    renderQuestionView(treatmentForHypertensionQuestionView, MedicalHistoryQuestion.IS_ON_TREATMENT_FOR_HYPERTENSION, medicalHistory.isOnTreatmentForHypertension)
+    renderQuestionView(heartAttackQuestionView, MedicalHistoryQuestion.HAS_HAD_A_HEART_ATTACK, medicalHistory.hasHadHeartAttack)
+    renderQuestionView(strokeQuestionView, MedicalHistoryQuestion.HAS_HAD_A_STROKE, medicalHistory.hasHadStroke)
+    renderQuestionView(kidneyDiseaseQuestionView, MedicalHistoryQuestion.HAS_HAD_A_KIDNEY_DISEASE, medicalHistory.hasHadKidneyDisease)
+    renderQuestionView(diabetesQuestionView, MedicalHistoryQuestion.HAS_DIABETES, medicalHistory.hasDiabetes)
   }
 }

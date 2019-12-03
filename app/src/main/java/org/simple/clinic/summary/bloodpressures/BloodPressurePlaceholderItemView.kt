6514f2209ea4b580557c29b78d601b812b93dc73
Vinay S Shenoy <vinay@obvious.in>
2019-12-03 14:08:38 +0530

    Move "New BP" into the `BloodPressureSummaryView`

--- app/src/main/java/org/simple/clinic/summary/bloodpressures/BloodPressurePlaceholderItemView.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/BloodPressurePlaceholderItemView.kt
@@ -3,14 +3,14 @@ package org.simple.clinic.summary.bloodpressures
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import androidx.cardview.widget.CardView
+import android.widget.FrameLayout
 import kotlinx.android.synthetic.main.patientsummary_bpplaceholderitem_content.view.*
 import org.simple.clinic.R
 
 class BloodPressurePlaceholderItemView(
     context: Context,
     attributeSet: AttributeSet
-) : CardView(context, attributeSet) {
+) : FrameLayout(context, attributeSet) {
 
   init {
     LayoutInflater.from(context).inflate(R.layout.patientsummary_bpplaceholderitem_content, this, true)

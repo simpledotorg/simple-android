7196bc77cbf86020851f78519303665e474dcc44
Vinay S Shenoy <code@vinaysshenoy.com>
2019-12-03 11:12:34 +0530

    Move BP rendering logic to `BloodPressureItemView`

--- app/src/main/java/org/simple/clinic/summary/SummaryBloodPressureListItem.kt
+++ app/src/main/java/org/simple/clinic/summary/SummaryBloodPressureListItem.kt
@@ -1,33 +1,19 @@
 package org.simple.clinic.summary
 
-import android.content.Context
-import android.content.res.Resources
-import android.text.style.ForegroundColorSpan
 import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
-import android.view.ViewGroup
 import androidx.annotation.VisibleForTesting
-import androidx.core.content.res.ResourcesCompat
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import io.reactivex.subjects.Subject
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.patientsummary_bpitem_content.*
 import org.simple.clinic.R
 import org.simple.clinic.bp.BloodPressureMeasurement
-import org.simple.clinic.util.Just
-import org.simple.clinic.util.None
+import org.simple.clinic.summary.bloodpressures.BloodPressureItemView
 import org.simple.clinic.util.RelativeTimestamp
 import org.simple.clinic.util.RelativeTimestampGenerator
-import org.simple.clinic.util.Truss
-import org.simple.clinic.util.Unicode
 import org.simple.clinic.util.UserClock
 import org.simple.clinic.util.UtcClock
 import org.simple.clinic.widgets.UiEvent
-import org.simple.clinic.widgets.setPaddingBottom
-import org.simple.clinic.widgets.setPaddingTop
-import org.simple.clinic.widgets.setTextAppearanceCompat
 import org.threeten.bp.Duration
 import org.threeten.bp.Instant
 import org.threeten.bp.ZoneId
@@ -102,87 +88,16 @@ data class SummaryBloodPressureListItem(
   }
 
   override fun bind(holder: BpViewHolder, position: Int) {
-    val context = holder.itemView.context
-    val resources = context.resources
-
-    holder.itemView.isClickable = isBpEditable
-    holder.itemView.isFocusable = isBpEditable
-    if (isBpEditable) holder.itemView.setOnClickListener { uiEvents.onNext(PatientSummaryBpClicked(measurement)) }
-
-    val level = measurement.level
-
-    holder.levelTextView.text = when (level.displayTextRes) {
-      is Just -> context.getString(level.displayTextRes.value)
-      is None -> ""
-    }
-
-    val readingsTextAppearanceResId = when {
-      level.isUrgent() -> R.style.Clinic_V2_TextAppearance_PatientSummary_BloodPressure_High
-      else -> R.style.Clinic_V2_TextAppearance_PatientSummary_BloodPressure_Normal
-    }
-    holder.readingsTextView.setTextAppearanceCompat(readingsTextAppearanceResId)
-    holder.readingsTextView.text = context.resources.getString(R.string.patientsummary_bp_reading, measurement.systolic, measurement.diastolic)
-
-    holder.daysAgoTextView.text = daysAgoWithEditButton(resources, context, daysAgo)
-
-    val measurementImageTint = when {
-      level.isUrgent() -> R.color.patientsummary_bp_reading_high
-      else -> R.color.patientsummary_bp_reading_normal
-    }
-    holder.heartImageView.imageTintList = ResourcesCompat.getColorStateList(resources, measurementImageTint, null)
-
-    holder.divider.visibility = if (showDivider) VISIBLE else GONE
-
-    holder.timeTextView.visibility = if (formattedTime != null) VISIBLE else GONE
-    holder.timeTextView.text = formattedTime
-
-    val multipleItemsInThisGroup = formattedTime != null
-    addTopPadding(holder.itemLayout, multipleItemsInThisGroup)
-    addBottomPadding(holder.itemLayout, multipleItemsInThisGroup)
-  }
-
-  private fun daysAgoWithEditButton(
-      resources: Resources,
-      context: Context,
-      daysAgo: RelativeTimestamp
-  ): CharSequence {
-    val daysAgoText = daysAgo.displayText(context, dateFormatter)
-    return when {
-      isBpEditable -> {
-        val colorSpanForEditLabel = ForegroundColorSpan(ResourcesCompat.getColor(resources, R.color.blue1, context.theme))
-        Truss()
-            .pushSpan(colorSpanForEditLabel)
-            .append(resources.getString(R.string.patientsummary_edit))
-            .popSpan()
-            .append(" ${Unicode.bullet} ")
-            .append(daysAgoText)
-            .build()
-
-      }
-      else -> daysAgoText
-    }
-  }
-
-  private fun addTopPadding(itemLayout: ViewGroup, multipleItemsInThisGroup: Boolean) {
-    if (multipleItemsInThisGroup) {
-      itemLayout.setPaddingTop(when {
-        addTopPadding -> R.dimen.patientsummary_bp_list_item_first_in_group_top_padding
-        else -> R.dimen.patientsummary_bp_list_item_multiple_in_group_bp_top_padding
-      })
-    } else {
-      itemLayout.setPaddingTop(R.dimen.patientsummary_bp_list_item_single_group_padding)
-    }
-  }
-
-  private fun addBottomPadding(itemLayout: ViewGroup, multipleItemsInThisGroup: Boolean) {
-    if (multipleItemsInThisGroup) {
-      itemLayout.setPaddingBottom(when {
-        showDivider -> R.dimen.patientsummary_bp_list_item_last_in_group_bottom_padding
-        else -> R.dimen.patientsummary_bp_list_item_multiple_in_group_bp_bottom_padding
-      })
-    } else {
-      itemLayout.setPaddingBottom(R.dimen.patientsummary_bp_list_item_single_group_padding)
-    }
+    (holder.itemView as BloodPressureItemView).render(
+        isBpEditable = isBpEditable,
+        uiEvents = uiEvents,
+        measurement = measurement,
+        daysAgo = daysAgo,
+        showDivider = showDivider,
+        formattedTime = formattedTime,
+        dateFormatter = dateFormatter,
+        addTopPadding = addTopPadding
+    )
   }
 
   override fun isSameAs(other: Item<*>?): Boolean {

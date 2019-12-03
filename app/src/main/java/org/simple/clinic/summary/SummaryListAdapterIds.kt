c22f16a866470fa7cb152e149674206a946b9c07
Vinay S Shenoy <vinay@obvious.in>
2019-12-03 13:55:38 +0530

    Remove unused summary list item class
    - Remove `SummaryBloodPressureListItem`
    - Remove `SummaryBloodPressurePlaceholderListItem`

--- app/src/main/java/org/simple/clinic/summary/SummaryListAdapterIds.kt
+++ app/src/main/java/org/simple/clinic/summary/SummaryListAdapterIds.kt
@@ -3,9 +3,4 @@ package org.simple.clinic.summary
 object SummaryListAdapterIds {
   const val ADD_PRESCRIPTION = -2L
   const val ADD_NEW_BP = -3L
-
-  // We can have upto three placeholders (might change in the future) so we can't hardcode an id.
-  // At the same time, we we have to choose IDs that will stop it from clashing with other IDs in the
-  // adapter, so we increment the placeholder number from a particular offset.
-  val BP_PLACEHOLDER = { placeholderNumber: Int -> -(placeholderNumber + 100L) }
 }

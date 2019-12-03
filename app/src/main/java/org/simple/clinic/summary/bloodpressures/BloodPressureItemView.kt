ac02e6e587163d67a5cf53da6fada7ddde7d5091
Vinay S Shenoy <code@vinaysshenoy.com>
2019-12-03 11:32:55 +0530

    Extract discrete methods for rending BP item

--- app/src/main/java/org/simple/clinic/summary/bloodpressures/BloodPressureItemView.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/BloodPressureItemView.kt
@@ -1,7 +1,6 @@
 package org.simple.clinic.summary.bloodpressures
 
 import android.content.Context
-import android.content.res.Resources
 import android.text.style.ForegroundColorSpan
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -48,47 +47,62 @@ class BloodPressureItemView(
     isFocusable = isBpEditable
     if (isBpEditable) setOnClickListener { uiEvents.onNext(PatientSummaryBpClicked(measurement)) }
 
-    val level = measurement.level
+    renderBpReading(measurement)
+    renderBpLevel(measurement)
+    renderRelativeTimestampWithEditButton(daysAgo, dateFormatter, isBpEditable)
+    setIconTint(measurement)
+    renderDivider(showDivider)
+    renderTimeOfDay(formattedTime)
+    addPadding(formattedTime, addTopPadding, showDivider)
+  }
 
-    levelTextView.text = when (level.displayTextRes) {
-      is Just -> context.getString(level.displayTextRes.value)
-      is None -> ""
-    }
-
-    val readingsTextAppearanceResId = when {
-      level.isUrgent() -> R.style.Clinic_V2_TextAppearance_PatientSummary_BloodPressure_High
-      else -> R.style.Clinic_V2_TextAppearance_PatientSummary_BloodPressure_Normal
-    }
-    readingsTextView.setTextAppearanceCompat(readingsTextAppearanceResId)
-    readingsTextView.text = context.resources.getString(R.string.patientsummary_bp_reading, measurement.systolic, measurement.diastolic)
-
-    daysAgoTextView.text = daysAgoWithEditButton(resources, context, daysAgo, dateFormatter, isBpEditable)
-
-    val measurementImageTint = when {
-      level.isUrgent() -> R.color.patientsummary_bp_reading_high
-      else -> R.color.patientsummary_bp_reading_normal
-    }
-    heartImageView.imageTintList = ResourcesCompat.getColorStateList(resources, measurementImageTint, null)
-
-    divider.visibility = if (showDivider) View.VISIBLE else View.GONE
-
-    timeTextView.visibility = if (formattedTime != null) View.VISIBLE else View.GONE
-    timeTextView.text = formattedTime
-
+  private fun addPadding(formattedTime: String?, addTopPadding: Boolean, showDivider: Boolean) {
     val multipleItemsInThisGroup = formattedTime != null
     addTopPadding(itemLayout = itemLayout, multipleItemsInThisGroup = multipleItemsInThisGroup, addTopPadding = addTopPadding)
     addBottomPadding(itemLayout = itemLayout, multipleItemsInThisGroup = multipleItemsInThisGroup, showDivider = showDivider)
   }
 
-  private fun daysAgoWithEditButton(
-      resources: Resources,
-      context: Context,
+  private fun renderTimeOfDay(formattedTime: String?) {
+    timeTextView.visibility = if (formattedTime != null) View.VISIBLE else View.GONE
+    timeTextView.text = formattedTime
+  }
+
+  private fun renderDivider(showDivider: Boolean) {
+    divider.visibility = if (showDivider) View.VISIBLE else View.GONE
+  }
+
+  private fun setIconTint(measurement: BloodPressureMeasurement) {
+    val measurementImageTint = when {
+      measurement.level.isUrgent() -> R.color.patientsummary_bp_reading_high
+      else -> R.color.patientsummary_bp_reading_normal
+    }
+    heartImageView.imageTintList = ResourcesCompat.getColorStateList(resources, measurementImageTint, null)
+  }
+
+  private fun renderBpReading(measurement: BloodPressureMeasurement) {
+    val readingsTextAppearanceResId = when {
+      measurement.level.isUrgent() -> R.style.Clinic_V2_TextAppearance_PatientSummary_BloodPressure_High
+      else -> R.style.Clinic_V2_TextAppearance_PatientSummary_BloodPressure_Normal
+    }
+    readingsTextView.setTextAppearanceCompat(readingsTextAppearanceResId)
+    readingsTextView.text = context.resources.getString(R.string.patientsummary_bp_reading, measurement.systolic, measurement.diastolic)
+  }
+
+  private fun renderBpLevel(measurement: BloodPressureMeasurement) {
+    levelTextView.text = when (measurement.level.displayTextRes) {
+      is Just -> context.getString(measurement.level.displayTextRes.value)
+      is None -> ""
+    }
+  }
+
+  private fun renderRelativeTimestampWithEditButton(
       daysAgo: RelativeTimestamp,
       dateFormatter: DateTimeFormatter,
       isBpEditable: Boolean
-  ): CharSequence {
+  ) {
     val daysAgoText = daysAgo.displayText(context, dateFormatter)
-    return when {
+
+    daysAgoTextView.text = when {
       isBpEditable -> {
         val colorSpanForEditLabel = ForegroundColorSpan(ResourcesCompat.getColor(resources, R.color.blue1, context.theme))
         Truss()

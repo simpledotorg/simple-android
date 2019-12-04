d75345d002fded8d6465b0f07e60b877b1dfc9e4
Vinay S Shenoy <vinay@obvious.in>
2019-12-04 11:44:11 +0530

    Create a separate module for assisted injections
    This is a limitation by the library that only one module in the DI
    graph can be assisted.

--- app/src/main/java/org/simple/clinic/setup/SetupActivityModule.kt
+++ app/src/main/java/org/simple/clinic/setup/SetupActivityModule.kt
@@ -1,15 +1,14 @@
 package org.simple.clinic.setup
 
-import com.squareup.inject.assisted.dagger2.AssistedModule
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.BuildConfig
 import org.simple.clinic.appconfig.Country
+import org.simple.clinic.di.AssistedInjectModule
 import java.net.URI
 import javax.inject.Named
 
-@AssistedModule
-@Module(includes = [AssistedInject_SetupActivityModule::class])
+@Module(includes = [AssistedInjectModule::class])
 class SetupActivityModule {
 
   @Provides

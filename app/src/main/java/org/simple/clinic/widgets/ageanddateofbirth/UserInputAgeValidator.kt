c2ef2aab666d3a1a86904adddcbb2f23b66a5442
janhavisinghh <janhavi.shanu@gmail.com>
2019-12-18 12:27:06 +0530

    Add date validation methods

--- app/src/main/java/org/simple/clinic/widgets/ageanddateofbirth/UserInputAgeValidator.kt
+++ app/src/main/java/org/simple/clinic/widgets/ageanddateofbirth/UserInputAgeValidator.kt
@@ -1,17 +1,31 @@
 package org.simple.clinic.widgets.ageanddateofbirth
 
+import androidx.annotation.VisibleForTesting
+import org.simple.clinic.util.UserClock
 import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator.Result.Invalid.AgeIsInvalid
+import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator.Result.Invalid.DateIsInvalid
 import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator.Result.Valid.AgeIsValid
+import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator.Result.Valid.DateIsValid
+import org.threeten.bp.LocalDate
+import org.threeten.bp.format.DateTimeFormatter
+import javax.inject.Inject
+import javax.inject.Named
 
-class UserInputAgeValidator {
+
+class UserInputAgeValidator @Inject constructor(
+        private val userClock: UserClock,
+        @Named("date_for_user_input") private val dateOfBirthFormat: DateTimeFormatter
+) {
 
     sealed class Result {
         sealed class Valid : Result() {
             data class AgeIsValid(val age: Int) : Valid()
+            data class DateIsValid(val parsedDate: LocalDate) : Valid()
         }
 
         sealed class Invalid : Result() {
             object AgeIsInvalid : Invalid()
+            object DateIsInvalid : Invalid()
         }
     }
 
@@ -23,4 +37,16 @@ class UserInputAgeValidator {
         }
     }
 
+    fun dateValidator(dateText: String, nowDate: LocalDate = dateInUserTimeZone()): Result {
+        val parsedDate = dateOfBirthFormat.parse(dateText, LocalDate::from)
+        return when {
+            parsedDate < nowDate.minusYears(120) -> DateIsInvalid
+            else -> DateIsValid(parsedDate)
+        }
+    }
+
+    @VisibleForTesting
+    fun dateInUserTimeZone(): LocalDate {
+        return LocalDate.now(userClock)
+    }
 }
--- app/src/test/java/org/simple/clinic/newentry/UserInputAgeValidatorTest.kt
+++ app/src/test/java/org/simple/clinic/newentry/UserInputAgeValidatorTest.kt
@@ -2,12 +2,21 @@ package org.simple.clinic.newentry
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import org.simple.clinic.util.TestUserClock
 import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator
 import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator.Result.Invalid.AgeIsInvalid
+import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator.Result.Invalid.DateIsInvalid
 import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator.Result.Valid.AgeIsValid
+import org.simple.clinic.widgets.ageanddateofbirth.UserInputAgeValidator.Result.Valid.DateIsValid
+import org.threeten.bp.LocalDate
+import org.threeten.bp.format.DateTimeFormatter
+import java.util.*
+import javax.inject.Inject
 
 class UserInputAgeValidatorTest {
-    private val validator = UserInputAgeValidator()
+    @Inject
+    private val validator = UserInputAgeValidator(userClock = TestUserClock(LocalDate.parse("2018-01-01")),
+            dateOfBirthFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH))
 
     @Test
     fun `when user inputs age greater than 120 years, then return invalid error`() {
@@ -43,6 +52,42 @@ class UserInputAgeValidatorTest {
 
         //then
         assertThat(expectedResult).isEqualTo(AgeIsValid(age))
+    }
+
+    @Test
+    fun `when user inputs date greater than 120 years, return invalid`() {
+        //given
+        val dateText = "12/03/1810"
+
+        //when
+        val validation = validator.dateValidator(dateText)
+
+        //then
+        assertThat(validation).isEqualTo(DateIsInvalid)
+    }
+
+    @Test
+    fun `when user inputs date less than 120 years, then return valid`() {
+        //given
+        val dateText = "15/12/2019"
+
+        //when
+        val validation = validator.dateValidator(dateText)
+
+        //then
+        assertThat(validation).isEqualTo(DateIsValid(LocalDate.of(2019, 12, 15)))
+    }
+
+    @Test
+    fun `when user inputs date equal to 120 years, then return valid`() {
+        //given
+        val dateText = "17/12/1899"
+
+        //when
+        val validation = validator.dateValidator(dateText)
+
+        //then
+        assertThat(validation).isEqualTo(DateIsValid(LocalDate.of(1899, 12, 17)))
     }
 
 

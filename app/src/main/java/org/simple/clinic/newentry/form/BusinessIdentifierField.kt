4045567a87a2fde5c0346f7c4fe84cc6b0019562
Rakshak R.Hegde <rakshak@obvious.in>
2019-12-23 12:58:00 +0530

    Remove BusinessIdentifierField

--- app/src/main/java/org/simple/clinic/newentry/form/BusinessIdentifierField.kt
+++
@@ -1,9 +0,0 @@
-package org.simple.clinic.newentry.form
-
-import org.simple.clinic.newentry.form.ValidationError.MissingValue
-
-class BusinessIdentifierField : InputField<String>() {
-  override fun validate(value: String): Set<ValidationError> {
-    return if (value.isBlank()) setOf(MissingValue) else emptySet()
-  }
-}

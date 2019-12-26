5e7a6615f2d7511734b934d86fb6efbea4832adf
Sanchita Agarwal <sanchita@obvious.in>
2019-12-26 17:59:59 +0530

    Open type selector when new blood sugar is clicked

--- app/src/test/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewUpdateTest.kt
+++ app/src/test/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewUpdateTest.kt
@@ -1,24 +1,37 @@
 package org.simple.clinic.summary.bloodsugar
 
+import com.spotify.mobius.test.NextMatchers.hasEffects
 import com.spotify.mobius.test.NextMatchers.hasModel
 import com.spotify.mobius.test.NextMatchers.hasNoEffects
+import com.spotify.mobius.test.NextMatchers.hasNoModel
 import com.spotify.mobius.test.UpdateSpec
 import com.spotify.mobius.test.UpdateSpec.assertThatNext
 import org.junit.Test
 
 class BloodSugarSummaryViewUpdateTest {
 
+  private val spec = UpdateSpec<BloodSugarSummaryViewModel, BloodSugarSummaryViewEvent, BloodSugarSummaryViewEffect>(BloodSugarSummaryViewUpdate())
+  private val defaultModel = BloodSugarSummaryViewModel.EMPTY
+
   @Test
   fun `when blood sugar is loaded then ui must be updated`() {
-    val spec = UpdateSpec<BloodSugarSummaryViewModel, BloodSugarSummaryViewEvent, BloodSugarSummaryViewEffect>(BloodSugarSummaryViewUpdate())
-    val defaultModel = BloodSugarSummaryViewModel.EMPTY
-
     spec
         .given(defaultModel)
         .whenEvent(BloodSugarSummaryFetched)
         .then(assertThatNext(
             hasModel(defaultModel.summaryFetched()),
             hasNoEffects()
+        ))
+  }
+
+  @Test
+  fun `when add new blood sugar is clicked then blood sugar type selector should open`() {
+    spec
+        .given(defaultModel)
+        .whenEvent(NewBloodSugarClicked)
+        .then(assertThatNext(
+            hasNoModel(),
+            hasEffects(OpenBloodSugarTypeSelector as BloodSugarSummaryViewEffect)
         ))
   }
 }

871cf3949d2459595a1893d1b4e168d7c963aa63
Sanchita Agarwal <sanchita@obvious.in>
2019-12-26 14:27:42 +0530

    Resync appointments to fetch creation_facility_id

--- app/src/main/java/org/simple/clinic/overdue/AppointmentSyncApi.kt
+++ app/src/main/java/org/simple/clinic/overdue/AppointmentSyncApi.kt
@@ -15,7 +15,7 @@ interface AppointmentSyncApi {
       @Body body: AppointmentPushRequest
   ): Single<DataPushResponse>
 
-  @Headers(value = ["X-RESYNC-TOKEN: 1"])
+  @Headers(value = ["X-RESYNC-TOKEN: 2"])
   @GET("appointments/sync")
   fun pull(
       @Query("limit") recordsToPull: Int,

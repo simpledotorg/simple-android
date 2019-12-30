b04c52c536671a6301ec363ad7d219e3f7b7e73e
Sasikanth Miriyampalli <sasikanth@obvious.in>
2019-12-30 12:42:13 +0530

    Add `BloodSugarEntryUi`

--- app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntrySheet.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntrySheet.kt
@@ -3,8 +3,9 @@ package org.simple.clinic.bloodsugar.entry
 import android.os.Bundle
 import org.simple.clinic.R
 import org.simple.clinic.widgets.BottomSheetActivity
+import org.threeten.bp.LocalDate
 
-class BloodSugarEntrySheet : BottomSheetActivity() {
+class BloodSugarEntrySheet : BottomSheetActivity(), BloodSugarEntryUi {
 
   enum class ScreenType {
     BLOOD_SUGAR_ENTRY,
@@ -15,5 +16,69 @@ class BloodSugarEntrySheet : BottomSheetActivity() {
     super.onCreate(savedInstanceState)
 
     setContentView(R.layout.sheet_blood_sugar_entry)
+  }
+
+  override fun setBloodSugarSavedResultAndFinish() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun hideBloodSugarErrorMessage() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showBloodSugarEmptyError() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showBloodSugarHighError() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showBloodSugarLowError() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showRandomBloodSugarTitle() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showPostPrandialBloodSugarTitle() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showFastingBloodSugarTitle() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showBloodSugarEntryScreen() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showDateEntryScreen() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showInvalidDateError() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showDateIsInFutureError() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun hideDateErrorMessage() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun setDateOnInputFields(dayOfMonth: String, month: String, twoDigitYear: String) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun showDateOnDateButton(date: LocalDate) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun dismiss() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
   }
 }
---
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntryUi.kt
@@ -0,0 +1,22 @@
+package org.simple.clinic.bloodsugar.entry
+
+import org.threeten.bp.LocalDate
+
+interface BloodSugarEntryUi {
+  fun setBloodSugarSavedResultAndFinish()
+  fun hideBloodSugarErrorMessage()
+  fun showBloodSugarEmptyError()
+  fun showBloodSugarHighError()
+  fun showBloodSugarLowError()
+  fun showRandomBloodSugarTitle()
+  fun showPostPrandialBloodSugarTitle()
+  fun showFastingBloodSugarTitle()
+  fun showBloodSugarEntryScreen()
+  fun showDateEntryScreen()
+  fun showInvalidDateError()
+  fun showDateIsInFutureError()
+  fun hideDateErrorMessage()
+  fun setDateOnInputFields(dayOfMonth: String, month: String, twoDigitYear: String)
+  fun showDateOnDateButton(date: LocalDate)
+  fun dismiss()
+}

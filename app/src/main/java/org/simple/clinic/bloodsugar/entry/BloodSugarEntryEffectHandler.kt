01bb6044d50c3a7433e176b78c5eaf62c66434dd
Sasikanth Miriyampalli <sasikanth@obvious.in>
2019-12-31 12:58:52 +0530

    Handle `ShowBloodSugarValidationError` effect

--- app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntryEffectHandler.kt
@@ -2,6 +2,9 @@ package org.simple.clinic.bloodsugar.entry
 
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarEmpty
+import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarTooHigh
+import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarTooLow
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class BloodSugarEntryEffectHandler(
@@ -15,6 +18,15 @@ class BloodSugarEntryEffectHandler(
         .addAction(HideDateErrorMessage::class.java, ui::hideDateErrorMessage, schedulersProvider.ui())
         .addAction(Dismiss::class.java, ui::dismiss, schedulersProvider.ui())
         .addAction(ShowDateEntryScreen::class.java, ui::showDateEntryScreen, schedulersProvider.ui())
+        .addConsumer(ShowBloodSugarValidationError::class.java, { showBloodSugarValidationError(it.result) }, schedulersProvider.ui())
         .build()
+  }
+
+  private fun showBloodSugarValidationError(result: BloodSugarValidator.Result) {
+    when (result) {
+      ErrorBloodSugarEmpty -> ui.showBloodSugarEmptyError()
+      ErrorBloodSugarTooHigh -> ui.showBloodSugarHighError()
+      ErrorBloodSugarTooLow -> ui.showBloodSugarLowError()
+    }
   }
 }

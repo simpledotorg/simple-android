1987b88eb78b7bb824d16a3f1d9829ec5783182c
Sanchita Agarwal <sanchita@obvious.in>
2020-01-02 19:01:35 +0530

    Update BloodSugarSummaryViewModel to add measurements

--- app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewModel.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewModel.kt
@@ -1,16 +1,20 @@
 package org.simple.clinic.summary.bloodsugar
 
+import org.simple.clinic.bloodsugar.BloodSugarMeasurement
 import java.util.UUID
 
-data class BloodSugarSummaryViewModel(val patientUuid: UUID) {
+data class BloodSugarSummaryViewModel(
+    val patientUuid: UUID,
+    val measurements: List<BloodSugarMeasurement>
+) {
 
   companion object {
     fun create(patientUuid: UUID): BloodSugarSummaryViewModel {
-      return BloodSugarSummaryViewModel(patientUuid)
+      return BloodSugarSummaryViewModel(patientUuid, emptyList())
     }
   }
 
-  fun summaryFetched(): BloodSugarSummaryViewModel {
-    return create(patientUuid)
+  fun summaryFetched(measurements: List<BloodSugarMeasurement>): BloodSugarSummaryViewModel {
+    return copy(measurements = measurements)
   }
 }

d3f00d28b03f45c9a96cb854c402501acd69f3e2
Vinay S Shenoy <code@vinaysshenoy.com>
2020-01-02 14:58:47 +0530

    Remove `HomescreenIllustration` class

--- app/src/main/java/org/simple/clinic/illustration/HomescreenIllustration.kt
+++
@@ -1,8 +0,0 @@
-package org.simple.clinic.illustration
-
-data class HomescreenIllustration(
-    val eventId: String,
-    val illustrationUrl: String,
-    val from: DayOfMonth,
-    val to: DayOfMonth
-)

740403119603c76d526631474d03b4efb5d71720
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-01-03 09:00:22 +0530

    Make `BloodSugarMeasurementType` and `OpenAs` sealed classes parcelable

--- app/src/main/java/org/simple/clinic/bloodsugar/BloodSugarMeasurementType.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/BloodSugarMeasurementType.kt
@@ -1,11 +1,13 @@
 package org.simple.clinic.bloodsugar
 
+import android.os.Parcelable
 import androidx.room.TypeConverter
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
+import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.util.room.SafeEnumTypeAdapter
 
-sealed class BloodSugarMeasurementType {
+sealed class BloodSugarMeasurementType : Parcelable {
 
   object TypeAdapter : SafeEnumTypeAdapter<BloodSugarMeasurementType>(
       knownMappings = mapOf(
@@ -36,7 +38,14 @@ sealed class BloodSugarMeasurementType {
   }
 }
 
+@Parcelize
 object Random : BloodSugarMeasurementType()
+
+@Parcelize
 object PostPrandial : BloodSugarMeasurementType()
+
+@Parcelize
 object Fasting : BloodSugarMeasurementType()
+
+@Parcelize
 data class Unknown(val actualValue: String) : BloodSugarMeasurementType()

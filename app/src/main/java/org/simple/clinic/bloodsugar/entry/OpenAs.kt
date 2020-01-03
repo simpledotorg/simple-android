7f43c615103ecc9dc1b618aa255d980564bc85bf
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-03 09:00:22 +0530

    Make `BloodSugarMeasurementType` and `OpenAs` sealed classes parcelable

--- app/src/main/java/org/simple/clinic/bloodsugar/entry/OpenAs.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/OpenAs.kt
@@ -1,8 +1,11 @@
 package org.simple.clinic.bloodsugar.entry
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.bloodsugar.BloodSugarMeasurementType
 import java.util.UUID
 
-sealed class OpenAs
+sealed class OpenAs : Parcelable
 
+@Parcelize
 data class New(val patientId: UUID, val measurementType: BloodSugarMeasurementType) : OpenAs()

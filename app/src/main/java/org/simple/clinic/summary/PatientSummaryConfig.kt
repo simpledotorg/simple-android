c46711c08fe0df60afb09744cb749da125edbee2
Sanchita Agarwal <sanchita@obvious.in>
2020-01-07 18:29:36 +0530

    Add numberOfBloodSugarsToDisplay in PatientSummaryConfig

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
@@ -6,7 +6,8 @@ import org.threeten.bp.Duration
 data class PatientSummaryConfig(
     val numberOfBpPlaceholders: Int,
     val numberOfBpsToDisplay: Int,
-    val bpEditableDuration: Duration
+    val bpEditableDuration: Duration,
+    val numberOfBloodSugarsToDisplay: Int
 ) {
 
   companion object {
@@ -17,7 +18,8 @@ data class PatientSummaryConfig(
       return PatientSummaryConfig(
           numberOfBpPlaceholders = 3,
           numberOfBpsToDisplay = 100,
-          bpEditableDuration = Duration.ofSeconds(bpEditableDurationConfig))
+          bpEditableDuration = Duration.ofSeconds(bpEditableDurationConfig),
+          numberOfBloodSugarsToDisplay = 100)
     }
   }
 }

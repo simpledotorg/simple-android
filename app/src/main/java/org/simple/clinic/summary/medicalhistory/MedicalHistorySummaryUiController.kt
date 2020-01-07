ca4297e17b025a4c42bda0f41a4bbab5bd34a6cb
Vinay S Shenoy <code@vinaysshenoy.com>
2020-01-07 16:41:35 +0530

    Setup controller in `MedicalHistorySummaryView`

--- app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryUiController.kt
+++ app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryUiController.kt
@@ -1,33 +1,44 @@
 package org.simple.clinic.summary.medicalhistory
 
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.rxkotlin.withLatestFrom
+import org.simple.clinic.ReplayUntilScreenIsDestroyed
+import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.medicalhistory.MedicalHistoryRepository
-import org.simple.clinic.summary.PatientSummaryScreenUi
 import org.simple.clinic.util.UtcClock
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
 import org.threeten.bp.Instant
 import java.util.UUID
 
-// TODO(vs): 2020-01-07 Change to screen specific types
-typealias Ui = PatientSummaryScreenUi
+typealias Ui = MedicalHistorySummaryUi
 
 typealias UiChange = (Ui) -> Unit
 
-class MedicalHistorySummaryUiController(
-    private val patientUuid: UUID,
+class MedicalHistorySummaryUiController @AssistedInject constructor(
+    @Assisted private val patientUuid: UUID,
     private val repository: MedicalHistoryRepository,
     private val clock: UtcClock
 ) : ObservableTransformer<UiEvent, UiChange> {
 
+  @AssistedInject.Factory
+  interface Factory {
+    fun create(patientUuid: UUID): MedicalHistorySummaryUiController
+  }
+
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
+    val replayedEvents = ReplayUntilScreenIsDestroyed(events)
+        .compose(ReportAnalyticsEvents())
+        .replay()
+
     return Observable.merge(
-        displayMedicalHistory(events),
-        updateMedicalHistory(events)
+        displayMedicalHistory(replayedEvents),
+        updateMedicalHistory(replayedEvents)
     )
   }
 
@@ -35,7 +46,7 @@ class MedicalHistorySummaryUiController(
     return events
         .ofType<ScreenCreated>()
         .switchMap { repository.historyForPatientOrDefault(patientUuid) }
-        .map { { ui: Ui -> ui.medicalHistorySummaryUi().populateMedicalHistory(it) } }
+        .map { { ui: Ui -> ui.populateMedicalHistory(it) } }
   }
 
   private fun updateMedicalHistory(events: Observable<UiEvent>): Observable<UiChange> {
--- app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryView.kt
+++ app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryView.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.cardview.widget.CardView
+import com.jakewharton.rxbinding3.view.detaches
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.medicalhistory_summary_view.view.*
 import org.simple.clinic.R
+import org.simple.clinic.bindUiToController
 import org.simple.clinic.di.injector
 import org.simple.clinic.medicalhistory.Answer
 import org.simple.clinic.medicalhistory.MedicalHistory
@@ -17,25 +21,36 @@ import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_KIDNEY_
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_STROKE
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IS_ON_TREATMENT_FOR_HYPERTENSION
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestionView
+import org.simple.clinic.router.screen.ScreenRouter
+import org.simple.clinic.summary.PatientSummaryScreenKey
 import org.simple.clinic.util.RelativeTimestamp
 import org.simple.clinic.util.RelativeTimestampGenerator
 import org.simple.clinic.util.UserClock
+import org.simple.clinic.widgets.ScreenCreated
+import org.simple.clinic.widgets.ScreenDestroyed
+import org.simple.clinic.widgets.UiEvent
 import org.threeten.bp.format.DateTimeFormatter
 import javax.inject.Inject
 import javax.inject.Named
-
-private typealias AnswerToggled = (MedicalHistoryQuestion, Answer) -> Unit
 
 class MedicalHistorySummaryView(
     context: Context,
     attributeSet: AttributeSet
 ) : CardView(context, attributeSet), MedicalHistorySummaryUi {
 
+  private val internalEvents = PublishSubject.create<MedicalHistorySummaryEvent>()
+
   @Inject
   lateinit var timestampGenerator: RelativeTimestampGenerator
 
   @Inject
   lateinit var userClock: UserClock
+
+  @Inject
+  lateinit var screenRouter: ScreenRouter
+
+  @Inject
+  lateinit var controllerFactory: MedicalHistorySummaryUiController.Factory
 
   @field:[Inject Named("exact_date")]
   lateinit var exactDateFormatter: DateTimeFormatter
@@ -47,14 +62,23 @@ class MedicalHistorySummaryView(
 
   override fun onFinishInflate() {
     super.onFinishInflate()
-    if(isInEditMode) {
+    if (isInEditMode) {
       return
     }
 
     context.injector<MedicalHistorySummaryViewInjector>().inject(this)
+
+    val screenKey = screenRouter.key<PatientSummaryScreenKey>(this)
+
+    bindUiToController(
+        ui = this,
+        events = Observable.merge(screenCreates(), internalEvents),
+        controller = controllerFactory.create(screenKey.patientUuid),
+        screenDestroys = detaches().map { ScreenDestroyed() }
+    )
   }
 
-  var answerToggled: AnswerToggled? = null
+  private fun screenCreates(): Observable<UiEvent> = Observable.just(ScreenCreated())
 
   override fun populateMedicalHistory(medicalHistory: MedicalHistory) {
     bind(
@@ -64,7 +88,7 @@ class MedicalHistorySummaryView(
     )
   }
 
-  fun bind(
+  private fun bind(
       medicalHistory: MedicalHistory,
       lastUpdatedAt: RelativeTimestamp,
       dateFormatter: DateTimeFormatter
@@ -76,20 +100,24 @@ class MedicalHistorySummaryView(
         updatedAtDisplayText
     )
 
-    diagnosedForHypertensionQuestionView.render(DIAGNOSED_WITH_HYPERTENSION, medicalHistory.diagnosedWithHypertension, answerToggled)
-    treatmentForHypertensionQuestionView.render(IS_ON_TREATMENT_FOR_HYPERTENSION, medicalHistory.isOnTreatmentForHypertension, answerToggled)
-    heartAttackQuestionView.render(HAS_HAD_A_HEART_ATTACK, medicalHistory.hasHadHeartAttack, answerToggled)
-    strokeQuestionView.render(HAS_HAD_A_STROKE, medicalHistory.hasHadStroke, answerToggled)
-    kidneyDiseaseQuestionView.render(HAS_HAD_A_KIDNEY_DISEASE, medicalHistory.hasHadKidneyDisease, answerToggled)
-    diabetesQuestionView.render(HAS_DIABETES, medicalHistory.hasDiabetes, answerToggled)
+    diagnosedForHypertensionQuestionView.render(DIAGNOSED_WITH_HYPERTENSION, medicalHistory.diagnosedWithHypertension, ::answerToggled)
+    treatmentForHypertensionQuestionView.render(IS_ON_TREATMENT_FOR_HYPERTENSION, medicalHistory.isOnTreatmentForHypertension, ::answerToggled)
+    heartAttackQuestionView.render(HAS_HAD_A_HEART_ATTACK, medicalHistory.hasHadHeartAttack, ::answerToggled)
+    strokeQuestionView.render(HAS_HAD_A_STROKE, medicalHistory.hasHadStroke, ::answerToggled)
+    kidneyDiseaseQuestionView.render(HAS_HAD_A_KIDNEY_DISEASE, medicalHistory.hasHadKidneyDisease, ::answerToggled)
+    diabetesQuestionView.render(HAS_DIABETES, medicalHistory.hasDiabetes, ::answerToggled)
+  }
+
+  private fun answerToggled(question: MedicalHistoryQuestion, answer: Answer) {
+    internalEvents.onNext(SummaryMedicalHistoryAnswerToggled(question, answer))
   }
 
   private fun MedicalHistoryQuestionView.render(
       question: MedicalHistoryQuestion,
       answer: Answer,
-      answerToggled: AnswerToggled?
+      answerToggled: (MedicalHistoryQuestion, Answer) -> Unit
   ) {
     render(question, answer)
-    answerChangeListener = { newAnswer -> answerToggled?.invoke(question, newAnswer) }
+    answerChangeListener = { newAnswer -> answerToggled.invoke(question, newAnswer) }
   }
 }

f78111120fa44f1a7cfd3b75e9036219a5416d1f
Vinay S Shenoy <vinay@obvious.in>
2020-01-07 14:20:14 +0530

    Clean up `MedicalHistorySummaryUiController` and test
    - Add convenience method to `MedicalHistory` to update an answer
    - Clean up controller and test to use the convenience method

--- app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryUiController.kt
+++ app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryUiController.kt
@@ -5,14 +5,12 @@ import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.rxkotlin.withLatestFrom
-import org.simple.clinic.medicalhistory.Answer
-import org.simple.clinic.medicalhistory.MedicalHistory
-import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
-import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.*
 import org.simple.clinic.medicalhistory.MedicalHistoryRepository
 import org.simple.clinic.summary.PatientSummaryScreenUi
+import org.simple.clinic.util.UtcClock
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
+import org.threeten.bp.Instant
 import java.util.UUID
 
 // TODO(vs): 2020-01-07 Change to screen specific types
@@ -22,7 +20,8 @@ typealias UiChange = (Ui) -> Unit
 
 class MedicalHistorySummaryUiController(
     private val patientUuid: UUID,
-    private val repository: MedicalHistoryRepository
+    private val repository: MedicalHistoryRepository,
+    private val clock: UtcClock
 ) : ObservableTransformer<UiEvent, UiChange> {
 
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
@@ -42,25 +41,12 @@ class MedicalHistorySummaryUiController(
   private fun updateMedicalHistory(events: Observable<UiEvent>): Observable<UiChange> {
     val medicalHistories = repository.historyForPatientOrDefault(patientUuid)
 
-    val updateHistory = { medicalHistory: MedicalHistory, question: MedicalHistoryQuestion, answer: Answer ->
-      when (question) {
-        DIAGNOSED_WITH_HYPERTENSION -> medicalHistory.copy(diagnosedWithHypertension = answer)
-        IS_ON_TREATMENT_FOR_HYPERTENSION -> medicalHistory.copy(isOnTreatmentForHypertension = answer)
-        HAS_HAD_A_HEART_ATTACK -> medicalHistory.copy(hasHadHeartAttack = answer)
-        HAS_HAD_A_STROKE -> medicalHistory.copy(hasHadStroke = answer)
-        HAS_HAD_A_KIDNEY_DISEASE -> medicalHistory.copy(hasHadKidneyDisease = answer)
-        HAS_DIABETES -> medicalHistory.copy(hasDiabetes = answer)
-      }
-    }
-
     return events.ofType<SummaryMedicalHistoryAnswerToggled>()
         .withLatestFrom(medicalHistories)
-        .map { (toggleEvent, medicalHistory) ->
-          updateHistory(medicalHistory, toggleEvent.question, toggleEvent.answer)
-        }
-        .flatMap {
+        .map { (toggleEvent, medicalHistory) -> medicalHistory.answered(toggleEvent.question, toggleEvent.answer) }
+        .flatMap { medicalHistory ->
           repository
-              .save(it)
+              .save(medicalHistory, Instant.now(clock))
               .andThen(Observable.never<UiChange>())
         }
   }

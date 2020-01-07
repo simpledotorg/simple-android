6f6dd4d993f3d4f3e6de7dc3eb963fa143e1060f
Vinay S Shenoy <vinay@obvious.in>
2020-01-07 13:01:57 +0530

    Remove `PatientSummaryItems#medicalHistory`

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryItems.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryItems.kt
@@ -1,9 +1,5 @@
 package org.simple.clinic.summary
 
 import org.simple.clinic.drugs.PrescribedDrug
-import org.simple.clinic.medicalhistory.MedicalHistory
 
-data class PatientSummaryItems(
-    val prescription: List<PrescribedDrug>,
-    val medicalHistory: MedicalHistory
-)
+data class PatientSummaryItems(val prescription: List<PrescribedDrug>)

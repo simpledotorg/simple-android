b6ad1c1ba42b21b471d705f23009a48d04b84112
Sanchita Agarwal <sanchita@obvious.in>
2020-01-08 18:20:45 +0530

    Make BloodSugarSummaryViewEvent a type of UiEvent

--- app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewEvent.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewEvent.kt
@@ -1,8 +1,9 @@
 package org.simple.clinic.summary.bloodsugar
 
 import org.simple.clinic.bloodsugar.BloodSugarMeasurement
+import org.simple.clinic.widgets.UiEvent
 
-sealed class BloodSugarSummaryViewEvent
+sealed class BloodSugarSummaryViewEvent : UiEvent
 
 data class BloodSugarSummaryFetched(val measurements: List<BloodSugarMeasurement>) : BloodSugarSummaryViewEvent()
 

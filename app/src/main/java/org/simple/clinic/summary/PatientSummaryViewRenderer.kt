1746d71e085709b92d04d17d9d6c6a2fb5781404
Vinay S Shenoy <vinay@obvious.in>
2020-01-08 16:13:32 +0530

    Add mobius classes for `PatientSummaryScreenController`

---
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryViewRenderer.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.summary
+
+import org.simple.clinic.mobius.ViewRenderer
+
+class PatientSummaryViewRenderer(
+    private val ui: PatientSummaryScreenUi
+): ViewRenderer<PatientSummaryModel> {
+
+  override fun render(model: PatientSummaryModel) {
+    // Nothing to do here
+  }
+}

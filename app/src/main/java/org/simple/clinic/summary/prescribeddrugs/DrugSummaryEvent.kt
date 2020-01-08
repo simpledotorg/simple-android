e1e6a8b728f80c38e868e4b352f3b646eecdaed8
Vinay S Shenoy <vinay@obvious.in>
2020-01-08 12:43:30 +0530

    Move `PatientSummaryUpdateDrugsClicked` to `DrugSummaryEvent`

--- app/src/main/java/org/simple/clinic/summary/prescribeddrugs/DrugSummaryEvent.kt
+++ app/src/main/java/org/simple/clinic/summary/prescribeddrugs/DrugSummaryEvent.kt
@@ -3,3 +3,7 @@ package org.simple.clinic.summary.prescribeddrugs
 import org.simple.clinic.widgets.UiEvent
 
 sealed class DrugSummaryEvent : UiEvent
+
+class PatientSummaryUpdateDrugsClicked : DrugSummaryEvent() {
+  override val analyticsName = "Patient Summary:Update Drugs Clicked"
+}

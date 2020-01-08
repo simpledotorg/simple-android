c9aa2fd0024fb9a0e4be59ddbfcfe9acfe0ed561
Sanchita Agarwal <sanchita@obvious.in>
2020-01-08 18:12:48 +0530

    Make BloodSugarSummaryViewModel as Parcelable

--- app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewModel.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewModel.kt
@@ -1,12 +1,15 @@
 package org.simple.clinic.summary.bloodsugar
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.bloodsugar.BloodSugarMeasurement
 import java.util.UUID
 
+@Parcelize
 data class BloodSugarSummaryViewModel(
     val patientUuid: UUID,
     val measurements: List<BloodSugarMeasurement>?
-) {
+) : Parcelable {
 
   companion object {
     fun create(patientUuid: UUID): BloodSugarSummaryViewModel {

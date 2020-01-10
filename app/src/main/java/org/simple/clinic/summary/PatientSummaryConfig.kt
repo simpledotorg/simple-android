948febc97355d7007d00c59184f2064160851bf3
Sanchita Agarwal <sanchita@obvious.in>
2020-01-10 13:22:50 +0530

    Add isDiabetesEnabled flag to PatientSummaryConfig

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
@@ -7,19 +7,22 @@ data class PatientSummaryConfig(
     val numberOfBpPlaceholders: Int,
     val numberOfBpsToDisplay: Int,
     val bpEditableDuration: Duration,
-    val numberOfBloodSugarsToDisplay: Int
+    val numberOfBloodSugarsToDisplay: Int,
+    val isDiabetesEnabled: Boolean
 ) {
 
   companion object {
 
     fun read(configReader: ConfigReader): PatientSummaryConfig {
       val bpEditableDurationConfig = configReader.long("bp_editable_duration_in_seconds", 3600)
+      val isDiabetesEnabled = configReader.boolean("diabetes_enabled", false)
 
       return PatientSummaryConfig(
           numberOfBpPlaceholders = 3,
           numberOfBpsToDisplay = 100,
           bpEditableDuration = Duration.ofSeconds(bpEditableDurationConfig),
-          numberOfBloodSugarsToDisplay = 100)
+          numberOfBloodSugarsToDisplay = 100,
+          isDiabetesEnabled = isDiabetesEnabled)
     }
   }
 }

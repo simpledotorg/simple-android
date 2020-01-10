d0e5923304ce12e7402e66965eb083417dbdb152
Vinay S Shenoy <vinay@obvious.in>
2020-01-10 14:49:08 +0530

    Move `PatientSummaryProfile` to the `.summary` package

---
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryProfile.kt
@@ -0,0 +1,16 @@
+package org.simple.clinic.summary
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.patient.Patient
+import org.simple.clinic.patient.PatientAddress
+import org.simple.clinic.patient.PatientPhoneNumber
+import org.simple.clinic.patient.businessid.BusinessId
+
+@Parcelize
+data class PatientSummaryProfile(
+    val patient: Patient,
+    val address: PatientAddress,
+    val phoneNumber: PatientPhoneNumber?,
+    val bpPassport: BusinessId?
+): Parcelable

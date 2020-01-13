7b90fd67faa7caa697aa6bd68585ac775e9456f4
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-13 13:02:55 +0530

    Open update sheet when blood pressure is clicked

--- app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenEffect.kt
+++ app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenEffect.kt
@@ -1,9 +1,12 @@
 package org.simple.clinic.bp.history
 
 import java.util.UUID
+import org.simple.clinic.bp.BloodPressureMeasurement
 
 sealed class BloodPressureHistoryScreenEffect
 
 data class LoadBloodPressureHistory(val patientUuid: UUID) : BloodPressureHistoryScreenEffect()
 
 object OpenBloodPressureEntrySheet : BloodPressureHistoryScreenEffect()
+
+data class OpenBloodPressureUpdateSheet(val bloodPressureMeasurement: BloodPressureMeasurement) : BloodPressureHistoryScreenEffect()

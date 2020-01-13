3b582ee420236da966e1c8027afe46d11bccd1ba
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-13 14:10:38 +0530

    Handle `OpenBloodPressureEntrySheet` effect

--- app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenEffectHandler.kt
@@ -1,21 +1,29 @@
 package org.simple.clinic.bp.history
 
 import com.spotify.mobius.rx2.RxMobius
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
 import io.reactivex.Scheduler
 import org.simple.clinic.bp.BloodPressureRepository
 import org.simple.clinic.util.scheduler.SchedulersProvider
-import javax.inject.Inject
 
-class BloodPressureHistoryScreenEffectHandler @Inject constructor(
+class BloodPressureHistoryScreenEffectHandler @AssistedInject constructor(
     private val bloodPressureRepository: BloodPressureRepository,
-    private val schedulersProvider: SchedulersProvider
+    private val schedulersProvider: SchedulersProvider,
+    @Assisted private val uiActions: BloodPressureHistoryScreenUiActions
 ) {
+
+  @AssistedInject.Factory
+  interface Factory {
+    fun create(uiActions: BloodPressureHistoryScreenUiActions): BloodPressureHistoryScreenEffectHandler
+  }
 
   fun build(): ObservableTransformer<BloodPressureHistoryScreenEffect, BloodPressureHistoryScreenEvent> {
     return RxMobius
         .subtypeEffectHandler<BloodPressureHistoryScreenEffect, BloodPressureHistoryScreenEvent>()
         .addTransformer(LoadBloodPressureHistory::class.java, loadBloodPressureHistory(schedulersProvider.io()))
+        .addAction(OpenBloodPressureEntrySheet::class.java, uiActions::openBloodPressureEntrySheet, schedulersProvider.ui())
         .build()
   }
 
--- app/src/test/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenEffectHandlerTest.kt
+++ app/src/test/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenEffectHandlerTest.kt
@@ -2,8 +2,12 @@ package org.simple.clinic.bp.history
 
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
+import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Observable
+import org.junit.After
 import org.junit.Test
 import org.simple.clinic.bp.BloodPressureRepository
 import org.simple.clinic.mobius.EffectHandlerTestCase
@@ -12,22 +16,30 @@ import org.simple.clinic.util.scheduler.TrampolineSchedulersProvider
 import java.util.UUID
 
 class BloodPressureHistoryScreenEffectHandlerTest {
+
+  private val bloodPressureRepository = mock<BloodPressureRepository>()
+  private val patientUuid = UUID.fromString("433d058f-daef-47a7-8c61-95f1a220cbcb")
+  private val uiActions = mock<BloodPressureHistoryScreenUiActions>()
+  private val effectHandler = BloodPressureHistoryScreenEffectHandler(
+      bloodPressureRepository,
+      TrampolineSchedulersProvider(),
+      uiActions
+  ).build()
+  private val testCase = EffectHandlerTestCase(effectHandler)
+
+  @After
+  fun tearDown() {
+    testCase.dispose()
+  }
+
   @Test
   fun `when load blood pressure history effect is received, then load all blood pressures`() {
     // given
-    val bloodPressureRepository = mock<BloodPressureRepository>()
-    val patientUuid = UUID.fromString("433d058f-daef-47a7-8c61-95f1a220cbcb")
     val bloodPressureMeasurement = PatientMocker.bp(
         UUID.fromString("51ac042d-2f70-495c-a3e3-2599d8990da2"),
         patientUuid
     )
     val bloodPressures = listOf(bloodPressureMeasurement)
-    val effectHandler = BloodPressureHistoryScreenEffectHandler(
-        bloodPressureRepository,
-        TrampolineSchedulersProvider()
-    ).build()
-    val testCase = EffectHandlerTestCase(effectHandler)
-
     whenever(bloodPressureRepository.allBloodPressures(patientUuid)) doReturn Observable.just(bloodPressures)
 
     // when
@@ -35,5 +47,17 @@ class BloodPressureHistoryScreenEffectHandlerTest {
 
     // then
     testCase.assertOutgoingEvents(BloodPressureHistoryLoaded(bloodPressures))
+    verifyZeroInteractions(uiActions)
+  }
+
+  @Test
+  fun `when open blood pressure entry sheet effect is received, then open blood pressure entry sheet`() {
+    // when
+    testCase.dispatch(OpenBloodPressureEntrySheet)
+
+    // then
+    testCase.assertNoOutgoingEvents()
+    verify(uiActions).openBloodPressureEntrySheet()
+    verifyNoMoreInteractions(uiActions)
   }
 }

6cbb6e58b4e2eb44e7440f9bcd121c545b97c203
Vinay S Shenoy <code@vinaysshenoy.com>
2020-01-14 10:10:43 +0530

    Fix incorrect migration from 55 -> 56
    The earlier migration would set all appointment types to `manual`,
    regardless of what their actual type was. This commit updates the
    migration.
    
    (cherry picked from commit f8d5558cb3e8f13647fa272a6f1719b6ed888326)

--- app/src/androidTest/java/org/simple/clinic/storage/migrations/Migration56AndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/storage/migrations/Migration56AndroidTest.kt
@@ -18,13 +18,16 @@ class Migration56AndroidTest : BaseDatabaseMigrationTest(fromVersion = 55, toVer
   fun migrating_from_55_to_56_should_add_creation_facility_uuid_column_in_Appointment() {
     before.assertColumnCount(tableName, 13)
 
-    val uuid = UUID.fromString("00cf4e56-cccf-4fad-b087-a5bacc35d5d0")
+    val uuidOfManualAppointment = UUID.fromString("00cf4e56-cccf-4fad-b087-a5bacc35d5d0")
+    val uuidOfAutomaticAppointment = UUID.fromString("2ba8ac68-7d68-4da9-adfc-ea6033266792")
     val patientUuid = UUID.fromString("b727faa6-b5cb-4fb4-8ba8-bb3bda86f1f9")
-    val facilityUuid = UUID.fromString("74bcfbb1-5a70-4fee-8237-ae454b0b39a0")
+    val facilityUuidForManualAppointment = UUID.fromString("74bcfbb1-5a70-4fee-8237-ae454b0b39a0")
+    val facilityUuidForAutomaticAppointment = UUID.fromString("eb0efb8b-0d7a-46f7-842c-b05b1bfe68d6")
+
     before.insert(tableName, mapOf(
-        "uuid" to uuid,
+        "uuid" to uuidOfManualAppointment,
         "patientUuid" to patientUuid,
-        "facilityUuid" to facilityUuid,
+        "facilityUuid" to facilityUuidForManualAppointment,
         "scheduledDate" to "2018-01-01",
         "status" to "SCHEDULED",
         "cancelReason" to null,
@@ -36,17 +39,33 @@ class Migration56AndroidTest : BaseDatabaseMigrationTest(fromVersion = 55, toVer
         "updatedAt" to "2018-01-01T00:00:00Z",
         "deletedAt" to null
     ))
+    before.insert(tableName, mapOf(
+        "uuid" to uuidOfAutomaticAppointment,
+        "patientUuid" to patientUuid,
+        "facilityUuid" to facilityUuidForAutomaticAppointment,
+        "scheduledDate" to "2018-01-01",
+        "status" to "SCHEDULED",
+        "cancelReason" to null,
+        "remindOn" to "2018-01-30",
+        "agreedToVisit" to "true",
+        "appointmentType" to "automatic",
+        "syncStatus" to "DONE",
+        "createdAt" to "2018-01-01T00:00:00Z",
+        "updatedAt" to "2018-01-01T00:00:00Z",
+        "deletedAt" to null
+    ))
 
     after.assertColumnCount(tableName, 14)
     after.query("""SELECT * FROM $tableName""").use { cursor ->
       with(cursor) {
-        assertThat(count).isEqualTo(1)
+        assertThat(count).isEqualTo(2)
+
         moveToFirst()
         assertValues(mapOf(
-            "uuid" to uuid,
+            "uuid" to uuidOfManualAppointment,
             "patientUuid" to patientUuid,
-            "facilityUuid" to facilityUuid,
-            "creationFacilityUuid" to facilityUuid,
+            "facilityUuid" to facilityUuidForManualAppointment,
+            "creationFacilityUuid" to facilityUuidForManualAppointment,
             "scheduledDate" to "2018-01-01",
             "status" to "SCHEDULED",
             "cancelReason" to null,
@@ -58,6 +77,25 @@ class Migration56AndroidTest : BaseDatabaseMigrationTest(fromVersion = 55, toVer
             "updatedAt" to "2018-01-01T00:00:00Z",
             "deletedAt" to null)
         )
+
+        moveToNext()
+        assertValues(mapOf(
+            "uuid" to uuidOfAutomaticAppointment,
+            "patientUuid" to patientUuid,
+            "facilityUuid" to facilityUuidForAutomaticAppointment,
+            "creationFacilityUuid" to facilityUuidForAutomaticAppointment,
+            "scheduledDate" to "2018-01-01",
+            "status" to "SCHEDULED",
+            "cancelReason" to null,
+            "remindOn" to "2018-01-30",
+            "agreedToVisit" to "true",
+            "appointmentType" to "automatic",
+            "syncStatus" to "DONE",
+            "createdAt" to "2018-01-01T00:00:00Z",
+            "updatedAt" to "2018-01-01T00:00:00Z",
+            "deletedAt" to null)
+        )
+
       }
     }
   }

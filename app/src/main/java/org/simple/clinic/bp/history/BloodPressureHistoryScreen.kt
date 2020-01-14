a01ed0c15e3a8255c0377ee0636a7637470bccef
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-14 17:13:10 +0530

    Set up blood pressure history list

--- app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreen.kt
+++ app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreen.kt
@@ -3,14 +3,30 @@ package org.simple.clinic.bp.history
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.screen_bp_history.view.*
 import org.simple.clinic.bp.BloodPressureMeasurement
+import org.simple.clinic.bp.history.adapter.BloodPressureHistoryListItem
+import org.simple.clinic.bp.history.adapter.BloodPressureHistoryListItemDiffCallback
 import org.simple.clinic.di.injector
+import org.simple.clinic.summary.PatientSummaryConfig
+import org.simple.clinic.util.UtcClock
+import org.simple.clinic.widgets.ItemAdapter
 import java.util.UUID
+import javax.inject.Inject
 
 class BloodPressureHistoryScreen(
     context: Context,
     attrs: AttributeSet
 ) : ConstraintLayout(context, attrs), BloodPressureHistoryScreenUi, BloodPressureHistoryScreenUiActions {
+
+  @Inject
+  lateinit var utcClock: UtcClock
+
+  @Inject
+  lateinit var config: PatientSummaryConfig
+
+  private val bloodPressureHistoryAdapter = ItemAdapter(BloodPressureHistoryListItemDiffCallback())
 
   override fun onFinishInflate() {
     super.onFinishInflate()
@@ -18,9 +34,20 @@ class BloodPressureHistoryScreen(
       return
     }
     context.injector<BloodPressureHistoryScreenInjector>().inject(this)
+
+    setupBloodPressureHistoryList()
+  }
+
+  private fun setupBloodPressureHistoryList() {
+    bpHistoryList.apply {
+      setHasFixedSize(true)
+      layoutManager = LinearLayoutManager(context)
+      adapter = bloodPressureHistoryAdapter
+    }
   }
 
   override fun showBloodPressureHistory(bloodPressures: List<BloodPressureMeasurement>) {
+    bloodPressureHistoryAdapter.submitList(BloodPressureHistoryListItem.from(bloodPressures, config.bpEditableDuration, utcClock))
   }
 
   override fun openBloodPressureEntrySheet() {

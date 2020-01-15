f025c9f16b3e78a0602456a044de62d33647e217
Vinay S Shenoy <vinay@obvious.in>
2020-01-15 14:08:58 +0530

    Move events to `PatientSummaryEvent`
    - PatientSummaryBackClicked
    - PatientSummaryDoneClicked

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryScreenEvents.kt
@@ -2,14 +2,6 @@ package org.simple.clinic.summary
 
 import org.simple.clinic.widgets.UiEvent
 
-class PatientSummaryBackClicked : UiEvent {
-  override val analyticsName = "Patient Summary:Back Clicked"
-}
-
-class PatientSummaryDoneClicked : UiEvent {
-  override val analyticsName = "Patient Summary:Done Clicked"
-}
-
 class ScheduleAppointmentSheetClosed : UiEvent {
   override val analyticsName = "Patient Summary:Schedule Appointment Sheet Closed"
 }

bb42e34a088e8f3c8f7afded379eb6993a6ed4c9
Vinay S Shenoy <code@vinaysshenoy.com>
2020-01-15 14:44:53 +0530

    Add `patientUuid` and `screenCreatedTimestamp` to back clicked event
    This is in preparation for moving the functionality to show the
    schedule appointment sheet to the effect handler.

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryEvent.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryEvent.kt
@@ -1,12 +1,18 @@
 package org.simple.clinic.summary
 
 import org.simple.clinic.widgets.UiEvent
+import org.threeten.bp.Instant
+import java.util.UUID
 
 sealed class PatientSummaryEvent : UiEvent
 
 data class PatientSummaryProfileLoaded(val patientSummaryProfile: PatientSummaryProfile) : PatientSummaryEvent()
 
-class PatientSummaryBackClicked : PatientSummaryEvent() {
+// TODO(vs): 2020-01-15 Consider whether these should be moved to the effect handler as properties later
+data class PatientSummaryBackClicked(
+    val patientUuid: UUID,
+    val screenCreatedTimestamp: Instant
+) : PatientSummaryEvent() {
   override val analyticsName = "Patient Summary:Back Clicked"
 }
 

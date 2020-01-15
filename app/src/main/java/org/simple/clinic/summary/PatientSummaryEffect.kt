654efec45c3c7e31817e37cf863ac611cc85e7d4
Vinay S Shenoy <vinay@obvious.in>
2020-01-15 15:37:35 +0530

    Add `HandleBackClick` patient summary effect

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryEffect.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryEffect.kt
@@ -1,7 +1,14 @@
 package org.simple.clinic.summary
 
+import org.threeten.bp.Instant
 import java.util.UUID
 
 sealed class PatientSummaryEffect
 
 data class LoadPatientSummaryProfile(val patientUuid: UUID) : PatientSummaryEffect()
+
+// TODO(vs): 2020-01-15 Revisit this effect once the patient summary migration is done
+data class HandleBackClick(
+    val patientUuid: UUID,
+    val screenCreatedTimestamp: Instant
+) : PatientSummaryEffect()

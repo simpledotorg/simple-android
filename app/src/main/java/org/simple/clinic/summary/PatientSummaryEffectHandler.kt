eb57ff0bc83da61e0e856f2ac5fee29719950e14
Vinay S Shenoy <vinay@obvious.in>
2020-01-15 16:18:58 +0530

    Extract effect handler transformers to discrete functions

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryEffectHandler.kt
@@ -5,6 +5,7 @@ import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
 import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.cast
 import org.simple.clinic.bp.BloodPressureRepository
@@ -28,62 +29,73 @@ class PatientSummaryEffectHandler @AssistedInject constructor(
   fun build(): ObservableTransformer<PatientSummaryEffect, PatientSummaryEvent> {
     return RxMobius
         .subtypeEffectHandler<PatientSummaryEffect, PatientSummaryEvent>()
-        .addTransformer(LoadPatientSummaryProfile::class.java) { effects ->
-          // TODO(vs): 2020-01-15 Revisit after Mobius migration
-          effects.flatMap { fetchPatientSummaryProfile ->
-            val patientUuid = fetchPatientSummaryProfile.patientUuid
-
-            val scheduler = schedulersProvider.io()
-
-            val sharedPatients = patientRepository.patient(patientUuid)
-                .subscribeOn(scheduler)
-                .map {
-                  // We do not expect the patient to get deleted while this screen is already open.
-                  (it as Just).value
-                }
-                .replay(1)
-                .refCount()
-
-            val addresses = sharedPatients
-                .flatMap { patient -> patientRepository.address(patient.addressUuid).subscribeOn(scheduler) }
-                .map { (it as Just).value }
-
-            val latestPhoneNumberStream = patientRepository.phoneNumber(patientUuid).subscribeOn(scheduler)
-            val latestBpPassportStream = patientRepository.bpPassportForPatient(patientUuid).subscribeOn(scheduler)
-
-            Observables
-                .combineLatest(sharedPatients, addresses, latestPhoneNumberStream, latestBpPassportStream) { patient, address, phoneNumber, bpPassport ->
-                  PatientSummaryProfile(patient, address, phoneNumber.toNullable(), bpPassport.toNullable())
-                }
-                .take(1)
-                .map(::PatientSummaryProfileLoaded)
-                .cast<PatientSummaryEvent>()
-          }
-        }
-        .addTransformer(HandleBackClick::class.java) { effects ->
-          // TODO(vs): 2020-01-15 Revisit after Mobius migration
-          effects
-              .observeOn(schedulersProvider.io())
-              .map { handleBackClick ->
-
-                val (patientUuid, screenCreatedTime) = handleBackClick
-
-                val hasPatientDataChangedSinceScreenCreated = patientRepository.hasPatientDataChangedSince(patientUuid, screenCreatedTime)
-                val noBloodPressuresRecordedForPatient = doesNotHaveBloodPressures(patientUuid)
-
-                val shouldShowScheduleAppointmentSheet = if (noBloodPressuresRecordedForPatient) false else hasPatientDataChangedSinceScreenCreated
-
-                shouldShowScheduleAppointmentSheet to patientUuid
-              }
-              .observeOn(schedulersProvider.ui())
-              .doOnNext { (showScheduleAppointmentSheet, patientUuid) ->
-                if (showScheduleAppointmentSheet) {
-                  uiActions.showScheduleAppointmentSheet(patientUuid)
-                }
-              }
-              .flatMap { Observable.empty<PatientSummaryEvent>() }
-        }
+        .addTransformer(LoadPatientSummaryProfile::class.java, loadPatientSummaryProfile(schedulersProvider.io()))
+        .addTransformer(HandleBackClick::class.java, handleBackClick(
+            backgroundWorkScheduler = schedulersProvider.io(),
+            uiWorkScheduler = schedulersProvider.ui()
+        ))
         .build()
+  }
+
+  // TODO(vs): 2020-01-15 Revisit after Mobius migration
+  private fun loadPatientSummaryProfile(scheduler: Scheduler): ObservableTransformer<LoadPatientSummaryProfile, PatientSummaryEvent> {
+    return ObservableTransformer { effects ->
+      effects.flatMap { fetchPatientSummaryProfile ->
+        val patientUuid = fetchPatientSummaryProfile.patientUuid
+
+        val sharedPatients = patientRepository.patient(patientUuid)
+            .subscribeOn(scheduler)
+            .map {
+              // We do not expect the patient to get deleted while this screen is already open.
+              (it as Just).value
+            }
+            .replay(1)
+            .refCount()
+
+        val addresses = sharedPatients
+            .flatMap { patient -> patientRepository.address(patient.addressUuid).subscribeOn(scheduler) }
+            .map { (it as Just).value }
+
+        val latestPhoneNumberStream = patientRepository.phoneNumber(patientUuid).subscribeOn(scheduler)
+        val latestBpPassportStream = patientRepository.bpPassportForPatient(patientUuid).subscribeOn(scheduler)
+
+        Observables
+            .combineLatest(sharedPatients, addresses, latestPhoneNumberStream, latestBpPassportStream) { patient, address, phoneNumber, bpPassport ->
+              PatientSummaryProfile(patient, address, phoneNumber.toNullable(), bpPassport.toNullable())
+            }
+            .take(1)
+            .map(::PatientSummaryProfileLoaded)
+            .cast<PatientSummaryEvent>()
+      }
+    }
+  }
+
+  // TODO(vs): 2020-01-15 Revisit after Mobius migration
+  private fun handleBackClick(
+      backgroundWorkScheduler: Scheduler,
+      uiWorkScheduler: Scheduler
+  ): ObservableTransformer<HandleBackClick, PatientSummaryEvent> {
+    return ObservableTransformer { effects ->
+      effects
+          .observeOn(backgroundWorkScheduler)
+          .map { handleBackClick ->
+            val (patientUuid, screenCreatedTime) = handleBackClick
+
+            val hasPatientDataChangedSinceScreenCreated = patientRepository.hasPatientDataChangedSince(patientUuid, screenCreatedTime)
+            val noBloodPressuresRecordedForPatient = doesNotHaveBloodPressures(patientUuid)
+
+            val shouldShowScheduleAppointmentSheet = if (noBloodPressuresRecordedForPatient) false else hasPatientDataChangedSinceScreenCreated
+
+            shouldShowScheduleAppointmentSheet to patientUuid
+          }
+          .observeOn(uiWorkScheduler)
+          .doOnNext { (showScheduleAppointmentSheet, patientUuid) ->
+            if (showScheduleAppointmentSheet) {
+              uiActions.showScheduleAppointmentSheet(patientUuid)
+            }
+          }
+          .flatMap { Observable.empty<PatientSummaryEvent>() }
+    }
   }
 
   private fun doesNotHaveBloodPressures(patientUuid: UUID): Boolean {

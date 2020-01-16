3738fdd16fcf3fe53680608a00041109f4fc00de
Vinay S Shenoy <vinay@obvious.in>
2020-01-16 17:44:47 +0530

    Replace deprecated method usage when fetching remote config
    `FirebaseRemoteConfig#activateFetched()` has been deprecated in favour
    of the `activate()` call. This commit changes the multiple calls to
    `fetch()`, then `activate()` to the convenience method,
    `fetchAndActivate()`, which is provided for this purpose.

--- app/src/main/java/org/simple/clinic/remoteconfig/RemoteConfigModule.kt
+++ app/src/main/java/org/simple/clinic/remoteconfig/RemoteConfigModule.kt
@@ -3,6 +3,7 @@ package org.simple.clinic.remoteconfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import dagger.Module
 import dagger.Provides
+import org.simple.clinic.platform.crash.CrashReporter
 import org.simple.clinic.remoteconfig.firebase.FirebaseRemoteConfigService
 import org.threeten.bp.Duration
 import javax.inject.Named
@@ -14,8 +15,9 @@ class RemoteConfigModule {
   fun provideRemoteConfigService(
       firebaseRemoteConfig: FirebaseRemoteConfig,
       @Named("firebase_cache_expiration_duration") cacheExpirationDuration: Duration
+      crashReporter: dagger.Lazy<CrashReporter>
   ): RemoteConfigService {
-    return FirebaseRemoteConfigService(firebaseRemoteConfig, cacheExpirationDuration)
+    return FirebaseRemoteConfigService(firebaseRemoteConfig, crashReporter, cacheExpirationDuration)
   }
 
   @Provides

24cf12fef079731efb8c8b0d7cfc2573a554a598
Vinay S Shenoy <code@vinaysshenoy.com>
2020-01-16 17:44:47 +0530

    Replace deprecated method usage when fetching remote config
    `FirebaseRemoteConfig#activateFetched()` has been deprecated in favour
    of the `activate()` call. This commit changes the multiple calls to
    `fetch()`, then `activate()` to the convenience method,
    `fetchAndActivate()`, which is provided for this purpose.

--- app/src/main/java/org/simple/clinic/remoteconfig/firebase/FirebaseRemoteConfigService.kt
+++ app/src/main/java/org/simple/clinic/remoteconfig/firebase/FirebaseRemoteConfigService.kt
@@ -2,6 +2,7 @@ package org.simple.clinic.remoteconfig.firebase
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import io.reactivex.Completable
+import org.simple.clinic.platform.crash.CrashReporter
 import org.simple.clinic.remoteconfig.ConfigReader
 import org.simple.clinic.remoteconfig.RemoteConfigService
 import org.simple.clinic.util.toCompletable
@@ -11,6 +12,10 @@ import javax.inject.Inject
 
 class FirebaseRemoteConfigService @Inject constructor(
     private val firebaseRemoteConfig: FirebaseRemoteConfig,
+    // This is marked as Lazy to avoid a cyclic dependency between FirebaseRemoteConfigService and
+    // SentryCrashReporter (which happens only in release builds).
+    // TODO(vs): 2020-01-17 Change CrashReporter to be something like Timber, which can live outside the DI graph
+    private val crashReporter: dagger.Lazy<CrashReporter>,
     /**
      * Duration to cache local values before they're synced with Firebase again.
      * This is required because Firebase throttles network calls by returning cached
@@ -25,11 +30,9 @@ class FirebaseRemoteConfigService @Inject constructor(
 
   override fun update(): Completable {
     return firebaseRemoteConfig
-        .fetch(cacheExpirationDuration.seconds)
+        .fetchAndActivate()
         .toCompletable { Timber.w("Failed to update Firebase remote config") }
-        .doOnComplete {
-          Timber.i("Firebase remote config updated successfully")
-          firebaseRemoteConfig.activateFetched()
-        }
+        .doOnComplete { Timber.i("Firebase remote config updated successfully") }
+        .doOnError { crashReporter.get().report(it) }
   }
 }

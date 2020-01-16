8c03044375323f1c3cddfd06f6916cde4fe9e28c
Vinay S Shenoy <code@vinaysshenoy.com>
2020-01-16 15:44:05 +0530

    Set fetch interval initializing Firebase Remote Config

--- app/src/release/java/org/simple/clinic/remoteconfig/firebase/FirebaseRemoteConfigModule.kt
+++ app/src/release/java/org/simple/clinic/remoteconfig/firebase/FirebaseRemoteConfigModule.kt
@@ -1,10 +1,13 @@
 package org.simple.clinic.remoteconfig.firebase
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.di.AppScope
+import org.simple.clinic.platform.crash.CrashReporter
 import org.threeten.bp.Duration
+import timber.log.Timber
 import javax.inject.Named
 
 @Module
@@ -12,8 +15,17 @@ class FirebaseRemoteConfigModule {
 
   @Provides
   @AppScope
-  fun remoteConfig(): FirebaseRemoteConfig {
-    return FirebaseRemoteConfig.getInstance()
+  fun remoteConfig(crashReporter: dagger.Lazy<CrashReporter>): FirebaseRemoteConfig {
+    return FirebaseRemoteConfig.getInstance().apply {
+      val settings = FirebaseRemoteConfigSettings
+          .Builder()
+          .setMinimumFetchIntervalInSeconds(Duration.ofHours(1).seconds)
+          .build()
+
+      setConfigSettingsAsync(settings)
+          .addOnSuccessListener { Timber.tag("FRC").i("Set remote config settings") }
+          .addOnFailureListener { crashReporter.get().report(it) }
+    }
   }
 
   @Provides

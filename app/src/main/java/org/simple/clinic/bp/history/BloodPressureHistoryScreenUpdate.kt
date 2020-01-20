6f8129cdfb560ebe685a788234ddb2382a209d43
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-01-20 18:23:56 +0530

    Show patient information when patient is loaded

--- app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenUpdate.kt
+++ app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenUpdate.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.bp.history
 
 import com.spotify.mobius.Next
+import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
 import org.simple.clinic.mobius.dispatch
 import org.simple.clinic.mobius.next
@@ -11,6 +12,7 @@ class BloodPressureHistoryScreenUpdate : Update<BloodPressureHistoryScreenModel,
       event: BloodPressureHistoryScreenEvent
   ): Next<BloodPressureHistoryScreenModel, BloodPressureHistoryScreenEffect> {
     return when (event) {
+      is PatientLoaded -> next(model.patientLoaded(event.patient))
       is BloodPressureHistoryLoaded -> next(model.historyLoaded(event.bloodPressures))
       is NewBloodPressureClicked -> dispatch(OpenBloodPressureEntrySheet)
       is BloodPressureClicked -> dispatch(OpenBloodPressureUpdateSheet(event.bloodPressureMeasurement))

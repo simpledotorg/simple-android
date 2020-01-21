04d34fe83499d4b5ed64317058cfcb03094264fb
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-21 15:00:44 +0530

    Show loaded BPs in BP summary view

---
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/NewBloodPressureSummaryViewUpdate.kt
@@ -0,0 +1,16 @@
+package org.simple.clinic.summary.bloodpressures.newbpsummary
+
+import com.spotify.mobius.Next
+import com.spotify.mobius.Update
+import org.simple.clinic.mobius.next
+
+class NewBloodPressureSummaryViewUpdate : Update<NewBloodPressureSummaryViewModel, NewBloodPressureSummaryViewEvent, NewBloodPressureSummaryViewEffect> {
+  override fun update(
+      model: NewBloodPressureSummaryViewModel,
+      event: NewBloodPressureSummaryViewEvent
+  ): Next<NewBloodPressureSummaryViewModel, NewBloodPressureSummaryViewEffect> {
+    return when (event) {
+      is BloodPressuresLoaded -> next(model.bloodPressuresLoaded(event.measurements))
+    }
+  }
+}

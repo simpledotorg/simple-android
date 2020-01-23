96d7829e9b0231dc4a979c425e4524b1e17b00e2
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-01-23 14:36:57 +0530

    When BPs are loading then do nothing

---
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/NewBloodPressureSummaryViewUiRenderer.kt
@@ -0,0 +1,10 @@
+package org.simple.clinic.summary.bloodpressures.newbpsummary
+
+import org.simple.clinic.mobius.ViewRenderer
+
+class NewBloodPressureSummaryViewUiRenderer(
+    private val ui: NewBloodPressureSummaryViewUi
+) : ViewRenderer<NewBloodPressureSummaryViewModel> {
+  override fun render(model: NewBloodPressureSummaryViewModel) {
+  }
+}

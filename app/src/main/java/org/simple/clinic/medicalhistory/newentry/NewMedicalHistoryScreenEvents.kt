45131d8feaa72acc8c0a857f98591342c83cfde3
Vinay Shenoy <vinay@obvious.in>
2020-01-24 10:08:48 +0530

    Move `SaveMedicalHistoryClicked` event to `NewMedicalHistoryEvent`

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
@@ -4,10 +4,6 @@ import org.simple.clinic.medicalhistory.Answer
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
 import org.simple.clinic.widgets.UiEvent
 
-class SaveMedicalHistoryClicked : UiEvent {
-  override val analyticsName = "New Medical History:Save Clicked"
-}
-
 data class NewMedicalHistoryAnswerToggled(val question: MedicalHistoryQuestion, val answer: Answer) : UiEvent {
   override val analyticsName = "New Medical History:Answer for $question set to $answer"
 }

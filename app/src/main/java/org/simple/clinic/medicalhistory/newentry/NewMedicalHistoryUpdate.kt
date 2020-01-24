1926ab2558fe804eb4fca8005c956306934bc90b
Vinay Shenoy <code@vinaysshenoy.com>
2020-01-24 14:24:01 +0530

    Add `OngoingPatientEntryLoaded` event

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryUpdate.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryUpdate.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.medicalhistory.newentry
 
 import com.spotify.mobius.Next
+import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
 import org.simple.clinic.mobius.dispatch
 import org.simple.clinic.mobius.next
@@ -8,10 +9,11 @@ import org.simple.clinic.mobius.next
 class NewMedicalHistoryUpdate : Update<NewMedicalHistoryModel, NewMedicalHistoryEvent, NewMedicalHistoryEffect> {
 
   override fun update(model: NewMedicalHistoryModel, event: NewMedicalHistoryEvent): Next<NewMedicalHistoryModel, NewMedicalHistoryEffect> {
-    return when(event) {
+    return when (event) {
       is NewMedicalHistoryAnswerToggled -> next(model.answerChanged(event.question, event.answer))
       is SaveMedicalHistoryClicked -> dispatch(RegisterPatient(model.ongoingMedicalHistoryEntry))
       is PatientRegistered -> dispatch(OpenPatientSummaryScreen(event.patientUuid))
+      is OngoingPatientEntryLoaded -> noChange()
     }
   }
 }

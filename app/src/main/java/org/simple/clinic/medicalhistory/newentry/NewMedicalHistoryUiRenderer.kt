067def95959005cc99e981ef37a2d18b7d5428b2
Vinay Shenoy <vinay@obvious.in>
2020-01-24 15:13:04 +0530

    Move rendering of answers to `NewMedicalHistoryUiRenderer`

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryUiRenderer.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryUiRenderer.kt
@@ -1,5 +1,11 @@
 package org.simple.clinic.medicalhistory.newentry
 
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.DIAGNOSED_WITH_HYPERTENSION
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_DIABETES
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_HEART_ATTACK
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_KIDNEY_DISEASE
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_STROKE
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IS_ON_TREATMENT_FOR_HYPERTENSION
 import org.simple.clinic.mobius.ViewRenderer
 
 class NewMedicalHistoryUiRenderer(
@@ -7,8 +13,17 @@ class NewMedicalHistoryUiRenderer(
 ) : ViewRenderer<NewMedicalHistoryModel> {
 
   override fun render(model: NewMedicalHistoryModel) {
-    if(model.hasLoadedPatientEntry) {
+    if (model.hasLoadedPatientEntry) {
       ui.setPatientName(model.ongoingPatientEntry!!.personalDetails!!.fullName)
+    }
+
+    with(model.ongoingMedicalHistoryEntry) {
+      ui.renderAnswerForQuestion(HAS_HAD_A_HEART_ATTACK, hasHadHeartAttack)
+      ui.renderAnswerForQuestion(HAS_DIABETES, hasDiabetes)
+      ui.renderAnswerForQuestion(HAS_HAD_A_KIDNEY_DISEASE, hasHadKidneyDisease)
+      ui.renderAnswerForQuestion(HAS_HAD_A_STROKE, hasHadStroke)
+      ui.renderAnswerForQuestion(IS_ON_TREATMENT_FOR_HYPERTENSION, isOnTreatmentForHypertension)
+      ui.renderAnswerForQuestion(DIAGNOSED_WITH_HYPERTENSION, diagnosedWithHypertension)
     }
   }
 }

58c067e58b27d63e14d4e9fff7f5c5ce0c0c8613
Vinay Shenoy <code@vinaysshenoy.com>
2020-01-24 10:40:51 +0530

    Move updating of ongoing CV history entry to `NewMedicalHistoryUpdate`

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryModel.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryModel.kt
@@ -2,6 +2,8 @@ package org.simple.clinic.medicalhistory.newentry
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.medicalhistory.Answer
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
 import org.simple.clinic.medicalhistory.OngoingMedicalHistoryEntry
 
 @Parcelize
@@ -9,5 +11,9 @@ data class NewMedicalHistoryModel(val ongoingMedicalHistoryEntry: OngoingMedical
 
   companion object {
     fun default(): NewMedicalHistoryModel = NewMedicalHistoryModel(OngoingMedicalHistoryEntry())
+  }
+
+  fun answerChanged(question: MedicalHistoryQuestion, answer: Answer): NewMedicalHistoryModel {
+    return copy(ongoingMedicalHistoryEntry = ongoingMedicalHistoryEntry.answerChanged(question, answer))
   }
 }

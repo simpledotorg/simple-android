4f822722094dd842482c1919213886e91a045d05
Vinay Shenoy <vinay@obvious.in>
2020-01-24 16:42:47 +0530

    Use new `MedicalHistoryQuestionView` in `NewMedicalHistoryScreen`

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreen.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreen.kt
@@ -7,11 +7,12 @@ import android.widget.RelativeLayout
 import com.jakewharton.rxbinding3.view.clicks
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.cast
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
 import kotlinx.android.synthetic.main.screen_new_medical_history.view.*
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.main.TheActivity
 import org.simple.clinic.medicalhistory.Answer
-import org.simple.clinic.medicalhistory.Answer.Unanswered
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.DIAGNOSED_WITH_HYPERTENSION
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_DIABETES
@@ -19,7 +20,6 @@ import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_HEART_A
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_KIDNEY_DISEASE
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_STROKE
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IS_ON_TREATMENT_FOR_HYPERTENSION
-import org.simple.clinic.medicalhistory.MedicalHistoryQuestionView_Old
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.mobius.ViewRenderer
 import org.simple.clinic.platform.crash.CrashReporter
@@ -28,7 +28,6 @@ import org.simple.clinic.summary.OpenIntention
 import org.simple.clinic.summary.PatientSummaryScreenKey
 import org.simple.clinic.util.UtcClock
 import org.simple.clinic.util.unsafeLazy
-import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.hideKeyboard
 import org.threeten.bp.Instant
 import java.util.UUID
@@ -51,9 +50,11 @@ class NewMedicalHistoryScreen(
   @Inject
   lateinit var effectHandlerFactory: NewMedicalHistoryEffectHandler.Factory
 
+  private val questionViewEvents: Subject<NewMedicalHistoryEvent> = PublishSubject.create()
+
   private val events: Observable<NewMedicalHistoryEvent> by unsafeLazy {
     Observable
-        .merge(answerToggles(), saveClicks())
+        .merge(questionViewEvents, saveClicks())
         .compose(ReportAnalyticsEvents())
         .cast<NewMedicalHistoryEvent>()
   }
@@ -83,13 +84,6 @@ class NewMedicalHistoryScreen(
       screenRouter.pop()
     }
 
-    diagnosedForHypertensionQuestionView.render(DIAGNOSED_WITH_HYPERTENSION, Unanswered)
-    treatmentForHypertensionQuestionView.render(IS_ON_TREATMENT_FOR_HYPERTENSION, Unanswered)
-    heartAttackQuestionView.render(HAS_HAD_A_HEART_ATTACK, Unanswered)
-    strokeQuestionView.render(HAS_HAD_A_STROKE, Unanswered)
-    kidneyDiseaseQuestionView.render(HAS_HAD_A_KIDNEY_DISEASE, Unanswered)
-    diabetesQuestionView.render(HAS_DIABETES, Unanswered)
-
     diabetesQuestionView.hideDivider()
 
     mobiusDelegate.prepare()
@@ -117,20 +111,6 @@ class NewMedicalHistoryScreen(
     super.onRestoreInstanceState(mobiusDelegate.onRestoreInstanceState(state))
   }
 
-  private fun answerToggles(): Observable<UiEvent> {
-    val toggles = { view: MedicalHistoryQuestionView_Old ->
-      view.answers().map { answer -> NewMedicalHistoryAnswerToggled(view.question, answer) }
-    }
-
-    return Observable.mergeArray(
-        toggles(diagnosedForHypertensionQuestionView),
-        toggles(treatmentForHypertensionQuestionView),
-        toggles(heartAttackQuestionView),
-        toggles(strokeQuestionView),
-        toggles(kidneyDiseaseQuestionView),
-        toggles(diabetesQuestionView))
-  }
-
   private fun saveClicks() =
       nextButtonFrame.button
           .clicks()
@@ -154,6 +134,8 @@ class NewMedicalHistoryScreen(
       HAS_DIABETES -> diabetesQuestionView
     }
 
-    view.render(question, answer)
+    view.render(question, answer) { questionForView, newAnswer ->
+      questionViewEvents.onNext(NewMedicalHistoryAnswerToggled(questionForView, newAnswer))
+    }
   }
 }

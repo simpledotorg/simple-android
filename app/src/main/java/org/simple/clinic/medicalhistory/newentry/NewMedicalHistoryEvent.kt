b0ac78887ab2476ebc1529645bb74b517d0ee9cc
Vinay Shenoy <code@vinaysshenoy.com>
2020-01-24 10:18:33 +0530

    Move `NewMedicalHistoryAnswerToggled` event to `NewMedicalHistoryEvent`

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryEvent.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryEvent.kt
@@ -1,9 +1,16 @@
 package org.simple.clinic.medicalhistory.newentry
 
+import org.simple.clinic.medicalhistory.Answer
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
 import org.simple.clinic.widgets.UiEvent
 
 sealed class NewMedicalHistoryEvent : UiEvent
 
 class SaveMedicalHistoryClicked : NewMedicalHistoryEvent() {
   override val analyticsName = "New Medical History:Save Clicked"
+}
+
+data class NewMedicalHistoryAnswerToggled(val question: MedicalHistoryQuestion, val answer: Answer) : NewMedicalHistoryEvent() {
+  // TODO(vs): 2020-01-24 Fix .toString() issues with the answer property since it is not an enum
+  override val analyticsName = "New Medical History:Answer for $question set to $answer"
 }

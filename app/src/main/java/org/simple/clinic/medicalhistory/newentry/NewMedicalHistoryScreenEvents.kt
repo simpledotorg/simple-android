add2519a058cfe677c3e6862326936bf60188eae
Vinay Shenoy <vinay@obvious.in>
2020-01-24 10:18:33 +0530

    Move `NewMedicalHistoryAnswerToggled` event to `NewMedicalHistoryEvent`

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryScreenEvents.kt
+++
@@ -1,9 +0,0 @@
-package org.simple.clinic.medicalhistory.newentry
-
-import org.simple.clinic.medicalhistory.Answer
-import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
-import org.simple.clinic.widgets.UiEvent
-
-data class NewMedicalHistoryAnswerToggled(val question: MedicalHistoryQuestion, val answer: Answer) : UiEvent {
-  override val analyticsName = "New Medical History:Answer for $question set to $answer"
-}

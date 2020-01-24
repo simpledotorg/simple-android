5e9751945ac020351674cde3d6a2ba3aa75f9b67
Vinay Shenoy <code@vinaysshenoy.com>
2020-01-24 10:22:59 +0530

    Refactor updating of CV history entry to a separate helper method

--- app/src/main/java/org/simple/clinic/medicalhistory/OngoingMedicalHistoryEntry.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/OngoingMedicalHistoryEntry.kt
@@ -3,6 +3,12 @@ package org.simple.clinic.medicalhistory
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.medicalhistory.Answer.Unanswered
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.DIAGNOSED_WITH_HYPERTENSION
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_DIABETES
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_HEART_ATTACK
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_KIDNEY_DISEASE
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_STROKE
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IS_ON_TREATMENT_FOR_HYPERTENSION
 
 @Parcelize
 data class OngoingMedicalHistoryEntry(
@@ -12,4 +18,16 @@ data class OngoingMedicalHistoryEntry(
     val diagnosedWithHypertension: Answer = Unanswered,
     val isOnTreatmentForHypertension: Answer = Unanswered,
     val hasDiabetes: Answer = Unanswered
-): Parcelable
+) : Parcelable {
+
+  fun answerChanged(question: MedicalHistoryQuestion, answer: Answer): OngoingMedicalHistoryEntry {
+    return when (question) {
+      DIAGNOSED_WITH_HYPERTENSION -> copy(diagnosedWithHypertension = answer)
+      IS_ON_TREATMENT_FOR_HYPERTENSION -> copy(isOnTreatmentForHypertension = answer)
+      HAS_HAD_A_HEART_ATTACK -> copy(hasHadHeartAttack = answer)
+      HAS_HAD_A_STROKE -> copy(hasHadStroke = answer)
+      HAS_HAD_A_KIDNEY_DISEASE -> copy(hasHadKidneyDisease = answer)
+      HAS_DIABETES -> copy(hasDiabetes = answer)
+    }
+  }
+}

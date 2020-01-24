12a5ded987815fdb26890e011222fc169de69465
Vinay Shenoy <code@vinaysshenoy.com>
2020-01-24 13:14:55 +0530

    Move registering a patient to the Mobius loop

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryEffectHandler.kt
@@ -4,11 +4,23 @@ import com.spotify.mobius.rx2.RxMobius
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
+import org.simple.clinic.facility.Facility
+import org.simple.clinic.facility.FacilityRepository
+import org.simple.clinic.medicalhistory.MedicalHistoryRepository
+import org.simple.clinic.medicalhistory.OngoingMedicalHistoryEntry
+import org.simple.clinic.patient.PatientRepository
+import org.simple.clinic.user.User
+import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class NewMedicalHistoryEffectHandler @AssistedInject constructor(
     private val schedulersProvider: SchedulersProvider,
-    @Assisted private val uiActions: NewMedicalHistoryUiActions
+    @Assisted private val uiActions: NewMedicalHistoryUiActions,
+    private val userSession: UserSession,
+    private val facilityRepository: FacilityRepository,
+    private val patientRepository: PatientRepository,
+    private val medicalHistoryRepository: MedicalHistoryRepository
 ) {
 
   @AssistedInject.Factory
@@ -19,7 +31,42 @@ class NewMedicalHistoryEffectHandler @AssistedInject constructor(
   fun build(): ObservableTransformer<NewMedicalHistoryEffect, NewMedicalHistoryEvent> {
     return RxMobius
         .subtypeEffectHandler<NewMedicalHistoryEffect, NewMedicalHistoryEvent>()
-        .addConsumer(OpenPatientSummaryScreen::class.java, { effect -> uiActions.openPatientSummaryScreen(effect.patientUuid)}, schedulersProvider.ui())
+        .addConsumer(OpenPatientSummaryScreen::class.java, { effect -> uiActions.openPatientSummaryScreen(effect.patientUuid) }, schedulersProvider.ui())
+        .addTransformer(RegisterPatient::class.java, registerPatient(schedulersProvider.io()))
         .build()
   }
+
+  private fun registerPatient(scheduler: Scheduler): ObservableTransformer<RegisterPatient, NewMedicalHistoryEvent> {
+    return ObservableTransformer { effects ->
+      effects
+          .observeOn(scheduler)
+          .flatMap { registerPatientEffect ->
+            val loggedInUser = userSession.loggedInUserImmediate()
+            requireNotNull(loggedInUser)
+
+            facilityRepository
+                .currentFacility(loggedInUser)
+                .take(1)
+                .map { facility ->
+                  val ongoingMedicalHistoryEntry = registerPatientEffect.ongoingMedicalHistoryEntry
+
+                  RegisterPatientData(loggedInUser, facility, ongoingMedicalHistoryEntry)
+                }
+          }
+          .flatMapSingle { (user, facility, ongoingMedicalHistoryEntry) ->
+            patientRepository.saveOngoingEntryAsPatient(user, facility)
+                .flatMap { registeredPatient ->
+                  medicalHistoryRepository
+                      .save(registeredPatient.uuid, ongoingMedicalHistoryEntry)
+                      .toSingleDefault(PatientRegistered(registeredPatient.uuid))
+                }
+          }
+    }
+  }
+
+  private data class RegisterPatientData(
+      val user: User,
+      val facility: Facility,
+      val ongoingMedicalHistoryEntry: OngoingMedicalHistoryEntry
+  )
 }
--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryEvent.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryEvent.kt
@@ -3,6 +3,7 @@ package org.simple.clinic.medicalhistory.newentry
 import org.simple.clinic.medicalhistory.Answer
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
 import org.simple.clinic.widgets.UiEvent
+import java.util.UUID
 
 sealed class NewMedicalHistoryEvent : UiEvent
 
@@ -14,3 +15,5 @@ data class NewMedicalHistoryAnswerToggled(val question: MedicalHistoryQuestion, 
   // TODO(vs): 2020-01-24 Fix .toString() issues with the answer property since it is not an enum
   override val analyticsName = "New Medical History:Answer for $question set to $answer"
 }
+
+data class PatientRegistered(val patientUuid: UUID): NewMedicalHistoryEvent()

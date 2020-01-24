bf094796ca12ed8e0a62ebfcd437502155e68a3c
Vinay Shenoy <vinay@obvious.in>
2020-01-24 10:40:51 +0530

    Move updating of ongoing CV history entry to `NewMedicalHistoryUpdate`

--- app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryUpdate.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/newentry/NewMedicalHistoryUpdate.kt
@@ -3,10 +3,14 @@ package org.simple.clinic.medicalhistory.newentry
 import com.spotify.mobius.Next
 import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
+import org.simple.clinic.mobius.next
 
 class NewMedicalHistoryUpdate : Update<NewMedicalHistoryModel, NewMedicalHistoryEvent, NewMedicalHistoryEffect> {
 
   override fun update(model: NewMedicalHistoryModel, event: NewMedicalHistoryEvent): Next<NewMedicalHistoryModel, NewMedicalHistoryEffect> {
-    return noChange()
+    return when(event) {
+      is NewMedicalHistoryAnswerToggled -> next(model.answerChanged(event.question, event.answer))
+      else -> noChange()
+    }
   }
 }

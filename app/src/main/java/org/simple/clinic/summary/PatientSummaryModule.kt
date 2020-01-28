ee06739bb0de63ac5c61ec455fe17ec5039bb8ae
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-28 10:14:34 +0530

    Provide `NewBloodPressureSummaryViewConfig` in `PatientSummaryModule`

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryModule.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.remoteconfig.ConfigReader
+import org.simple.clinic.summary.bloodpressures.newbpsummary.NewBloodPressureSummaryViewConfig
 
 @Module
 class PatientSummaryModule {
@@ -11,6 +12,11 @@ class PatientSummaryModule {
   @Provides
   fun providesSummaryConfig(configReader: ConfigReader): PatientSummaryConfig {
     return PatientSummaryConfig.read(configReader)
+  }
+
+  @Provides
+  fun providesNewBloodPressureSummaryConfig(configReader: ConfigReader): NewBloodPressureSummaryViewConfig {
+    return NewBloodPressureSummaryViewConfig.read(configReader)
   }
 
   @Provides

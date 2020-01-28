72496be5e48117cba2f3202038dd8fbb602f1190
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-01-28 10:14:34 +0530

    Provide `NewBloodPressureSummaryViewConfig` in `PatientSummaryModule`

--- app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/NewBloodPressureSummaryViewConfig.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/NewBloodPressureSummaryViewConfig.kt
@@ -1,3 +1,15 @@
 package org.simple.clinic.summary.bloodpressures.newbpsummary
 
-data class NewBloodPressureSummaryViewConfig(val numberOfBpsToDisplay: Int)
+import org.simple.clinic.remoteconfig.ConfigReader
+
+data class NewBloodPressureSummaryViewConfig(val numberOfBpsToDisplay: Int) {
+  companion object {
+    fun read(config: ConfigReader): NewBloodPressureSummaryViewConfig {
+      val numberOfBpsToDisplay = config.long("number_of_bps_to_display", 3)
+
+      return NewBloodPressureSummaryViewConfig(
+          numberOfBpsToDisplay = numberOfBpsToDisplay.toInt()
+      )
+    }
+  }
+}

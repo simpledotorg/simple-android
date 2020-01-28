bf0f48684b4fbf0032b2a76ea7f6a5cd86896ed6
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-28 10:17:46 +0530

    Add `events` in `NewBloodPressureSummaryView`

--- app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/NewBloodPressureSummaryViewEvent.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/NewBloodPressureSummaryViewEvent.kt
@@ -1,15 +1,22 @@
 package org.simple.clinic.summary.bloodpressures.newbpsummary
 
 import org.simple.clinic.bp.BloodPressureMeasurement
+import org.simple.clinic.widgets.UiEvent
 
-sealed class NewBloodPressureSummaryViewEvent
+sealed class NewBloodPressureSummaryViewEvent : UiEvent
 
 data class BloodPressuresLoaded(val measurements: List<BloodPressureMeasurement>) : NewBloodPressureSummaryViewEvent()
 
 data class BloodPressuresCountLoaded(val count: Int) : NewBloodPressureSummaryViewEvent()
 
-object AddNewBloodPressureClicked : NewBloodPressureSummaryViewEvent()
+object AddNewBloodPressureClicked : NewBloodPressureSummaryViewEvent() {
+  override val analyticsName = "Patient Summary:Add New BP Clicked"
+}
 
-data class BloodPressureClicked(val measurement: BloodPressureMeasurement) : NewBloodPressureSummaryViewEvent()
+data class BloodPressureClicked(val measurement: BloodPressureMeasurement) : NewBloodPressureSummaryViewEvent() {
+  override val analyticsName = "Patient Summary:BP Clicked for editing"
+}
 
-object SeeAllClicked : NewBloodPressureSummaryViewEvent()
+object SeeAllClicked : NewBloodPressureSummaryViewEvent() {
+  override val analyticsName = "Patient Summary:See All Clicked"
+}

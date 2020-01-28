1ce87365570d367060d596669dd0f1d3a267e506
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-28 10:17:46 +0530

    Add `events` in `NewBloodPressureSummaryView`

--- app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/view/NewBloodPressureSummaryView.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/view/NewBloodPressureSummaryView.kt
@@ -3,16 +3,39 @@ package org.simple.clinic.summary.bloodpressures.newbpsummary.view
 import android.content.Context
 import android.util.AttributeSet
 import androidx.cardview.widget.CardView
+import com.jakewharton.rxbinding3.view.clicks
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.cast
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.patientsummary_newbpsummary_content.view.*
+import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.bp.BloodPressureMeasurement
 import org.simple.clinic.di.injector
+import org.simple.clinic.summary.bloodpressures.newbpsummary.AddNewBloodPressureClicked
+import org.simple.clinic.summary.bloodpressures.newbpsummary.NewBloodPressureSummaryViewEvent
 import org.simple.clinic.summary.bloodpressures.newbpsummary.NewBloodPressureSummaryViewUi
 import org.simple.clinic.summary.bloodpressures.newbpsummary.NewBloodPressureSummaryViewUiActions
+import org.simple.clinic.summary.bloodpressures.newbpsummary.SeeAllClicked
+import org.simple.clinic.util.unsafeLazy
 import java.util.UUID
 
 class NewBloodPressureSummaryView(
     context: Context,
     attrs: AttributeSet
 ) : CardView(context, attrs), NewBloodPressureSummaryViewUi, NewBloodPressureSummaryViewUiActions {
+
+  private val viewEvents = PublishSubject.create<NewBloodPressureSummaryViewEvent>()
+
+  private val events: Observable<NewBloodPressureSummaryViewEvent> by unsafeLazy {
+    Observable
+        .merge(
+            addNewBpClicked(),
+            seeAllClicked(),
+            viewEvents
+        )
+        .compose(ReportAnalyticsEvents())
+        .cast<NewBloodPressureSummaryViewEvent>()
+  }
 
   override fun onFinishInflate() {
     super.onFinishInflate()
@@ -41,5 +64,13 @@ class NewBloodPressureSummaryView(
   }
 
   override fun showBloodPressureHistoryScreen(patientUuid: UUID) {
+  }
+
+  private fun addNewBpClicked(): Observable<NewBloodPressureSummaryViewEvent> {
+    return addNewBP.clicks().map { AddNewBloodPressureClicked }
+  }
+
+  private fun seeAllClicked(): Observable<NewBloodPressureSummaryViewEvent> {
+    return seeAll.clicks().map { SeeAllClicked }
   }
 }

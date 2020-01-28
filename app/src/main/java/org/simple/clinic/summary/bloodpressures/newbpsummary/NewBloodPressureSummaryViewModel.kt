3868205e034f94bcfcc28cb734eb54eb6727809e
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-01-28 10:18:26 +0530

    Add mobius delegate to `NewBloodPressureSummaryView`

--- app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/NewBloodPressureSummaryViewModel.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/newbpsummary/NewBloodPressureSummaryViewModel.kt
@@ -1,13 +1,16 @@
 package org.simple.clinic.summary.bloodpressures.newbpsummary
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.bp.BloodPressureMeasurement
 import java.util.UUID
 
+@Parcelize
 data class NewBloodPressureSummaryViewModel(
     val patientUuid: UUID,
     val latestBloodPressuresToDisplay: List<BloodPressureMeasurement>?,
     val totalRecordedBloodPressureCount: Int?
-) {
+) : Parcelable {
   companion object {
     fun create(patientUuid: UUID) = NewBloodPressureSummaryViewModel(patientUuid, null, null)
   }

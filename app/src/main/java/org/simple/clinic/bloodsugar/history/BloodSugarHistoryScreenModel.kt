a933a882124667a51295fe90f3283bcaea6f76a5
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-30 12:00:40 +0530

    Wire mobius in `BloodSugarHistoryScreen`

--- app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreenModel.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreenModel.kt
@@ -1,14 +1,17 @@
 package org.simple.clinic.bloodsugar.history
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.bloodsugar.BloodSugarMeasurement
 import org.simple.clinic.patient.Patient
 import java.util.UUID
 
+@Parcelize
 data class BloodSugarHistoryScreenModel(
     val patientUuid: UUID,
     val patient: Patient?,
     val bloodSugars: List<BloodSugarMeasurement>?
-) {
+) : Parcelable {
   companion object {
     fun create(patientUuid: UUID) = BloodSugarHistoryScreenModel(patientUuid, null, null)
   }

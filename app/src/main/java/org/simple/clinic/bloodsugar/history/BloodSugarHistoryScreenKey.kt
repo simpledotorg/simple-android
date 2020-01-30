3da44b3c4ca8ae2c0e9a384445e203ba32e34dcd
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-30 11:44:00 +0530

    Implement UI action methods in `BloodSugarHistoryScreen`

---
+++ app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreenKey.kt
@@ -0,0 +1,22 @@
+package org.simple.clinic.bloodsugar.history
+
+import android.os.Parcelable
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.R
+import org.simple.clinic.router.screen.FullScreenKey
+import java.util.UUID
+
+@Parcelize
+data class BloodSugarHistoryScreenKey(
+    val patientUuid: UUID
+) : FullScreenKey, Parcelable {
+
+  @IgnoredOnParcel
+  override val analyticsName: String = "Blood Sugar History"
+
+  override fun layoutRes(): Int {
+    return R.layout.screen_blood_sugar_history
+  }
+}
+

ca2b2976435112125538549d611dfce85c2ddf7a
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-30 11:38:37 +0530

    Implement UI methods in `BloodSugarHistoryScreen`

--- app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreen.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreen.kt
@@ -3,15 +3,45 @@ package org.simple.clinic.bloodsugar.history
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.screen_blood_sugar_history.view.*
+import org.simple.clinic.R
 import org.simple.clinic.bloodsugar.BloodSugarMeasurement
+import org.simple.clinic.bloodsugar.history.adapter.BloodSugarHistoryListItem
+import org.simple.clinic.bloodsugar.history.adapter.BloodSugarHistoryListItemDiffCallback
 import org.simple.clinic.di.injector
+import org.simple.clinic.patient.DateOfBirth
+import org.simple.clinic.patient.Gender
 import org.simple.clinic.patient.Patient
+import org.simple.clinic.patient.displayLetterRes
+import org.simple.clinic.router.screen.ScreenRouter
+import org.simple.clinic.util.UserClock
+import org.simple.clinic.widgets.DividerItemDecorator
+import org.simple.clinic.widgets.ItemAdapter
+import org.simple.clinic.widgets.dp
+import org.threeten.bp.format.DateTimeFormatter
 import java.util.UUID
+import javax.inject.Inject
+import javax.inject.Named
 
 class BloodSugarHistoryScreen(
     context: Context,
     attrs: AttributeSet
 ) : ConstraintLayout(context, attrs), BloodSugarHistoryScreenUi, BloodSugarHistoryScreenUiActions {
+
+  @Inject
+  lateinit var screenRouter: ScreenRouter
+
+  @Inject
+  lateinit var userClock: UserClock
+
+  @field:[Inject Named("date_for_measurement_history")]
+  lateinit var dateFormatter: DateTimeFormatter
+
+  @field:[Inject Named("time_for_measurement_history")]
+  lateinit var timeFormatter: DateTimeFormatter
+
+  private val bloodSugarHistoryAdapter = ItemAdapter(BloodSugarHistoryListItemDiffCallback())
 
   override fun onFinishInflate() {
     super.onFinishInflate()
@@ -19,14 +49,48 @@ class BloodSugarHistoryScreen(
       return
     }
     context.injector<BloodSugarHistoryScreenInjector>().inject(this)
+
+    handleToolbarBackClick()
+    setupBloodSugarHistoryList()
   }
 
   override fun showPatientInformation(patient: Patient) {
+    val ageValue = DateOfBirth.fromPatient(patient, userClock).estimateAge(userClock)
+    displayNameGenderAge(patient.fullName, patient.gender, ageValue)
   }
 
   override fun showBloodSugarHistory(bloodSugars: List<BloodSugarMeasurement>) {
+    bloodSugarHistoryAdapter.submitList(BloodSugarHistoryListItem.from(
+        bloodSugars,
+        userClock,
+        dateFormatter,
+        timeFormatter
+    ))
   }
 
   override fun openBloodSugarEntrySheet(patientUuid: UUID) {
+  }
+
+  private fun displayNameGenderAge(name: String, gender: Gender, age: Int) {
+    val genderLetter = resources.getString(gender.displayLetterRes)
+    toolbar.title = resources.getString(R.string.bloodsugarhistory_toolbar_title, name, genderLetter, age)
+  }
+
+  private fun handleToolbarBackClick() {
+    toolbar.setNavigationOnClickListener {
+      screenRouter.pop()
+    }
+  }
+
+  private fun setupBloodSugarHistoryList() {
+    val dividerMargin = 8.dp
+    val divider = DividerItemDecorator(context = context, marginStart = dividerMargin, marginEnd = dividerMargin)
+
+    bloodSugarHistoryList.apply {
+      setHasFixedSize(true)
+      layoutManager = LinearLayoutManager(context)
+      addItemDecoration(divider)
+      adapter = bloodSugarHistoryAdapter
+    }
   }
 }

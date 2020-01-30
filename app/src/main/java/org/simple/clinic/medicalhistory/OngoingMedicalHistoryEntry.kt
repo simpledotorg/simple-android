53a2e62d5caebae3878695db931c0219f80f3fea
Vinay S Shenoy <vinay@obvious.in>
2020-01-30 18:41:35 +0530

    Remove `IS_ON_TREATMENT_FOR_HYPERTENSION`
    This type is no longer in use.

--- app/src/main/java/org/simple/clinic/medicalhistory/OngoingMedicalHistoryEntry.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/OngoingMedicalHistoryEntry.kt
@@ -8,7 +8,6 @@ import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.DIAGNOSED_WITH_DI
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_HEART_ATTACK
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_KIDNEY_DISEASE
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HAS_HAD_A_STROKE
-import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IS_ON_TREATMENT_FOR_HYPERTENSION
 
 @Parcelize
 data class OngoingMedicalHistoryEntry(
@@ -16,14 +15,12 @@ data class OngoingMedicalHistoryEntry(
     val hasHadStroke: Answer = Unanswered,
     val hasHadKidneyDisease: Answer = Unanswered,
     val diagnosedWithHypertension: Answer = Unanswered,
-    val isOnTreatmentForHypertension: Answer = Unanswered,
     val hasDiabetes: Answer = Unanswered
 ) : Parcelable {
 
   fun answerChanged(question: MedicalHistoryQuestion, answer: Answer): OngoingMedicalHistoryEntry {
     return when (question) {
       DIAGNOSED_WITH_HYPERTENSION -> copy(diagnosedWithHypertension = answer)
-      IS_ON_TREATMENT_FOR_HYPERTENSION -> copy(isOnTreatmentForHypertension = answer)
       HAS_HAD_A_HEART_ATTACK -> copy(hasHadHeartAttack = answer)
       HAS_HAD_A_STROKE -> copy(hasHadStroke = answer)
       HAS_HAD_A_KIDNEY_DISEASE -> copy(hasHadKidneyDisease = answer)

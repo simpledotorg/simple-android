62d9c59bab872d4824220839fed11c65ca1d8912
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-01-30 11:52:14 +0530

    Add `events` to `BloodSugarHistoryScreen`

--- app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreenEvent.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreenEvent.kt
@@ -2,11 +2,14 @@ package org.simple.clinic.bloodsugar.history
 
 import org.simple.clinic.bloodsugar.BloodSugarMeasurement
 import org.simple.clinic.patient.Patient
+import org.simple.clinic.widgets.UiEvent
 
-sealed class BloodSugarHistoryScreenEvent
+sealed class BloodSugarHistoryScreenEvent : UiEvent
 
 data class PatientLoaded(val patient: Patient) : BloodSugarHistoryScreenEvent()
 
 data class BloodSugarHistoryLoaded(val bloodSugars: List<BloodSugarMeasurement>) : BloodSugarHistoryScreenEvent()
 
-object AddNewBloodSugarClicked : BloodSugarHistoryScreenEvent()
+object AddNewBloodSugarClicked : BloodSugarHistoryScreenEvent() {
+  override val analyticsName: String = "Blood Sugar History:Add New Blood Sugar"
+}

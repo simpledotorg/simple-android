adfb88f37623d1e0e6cc76bc127d245a7763a448
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-30 12:00:40 +0530

    Wire mobius in `BloodSugarHistoryScreen`

--- app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreen.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreen.kt
@@ -3,6 +3,7 @@ package org.simple.clinic.bloodsugar.history
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.os.Parcelable
 import android.util.AttributeSet
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -21,10 +22,12 @@ import org.simple.clinic.bloodsugar.history.adapter.BloodSugarHistoryListItemDif
 import org.simple.clinic.bloodsugar.history.adapter.NewBloodSugarClicked
 import org.simple.clinic.bloodsugar.selection.type.BloodSugarTypePickerSheet
 import org.simple.clinic.di.injector
+import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.patient.DateOfBirth
 import org.simple.clinic.patient.Gender
 import org.simple.clinic.patient.Patient
 import org.simple.clinic.patient.displayLetterRes
+import org.simple.clinic.platform.crash.CrashReporter
 import org.simple.clinic.router.screen.ActivityResult
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.summary.TYPE_PICKER_SHEET
@@ -48,9 +51,6 @@ class BloodSugarHistoryScreen(
   lateinit var activity: AppCompatActivity
 
   @Inject
-  lateinit var screenRouter: ScreenRouter
-
-  @Inject
   lateinit var userClock: UserClock
 
   @field:[Inject Named("date_for_measurement_history")]
@@ -59,12 +59,36 @@ class BloodSugarHistoryScreen(
   @field:[Inject Named("time_for_measurement_history")]
   lateinit var timeFormatter: DateTimeFormatter
 
+  @Inject
+  lateinit var screenRouter: ScreenRouter
+
+  @Inject
+  lateinit var effectHandlerFactory: BloodSugarHistoryScreenEffectHandler.Factory
+
+  @Inject
+  lateinit var crashReporter: CrashReporter
+
   private val bloodSugarHistoryAdapter = ItemAdapter(BloodSugarHistoryListItemDiffCallback())
 
   private val events: Observable<BloodSugarHistoryScreenEvent> by unsafeLazy {
     addNewBloodSugarClicked()
         .compose(ReportAnalyticsEvents())
         .cast<BloodSugarHistoryScreenEvent>()
+  }
+
+  private val uiRenderer = BloodSugarHistoryScreenUiRenderer(this)
+
+  private val delegate: MobiusDelegate<BloodSugarHistoryScreenModel, BloodSugarHistoryScreenEvent, BloodSugarHistoryScreenEffect> by unsafeLazy {
+    val screenKey = screenRouter.key<BloodSugarHistoryScreenKey>(this)
+    MobiusDelegate(
+        events = events,
+        defaultModel = BloodSugarHistoryScreenModel.create(screenKey.patientUuid),
+        init = BloodSugarHistoryScreenInit(),
+        update = BloodSugarHistoryScreenUpdate(),
+        effectHandler = effectHandlerFactory.create(this).build(),
+        modelUpdateListener = uiRenderer::render,
+        crashReporter = crashReporter
+    )
   }
 
   override fun onFinishInflate() {
@@ -77,8 +101,28 @@ class BloodSugarHistoryScreen(
     val screenDestroys: Observable<ScreenDestroyed> = detaches().map { ScreenDestroyed() }
     openEntrySheetAfterTypeIsSelected(screenDestroys)
 
+    delegate.prepare()
+
     handleToolbarBackClick()
     setupBloodSugarHistoryList()
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    delegate.start()
+  }
+
+  override fun onDetachedFromWindow() {
+    delegate.stop()
+    super.onDetachedFromWindow()
+  }
+
+  override fun onSaveInstanceState(): Parcelable? {
+    return delegate.onSaveInstanceState(super.onSaveInstanceState())
+  }
+
+  override fun onRestoreInstanceState(state: Parcelable?) {
+    super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
 
   override fun showPatientInformation(patient: Patient) {

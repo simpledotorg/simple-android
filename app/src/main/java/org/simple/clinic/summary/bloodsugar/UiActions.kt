611e8fcc7c9d17bcb892074c0bb01c7c783a50bb
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-01-31 11:26:32 +0530

    Handle `ShowBloodSugarHistoryScreen` effect

--- app/src/main/java/org/simple/clinic/summary/bloodsugar/UiActions.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodsugar/UiActions.kt
@@ -1,5 +1,8 @@
 package org.simple.clinic.summary.bloodsugar
 
+import java.util.UUID
+
 interface UiActions {
   fun showBloodSugarTypeSelector()
+  fun showBloodSugarHistoryScreen(patientUuid: UUID)
 }

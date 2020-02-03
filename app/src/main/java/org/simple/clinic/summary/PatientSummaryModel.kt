352540bb9f4356cecdbce14a08d24d14b7085fac
Vinay S Shenoy <code@vinaysshenoy.com>
2020-02-03 13:55:06 +0530

    Add `currentFacility` to `PatientSummaryModel`
    This will be used to toggle the diabetes widget on the summary screen
    instead of the config field which is in use now.

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryModel.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryModel.kt
@@ -2,21 +2,33 @@ package org.simple.clinic.summary
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.facility.Facility
 import java.util.UUID
 
 @Parcelize
 data class PatientSummaryModel(
     val patientUuid: UUID,
-    val patientSummaryProfile: PatientSummaryProfile?
+    val patientSummaryProfile: PatientSummaryProfile?,
+    val currentFacility: Facility?
 ) : Parcelable {
 
   companion object {
     fun from(patientUuid: UUID): PatientSummaryModel {
-      return PatientSummaryModel(patientUuid, null)
+      return PatientSummaryModel(patientUuid, null, null)
     }
   }
 
+  val hasLoadedCurrentFacility: Boolean
+    get() = currentFacility != null
+
+  val isDiabetesManagementEnabled: Boolean
+    get() = currentFacility!!.config.diabetesManagementEnabled
+
   fun patientSummaryProfileLoaded(patientSummaryProfile: PatientSummaryProfile): PatientSummaryModel {
     return copy(patientSummaryProfile = patientSummaryProfile)
+  }
+
+  fun currentFacilityLoaded(facility: Facility): PatientSummaryModel {
+    return copy(currentFacility = facility)
   }
 }

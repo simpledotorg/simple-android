38e688609be0771dc4e742fcaad38da472d0f74d
Vinay S Shenoy <code@vinaysshenoy.com>
2020-02-03 14:43:36 +0530

    Remove `isDiabetesEnabled` flag from `PatientSummaryConfig`

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
@@ -7,22 +7,20 @@ data class PatientSummaryConfig(
     val numberOfBpPlaceholders: Int,
     val numberOfBpsToDisplay: Int,
     val bpEditableDuration: Duration,
-    val numberOfBloodSugarsToDisplay: Int,
-    val isDiabetesEnabled: Boolean
+    val numberOfBloodSugarsToDisplay: Int
 ) {
 
   companion object {
 
     fun read(configReader: ConfigReader): PatientSummaryConfig {
       val bpEditableDurationConfig = configReader.long("bp_editable_duration_in_seconds", 3600)
-      val isDiabetesEnabled = configReader.boolean("diabetes_enabled", false)
 
       return PatientSummaryConfig(
           numberOfBpPlaceholders = 3,
           numberOfBpsToDisplay = 100,
           bpEditableDuration = Duration.ofSeconds(bpEditableDurationConfig),
-          numberOfBloodSugarsToDisplay = 100,
-          isDiabetesEnabled = isDiabetesEnabled)
+          numberOfBloodSugarsToDisplay = 100
+      )
     }
   }
 }

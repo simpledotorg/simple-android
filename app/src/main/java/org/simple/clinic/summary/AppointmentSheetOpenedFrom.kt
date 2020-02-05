47ad5664494a7cbcf8e79657b8b520f2a12de5cc
Vinay S Shenoy <vinay@obvious.in>
2020-02-05 14:26:05 +0530

    Add enum `AppointmentSheetOpenedFrom`

---
+++ app/src/main/java/org/simple/clinic/summary/AppointmentSheetOpenedFrom.kt
@@ -0,0 +1,20 @@
+package org.simple.clinic.summary
+
+import org.simple.clinic.scheduleappointment.ScheduleAppointmentSheet
+
+/**
+ * When the [ScheduleAppointmentSheet] is closed, we need to go to a different screen from the
+ * [PatientSummaryScreen] depending on whether the sheet was opened by clicking "Back" or "Save"
+ * from the summary screen.
+ *
+ * In the v1 architecture, since we were replaying the events that happened since the screen was
+ * opened, we could reach into the string using `.combineLatest()` or `.withLatestFrom()` and find
+ * out what triggered the sheet to be shown.
+ *
+ * However with Mobius, we cannot do this anymore. We need a way to find out what was the event
+ * that triggered the sheet to be opened and this enum will be used to track that.
+ **/
+enum class AppointmentSheetOpenedFrom {
+  BACK_CLICK,
+  DONE_CLICK
+}

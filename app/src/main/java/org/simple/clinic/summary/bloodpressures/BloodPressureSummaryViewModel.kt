5ef287f6fdd87ab01fcece6b7a27a913c61fc8fd
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-02-05 10:41:19 +0530

    When current facility is loaded then update the model

--- app/src/main/java/org/simple/clinic/summary/bloodpressures/BloodPressureSummaryViewModel.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodpressures/BloodPressureSummaryViewModel.kt
@@ -3,16 +3,18 @@ package org.simple.clinic.summary.bloodpressures
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.bp.BloodPressureMeasurement
+import org.simple.clinic.facility.Facility
 import java.util.UUID
 
 @Parcelize
 data class BloodPressureSummaryViewModel(
     val patientUuid: UUID,
     val latestBloodPressuresToDisplay: List<BloodPressureMeasurement>?,
-    val totalRecordedBloodPressureCount: Int?
+    val totalRecordedBloodPressureCount: Int?,
+    val facility: Facility?
 ) : Parcelable {
   companion object {
-    fun create(patientUuid: UUID) = BloodPressureSummaryViewModel(patientUuid, null, null)
+    fun create(patientUuid: UUID) = BloodPressureSummaryViewModel(patientUuid, null, null, null)
   }
 
   fun bloodPressuresLoaded(bloodPressures: List<BloodPressureMeasurement>): BloodPressureSummaryViewModel =
@@ -20,4 +22,7 @@ data class BloodPressureSummaryViewModel(
 
   fun bloodPressuresCountLoaded(bloodPressuresCount: Int): BloodPressureSummaryViewModel =
       copy(totalRecordedBloodPressureCount = bloodPressuresCount)
+
+  fun currentFacilityLoaded(facility: Facility): BloodPressureSummaryViewModel =
+      copy(facility = facility)
 }

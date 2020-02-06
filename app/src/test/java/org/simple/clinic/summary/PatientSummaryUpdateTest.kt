52298aa095a1f9b0985115b17aba34e8622dd3b3
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-02-06 16:56:49 +0530

    Update UI when patient summary profile is loaded

--- app/src/test/java/org/simple/clinic/summary/PatientSummaryUpdateTest.kt
+++ app/src/test/java/org/simple/clinic/summary/PatientSummaryUpdateTest.kt
@@ -7,9 +7,15 @@ import com.spotify.mobius.test.UpdateSpec.assertThatNext
 import org.junit.Test
 import org.simple.clinic.facility.FacilityConfig
 import org.simple.clinic.patient.PatientMocker
+import org.simple.clinic.patient.businessid.Identifier
+import org.simple.clinic.patient.businessid.Identifier.IdentifierType.BangladeshNationalId
+import org.simple.clinic.patient.businessid.Identifier.IdentifierType.BpPassport
 import java.util.UUID
 
 class PatientSummaryUpdateTest {
+
+  private val defaultModel = PatientSummaryModel.from(UUID.fromString("93a131b0-890e-41a3-88ec-b35b48efc6c5"))
+  private val updateSpec = UpdateSpec(PatientSummaryUpdate())
 
   @Test
   fun `when the current facility is loaded, update the UI`() {
@@ -18,18 +24,40 @@ class PatientSummaryUpdateTest {
         facilityConfig = FacilityConfig(diabetesManagementEnabled = true)
     )
 
-    val model = PatientSummaryModel.from(UUID.fromString("93a131b0-890e-41a3-88ec-b35b48efc6c5"))
-
-    val updateSpec = UpdateSpec(PatientSummaryUpdate())
-
     updateSpec
-        .given(model)
+        .given(defaultModel)
         .whenEvent(CurrentFacilityLoaded(facility))
         .then(
             assertThatNext(
-                hasModel(model.currentFacilityLoaded(facility)),
+                hasModel(defaultModel.currentFacilityLoaded(facility)),
                 hasNoEffects()
             )
         )
+  }
+
+  @Test
+  fun `when the patient summary profile is loaded, then update the UI`() {
+    val patientUuid = UUID.fromString("c995c56d-9515-4991-9c63-dfcca06d93b0")
+    val patient = PatientMocker.patient(patientUuid)
+    val patientAddress = PatientMocker.address(patient.addressUuid)
+    val phoneNumber = PatientMocker.phoneNumber(patientUuid = patientUuid)
+    val bpPassport = PatientMocker.businessId(patientUuid = patientUuid, identifier = Identifier("526 780", BpPassport))
+    val bangladeshNationalId = PatientMocker.businessId(patientUuid = patientUuid, identifier = Identifier("123456789012", BangladeshNationalId))
+
+    val patientSummaryProfile = PatientSummaryProfile(
+        patient = patient,
+        address = patientAddress,
+        phoneNumber = phoneNumber,
+        bpPassport = bpPassport,
+        bangladeshNationalId = bangladeshNationalId
+    )
+
+    updateSpec
+        .given(defaultModel)
+        .whenEvent(PatientSummaryProfileLoaded(patientSummaryProfile))
+        .then(assertThatNext(
+            hasModel(defaultModel.patientSummaryProfileLoaded(patientSummaryProfile)),
+            hasNoEffects()
+        ))
   }
 }

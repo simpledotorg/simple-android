068204e5205368cdb7825bafe33e120f9dfef3f5
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-02-12 08:38:43 +0530

    Add `bloodSugarEditableDuration` to `PatientSummaryConfig`

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
@@ -5,6 +5,7 @@ import org.threeten.bp.Duration
 
 data class PatientSummaryConfig(
     val bpEditableDuration: Duration,
+    val bloodSugarEditableDuration: Duration,
     val numberOfBloodSugarsToDisplay: Int,
     val isDiabetesEnabled: Boolean
 ) {
@@ -13,11 +14,13 @@ data class PatientSummaryConfig(
 
     fun read(configReader: ConfigReader): PatientSummaryConfig {
       val bpEditableDurationConfig = configReader.long("bp_editable_duration_in_seconds", 3600)
+      val bloodSugarEditableDurationConfig = configReader.long("blood_sugar_editable_duration_in_seconds", 3600)
       val isDiabetesEnabled = configReader.boolean("diabetes_enabled", false)
       val numberOfMeasurementsToDisplay = configReader.long("number_of_measurements_to_display_in_summary", 3)
 
       return PatientSummaryConfig(
           bpEditableDuration = Duration.ofSeconds(bpEditableDurationConfig),
+          bloodSugarEditableDuration = Duration.ofSeconds(bloodSugarEditableDurationConfig),
           numberOfBloodSugarsToDisplay = numberOfMeasurementsToDisplay.toInt(),
           isDiabetesEnabled = isDiabetesEnabled)
     }

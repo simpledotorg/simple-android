e9de53cef4065d80707d341893cc8677b467aa1a
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-02-12 09:51:36 +0530

    Open blood sugar update sheet when blood sugar is clicked

--- app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewUpdate.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewUpdate.kt
@@ -16,6 +16,7 @@ class BloodSugarSummaryViewUpdate : Update<BloodSugarSummaryViewModel, BloodSuga
       is BloodSugarCountFetched -> next(model.countFetched(event.count))
       NewBloodSugarClicked -> dispatch(OpenBloodSugarTypeSelector)
       is SeeAllClicked -> dispatch(ShowBloodSugarHistoryScreen(model.patientUuid))
+      is BloodSugarClicked -> dispatch(OpenBloodSugarUpdateSheet(event.bloodSugarMeasurement))
     }
   }
 }

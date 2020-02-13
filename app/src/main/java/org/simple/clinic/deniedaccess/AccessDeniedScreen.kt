4488217a2f90e98d4ef97fbf828b2debe4666e12
Sanchita Agarwal <sanchita@obvious.in>
2020-02-13 18:40:36 +0530

    Match UI for `AccessDeniedScreen`

--- app/src/main/java/org/simple/clinic/deniedaccess/AccessDeniedScreen.kt
+++ app/src/main/java/org/simple/clinic/deniedaccess/AccessDeniedScreen.kt
@@ -2,31 +2,15 @@ package org.simple.clinic.deniedaccess
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.Button
 import android.widget.RelativeLayout
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
-import io.reactivex.rxkotlin.ofType
-import kotterknife.bindView
-import org.simple.clinic.R
+import kotlinx.android.synthetic.main.screen_access_denied.view.*
+import kotlinx.android.synthetic.main.screen_patient_summary.view.*
 import org.simple.clinic.main.TheActivity
-import org.simple.clinic.bindUiToController
-import org.simple.clinic.login.applock.AppLockBackClicked
-import org.simple.clinic.login.applock.AppLockForgotPinClicked
-import org.simple.clinic.login.applock.AppLockPinAuthenticated
-import org.simple.clinic.login.applock.AppLockScreenCreated
-import org.simple.clinic.login.applock.ConfirmResetPinDialog
-import org.simple.clinic.router.screen.BackPressInterceptCallback
-import org.simple.clinic.router.screen.BackPressInterceptor
 import org.simple.clinic.router.screen.ScreenRouter
-import org.simple.clinic.security.pin.PinAuthenticated
-import org.simple.clinic.security.pin.PinDigestToVerify
-import org.simple.clinic.security.pin.PinEntryCardView
-import org.simple.clinic.widgets.ScreenDestroyed
-import org.simple.clinic.widgets.showKeyboard
+import org.simple.clinic.widgets.ScreenCreated
 import javax.inject.Inject
 
 class AccessDeniedScreen(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
@@ -36,10 +20,6 @@ class AccessDeniedScreen(context: Context, attrs: AttributeSet) : RelativeLayout
 
   @Inject
   lateinit var activity: AppCompatActivity
-
-  private val fullNameTextView by bindView<TextView>(R.id.applock_user_fullname)
-  private val logoutButton by bindView<Button>(R.id.applock_logout)
-  private val pinEntryCardView by bindView<PinEntryCardView>(R.id.applock_pin_entry_card)
 
   override fun onFinishInflate() {
     super.onFinishInflate()
@@ -51,33 +31,12 @@ class AccessDeniedScreen(context: Context, attrs: AttributeSet) : RelativeLayout
     logoutButton.setOnClickListener {
       Toast.makeText(context, "Work in progress", Toast.LENGTH_SHORT).show()
     }
-
-    // The keyboard shows up on PIN field automatically when the app is
-    // starting, but not when the user comes back from FacilityChangeScreen.
-    pinEntryCardView.pinEditText.showKeyboard()
   }
 
-  private fun screenCreates() = Observable.just(AppLockScreenCreated())
-
-  private fun backClicks(): Observable<AppLockBackClicked> {
-    return Observable.create { emitter ->
-      val interceptor = object : BackPressInterceptor {
-        override fun onInterceptBackPress(callback: BackPressInterceptCallback) {
-          emitter.onNext(AppLockBackClicked())
-          callback.markBackPressIntercepted()
-        }
-      }
-      emitter.setCancellable { screenRouter.unregisterBackPressInterceptor(interceptor) }
-      screenRouter.registerBackPressInterceptor(interceptor)
-    }
-  }
+  private fun screenCreates() = Observable.just(ScreenCreated())
 
   fun setUserFullName(fullName: String) {
     fullNameTextView.text = fullName
-  }
-
-  fun restorePreviousScreen() {
-    screenRouter.pop()
   }
 
   fun exitApp() {

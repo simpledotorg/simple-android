0ccc566a2a9e5e43881119bb16f329d1ce72a932
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-02-13 09:20:50 +0530

    Remove `isDiabetesEnabled` from `PatientSummaryConfig`

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
@@ -3,20 +3,15 @@ package org.simple.clinic.summary
 import org.simple.clinic.remoteconfig.ConfigReader
 import org.threeten.bp.Duration
 
-data class PatientSummaryConfig(
-    val bpEditableDuration: Duration,
-    val isDiabetesEnabled: Boolean
-) {
+data class PatientSummaryConfig(val bpEditableDuration: Duration) {
 
   companion object {
 
     fun read(configReader: ConfigReader): PatientSummaryConfig {
       val bpEditableDurationConfig = configReader.long("bp_editable_duration_in_seconds", 3600)
-      val isDiabetesEnabled = configReader.boolean("diabetes_enabled", false)
 
       return PatientSummaryConfig(
-          bpEditableDuration = Duration.ofSeconds(bpEditableDurationConfig),
-          isDiabetesEnabled = isDiabetesEnabled)
+          bpEditableDuration = Duration.ofSeconds(bpEditableDurationConfig))
     }
   }
 }

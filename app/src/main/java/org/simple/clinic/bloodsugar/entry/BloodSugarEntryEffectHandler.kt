9177513fb92b790d092164ba9cddbf5e5527189e
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-02-17 14:59:23 +0530

    Show confirm remove blood sugar dialog when remove button is clicked

--- app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntryEffectHandler.kt
@@ -68,6 +68,9 @@ class BloodSugarEntryEffectHandler @AssistedInject constructor(
         .addTransformer(FetchBloodSugarMeasurement::class.java, fetchBloodSugarMeasurement(schedulersProvider.io()))
         .addConsumer(SetBloodSugarReading::class.java, { ui.setBloodSugarReading(it.bloodSugarReading) }, schedulersProvider.ui())
         .addTransformer(UpdateBloodSugarEntry::class.java, updateBloodSugarEntryTransformer(schedulersProvider.io()))
+        .addConsumer(ShowConfirmRemoveBloodSugarDialog::class.java, {
+          // TODO (SM): Show confirm remove blood sugar dialog
+        })
         .build()
   }
 

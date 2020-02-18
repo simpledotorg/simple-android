f21c6c1076b0b4a846d885aad0428910e6628294
Vinay S Shenoy <code@vinaysshenoy.com>
2020-02-18 14:38:21 +0530

    Remove unused code from `PatientSummaryScreenController`

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryScreenController.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryScreenController.kt
@@ -9,16 +9,11 @@ import io.reactivex.rxkotlin.ofType
 import io.reactivex.rxkotlin.zipWith
 import org.simple.clinic.ReplayUntilScreenIsDestroyed
 import org.simple.clinic.analytics.Analytics
-import org.simple.clinic.overdue.Appointment
-import org.simple.clinic.overdue.Appointment.Status.Cancelled
-import org.simple.clinic.overdue.AppointmentCancelReason.InvalidPhoneNumber
-import org.simple.clinic.overdue.AppointmentRepository
 import org.simple.clinic.patient.PatientRepository
 import org.simple.clinic.summary.OpenIntention.LinkIdWithPatient
 import org.simple.clinic.summary.OpenIntention.ViewNewPatient
 import org.simple.clinic.summary.addphone.MissingPhoneReminderRepository
 import org.simple.clinic.util.None
-import org.simple.clinic.util.filterAndUnwrapJust
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
 import java.util.UUID
@@ -30,7 +25,6 @@ class PatientSummaryScreenController @AssistedInject constructor(
     @Assisted private val patientUuid: UUID,
     @Assisted private val openIntention: OpenIntention,
     private val patientRepository: PatientRepository,
-    private val appointmentRepository: AppointmentRepository,
     private val missingPhoneReminderRepository: MissingPhoneReminderRepository
 ) : ObservableTransformer<UiEvent, UiChange> {
 
@@ -71,16 +65,9 @@ class PatientSummaryScreenController @AssistedInject constructor(
   }
 
   private fun showUpdatePhoneDialogIfRequired(events: Observable<UiEvent>): Observable<UiChange> {
-    val showForInvalidPhone = hasInvalidPhone(patientUuid)
-        .take(1)
-        .filter { invalid -> invalid }
-        .map { { ui: Ui -> /*ui.showUpdatePhoneDialog(patientUuid)*/ } }
-
-    val waitTillABpIsRecorded = events
+    return events
         .ofType<PatientSummaryBloodPressureSaved>()
         .take(1)
-
-    val showForMissingPhone = waitTillABpIsRecorded
         .filter { openIntention != ViewNewPatient }
         .switchMap {
           isMissingPhoneAndShouldBeReminded(patientUuid)
@@ -92,15 +79,6 @@ class PatientSummaryScreenController @AssistedInject constructor(
                     .andThen(Observable.just { ui: Ui -> ui.showAddPhoneDialog(patientUuid) })
               }
         }
-
-    return showForInvalidPhone.mergeWith(showForMissingPhone)
-  }
-
-  private fun hasInvalidPhone(patientUuid: UUID): Observable<Boolean> {
-    return patientRepository.phoneNumber(patientUuid)
-        .filterAndUnwrapJust()
-        .zipWith(lastCancelledAppointmentWithInvalidPhone(patientUuid))
-        .map { (number, appointment) -> appointment.updatedAt > number.updatedAt }
   }
 
   private fun isMissingPhoneAndShouldBeReminded(patientUuid: UUID): Observable<Boolean> {
@@ -108,13 +86,6 @@ class PatientSummaryScreenController @AssistedInject constructor(
         .phoneNumber(patientUuid)
         .zipWith(hasShownReminderForMissingPhone(patientUuid))
         .map { (number, reminderShown) -> number is None && reminderShown.not() }
-  }
-
-  private fun lastCancelledAppointmentWithInvalidPhone(patientUuid: UUID): Observable<Appointment> {
-    return appointmentRepository
-        .lastCreatedAppointmentForPatient(patientUuid)
-        .filterAndUnwrapJust()
-        .filter { it.status == Cancelled && it.cancelReason == InvalidPhoneNumber }
   }
 
   private fun hasShownReminderForMissingPhone(patientUuid: UUID): Observable<Boolean> {

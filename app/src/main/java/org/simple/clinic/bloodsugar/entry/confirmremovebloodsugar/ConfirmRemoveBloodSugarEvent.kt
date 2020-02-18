95cac767413767358c5ba93a1e5f844bb629f666
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-02-18 17:02:20 +0530

    Delete blood sugar when remove button is clicked in confirm dialog

--- app/src/main/java/org/simple/clinic/bloodsugar/entry/confirmremovebloodsugar/ConfirmRemoveBloodSugarEvent.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/confirmremovebloodsugar/ConfirmRemoveBloodSugarEvent.kt
@@ -5,3 +5,5 @@ import org.simple.clinic.widgets.UiEvent
 sealed class ConfirmRemoveBloodSugarEvent : UiEvent
 
 object BloodSugarMarkedAsDeleted : ConfirmRemoveBloodSugarEvent()
+
+object RemoveBloodSugarClicked : ConfirmRemoveBloodSugarEvent()

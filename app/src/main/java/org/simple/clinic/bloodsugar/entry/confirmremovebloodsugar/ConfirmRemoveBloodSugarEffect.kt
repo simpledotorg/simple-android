c33f2a1734aa3a29483bf7ee81da2b931f31464c
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-02-18 10:08:18 +0530

    Handle `MarkBloodSugarAsDeleted` effect

---
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/confirmremovebloodsugar/ConfirmRemoveBloodSugarEffect.kt
@@ -0,0 +1,7 @@
+package org.simple.clinic.bloodsugar.entry.confirmremovebloodsugar
+
+import java.util.UUID
+
+sealed class ConfirmRemoveBloodSugarEffect
+
+data class MarkBloodSugarAsDeleted(val bloodSugarMeasurementUuid: UUID) : ConfirmRemoveBloodSugarEffect()

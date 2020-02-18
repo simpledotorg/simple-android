61fcac5ea102b36dc42dfc57fbd52ebf9095a107
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-02-18 17:02:20 +0530

    Delete blood sugar when remove button is clicked in confirm dialog

---
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/confirmremovebloodsugar/ConfirmRemoveBloodSugarUpdate.kt
@@ -0,0 +1,18 @@
+package org.simple.clinic.bloodsugar.entry.confirmremovebloodsugar
+
+import com.spotify.mobius.Next
+import com.spotify.mobius.Next.noChange
+import com.spotify.mobius.Update
+import org.simple.clinic.mobius.dispatch
+
+class ConfirmRemoveBloodSugarUpdate : Update<ConfirmRemoveBloodSugarModel, ConfirmRemoveBloodSugarEvent, ConfirmRemoveBloodSugarEffect> {
+  override fun update(
+      model: ConfirmRemoveBloodSugarModel,
+      event: ConfirmRemoveBloodSugarEvent
+  ): Next<ConfirmRemoveBloodSugarModel, ConfirmRemoveBloodSugarEffect> {
+    return when (event) {
+      RemoveBloodSugarClicked -> dispatch(MarkBloodSugarAsDeleted(model.bloodSugarMeasurementUuid))
+      else -> noChange()
+    }
+  }
+}

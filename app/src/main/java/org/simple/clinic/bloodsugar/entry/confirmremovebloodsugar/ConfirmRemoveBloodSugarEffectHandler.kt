66f2f9eb86c5605a780276ea76c5c9ddcb66b229
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-02-18 10:08:18 +0530

    Handle `MarkBloodSugarAsDeleted` effect

---
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/confirmremovebloodsugar/ConfirmRemoveBloodSugarEffectHandler.kt
@@ -0,0 +1,38 @@
+package org.simple.clinic.bloodsugar.entry.confirmremovebloodsugar
+
+import com.spotify.mobius.rx2.RxMobius
+import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
+import org.simple.clinic.bloodsugar.BloodSugarRepository
+import org.simple.clinic.patient.PatientRepository
+import org.simple.clinic.util.scheduler.SchedulersProvider
+import javax.inject.Inject
+
+class ConfirmRemoveBloodSugarEffectHandler @Inject constructor(
+    private val patientRepository: PatientRepository,
+    private val bloodSugarRepository: BloodSugarRepository,
+    private val schedulersProvider: SchedulersProvider
+) {
+
+  fun build(): ObservableTransformer<ConfirmRemoveBloodSugarEffect, ConfirmRemoveBloodSugarEvent> {
+    return RxMobius
+        .subtypeEffectHandler<ConfirmRemoveBloodSugarEffect, ConfirmRemoveBloodSugarEvent>()
+        .addTransformer(MarkBloodSugarAsDeleted::class.java, markBloodSugarAsDeleted(schedulersProvider.io()))
+        .build()
+  }
+
+  private fun markBloodSugarAsDeleted(
+      scheduler: Scheduler
+  ): ObservableTransformer<MarkBloodSugarAsDeleted, ConfirmRemoveBloodSugarEvent> {
+    return ObservableTransformer { markBloodSugarAsDeletedStream ->
+      markBloodSugarAsDeletedStream
+          .observeOn(scheduler)
+          .map { bloodSugarRepository.measurement(it.bloodSugarMeasurementUuid)!! }
+          .doOnNext(bloodSugarRepository::markBloodSugarAsDeleted)
+          .flatMap {
+            patientRepository.updateRecordedAt(it.patientUuid)
+                .andThen(Observable.just(BloodSugarMarkedAsDeleted))
+          }
+    }
+  }
+}

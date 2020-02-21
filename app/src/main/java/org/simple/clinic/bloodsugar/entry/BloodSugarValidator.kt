bb53bb268702ddd883303ef5bdba4a9e79611506
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-02-21 11:00:09 +0530

    Add validation for `HbA1c` blood sugar type

--- app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarValidator.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarValidator.kt
@@ -1,31 +1,36 @@
 package org.simple.clinic.bloodsugar.entry
 
+import org.simple.clinic.bloodsugar.BloodSugarMeasurementType
+import org.simple.clinic.bloodsugar.HbA1c
 import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarEmpty
 import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarTooHigh
 import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarTooLow
+import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.Valid
 import javax.inject.Inject
 
 class BloodSugarValidator @Inject constructor() {
 
   sealed class Result {
-    data class Valid(val bloodSugarReading: Int) : Result()
+    data class Valid(val bloodSugarReading: Float) : Result()
 
     object ErrorBloodSugarEmpty : Result()
     object ErrorBloodSugarTooHigh : Result()
     object ErrorBloodSugarTooLow : Result()
   }
 
-  fun validate(bloodSugarReading: String): Result {
+  fun validate(bloodSugarReading: String, measurementType: BloodSugarMeasurementType): Result {
     if (bloodSugarReading.isBlank()) {
       return ErrorBloodSugarEmpty
     }
 
-    val bloodSugarNumber = bloodSugarReading.trim().toInt()
+    val bloodSugarNumber = bloodSugarReading.trim().toFloat()
+    val minAllowedBloodSugarValue = if (measurementType is HbA1c) 3 else 30
+    val maxAllowedBloodSugarValue = if (measurementType is HbA1c) 25 else 1000
 
     return when {
-      bloodSugarNumber < 30 -> ErrorBloodSugarTooLow
-      bloodSugarNumber > 1000 -> ErrorBloodSugarTooHigh
-      else -> Result.Valid(bloodSugarNumber)
+      bloodSugarNumber < minAllowedBloodSugarValue -> ErrorBloodSugarTooLow
+      bloodSugarNumber > maxAllowedBloodSugarValue -> ErrorBloodSugarTooHigh
+      else -> Valid(bloodSugarNumber)
     }
   }
 }
--- app/src/test/java/org/simple/clinic/bloodsugar/entry/BloodSugarValidatorTest.kt
+++ app/src/test/java/org/simple/clinic/bloodsugar/entry/BloodSugarValidatorTest.kt
@@ -2,6 +2,9 @@ package org.simple.clinic.bloodsugar.entry
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import org.simple.clinic.bloodsugar.HbA1c
+import org.simple.clinic.bloodsugar.PostPrandial
+import org.simple.clinic.bloodsugar.Random
 import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarEmpty
 import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarTooHigh
 import org.simple.clinic.bloodsugar.entry.BloodSugarValidator.Result.ErrorBloodSugarTooLow
@@ -12,7 +15,7 @@ class BloodSugarValidatorTest {
   @Test
   fun `when blood sugar reading is empty, return error`() {
     val bloodSugarReading = ""
-    val result = bloodSugarValidator.validate(bloodSugarReading)
+    val result = bloodSugarValidator.validate(bloodSugarReading, Random)
 
     assertThat(result).isEqualTo(ErrorBloodSugarEmpty)
   }
@@ -20,15 +23,32 @@ class BloodSugarValidatorTest {
   @Test
   fun `when blood sugar reading is more than maximum possible, return error`() {
     val bloodSugarReading = "1001"
-    val result = bloodSugarValidator.validate(bloodSugarReading)
+    val result = bloodSugarValidator.validate(bloodSugarReading, PostPrandial)
 
     assertThat(result).isEqualTo(ErrorBloodSugarTooHigh)
   }
 
   @Test
+  fun `when hba1c blood sugar reading is more than maximum possible, return error`() {
+    val bloodSugarReading = "26"
+    val result = bloodSugarValidator.validate(bloodSugarReading, HbA1c)
+
+    assertThat(result).isEqualTo(ErrorBloodSugarTooHigh)
+  }
+
+
+  @Test
   fun `when blood sugar reading is less than minimum possible, return error`() {
     val bloodSugarReading = "29"
-    val result = bloodSugarValidator.validate(bloodSugarReading)
+    val result = bloodSugarValidator.validate(bloodSugarReading, Random)
+
+    assertThat(result).isEqualTo(ErrorBloodSugarTooLow)
+  }
+
+  @Test
+  fun `when hba1c blood sugar reading is less than minimum possible, return error`() {
+    val bloodSugarReading = "2"
+    val result = bloodSugarValidator.validate(bloodSugarReading, HbA1c)
 
     assertThat(result).isEqualTo(ErrorBloodSugarTooLow)
   }

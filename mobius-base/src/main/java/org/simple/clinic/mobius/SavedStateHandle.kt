0f75acd506a2d7c579e75f149d50b5ccc36ccc42
Vinay S Shenoy <vinay@obvious.in>
2020-02-24 18:53:12 +0530

    Add implementation `ActivitySavedStateHandle` of `SavedStateHandle`
    This will be used to implement the state saving and restoration
    mechanism as understood by `AppCompatActivity`.

--- mobius-base/src/main/java/org/simple/clinic/mobius/SavedStateHandle.kt
+++ mobius-base/src/main/java/org/simple/clinic/mobius/SavedStateHandle.kt
@@ -1,10 +1,34 @@
 package org.simple.clinic.mobius
 
+import android.os.Bundle
 import android.os.Parcelable
 
-interface SavedStateHandle {
+interface SavedStateHandle<M : Parcelable> {
 
-  fun save(state: Parcelable?): Parcelable
+  fun save(state: Parcelable?, model: M): Parcelable
 
-  fun restore(state: Parcelable): Parcelable
+  fun restore(state: Parcelable): Pair<M, Parcelable>
+}
+
+class ActivitySavedStateHandle<M : Parcelable>(
+    private val modelKey: String
+) : SavedStateHandle<M> {
+
+  override fun save(state: Parcelable?, model: M): Parcelable {
+    requireNotNull(state)
+
+    val bundle = state as Bundle
+    bundle.putParcelable(modelKey, model)
+
+    return bundle
+  }
+
+  override fun restore(state: Parcelable): Pair<M, Parcelable> {
+    val bundle = state as Bundle
+
+    val model = bundle.getParcelable<M>(modelKey)
+    requireNotNull(model)
+
+    return model to bundle
+  }
 }

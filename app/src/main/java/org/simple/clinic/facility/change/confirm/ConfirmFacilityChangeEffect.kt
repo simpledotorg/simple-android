3f1e87fdda619f52533614d6447b6ab9a91a4ded
Sanchita Agarwal <sanchita@obvious.in>
2020-03-06 15:03:48 +0530

    Add `ConfirmFacilityChangeUpdate`

---
+++ app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeEffect.kt
@@ -0,0 +1,5 @@
+package org.simple.clinic.facility.change.confirm
+
+sealed class ConfirmFacilityChangeEffect
+
+object ChangeFacilityEffect : ConfirmFacilityChangeEffect()
---
+++ app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeModel.kt
@@ -0,0 +1,3 @@
+package org.simple.clinic.facility.change.confirm
+
+class ConfirmFacilityChangeModel

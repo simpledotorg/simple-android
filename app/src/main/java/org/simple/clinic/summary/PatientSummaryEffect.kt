6cdfb73eb23843895a685465c2ead1fbc1a89a79
Vinay Shenoy <vinay@obvious.in>
2020-03-09 20:23:32 +0530

    Make handling of back clicks mobius-idiomatic in patient summary

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryEffect.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryEffect.kt
@@ -8,13 +8,6 @@ sealed class PatientSummaryEffect
 
 data class LoadPatientSummaryProfile(val patientUuid: UUID) : PatientSummaryEffect()
 
-// TODO(vs): 2020-01-15 Revisit this effect once the patient summary migration is done
-data class HandleBackClick(
-    val patientUuid: UUID,
-    val screenCreatedTimestamp: Instant,
-    val openIntention: OpenIntention
-) : PatientSummaryEffect()
-
 // TODO(vs): 2020-01-16 Revisit this effect once the patient summary migration is done
 data class HandleDoneClick(
     val patientUuid: UUID

dbf63cb2f10f278e28197599b082783d7207bec8
Vinay Shenoy <code@vinaysshenoy.com>
2020-03-10 21:06:00 +0530

    Report instrumentation test flakiness

--- app/src/sharedTest/java/org/simple/clinic/util/Rules.kt
+++ app/src/sharedTest/java/org/simple/clinic/util/Rules.kt
@@ -1,10 +1,18 @@
 package org.simple.clinic.util
 
+import com.vinaysshenoy.quarantine.QuarantineTestRule
 import org.junit.rules.RuleChain
 
 object Rules {
 
+  private var quarantineClassLoader: ClassLoader = ClassLoader.getSystemClassLoader()
+
+  fun overrideQuarantineClassloader(classLoader: ClassLoader) {
+    quarantineClassLoader = classLoader
+  }
+
   fun global(): RuleChain = RuleChain
       .emptyRuleChain()
+      .around(QuarantineTestRule(quarantineClassLoader))
       .around(RxErrorsRule())
 }

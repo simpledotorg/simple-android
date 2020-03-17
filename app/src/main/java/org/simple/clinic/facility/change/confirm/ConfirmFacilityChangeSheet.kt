5c7633280cd78a18304f4ce097c4cf62390dfdf1
Sanchita Agarwal <sanchita@obvious.in>
2020-03-17 20:28:00 +0530

    Add event for confirming facility change

--- app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeSheet.kt
+++ app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeSheet.kt
@@ -3,11 +3,16 @@ package org.simple.clinic.facility.change.confirm
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.jakewharton.rxbinding3.view.clicks
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.sheet_confirm_facility_change.*
 import org.simple.clinic.ClinicApp
 import org.simple.clinic.R
 import org.simple.clinic.facility.Facility
 import org.simple.clinic.facility.change.confirm.di.ConfirmFacilityChangeComponent
+import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.BottomSheetActivity
+import org.simple.clinic.widgets.UiEvent
 
 class ConfirmFacilityChangeSheet : BottomSheetActivity() {
 
@@ -26,6 +31,14 @@ class ConfirmFacilityChangeSheet : BottomSheetActivity() {
     }
   }
 
+  private val selectedFacility: Facility by lazy {
+    intent.getParcelableExtra<Facility>(SELECTED_FACILITY)!!
+  }
+
+  private val events: Observable<UiEvent> by unsafeLazy {
+    positiveButtonClicks()
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
@@ -41,4 +54,7 @@ class ConfirmFacilityChangeSheet : BottomSheetActivity() {
 
     component.inject(this)
   }
+
+  private fun positiveButtonClicks(): Observable<UiEvent> =
+      yesButton.clicks().map { FacilityChangeConfirmed(selectedFacility) }
 }

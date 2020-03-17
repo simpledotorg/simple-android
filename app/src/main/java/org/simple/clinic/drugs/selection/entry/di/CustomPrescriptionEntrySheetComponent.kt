da82facb85a0113a504f78b8c39fa666f7bb6234
Vinay Shenoy <vinay@obvious.in>
2020-03-17 21:22:35 +0530

    Change `ConfirmRemovePrescriptionDialog` to inject using the context

--- app/src/main/java/org/simple/clinic/drugs/selection/entry/di/CustomPrescriptionEntrySheetComponent.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/di/CustomPrescriptionEntrySheetComponent.kt
@@ -4,9 +4,10 @@ import dagger.Subcomponent
 import org.simple.clinic.activity.BindsActivity
 import org.simple.clinic.di.AssistedInjectModule
 import org.simple.clinic.drugs.selection.entry.CustomPrescriptionEntrySheet
+import org.simple.clinic.drugs.selection.entry.confirmremovedialog.ConfirmRemovePrescriptionDialog
 
 @Subcomponent(modules = [AssistedInjectModule::class])
-interface CustomPrescriptionEntrySheetComponent {
+interface CustomPrescriptionEntrySheetComponent: ConfirmRemovePrescriptionDialog.Injector {
 
   fun inject(target: CustomPrescriptionEntrySheet)
 

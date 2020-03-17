e324879cd54888b16646f3e44fa96eb0ac848a35
Sanchita Agarwal <sanchita@obvious.in>
2020-03-17 14:01:38 +0530

    Handle `CloseSheet` effect

--- app/src/test/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeEffectHandlerTest.kt
+++ app/src/test/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeEffectHandlerTest.kt
@@ -3,6 +3,8 @@ package org.simple.clinic.facility.change.confirm
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
+import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Completable
 import io.reactivex.Single
@@ -19,16 +21,17 @@ import java.util.UUID
 
 class ConfirmFacilityChangeEffectHandlerTest {
 
+  private val userSession = mock<UserSession>()
+  private val facilityRepository = mock<FacilityRepository>()
+  private val reportsRepository = mock<ReportsRepository>()
+  private val reportsSync = mock<ReportsSync>()
+  private val uiActions = mock<ConfirmFacilityChangeUiActions>()
+  private val effectHandler = ConfirmFacilityChangeEffectHandler(facilityRepository, userSession, reportsRepository, reportsSync, TrampolineSchedulersProvider(), uiActions)
+  private val testCase = EffectHandlerTestCase(effectHandler.build())
+
   @Test
   fun `when facility change effect is received, then change user's current facility`() {
     //given
-    val userSession = mock<UserSession>()
-    val facilityRepository = mock<FacilityRepository>()
-    val reportsRepository = mock<ReportsRepository>()
-    val reportsSync = mock<ReportsSync>()
-    val effectHandler = ConfirmFacilityChangeEffectHandler(facilityRepository, userSession, reportsRepository, reportsSync, TrampolineSchedulersProvider())
-    val testCase = EffectHandlerTestCase(effectHandler.build())
-
     val facility = TestData.facility(UUID.fromString("98a260cb-45b1-46f7-a7ca-d217a27c43c0"))
     val loggedInUser = TestData.loggedInUser(UUID.fromString("7d8dce15-701b-4cf9-8dee-e003c51ccde9"))
 
@@ -39,11 +42,23 @@ class ConfirmFacilityChangeEffectHandlerTest {
     whenever(reportsSync.sync()) doReturn Completable.complete()
 
     //when
-    testCase.dispatch(ChangeFacilityEffect(facility) as ConfirmFacilityChangeEffect)
+    testCase.dispatch(ChangeFacilityEffect(facility))
 
     //then
     testCase.assertOutgoingEvents(FacilityChanged)
     verify(reportsRepository).deleteReportsFile()
     verify(reportsSync).sync()
+    verifyZeroInteractions(uiActions)
+  }
+
+  @Test
+  fun `when close sheet effect is received then the sheet should be closed`() {
+    //when
+    testCase.dispatch(CloseSheet)
+
+    //then
+    testCase.assertNoOutgoingEvents()
+    verify(uiActions).closeSheet()
+    verifyNoMoreInteractions(uiActions)
   }
 }

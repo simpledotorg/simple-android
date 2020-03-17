8563f0b9c10ec4e5cecb5e07f232b2b154a633f1
Sanchita Agarwal <sanchita@obvious.in>
2020-03-17 20:36:57 +0530

    Add `ConfirmFacilityChangeInit`

---
+++ app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeInit.kt
@@ -0,0 +1,10 @@
+package org.simple.clinic.facility.change.confirm
+
+import com.spotify.mobius.First
+import com.spotify.mobius.Init
+
+class ConfirmFacilityChangeInit : Init<ConfirmFacilityChangeModel, ConfirmFacilityChangeEffect> {
+  override fun init(model: ConfirmFacilityChangeModel): First<ConfirmFacilityChangeModel, ConfirmFacilityChangeEffect> {
+    return First.first(ConfirmFacilityChangeModel())
+  }
+}

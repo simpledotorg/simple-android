78a91fb5a8215533c52749fc6bce8c4407bdb118
Vinay Shenoy <vinay@obvious.in>
2020-03-17 20:27:28 +0530

    Use dedicated injector in `BloodSugarTypePickerSheet`

--- app/src/main/java/org/simple/clinic/bloodsugar/selection/type/BloodSugarTypePickerSheet.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/selection/type/BloodSugarTypePickerSheet.kt
@@ -4,17 +4,30 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import kotlinx.android.synthetic.main.sheet_blood_sugar_type_picker.*
+import org.simple.clinic.ClinicApp
 import org.simple.clinic.R
 import org.simple.clinic.bloodsugar.BloodSugarMeasurementType
 import org.simple.clinic.bloodsugar.Fasting
 import org.simple.clinic.bloodsugar.HbA1c
 import org.simple.clinic.bloodsugar.PostPrandial
 import org.simple.clinic.bloodsugar.Random
+import org.simple.clinic.bloodsugar.selection.type.di.BloodSugarTypePickerSheetComponent
+import org.simple.clinic.di.InjectorProviderContextWrapper
+import org.simple.clinic.util.LocaleOverrideContextWrapper
 import org.simple.clinic.util.unsafeLazy
-import org.simple.clinic.widgets.BottomSheetActivityOld
+import org.simple.clinic.util.wrap
+import org.simple.clinic.widgets.BottomSheetActivity
+import java.util.Locale
+import javax.inject.Inject
 
-class BloodSugarTypePickerSheet : BottomSheetActivityOld() {
+class BloodSugarTypePickerSheet : BottomSheetActivity() {
+
+  @Inject
+  lateinit var locale: Locale
+
+  private lateinit var component: BloodSugarTypePickerSheetComponent
 
   private val bloodSugarTypesList by unsafeLazy {
     listOf(
@@ -37,6 +50,26 @@ class BloodSugarTypePickerSheet : BottomSheetActivityOld() {
 
     typesRecyclerView.adapter = adapter
     adapter.submitList(bloodSugarTypesList)
+  }
+
+  override fun attachBaseContext(baseContext: Context) {
+    setupDiGraph()
+
+    val wrappedContext = baseContext
+        .wrap { LocaleOverrideContextWrapper.wrap(it, locale) }
+        .wrap { InjectorProviderContextWrapper.wrap(it, component) }
+        .wrap { ViewPumpContextWrapper.wrap(it) }
+
+    super.attachBaseContext(wrappedContext)
+  }
+
+  private fun setupDiGraph() {
+    component = ClinicApp.appComponent
+        .bloodSugarTypePickerSheetComponentBuilder()
+        .activity(this)
+        .build()
+
+    component.inject(this)
   }
 
   companion object {

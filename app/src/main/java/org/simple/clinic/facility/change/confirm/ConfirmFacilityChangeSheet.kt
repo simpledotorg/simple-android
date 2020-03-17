9118dc99b2404bd0ee7a1df7cf1bf99a538a1bcd
Sanchita Agarwal <sanchita@obvious.in>
2020-03-17 20:20:17 +0530

    Add intent for opening `ConfirmFacilityChangeSheet`

--- app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeSheet.kt
+++ app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeSheet.kt
@@ -1,8 +1,11 @@
 package org.simple.clinic.facility.change.confirm
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import org.simple.clinic.ClinicApp
 import org.simple.clinic.R
+import org.simple.clinic.facility.Facility
 import org.simple.clinic.facility.change.confirm.di.ConfirmFacilityChangeComponent
 import org.simple.clinic.widgets.BottomSheetActivity
 
@@ -10,6 +13,17 @@ class ConfirmFacilityChangeSheet : BottomSheetActivity() {
 
   companion object {
     lateinit var component: ConfirmFacilityChangeComponent
+
+    private const val SELECTED_FACILITY = "seleceted_facility"
+
+    fun intent(
+        context: Context,
+        facility: Facility
+    ): Intent {
+      val intent = Intent(context, ConfirmFacilityChangeSheet::class.java)
+      intent.putExtra(SELECTED_FACILITY, facility)
+      return intent
+    }
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {

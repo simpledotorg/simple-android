71cbd36bfa7c612c796a5c52fee4a6ad7b84f5ea
Vinay Shenoy <vinay@obvious.in>
2020-03-20 14:53:52 +0530

    Remove unused methods from `MissingPhoneReminderRepository`

--- app/src/main/java/org/simple/clinic/summary/addphone/MissingPhoneReminderRepository.kt
+++ app/src/main/java/org/simple/clinic/summary/addphone/MissingPhoneReminderRepository.kt
@@ -1,7 +1,6 @@
 package org.simple.clinic.summary.addphone
 
 import io.reactivex.Completable
-import io.reactivex.Single
 import org.simple.clinic.patient.PatientUuid
 import org.simple.clinic.util.UtcClock
 import org.threeten.bp.Instant
@@ -11,12 +10,6 @@ class MissingPhoneReminderRepository @Inject constructor(
     private val dao: MissingPhoneReminder.RoomDao,
     val utcClock: UtcClock
 ) {
-
-  fun hasShownReminderFor(patientUuid: PatientUuid): Single<Boolean> {
-    return dao.get(patientUuid)
-        .map { reminders -> reminders.isNotEmpty() }
-        .firstOrError()
-  }
 
   fun hasShownReminderForPatient(patientUuid: PatientUuid): Boolean {
     return dao.forPatient(patientUuid) != null

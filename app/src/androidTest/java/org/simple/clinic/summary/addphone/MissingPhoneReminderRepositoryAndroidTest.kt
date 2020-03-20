ad8020bf537270a83da94713ad85923538b8ed50
Vinay Shenoy <vinay@obvious.in>
2020-03-20 10:21:37 +0530

    Add synchronous method to get whether a missing phone reminder was shown

--- app/src/androidTest/java/org/simple/clinic/summary/addphone/MissingPhoneReminderRepositoryAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/summary/addphone/MissingPhoneReminderRepositoryAndroidTest.kt
@@ -57,4 +57,35 @@ class MissingPhoneReminderRepositoryAndroidTest {
     val hasASavedReminder = repository.hasShownReminderFor(patientUuid).blockingGet()
     assertThat(hasASavedReminder).isTrue()
   }
+
+  @Suppress("LocalVariableName", "IllegalIdentifier")
+  @Test
+  fun when_a_missing_phone_reminder_is_present_for_a_patient_then_checking_whether_a_reminder_has_been_shown_should_return_true() {
+    // given
+    val `patient who has been shown reminder` = UUID.fromString("b9eac296-9b12-4601-8462-e07b5338acd3")
+
+    repository.markReminderAsShownFor(`patient who has been shown reminder`).blockingAwait()
+
+    // when
+    val hasASavedReminder = repository.hasShownReminderForPatient(`patient who has been shown reminder`)
+
+    // then
+    assertThat(hasASavedReminder).isTrue()
+  }
+
+  @Suppress("LocalVariableName", "IllegalIdentifier")
+  @Test
+  fun when_a_missing_phone_reminder_is_not_present_for_a_patient_then_checking_whether_a_reminder_has_been_shown_should_return_false() {
+    //given
+    val `patient who has been shown reminder` = UUID.fromString("b9eac296-9b12-4601-8462-e07b5338acd3")
+    val `patient who has not been shown reminder` = UUID.fromString("d0414d6d-9813-4d85-ad61-634f7cddab76")
+
+    repository.markReminderAsShownFor(`patient who has been shown reminder`).blockingAwait()
+
+    // when
+    val hasASavedReminder = repository.hasShownReminderForPatient(`patient who has not been shown reminder`)
+
+    // then
+    assertThat(hasASavedReminder).isFalse()
+  }
 }

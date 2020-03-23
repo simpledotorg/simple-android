1c5ec2b4fc0f46ecf4c81034e9537af5155b720e
Vinay Shenoy <code@vinaysshenoy.com>
2020-03-23 22:49:30 +0530

    Keep entered PIN updated in `PinEntryModel`

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryEvent.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryEvent.kt
@@ -3,3 +3,7 @@ package org.simple.clinic.security.pin
 import org.simple.clinic.widgets.UiEvent
 
 sealed class PinEntryEvent: UiEvent
+
+data class PinTextChanged(val pin: String) : PinEntryEvent() {
+  override val analyticsName = "PIN text changed"
+}

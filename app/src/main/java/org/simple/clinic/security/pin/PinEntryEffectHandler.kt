c3f47d37726abb190fcc07f0a502331e85693b2f
Vinay Shenoy <code@vinaysshenoy.com>
2020-03-23 23:29:58 +0530

    Add `PasswordHasher` as a dependency to `PinEntryEffectHandler`

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryEffectHandler.kt
@@ -1,9 +1,18 @@
 package org.simple.clinic.security.pin
 
 import com.spotify.mobius.rx2.RxMobius
+import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.security.PasswordHasher
 
-class PinEntryEffectHandler {
+class PinEntryEffectHandler @AssistedInject constructor(
+    private val passwordHasher: PasswordHasher
+) {
+
+  @AssistedInject.Factory
+  interface Factory {
+    fun create(): PinEntryEffectHandler
+  }
 
   fun build(): ObservableTransformer<PinEntryEffect, PinEntryEvent> {
     return RxMobius

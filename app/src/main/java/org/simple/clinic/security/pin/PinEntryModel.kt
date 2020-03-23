2966d17bc2ee38af3df5945010158c1e9e982cc4
Vinay Shenoy <vinay@obvious.in>
2020-03-23 23:21:48 +0530

    Keep pin digest updated in `PinEntryModel`

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryModel.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryModel.kt
@@ -5,16 +5,21 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class PinEntryModel(
-    val enteredPin: String
+    val enteredPin: String,
+    val pinDigestToVerify: String?
 ): Parcelable {
 
   companion object {
     fun default(): PinEntryModel {
-      return PinEntryModel(enteredPin = "")
+      return PinEntryModel(enteredPin = "", pinDigestToVerify = null)
     }
   }
 
   fun enteredPinChanged(pin: String): PinEntryModel {
     return copy(enteredPin = pin)
+  }
+
+  fun updatePinDigest(pinDigest: String): PinEntryModel {
+    return copy(pinDigestToVerify = pinDigest)
   }
 }

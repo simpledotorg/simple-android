d2e0e8518854fd2b8c6ad12ad855577ae80e864a
Vinay Shenoy <vinay@obvious.in>
2020-03-23 19:59:06 +0530

    Integrate the Mobius loop into `PinEntryCardView`

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryUiRenderer.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryUiRenderer.kt
@@ -2,7 +2,7 @@ package org.simple.clinic.security.pin
 
 import org.simple.clinic.mobius.ViewRenderer
 
-class PinEntryUiRenderer: ViewRenderer<PinEntryModel> {
+class PinEntryUiRenderer(private val ui: PinEntryUi) : ViewRenderer<PinEntryModel> {
 
   override fun render(model: PinEntryModel) {
     

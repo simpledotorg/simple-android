136e76b34cd679994f426d89eb290187058e2122
Vinay Shenoy <vinay@obvious.in>
2020-03-23 19:38:00 +0530

    Move reporting of analytics events to `PinEntryCardView`

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryCardController.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryCardController.kt
@@ -6,7 +6,6 @@ import io.reactivex.ObservableTransformer
 import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.ofType
 import org.simple.clinic.ReplayUntilScreenIsDestroyed
-import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.security.ComparisonResult.DIFFERENT
 import org.simple.clinic.security.ComparisonResult.SAME
 import org.simple.clinic.security.PasswordHasher
@@ -31,7 +30,6 @@ class PinEntryCardController @Inject constructor(
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
     val replayedEvents = ReplayUntilScreenIsDestroyed(events)
         .compose(autoSubmitPin())
-        .compose(ReportAnalyticsEvents())
         .replay()
 
     return Observable.merge(

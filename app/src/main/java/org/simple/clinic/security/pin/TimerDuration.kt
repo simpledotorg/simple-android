a33d115407e20adae5ff9c9574e2de2bff5fac59
Vinay Shenoy <code@vinaysshenoy.com>
2020-03-24 11:15:58 +0530

    Move rendering of brute force timer countdown to the UI

--- app/src/main/java/org/simple/clinic/security/pin/TimerDuration.kt
+++
@@ -1,7 +0,0 @@
-package org.simple.clinic.security.pin
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class TimerDuration(val minutes: String, val seconds: String): Parcelable

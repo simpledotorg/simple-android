4879ce64ff647882c3b310d0614730e76c66208a
Vinay Shenoy <vinay@obvious.in>
2020-03-24 07:43:02 +0530

    Add `PinEntryCardView.State` as a property to `PinEntryModel`

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryModel.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryModel.kt
@@ -2,16 +2,18 @@ package org.simple.clinic.security.pin
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.security.pin.PinEntryCardView.State.PinEntry
 
 @Parcelize
 data class PinEntryModel(
+    val state: PinEntryCardView.State,
     val enteredPin: String,
     val pinDigestToVerify: String?
 ): Parcelable {
 
   companion object {
     fun default(): PinEntryModel {
-      return PinEntryModel(enteredPin = "", pinDigestToVerify = null)
+      return PinEntryModel(state = PinEntry, enteredPin = "", pinDigestToVerify = null)
     }
   }
 

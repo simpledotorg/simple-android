cc0678a1b14cab509b4a2fb41ee8adda5524205b
Vinay Shenoy <vinay@obvious.in>
2020-03-24 09:26:13 +0530

    Implement displaying of error messages in effect handler

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryEffect.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryEffect.kt
@@ -5,3 +5,9 @@ sealed class PinEntryEffect
 data class ValidateEnteredPin(val enteredPin: String, val pinDigest: String): PinEntryEffect()
 
 object LoadPinEntryProtectedStates: PinEntryEffect()
+
+object HideError: PinEntryEffect()
+
+data class ShowIncorrectPinError(val attemptsMade: Int, val attemptsRemaining: Int): PinEntryEffect()
+
+data class ShowIncorrectPinLimitReachedError(val attemptsMade: Int): PinEntryEffect()

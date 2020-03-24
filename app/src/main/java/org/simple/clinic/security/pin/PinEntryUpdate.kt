eb91d54bb33184b1206c8188fdb6b5722b8894a1
Vinay Shenoy <code@vinaysshenoy.com>
2020-03-24 09:35:02 +0530

    Move displaying of error messages to Mobius

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryUpdate.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryUpdate.kt
@@ -3,17 +3,36 @@ package org.simple.clinic.security.pin
 import com.spotify.mobius.Next
 import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
+import org.simple.clinic.mobius.dispatch
 import org.simple.clinic.mobius.next
+import org.simple.clinic.security.pin.BruteForceProtection.ProtectedState.Allowed
+import org.simple.clinic.security.pin.BruteForceProtection.ProtectedState.Blocked
 
 class PinEntryUpdate(
     private val submitPinAtLength: Int
 ) : Update<PinEntryModel, PinEntryEvent, PinEntryEffect> {
 
   override fun update(model: PinEntryModel, event: PinEntryEvent): Next<PinEntryModel, PinEntryEffect> {
-    return when(event) {
+    return when (event) {
       is PinTextChanged -> next(model.enteredPinChanged(event.pin))
       is PinDigestToVerify -> next(model.updatePinDigest(event.pinDigest))
+      is PinEntryStateChanged -> {
+        val effect = when (val protectedState = event.state) {
+          is Allowed -> generateEffectForAllowingPinEntry(protectedState)
+          is Blocked -> ShowIncorrectPinLimitReachedError(protectedState.attemptsMade)
+        }
+
+        dispatch(effect)
+      }
       else -> noChange()
+    }
+  }
+
+  private fun generateEffectForAllowingPinEntry(protectedState: Allowed): PinEntryEffect {
+    return if (protectedState.attemptsMade == 0) {
+      HideError
+    } else {
+      ShowIncorrectPinError(protectedState.attemptsMade, protectedState.attemptsRemaining)
     }
   }
 }

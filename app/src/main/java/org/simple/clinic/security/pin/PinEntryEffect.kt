80fd3f9c106d065ca7001f56a5896048181b994b
Vinay Shenoy <vinay@obvious.in>
2020-03-24 08:48:23 +0530

    Subscribe to PIN entry protected state changes in the Mobius loop

--- app/src/main/java/org/simple/clinic/security/pin/PinEntryEffect.kt
+++ app/src/main/java/org/simple/clinic/security/pin/PinEntryEffect.kt
@@ -3,3 +3,5 @@ package org.simple.clinic.security.pin
 sealed class PinEntryEffect
 
 data class ValidateEnteredPin(val enteredPin: String, val pinDigest: String): PinEntryEffect()
+
+object LoadPinEntryProtectedStates: PinEntryEffect()

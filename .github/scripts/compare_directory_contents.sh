d3a6b3d955e627dcb38b9b1adc440684d27ca411
Vinay Shenoy <vinay@obvious.in>
2020-03-25 16:22:44 +0530

    Add CI check to verify Room schemas are not changed

---
+++ .github/scripts/compare_directory_contents.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sourceDirectory=${1}
+targetDirectory=${2}
+
+changedFile=
+
+for file in $(ls $sourceDirectory)
+do
+	sourceFile="$sourceDirectory/$file"
+	sourceChecksum=($(shasum ${sourceFile}))
+
+	targetFile="$targetDirectory/$file"
+	targetChecksum=($(shasum ${targetFile}))
+
+	if [[ "$targetChecksum" != "$sourceChecksum" ]]; then
+		changedFile=$file
+		break
+	fi
+done
+
+if [[ -z $changedFile ]]; then
+	exit 0
+else
+	>&2 echo "$changedFile was changed!"
+	exit 1
+fi

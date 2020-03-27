740e79658bda2123df4f12ad611f7611fc908e4e
Vinay Shenoy <vinay@obvious.in>
2020-03-27 09:58:35 +0530

    Add api call for activating the user

--- app/src/main/java/org/simple/clinic/login/LoginApi.kt
+++ app/src/main/java/org/simple/clinic/login/LoginApi.kt
@@ -4,6 +4,9 @@ import io.reactivex.Completable
 import io.reactivex.Single
 import org.simple.clinic.forgotpin.ForgotPinResponse
 import org.simple.clinic.forgotpin.ResetPinRequest
+import org.simple.clinic.login.activateuser.ActivateUserRequest
+import org.simple.clinic.login.activateuser.ActivateUserResponse
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -19,4 +22,7 @@ interface LoginApi {
 
   @POST("v3/users/me/reset_password")
   fun resetPin(@Body request: ResetPinRequest): Single<ForgotPinResponse>
+
+  @POST("v4/users/activate")
+  fun activate(@Body request: ActivateUserRequest): Call<ActivateUserResponse>
 }

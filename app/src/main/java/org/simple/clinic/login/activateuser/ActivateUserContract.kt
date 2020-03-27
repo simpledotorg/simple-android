4102815ef347dba9f2053f64145fd8b4e3149159
Vinay Shenoy <code@vinaysshenoy.com>
2020-03-27 09:58:35 +0530

    Add api call for activating the user

---
+++ app/src/main/java/org/simple/clinic/login/activateuser/ActivateUserContract.kt
@@ -0,0 +1,33 @@
+package org.simple.clinic.login.activateuser
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import org.simple.clinic.user.LoggedInUserPayload
+import java.util.UUID
+
+@JsonClass(generateAdapter = true)
+data class ActivateUserRequest(
+
+    @Json(name = "user")
+    val body: Body
+) {
+
+  @JsonClass(generateAdapter = true)
+  data class Body(
+
+      @Json(name = "id")
+      val id: UUID,
+
+      @Json(name = "password")
+      val pin: String
+  )
+}
+
+@JsonClass(generateAdapter = true)
+data class ActivateUserResponse(
+
+    @Json(name = "user")
+    val user: LoggedInUserPayload
+)
+
+

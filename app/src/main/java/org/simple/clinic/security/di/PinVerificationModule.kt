d581298d4ead237ead61d7c20f7686860e1b5798
Vinay Shenoy <vinay@obvious.in>
2020-03-30 16:02:12 +0530

    Provide `PinVerificationMethod` dependency to `PinEntryEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/security/di/PinVerificationModule.kt
@@ -0,0 +1,26 @@
+package org.simple.clinic.security.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import org.simple.clinic.security.pin.verification.LocalUserPinVerificationMethod
+import org.simple.clinic.security.pin.verification.OngoingLoginEntryPinVerificationMethod
+import org.simple.clinic.security.pin.verification.PinVerificationMethod
+
+@Module
+abstract class PinVerificationModule {
+
+  @Binds
+  @IntoMap
+  @VerificationMethodKey(Method.Local)
+  abstract fun bindsLocalPinVerificationMethod(
+      pinVerificationMethod: LocalUserPinVerificationMethod
+  ): PinVerificationMethod
+
+  @Binds
+  @IntoMap
+  @VerificationMethodKey(Method.OngoingEntry)
+  abstract fun bindsOngoingEntryPinVerificationMethod(
+      pinVerificationMethod: OngoingLoginEntryPinVerificationMethod
+  ): PinVerificationMethod
+}
---
+++ app/src/main/java/org/simple/clinic/security/di/VerificationMethodKey.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.security.di
+
+import dagger.MapKey
+
+@MapKey
+annotation class VerificationMethodKey(val value: Method)
+
+enum class Method {
+  Local,
+  OngoingEntry
+}

80417d752fbaa4409ef4ee48b4b08cb617811053
Vinay Shenoy <vinay@obvious.in>
2020-03-31 09:39:47 +0530

    Change result of server login method to contain the entire user data

--- app/src/main/java/org/simple/clinic/security/pin/verification/LoginPinServerVerificationMethod.kt
+++ app/src/main/java/org/simple/clinic/security/pin/verification/LoginPinServerVerificationMethod.kt
@@ -5,7 +5,11 @@ import org.simple.clinic.login.activateuser.ActivateUser.Result
 import org.simple.clinic.security.pin.verification.PinVerificationMethod.VerificationResult
 import org.simple.clinic.security.pin.verification.PinVerificationMethod.VerificationResult.Correct
 import org.simple.clinic.security.pin.verification.PinVerificationMethod.VerificationResult.Incorrect
+import org.simple.clinic.user.LoggedInUserPayload
 import org.simple.clinic.user.OngoingLoginEntryRepository
+import org.simple.clinic.user.UserStatus
+import org.threeten.bp.Instant
+import java.util.UUID
 import javax.inject.Inject
 
 class LoginPinServerVerificationMethod @Inject constructor(
@@ -17,11 +21,43 @@ class LoginPinServerVerificationMethod @Inject constructor(
     val entry = repository.entryImmediate()
 
     return when (val result = activateUser.activate(entry.uuid, pin)) {
-      is Result.Success -> Correct(result.userPayload)
+      is Result.Success -> Correct(UserData.create(pin, result.userPayload))
       Result.IncorrectPin -> Incorrect(pin)
       Result.NetworkError -> VerificationResult.NetworkError
       is Result.ServerError -> VerificationResult.ServerError
       is Result.OtherError -> VerificationResult.OtherError(result.cause)
+    }
+  }
+
+  data class UserData(
+      val pin: String,
+      val uuid: UUID,
+      val fullName: String,
+      val phoneNumber: String,
+      val pinDigest: String,
+      val registrationFacilityUuid: UUID,
+      val status: UserStatus,
+      val createdAt: Instant,
+      val updatedAt: Instant
+  ) {
+
+    companion object {
+      fun create(
+          pin: String,
+          payload: LoggedInUserPayload
+      ): UserData {
+        return UserData(
+            pin = pin,
+            uuid = payload.uuid,
+            fullName = payload.fullName,
+            phoneNumber = payload.phoneNumber,
+            pinDigest = payload.pinDigest,
+            registrationFacilityUuid = payload.registrationFacilityId,
+            status = payload.status,
+            createdAt = payload.createdAt,
+            updatedAt = payload.updatedAt
+        )
+      }
     }
   }
 }

742f27462dd1e87f8bd563895db33e813f6ea2bd
Vinay Shenoy <vinay@obvious.in>
2020-03-31 08:53:59 +0530

    Add flaky tests as a control to verify flakiness tool

--- app/src/androidTest/java/org/simple/clinic/CanaryInstrumentationTest.kt
+++ app/src/androidTest/java/org/simple/clinic/CanaryInstrumentationTest.kt
@@ -1,11 +1,13 @@
 package org.simple.clinic
 
-import androidx.test.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry
 import com.google.common.truth.Truth.assertThat
+import com.vinaysshenoy.quarantine.Quarantine
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.simple.clinic.util.Rules
+import kotlin.random.Random
 
 
 class CanaryInstrumentationTest {
@@ -15,8 +17,22 @@ class CanaryInstrumentationTest {
 
   @Test
   fun testEnvironmentWorks() {
-    val context = InstrumentationRegistry.getTargetContext()
+    val context = InstrumentationRegistry.getInstrumentation().targetContext
 
     assertThat(context).isNotNull()
+  }
+
+  @Test
+  fun test_that_30p_flaky_test_is_captured() {
+    if (Quarantine.isEnabled) {
+      assertThat(Random.nextFloat()).isGreaterThan(0.3F)
+    }
+  }
+
+  @Test
+  fun test_that_70p_flaky_test_is_captured() {
+    if (Quarantine.isEnabled) {
+      assertThat(Random.nextFloat()).isGreaterThan(0.7F)
+    }
   }
 }

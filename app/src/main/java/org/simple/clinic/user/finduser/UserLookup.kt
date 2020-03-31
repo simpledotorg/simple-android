f01df1c154ca5c7e83fe11f80798464fdbe521c9
Vinay Shenoy <vinay@obvious.in>
2020-03-31 16:38:22 +0530

    Add method to `UserLookup` to use the new find user call

--- app/src/main/java/org/simple/clinic/user/finduser/UserLookup.kt
+++ app/src/main/java/org/simple/clinic/user/finduser/UserLookup.kt
@@ -1,11 +1,15 @@
 package org.simple.clinic.user.finduser
 
 import io.reactivex.Single
+import org.simple.clinic.registration.FindUserRequest
 import org.simple.clinic.registration.RegistrationApi
+import org.simple.clinic.user.finduser.FindUserResult.Found
 import org.simple.clinic.user.finduser.FindUserResult.Found_Old
 import org.simple.clinic.user.finduser.FindUserResult.NetworkError
 import org.simple.clinic.user.finduser.FindUserResult.NotFound
 import org.simple.clinic.user.finduser.FindUserResult.UnexpectedError
+import org.simple.clinic.util.ErrorResolver
+import org.simple.clinic.util.ResolvedError.NetworkRelated
 import retrofit2.HttpException
 import timber.log.Timber
 import java.io.IOException
@@ -31,6 +35,33 @@ class UserLookup @Inject constructor(
         Timber.e(error)
         UnexpectedError
       }
+    }
+  }
+
+  fun find(phoneNumber: String): FindUserResult {
+    Timber.i("Finding user with phone number")
+    return try {
+      makeFindUserApiCall(phoneNumber)
+    } catch (e: Throwable) {
+      when (ErrorResolver.resolve(e)) {
+        is NetworkRelated -> NetworkError
+        else -> UnexpectedError
+      }
+    }
+  }
+
+  private fun makeFindUserApiCall(phoneNumber: String): FindUserResult {
+    val request = FindUserRequest(phoneNumber)
+    val response = registrationApi.findUserByPhoneNumber(request).execute()
+
+    return when (val code = response.code()) {
+      200 -> {
+        val responseData = response.body()!!.body
+
+        Found(responseData.userUuid, responseData.status)
+      }
+      404 -> NotFound
+      else -> throw RuntimeException("Not an expected response code: $code")
     }
   }
 }

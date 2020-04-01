e2cf0975ea8c50fe824258510fd4d3044a496c01
Vinay Shenoy <vinay@obvious.in>
2020-04-01 16:02:35 +0530

    Remove unused code from `UserLookup`

--- app/src/main/java/org/simple/clinic/registration/RegistrationApi.kt
+++ app/src/main/java/org/simple/clinic/registration/RegistrationApi.kt
@@ -1,19 +1,11 @@
 package org.simple.clinic.registration
 
 import io.reactivex.Single
-import org.simple.clinic.user.LoggedInUserPayload
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface RegistrationApi {
-
-  @GET("v3/users/find")
-  fun findUser(
-      @Query("phone_number") phoneNumber: String
-  ): Single<LoggedInUserPayload>
 
   @POST("v3/users/register")
   fun createUser(
--- app/src/main/java/org/simple/clinic/user/finduser/FindUserResult.kt
+++ app/src/main/java/org/simple/clinic/user/finduser/FindUserResult.kt
@@ -1,15 +1,11 @@
 package org.simple.clinic.user.finduser
 
-import org.simple.clinic.user.LoggedInUserPayload
 import org.simple.clinic.user.UserStatus
 import java.util.UUID
 
 sealed class FindUserResult {
 
-  // TODO: Remove this once we move to the new user refresh call
-  data class Found_Old(val user: LoggedInUserPayload) : FindUserResult()
-
-  data class Found(val uuid: UUID, val status: UserStatus): FindUserResult()
+  data class Found(val uuid: UUID, val status: UserStatus) : FindUserResult()
 
   object NotFound : FindUserResult()
 

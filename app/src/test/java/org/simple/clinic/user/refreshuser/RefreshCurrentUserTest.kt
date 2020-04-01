fafade3d1f06ab3c9067ea85bc40ea9c1305fd2a
Vinay Shenoy <code@vinaysshenoy.com>
2020-04-01 15:41:47 +0530

    Use new api in `RefreshCurrentUser`

--- app/src/test/java/org/simple/clinic/user/refreshuser/RefreshCurrentUserTest.kt
+++ app/src/test/java/org/simple/clinic/user/refreshuser/RefreshCurrentUserTest.kt
@@ -4,17 +4,16 @@ import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
-import io.reactivex.Single
 import org.junit.Rule
 import org.junit.Test
+import org.simple.clinic.FakeCall
 import org.simple.clinic.TestData
+import org.simple.clinic.login.CurrentUserResponse
 import org.simple.clinic.login.LoginApi
 import org.simple.clinic.user.User
 import org.simple.clinic.user.User.LoggedInStatus.LOGGED_IN
 import org.simple.clinic.user.User.LoggedInStatus.RESET_PIN_REQUESTED
 import org.simple.clinic.user.UserStatus.ApprovedForSyncing
-import org.simple.clinic.user.finduser.FindUserResult
-import org.simple.clinic.user.finduser.FindUserResult.Found_Old
 import org.simple.clinic.user.finduser.UserLookup
 import org.simple.clinic.util.RxErrorsRule
 import org.simple.clinic.util.toUser
@@ -29,16 +28,15 @@ class RefreshCurrentUserTest {
   private val phone = "1234567890"
 
   private val userDao = mock<User.RoomDao>()
-  private val findUserWithPhoneNumber = mock<UserLookup>()
   private val loginApi = mock<LoginApi>()
 
-  private val refreshCurrentUser = RefreshCurrentUser(userDao, findUserWithPhoneNumber, loginApi)
+  private val refreshCurrentUser = RefreshCurrentUser(userDao, loginApi)
 
   @Test
   fun `when refreshing the user succeeds, the updated user details must be saved in the database`() {
     // given
     val userPayload = TestData.loggedInUserPayload(uuid = userUuid, phone = phone, status = ApprovedForSyncing)
-    whenever(findUserWithPhoneNumber.find_old(phone)) doReturn Single.just<FindUserResult>(Found_Old(userPayload))
+    whenever(loginApi.self()) doReturn FakeCall.success(CurrentUserResponse(userPayload))
 
     val savedUser = userPayload.toUser(LOGGED_IN)
     whenever(userDao.userImmediate()) doReturn savedUser
@@ -53,7 +51,7 @@ class RefreshCurrentUserTest {
   @Test
   fun `when the locally saved user has requested a PIN reset and the refreshed user has been approved for syncing, the user must be updated as logged in`() {
     val userPayload = TestData.loggedInUserPayload(uuid = userUuid, phone = phone, status = ApprovedForSyncing)
-    whenever(findUserWithPhoneNumber.find_old(phone)) doReturn Single.just<FindUserResult>(Found_Old(userPayload))
+    whenever(loginApi.self()) doReturn FakeCall.success(CurrentUserResponse(userPayload))
 
     val savedUser = userPayload.toUser(RESET_PIN_REQUESTED)
     whenever(userDao.userImmediate()) doReturn savedUser

32df5e75b7b078d96d725c641c853089a7e90ec2
Vinay Shenoy <vinay@obvious.in>
2020-04-01 14:51:58 +0530

    Remove API to request login OTP
    This has been replaced with the `v4/users/activate` endpoint.

--- app/src/main/java/org/simple/clinic/login/LoginApi.kt
+++ app/src/main/java/org/simple/clinic/login/LoginApi.kt
@@ -1,6 +1,5 @@
 package org.simple.clinic.login
 
-import io.reactivex.Completable
 import io.reactivex.Single
 import org.simple.clinic.forgotpin.ForgotPinResponse
 import org.simple.clinic.forgotpin.ResetPinRequest
@@ -9,16 +8,11 @@ import org.simple.clinic.login.activateuser.ActivateUserResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.Path
-import java.util.UUID
 
 interface LoginApi {
 
   @POST("v3/login")
   fun login(@Body body: LoginRequest): Single<LoginResponse>
-
-  @POST("v3/users/{userId}/request_otp")
-  fun requestLoginOtp(@Path("userId") userId: UUID): Completable
 
   @POST("v3/users/me/reset_password")
   fun resetPin(@Body request: ResetPinRequest): Single<ForgotPinResponse>

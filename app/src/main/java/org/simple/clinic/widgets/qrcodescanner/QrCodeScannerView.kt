374f094d0b979ebaf6a2099623e14aac16ed0520
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-04-02 14:00:31 +0530

    Implement camera preview in `QrCodeScannerView`

--- app/src/main/java/org/simple/clinic/widgets/qrcodescanner/QrCodeScannerView.kt
+++ app/src/main/java/org/simple/clinic/widgets/qrcodescanner/QrCodeScannerView.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.view_qrcode_scanner.view.*
 import org.simple.clinic.R
 
 class QrCodeScannerView @JvmOverloads
@@ -15,6 +20,8 @@ constructor(
 
   private val qrCodeScannerLifecycle = QrCodeScannerLifecycle()
 
+  private val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
   init {
     View.inflate(context, R.layout.view_qrcode_scanner, this)
   }
@@ -22,8 +29,29 @@ constructor(
   override fun onFinishInflate() {
     super.onFinishInflate()
     if (isInEditMode) return
+  }
 
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
     qrCodeScannerLifecycle.bindCamera()
+
+    cameraProviderFuture.addListener(Runnable {
+      val cameraProvider = cameraProviderFuture.get()
+      startCamera(cameraProvider)
+    }, ContextCompat.getMainExecutor(context))
+  }
+
+  private fun startCamera(cameraProvider: ProcessCameraProvider) {
+    val cameraSelector = CameraSelector.Builder()
+        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+        .build()
+
+    val preview = Preview.Builder().build()
+
+    cameraProvider.unbindAll()
+
+    val camera = cameraProvider.bindToLifecycle(qrCodeScannerLifecycle, cameraSelector, preview)
+    preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.cameraInfo))
   }
 
   override fun onDetachedFromWindow() {

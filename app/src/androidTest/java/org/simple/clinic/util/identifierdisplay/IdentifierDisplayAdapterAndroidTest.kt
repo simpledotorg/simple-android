a2faa37f308f0f09d2732796e3b2ed158373007e
Vinay Shenoy <code@vinaysshenoy.com>
2020-04-03 08:51:06 +0530

    Add regression tests for formatting Identifiers
    This is in preparation for future commits where we will remove the
    `IdentifierDisplayAdapter` class in favour of methods on the
    `Identifier` type. This adds tests to capture the current behaviour
    so we can remove the current set of tests which will start breaking
    once we start refactoring.

--- app/src/androidTest/java/org/simple/clinic/util/identifierdisplay/IdentifierDisplayAdapterAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/util/identifierdisplay/IdentifierDisplayAdapterAndroidTest.kt
@@ -1,12 +1,15 @@
 package org.simple.clinic.util.identifierdisplay
 
-import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth.assertWithMessage
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.simple.clinic.TestClinicApp
+import org.simple.clinic.patient.businessid.Identifier
 import org.simple.clinic.patient.businessid.Identifier.IdentifierType
+import org.simple.clinic.patient.businessid.Identifier.IdentifierType.BangladeshNationalId
+import org.simple.clinic.patient.businessid.Identifier.IdentifierType.BpPassport
 import org.simple.clinic.patient.businessid.Identifier.IdentifierType.Unknown
 import org.simple.clinic.util.Rules
 import javax.inject.Inject
@@ -26,19 +29,46 @@ class IdentifierDisplayAdapterAndroidTest {
   }
 
   @Test
-  fun all_known_types_of_adapters_must_be_registered_in_the_adapter() {
-    val registeredIdentifierTypes = identifierDisplayAdapter
-        .formatters
-        .keys
-
-    val expected = IdentifierType
+  fun all_types_of_identifiers_should_have_a_display_value() {
+    // given
+    val testData = IdentifierType
         .values()
-        .map { it::class.java }
-        .plus(Unknown::class.java)
-        .toSet()
+        .map(::generateIdentifierTestData)
 
-    // If this fails, you probably forgot to register an adapter for a newly added IdentifierType in
-    // IdentifierDisplayAdapterModule
-    assertThat(registeredIdentifierTypes).isEqualTo(expected)
+    // then
+    testData.forEach { (identifier, expectedValue) ->
+
+      assertWithMessage("formatting Identifier of type [${identifier.type}]")
+          .that(identifierDisplayAdapter.valueAsText(identifier))
+          .isEqualTo(expectedValue)
+    }
   }
+
+  private fun generateIdentifierTestData(type: IdentifierType): IdentifierTestData {
+    return when (type) {
+      BpPassport -> {
+        val bpPassportId = "4b1de973-48e6-4f27-a384-65174748f0b1"
+        val bpPassport = Identifier(value = bpPassportId, type = BpPassport)
+
+        IdentifierTestData(identifier = bpPassport, expectedDisplayValue = "419\u00A07348")
+      }
+      BangladeshNationalId -> {
+        val bangladeshNationalIdValue = "123456783456"
+        val bangladeshNationalId = Identifier(value = bangladeshNationalIdValue, type = BangladeshNationalId)
+
+        IdentifierTestData(identifier = bangladeshNationalId, expectedDisplayValue = bangladeshNationalIdValue)
+      }
+      is Unknown -> {
+        val someOtherIdValue = "asdf-2345-rftg"
+        val unknownId = Identifier(value = someOtherIdValue, type = Unknown("some-other-id"))
+
+        IdentifierTestData(identifier = unknownId, expectedDisplayValue = someOtherIdValue)
+      }
+    }
+  }
+
+  private data class IdentifierTestData(
+      val identifier: Identifier,
+      val expectedDisplayValue: String
+  )
 }

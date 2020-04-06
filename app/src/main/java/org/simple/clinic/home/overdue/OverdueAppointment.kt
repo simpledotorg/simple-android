c08e154547dc2a2b1fe792a7fc3fd994cc097b19
Vinay Shenoy <vinay@obvious.in>
2020-04-06 17:14:41 +0530

    Change `AppointmentRepository` to use the new overdue appointment view

--- app/src/main/java/org/simple/clinic/home/overdue/OverdueAppointment.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/OverdueAppointment.kt
@@ -101,72 +101,18 @@ data class OverdueAppointment(
   interface RoomDao {
 
     @Query("""
-          SELECT P.fullName, P.gender, P.dateOfBirth, P.age_value, P.age_updatedAt,
-
-          A.uuid appt_uuid, A.patientUuid appt_patientUuid, A.facilityUuid appt_facilityUuid, A.scheduledDate appt_scheduledDate, A.status appt_status,
-          A.cancelReason appt_cancelReason, A.remindOn appt_remindOn, A.agreedToVisit appt_agreedToVisit, A.appointmentType appt_appointmentType,
-          A.syncStatus appt_syncStatus, A.createdAt appt_createdAt, A.updatedAt appt_updatedAt, A.creationFacilityUuid appt_creationFacilityUuid,
-
-          PPN.uuid phone_uuid, PPN.patientUuid phone_patientUuid, PPN.number phone_number, PPN.phoneType phone_phoneType, PPN.active phone_active,
-          PPN.createdAt phone_createdAt, PPN.updatedAt phone_updatedAt,
-
-          MH.hasDiabetes diagnosedWithDiabetes, MH.diagnosedWithHypertension diagnosedWithHypertension,
-
-          (
-            CASE
-                WHEN BP.uuid IS NULL THEN BloodSugar.recordedAt
-                WHEN BloodSugar.uuid IS NULL THEN BP.recordedAt
-                ELSE MAX(BP.recordedAt, BloodSugar.recordedAt)
-            END
-          ) AS patientLastSeen,
-
-          (
-            CASE
-              WHEN BloodSugar.reading_type = "fasting" AND CAST (BloodSugar.reading_value AS REAL) >= 200 THEN 1
-              WHEN BloodSugar.reading_type = "random" AND CAST (BloodSugar.reading_value AS REAL) >= 300 THEN 1
-              WHEN BloodSugar.reading_type = "post_prandial" AND CAST (BloodSugar.reading_value AS REAL) >= 300 THEN 1
-              WHEN BloodSugar.reading_type = "hba1c" AND CAST (BloodSugar.reading_value AS REAL) >= 9 THEN 1
-              WHEN BP.systolic >= 180 OR BP.diastolic >= 110 THEN 1
-              WHEN (MH.hasHadHeartAttack = :yesAnswer OR MH.hasHadStroke = :yesAnswer) AND (BP.systolic >= 140 OR BP.diastolic >= 110) 
-                THEN 1 
-              ELSE 0
-            END
-          ) AS isAtHighRisk
-
-          FROM Patient P
-
-          INNER JOIN Appointment A ON A.patientUuid = P.uuid
-          LEFT JOIN PatientPhoneNumber PPN ON (PPN.patientUuid = P.uuid AND PPN.deletedAt IS NULL)
-          LEFT JOIN MedicalHistory MH ON MH.patientUuid = P.uuid
-
-          LEFT JOIN (
-            SELECT * FROM BloodPressureMeasurement WHERE deletedAt IS NULL GROUP BY patientUuid HAVING max(recordedAt)
-          ) BP ON BP.patientUuid = P.uuid
-
-          LEFT JOIN (
-            SELECT * FROM BloodSugarMeasurements WHERE deletedAt IS NULL GROUP BY patientUuid HAVING max(recordedAt)
-          ) BloodSugar ON BloodSugar.patientUuid = P.uuid
-
-          WHERE 
-            P.deletedAt IS NULL
-            AND A.deletedAt IS NULL
-            AND A.facilityUuid = :facilityUuid
-            AND A.status = :scheduledStatus
-            AND A.scheduledDate < :scheduledBefore
-            AND A.scheduledDate > :scheduledAfter
-            AND PPN.number IS NOT NULL
-            AND (A.remindOn < :scheduledBefore OR A.remindOn IS NULL)
-            AND (BP.recordedAt IS NOT NULL OR BloodSugar.recordedAt IS NOT NULL)
-
-          GROUP BY P.uuid
-          ORDER BY isAtHighRisk DESC, A.scheduledDate DESC, A.updatedAt ASC
-          """)
-    fun appointmentsForFacility(
+      SELECT * FROM OverdueAppointment
+      WHERE 
+        appt_facilityUuid = :facilityUuid 
+        AND (appt_scheduledDate < :scheduledBefore AND appt_scheduledDate > :scheduledAfter)
+        AND (appt_remindOn < :scheduledBefore OR appt_remindOn IS NULL)
+        GROUP BY appt_patientUuid
+        ORDER BY isAtHighRisk DESC, appt_scheduledDate DESC, appt_updatedAt ASC
+    """)
+    fun overdueAtFacility(
         facilityUuid: UUID,
-        scheduledStatus: Appointment.Status,
         scheduledBefore: LocalDate,
-        scheduledAfter: LocalDate,
-        yesAnswer: Answer = Answer.Yes
-    ): Flowable<List<OverdueAppointment>>
+        scheduledAfter: LocalDate
+    ): Observable<List<OverdueAppointment>>
   }
 }

e6a07459ea5e29554bda4986e3930733658a3a43
Vinay Shenoy <vinay@obvious.in>
2020-04-07 15:59:40 +0530

    Load the patient profile in the patient contact sheet

--- app/src/main/java/org/simple/clinic/patientcontact/PatientContactModel.kt
+++ app/src/main/java/org/simple/clinic/patientcontact/PatientContactModel.kt
@@ -1,8 +1,17 @@
 package org.simple.clinic.patientcontact
 
-class PatientContactModel {
+import org.simple.clinic.patient.PatientProfile
+
+data class PatientContactModel(
+    private val patientProfile: PatientProfile? = null
+) {
+
 
   companion object {
     fun create(): PatientContactModel = PatientContactModel()
+  }
+
+  fun patientProfileLoaded(patientProfile: PatientProfile): PatientContactModel {
+    return copy(patientProfile = patientProfile)
   }
 }

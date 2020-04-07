a740cf14eac20b1aabf0c54fcc615ec6e83c7d20
Vinay Shenoy <vinay@obvious.in>
2020-04-07 16:49:55 +0530

    Add effect to load the patient profile

--- app/src/main/java/org/simple/clinic/patientcontact/PatientContactEffect.kt
+++ app/src/main/java/org/simple/clinic/patientcontact/PatientContactEffect.kt
@@ -1,3 +1,7 @@
 package org.simple.clinic.patientcontact
 
+import java.util.UUID
+
 sealed class PatientContactEffect
+
+data class LoadPatientProfile(val patientUuid: UUID): PatientContactEffect()

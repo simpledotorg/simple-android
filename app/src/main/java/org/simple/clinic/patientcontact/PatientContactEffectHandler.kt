a52a5926a24d7b89c9f96c2e5cd0a7f6c3287eb3
Vinay Shenoy <vinay@obvious.in>
2020-04-07 16:49:55 +0530

    Add effect to load the patient profile

--- app/src/main/java/org/simple/clinic/patientcontact/PatientContactEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/patientcontact/PatientContactEffectHandler.kt
@@ -2,19 +2,33 @@ package org.simple.clinic.patientcontact
 
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
+import org.simple.clinic.patient.PatientRepository
+import org.simple.clinic.util.filterAndUnwrapJust
+import org.simple.clinic.util.scheduler.SchedulersProvider
 
-class PatientContactEffectHandler {
+class PatientContactEffectHandler(
+    private val patientRepository: PatientRepository,
+    private val schedulers: SchedulersProvider
+) {
 
-  companion object {
-    fun create(): ObservableTransformer<PatientContactEffect, PatientContactEvent> {
-      return PatientContactEffectHandler()
-          .buildEffectHandler()
-    }
-  }
-
-  private fun buildEffectHandler(): ObservableTransformer<PatientContactEffect, PatientContactEvent> {
+  fun build(): ObservableTransformer<PatientContactEffect, PatientContactEvent> {
     return RxMobius
         .subtypeEffectHandler<PatientContactEffect, PatientContactEvent>()
+        .addTransformer(LoadPatient::class.java, loadPatientProfile(schedulers.io()))
         .build()
+  }
+
+  private fun loadPatientProfile(
+      scheduler: Scheduler
+  ): ObservableTransformer<LoadPatient, PatientContactEvent> {
+    return ObservableTransformer { effects ->
+      effects
+          .observeOn(scheduler)
+          .map { patientRepository.patientProfileImmediate(it.patientUuid) }
+          .filterAndUnwrapJust()
+          .map { it.withoutDeletedPhoneNumbers().withoutDeletedBusinessIds() }
+          .map(::PatientProfileLoaded)
+    }
   }
 }

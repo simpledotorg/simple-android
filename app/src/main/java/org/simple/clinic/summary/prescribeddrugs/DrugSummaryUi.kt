08971adbe5f8237da416328594302c0ac17fd2fb
Sanchita Agarwal <sanchita@obvious.in>
2020-04-08 23:18:29 +0530

    Show alert for facility change on add medicine

--- app/src/main/java/org/simple/clinic/summary/prescribeddrugs/DrugSummaryUi.kt
+++ app/src/main/java/org/simple/clinic/summary/prescribeddrugs/DrugSummaryUi.kt
@@ -1,9 +1,10 @@
 package org.simple.clinic.summary.prescribeddrugs
 
 import org.simple.clinic.drugs.PrescribedDrug
+import org.simple.clinic.facility.Facility
 import java.util.UUID
 
 interface DrugSummaryUi {
   fun populatePrescribedDrugs(prescribedDrugs: List<PrescribedDrug>)
-  fun showUpdatePrescribedDrugsScreen(patientUuid: UUID)
+  fun showUpdatePrescribedDrugsScreen(patientUuid: UUID, currentFacility: Facility)
 }

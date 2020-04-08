cac5b19c16a7b977d51e1ea6d3f55348752dd93e
Sanchita Agarwal <sanchita@obvious.in>
2020-04-08 19:59:05 +0530

    Add `FetchCurrentFacility` effect in `BloodSugarSummaryViewInit`

--- app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewModel.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewModel.kt
@@ -3,18 +3,20 @@ package org.simple.clinic.summary.bloodsugar
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.bloodsugar.BloodSugarMeasurement
+import org.simple.clinic.facility.Facility
 import java.util.UUID
 
 @Parcelize
 data class BloodSugarSummaryViewModel(
     val patientUuid: UUID,
     val measurements: List<BloodSugarMeasurement>?,
-    val totalRecordedBloodSugarCount: Int?
+    val totalRecordedBloodSugarCount: Int?,
+    val currentFacility: Facility?
 ) : Parcelable {
 
   companion object {
     fun create(patientUuid: UUID): BloodSugarSummaryViewModel {
-      return BloodSugarSummaryViewModel(patientUuid, null, null)
+      return BloodSugarSummaryViewModel(patientUuid, null, null, null)
     }
   }
 

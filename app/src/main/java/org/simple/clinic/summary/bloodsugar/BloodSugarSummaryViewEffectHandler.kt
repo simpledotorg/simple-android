dc8eaae72e4118ab68d6de57e16d15f0a9bd60d9
Sanchita Agarwal <sanchita@obvious.in>
2020-04-08 21:05:00 +0530

    Handle `FetchCurrentFacility` effect in `BloodSugarSummaryViewEffectHandler`

--- app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodsugar/BloodSugarSummaryViewEffectHandler.kt
@@ -6,13 +6,17 @@ import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
 import io.reactivex.Scheduler
 import org.simple.clinic.bloodsugar.BloodSugarRepository
+import org.simple.clinic.facility.FacilityRepository
+import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class BloodSugarSummaryViewEffectHandler @AssistedInject constructor(
     private val bloodSugarRepository: BloodSugarRepository,
     private val schedulersProvider: SchedulersProvider,
     @Assisted private val uiActions: UiActions,
-    private val config: BloodSugarSummaryConfig
+    private val config: BloodSugarSummaryConfig,
+    private val userSession: UserSession,
+    private val facilityRepository: FacilityRepository
 ) {
 
   @AssistedInject.Factory
@@ -25,11 +29,23 @@ class BloodSugarSummaryViewEffectHandler @AssistedInject constructor(
         .subtypeEffectHandler<BloodSugarSummaryViewEffect, BloodSugarSummaryViewEvent>()
         .addTransformer(FetchBloodSugarSummary::class.java, fetchBloodSugarMeasurements(bloodSugarRepository, schedulersProvider.ui()))
         .addTransformer(FetchBloodSugarCount::class.java, fetchBloodSugarMeasurementsCount(schedulersProvider.io()))
+        .addTransformer(FetchCurrentFacility::class.java, fetchCurrentFacility(schedulersProvider.io()))
         .addAction(OpenBloodSugarTypeSelector::class.java, uiActions::showBloodSugarTypeSelector, schedulersProvider.ui())
-        .addAction(FetchCurrentFacility::class.java) { /*no-op*/ }
         .addConsumer(ShowBloodSugarHistoryScreen::class.java, { uiActions.showBloodSugarHistoryScreen(it.patientUuid) }, schedulersProvider.ui())
         .addConsumer(OpenBloodSugarUpdateSheet::class.java, { uiActions.openBloodSugarUpdateSheet(it.measurement.uuid, it.measurement.reading.type) }, schedulersProvider.ui())
         .build()
+  }
+
+  private fun fetchCurrentFacility(io: Scheduler): ObservableTransformer<FetchCurrentFacility, BloodSugarSummaryViewEvent> {
+    return ObservableTransformer { effects ->
+      effects
+          .observeOn(io)
+          .map {
+            val user = userSession.loggedInUserImmediate()!!
+            facilityRepository.currentFacilityImmediate(user)
+          }
+          .map(::CurrentFacilityFetched)
+    }
   }
 
   private fun fetchBloodSugarMeasurements(

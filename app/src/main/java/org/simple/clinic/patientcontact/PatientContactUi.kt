10ef8fbf9e06e19c4b5804d4591e601cb573e3fe
Vinay Shenoy <vinay@obvious.in>
2020-04-08 09:47:40 +0530

    Render the patient details on the patient contact UI

--- app/src/main/java/org/simple/clinic/patientcontact/PatientContactUi.kt
+++ app/src/main/java/org/simple/clinic/patientcontact/PatientContactUi.kt
@@ -1,4 +1,7 @@
 package org.simple.clinic.patientcontact
 
+import org.simple.clinic.patient.Gender
+
 interface PatientContactUi {
+  fun renderPatientDetails(name: String, gender: Gender, age: Int, phoneNumber: String)
 }

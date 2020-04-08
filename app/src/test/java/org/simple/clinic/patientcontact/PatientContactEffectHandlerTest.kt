245bd21fd4a4cfe2b332a6d35fb8c72e8d7c7e3b
Vinay Shenoy <code@vinaysshenoy.com>
2020-04-08 14:13:16 +0530

    Add empty interface for UI actions in patient contact

--- app/src/test/java/org/simple/clinic/patientcontact/PatientContactEffectHandlerTest.kt
+++ app/src/test/java/org/simple/clinic/patientcontact/PatientContactEffectHandlerTest.kt
@@ -2,6 +2,7 @@ package org.simple.clinic.patientcontact
 
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
 import com.nhaarman.mockitokotlin2.whenever
 import org.junit.After
 import org.junit.Test
@@ -20,6 +21,7 @@ class PatientContactEffectHandlerTest {
   private val patientUuid = UUID.fromString("8a490518-a016-4818-b725-22c25dec310b")
   private val patientRepository = mock<PatientRepository>()
   private val appointmentRepository = mock<AppointmentRepository>()
+  private val uiActions = mock<PatientContactUiActions>()
 
   private val clock = TestUserClock(LocalDate.parse("2018-01-01"))
 
@@ -27,7 +29,8 @@ class PatientContactEffectHandlerTest {
       patientRepository = patientRepository,
       appointmentRepository = appointmentRepository,
       clock = clock,
-      schedulers = TrampolineSchedulersProvider()
+      schedulers = TrampolineSchedulersProvider(),
+      uiActions = uiActions
   ).build()
   private val testCase = EffectHandlerTestCase(effectHandler)
 
@@ -47,6 +50,7 @@ class PatientContactEffectHandlerTest {
 
     // then
     testCase.assertOutgoingEvents(PatientProfileLoaded(patientProfile))
+    verifyZeroInteractions(uiActions)
   }
 
   @Test
@@ -64,5 +68,6 @@ class PatientContactEffectHandlerTest {
 
     // then
     testCase.assertOutgoingEvents(OverdueAppointmentLoaded(overdueAppointment))
+    verifyZeroInteractions(uiActions)
   }
 }

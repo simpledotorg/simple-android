3944cf4a2af5382416455d17298ae0a362be5afb
Vinay Shenoy <vinay@obvious.in>
2020-04-09 12:43:33 +0530

    Use automatic permissions in `PhoneMaskBottomSheet`

--- app/src/main/java/org/simple/clinic/home/overdue/phonemask/PhoneMaskBottomSheetEvents.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/phonemask/PhoneMaskBottomSheetEvents.kt
@@ -1,19 +1,27 @@
 package org.simple.clinic.home.overdue.phonemask
 
+import android.Manifest
+import org.simple.clinic.util.None
+import org.simple.clinic.util.Optional
+import org.simple.clinic.util.RequiresPermission
 import org.simple.clinic.util.RuntimePermissionResult
 import org.simple.clinic.widgets.UiEvent
 import java.util.UUID
 
 data class PhoneMaskBottomSheetCreated(val patientUuid: UUID) : UiEvent
 
-object NormalCallClicked : UiEvent {
+data class NormalCallClicked(
+    override var permission: Optional<RuntimePermissionResult> = None,
+    override val permissionRequestCode: Int = 1,
+    override val permissionString: String = Manifest.permission.CALL_PHONE
+) : UiEvent, RequiresPermission {
   override val analyticsName = "Phone mask bottom sheet:Normal call clicked"
 }
 
-object SecureCallClicked : UiEvent {
+data class SecureCallClicked(
+    override var permission: Optional<RuntimePermissionResult> = None,
+    override val permissionRequestCode: Int = 2,
+    override val permissionString: String = Manifest.permission.CALL_PHONE
+) : UiEvent, RequiresPermission {
   override val analyticsName = "Phone mask bottom sheet:Secure call clicked"
-}
-
-data class CallPhonePermissionChanged(val result: RuntimePermissionResult) : UiEvent {
-  override val analyticsName = "Phone mask bottom sheet:Call Phone Permission:$result"
 }

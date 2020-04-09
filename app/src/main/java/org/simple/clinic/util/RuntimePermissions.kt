d954ef426f7ef69bd009b14ccf714c965dc7f6b4
Vinay Shenoy <vinay@obvious.in>
2020-04-09 08:25:15 +0530

    Change `RuntimePermissions` to be an injected class instead of object
    This is done in preparation for a future commit where we need to make
    this testable.

--- app/src/main/java/org/simple/clinic/util/RuntimePermissions.kt
+++ app/src/main/java/org/simple/clinic/util/RuntimePermissions.kt
@@ -3,6 +3,10 @@ package org.simple.clinic.util
 import android.app.Activity
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
+import org.simple.clinic.util.RuntimePermissionResult.DENIED
+import org.simple.clinic.util.RuntimePermissionResult.GRANTED
+import org.simple.clinic.util.RuntimePermissionResult.NEVER_ASK_AGAIN
+import javax.inject.Inject
 
 enum class RuntimePermissionResult {
   GRANTED,
@@ -10,17 +14,17 @@ enum class RuntimePermissionResult {
   NEVER_ASK_AGAIN
 }
 
-object RuntimePermissions {
+class RuntimePermissions @Inject constructor() {
 
   fun check(activity: Activity, permission: String): RuntimePermissionResult {
     return if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
       if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
-        RuntimePermissionResult.NEVER_ASK_AGAIN
+        NEVER_ASK_AGAIN
       } else {
-        RuntimePermissionResult.DENIED
+        DENIED
       }
     } else {
-      RuntimePermissionResult.GRANTED
+      GRANTED
     }
   }
 

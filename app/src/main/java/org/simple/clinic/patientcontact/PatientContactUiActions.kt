6a514c389c56f980ab7b1afcc3b2bd5beb4f6aab
Vinay Shenoy <vinay@obvious.in>
2020-04-10 09:58:18 +0530

    Add effects to perform different types of phone calls

--- app/src/main/java/org/simple/clinic/patientcontact/PatientContactUiActions.kt
+++ app/src/main/java/org/simple/clinic/patientcontact/PatientContactUiActions.kt
@@ -1,4 +1,8 @@
 package org.simple.clinic.patientcontact
 
+import org.simple.clinic.phone.Dialer
+
 interface PatientContactUiActions {
+  fun directlyCallPatient(patientPhoneNumber: String, dialer: Dialer)
+  fun maskedCallPatient(patientPhoneNumber: String, proxyNumber: String, dialer: Dialer)
 }

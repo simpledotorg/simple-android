28d7c1728f1fec8db5aaba8c896c6559ea12ed2f
Vinay Shenoy <code@vinaysshenoy.com>
2020-04-13 14:36:30 +0530

    Inject `PatientSummaryConfig` into `PatientSummaryScreen`

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryConfig.kt
@@ -2,21 +2,16 @@ package org.simple.clinic.summary
 
 import org.simple.clinic.remoteconfig.ConfigReader
 import org.threeten.bp.Duration
+import javax.inject.Inject
 
 data class PatientSummaryConfig(
     val bpEditableDuration: Duration,
     val callPatientFeatureEnabled: Boolean
 ) {
 
-  companion object {
-
-    fun read(configReader: ConfigReader): PatientSummaryConfig {
-      val bpEditableDurationConfig = configReader.long("bp_editable_duration_in_seconds", 3600)
-
-      return PatientSummaryConfig(
-          bpEditableDuration = Duration.ofSeconds(bpEditableDurationConfig),
-          callPatientFeatureEnabled = configReader.boolean("call_patient_from_summary_feature_enabled", false)
-      )
-    }
-  }
+  @Inject
+  constructor(configReader: ConfigReader) : this(
+      bpEditableDuration = Duration.ofSeconds(configReader.long("bp_editable_duration_in_seconds", 3600)),
+      callPatientFeatureEnabled = configReader.boolean("call_patient_from_summary_feature_enabled", false)
+  )
 }

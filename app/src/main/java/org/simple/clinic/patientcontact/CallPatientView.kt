d9d40d710da2bd2c66e242d21883ba0db93a5efd
Vinay Shenoy <vinay@obvious.in>
2020-04-13 11:35:36 +0530

    Add methods to `CallPatientView` to render the UI

--- app/src/main/java/org/simple/clinic/patientcontact/CallPatientView.kt
+++ app/src/main/java/org/simple/clinic/patientcontact/CallPatientView.kt
@@ -4,16 +4,37 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.patientcontact_view_callpatient.view.*
 import org.simple.clinic.R
+import org.simple.clinic.patient.Gender
+import org.simple.clinic.patient.displayLetterRes
 
 class CallPatientView(
     context: Context,
     attributeSet: AttributeSet
 ) : ConstraintLayout(context, attributeSet) {
 
+  var callResultSectionVisible: Boolean = false
+    set(value) {
+      field = value
+      callResultsGroup.visibility = if (field) View.VISIBLE else View.GONE
+    }
+
+  var secureCallingSectionVisible: Boolean = false
+    set(value) {
+      field = value
+      secureCallingGroup.visibility = if (field) View.VISIBLE else View.GONE
+    }
+
   override fun onFinishInflate() {
     super.onFinishInflate()
 
     View.inflate(context, R.layout.patientcontact_view_callpatient, this)
+  }
+
+  fun renderPatientDetails(name: String, gender: Gender, age: Int, phoneNumber: String) {
+    val genderLetter = resources.getString(gender.displayLetterRes)
+    nameTextView.text = resources.getString(R.string.patientcontact_patientdetails, name, genderLetter, age.toString())
+    phoneNumberTextView.text = phoneNumber
   }
 }

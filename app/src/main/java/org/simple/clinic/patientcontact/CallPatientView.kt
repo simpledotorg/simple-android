f21e1c9f8a0f4f4ccf6a293c2c1e0a4686bd65c8
Vinay Shenoy <vinay@obvious.in>
2020-04-14 09:27:19 +0530

    Set up click events in `PatientContactBottomSheet`

--- app/src/main/java/org/simple/clinic/patientcontact/CallPatientView.kt
+++ app/src/main/java/org/simple/clinic/patientcontact/CallPatientView.kt
@@ -3,11 +3,18 @@ package org.simple.clinic.patientcontact
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.patientcontact_view_callpatient.view.*
 import org.simple.clinic.R
 import org.simple.clinic.patient.Gender
 import org.simple.clinic.patient.displayLetterRes
+
+private typealias AgreedToVisitClicked = () -> Unit
+private typealias RemindToCallLaterClicked = () -> Unit
+private typealias RemoveFromOverdueListClicked = () -> Unit
+private typealias NormalCallButtonClicked = () -> Unit
+private typealias SecureCallButtonClicked = () -> Unit
 
 class CallPatientView(
     context: Context,
@@ -26,10 +33,26 @@ class CallPatientView(
       secureCallingGroup.visibility = if (field) View.VISIBLE else View.GONE
     }
 
+  var agreedToVisitClicked: AgreedToVisitClicked? = { Toast.makeText(context, "WIP", Toast.LENGTH_SHORT).show() }
+
+  var remindToCallLaterClicked: RemindToCallLaterClicked? = { Toast.makeText(context, "WIP", Toast.LENGTH_SHORT).show() }
+
+  var removeFromOverdueListClicked: RemoveFromOverdueListClicked? = { Toast.makeText(context, "WIP", Toast.LENGTH_SHORT).show() }
+
+  var normalCallButtonClicked: NormalCallButtonClicked? = null
+
+  var secureCallButtonClicked: SecureCallButtonClicked? = null
+
   override fun onFinishInflate() {
     super.onFinishInflate()
 
     View.inflate(context, R.layout.patientcontact_view_callpatient, this)
+
+    agreedToVisitTextView.setOnClickListener { agreedToVisitClicked?.invoke() }
+    remindToCallLaterTextView.setOnClickListener { remindToCallLaterClicked?.invoke() }
+    removeFromOverdueListTextView.setOnClickListener { removeFromOverdueListClicked?.invoke() }
+    normalCallButton.setOnClickListener { normalCallButtonClicked?.invoke() }
+    secureCallButton.setOnClickListener { secureCallButtonClicked?.invoke() }
   }
 
   fun renderPatientDetails(name: String, gender: Gender, age: Int, phoneNumber: String) {

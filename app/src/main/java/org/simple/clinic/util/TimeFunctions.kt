08d91c11b066a5a1749dbeccfe294de5cc2a0513
Vinay Shenoy <vinay@obvious.in>
2020-04-15 10:39:06 +0530

    Change generation of `PotentialAppointmentDate` to be factory function
    This is in preparation for a future commit where we will need to
    generate this list from the `ContactPatientModel`.

--- app/src/main/java/org/simple/clinic/util/TimeFunctions.kt
+++ app/src/main/java/org/simple/clinic/util/TimeFunctions.kt
@@ -1,10 +1,12 @@
 package org.simple.clinic.util
 
+import org.simple.clinic.overdue.TimeToAppointment
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalTime
 import org.threeten.bp.ZoneId
 import org.threeten.bp.ZoneOffset.UTC
+import org.threeten.bp.temporal.ChronoUnit
 
 fun Instant.toLocalDateAtZone(zone: ZoneId): LocalDate {
   return this.atZone(UTC)
@@ -17,4 +19,15 @@ fun LocalDate.toUtcInstant(userClock: UserClock): Instant {
   val userDateTime = this.atTime(userTime)
   val utcDateTime = userDateTime.atZone(userClock.zone).withZoneSameInstant(UTC)
   return utcDateTime.toInstant()
+}
+
+fun LocalDate.plus(timeToAppointment: TimeToAppointment): LocalDate {
+  return this.plus(
+      timeToAppointment.value.toLong(),
+      when (timeToAppointment) {
+        is TimeToAppointment.Days -> ChronoUnit.DAYS
+        is TimeToAppointment.Weeks -> ChronoUnit.WEEKS
+        is TimeToAppointment.Months -> ChronoUnit.MONTHS
+      }
+  )
 }

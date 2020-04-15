21de1aa9ca6023227bea5ef7ed2498923ec08bae
Vinay Shenoy <vinay@obvious.in>
2020-04-15 10:39:06 +0530

    Change generation of `PotentialAppointmentDate` to be factory function
    This is in preparation for a future commit where we will need to
    generate this list from the `ContactPatientModel`.

--- app/src/main/java/org/simple/clinic/overdue/PotentialAppointmentDate.kt
+++ app/src/main/java/org/simple/clinic/overdue/PotentialAppointmentDate.kt
@@ -1,11 +1,26 @@
 package org.simple.clinic.overdue
 
+import org.simple.clinic.util.UserClock
+import org.simple.clinic.util.plus
 import org.threeten.bp.LocalDate
 
 data class PotentialAppointmentDate(
     val scheduledFor: LocalDate,
     val timeToAppointment: TimeToAppointment
 ) : Comparable<PotentialAppointmentDate> {
+
+  companion object {
+    fun from(
+        appointmentTimes: List<TimeToAppointment>,
+        userClock: UserClock
+    ): List<PotentialAppointmentDate> {
+      val today = LocalDate.now(userClock)
+      return appointmentTimes
+          .map { timeToAppointment -> today.plus(timeToAppointment) to timeToAppointment }
+          .map { (appointmentDate, timeToAppointment) -> PotentialAppointmentDate(appointmentDate, timeToAppointment) }
+    }
+  }
+
   override fun compareTo(other: PotentialAppointmentDate): Int {
     return this.scheduledFor.compareTo(other.scheduledFor)
   }

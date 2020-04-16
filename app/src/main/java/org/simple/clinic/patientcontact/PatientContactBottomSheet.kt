51396c76611252d733a5d4b5f1e348e6e5835511
Vinay Shenoy <code@vinaysshenoy.com>
2020-04-16 09:30:47 +0530

    Implement screen interfaces on `PatientContactBottomSheet`

--- app/src/main/java/org/simple/clinic/patientcontact/PatientContactBottomSheet.kt
+++ app/src/main/java/org/simple/clinic/patientcontact/PatientContactBottomSheet.kt
@@ -1,10 +1,16 @@
 package org.simple.clinic.patientcontact
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.sheet_patientcontact.*
 import org.simple.clinic.R
+import org.simple.clinic.patient.Gender
+import org.simple.clinic.phone.Dialer
+import org.simple.clinic.phone.PhoneCaller
 import org.simple.clinic.widgets.BottomSheetActivity
 
-class PatientContactBottomSheet: BottomSheetActivity() {
+class PatientContactBottomSheet: BottomSheetActivity(), PatientContactUi, PatientContactUiActions {
+
+  lateinit var phoneCaller: PhoneCaller
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -14,5 +20,37 @@ class PatientContactBottomSheet: BottomSheetActivity() {
 
   override fun onBackgroundClick() {
     finish()
+  }
+
+  override fun renderPatientDetails(name: String, gender: Gender, age: Int, phoneNumber: String) {
+    callPatientView.renderPatientDetails(name, gender, age, phoneNumber)
+  }
+
+  override fun showCallResultSection() {
+    callPatientView.callResultSectionVisible = true
+  }
+
+  override fun hideCallResultSection() {
+    callPatientView.callResultSectionVisible = false
+  }
+
+  override fun showSecureCallUi() {
+    callPatientView.secureCallingSectionVisible = true
+  }
+
+  override fun hideSecureCallUi() {
+    callPatientView.secureCallingSectionVisible = false
+  }
+
+  override fun directlyCallPatient(patientPhoneNumber: String, dialer: Dialer) {
+    phoneCaller.normalCall(patientPhoneNumber, dialer)
+  }
+
+  override fun maskedCallPatient(patientPhoneNumber: String, proxyNumber: String, dialer: Dialer) {
+    phoneCaller.secureCall(
+        visibleNumber = proxyNumber,
+        hiddenNumber = patientPhoneNumber,
+        dialer = dialer
+    )
   }
 }

7534df944a6dd49356cd404ff07972fd4e769766
Vinay Shenoy <vinay@obvious.in>
2020-04-20 16:38:21 +0530

    Add `RemoveAppointmentReasonItem` adapter list item

--- app/src/main/java/org/simple/clinic/contactpatient/RemoveAppointmentReason.kt
+++ app/src/main/java/org/simple/clinic/contactpatient/RemoveAppointmentReason.kt
@@ -1,11 +1,14 @@
 package org.simple.clinic.contactpatient
 
-enum class RemoveAppointmentReason {
-  AlreadyVisited,
-  NotResponding,
-  PhoneNumberNotWorking,
-  TransferredToAnotherFacility,
-  MovedToPrivatePractitioner,
-  Died,
-  OtherReason
+import androidx.annotation.StringRes
+import org.simple.clinic.R
+
+enum class RemoveAppointmentReason(@StringRes val displayText: Int) {
+  AlreadyVisited(R.string.contactpatient_patient_already_visited),
+  NotResponding(R.string.contactpatient_patient_is_not_responding),
+  PhoneNumberNotWorking(R.string.contactpatient_invalid_phone_number),
+  TransferredToAnotherFacility(R.string.contactpatient_public_hospital_transfer),
+  MovedToPrivatePractitioner(R.string.contactpatient_moved_to_private),
+  Died(R.string.contactpatient_patient_died),
+  OtherReason(R.string.contactpatient_other_reason)
 }

c24d2cd5f458ee72d74ef67f41a7c8622bf91e28
Sanchita Agarwal <sanchita@obvious.in>
2020-04-20 19:52:22 +0530

    Create all Mobius classes for drugs screen

---
+++ app/src/main/java/org/simple/clinic/drugs/EditMedicinesInit.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.drugs
+
+import com.spotify.mobius.First
+import com.spotify.mobius.Init
+
+class EditMedicinesInit : Init<EditMedicinesModel, EditMedicinesEffect> {
+  
+  override fun init(model: EditMedicinesModel): First<EditMedicinesModel, EditMedicinesEffect> {
+    TODO("not implemented")
+  }
+
+}
---
+++ app/src/main/java/org/simple/clinic/drugs/EditMedicinesModel.kt
@@ -0,0 +1,8 @@
+package org.simple.clinic.drugs
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class EditMedicinesModel : Parcelable {
+}

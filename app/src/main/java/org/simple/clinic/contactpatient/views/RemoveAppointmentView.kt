c283aa5fba1a90eb7825bbd5f496b8d1f56b91fe
Vinay Shenoy <vinay@obvious.in>
2020-04-20 16:38:49 +0530

    Add methods to display the UI in `RemoveAppointmentView`

--- app/src/main/java/org/simple/clinic/contactpatient/views/RemoveAppointmentView.kt
+++ app/src/main/java/org/simple/clinic/contactpatient/views/RemoveAppointmentView.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.contactpatient_removeappointment.view.*
 import org.simple.clinic.R
+import org.simple.clinic.contactpatient.RemoveAppointmentReason
 import org.simple.clinic.widgets.DividerItemDecorator
+import org.simple.clinic.widgets.ItemAdapter
 import org.simple.clinic.widgets.dp
 
 class RemoveAppointmentView(
@@ -14,11 +17,33 @@ class RemoveAppointmentView(
     attributeSet: AttributeSet
 ) : ConstraintLayout(context, attributeSet) {
 
+  private val removalReasonsAdapter = ItemAdapter(RemoveAppointmentReasonItem.DiffCallback())
+
   override fun onFinishInflate() {
     super.onFinishInflate()
 
     View.inflate(context, R.layout.contactpatient_removeappointment, this)
 
-    removalReasonsRecyclerView.addItemDecoration(DividerItemDecorator(context, marginStart = 56.dp, marginEnd = 16.dp))
+    removalReasonsRecyclerView.apply {
+      setHasFixedSize(true)
+      layoutManager = LinearLayoutManager(context)
+      adapter = removalReasonsAdapter
+      addItemDecoration(DividerItemDecorator(context, marginStart = 56.dp, marginEnd = 16.dp))
+    }
+  }
+
+  fun renderAppointmentRemoveReasons(
+      reasons: List<RemoveAppointmentReason>,
+      selectedReason: RemoveAppointmentReason?
+  ) {
+    removalReasonsAdapter.submitList(RemoveAppointmentReasonItem.from(reasons, selectedReason))
+  }
+
+  fun enableRemoveAppointmentDoneButton() {
+    removeAppointmentDone.isEnabled = true
+  }
+
+  fun disableRemoveAppointmentDoneButton() {
+    removeAppointmentDone.isEnabled = false
   }
 }

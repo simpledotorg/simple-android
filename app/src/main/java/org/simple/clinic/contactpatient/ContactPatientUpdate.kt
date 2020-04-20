a05440ac1390b9aab0f6f4b78abb1b0d9fa01ae3
Vinay Shenoy <vinay@obvious.in>
2020-04-20 10:29:13 +0530

    Close sheet when the patient has been marked as dead

--- app/src/main/java/org/simple/clinic/contactpatient/ContactPatientUpdate.kt
+++ app/src/main/java/org/simple/clinic/contactpatient/ContactPatientUpdate.kt
@@ -28,7 +28,7 @@ class ContactPatientUpdate(
       is OverdueAppointmentLoaded -> next(model.overdueAppointmentLoaded(event.overdueAppointment))
       is NormalCallClicked -> directlyCallPatient(model, event)
       is SecureCallClicked -> maskedCallPatient(model, event)
-      PatientMarkedAsAgreedToVisit, ReminderSetForAppointment, PatientMarkedAsVisited -> dispatch(CloseScreen)
+      PatientMarkedAsAgreedToVisit, ReminderSetForAppointment, PatientMarkedAsVisited, PatientMarkedAsDead -> dispatch(CloseScreen)
       PatientAgreedToVisitClicked -> dispatch(MarkPatientAsAgreedToVisit(model.appointment!!.get().appointment.uuid))
       NextReminderDateClicked -> selectNextReminderDate(model)
       PreviousReminderDateClicked -> selectPreviousReminderDate(model)
@@ -41,7 +41,6 @@ class ContactPatientUpdate(
       RemindToCallLaterClicked -> next(model.changeUiModeTo(SetAppointmentReminder))
       BackClicked -> backClicks(model)
       is RemoveAppointmentReasonSelected -> next(model.removeAppointmentReasonSelected(event.reason))
-      PatientMarkedAsDead -> noChange()
     }
   }
 

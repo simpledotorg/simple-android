7928a94877771625c1df9bb8228744fe7df93660
Vinay Shenoy <vinay@obvious.in>
2020-04-20 11:41:18 +0530

    Add `RemoveAppointmentReason` to represent remove appointment options
    This was added because `AppointmentCancelReason` is a subset of the
    options that need to be represented in the `RemoveAppointment` view.
    
    Depending on which reason we select, we will either:
    
    - Cancel the appointment with a reason, and/or
    - Perform some operations on the patient
    - Not cancel the appointment, but mark as scheduled
    
    This commit adds a separate enum to represent the options in the UI,
    which will get mapped to the correct cancel reasons in the update loop.

---
+++ app/src/main/java/org/simple/clinic/contactpatient/RemoveAppointmentReason.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.contactpatient
+
+enum class RemoveAppointmentReason {
+  AlreadyVisited,
+  NotResponding,
+  PhoneNumberNotWorking,
+  TransferredToAnotherFacility,
+  MovedToPrivatePractitioner,
+  Died,
+  OtherReason
+}

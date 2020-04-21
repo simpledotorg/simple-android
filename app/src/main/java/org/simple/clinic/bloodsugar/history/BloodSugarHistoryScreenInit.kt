d7dfb33599566efbd3b89029cb79abc90efdcab9
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-04-21 10:35:46 +0530

    Show blood sugars when screen is created

--- app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreenInit.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreenInit.kt
@@ -6,6 +6,6 @@ import org.simple.clinic.mobius.first
 
 class BloodSugarHistoryScreenInit : Init<BloodSugarHistoryScreenModel, BloodSugarHistoryScreenEffect> {
   override fun init(model: BloodSugarHistoryScreenModel): First<BloodSugarHistoryScreenModel, BloodSugarHistoryScreenEffect> {
-    return first(model, LoadPatient(model.patientUuid), LoadBloodSugarHistory(model.patientUuid))
+    return first(model, LoadPatient(model.patientUuid), LoadBloodSugarHistory(model.patientUuid), ShowBloodSugars(model.patientUuid))
   }
 }

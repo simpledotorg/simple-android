544074343e44cde7c7047aba5b96f284d2af69d5
Sanchita Agarwal <sanchita@obvious.in>
2020-04-21 13:37:28 +0530

    Pass `EditMedicinesUiActions` to `EffectHandler`

--- app/src/main/java/org/simple/clinic/drugs/EditMedicinesEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/drugs/EditMedicinesEffectHandler.kt
@@ -3,8 +3,9 @@ package org.simple.clinic.drugs
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.drugs.selection.EditMedicinesUiActions
 
-class EditMedicinesEffectHandler : ObservableTransformer<EditMedicinesEffect, EditMedicinesEvent> {
+class EditMedicinesEffectHandler(val uiActions: EditMedicinesUiActions) : ObservableTransformer<EditMedicinesEffect, EditMedicinesEvent> {
 
   override fun apply(upstream: Observable<EditMedicinesEffect>): ObservableSource<EditMedicinesEvent> {
     return Observable.empty()

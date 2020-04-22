e7cdfe22f73c2c99318369953897a3b163db7b02
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-04-22 07:52:34 +0530

    Use `PatientSearchResult` database view in `PatientSearchResult.RoomDao`

--- app/src/main/java/org/simple/clinic/patient/PatientSearchResult.kt
+++ app/src/main/java/org/simple/clinic/patient/PatientSearchResult.kt
@@ -8,7 +8,6 @@ import androidx.room.Query
 import io.reactivex.Flowable
 import io.reactivex.Single
 import kotlinx.android.parcel.Parcelize
-import org.intellij.lang.annotations.Language
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
 import java.util.UUID
@@ -106,55 +105,12 @@ data class PatientSearchResult(
   @Dao
   interface RoomDao {
 
-    companion object {
-      @Language("RoomSql")
-      const val mainQuery = """
-          SELECT P.uuid, P.fullName, P.gender, P.dateOfBirth, P.age_value, P.age_updatedAt, P.status, P.createdAt, P.updatedAt, P.syncStatus, P.recordedAt,
-          PA.uuid addr_uuid, PA.streetAddress addr_streetAddress, PA.colonyOrVillage addr_colonyOrVillage, PA.zone addr_zone, PA.district addr_district, 
-          PA.state addr_state, PA.country addr_country,
-          PA.createdAt addr_createdAt, PA.updatedAt addr_updatedAt,
-          PP.uuid phoneUuid, PP.number phoneNumber, PP.phoneType phoneType, PP.active phoneActive, PP.createdAt phoneCreatedAt, PP.updatedAt phoneUpdatedAt,
-          PatientLastSeen.lastSeenTime lastSeen_lastSeenOn, F.name lastSeen_lastSeenAtFacilityName, PatientLastSeen.lastSeenFacilityUuid lastSeen_lastSeenAtFacilityUuid
-          FROM Patient P
-          INNER JOIN PatientAddress PA ON PA.uuid = P.addressUuid
-          LEFT JOIN PatientPhoneNumber PP ON PP.patientUuid = P.uuid
-          LEFT JOIN (
-            SELECT P.uuid patientUuid,
-            (
-                CASE
-                    WHEN LatestBloodSugar.uuid IS NULL THEN LatestBP.recordedAt
-                    WHEN LatestBP.uuid IS NULL THEN LatestBloodSugar.recordedAt
-                    ELSE MAX(LatestBP.recordedAt, LatestBloodSugar.recordedAt)
-                END
-            ) lastSeenTime,
-            (
-                CASE
-                    WHEN LatestBloodSugar.uuid IS NULL THEN LatestBP.facilityUuid
-                    WHEN LatestBP.uuid IS NULL THEN LatestBloodSugar.facilityUuid
-                    WHEN LatestBP.recordedAt > LatestBloodSugar.recordedAt THEN LatestBP.facilityUuid
-                    ELSE LatestBloodSugar.facilityUuid
-                END
-            ) lastSeenFacilityUuid
-            FROM Patient P
-            LEFT JOIN (
-                SELECT BP.uuid, BP.patientUuid, BP.recordedAt, F.uuid facilityUuid FROM BloodPressureMeasurement BP
-                INNER JOIN Facility F on F.uuid = BP.facilityUuid
-                WHERE BP.deletedAt IS NULL
-                GROUP BY BP.patientUuid HAVING MAX(BP.recordedAt)
-            ) LatestBP ON LatestBP.patientUuid = P.uuid
-            LEFT JOIN (
-                SELECT BloodSugar.uuid, BloodSugar.patientUuid, BloodSugar.recordedAt, F.uuid facilityUuid FROM BloodSugarMeasurements BloodSugar
-                INNER JOIN Facility F ON F.uuid = BloodSugar.facilityUuid
-                WHERE BloodSugar.deletedAt IS NULL
-                GROUP BY BloodSugar.patientUuid HAVING MAX(BloodSugar.recordedAt)
-            ) LatestBloodSugar ON LatestBloodSugar.patientUuid = P.uuid
-            WHERE LatestBP.uuid IS NOT NULL OR LatestBloodSugar.uuid IS NOT NULL
-          ) PatientLastSeen ON (PatientLastSeen.patientUuid == P.uuid)
-          LEFT JOIN Facility F ON F.uuid = PatientLastSeen.lastSeenFacilityUuid
-    """
-    }
-
-    @Query("""$mainQuery WHERE P.uuid IN (:uuids) AND P.status = :status AND P.deletedAt IS NULL""")
+    @Query("""
+      SELECT * FROM 
+      PatientSearchResult searchResult
+      LEFT JOIN Patient P ON P.uuid = searchResult.uuid
+      WHERE P.uuid IN (:uuids) AND P.status = :status AND P.deletedAt IS NULL
+      """)
     fun searchByIds(uuids: List<UUID>, status: PatientStatus): Single<List<PatientSearchResult>>
 
     @Query("""SELECT Patient.uuid, Patient.fullName FROM Patient WHERE Patient.status = :status AND Patient.deletedAt IS NULL""")
@@ -162,7 +118,7 @@ data class PatientSearchResult(
 
     @Suppress("AndroidUnresolvedRoomSqlReference")
     @Query("""
-      SELECT * FROM ($mainQuery) AllSearchResults
+      SELECT * FROM PatientSearchResult searchResults
       INNER JOIN (
         SELECT DISTINCT P.uuid FROM Patient P
           LEFT JOIN BloodPressureMeasurement BP ON BP.patientUuid = P.uuid
@@ -173,14 +129,17 @@ data class PatientSearchResult(
                 (BP.deletedAt IS NULL AND BP.facilityUuid = :facilityUuid) OR
                 (BloodSugar.deletedAt IS NULL AND BloodSugar.facilityUuid = :facilityUuid)
             )
-      ) PatientsAtFacility ON AllSearchResults.uuid = PatientsAtFacility.uuid
-      ORDER BY AllSearchResults.fullName COLLATE NOCASE ASC
+      ) PatientsAtFacility ON searchResults.uuid = PatientsAtFacility.uuid
+      ORDER BY searchResults.fullName COLLATE NOCASE ASC
     """)
     fun searchInFacilityAndSortByName(facilityUuid: UUID, status: PatientStatus): Flowable<List<PatientSearchResult>>
 
     @Suppress("AndroidUnresolvedRoomSqlReference")
     @Query("""
-        $mainQuery WHERE phoneNumber LIKE '%' || :phoneNumber || '%' AND P.deletedAt IS NULL
+        SELECT * FROM 
+        PatientSearchResult searchResult
+        LEFT JOIN Patient P ON P.uuid = searchResult.uuid
+        WHERE phoneNumber LIKE '%' || :phoneNumber || '%' AND P.deletedAt IS NULL
         ORDER BY P.fullName COLLATE NOCASE ASC LIMIT :limit
     """)
     fun searchByPhoneNumber(phoneNumber: String, limit: Int): Flowable<List<PatientSearchResult>>

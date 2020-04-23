f669836a627e0915018f888181101c403aace4fd
Sanchita Agarwal <sanchita@obvious.in>
2020-04-23 17:13:05 +0530

    Fetch prescribed and protocol drugs in `EditMedicinesEffectHandler`

--- app/src/main/java/org/simple/clinic/drugs/EditMedicinesInit.kt
+++ app/src/main/java/org/simple/clinic/drugs/EditMedicinesInit.kt
@@ -2,10 +2,11 @@ package org.simple.clinic.drugs
 
 import com.spotify.mobius.First
 import com.spotify.mobius.Init
+import org.simple.clinic.mobius.first
 
 class EditMedicinesInit : Init<EditMedicinesModel, EditMedicinesEffect> {
 
   override fun init(model: EditMedicinesModel): First<EditMedicinesModel, EditMedicinesEffect> {
-    return First.first(model)
+    return first(model, FetchPrescribedAndProtocolDrugs(model.patientUuid))
   }
 }

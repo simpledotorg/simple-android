90222a48e090cb2fc3db2878ab45adbadc277fae
Vinay Shenoy <vinay@obvious.in>
2020-04-28 16:38:50 +0530

    Remove `BpValidator` class

--- app/src/test/java/org/simple/clinic/bp/entry/BpValidationTest.kt
+++ app/src/test/java/org/simple/clinic/bp/entry/BpValidationTest.kt
@@ -2,52 +2,52 @@ package org.simple.clinic.bp.entry
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import org.simple.clinic.bp.BloodPressureReading
 import org.simple.clinic.bp.Validation
 
-class BpValidatorTest {
-  private val bpValidator = BpValidator()
+class BpValidationTest {
 
   @Test
   fun `when systolic is less than diastolic, return error`() {
-    val systolic = "90"
-    val diastolic = "140"
-    val result = bpValidator.validate(systolic, diastolic)
+    val systolic = 90
+    val diastolic = 140
+    val result = BloodPressureReading(systolic, diastolic).validate()
 
     assertThat(result).isEqualTo(Validation.ErrorSystolicLessThanDiastolic)
   }
 
   @Test
   fun `when systolic is less than minimum possible, return error`() {
-    val systolic = "55"
-    val diastolic = "55"
-    val result = bpValidator.validate(systolic, diastolic)
+    val systolic = 55
+    val diastolic = 55
+    val result = BloodPressureReading(systolic, diastolic).validate()
 
     assertThat(result).isEqualTo(Validation.ErrorSystolicTooLow)
   }
 
   @Test
   fun `when systolic is more than maximum possible, return error`() {
-    val systolic = "333"
-    val diastolic = "88"
-    val result = bpValidator.validate(systolic, diastolic)
+    val systolic = 333
+    val diastolic = 88
+    val result = BloodPressureReading(systolic, diastolic).validate()
 
     assertThat(result).isEqualTo(Validation.ErrorSystolicTooHigh)
   }
 
   @Test
   fun `when diastolic is less than minimum possible, return error`() {
-    val systolic = "110"
-    val diastolic = "33"
-    val result = bpValidator.validate(systolic, diastolic)
+    val systolic = 110
+    val diastolic = 33
+    val result = BloodPressureReading(systolic, diastolic).validate()
 
     assertThat(result).isEqualTo(Validation.ErrorDiastolicTooLow)
   }
 
   @Test
   fun `when diastolic is more than maximum possible, return error`() {
-    val systolic = "233"
-    val diastolic = "190"
-    val result = bpValidator.validate(systolic, diastolic)
+    val systolic = 233
+    val diastolic = 190
+    val result = BloodPressureReading(systolic, diastolic).validate()
 
     assertThat(result).isEqualTo(Validation.ErrorDiastolicTooHigh)
   }

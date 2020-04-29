8fe4761af7f9255a0bb11028fca241b33ce7552e
Vinay Shenoy <vinay@obvious.in>
2020-04-29 15:49:55 +0530

    Add Mobius classes for `MedicalHistorySummaryView`

---
+++ app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryInit.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.summary.medicalhistory
+
+import com.spotify.mobius.First
+import com.spotify.mobius.Init
+import org.simple.clinic.mobius.first
+
+class MedicalHistorySummaryInit: Init<MedicalHistorySummaryModel, MedicalHistorySummaryEffect> {
+
+  override fun init(model: MedicalHistorySummaryModel): First<MedicalHistorySummaryModel, MedicalHistorySummaryEffect> {
+    return first(model)
+  }
+}
---
+++ app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryUiRenderer.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.summary.medicalhistory
+
+import org.simple.clinic.mobius.ViewRenderer
+
+class MedicalHistorySummaryUiRenderer(
+    private val ui: MedicalHistorySummaryUi
+) : ViewRenderer<MedicalHistorySummaryModel> {
+
+  override fun render(model: MedicalHistorySummaryModel) {
+  }
+}

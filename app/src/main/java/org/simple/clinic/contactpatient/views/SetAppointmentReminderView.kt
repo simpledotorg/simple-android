cba4bc4d868716994913d9dc21607dfff6d5ab52
Vinay Shenoy <vinay@obvious.in>
2020-04-29 10:03:32 +0530

    Trigger events from `SetAppointmentReminderView`

--- app/src/main/java/org/simple/clinic/contactpatient/views/SetAppointmentReminderView.kt
+++ app/src/main/java/org/simple/clinic/contactpatient/views/SetAppointmentReminderView.kt
@@ -4,16 +4,35 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.contactpatient_appointmentreminder.view.*
 import org.simple.clinic.R
+
+private typealias DecrementStepperClicked = () -> Unit
+private typealias IncrementStepperClicked = () -> Unit
+private typealias AppointmentDateClicked = () -> Unit
+private typealias DoneClicked = () -> Unit
 
 class SetAppointmentReminderView(
     context: Context,
     attributeSet: AttributeSet
 ) : ConstraintLayout(context, attributeSet) {
 
+  var decrementStepperClicked: DecrementStepperClicked? = null
+
+  var incrementStepperClicked: IncrementStepperClicked? = null
+
+  var appointmentDateClicked: AppointmentDateClicked? = null
+
+  var doneClicked: DoneClicked? = null
+
   override fun onFinishInflate() {
     super.onFinishInflate()
 
     View.inflate(context, R.layout.contactpatient_appointmentreminder, this)
+
+    previousDateStepper.setOnClickListener { decrementStepperClicked?.invoke() }
+    nextDateStepper.setOnClickListener { incrementStepperClicked?.invoke() }
+    saveReminder.setOnClickListener { doneClicked?.invoke() }
+    actualAppointmentDateButton.setOnClickListener { appointmentDateClicked?.invoke() }
   }
 }

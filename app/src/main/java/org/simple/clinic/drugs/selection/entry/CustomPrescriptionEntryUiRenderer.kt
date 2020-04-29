a79b451868f7f4e7d286569e75b4c1ec40234b11
Sanchita Agarwal <sanchita@obvious.in>
2020-04-29 18:00:01 +0530

    Add Mobius classes for `CustomPrescriptionEntry`

---
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryUiRenderer.kt
@@ -0,0 +1,9 @@
+package org.simple.clinic.drugs.selection.entry
+
+import org.simple.clinic.mobius.ViewRenderer
+
+class CustomPrescriptionEntryUiRenderer : ViewRenderer<CustomPrescriptionEntryModel> {
+
+  override fun render(model: CustomPrescriptionEntryModel) {
+  }
+}

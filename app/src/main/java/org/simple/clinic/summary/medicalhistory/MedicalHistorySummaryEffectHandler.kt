3bcf415316f7faa53e3f015e0b7e14060a9b587b
Vinay Shenoy <code@vinaysshenoy.com>
2020-04-30 11:41:44 +0530

    Move updating of medical history to the Mobius loop

--- app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/summary/medicalhistory/MedicalHistorySummaryEffectHandler.kt
@@ -3,17 +3,21 @@ package org.simple.clinic.summary.medicalhistory
 import com.spotify.mobius.rx2.RxMobius
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 import org.simple.clinic.facility.FacilityRepository
 import org.simple.clinic.medicalhistory.MedicalHistoryRepository
 import org.simple.clinic.user.UserSession
+import org.simple.clinic.util.UtcClock
 import org.simple.clinic.util.scheduler.SchedulersProvider
+import org.threeten.bp.Instant
 
 class MedicalHistorySummaryEffectHandler @AssistedInject constructor(
     private val schedulers: SchedulersProvider,
     private val medicalHistoryRepository: MedicalHistoryRepository,
     private val userSession: UserSession,
     private val facilityRepository: FacilityRepository,
+    private val clock: UtcClock,
     @Assisted private val uiActions: MedicalHistorySummaryUiActions
 ) {
 
@@ -28,6 +32,7 @@ class MedicalHistorySummaryEffectHandler @AssistedInject constructor(
         .addTransformer(LoadMedicalHistory::class.java, loadMedicalHistory())
         .addTransformer(LoadCurrentFacility::class.java, loadCurrentFacility())
         .addAction(HideDiagnosisError::class.java, uiActions::hideDiagnosisError, schedulers.ui())
+        .addTransformer(SaveUpdatedMedicalHistory::class.java, updateMedicalHistory())
         .build()
   }
 
@@ -45,6 +50,14 @@ class MedicalHistorySummaryEffectHandler @AssistedInject constructor(
           .observeOn(schedulers.io())
           .map { facilityRepository.currentFacilityImmediate(userSession.loggedInUserImmediate()!!) }
           .map(::CurrentFacilityLoaded)
+    }
+  }
+
+  private fun updateMedicalHistory(): ObservableTransformer<SaveUpdatedMedicalHistory, MedicalHistorySummaryEvent> {
+    return ObservableTransformer { effects ->
+      effects
+          .flatMapCompletable { medicalHistoryRepository.save(it.medicalHistory, Instant.now(clock)).subscribeOn(schedulers.io()) }
+          .andThen(Observable.empty())
     }
   }
 }

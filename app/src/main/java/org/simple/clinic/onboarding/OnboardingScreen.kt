c69f06d25b4db2fc32e6efbec52b85987b6af4c6
Sasikanth <sasikanth@obvious.in>
2020-05-04 10:29:16 +0530

    Replace screen router usage with navigation component in `SetupActivity`

--- app/src/main/java/org/simple/clinic/onboarding/OnboardingScreen.kt
+++ app/src/main/java/org/simple/clinic/onboarding/OnboardingScreen.kt
@@ -5,18 +5,17 @@ import android.util.AttributeSet
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.NestedScrollView
+import androidx.navigation.findNavController
 import com.f2prateek.rx.preferences2.Preference
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.cast
 import kotlinx.android.synthetic.main.screen_onboarding.view.*
+import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.di.injector
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.platform.crash.CrashReporter
-import org.simple.clinic.router.screen.RouterDirection
-import org.simple.clinic.router.screen.ScreenRouter
-import org.simple.clinic.selectcountry.SelectCountryScreenKey
 import org.simple.clinic.util.clamp
 import org.simple.clinic.util.scheduler.SchedulersProvider
 import org.simple.clinic.util.unsafeLazy
@@ -36,9 +35,6 @@ class OnboardingScreen(context: Context, attributeSet: AttributeSet) : RelativeL
 
   @Inject
   lateinit var crashReporter: CrashReporter
-
-  @Inject
-  lateinit var screenRouter: ScreenRouter
 
   private val events: Observable<OnboardingEvent>
     get() = getStartedClicks()
@@ -93,6 +89,6 @@ class OnboardingScreen(context: Context, attributeSet: AttributeSet) : RelativeL
   }
 
   override fun moveToRegistrationScreen() {
-    screenRouter.clearHistoryAndPush(SelectCountryScreenKey(), RouterDirection.FORWARD)
+    findNavController().navigate(R.id.action_onboardingScreen_to_selectCountryScreen)
   }
 }

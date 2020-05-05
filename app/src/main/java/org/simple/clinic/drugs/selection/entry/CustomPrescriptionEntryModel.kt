4827eb51b5c46fa1fc10fae5c83873d0be4faeec
Sanchita Agarwal <sanchita@obvious.in>
2020-05-05 16:41:20 +0530

    Add `dosage` to `CustomPrescriptionEntryModel`

--- app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryModel.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryModel.kt
@@ -4,15 +4,19 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class CustomPrescriptionEntryModel(val drugName: String?) : Parcelable {
+data class CustomPrescriptionEntryModel(val drugName: String?, val dosage: String?) : Parcelable {
 
   companion object {
     fun create(): CustomPrescriptionEntryModel {
-      return CustomPrescriptionEntryModel(null)
+      return CustomPrescriptionEntryModel(null, null)
     }
   }
 
   fun drugNameChanged(name: String): CustomPrescriptionEntryModel {
     return copy(drugName = name)
+  }
+
+  fun dosageChanged(dosage: String): CustomPrescriptionEntryModel {
+    return copy(dosage = dosage)
   }
 }

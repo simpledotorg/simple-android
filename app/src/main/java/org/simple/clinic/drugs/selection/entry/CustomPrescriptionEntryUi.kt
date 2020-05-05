da0352ec5e4f114bc42b9e3e1cf47240604a33ce
Sanchita Agarwal <sanchita@obvious.in>
2020-05-05 20:05:50 +0530

    Move `saveNewPrescriptionsAndDismiss` to Mobius

--- app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryUi.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryUi.kt
@@ -2,9 +2,8 @@ package org.simple.clinic.drugs.selection.entry
 
 import java.util.UUID
 
-interface CustomPrescriptionEntryUi {
+interface CustomPrescriptionEntryUi : CustomPrescriptionEntryUiActions {
   fun setSaveButtonEnabled(canBeSaved: Boolean)
-  fun finish()
   fun setDrugDosageText(dosagePlaceholder: String)
   fun moveDrugDosageCursorToBeginning()
   fun showEnterNewPrescriptionTitle()

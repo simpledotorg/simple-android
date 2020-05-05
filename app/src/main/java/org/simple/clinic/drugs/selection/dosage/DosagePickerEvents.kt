d0d963fff0286d74ff5d0c20277f99ac83c26042
Vinay Shenoy <vinay@obvious.in>
2020-05-05 16:07:05 +0530

    Use `existingPrescribedDrugUuid` in `DosagePickerSheetController`

--- app/src/main/java/org/simple/clinic/drugs/selection/dosage/DosagePickerEvents.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/dosage/DosagePickerEvents.kt
@@ -1,11 +1,9 @@
 package org.simple.clinic.drugs.selection.dosage
 
 import org.simple.clinic.protocol.ProtocolDrug
-import org.simple.clinic.util.Optional
 import org.simple.clinic.widgets.UiEvent
-import java.util.UUID
 
-data class DosagePickerSheetCreated(val existingPrescribedDrugUuid: Optional<UUID>) : UiEvent
+class DosagePickerSheetCreated : UiEvent
 
 data class DosageItemClicked(val dosageOption: DosageOption) : UiEvent
 

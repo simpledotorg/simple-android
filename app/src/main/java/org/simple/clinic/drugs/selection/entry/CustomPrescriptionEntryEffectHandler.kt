4a6a9876a82e2c448b4b24f13fca6713111c3fa9
Sanchita Agarwal <sanchita@obvious.in>
2020-05-05 19:00:18 +0530

    Save prescription in `CustomPrescriptionEntryEffectHandler`

--- app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryEffectHandler.kt
@@ -3,10 +3,23 @@ package org.simple.clinic.drugs.selection.entry
 import com.spotify.mobius.rx2.RxMobius
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
+import io.reactivex.Scheduler
+import org.simple.clinic.drugs.PrescriptionRepository
+import org.simple.clinic.facility.Facility
+import org.simple.clinic.facility.FacilityRepository
+import org.simple.clinic.user.UserSession
+import org.simple.clinic.util.nullIfBlank
+import org.simple.clinic.util.scheduler.SchedulersProvider
+import java.util.UUID
 
 class CustomPrescriptionEntryEffectHandler @AssistedInject constructor(
-    @Assisted val uiActions: CustomPrescriptionEntryUiActions
+    @Assisted val uiActions: CustomPrescriptionEntryUiActions,
+    val schedulersProvider: SchedulersProvider,
+    val userSession: UserSession,
+    val facilityRepository: FacilityRepository,
+    val prescriptionRepository: PrescriptionRepository
 ) {
 
   @AssistedInject.Factory
@@ -18,6 +31,44 @@ class CustomPrescriptionEntryEffectHandler @AssistedInject constructor(
       : ObservableTransformer<CustomPrescriptionEntryEffect, CustomPrescriptionEntryEvent> {
     return RxMobius
         .subtypeEffectHandler<CustomPrescriptionEntryEffect, CustomPrescriptionEntryEvent>()
+        .addTransformer(SaveCustomPrescription::class.java, saveCustomPrescription(schedulersProvider.io()))
         .build()
   }
+
+  private fun saveCustomPrescription(io: Scheduler): ObservableTransformer<SaveCustomPrescription, CustomPrescriptionEntryEvent> {
+    return ObservableTransformer { effects ->
+      effects
+          .observeOn(io)
+          .map {
+            val user = userSession.loggedInUserImmediate()!!
+            val currentFacility = facilityRepository.currentFacilityImmediate(user)!!
+            SavePrescription(
+                patientUuid = it.patientUuid,
+                name = it.drugName,
+                dosage = it.dosage,
+                facility = currentFacility
+            )
+          }
+          .flatMap { savePrescription ->
+            prescriptionRepository
+                .savePrescription(
+                    patientUuid = savePrescription.patientUuid,
+                    name = savePrescription.name,
+                    dosage = savePrescription.dosage.nullIfBlank(),
+                    rxNormCode = null,
+                    isProtocolDrug = false,
+                    facility = savePrescription.facility
+                )
+                .andThen(Observable.just(CustomPrescriptionSaved))
+          }
+    }
+  }
 }
+
+private data class SavePrescription(
+    val patientUuid: UUID,
+    val name: String,
+    val dosage: String?,
+    val facility: Facility
+)
+
--- app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryEvent.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryEvent.kt
@@ -7,3 +7,13 @@ sealed class CustomPrescriptionEntryEvent : UiEvent
 data class CustomPrescriptionDrugNameTextChanged(val name: String) : CustomPrescriptionEntryEvent() {
   override val analyticsName = "Drugs:Custom:Name Changed"
 }
+
+data class CustomPrescriptionDrugDosageTextChanged(val dosage: String) : CustomPrescriptionEntryEvent() {
+  override val analyticsName = "Drugs:Custom:Dosage Changed"
+}
+
+object SaveCustomPrescriptionClicked : CustomPrescriptionEntryEvent() {
+  override val analyticsName = "Drugs:Custom:Saved"
+}
+
+object CustomPrescriptionSaved : CustomPrescriptionEntryEvent()

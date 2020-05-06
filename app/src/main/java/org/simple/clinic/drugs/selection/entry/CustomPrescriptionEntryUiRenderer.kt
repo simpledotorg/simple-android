fe2f0d5d1a518dae2a73afeac1ef11ead2089a91
Sanchita Agarwal <sanchita@obvious.in>
2020-05-06 19:49:36 +0530

    Move `showDefaultDosagePlaceholder` to Mobius loop

--- app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryUiRenderer.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/CustomPrescriptionEntryUiRenderer.kt
@@ -8,9 +8,30 @@ class CustomPrescriptionEntryUiRenderer(val ui: CustomPrescriptionEntryUi) : Vie
   private val drugNameValueCallback = ValueChangedCallback<Boolean>()
 
   override fun render(model: CustomPrescriptionEntryModel) {
+    showDefaultDosagePlaceholder(model.dosage, model.dosageHasFocus)
+
     if (model.drugName == null) return
 
     val isDrugNameNotBlank = model.drugName.isNotBlank()
     drugNameValueCallback.pass(isDrugNameNotBlank, ui::setSaveButtonEnabled)
+
+  }
+
+  /**
+   * The dosage field shows a default text as "mg". When it is focused, the cursor will
+   * by default be moved to the end. This will force the user to either move the cursor
+   * to the end manually or delete everything and essentially making the placeholder
+   * useless. As a workaround, we move the cursor to the starting again.
+   */
+  private fun showDefaultDosagePlaceholder(dosage: String?, dosageHasFocus: Boolean?) {
+    if (dosageHasFocus == null) return
+
+    when {
+      dosage != null && dosage.trim() == DOSAGE_PLACEHOLDER && dosageHasFocus.not() -> ui.setDrugDosageText("")    //reset
+      dosage.isNullOrBlank() && dosageHasFocus -> {
+        ui.setDrugDosageText(DOSAGE_PLACEHOLDER)
+        ui.moveDrugDosageCursorToBeginning()
+      }
+    }
   }
 }

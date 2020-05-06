be96ee7d6912e1889d12fdc3c0b2f3ebc7dbfa99
Vinay Shenoy <vinay@obvious.in>
2020-05-06 19:03:53 +0530

    Remove `DosagePickerSheetController`

--- app/src/main/java/org/simple/clinic/drugs/selection/dosage/DosagePickerSheet.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/dosage/DosagePickerSheet.kt
@@ -7,25 +7,20 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
-import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.sheet_dosage_picker.*
 import org.simple.clinic.ClinicApp
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
-import org.simple.clinic.bindUiToController
 import org.simple.clinic.di.InjectorProviderContextWrapper
 import org.simple.clinic.drugs.selection.dosage.di.DosagePickerSheetComponent
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.util.LocaleOverrideContextWrapper
-import org.simple.clinic.util.Optional
-import org.simple.clinic.util.toOptional
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.util.wrap
 import org.simple.clinic.widgets.BottomSheetActivity
 import org.simple.clinic.widgets.DividerItemDecorator
 import org.simple.clinic.widgets.ItemAdapter
 import org.simple.clinic.widgets.ScreenCreated
-import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.dp
 import java.util.Locale
@@ -35,17 +30,12 @@ import javax.inject.Inject
 class DosagePickerSheet : BottomSheetActivity(), DosagePickerUi {
 
   @Inject
-  lateinit var controllerFactory: DosagePickerSheetController.Factory
-
-  @Inject
   lateinit var locale: Locale
 
   @Inject
   lateinit var effectHandlerFactory: DosagePickerEffectHandler.Factory
 
   private lateinit var component: DosagePickerSheetComponent
-
-  private val onDestroys = PublishSubject.create<ScreenDestroyed>()
 
   private val dosageAdapter = ItemAdapter(DosageDiffer())
 
@@ -57,7 +47,6 @@ class DosagePickerSheet : BottomSheetActivity(), DosagePickerUi {
             noneClicks()
         )
         .compose(ReportAnalyticsEvents())
-        .share()
   }
 
   private val uiRenderer = DosagePickerUiRenderer(this)
@@ -92,17 +81,6 @@ class DosagePickerSheet : BottomSheetActivity(), DosagePickerUi {
       addItemDecoration(DividerItemDecorator(this@DosagePickerSheet, 24.dp, 24.dp))
     }
     displayDrugName()
-
-    val drugName = intent.getStringExtra(KEY_DRUG_NAME) as String
-    val patientUuid = intent.getSerializableExtra(KEY_PATIENT_UUID) as UUID
-    val prescribedDrugUuid = intent.getSerializableExtra(KEY_PRESCRIBED_DRUG_UUID).toOptional() as Optional<UUID>
-
-    bindUiToController(
-        ui = this,
-        events = events,
-        controller = controllerFactory.create(drugName, patientUuid, prescribedDrugUuid),
-        screenDestroys = onDestroys
-    )
   }
 
   override fun attachBaseContext(baseContext: Context) {
@@ -138,11 +116,6 @@ class DosagePickerSheet : BottomSheetActivity(), DosagePickerUi {
   override fun onSaveInstanceState(outState: Bundle) {
     delegate.onSaveInstanceState(outState)
     super.onSaveInstanceState(outState)
-  }
-
-  override fun onDestroy() {
-    onDestroys.onNext(ScreenDestroyed())
-    super.onDestroy()
   }
 
   private fun sheetCreates(): Observable<UiEvent> {

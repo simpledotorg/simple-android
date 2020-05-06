ef6c28328d875ce1fad08f1a626f22561ed7bfce
Vinay Shenoy <code@vinaysshenoy.com>
2020-05-06 12:27:52 +0530

    Add no-op mobius class for `DosagePickerSheet`

---
+++ app/src/main/java/org/simple/clinic/drugs/selection/dosage/DosagePickerModel.kt
@@ -0,0 +1,7 @@
+package org.simple.clinic.drugs.selection.dosage
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class DosagePickerModel: Parcelable
---
+++ app/src/main/java/org/simple/clinic/drugs/selection/dosage/DosagePickerUiRenderer.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.drugs.selection.dosage
+
+import org.simple.clinic.mobius.ViewRenderer
+
+class DosagePickerUiRenderer(
+    private val ui: DosagePickerUi
+): ViewRenderer<DosagePickerModel> {
+
+  override fun render(model: DosagePickerModel) {
+  }
+}

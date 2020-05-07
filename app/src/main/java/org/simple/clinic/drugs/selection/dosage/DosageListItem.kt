b20b453aa8883470ab1f98d896ae4c47fbcd6d02
Vinay Shenoy <code@vinaysshenoy.com>
2020-05-07 10:50:33 +0530

    Separate dosage list item events from `DosagePickerSheet` events

--- app/src/main/java/org/simple/clinic/drugs/selection/dosage/DosageListItem.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/dosage/DosageListItem.kt
@@ -7,14 +7,11 @@ import kotlinx.android.synthetic.main.prescribed_drug_with_dosage_list_item.*
 import org.simple.clinic.R
 import org.simple.clinic.drugs.selection.dosage.DosageListItem.WithDosage
 import org.simple.clinic.drugs.selection.dosage.DosageListItem.WithoutDosage
-import org.simple.clinic.drugs.selection.dosage.DosageOption.Dosage
-import org.simple.clinic.drugs.selection.dosage.DosageOption.None
 import org.simple.clinic.protocol.ProtocolDrug
 import org.simple.clinic.widgets.ItemAdapter
-import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.recyclerview.ViewHolderX
 
-sealed class DosageListItem(val dosageOption: DosageOption) : ItemAdapter.Item<UiEvent> {
+sealed class DosageListItem : ItemAdapter.Item<DosageListItem.Event> {
 
   companion object {
     fun from(protocolDrugs: List<ProtocolDrug>): List<DosageListItem> {
@@ -24,24 +21,29 @@ sealed class DosageListItem(val dosageOption: DosageOption) : ItemAdapter.Item<U
     }
   }
 
-  data class WithDosage(val protocolDrug: ProtocolDrug) : DosageListItem(Dosage(protocolDrug)) {
+  data class WithDosage(val protocolDrug: ProtocolDrug) : DosageListItem() {
 
     override fun layoutResId() = R.layout.prescribed_drug_with_dosage_list_item
 
-    override fun render(holder: ViewHolderX, subject: Subject<UiEvent>) {
+    override fun render(holder: ViewHolderX, subject: Subject<Event>) {
       holder.dosageTextView.text = protocolDrug.dosage
-      holder.itemView.setOnClickListener { subject.onNext(DosageItemClicked(dosageOption)) }
+      holder.itemView.setOnClickListener { subject.onNext(Event.DosageClicked(protocolDrug)) }
     }
   }
 
-  object WithoutDosage : DosageListItem(None) {
+  object WithoutDosage : DosageListItem() {
 
     override fun layoutResId() = R.layout.prescribed_drug_with_dosage_list_item
 
-    override fun render(holder: ViewHolderX, subject: Subject<UiEvent>) {
+    override fun render(holder: ViewHolderX, subject: Subject<Event>) {
       holder.dosageTextView.text = holder.itemView.context.getString(R.string.prescribed_drugs_dosage_none)
-      holder.itemView.setOnClickListener { subject.onNext(DosageItemClicked(dosageOption)) }
+      holder.itemView.setOnClickListener { subject.onNext(Event.NoneClicked) }
     }
+  }
+
+  sealed class Event {
+    object NoneClicked : Event()
+    data class DosageClicked(val protocolDrug: ProtocolDrug) : Event()
   }
 }
 

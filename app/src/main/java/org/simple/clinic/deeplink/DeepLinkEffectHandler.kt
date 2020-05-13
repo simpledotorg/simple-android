87fac1ba4c243b65ab3f0541df0a83dc2e8290c3
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-05-13 15:00:30 +0530

    Deep link: Handle `NavigateToSetupActivity` effect

--- app/src/main/java/org/simple/clinic/deeplink/DeepLinkEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/deeplink/DeepLinkEffectHandler.kt
@@ -1,20 +1,28 @@
 package org.simple.clinic.deeplink
 
 import com.spotify.mobius.rx2.RxMobius
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
 import dagger.Lazy
 import io.reactivex.ObservableTransformer
 import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.scheduler.SchedulersProvider
-import javax.inject.Inject
 
-class DeepLinkEffectHandler @Inject constructor(
+class DeepLinkEffectHandler @AssistedInject constructor(
     private val userSession: Lazy<UserSession>,
-    private val schedulerProvider: SchedulersProvider
+    private val schedulerProvider: SchedulersProvider,
+    @Assisted private val uiActions: DeepLinkUiActions
 ) {
+
+  @AssistedInject.Factory
+  interface Factory {
+    fun create(uiActions: DeepLinkUiActions): DeepLinkEffectHandler
+  }
 
   fun build(): ObservableTransformer<DeepLinkEffect, DeepLinkEvent> = RxMobius
       .subtypeEffectHandler<DeepLinkEffect, DeepLinkEvent>()
       .addTransformer(FetchUser::class.java, fetchUser())
+      .addAction(NavigateToSetupActivity::class.java, { uiActions.navigateToSetupActivity() }, schedulerProvider.ui())
       .build()
 
   private fun fetchUser(): ObservableTransformer<FetchUser, DeepLinkEvent> {

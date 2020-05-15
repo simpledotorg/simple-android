36a7346fdff965e5c00475992dcd2e795e64a05f
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-05-15 14:25:23 +0530

    Handle `FetchPatient` effect

--- app/src/main/java/org/simple/clinic/deeplink/DeepLinkEvent.kt
+++ app/src/main/java/org/simple/clinic/deeplink/DeepLinkEvent.kt
@@ -1,7 +1,10 @@
 package org.simple.clinic.deeplink
 
+import org.simple.clinic.patient.Patient
 import org.simple.clinic.user.User
 
 sealed class DeepLinkEvent
 
 data class UserFetched(val user: User?) : DeepLinkEvent()
+
+data class PatientFetched(val patient: Patient?) : DeepLinkEvent()
--- app/src/test/java/org/simple/clinic/deeplink/DeepLinkEffectHandlerTest.kt
+++ app/src/test/java/org/simple/clinic/deeplink/DeepLinkEffectHandlerTest.kt
@@ -4,12 +4,14 @@ import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
 import com.nhaarman.mockitokotlin2.whenever
 import dagger.Lazy
 import org.junit.After
 import org.junit.Test
 import org.simple.clinic.TestData
 import org.simple.clinic.mobius.EffectHandlerTestCase
+import org.simple.clinic.patient.PatientRepository
 import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.scheduler.TrampolineSchedulersProvider
 import java.util.UUID
@@ -17,11 +19,13 @@ import java.util.UUID
 class DeepLinkEffectHandlerTest {
 
   private val userSession = mock<UserSession>()
+  private val patientRepository = mock<PatientRepository>()
   private val uiActions = mock<DeepLinkUiActions>()
 
   private val effectHandler = DeepLinkEffectHandler(
       userSession = Lazy { userSession },
       schedulerProvider = TrampolineSchedulersProvider(),
+      patientRepository = patientRepository,
       uiActions = uiActions
   ).build()
   private val testCase = EffectHandlerTestCase(effectHandler)
@@ -68,5 +72,24 @@ class DeepLinkEffectHandlerTest {
     verifyNoMoreInteractions(uiActions)
 
     testCase.assertNoOutgoingEvents()
+  }
+
+  @Test
+  fun `when fetch patient effect is received, then fetch the patient`() {
+    // given
+    val patientUuid = UUID.fromString("4db4acb9-5b0e-459f-b81d-650938029666")
+    val patient = TestData.patient(
+        uuid = patientUuid
+    )
+
+    whenever(patientRepository.patientImmediate(patientUuid)) doReturn patient
+
+    // when
+    testCase.dispatch(FetchPatient(patientUuid))
+
+    // then
+    verifyZeroInteractions(uiActions)
+
+    testCase.assertOutgoingEvents(PatientFetched(patient))
   }
 }

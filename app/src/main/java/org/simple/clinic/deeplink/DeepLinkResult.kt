bc93553ca6412cdc046b79195036a471d4452274
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-05-15 18:45:05 +0530

    Open patient summary from `TheActivity` when deep link is received

---
+++ app/src/main/java/org/simple/clinic/deeplink/DeepLinkResult.kt
@@ -0,0 +1,10 @@
+package org.simple.clinic.deeplink
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.util.UUID
+
+sealed class DeepLinkResult : Parcelable
+
+@Parcelize
+data class OpenPatientSummary(val patientUuid: UUID) : DeepLinkResult()

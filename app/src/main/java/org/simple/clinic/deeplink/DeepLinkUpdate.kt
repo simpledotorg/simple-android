20b2f06afd4ce88b6f7542f5a6b0544937b56100
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-05-15 14:18:15 +0530

    Navigate to MainActivity if there is logged in user but patient uuid is null

--- app/src/main/java/org/simple/clinic/deeplink/DeepLinkUpdate.kt
+++ app/src/main/java/org/simple/clinic/deeplink/DeepLinkUpdate.kt
@@ -7,6 +7,15 @@ import org.simple.clinic.mobius.dispatch
 class DeepLinkUpdate : Update<DeepLinkModel, DeepLinkEvent, DeepLinkEffect> {
 
   override fun update(model: DeepLinkModel, event: DeepLinkEvent): Next<DeepLinkModel, DeepLinkEffect> {
-    return dispatch(NavigateToSetupActivity)
+    return when (event) {
+      is UserFetched -> {
+        val effect = if (event.user == null) {
+          NavigateToSetupActivity
+        } else {
+          NavigateToMainActivity
+        }
+        dispatch(effect)
+      }
+    }
   }
 }
--- app/src/test/java/org/simple/clinic/deeplink/DeepLinkUpdateTest.kt
+++ app/src/test/java/org/simple/clinic/deeplink/DeepLinkUpdateTest.kt
@@ -5,12 +5,17 @@ import com.spotify.mobius.test.NextMatchers.hasNoModel
 import com.spotify.mobius.test.UpdateSpec
 import com.spotify.mobius.test.UpdateSpec.assertThatNext
 import org.junit.Test
+import org.simple.clinic.TestData
+import org.simple.clinic.user.User
+import java.util.UUID
 
 class DeepLinkUpdateTest {
 
+  private val updateSpec = UpdateSpec(DeepLinkUpdate())
+  private val patientUuid = UUID.fromString("f88cc05b-620a-490a-92f3-1c0c43fb76ab")
+
   @Test
   fun `if there is no user logged in, then open setup activity`() {
-    val updateSpec = UpdateSpec(DeepLinkUpdate())
     val defaultModel = DeepLinkModel.default()
 
     updateSpec
@@ -19,6 +24,23 @@ class DeepLinkUpdateTest {
         .then(assertThatNext(
             hasNoModel(),
             hasEffects(NavigateToSetupActivity as DeepLinkEffect)
+        ))
+  }
+
+  @Test
+  fun `if there is a logged in user and patient uuid is null, then navigate to main activity`() {
+    val model = DeepLinkModel.default()
+    val user = TestData.loggedInUser(
+        uuid = UUID.fromString("dc0a9d11-aee4-4792-820f-c5cb66ae5e47"),
+        loggedInStatus = User.LoggedInStatus.LOGGED_IN
+    )
+
+    updateSpec
+        .given(model)
+        .whenEvent(UserFetched(user))
+        .then(assertThatNext(
+            hasNoModel(),
+            hasEffects(NavigateToMainActivity as DeepLinkEffect)
         ))
   }
 }

c8ff85b5034edd78af07e4ecc35fe6a96467524f
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-05-18 08:58:48 +0530

    Check if teleconsultation is enabled from updated model
    Previously we were using the event to get the facility config to check if teleconsultation is enabled or not. Since we are already updating the model before that, we can now instead use the model directly to check is teleconsultation is enabled or not using `updatedModel.isTeleconsultationEnabled`

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryUpdate.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryUpdate.kt
@@ -66,7 +66,7 @@ class PatientSummaryUpdate : Update<PatientSummaryModel, PatientSummaryEvent, Pa
   private fun currentFacilityLoaded(model: PatientSummaryModel, event: CurrentFacilityLoaded): Next<PatientSummaryModel, PatientSummaryEffect> {
     val updatedModel = model.currentFacilityLoaded(event.facility)
 
-    return if (event.facility.config.teleconsultationEnabled == true && model.isUserLoggedIn) {
+    return if (updatedModel.isTeleconsultationEnabled && model.isUserLoggedIn) {
       next(
           updatedModel.fetchingTeleconsultationInfo(),
           FetchTeleconsultationInfo(event.facility.uuid)

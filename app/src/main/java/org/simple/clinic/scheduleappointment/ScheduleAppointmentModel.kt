9a4151728f02d37d0be4ded1abf046b780ca3127
Vinay Shenoy <vinay@obvious.in>
2020-05-18 12:42:15 +0530

    Move generation of potential appointment dates to `ScheduleAppointmentModel`

--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentModel.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentModel.kt
@@ -2,11 +2,32 @@ package org.simple.clinic.scheduleappointment
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.overdue.PotentialAppointmentDate
+import org.simple.clinic.overdue.TimeToAppointment
+import org.simple.clinic.util.UserClock
 
 @Parcelize
-class ScheduleAppointmentModel : Parcelable {
+class ScheduleAppointmentModel(
+    val potentialAppointmentDates: List<PotentialAppointmentDate>
+) : Parcelable {
 
   companion object {
-    fun create(): ScheduleAppointmentModel = ScheduleAppointmentModel()
+    fun create(
+        timeToAppointments: List<TimeToAppointment>,
+        userClock: UserClock
+    ): ScheduleAppointmentModel {
+      return ScheduleAppointmentModel(
+          potentialAppointmentDates = generatePotentialAppointmentDatesForScheduling(timeToAppointments, userClock)
+      )
+    }
+
+    private fun generatePotentialAppointmentDatesForScheduling(
+        timeToAppointments: List<TimeToAppointment>,
+        clock: UserClock
+    ): List<PotentialAppointmentDate> {
+      return PotentialAppointmentDate.from(timeToAppointments, clock)
+          .distinctBy(PotentialAppointmentDate::scheduledFor)
+          .sorted()
+    }
   }
 }

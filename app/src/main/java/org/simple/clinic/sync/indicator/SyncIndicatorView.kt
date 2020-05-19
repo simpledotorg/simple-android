fae9b66b1d50b20a348abce597006ddf24becc87
Sanchita Agarwal <sanchita@obvious.in>
2020-05-19 19:26:43 +0530

    Use synthetic accessors in `SyncIndicatorView`

--- app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorView.kt
+++ app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorView.kt
@@ -5,14 +5,13 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
-import kotterknife.bindView
+import kotlinx.android.synthetic.main.sync_indicator.view.*
 import org.simple.clinic.R
 import org.simple.clinic.bindUiToController
 import org.simple.clinic.main.TheActivity
@@ -32,9 +31,6 @@ import org.simple.clinic.widgets.setCompoundDrawableStart
 import javax.inject.Inject
 
 class SyncIndicatorView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs), SyncIndicatorUi {
-
-  private val syncStatusTextView by bindView<TextView>(R.id.sync_indicator_status_text)
-  private val syncIndicatorLayout by bindView<LinearLayout>(R.id.sync_indicator_root_layout)
 
   @Inject
   lateinit var controller: SyncIndicatorViewController
@@ -60,37 +56,37 @@ class SyncIndicatorView(context: Context, attrs: AttributeSet) : LinearLayout(co
 
   private fun screenCreates() = Observable.just(ScreenCreated())
 
-  private fun viewClicks() = RxView.clicks(syncIndicatorLayout).map { SyncIndicatorViewClicked }
+  private fun viewClicks() = RxView.clicks(rootLayout).map { SyncIndicatorViewClicked }
 
   @SuppressLint("StringFormatMatches")
   override fun updateState(syncState: SyncIndicatorState) {
     val transition = AutoTransition().setInterpolator(FastOutSlowInInterpolator())
     TransitionManager.beginDelayedTransition(this, transition)
 
-    syncIndicatorLayout.isEnabled = true
+    rootLayout.isEnabled = true
 
     when (syncState) {
       ConnectToSync -> {
-        syncStatusTextView.text = context.getString(R.string.syncindicator_status_failed)
-        syncStatusTextView.setCompoundDrawableStart(R.drawable.ic_round_warning_16px)
+        statusTextView.text = context.getString(R.string.syncindicator_status_failed)
+        statusTextView.setCompoundDrawableStart(R.drawable.ic_round_warning_16px)
       }
       is Synced -> {
         val durationToMinsAgo = syncState.durationSince.toMinutes().toInt()
-        syncStatusTextView.text = if (durationToMinsAgo == 0) {
+        statusTextView.text = if (durationToMinsAgo == 0) {
           context.getString(R.string.syncindicator_status_synced_just_now)
         } else {
           context.getString(R.string.syncindicator_status_synced_min_ago, "$durationToMinsAgo")
         }
-        syncStatusTextView.setCompoundDrawableStart(R.drawable.ic_cloud_done_16dp)
+        statusTextView.setCompoundDrawableStart(R.drawable.ic_cloud_done_16dp)
       }
       SyncPending -> {
-        syncStatusTextView.text = context.getString(R.string.syncindicator_status_pending)
-        syncStatusTextView.setCompoundDrawableStart(R.drawable.ic_cloud_upload_16dp)
+        statusTextView.text = context.getString(R.string.syncindicator_status_pending)
+        statusTextView.setCompoundDrawableStart(R.drawable.ic_cloud_upload_16dp)
       }
       Syncing -> {
-        syncStatusTextView.text = context.getString(R.string.syncindicator_status_syncing)
-        syncStatusTextView.setCompoundDrawableStart(R.drawable.ic_round_sync_16px)
-        syncIndicatorLayout.isEnabled = false
+        statusTextView.text = context.getString(R.string.syncindicator_status_syncing)
+        statusTextView.setCompoundDrawableStart(R.drawable.ic_round_sync_16px)
+        rootLayout.isEnabled = false
       }
     }
   }

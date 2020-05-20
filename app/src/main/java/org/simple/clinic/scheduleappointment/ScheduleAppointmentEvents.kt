b17d618d4056e265f32dc2285959c5988ad54ea2
Vinay Shenoy <code@vinaysshenoy.com>
2020-05-20 17:15:18 +0530

    Move scheduling of manual appointments to Mobius

--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentEvents.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentEvents.kt
@@ -2,10 +2,6 @@ package org.simple.clinic.scheduleappointment
 
 import org.simple.clinic.widgets.UiEvent
 
-object AppointmentDone : UiEvent {
-  override val analyticsName = "Schedule Appointment:Appointment done"
-}
-
 object SchedulingSkipped : UiEvent {
   override val analyticsName = "Schedule Appointment:Scheduling skipped"
 }
--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentUi.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentUi.kt
@@ -4,7 +4,6 @@ import org.simple.clinic.overdue.TimeToAppointment
 import org.threeten.bp.LocalDate
 
 interface ScheduleAppointmentUi : ScheduleAppointmentUiActions {
-  fun closeSheet()
   fun updateScheduledAppointment(appointmentDate: LocalDate, timeToAppointment: TimeToAppointment)
   fun enableIncrementButton(state: Boolean)
   fun enableDecrementButton(state: Boolean)

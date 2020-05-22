d2c44765ef88426b3b3c0f787c4697f0aa337001
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-05-22 12:32:09 +0530

    Handle `ShowConfirmDiedDialog` effect

--- app/src/main/java/org/simple/clinic/editpatient/deletepatient/DeletePatientEffect.kt
+++ app/src/main/java/org/simple/clinic/editpatient/deletepatient/DeletePatientEffect.kt
@@ -5,3 +5,5 @@ import org.simple.clinic.patient.DeletedReason
 sealed class DeletePatientEffect
 
 data class ShowConfirmDeleteDialog(val patientName: String, val deletedReason: DeletedReason) : DeletePatientEffect()
+
+data class ShowConfirmDiedDialog(val patientName: String) : DeletePatientEffect()

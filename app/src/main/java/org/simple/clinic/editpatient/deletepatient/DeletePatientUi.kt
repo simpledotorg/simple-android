1eea98f06718f9b1495964b381581576c26a2a1e
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-05-26 15:35:28 +0530

    Implement `DeletePatientViewRenderer`

---
+++ app/src/main/java/org/simple/clinic/editpatient/deletepatient/DeletePatientUi.kt
@@ -0,0 +1,5 @@
+package org.simple.clinic.editpatient.deletepatient
+
+interface DeletePatientUi {
+  fun showDeleteReasons(patientDeleteReasons: List<PatientDeleteReason>, selectedReason: PatientDeleteReason?)
+}

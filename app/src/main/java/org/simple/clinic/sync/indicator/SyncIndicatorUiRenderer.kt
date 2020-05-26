423d243ebb966e13a5af2ef9ff6111fa94a1a0ed
Sanchita Agarwal <sanchita@obvious.in>
2020-05-26 17:41:09 +0530

    Update state of sync indicator based on `lastSyncState`

--- app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorUiRenderer.kt
+++ app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorUiRenderer.kt
@@ -1,9 +1,19 @@
 package org.simple.clinic.sync.indicator
 
 import org.simple.clinic.mobius.ViewRenderer
+import org.simple.clinic.sync.indicator.SyncIndicatorState.SyncPending
 
-class SyncIndicatorUiRenderer : ViewRenderer<SyncIndicatorModel> {
+class SyncIndicatorUiRenderer(private val ui: SyncIndicatorUi) : ViewRenderer<SyncIndicatorModel> {
   override fun render(model: SyncIndicatorModel) {
-    //TODO: implement later
+    if (model.lastSyncedState == null)
+      return
+
+    if (model.lastSyncedState.isEmpty()) {
+      ui.updateState(SyncPending)
+    }
+
+    if (model.syncIndicatorState != null) {
+      ui.updateState(model.syncIndicatorState)
+    }
   }
 }
--- app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorViewController.kt
+++ app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorViewController.kt
@@ -4,33 +4,22 @@ import com.f2prateek.rx.preferences2.Preference
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
-import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.combineLatest
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.rxkotlin.withLatestFrom
 import org.simple.clinic.ReplayUntilScreenIsDestroyed
-import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.sync.DataSync
 import org.simple.clinic.sync.LastSyncedState
 import org.simple.clinic.sync.SyncGroup.FREQUENT
-import org.simple.clinic.sync.SyncInterval
-import org.simple.clinic.sync.SyncProgress.FAILURE
-import org.simple.clinic.sync.SyncProgress.SUCCESS
 import org.simple.clinic.sync.SyncProgress.SYNCING
 import org.simple.clinic.sync.SynceableRepository
-import org.simple.clinic.sync.indicator.SyncIndicatorState.ConnectToSync
 import org.simple.clinic.sync.indicator.SyncIndicatorState.SyncPending
-import org.simple.clinic.sync.indicator.SyncIndicatorState.Synced
-import org.simple.clinic.sync.indicator.SyncIndicatorState.Syncing
 import org.simple.clinic.util.ResolvedError.NetworkRelated
 import org.simple.clinic.util.ResolvedError.ServerError
 import org.simple.clinic.util.ResolvedError.Unexpected
 import org.simple.clinic.util.UtcClock
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
-import org.threeten.bp.Duration
-import org.threeten.bp.Instant
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -56,66 +45,8 @@ class SyncIndicatorViewController @Inject constructor(
         .replay()
 
     return Observable.merge(
-        updateIndicatorView(replayedEvents),
         startSync(replayedEvents),
         showPendingSyncStatus(replayedEvents))
-  }
-
-  private fun updateIndicatorView(events: Observable<UiEvent>): Observable<UiChange> {
-    val screenCreated = events.ofType<ScreenCreated>()
-    val lastSyncedStateStream = lastSyncState
-        .asObservable()
-        .distinctUntilChanged()
-
-    val showDefaultSyncIndicatorState = lastSyncedStateStream
-        .filter { it.lastSyncProgress == null }
-        .map { { ui: Ui -> ui.updateState(SyncPending) } }
-
-    val syncProgress = lastSyncedStateStream.filter { it.lastSyncProgress != null }
-
-    val showSyncIndicatorState = Observables
-        .combineLatest(screenCreated, syncProgress, configProvider)
-        { _, stateStream, config ->
-          when (stateStream.lastSyncProgress!!) {
-            SUCCESS, FAILURE -> syncIndicatorState(stateStream, config.syncFailureThreshold)
-            SYNCING -> Syncing
-          }
-        }
-        .switchMap {
-          if (it is Synced) {
-            val durationSince = it.durationSince
-            Observable.interval(1, TimeUnit.MINUTES)
-                .map { min -> { ui: Ui -> ui.updateState(Synced(durationSince.plusMinutes(min))) } }
-                .startWith { ui: Ui -> ui.updateState(Synced(durationSince)) }
-          } else {
-            Observable.just { ui: Ui -> ui.updateState(it) }
-          }
-        }
-
-    return showSyncIndicatorState.mergeWith(showDefaultSyncIndicatorState)
-  }
-
-  private fun syncIndicatorState(syncState: LastSyncedState, maxIntervalSinceLastSync: Duration): SyncIndicatorState {
-    val timestamp = syncState.lastSyncSucceededAt ?: return SyncPending
-
-    val now = Instant.now(utcClock)
-    val timeSinceLastSync = Duration.between(timestamp, now)
-    val mostFrequentSyncInterval = enumValues<SyncInterval>()
-        .map { it.frequency }
-        .min()!!
-
-    val syncHappenedInTheFuture = timeSinceLastSync.isNegative
-
-    return when {
-      timeSinceLastSync > maxIntervalSinceLastSync -> ConnectToSync
-      timeSinceLastSync > mostFrequentSyncInterval -> SyncPending
-      syncHappenedInTheFuture -> SyncPending
-      else -> when (syncState.lastSyncProgress!!) {
-        SUCCESS -> Synced(timeSinceLastSync)
-        FAILURE -> SyncPending
-        SYNCING -> Syncing
-      }
-    }
   }
 
   private fun startSync(events: Observable<UiEvent>): Observable<UiChange> {

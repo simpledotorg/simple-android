ca3f6444b4b6b61f70d3152697adc41cb71062f2
Vinay Shenoy <vinay@obvious.in>
2020-05-26 11:21:16 +0530

    Remove redundant error reporting in remote config sync
    We were reporting the error in exactly the same fashion in `DataSync`,
    `RemoteConfigSync`, and `FirebaseRemoteConfigService`. All of this has
    been removed now and error reporting will be done only in `DataSync`.

--- app/src/main/java/org/simple/clinic/remoteconfig/RemoteConfigModule.kt
+++ app/src/main/java/org/simple/clinic/remoteconfig/RemoteConfigModule.kt
@@ -3,21 +3,16 @@ package org.simple.clinic.remoteconfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import dagger.Module
 import dagger.Provides
-import org.simple.clinic.platform.crash.CrashReporter
 import org.simple.clinic.remoteconfig.firebase.FirebaseRemoteConfigService
 
 @Module
-class RemoteConfigModule {
+  class RemoteConfigModule {
 
   @Provides
   fun provideRemoteConfigService(
-      firebaseRemoteConfig: FirebaseRemoteConfig,
-      // This is marked as Lazy to avoid a cyclic dependency between FirebaseRemoteConfigService and
-      // SentryCrashReporter (which happens only in release builds).
-      // TODO(vs): 2020-01-17 Change CrashReporter to be something like Timber, which can live outside the DI graph
-      crashReporter: dagger.Lazy<CrashReporter>
+      firebaseRemoteConfig: FirebaseRemoteConfig
   ): RemoteConfigService {
-    return FirebaseRemoteConfigService(firebaseRemoteConfig, crashReporter)
+    return FirebaseRemoteConfigService(firebaseRemoteConfig)
   }
 
   @Provides

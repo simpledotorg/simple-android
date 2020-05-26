2d42ed7050065112e32b9c1cceb50b4dea21cdfe
Sanchita Agarwal <sanchita@obvious.in>
2020-05-26 21:53:12 +0530

    Migrate `showPendingSyncStatus` to Mobius loop

--- app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorViewController.kt
+++ app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorViewController.kt
@@ -4,15 +4,11 @@ import com.f2prateek.rx.preferences2.Preference
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
-import io.reactivex.rxkotlin.combineLatest
-import io.reactivex.rxkotlin.ofType
 import org.simple.clinic.ReplayUntilScreenIsDestroyed
 import org.simple.clinic.sync.DataSync
 import org.simple.clinic.sync.LastSyncedState
 import org.simple.clinic.sync.SynceableRepository
-import org.simple.clinic.sync.indicator.SyncIndicatorState.SyncPending
 import org.simple.clinic.util.UtcClock
-import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
 import javax.inject.Inject
 import javax.inject.Named
@@ -27,27 +23,11 @@ class SyncIndicatorViewController @Inject constructor(
     private val dataSync: DataSync,
     @Named("frequently_syncing_repositories") private val frequentlySyncingRepositories: ArrayList<SynceableRepository<*, *>>
 ) : ObservableTransformer<UiEvent, UiChange> {
-  
+
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
     val replayedEvents = ReplayUntilScreenIsDestroyed(events)
         .replay()
 
-    return showPendingSyncStatus(replayedEvents)
-  }
-
-  private fun showPendingSyncStatus(events: Observable<UiEvent>): Observable<UiChange> {
-    val screenCreates = events.ofType<ScreenCreated>()
-
-    return screenCreates
-        .flatMap {
-          val recordCounts = frequentlySyncingRepositories
-              .map { it.pendingSyncRecordCount() }
-
-          recordCounts
-              .combineLatest { counts -> counts.any { it > 0 } }
-              .filter { isSyncPending -> isSyncPending }
-              .distinctUntilChanged()
-              .map { { ui: Ui -> ui.updateState(SyncPending) } }
-        }
+    return Observable.empty()
   }
 }

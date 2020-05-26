61a04ba337b0f0838e82979ffd0efdc6ff1beefd
Vinay Shenoy <vinay@obvious.in>
2020-05-26 12:07:25 +0530

    Migrate opening of patient search screen to Mobius

--- app/src/main/java/org/simple/clinic/home/patients/PatientsEvent.kt
+++ app/src/main/java/org/simple/clinic/home/patients/PatientsEvent.kt
@@ -7,3 +7,7 @@ sealed class PatientsEvent : UiEvent
 class PatientsEnterCodeManuallyClicked : PatientsEvent() {
   override val analyticsName = "Patients:Enter Code Manually Clicked"
 }
+
+object NewPatientClicked : PatientsEvent() {
+  override val analyticsName = "Patients:Search For Patient Clicked"
+}

dff23bcff83ee6341046c2a66d1487ca1598b9b8
Sanchita Agarwal <sanchita@obvious.in>
2020-05-26 17:05:52 +0530

    Add `SyncIndicatorState` to `SyncIndicatorModel`

--- app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorModel.kt
+++ app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorModel.kt
@@ -5,15 +5,22 @@ import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.sync.LastSyncedState
 
 @Parcelize
-data class SyncIndicatorModel(val lastSyncedState: LastSyncedState?) : Parcelable {
+data class SyncIndicatorModel(
+    val lastSyncedState: LastSyncedState?,
+    val syncIndicatorState: SyncIndicatorState?
+) : Parcelable {
 
   companion object {
     fun create(): SyncIndicatorModel {
-      return SyncIndicatorModel(null)
+      return SyncIndicatorModel(null, null)
     }
   }
 
   fun lastSyncedStateChanged(newState: LastSyncedState): SyncIndicatorModel {
     return copy(lastSyncedState = newState)
+  }
+
+  fun syncIndicatorStateChanged(newState: SyncIndicatorState): SyncIndicatorModel {
+    return copy(syncIndicatorState = newState)
   }
 }

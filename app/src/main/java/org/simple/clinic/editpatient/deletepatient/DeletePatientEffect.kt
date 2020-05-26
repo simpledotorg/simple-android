43e9c4e7762bab1bc0feee61d4647a6d4db110cf
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-05-26 10:11:26 +0530

    Handle `DeletePatient` effect

--- app/src/main/java/org/simple/clinic/editpatient/deletepatient/DeletePatientEffect.kt
+++ app/src/main/java/org/simple/clinic/editpatient/deletepatient/DeletePatientEffect.kt
@@ -1,9 +1,12 @@
 package org.simple.clinic.editpatient.deletepatient
 
 import org.simple.clinic.patient.DeletedReason
+import java.util.UUID
 
 sealed class DeletePatientEffect
 
 data class ShowConfirmDeleteDialog(val patientName: String, val deletedReason: DeletedReason) : DeletePatientEffect()
 
 data class ShowConfirmDiedDialog(val patientName: String) : DeletePatientEffect()
+
+data class DeletePatient(val patientUuid: UUID, val deletedReason: DeletedReason) : DeletePatientEffect()

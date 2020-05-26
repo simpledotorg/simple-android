c24debaf5f31d8f78fe3cf3000c84028bd6555e4
Sanchita Agarwal <sanchita@obvious.in>
2020-05-26 17:05:52 +0530

    Add `SyncIndicatorState` to `SyncIndicatorModel`

--- app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorState.kt
+++ app/src/main/java/org/simple/clinic/sync/indicator/SyncIndicatorState.kt
@@ -1,10 +1,12 @@
 package org.simple.clinic.sync.indicator
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import org.threeten.bp.Duration
 
-sealed class SyncIndicatorState {
-  object ConnectToSync : SyncIndicatorState()
-  data class Synced(val durationSince: Duration) : SyncIndicatorState()
-  object SyncPending : SyncIndicatorState()
-  object Syncing : SyncIndicatorState()
+sealed class SyncIndicatorState : Parcelable{
+  @Parcelize object ConnectToSync : SyncIndicatorState()
+  @Parcelize data class Synced(val durationSince: Duration) : SyncIndicatorState()
+  @Parcelize object SyncPending : SyncIndicatorState()
+  @Parcelize object Syncing : SyncIndicatorState()
 }

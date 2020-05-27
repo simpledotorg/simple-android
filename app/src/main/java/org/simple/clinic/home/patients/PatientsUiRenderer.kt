a07c65c354f06616d491f41c9c0342d013a044c3
Vinay Shenoy <code@vinaysshenoy.com>
2020-05-27 11:53:13 +0530

    Move toggling sync indicator visibility to Mobius

--- app/src/main/java/org/simple/clinic/home/patients/PatientsUiRenderer.kt
+++ app/src/main/java/org/simple/clinic/home/patients/PatientsUiRenderer.kt
@@ -1,10 +1,26 @@
 package org.simple.clinic.home.patients
 
 import org.simple.clinic.mobius.ViewRenderer
+import org.simple.clinic.user.User
+import org.simple.clinic.util.ValueChangedCallback
 
 class PatientsUiRenderer(private val ui: PatientsUi) : ViewRenderer<PatientsModel> {
 
+  private val userChangedCallback = ValueChangedCallback<User>()
+
   override fun render(model: PatientsModel) {
-    // Nothing to do here yet
+    if (model.hasLoadedUser) {
+      userChangedCallback.pass(model.user!!) { user ->
+        renderSyncIndicatorVisibility(user)
+      }
+    }
+  }
+
+  private fun renderSyncIndicatorVisibility(user: User) {
+    if (user.canSyncData) {
+      ui.showSyncIndicator()
+    } else {
+      ui.hideSyncIndicator()
+    }
   }
 }

bb9cce75410df4b807ac2885d7ad46938c5d521a
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-05-27 11:36:39 +0530

    Show delete button in `EditPatientScreen`

---
+++ app/src/main/java/org/simple/clinic/editpatient/EditPatientConfig.kt
@@ -0,0 +1,14 @@
+package org.simple.clinic.editpatient
+
+import org.simple.clinic.remoteconfig.ConfigReader
+import javax.inject.Inject
+
+data class EditPatientConfig(
+    val deletePatientFeatureEnabled: Boolean
+) {
+
+  @Inject
+  constructor(configReader: ConfigReader) : this(
+      deletePatientFeatureEnabled = configReader.boolean("delete_patient_feature_enabled", false)
+  )
+}

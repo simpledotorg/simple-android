9b5b1bcc508072bf1d2b9a844f9eb6bd211c2f5c
Vinay Shenoy <vinay@obvious.in>
2020-06-04 11:07:49 +0530

    Track current ongoing registration entry in `RegistrationPhoneModel`
    Since the UI interface requires us to pass the `OngoingRegistrationEntry`,
    we need to track the entry as well. Ideally, we could just track the
    phone number since that is what is needed, but that would require changes
    that are unrelated to the migration.
    
    This is something that can be picked up at a later date.

--- app/src/main/java/org/simple/clinic/registration/phone/RegistrationPhoneScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/registration/phone/RegistrationPhoneScreenEvents.kt
@@ -2,10 +2,6 @@ package org.simple.clinic.registration.phone
 
 import org.simple.clinic.widgets.UiEvent
 
-data class RegistrationPhoneNumberTextChanged(val phoneNumber: String) : UiEvent {
-  override val analyticsName = "Registration:Phone Entry:Phone Number Text Changed"
-}
-
 class RegistrationPhoneDoneClicked : UiEvent {
   override val analyticsName = "Registration:Phone Entry:Done Clicked"
 }
--- app/src/main/java/org/simple/clinic/registration/phone/RegistrationPhoneUpdate.kt
+++ app/src/main/java/org/simple/clinic/registration/phone/RegistrationPhoneUpdate.kt
@@ -1,8 +1,8 @@
 package org.simple.clinic.registration.phone
 
 import com.spotify.mobius.Next
-import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
+import org.simple.clinic.mobius.next
 
 class RegistrationPhoneUpdate : Update<RegistrationPhoneModel, RegistrationPhoneEvent, RegistrationPhoneEffect> {
 
@@ -10,6 +10,8 @@ class RegistrationPhoneUpdate : Update<RegistrationPhoneModel, RegistrationPhone
       model: RegistrationPhoneModel,
       event: RegistrationPhoneEvent
   ): Next<RegistrationPhoneModel, RegistrationPhoneEffect> {
-    return noChange()
+    return when(event) {
+      is RegistrationPhoneNumberTextChanged -> next(model.phoneNumberChanged(event.phoneNumber))
+    }
   }
 }

10f0abfb4ef532cd3dea18f81bcc88d3ee68fa0c
Vinay Shenoy <vinay@obvious.in>
2020-06-09 09:22:47 +0530

    Add `RegistrationNameUiActions` interface to represent one-off actions

--- app/src/main/java/org/simple/clinic/registration/name/RegistrationNameUi.kt
+++ app/src/main/java/org/simple/clinic/registration/name/RegistrationNameUi.kt
@@ -2,7 +2,7 @@ package org.simple.clinic.registration.name
 
 import org.simple.clinic.user.OngoingRegistrationEntry
 
-interface RegistrationNameUi {
+interface RegistrationNameUi: RegistrationNameUiActions {
   fun preFillUserDetails(ongoingEntry: OngoingRegistrationEntry)
   fun showEmptyNameValidationError()
   fun hideValidationError()

8944d11b771956c704d292f18a99c76dce689789
Vinay Shenoy <code@vinaysshenoy.com>
2020-06-10 12:00:47 +0530

    Extract `Optional#isNotEmpty` to an extension function
    This commit also adds a deprecation notice to the function to use
    the `Optional#isPresent` method instead.

--- app/src/main/java/org/simple/clinic/newentry/PatientEntryModel.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryModel.kt
@@ -7,6 +7,7 @@ import org.simple.clinic.patient.OngoingNewPatientEntry
 import org.simple.clinic.patient.ReminderConsent
 import org.simple.clinic.patient.businessid.Identifier
 import org.simple.clinic.util.Optional
+import org.simple.clinic.util.isNotEmpty
 
 @Parcelize
 data class PatientEntryModel(

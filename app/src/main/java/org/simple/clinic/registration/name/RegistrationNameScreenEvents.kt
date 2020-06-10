2c04f6b930f328a2da058476c042a392bb2dae4e
Vinay Shenoy <code@vinaysshenoy.com>
2020-06-10 14:51:28 +0530

    Keep entered name updated in `RegistrationNameModel`

--- app/src/main/java/org/simple/clinic/registration/name/RegistrationNameScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/registration/name/RegistrationNameScreenEvents.kt
@@ -4,10 +4,6 @@ import org.simple.clinic.widgets.UiEvent
 
 class RegistrationFullNameScreenCreated : UiEvent
 
-data class RegistrationFullNameTextChanged(val fullName: String) : UiEvent {
-  override val analyticsName = "Registration:Name Entry:Name Text Changed"
-}
-
 class RegistrationFullNameDoneClicked : UiEvent {
   override val analyticsName = "Registration:Name Entry:Done Clicked"
 }

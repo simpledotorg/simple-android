327b2bca383f0bd49173e78f1381b981b350b82a
Vinay Shenoy <vinay@obvious.in>
2020-06-10 12:23:45 +0530

    Deprecate extension functions on `Observable<Optional>` types

--- app/src/main/java/org/simple/clinic/util/RxOptional.kt
+++ app/src/main/java/org/simple/clinic/util/RxOptional.kt
@@ -3,19 +3,46 @@ package org.simple.clinic.util
 import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
-import io.reactivex.rxkotlin.ofType
 
+@Deprecated(
+    message = "Use `extractIfPresent()` instead",
+    replaceWith = ReplaceWith(
+        expression = "extractIfPresent()",
+        imports = ["org.simple.clinic.util.extractIfPresent"]
+    )
+)
 fun <T : Any> Observable<Optional<T>>.filterAndUnwrapJust(): Observable<T> {
-  return ofType<Just<T>>()
-      .map { (value) -> value }
+  return extractIfPresent()
 }
 
+fun <T : Any> Observable<Optional<T>>.extractIfPresent(): Observable<T> {
+  return filter(Optional<T>::isPresent)
+      .map(Optional<T>::get)
+}
+
+@Deprecated(
+    message = "Use `Optional.get()` instead",
+    replaceWith = ReplaceWith(
+        expression = "map { it.get() }",
+        imports = ["org.simple.clinic.util.extractIfPresent"]
+    )
+)
 fun <T : Any> Observable<Optional<T>>.unwrapJust(): Observable<T> {
-  return map { it as Just }
-      .map { (value) -> value }
+  return map(Optional<T>::get)
 }
 
+@Deprecated(
+    message = "Use `extractIfPresent()` instead",
+    replaceWith = ReplaceWith(
+        expression = "extractIfPresent()",
+        imports = ["org.simple.clinic.util.extractIfPresent"]
+    )
+)
 fun <T : Any> Single<Optional<T>>.filterAndUnwrapJust(): Maybe<T> {
-  return ofType<Just<T>>()
-      .map { (value) -> value }
+  return extractIfPresent()
+}
+
+fun <T : Any> Single<Optional<T>>.extractIfPresent(): Maybe<T> {
+  return filter(Optional<T>::isPresent)
+      .map(Optional<T>::get)
 }

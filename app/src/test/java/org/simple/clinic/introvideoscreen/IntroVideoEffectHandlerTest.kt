1103949a078443a52cf241081542e78bd8e4b07b
Honey Sonwani <honey@obvious.in>
2020-06-11 14:26:46 +0530

    Handle `OpenVideo` effect

---
+++ app/src/test/java/org/simple/clinic/introvideoscreen/IntroVideoEffectHandlerTest.kt
@@ -0,0 +1,39 @@
+package org.simple.clinic.introvideoscreen
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.junit.After
+import org.junit.Test
+import org.simple.clinic.mobius.EffectHandlerTestCase
+import org.simple.clinic.util.scheduler.SchedulersProvider
+import org.simple.clinic.util.scheduler.TrampolineSchedulersProvider
+
+class IntroVideoEffectHandlerTest {
+
+  private val uiActions = mock<UiActions>()
+  private val effectHandler = IntroVideoEffectHandler(
+      uiActions = uiActions,
+      schedulersProvider = TrampolineSchedulersProvider()
+  ).build()
+
+  private val effectHandlerTestCase = EffectHandlerTestCase(effectHandler)
+
+  @Test
+  fun `when open video effect is received, then open the video`() {
+    // when
+    effectHandlerTestCase.dispatch(OpenVideo)
+
+    // then
+    effectHandlerTestCase.assertNoOutgoingEvents()
+
+    verify(uiActions).openVideo()
+    verifyNoMoreInteractions(uiActions)
+  }
+
+
+  @After
+  fun teardown() {
+    effectHandlerTestCase.dispose()
+  }
+}

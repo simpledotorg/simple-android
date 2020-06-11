8e641a78aeac02b1c7755ae3dd24669417465c0a
Honey Sonwani <honey@obvious.in>
2020-06-11 14:53:56 +0530

    When intro video is clicked then open video and home screen

--- app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoUpdate.kt
+++ app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoUpdate.kt
@@ -1,10 +1,14 @@
 package org.simple.clinic.introvideoscreen
 
 import com.spotify.mobius.Next
-import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
+import org.simple.clinic.mobius.dispatch
 
 class IntroVideoUpdate : Update<IntroVideoModel, IntroVideoEvent, IntroVideoEffect> {
   override fun update(model: IntroVideoModel, event: IntroVideoEvent): Next<IntroVideoModel,
-      IntroVideoEffect> = noChange()
+      IntroVideoEffect> {
+    return when (event) {
+      VideoClicked -> dispatch(OpenVideo, OpenHome)
+    }
+  }
 }

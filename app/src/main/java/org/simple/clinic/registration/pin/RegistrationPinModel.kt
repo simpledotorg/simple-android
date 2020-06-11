06d4ebc4524b78066ca76ff71e8d7a564746e2f8
Vinay Shenoy <vinay@obvious.in>
2020-06-11 12:11:51 +0530

    Add no-op implementations of Mobius classes

---
+++ app/src/main/java/org/simple/clinic/registration/pin/RegistrationPinModel.kt
@@ -0,0 +1,7 @@
+package org.simple.clinic.registration.pin
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class RegistrationPinModel: Parcelable
---
+++ app/src/main/java/org/simple/clinic/registration/pin/RegistrationPinUiRenderer.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.registration.pin
+
+import org.simple.clinic.mobius.ViewRenderer
+
+class RegistrationPinUiRenderer(
+    private val ui: RegistrationPinUi
+) : ViewRenderer<RegistrationPinModel> {
+
+  override fun render(model: RegistrationPinModel) {
+
+  }
+}

4041be6b859b23694cc3d41d9636dff2b1d5e7e7
Vinay Shenoy <vinay@obvious.in>
2020-06-11 13:40:19 +0530

    Keep entered PIN updated in `RegistrationPinModel`

--- app/src/main/java/org/simple/clinic/registration/pin/RegistrationPinScreenEvents.kt
+++ app/src/main/java/org/simple/clinic/registration/pin/RegistrationPinScreenEvents.kt
@@ -4,10 +4,6 @@ import org.simple.clinic.widgets.UiEvent
 
 class RegistrationPinScreenCreated : UiEvent
 
-data class RegistrationPinTextChanged(val pin: String) : UiEvent {
-  override val analyticsName = "Registration:Pin Entry:Pin Text Changed"
-}
-
 class RegistrationPinDoneClicked : UiEvent {
   override val analyticsName = "Registration:Pin Entry:Done Clicked"
 }
--- app/src/main/java/org/simple/clinic/registration/pin/RegistrationPinUpdate.kt
+++ app/src/main/java/org/simple/clinic/registration/pin/RegistrationPinUpdate.kt
@@ -3,12 +3,14 @@ package org.simple.clinic.registration.pin
 import com.spotify.mobius.Next
 import com.spotify.mobius.Update
 import org.simple.clinic.mobius.dispatch
+import org.simple.clinic.mobius.next
 
 class RegistrationPinUpdate: Update<RegistrationPinModel, RegistrationPinEvent, RegistrationPinEffect> {
 
   override fun update(model: RegistrationPinModel, event: RegistrationPinEvent): Next<RegistrationPinModel, RegistrationPinEffect> {
     return when(event) {
       is CurrentOngoingEntrySaved -> dispatch(ProceedToConfirmPin)
+      is RegistrationPinTextChanged -> next(model.pinChanged(event.pin))
     }
   }
 }

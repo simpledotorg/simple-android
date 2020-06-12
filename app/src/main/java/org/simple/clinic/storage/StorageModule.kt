ff024dcc6f946ed634383deb24d0fce18a798ca0
Vinay Shenoy <vinay@obvious.in>
2020-06-12 11:31:54 +0530

    Add `TextStore` interface and local file implementation

--- app/src/main/java/org/simple/clinic/storage/StorageModule.kt
+++ app/src/main/java/org/simple/clinic/storage/StorageModule.kt
@@ -9,8 +9,12 @@ import dagger.Provides
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.di.AppScope
 import org.simple.clinic.di.AppSqliteOpenHelperFactory
+import org.simple.clinic.storage.files.FileStorage
 import org.simple.clinic.storage.migrations.RoomMigrationsModule
+import org.simple.clinic.storage.text.LocalFileTextStore
+import org.simple.clinic.storage.text.TextStore
 import org.simple.clinic.user.User
+import javax.inject.Named
 
 @Module(includes = [
   RoomMigrationsModule::class,
@@ -38,5 +42,20 @@ class StorageModule {
   @Provides
   fun userDao(appDatabase: AppDatabase): User.RoomDao {
     return appDatabase.userDao()
+  }
+
+  @Provides
+  fun textStore(
+      fileStorage: FileStorage,
+      @Named("reports_file_path") reportsFilePath: String,
+      @Named("help_file_path") helpFilePath: String
+  ): TextStore {
+    return LocalFileTextStore(
+        fileStorage = fileStorage,
+        keysToFilePath = mapOf(
+            "reports" to reportsFilePath,
+            "help" to helpFilePath
+        )
+    )
   }
 }

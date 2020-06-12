699ec02d294aef2eea665d38757df0698c4d0719
Vinay Shenoy <vinay@obvious.in>
2020-06-12 14:24:18 +0530

    Use synthetic view accessors in `ReportsScreen`

--- app/src/main/java/org/simple/clinic/home/report/ReportsScreen.kt
+++ app/src/main/java/org/simple/clinic/home/report/ReportsScreen.kt
@@ -3,15 +3,12 @@ package org.simple.clinic.home.report
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import android.webkit.WebView
 import android.widget.FrameLayout
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
-import kotterknife.bindView
-import org.simple.clinic.R
-import org.simple.clinic.main.TheActivity
+import kotlinx.android.synthetic.main.screen_report.view.*
 import org.simple.clinic.bindUiToController
+import org.simple.clinic.main.TheActivity
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
@@ -23,9 +20,6 @@ class ReportsScreen(context: Context, attrs: AttributeSet) : FrameLayout(context
 
   @Inject
   lateinit var controller: ReportsScreenController
-
-  private val webView by bindView<WebView>(R.id.reportsscreen_webview)
-  private val noReportView by bindView<View>(R.id.reportsscreen_no_report)
 
   @SuppressLint("SetJavaScriptEnabled")
   override fun onFinishInflate() {

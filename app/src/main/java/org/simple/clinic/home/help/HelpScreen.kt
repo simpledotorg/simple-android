cc06ada13a262a339039eba84e6c9672703284f7
Vinay Shenoy <vinay@obvious.in>
2020-06-12 10:39:05 +0530

    Use synthetic view accessors in `HelpScreen`

--- app/src/main/java/org/simple/clinic/home/help/HelpScreen.kt
+++ app/src/main/java/org/simple/clinic/home/help/HelpScreen.kt
@@ -3,21 +3,14 @@ package org.simple.clinic.home.help
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import android.webkit.WebView
-import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
-import com.google.android.material.button.MaterialButton
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
-import kotterknife.bindView
+import kotlinx.android.synthetic.main.screen_help.view.*
 import org.simple.clinic.R
-import org.simple.clinic.main.TheActivity
 import org.simple.clinic.bindUiToController
 import org.simple.clinic.help.HelpScreenTryAgainClicked
+import org.simple.clinic.main.TheActivity
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.ScreenDestroyed
@@ -32,13 +25,6 @@ class HelpScreen(context: Context, attrs: AttributeSet) : LinearLayout(context, 
 
   @Inject
   lateinit var screenRouter: ScreenRouter
-
-  private val webView by bindView<WebView>(R.id.help_webview)
-  private val noContentView by bindView<View>(R.id.help_no_content)
-  private val toolbar by bindView<Toolbar>(R.id.help_toolbar)
-  private val errorMessageTextView by bindView<TextView>(R.id.help_error_message)
-  private val tryAgainButton by bindView<MaterialButton>(R.id.help_try_again)
-  private val progresBar by bindView<ProgressBar>(R.id.help_progress)
 
   @SuppressLint("SetJavaScriptEnabled")
   override fun onFinishInflate() {
@@ -70,20 +56,20 @@ class HelpScreen(context: Context, attrs: AttributeSet) : LinearLayout(context, 
 
   fun showHelp(uri: URI) {
     webView.visibleOrGone(true)
-    progresBar.visibleOrGone(false)
+    progressBar.visibleOrGone(false)
     noContentView.visibleOrGone(false)
     webView.loadUrl(uri.toString())
   }
 
   fun showNoHelpAvailable() {
     webView.visibleOrGone(false)
-    progresBar.visibleOrGone(false)
+    progressBar.visibleOrGone(false)
     noContentView.visibleOrGone(true)
     webView.loadUrl("about:blank")
   }
 
   fun showLoadingView() {
-    progresBar.visibleOrGone(true)
+    progressBar.visibleOrGone(true)
     noContentView.visibleOrGone(false)
   }
 

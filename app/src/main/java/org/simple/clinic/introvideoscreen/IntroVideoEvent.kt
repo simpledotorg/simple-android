9866c2a871279763452ce54dca5f21c7f5eb6bd4
Honey Sonwani <honey@obvious.in>
2020-06-15 12:08:13 +0530

    Add `analyticsName` in `VideoClicked` & `SkipClicked`

--- app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoEvent.kt
+++ app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoEvent.kt
@@ -4,6 +4,10 @@ import org.simple.clinic.widgets.UiEvent
 
 sealed class IntroVideoEvent : UiEvent
 
-object VideoClicked : IntroVideoEvent()
+object VideoClicked : IntroVideoEvent() {
+  override val analyticsName: String = "IntroVideo:Video Clicked"
+}
 
-object SkipClicked : IntroVideoEvent()
+object SkipClicked : IntroVideoEvent() {
+  override val analyticsName: String = "IntroVideo:Skip Clicked"
+}

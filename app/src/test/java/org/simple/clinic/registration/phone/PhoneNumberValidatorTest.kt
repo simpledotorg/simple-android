abca3c7461605f9a3c5a52d26d3237cd333a25f2
Honey Sonwani <honey@obvious.in>
2020-06-17 12:02:20 +0530

    Add `LengthBasedNumberValidator`

--- app/src/test/java/org/simple/clinic/registration/phone/PhoneNumberValidatorTest.kt
+++ app/src/test/java/org/simple/clinic/registration/phone/PhoneNumberValidatorTest.kt
@@ -11,9 +11,21 @@ import org.simple.clinic.registration.phone.PhoneNumberValidator.Type.MOBILE
 
 class PhoneNumberValidatorTest {
 
+  private val validator = LengthBasedNumberValidator(
+      10,
+      10,
+      9,
+      10
+  )
+
   @Test
   fun `validate phone numbers from India`() {
-    val validator = IndianPhoneNumberValidator()
+    val validator = LengthBasedNumberValidator(
+        10,
+        10,
+        6,
+        12
+    )
     assertThat(validator.validate("", MOBILE)).isEqualTo(BLANK)
     assertThat(validator.validate("123456789", MOBILE)).isEqualTo(LENGTH_TOO_SHORT)
     assertThat(validator.validate("1234567890", MOBILE)).isEqualTo(VALID)
@@ -23,5 +35,26 @@ class PhoneNumberValidatorTest {
     assertThat(validator.validate("1982322", LANDLINE_OR_MOBILE)).isEqualTo(VALID)
     assertThat(validator.validate("129391872", LANDLINE_OR_MOBILE)).isEqualTo(VALID)
     assertThat(validator.validate("98681972638734", LANDLINE_OR_MOBILE)).isEqualTo(LENGTH_TOO_LONG)
+  }
+
+  @Test
+  fun `validate valid phone numbers from Ethiopia`() {
+    assertThat(validator.validate("987654321", LANDLINE_OR_MOBILE)).isEqualTo(VALID)
+    assertThat(validator.validate("9865327861", LANDLINE_OR_MOBILE)).isEqualTo(VALID)
+  }
+
+  @Test
+  fun `validate blank phone numbers from Ethiopia`() {
+    assertThat(validator.validate("", LANDLINE_OR_MOBILE)).isEqualTo(BLANK)
+  }
+
+  @Test
+  fun `validate too short phone numbers from Ethiopia `() {
+    assertThat(validator.validate("123456", LANDLINE_OR_MOBILE)).isEqualTo(LENGTH_TOO_SHORT)
+  }
+
+  @Test
+  fun `validate too long phone numbers from Ethiopia`() {
+    assertThat(validator.validate("98653278653", LANDLINE_OR_MOBILE)).isEqualTo(LENGTH_TOO_LONG)
   }
 }

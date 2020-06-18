1993113ec469c683fb85ab30782e21818a0e9383
Vinay Shenoy <vinay@obvious.in>
2020-06-18 16:07:36 +0530

    Add effect to go back to PIN entry screen

--- app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinUi.kt
+++ app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinUi.kt
@@ -1,8 +1,5 @@
 package org.simple.clinic.registration.confirmpin
 
-import org.simple.clinic.user.OngoingRegistrationEntry
-
 interface RegistrationConfirmPinUi : RegistrationConfirmPinUiActions {
   fun showPinMismatchError()
-  fun goBackToPinScreen(entry: OngoingRegistrationEntry)
 }

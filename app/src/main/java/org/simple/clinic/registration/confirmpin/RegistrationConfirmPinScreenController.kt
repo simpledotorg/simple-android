b94ddfbe1c90ac28f96cdbd804ba8bdb36dd8eed
Vinay Shenoy <code@vinaysshenoy.com>
2020-06-18 14:29:54 +0530

    Stop resetting PIN in `RegistrationConfirmPinScreenControllerTest`
    This was useless because the previous screen, `RegistrationPinScreen`,
    no longer reads the current entry from `UserSession` and instead reads
    this from the screen arguments.
    
    The right way to handle this behaviour is to create a new registration
    entry without the PIN and rewrite the backstack.

--- app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinScreenController.kt
+++ app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinScreenController.kt
@@ -1,24 +1,29 @@
 package org.simple.clinic.registration.confirmpin
 
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
 import io.reactivex.rxkotlin.ofType
 import org.simple.clinic.ReplayUntilScreenIsDestroyed
-import org.simple.clinic.user.OngoingRegistrationEntry
 import org.simple.clinic.user.UserSession
-import org.simple.clinic.util.Just
 import org.simple.clinic.util.UtcClock
 import org.simple.clinic.widgets.UiEvent
-import javax.inject.Inject
 
 typealias Ui = RegistrationConfirmPinUi
 typealias UiChange = (Ui) -> Unit
 
-class RegistrationConfirmPinScreenController @Inject constructor(
+class RegistrationConfirmPinScreenController @AssistedInject constructor(
     private val userSession: UserSession,
-    private val utcClock: UtcClock
+    private val utcClock: UtcClock,
+    @Assisted private val modelSupplier: () -> RegistrationConfirmPinModel
 ) : ObservableTransformer<UiEvent, UiChange> {
+
+  @AssistedInject.Factory
+  interface Factory {
+    fun create(modelSupplier: () -> RegistrationConfirmPinModel): RegistrationConfirmPinScreenController
+  }
 
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
     val replayedEvents = ReplayUntilScreenIsDestroyed(events)
@@ -30,11 +35,8 @@ class RegistrationConfirmPinScreenController @Inject constructor(
   private fun resetPins(events: Observable<UiEvent>): Observable<UiChange> {
     return events
         .ofType<RegistrationResetPinClicked>()
-        .map { ongoingRegistrationEntry() }
-        .map(OngoingRegistrationEntry::resetPin)
-        .doOnNext(userSession::saveOngoingRegistrationEntry)
-        .map { { ui: Ui -> ui.goBackToPinScreen() } }
+        .map { modelSupplier() }
+        .map { it.ongoingRegistrationEntry.resetPin() }
+        .map { { ui: Ui -> ui.goBackToPinScreen(it) } }
   }
-
-  private fun ongoingRegistrationEntry(): OngoingRegistrationEntry = (userSession.ongoingRegistrationEntry() as Just).value
 }

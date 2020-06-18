759c6a13e1e7d917e20452d30e4366af6f31662a
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-06-18 10:05:37 +0530

    Show blood sugar high and low level labels

--- app/src/main/java/org/simple/clinic/bloodsugar/history/adapter/BloodSugarHistoryListItem.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/history/adapter/BloodSugarHistoryListItem.kt
@@ -3,11 +3,13 @@ package org.simple.clinic.bloodsugar.history.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.text.style.TextAppearanceSpan
+import android.view.View
 import io.reactivex.subjects.Subject
 import kotlinx.android.synthetic.main.list_blood_sugar_history_item.*
 import kotlinx.android.synthetic.main.list_new_blood_sugar_button.*
 import org.simple.clinic.R
 import org.simple.clinic.bloodsugar.BloodSugarMeasurement
+import org.simple.clinic.bloodsugar.BloodSugarReading
 import org.simple.clinic.util.Truss
 import org.simple.clinic.widgets.PagingItemAdapter
 import org.simple.clinic.widgets.recyclerview.ViewHolderX
@@ -40,21 +42,16 @@ sealed class BloodSugarHistoryListItem : PagingItemAdapter.Item<Event> {
           .append(bloodSugarDateTime(context))
           .popSpan()
           .build()
-      val isBloodSugarHigh = measurement.reading.isHigh
-      val isBloodSugarLow = measurement.reading.isLow
 
       val displayUnit = context.getString(bloodSugarReading.displayUnit)
       val displayType = context.getString(bloodSugarReading.displayType)
       val readingPrefix = bloodSugarReading.displayValue
       val readingSuffix = "$displayUnit $displayType"
 
+      renderBloodSugarLevel(holder, context, measurement.reading)
+
       holder.readingsTextView.text = "$readingPrefix${bloodSugarReading.displayUnitSeparator}$readingSuffix"
       holder.dateTimeTextView.text = formattedBPDateTime
-      if (isBloodSugarHigh || isBloodSugarLow) {
-        holder.bloodSugarIconImageView.setImageResource(R.drawable.ic_blood_sugar_filled)
-      } else {
-        holder.bloodSugarIconImageView.setImageResource(R.drawable.ic_blood_sugar_outline)
-      }
 
       if (isBloodSugarEditable) {
         holder.itemView.setOnClickListener { subject.onNext(BloodSugarHistoryItemClicked(measurement)) }
@@ -64,6 +61,25 @@ sealed class BloodSugarHistoryListItem : PagingItemAdapter.Item<Event> {
       holder.itemView.isClickable = isBloodSugarEditable
       holder.itemView.isFocusable = isBloodSugarEditable
       holder.editButton.visibleOrGone(isBloodSugarEditable)
+    }
+
+    private fun renderBloodSugarLevel(holder: ViewHolderX, context: Context, reading: BloodSugarReading) {
+      when {
+        reading.isLow -> {
+          holder.bloodSugarLevelTextView.visibility = View.VISIBLE
+          holder.bloodSugarLevelTextView.text = context.getString(R.string.bloodsugar_level_low)
+          holder.bloodSugarIconImageView.setImageResource(R.drawable.ic_blood_sugar_filled)
+        }
+        reading.isHigh -> {
+          holder.bloodSugarLevelTextView.visibility = View.VISIBLE
+          holder.bloodSugarLevelTextView.text = context.getString(R.string.bloodsugar_level_high)
+          holder.bloodSugarIconImageView.setImageResource(R.drawable.ic_blood_sugar_filled)
+        }
+        else -> {
+          holder.bloodSugarLevelTextView.visibility = View.GONE
+          holder.bloodSugarIconImageView.setImageResource(R.drawable.ic_blood_sugar_outline)
+        }
+      }
     }
 
     private fun bloodSugarDateTime(context: Context): String {

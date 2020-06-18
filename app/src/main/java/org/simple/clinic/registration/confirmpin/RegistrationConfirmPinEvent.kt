f1d3673da2be8ecef72349152b23c4eccd052cef
Vinay Shenoy <code@vinaysshenoy.com>
2020-06-18 11:24:33 +0530

    Track PIN confirmation in `RegistrationConfirmPinUpdate`

--- app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinEvent.kt
+++ app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinEvent.kt
@@ -3,3 +3,7 @@ package org.simple.clinic.registration.confirmpin
 import org.simple.clinic.widgets.UiEvent
 
 sealed class RegistrationConfirmPinEvent: UiEvent
+
+data class RegistrationConfirmPinTextChanged(val confirmPin: String) : RegistrationConfirmPinEvent() {
+  override val analyticsName = "Registration:Confirm Pin:Pin Text Changed"
+}

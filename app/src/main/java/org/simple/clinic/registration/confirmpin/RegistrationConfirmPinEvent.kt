60d5a027115ee9bd65161e7cdfe5cd49787a2819
Vinay Shenoy <vinay@obvious.in>
2020-06-18 11:45:00 +0530

    Add effect to validate the entered PIN

--- app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinEvent.kt
+++ app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinEvent.kt
@@ -2,8 +2,10 @@ package org.simple.clinic.registration.confirmpin
 
 import org.simple.clinic.widgets.UiEvent
 
-sealed class RegistrationConfirmPinEvent: UiEvent
+sealed class RegistrationConfirmPinEvent : UiEvent
 
 data class RegistrationConfirmPinTextChanged(val confirmPin: String) : RegistrationConfirmPinEvent() {
   override val analyticsName = "Registration:Confirm Pin:Pin Text Changed"
 }
+
+data class PinConfirmationValidated(val result: RegistrationConfirmPinValidationResult) : RegistrationConfirmPinEvent()

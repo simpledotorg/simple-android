b4e89c3e753d30f193722e10d56596c1807e9571
Vinay Shenoy <vinay@obvious.in>
2020-06-19 13:39:10 +0530

    Clean up `RegistrationLocationPermissionScreenControllerTest`

--- app/src/test/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreenControllerTest.kt
@@ -9,7 +9,6 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.simple.clinic.util.Just
 import org.simple.clinic.platform.util.RuntimePermissionResult.GRANTED
 import org.simple.clinic.util.Optional
 import org.simple.clinic.util.RxErrorsRule
@@ -20,8 +19,8 @@ class RegistrationLocationPermissionScreenControllerTest {
   @get:Rule
   val rxErrorsRule = RxErrorsRule()
 
-  val uiEvents = PublishSubject.create<UiEvent>()
-  val screen = mock<RegistrationLocationPermissionScreen>()
+  private val uiEvents = PublishSubject.create<UiEvent>()!!
+  private val screen = mock<RegistrationLocationPermissionScreen>()
 
   private lateinit var controllerSubscription: Disposable
 

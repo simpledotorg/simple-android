c17d75502b42ed42227378a015b91284c5c90537
Vinay Shenoy <vinay@obvious.in>
2020-06-19 13:51:00 +0530

    Add additional test to verify permission denied flow

--- app/src/test/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreenControllerTest.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.registration.location
 
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
 import io.reactivex.disposables.Disposable
@@ -9,6 +10,7 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.simple.clinic.platform.util.RuntimePermissionResult.DENIED
 import org.simple.clinic.platform.util.RuntimePermissionResult.GRANTED
 import org.simple.clinic.util.Optional
 import org.simple.clinic.util.RxErrorsRule
@@ -34,9 +36,20 @@ class RegistrationLocationPermissionScreenControllerTest {
     // when
     setupController()
     uiEvents.onNext(RequestLocationPermission(permission = Optional.of(GRANTED)))
-
+    
     // then
     verify(screen).openFacilitySelectionScreen()
+    verifyNoMoreInteractions(screen)
+  }
+
+  @Test
+  fun `when location permission is denied then facility selection screen should not be opened`() {
+    // when
+    setupController()
+    uiEvents.onNext(RequestLocationPermission(permission = Optional.of(DENIED)))
+
+    // then
+    verify(screen, never()).openFacilitySelectionScreen()
     verifyNoMoreInteractions(screen)
   }
 

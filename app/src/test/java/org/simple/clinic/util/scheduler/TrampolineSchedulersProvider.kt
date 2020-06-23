da2d40a7e1225a8a39e5e7597a5ac54499bb8deb
Vinay Shenoy <vinay@obvious.in>
2020-06-23 14:24:28 +0530

    Change `TestSchedulersProvider` to support individual schedulers

--- app/src/test/java/org/simple/clinic/util/scheduler/TrampolineSchedulersProvider.kt
+++ app/src/test/java/org/simple/clinic/util/scheduler/TrampolineSchedulersProvider.kt
@@ -2,8 +2,18 @@ package org.simple.clinic.util.scheduler
 
 import io.reactivex.Scheduler
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
-class TrampolineSchedulersProvider : SchedulersProvider {
+class TrampolineSchedulersProvider
+
+@Deprecated(
+    message = "Use TestSchedulersProvider instead",
+    replaceWith = ReplaceWith(
+        expression = "TestSchedulersProvider.trampoline()",
+        imports = ["org.simple.clinic.util.scheduler.TestSchedulersProvider"]
+    )
+)
+@Inject constructor() : SchedulersProvider {
   override fun io(): Scheduler = Schedulers.trampoline()
   override fun computation(): Scheduler = Schedulers.trampoline()
   override fun ui(): Scheduler = Schedulers.trampoline()

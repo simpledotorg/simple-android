a112c727a1ea710efec63c0821832e98f8d86536
Vinay Shenoy <vinay@obvious.in>
2020-06-24 10:37:10 +0530

    Add `Feature` enum to represent all feature flags

---
+++ app/src/main/java/org/simple/clinic/feature/Feature.kt
@@ -0,0 +1,13 @@
+package org.simple.clinic.feature
+
+enum class Feature(
+    val enabledByDefault: Boolean,
+    val remoteConfigKey: String = ""
+) {
+  EditBloodSugar(true),
+  NotifyAppUpdateAvailable(false, "appupdate_enabled"),
+  DeletePatient(true, "delete_patient_feature_enabled"),
+  SecureCalling(false, "phonenumbermasker_masking_enabled"),
+  CameraXQrScanner(true, "use_new_qr_scanner"),
+  LogSavedStateSizes(false, "log_saved_state_sizes_enabled")
+}

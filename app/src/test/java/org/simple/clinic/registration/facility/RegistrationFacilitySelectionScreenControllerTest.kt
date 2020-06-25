10826b489305dba7c741e9b85388aa90148cd158
Vinay Shenoy <code@vinaysshenoy.com>
2020-06-25 14:40:26 +0530

    Add "Given-When-Then" assertions to all tests

--- app/src/test/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionScreenControllerTest.kt
@@ -70,14 +70,17 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
   @Test
   fun `when screen is started, location should be fetched`() {
+    // given
     val locationUpdateInterval = Duration.ofDays(5)
     whenever(facilityRepository.recordCount()).thenReturn(Observable.never())
 
+    // when
     setupController(
         registrationConfig.copy(locationUpdateInterval = locationUpdateInterval),
         locationUpdate = Observable.never()
     )
 
+    // then
     verify(screenLocationUpdates).streamUserLocation(
         updateInterval = locationUpdateInterval,
         timeout = registrationConfig.locationListenerExpiry,
@@ -89,12 +92,15 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
   @Test
   fun `while location is being fetched then progress indicator should be shown`() {
+    // given
     val facilities = emptyList<Facility>()
     whenever(facilityRepository.facilities()).thenReturn(Observable.just(facilities))
     whenever(facilityRepository.recordCount()).thenReturn(Observable.just(facilities.size))
 
+    // when
     setupController()
 
+    // then
     val inOrder = inOrder(screen)
     inOrder.verify(screen).showProgressIndicator()
     inOrder.verify(screen).hideProgressIndicator()
@@ -103,6 +109,7 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
   @Test
   fun `when facilities are fetched, but location is unavailable then facilities should still be shown`() {
+    // given
     val phcObvious = TestData.facility(name = "PHC Obvious", district = "Bangalore Central", state = "Karnataka")
     val chcNilenso = TestData.facility(name = "CHC Nilenso", streetAddress = "10th Cross Road", district = "Indiranagar", state = "Karnataka")
     val facilities = listOf(phcObvious, chcNilenso)
@@ -111,9 +118,11 @@ class RegistrationFacilitySelectionScreenControllerTest {
     whenever(facilityRepository.facilities(searchQuery)).thenReturn(Observable.just(facilities))
     whenever(facilityRepository.recordCount()).thenReturn(Observable.just(facilities.size))
 
+    // when
     setupController()
     uiEvents.onNext(RegistrationFacilitySearchQueryChanged(searchQuery))
 
+    // then
     val expectedFacilityListItems = listOf(
         FacilityOption(phcObvious, Name.Plain("PHC Obvious"), address = Address.WithoutStreet(district = "Bangalore Central", state = "Karnataka")),
         FacilityOption(chcNilenso, Name.Plain("CHC Nilenso"), address = Address.WithStreet(street = "10th Cross Road", district = "Indiranagar", state = "Karnataka"))
@@ -127,6 +136,7 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
   @Test
   fun `when search query is changed then the query should be used for fetching filtered facilities`() {
+    // given
     val phcObvious = TestData.facility(name = "PHC Obvious", streetAddress = "Richmond Road", district = "Bangalore Central", state = "Karnataka")
     val chcNilenso = TestData.facility(name = "CHC Nilenso", district = "Indiranagar", state = "Karnataka")
 
@@ -135,9 +145,11 @@ class RegistrationFacilitySelectionScreenControllerTest {
     whenever(facilityRepository.facilities("CHC")).thenReturn(Observable.just(listOf(chcNilenso)))
     whenever(facilityRepository.recordCount()).thenReturn(Observable.just(2))
 
+    // when
     setupController()
-
     uiEvents.onNext(RegistrationFacilitySearchQueryChanged(query = "HC"))
+
+    // then
     verify(screen).showProgressIndicator()
     verify(screen).hideProgressIndicator()
     verify(screen).showToolbarWithSearchField()
@@ -147,15 +159,21 @@ class RegistrationFacilitySelectionScreenControllerTest {
     ), FIRST_UPDATE)
     verifyNoMoreInteractions(screen)
 
+    // when
     clearInvocations(screen)
     uiEvents.onNext(RegistrationFacilitySearchQueryChanged(query = "PHC"))
+
+    // then
     verify(screen).updateFacilities(listOf(
         FacilityOption(phcObvious, Name.Highlighted("PHC Obvious", 0, 3), address = Address.WithStreet(street = "Richmond Road", district = "Bangalore Central", state = "Karnataka"))
     ), SUBSEQUENT_UPDATE)
     verifyNoMoreInteractions(screen)
 
+    // when
     clearInvocations(screen)
     uiEvents.onNext(RegistrationFacilitySearchQueryChanged(query = "CHC"))
+
+    // then
     verify(screen).updateFacilities(listOf(
         FacilityOption(chcNilenso, Name.Highlighted("CHC Nilenso", 0, 3), address = Address.WithoutStreet(district = "Indiranagar", state = "Karnataka"))
     ), SUBSEQUENT_UPDATE)
@@ -164,23 +182,25 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
   @Test
   fun `when facilities are received then their UI models for facility list should be created`() {
+    // given
     val phcObvious = TestData.facility(name = "PHC Obvious", streetAddress = "Richmond Road", district = "Bangalore Central", state = "Karnataka")
     val chcNilenso = TestData.facility(name = "CHC Nilenso", district = "Indiranagar", state = "Karnataka")
     val facilities = listOf(phcObvious, chcNilenso)
     val searchQuery = ""
+    val location = LocationUpdate.Available(Coordinates(0.0, 0.0), timeSinceBootWhenRecorded = Duration.ofNanos(0))
 
     whenever(facilityRepository.facilities(searchQuery)).thenReturn(Observable.just(facilities, facilities))
     whenever(facilityRepository.recordCount()).thenReturn(Observable.just(facilities.size, facilities.size))
 
+    // when
+    setupController(locationUpdate = Observable.just(location))
+    uiEvents.onNext(RegistrationFacilitySearchQueryChanged(""))
+
+    // then
     val expectedFacilityListItems = listOf(
         FacilityOption(phcObvious, Name.Plain("PHC Obvious"), address = Address.WithStreet(street = "Richmond Road", district = "Bangalore Central", state = "Karnataka")),
         FacilityOption(chcNilenso, Name.Plain("CHC Nilenso"), address = Address.WithoutStreet(district = "Indiranagar", state = "Karnataka"))
     )
-
-    val location = LocationUpdate.Available(Coordinates(0.0, 0.0), timeSinceBootWhenRecorded = Duration.ofNanos(0))
-    setupController(locationUpdate = Observable.just(location))
-
-    uiEvents.onNext(RegistrationFacilitySearchQueryChanged(""))
     verify(screen).showProgressIndicator()
     verify(screen).hideProgressIndicator()
     verify(screen).showToolbarWithSearchField()
@@ -192,6 +212,7 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
   @Test
   fun `when a facility is clicked then show confirm facility sheet`() {
+    // given
     val ongoingEntry = OngoingRegistrationEntry(
         uuid = UUID.fromString("eb0a9bc0-b24d-4f3f-9990-aa05e217be1a"),
         phoneNumber = "1234567890",
@@ -203,9 +224,11 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
     val facility1 = TestData.facility(name = "Hoshiarpur", uuid = UUID.fromString("5cf9d744-7f34-4633-aa46-a6c7e7542060"))
 
+    // when
     setupController()
     uiEvents.onNext(RegistrationFacilityClicked(facility1))
 
+    // then
     verify(screen).showProgressIndicator()
     verify(screen).hideProgressIndicator()
     verify(screen).showToolbarWithSearchField()
@@ -215,6 +238,7 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
   @Test
   fun `when a facility is confirmed then the ongoing entry should be updated with selected facility and the user should be logged in`() {
+    // given
     val ongoingEntry = OngoingRegistrationEntry(
         uuid = UUID.fromString("252ef188-318c-443c-9c0c-37644e84bb6d"),
         phoneNumber = "1234567890",
@@ -226,9 +250,11 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
     val facility1 = TestData.facility(name = "Hoshiarpur", uuid = UUID.fromString("bc761c6c-032f-4f1d-a66a-3ec81e9e8aa3"))
 
+    // when
     setupController()
     uiEvents.onNext(RegistrationFacilityConfirmed(facility1.uuid))
 
+    // then
     verify(screen).showProgressIndicator()
     verify(screen).hideProgressIndicator()
     verify(screen).showToolbarWithSearchField()
@@ -240,10 +266,13 @@ class RegistrationFacilitySelectionScreenControllerTest {
 
   @Test
   fun `search field should only be shown when facilities are available`() {
+    // given
     whenever(facilityRepository.recordCount()).thenReturn(Observable.just(0, 10))
 
+    // when
     setupController()
 
+    // then
     val inOrder = inOrder(screen)
     inOrder.verify(screen).showProgressIndicator()
     inOrder.verify(screen).showToolbarWithoutSearchField()

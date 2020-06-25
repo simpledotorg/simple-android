a6162e8c7530e04631c0f9998bf1d82b3e422086
Vinay Shenoy <code@vinaysshenoy.com>
2020-06-25 15:06:45 +0530

    Add no-op Mobius classes to `RegistrationFacilitySelectionScreen`

---
+++ app/src/main/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionEvent.kt
@@ -0,0 +1,5 @@
+package org.simple.clinic.registration.facility
+
+import org.simple.clinic.widgets.UiEvent
+
+sealed class RegistrationFacilitySelectionEvent: UiEvent
---
+++ app/src/main/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionUiActions.kt
@@ -0,0 +1,3 @@
+package org.simple.clinic.registration.facility
+
+interface RegistrationFacilitySelectionUiActions

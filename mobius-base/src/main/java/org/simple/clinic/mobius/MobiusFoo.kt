6169df40e5e8398f7611d5bb7655932836fa0896
Vinay Shenoy <code@vinaysshenoy.com>
2020-06-26 09:43:20 +0530

    Add support for additional event sources to `MobiusDelegate`

--- mobius-base/src/main/java/org/simple/clinic/mobius/MobiusFoo.kt
+++ mobius-base/src/main/java/org/simple/clinic/mobius/MobiusFoo.kt
@@ -1,6 +1,8 @@
 package org.simple.clinic.mobius
 
+import com.spotify.mobius.EventSource
 import com.spotify.mobius.First
+import com.spotify.mobius.MobiusLoop
 import com.spotify.mobius.Next
 
 fun <M, F> next(model: M, vararg effects: F): Next<M, F> = if (effects.isEmpty()) {
@@ -16,4 +18,12 @@ fun <M, F> first(model: M, vararg effects: F): First<M, F> = if (effects.isEmpty
   First.first(model)
 } else {
   First.first(model, setOf(*effects))
+}
+
+fun <M, E, F> MobiusLoop.Builder<M, E, F>.eventSources(eventsSources: List<EventSource<E>>): MobiusLoop.Builder<M, E, F> {
+  return when {
+    eventsSources.isEmpty() -> this
+    eventsSources.size == 1 -> eventSource(eventsSources.first())
+    else -> eventSources(eventsSources.first(), *eventsSources.subList(1, eventsSources.size).toTypedArray())
+  }
 }

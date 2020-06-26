9c985828dbf8aa42709adb94addd0fd537845cbf
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-06-26 15:08:45 +0530

    Add mobius test fixture to `ConfirmRemovePrescriptionDialogControllerTest`

--- app/src/test/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialogControllerTest.kt
+++ app/src/test/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialogControllerTest.kt
@@ -4,16 +4,20 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
 import com.nhaarman.mockitokotlin2.whenever
+import com.spotify.mobius.Init
 import io.reactivex.Completable
 import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.ofType
 import io.reactivex.subjects.PublishSubject
 import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.simple.clinic.bp.entry.confirmremovebloodpressure.RemovePrescriptionClicked
 import org.simple.clinic.drugs.PrescriptionRepository
+import org.simple.clinic.mobius.first
 import org.simple.clinic.util.RxErrorsRule
 import org.simple.clinic.widgets.UiEvent
+import org.simple.mobius.migration.MobiusTestFixture
 import java.util.UUID
 
 class ConfirmRemovePrescriptionDialogControllerTest {
@@ -29,10 +33,12 @@ class ConfirmRemovePrescriptionDialogControllerTest {
   private val prescribedDrugUuid = UUID.fromString("fe94ba47-4d34-476c-809f-c2adfc11914a")
 
   private lateinit var controllerSubscription: Disposable
+  private lateinit var testFixture: MobiusTestFixture<ConfirmRemovePrescriptionDialogModel, ConfirmRemovePrescriptionDialogEvent, ConfirmRemovePrescriptionDialogEffect>
 
   @After
   fun tearDown() {
     controllerSubscription.dispose()
+    testFixture.dispose()
   }
 
   @Test
@@ -55,5 +61,16 @@ class ConfirmRemovePrescriptionDialogControllerTest {
     controllerSubscription = uiEvents
         .compose(controller)
         .subscribe { uiChange -> uiChange(uiActions) }
+
+    testFixture = MobiusTestFixture(
+        events = uiEvents.ofType(),
+        defaultModel = ConfirmRemovePrescriptionDialogModel.create(),
+        init = Init { first(it) },
+        update = ConfirmRemovePrescriptionDialogUpdate(),
+        effectHandler = ConfirmRemovePrescriptionDialogEffectHandler(uiActions).build(),
+        modelUpdateListener = { }
+    )
+
+    testFixture.start()
   }
 }

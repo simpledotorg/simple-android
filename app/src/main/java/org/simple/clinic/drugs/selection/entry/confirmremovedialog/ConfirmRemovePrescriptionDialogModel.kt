ec512a22d44e2c82124e929acbe35996b3bf3522
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-06-26 15:02:21 +0530

    Add no-op Mobius implementation

---
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialogModel.kt
@@ -0,0 +1,7 @@
+package org.simple.clinic.drugs.selection.entry.confirmremovedialog
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class ConfirmRemovePrescriptionDialogModel : Parcelable

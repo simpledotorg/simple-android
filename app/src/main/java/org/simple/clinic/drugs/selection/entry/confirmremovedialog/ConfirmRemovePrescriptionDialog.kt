739cae5573695dbdec7cedc838068a52c9e9f7e5
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-06-26 15:24:12 +0530

    Integrate Mobius loop in `ConfirmRemovePrescriptionDialog`

--- app/src/main/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialog.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialog.kt
@@ -5,12 +5,14 @@ import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.FragmentManager
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
+import io.reactivex.rxkotlin.ofType
 import io.reactivex.schedulers.Schedulers.io
 import io.reactivex.subjects.PublishSubject
 import org.simple.clinic.R
@@ -18,6 +20,7 @@ import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.bp.entry.confirmremovebloodpressure.RemovePrescriptionClicked
 import org.simple.clinic.di.injector
 import org.simple.clinic.drugs.PrescriptionRepository
+import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
@@ -32,6 +35,9 @@ class ConfirmRemovePrescriptionDialog : AppCompatDialogFragment(), UiActions {
   @Inject
   lateinit var controller: ConfirmRemovePrescriptionDialogController.Factory
 
+  @Inject
+  lateinit var effectHandlerFactory: ConfirmRemovePrescriptionDialogEffectHandler.Factory
+
   private val onStarts = PublishSubject.create<Any>()
   private val screenDestroys = PublishSubject.create<ScreenDestroyed>()
 
@@ -39,6 +45,7 @@ class ConfirmRemovePrescriptionDialog : AppCompatDialogFragment(), UiActions {
     requireArguments().getSerializable(KEY_PRESCRIPTION_UUID) as UUID
   }
 
+  private val dialogEvents = PublishSubject.create<UiEvent>()
   private val events by unsafeLazy {
     Observable
         .merge(
@@ -47,6 +54,20 @@ class ConfirmRemovePrescriptionDialog : AppCompatDialogFragment(), UiActions {
         )
         .compose(ReportAnalyticsEvents())
         .share()
+  }
+
+  private val delegate: MobiusDelegate<ConfirmRemovePrescriptionDialogModel, ConfirmRemovePrescriptionDialogEvent, ConfirmRemovePrescriptionDialogEffect> by unsafeLazy {
+    MobiusDelegate.forActivity(
+        events = dialogEvents.ofType(),
+        defaultModel = ConfirmRemovePrescriptionDialogModel.create(),
+        update = ConfirmRemovePrescriptionDialogUpdate(),
+        effectHandler = effectHandlerFactory.create(this).build()
+    )
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    delegate.onRestoreInstanceState(savedInstanceState)
   }
 
   @SuppressLint("CheckResult")
@@ -65,6 +86,14 @@ class ConfirmRemovePrescriptionDialog : AppCompatDialogFragment(), UiActions {
         .subscribe { uiChange -> uiChange(this) }
 
     return dialog
+  }
+
+  @SuppressLint("CheckResult")
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    events
+        .takeUntil(screenDestroys)
+        .subscribe(dialogEvents::onNext)
   }
 
   private fun setupDialog(): Observable<UiChange> {
@@ -87,6 +116,17 @@ class ConfirmRemovePrescriptionDialog : AppCompatDialogFragment(), UiActions {
   override fun onStart() {
     super.onStart()
     onStarts.onNext(Any())
+    delegate.start()
+  }
+
+  override fun onStop() {
+    super.onStop()
+    delegate.stop()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    delegate.onSaveInstanceState(outState)
+    super.onSaveInstanceState(outState)
   }
 
   override fun closeDialog() {

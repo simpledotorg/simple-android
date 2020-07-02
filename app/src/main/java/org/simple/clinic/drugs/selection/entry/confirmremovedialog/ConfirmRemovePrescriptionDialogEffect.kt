72ca3c8354185356e44727d789bdeee0d630552b
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-07-02 14:58:33 +0530

    Add effect to remove prescription

--- app/src/main/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialogEffect.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialogEffect.kt
@@ -1,5 +1,9 @@
 package org.simple.clinic.drugs.selection.entry.confirmremovedialog
 
+import java.util.UUID
+
 sealed class ConfirmRemovePrescriptionDialogEffect
 
 object CloseDialog : ConfirmRemovePrescriptionDialogEffect()
+
+data class RemovePrescription(val prescriptionUuid: UUID) : ConfirmRemovePrescriptionDialogEffect()

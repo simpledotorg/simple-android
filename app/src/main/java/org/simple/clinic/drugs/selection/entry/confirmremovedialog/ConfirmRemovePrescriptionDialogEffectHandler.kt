16c7e75038a8b9950850ded221b25d3cd65e02c6
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-07-02 14:54:40 +0530

    Add effect to close the `ConfirmRemovePrescriptionDialog`

--- app/src/main/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialogEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/entry/confirmremovedialog/ConfirmRemovePrescriptionDialogEffectHandler.kt
@@ -4,8 +4,10 @@ import com.spotify.mobius.rx2.RxMobius
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class ConfirmRemovePrescriptionDialogEffectHandler @AssistedInject constructor(
+    private val schedulersProvider: SchedulersProvider,
     @Assisted private val uiActions: UiActions
 ) {
 
@@ -18,5 +20,6 @@ class ConfirmRemovePrescriptionDialogEffectHandler @AssistedInject constructor(
       ConfirmRemovePrescriptionDialogEvent> = RxMobius
       .subtypeEffectHandler<ConfirmRemovePrescriptionDialogEffect,
           ConfirmRemovePrescriptionDialogEvent>()
+      .addAction(CloseDialog::class.java, uiActions::closeDialog, schedulersProvider.ui())
       .build()
 }

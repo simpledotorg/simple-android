ee0f9d1ea81a2b39fd789ed0216f1524aa15e608
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-07-06 11:19:33 +0530

    Integrate Mobius loop in `RegistrationLocationPermissionScreen`

--- app/src/main/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreen.kt
+++ app/src/main/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreen.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.registration.location
 
 import android.content.Context
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.widget.RelativeLayout
 import com.jakewharton.rxbinding2.view.RxView
@@ -11,6 +12,7 @@ import kotlinx.android.synthetic.main.screen_registration_location_permission.vi
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.bindUiToController
 import org.simple.clinic.main.TheActivity
+import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.registration.facility.RegistrationFacilitySelectionScreenKey
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.util.RequestPermissions
@@ -30,6 +32,9 @@ class RegistrationLocationPermissionScreen(context: Context, attrs: AttributeSet
   lateinit var controller: RegistrationLocationPermissionScreenController
 
   @Inject
+  lateinit var effectHandlerFactory: RegistrationLocationPermissionEffectHandler.Factory
+
+  @Inject
   lateinit var runtimePermissions: RuntimePermissions
 
   private val events by unsafeLazy {
@@ -37,6 +42,15 @@ class RegistrationLocationPermissionScreen(context: Context, attrs: AttributeSet
         .compose(runtimePermissions())
         .compose(ReportAnalyticsEvents())
         .share()
+  }
+
+  private val delegate by unsafeLazy {
+    MobiusDelegate.forView(
+        events = events.ofType(),
+        defaultModel = RegistrationLocationPermissionModel.create(),
+        update = RegistrationLocationPermissionUpdate(),
+        effectHandler = effectHandlerFactory.create(this).build()
+    )
   }
 
   override fun onFinishInflate() {
@@ -72,5 +86,23 @@ class RegistrationLocationPermissionScreen(context: Context, attrs: AttributeSet
 
   override fun openFacilitySelectionScreen() {
     screenRouter.push(RegistrationFacilitySelectionScreenKey())
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    delegate.start()
+  }
+
+  override fun onDetachedFromWindow() {
+    delegate.stop()
+    super.onDetachedFromWindow()
+  }
+
+  override fun onSaveInstanceState(): Parcelable? {
+    return delegate.onSaveInstanceState(super.onSaveInstanceState())
+  }
+
+  override fun onRestoreInstanceState(state: Parcelable?) {
+    super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
 }

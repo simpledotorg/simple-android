43202ae64cb838fba3b6b85da7b42a88fa228302
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-07-06 09:46:17 +0530

    Setup controller in each test seperately

--- app/src/test/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/registration/location/RegistrationLocationPermissionScreenControllerTest.kt
@@ -2,7 +2,9 @@ package org.simple.clinic.registration.location
 
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.PublishSubject
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -16,24 +18,29 @@ class RegistrationLocationPermissionScreenControllerTest {
   @get:Rule
   val rxErrorsRule = RxErrorsRule()
 
-  val uiEvents = PublishSubject.create<UiEvent>()!!
+  val uiEvents = PublishSubject.create<UiEvent>()
   val screen = mock<RegistrationLocationPermissionScreen>()
 
-  private lateinit var controller: RegistrationLocationPermissionScreenController
+  private lateinit var controllerSubscription: Disposable
 
-  @Before
-  fun setUp() {
-    controller = RegistrationLocationPermissionScreenController()
-
-    uiEvents
-        .compose(controller)
-        .subscribe { uiChange -> uiChange(screen) }
+  @After
+  fun tearDown() {
+    controllerSubscription.dispose()
   }
 
   @Test
   fun `when location permission is received then facility selection screen should be opened`() {
+    setupController()
     uiEvents.onNext(RequestLocationPermission(permission = Just(GRANTED)))
 
     verify(screen).openFacilitySelectionScreen()
+  }
+
+  private fun setupController() {
+    val controller = RegistrationLocationPermissionScreenController()
+
+    controllerSubscription = uiEvents
+        .compose(controller)
+        .subscribe { uiChange -> uiChange(screen) }
   }
 }

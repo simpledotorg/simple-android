509167b876c034e287c80eb590b742f5c3acdad6
Vinay Shenoy <vinay@obvious.in>
2020-07-08 14:02:22 +0530

    Change `FacilitySelectionActiivty` to use `FacilityPickerView`

--- app/src/main/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionActivity.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionActivity.kt
@@ -4,29 +4,21 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.jakewharton.rxbinding2.widget.RxTextView
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import io.reactivex.Observable
-import io.reactivex.rxkotlin.ofType
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.screen_patient_facility_change.*
 import org.simple.clinic.ClinicApp
 import org.simple.clinic.R
 import org.simple.clinic.bindUiToController
+import org.simple.clinic.di.InjectorProviderContextWrapper
 import org.simple.clinic.facility.Facility
-import org.simple.clinic.facility.change.FacilityListItem
-import org.simple.clinic.location.LOCATION_PERMISSION
 import org.simple.clinic.util.LocaleOverrideContextWrapper
-import org.simple.clinic.util.RuntimePermissions
 import org.simple.clinic.util.wrap
-import org.simple.clinic.widgets.ItemAdapter
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
-import org.simple.clinic.widgets.displayedChildResId
 import java.util.Locale
 import javax.inject.Inject
 
@@ -46,14 +38,9 @@ class FacilitySelectionActivity : AppCompatActivity() {
   @Inject
   lateinit var controller: FacilitySelectionActivityController
 
-  @Inject
-  lateinit var runtimePermissions: RuntimePermissions
-
   private val onDestroys = PublishSubject.create<ScreenDestroyed>()
 
   private lateinit var component: FacilitySelectionActivityComponent
-
-  private val recyclerViewAdapter = ItemAdapter(FacilityListItem.Differ())
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -63,23 +50,13 @@ class FacilitySelectionActivity : AppCompatActivity() {
         ui = this,
         events = Observable.merge(
             screenCreates(),
-            searchQueryChanges(),
-            locationPermissionChanges(),
             facilityClicks()
         ),
         controller = controller,
         screenDestroys = onDestroys
     )
 
-    toolbarWithSearch.setNavigationOnClickListener {
-      finish()
-    }
-    toolbarWithoutSearch.setNavigationOnClickListener {
-      finish()
-    }
-
-    facilityList.layoutManager = LinearLayoutManager(this)
-    facilityList.adapter = recyclerViewAdapter
+    facilityPickerView.backClicked = this@FacilitySelectionActivity::finish
   }
 
   override fun attachBaseContext(baseContext: Context) {
@@ -93,6 +70,7 @@ class FacilitySelectionActivity : AppCompatActivity() {
     val wrappedContext = baseContext
         .wrap { LocaleOverrideContextWrapper.wrap(it, locale) }
         .wrap { ViewPumpContextWrapper.wrap(it) }
+        .wrap { InjectorProviderContextWrapper.wrap(it, component) }
 
     super.attachBaseContext(wrappedContext)
   }
@@ -104,40 +82,10 @@ class FacilitySelectionActivity : AppCompatActivity() {
 
   private fun screenCreates() = Observable.just<UiEvent>(ScreenCreated())
 
-  private fun searchQueryChanges() =
-      RxTextView
-          .textChanges(searchQueryEditText)
-          .map { text -> FacilitySelectionSearchQueryChanged(text.toString()) }
-
-  private fun locationPermissionChanges(): Observable<UiEvent> {
-    val permissionResult = runtimePermissions.check(LOCATION_PERMISSION)
-    return Observable.just(FacilitySelectionLocationPermissionChanged(permissionResult))
-  }
-
-  private fun facilityClicks() =
-      recyclerViewAdapter
-          .itemEvents
-          .ofType<FacilityListItem.FacilityItemClicked>()
-          .map { FacilitySelected(it.facility) }
-
-  fun updateFacilities(facilityItems: List<FacilityListItem>) {
-    recyclerViewAdapter.submitList(facilityItems)
-  }
-
-  fun showToolbarWithSearchField() {
-    toolbarContainer.displayedChildResId = toolbarWithSearch.id
-  }
-
-  fun showToolbarWithoutSearchField() {
-    toolbarContainer.displayedChildResId = toolbarWithoutSearch.id
-  }
-
-  fun showProgressIndicator() {
-    progress.visibility = RelativeLayout.VISIBLE
-  }
-
-  fun hideProgressIndicator() {
-    progress.visibility = RelativeLayout.GONE
+  private fun facilityClicks(): Observable<UiEvent> {
+    return Observable.create { emitter ->
+      facilityPickerView.facilitySelectedCallback = { emitter.onNext(FacilitySelected(it)) }
+    }
   }
 
   fun sendSelectedFacility(selectedFacility: Facility) {
--- app/src/test/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionActivityControllerTest.kt
+++ app/src/test/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionActivityControllerTest.kt
@@ -1,40 +1,15 @@
 package org.simple.clinic.scheduleappointment.facilityselection
 
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.eq
-import com.nhaarman.mockitokotlin2.inOrder
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.never
-import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
-import io.reactivex.Observable
-import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
-import junitparams.JUnitParamsRunner
-import junitparams.Parameters
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.simple.clinic.TestData
-import org.simple.clinic.facility.FacilityRepository
-import org.simple.clinic.facility.change.FacilityChangeConfig
-import org.simple.clinic.facility.change.FacilityListItem
-import org.simple.clinic.facility.change.FacilityListItemBuilder
-import org.simple.clinic.location.Coordinates
-import org.simple.clinic.location.LocationUpdate
-import org.simple.clinic.location.LocationUpdate.Unavailable
-import org.simple.clinic.location.ScreenLocationUpdates
-import org.simple.clinic.platform.util.RuntimePermissionResult
-import org.simple.clinic.user.UserSession
-import org.simple.clinic.util.Distance
 import org.simple.clinic.util.RxErrorsRule
-import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
-import java.time.Duration
 
-@RunWith(JUnitParamsRunner::class)
 class FacilitySelectionActivityControllerTest {
 
   @get:Rule
@@ -42,78 +17,16 @@ class FacilitySelectionActivityControllerTest {
 
   private val uiEvents = PublishSubject.create<UiEvent>()!!
   private val screen = mock<FacilitySelectionActivity>()
-  private val facilityRepository = mock<FacilityRepository>()
-  private val userSession = mock<UserSession>()
-  private val listItemBuilder = mock<FacilityListItemBuilder>()
-  private val user = TestData.loggedInUser()
-  private val screenLocationUpdates = mock<ScreenLocationUpdates>()
-
-  private val configTemplate = FacilityChangeConfig(
-      locationListenerExpiry = Duration.ofSeconds(0),
-      locationUpdateInterval = Duration.ofSeconds(0),
-      proximityThresholdForNearbyFacilities = Distance.ofKilometers(0.0),
-      staleLocationThreshold = Duration.ofSeconds(0))
-  private val configProvider = BehaviorSubject.createDefault(configTemplate)
 
   private lateinit var controller: FacilitySelectionActivityController
 
   @Before
   fun setUp() {
-    controller = FacilitySelectionActivityController(
-        facilityRepository = facilityRepository,
-        userSession = userSession,
-        configProvider = configProvider,
-        listItemBuilder = listItemBuilder,
-        screenLocationUpdates = screenLocationUpdates
-    )
-
-    whenever(userSession.requireLoggedInUser()).thenReturn(Observable.just(user))
+    controller = FacilitySelectionActivityController()
 
     uiEvents
         .compose(controller)
         .subscribe { uiChange -> uiChange(screen) }
-  }
-
-  @Test
-  fun `when screen starts then facilities UI models should be created`() {
-    val facility1 = TestData.facility()
-    val facility2 = TestData.facility()
-    val facilities = listOf(facility1, facility2)
-    whenever(facilityRepository.facilitiesInCurrentGroup(user = user)).thenReturn(Observable.just(facilities, facilities))
-    whenever(screenLocationUpdates.streamUserLocation(any(), any(), any())).thenReturn(Observable.just(Unavailable))
-
-    val searchQuery = ""
-    val facilityListItems = emptyList<FacilityListItem>()
-    whenever(listItemBuilder.build(any(), any(), any(), any())).thenReturn(facilityListItems)
-
-    uiEvents.onNext(FacilitySelectionUserLocationUpdated(Unavailable))
-    uiEvents.onNext(FacilitySelectionSearchQueryChanged(searchQuery))
-
-    verify(listItemBuilder, times(2)).build(
-        facilities = facilities,
-        searchQuery = searchQuery,
-        userLocation = null,
-        proximityThreshold = configTemplate.proximityThresholdForNearbyFacilities)
-    verify(screen, times(2)).updateFacilities(facilityListItems)
-  }
-
-  @Test
-  fun `when search query is changed then the query should be used for fetching filtered facilities`() {
-    val facilities = listOf(
-        TestData.facility(name = "Facility 1"),
-        TestData.facility(name = "Facility 2"))
-    whenever(facilityRepository.facilitiesInCurrentGroup(any(), eq(user))).thenReturn(Observable.just(facilities))
-    whenever(screenLocationUpdates.streamUserLocation(any(), any(), any())).thenReturn(Observable.just(Unavailable))
-
-    uiEvents.onNext(ScreenCreated())
-    uiEvents.onNext(FacilitySelectionUserLocationUpdated(Unavailable))
-    uiEvents.onNext(FacilitySelectionSearchQueryChanged(query = "F"))
-    uiEvents.onNext(FacilitySelectionSearchQueryChanged(query = "Fa"))
-    uiEvents.onNext(FacilitySelectionSearchQueryChanged(query = "Fac"))
-
-    verify(facilityRepository).facilitiesInCurrentGroup(searchQuery = "F", user = user)
-    verify(facilityRepository).facilitiesInCurrentGroup(searchQuery = "Fa", user = user)
-    verify(facilityRepository).facilitiesInCurrentGroup(searchQuery = "Fac", user = user)
   }
 
   @Test
@@ -124,105 +37,4 @@ class FacilitySelectionActivityControllerTest {
 
     verify(screen).sendSelectedFacility(newFacility)
   }
-
-  @Test
-  fun `when screen is started and location is not available, facilities should be shown`() {
-    val facilities = listOf(
-        TestData.facility(name = "Facility 1"),
-        TestData.facility(name = "Facility 2"))
-    whenever(facilityRepository.facilitiesInCurrentGroup(any(), any())).thenReturn(Observable.just(facilities))
-    whenever(screenLocationUpdates.streamUserLocation(any(), any(), any())).thenReturn(Observable.just(Unavailable))
-
-    uiEvents.onNext(ScreenCreated())
-    uiEvents.onNext(FacilitySelectionSearchQueryChanged(""))
-    uiEvents.onNext(FacilitySelectionLocationPermissionChanged(RuntimePermissionResult.DENIED))
-
-    verify(screen).updateFacilities(any())
-  }
-
-  @Test
-  fun `when screen starts and location permission is available then progress indicator should be shown`() {
-    whenever(screenLocationUpdates.streamUserLocation(any(), any(), any())).thenReturn(Observable.just(Unavailable))
-
-    uiEvents.onNext(ScreenCreated())
-    uiEvents.onNext(FacilitySelectionLocationPermissionChanged(RuntimePermissionResult.GRANTED))
-    uiEvents.onNext(FacilitySelectionLocationPermissionChanged(RuntimePermissionResult.GRANTED))
-
-    verify(screen, times(1)).showProgressIndicator()
-  }
-
-  @Test
-  fun `when screen starts and location permission was denied then progress indicator should not be shown`() {
-    whenever(screenLocationUpdates.streamUserLocation(any(), any(), any())).thenReturn(Observable.just(Unavailable))
-
-    uiEvents.onNext(ScreenCreated())
-    uiEvents.onNext(FacilitySelectionLocationPermissionChanged(RuntimePermissionResult.DENIED))
-
-    verify(screen, never()).showProgressIndicator()
-  }
-
-  @Test
-  @Parameters(method = "params for location updates")
-  fun `when a location update is received then progress indicator should be hidden`(
-      locationUpdate: LocationUpdate
-  ) {
-    whenever(screenLocationUpdates.streamUserLocation(any(), any(), any())).thenReturn(Observable.just(locationUpdate))
-
-    uiEvents.onNext(ScreenCreated())
-    uiEvents.onNext(FacilitySelectionLocationPermissionChanged(RuntimePermissionResult.GRANTED))
-
-    verify(screen).hideProgressIndicator()
-  }
-
-  @Suppress("unused")
-  fun `params for location updates`() = listOf(
-      Unavailable,
-      LocationUpdate.Available(location = Coordinates(0.0, 0.0), timeSinceBootWhenRecorded = Duration.ofNanos(0))
-  )
-
-  @Test
-  fun `when both facilities and location are fetched only then should facilities be shown`() {
-    configProvider.onNext(configTemplate.copy(locationListenerExpiry = Duration.ofSeconds(5)))
-
-    val facilities = listOf(
-        TestData.facility(name = "Facility 1"),
-        TestData.facility(name = "Facility 2"))
-    whenever(facilityRepository.facilitiesInCurrentGroup(any(), any())).thenReturn(Observable.just(facilities))
-
-    val locationUpdates = PublishSubject.create<LocationUpdate>()
-    whenever(screenLocationUpdates.streamUserLocation(any(), any(), any())).thenReturn(locationUpdates)
-
-    uiEvents.run {
-      onNext(ScreenCreated())
-      onNext(FacilitySelectionSearchQueryChanged(""))
-      onNext(FacilitySelectionLocationPermissionChanged(RuntimePermissionResult.GRANTED))
-    }
-    verify(screen, never()).updateFacilities(any())
-
-    locationUpdates.onNext(LocationUpdate.Available(Coordinates(0.0, 0.0), timeSinceBootWhenRecorded = Duration.ofNanos(0)))
-    verify(screen).updateFacilities(any())
-  }
-
-  @Test
-  @Parameters(method = "params for user location updates")
-  fun `search field should only be shown when a user location update is received`(
-      locationUpdate: LocationUpdate
-  ) {
-    whenever(screenLocationUpdates.streamUserLocation(any(), any(), any())).thenReturn(Observable.just(locationUpdate))
-    uiEvents.onNext(ScreenCreated())
-
-    val inOrder = inOrder(screen)
-    inOrder.verify(screen).showToolbarWithoutSearchField()
-
-    uiEvents.onNext(FacilitySelectionUserLocationUpdated(Unavailable))
-    inOrder.verify(screen).showToolbarWithSearchField()
-  }
-
-  @Suppress("unused")
-  fun `params for user location updates`(): List<Any> {
-    return listOf(
-        Unavailable,
-        LocationUpdate.Available(location = Coordinates(0.0, 0.0), timeSinceBootWhenRecorded = Duration.ofNanos(0)))
-  }
-
 }

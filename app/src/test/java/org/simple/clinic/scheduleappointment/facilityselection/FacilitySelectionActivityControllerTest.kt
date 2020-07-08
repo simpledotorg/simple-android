0052f2a9752e696df989645cfa2585b60cb45b9b
Vinay Shenoy <vinay@obvious.in>
2020-07-08 11:29:58 +0530

    Setup controller in each test

--- app/src/test/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionActivityControllerTest.kt
+++ app/src/test/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionActivityControllerTest.kt
@@ -2,8 +2,9 @@ package org.simple.clinic.scheduleappointment.facilityselection
 
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.PublishSubject
-import org.junit.Before
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.simple.clinic.TestData
@@ -15,26 +16,31 @@ class FacilitySelectionActivityControllerTest {
   @get:Rule
   val rxErrorsRule = RxErrorsRule()
 
-  private val uiEvents = PublishSubject.create<UiEvent>()!!
+  private val uiEvents = PublishSubject.create<UiEvent>()
   private val screen = mock<FacilitySelectionActivity>()
 
-  private lateinit var controller: FacilitySelectionActivityController
+  private lateinit var controllerSubscription: Disposable
 
-  @Before
-  fun setUp() {
-    controller = FacilitySelectionActivityController()
-
-    uiEvents
-        .compose(controller)
-        .subscribe { uiChange -> uiChange(screen) }
+  @After
+  fun tearDown() {
+    controllerSubscription.dispose()
   }
 
   @Test
   fun `when facility is selected then it should be passed back as result`() {
     val newFacility = TestData.facility()
+    setupController()
 
     uiEvents.onNext(FacilitySelected(newFacility))
 
     verify(screen).sendSelectedFacility(newFacility)
+  }
+
+  private fun setupController() {
+    val controller = FacilitySelectionActivityController()
+
+    controllerSubscription = uiEvents
+        .compose(controller)
+        .subscribe { uiChange -> uiChange(screen) }
   }
 }

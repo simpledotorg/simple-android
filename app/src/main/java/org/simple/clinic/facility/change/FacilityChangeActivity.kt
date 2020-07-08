19a5214704dc041443b3232b1f1faf0f59f1f595
Vinay Shenoy <vinay@obvious.in>
2020-07-08 17:27:57 +0530

    Remove `FacilityChangeActivityController`

--- app/src/main/java/org/simple/clinic/facility/change/FacilityChangeActivity.kt
+++ app/src/main/java/org/simple/clinic/facility/change/FacilityChangeActivity.kt
@@ -8,12 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
-import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.screen_facility_change.*
 import org.simple.clinic.ClinicApp
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
-import org.simple.clinic.bindUiToController
 import org.simple.clinic.di.InjectorProviderContextWrapper
 import org.simple.clinic.facility.Facility
 import org.simple.clinic.facility.change.confirm.ConfirmFacilityChangeSheet
@@ -23,16 +21,11 @@ import org.simple.clinic.util.LocaleOverrideContextWrapper
 import org.simple.clinic.util.RuntimePermissions
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.util.wrap
-import org.simple.clinic.widgets.ScreenCreated
-import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
 import java.util.Locale
 import javax.inject.Inject
 
 class FacilityChangeActivity : AppCompatActivity(), FacilityChangeUi {
-
-  @Inject
-  lateinit var controller: FacilityChangeActivityController
 
   @Inject
   lateinit var locale: Locale
@@ -43,14 +36,8 @@ class FacilityChangeActivity : AppCompatActivity(), FacilityChangeUi {
   @Inject
   lateinit var effectHandlerFactory: FacilityChangeEffectHandler.Factory
 
-  private val onDestroys = PublishSubject.create<ScreenDestroyed>()
-
   private val events by unsafeLazy {
-    Observable
-        .merge(
-            screenCreates(),
-            facilityClicks()
-        )
+    facilityClicks()
         .compose(ReportAnalyticsEvents())
         .share()
   }
@@ -71,13 +58,6 @@ class FacilityChangeActivity : AppCompatActivity(), FacilityChangeUi {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.screen_facility_change)
-
-    bindUiToController(
-        ui = this,
-        events = events,
-        controller = controller,
-        screenDestroys = onDestroys
-    )
 
     setupUiComponents()
 
@@ -110,11 +90,6 @@ class FacilityChangeActivity : AppCompatActivity(), FacilityChangeUi {
     super.onSaveInstanceState(outState)
   }
 
-  override fun onDestroy() {
-    onDestroys.onNext(ScreenDestroyed())
-    super.onDestroy()
-  }
-
   private fun setupDi() {
     component = ClinicApp.appComponent
         .facilityChangeComponentBuilder()
@@ -127,8 +102,6 @@ class FacilityChangeActivity : AppCompatActivity(), FacilityChangeUi {
   private fun setupUiComponents() {
     facilityPickerView.backClicked = this@FacilityChangeActivity::finish
   }
-
-  private fun screenCreates() = Observable.just(ScreenCreated())
 
   private fun facilityClicks(): Observable<UiEvent> {
     return Observable.create { emitter ->

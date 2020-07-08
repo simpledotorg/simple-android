5001bc4e0b0cb5cfa8536d6bc3f03bce36bdd608
Vinay Shenoy <vinay@obvious.in>
2020-07-08 14:31:44 +0530

    Rename `FacilitySelectionActivityControllerTest`

--- app/src/test/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionLogicTest.kt
+++ app/src/test/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionLogicTest.kt
@@ -16,7 +16,7 @@ import org.simple.clinic.widgets.UiEvent
 import org.simple.mobius.migration.MobiusTestFixture
 import java.util.UUID
 
-class FacilitySelectionActivityControllerTest {
+class FacilitySelectionLogicTest {
 
   @get:Rule
   val rxErrorsRule = RxErrorsRule()

56a3928df7c864d8ac2cea4d7fe8afd8a112790e
Vinay Shenoy <vinay@obvious.in>
2020-07-08 14:01:54 +0530

    Change `FacilityChangeActivity` to use `FacilityPickerView`

--- app/src/main/java/org/simple/clinic/facility/change/FacilityChangeActivityController.kt
+++ app/src/main/java/org/simple/clinic/facility/change/FacilityChangeActivityController.kt
@@ -3,16 +3,11 @@ package org.simple.clinic.facility.change
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
-import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.rxkotlin.withLatestFrom
 import org.simple.clinic.ReplayUntilScreenIsDestroyed
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.facility.FacilityRepository
-import org.simple.clinic.location.LocationUpdate.Available
-import org.simple.clinic.location.LocationUpdate.Unavailable
-import org.simple.clinic.location.ScreenLocationUpdates
-import org.simple.clinic.platform.util.RuntimePermissionResult.GRANTED
 import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.filterAndUnwrapJust
 import org.simple.clinic.widgets.ScreenCreated
@@ -24,96 +19,15 @@ typealias UiChange = (Ui) -> Unit
 
 class FacilityChangeActivityController @Inject constructor(
     private val facilityRepository: FacilityRepository,
-    private val userSession: UserSession,
-    private val configProvider: Observable<FacilityChangeConfig>,
-    private val listItemBuilder: FacilityListItemBuilder,
-    private val screenLocationUpdates: ScreenLocationUpdates
+    private val userSession: UserSession
 ) : ObservableTransformer<UiEvent, UiChange> {
 
   override fun apply(events: Observable<UiEvent>): ObservableSource<UiChange> {
     val replayedEvents = ReplayUntilScreenIsDestroyed(events)
-        .compose(locationUpdates())
         .compose(ReportAnalyticsEvents())
         .replay()
 
-    return Observable.mergeArray(
-        showProgressForReadingLocation(replayedEvents),
-        showFacilities(replayedEvents),
-        toggleSearchFieldInToolbar(replayedEvents),
-        confirmFacilityChange(replayedEvents))
-  }
-
-  private fun locationUpdates() = ObservableTransformer<UiEvent, UiEvent> { events ->
-    val locationUpdates = Observables
-        .combineLatest(events.ofType<ScreenCreated>(), configProvider) { _, config -> config }
-        .switchMap { config ->
-          screenLocationUpdates.streamUserLocation(
-              updateInterval = config.locationUpdateInterval,
-              timeout = config.locationListenerExpiry,
-              discardOlderThan = config.staleLocationThreshold
-          )
-        }
-        .take(1)
-        .map(::FacilityChangeUserLocationUpdated)
-
-    events.mergeWith(locationUpdates)
-  }
-
-  private fun showProgressForReadingLocation(events: Observable<UiEvent>): Observable<UiChange> {
-    val screenCreations = events.ofType<ScreenCreated>()
-    val locationPermissionChanges = events
-        .ofType<FacilityChangeLocationPermissionChanged>()
-        .map { it.result }
-
-    val showProgressWhenPermissionIsAvailable = Observables.combineLatest(screenCreations, locationPermissionChanges)
-        .take(1)
-        .filter { (_, permissionResult) -> permissionResult == GRANTED }
-        .map { { ui: Ui -> ui.showProgressIndicator() } }
-
-    val hideProgress = events
-        .ofType<FacilityChangeUserLocationUpdated>()
-        .map { { ui: Ui -> ui.hideProgressIndicator() } }
-
-    return showProgressWhenPermissionIsAvailable.mergeWith(hideProgress)
-  }
-
-  private fun showFacilities(events: Observable<UiEvent>): Observable<UiChange> {
-    val searchQueryChanges = events
-        .ofType<FacilityChangeSearchQueryChanged>()
-        .map { it.query }
-
-    val locationUpdates = events
-        .ofType<FacilityChangeUserLocationUpdated>()
-        .map { it.location }
-
-    val filteredFacilityListItems = Observables
-        .combineLatest(searchQueryChanges, locationUpdates, configProvider)
-        .switchMap { (query, locationUpdate, config) ->
-          val userLocation = when (locationUpdate) {
-            is Available -> locationUpdate.location
-            is Unavailable -> null
-          }
-
-          userSession.requireLoggedInUser()
-              .switchMap { user -> facilityRepository.facilitiesInCurrentGroup(query, user) }
-              .map {
-                listItemBuilder.build(
-                    facilities = it,
-                    searchQuery = query,
-                    userLocation = userLocation,
-                    proximityThreshold = config.proximityThresholdForNearbyFacilities)
-              }
-        }
-
-    return filteredFacilityListItems
-        .map { listItems -> { ui: Ui -> ui.updateFacilities(listItems) } }
-  }
-
-  private fun toggleSearchFieldInToolbar(events: Observable<UiEvent>): Observable<UiChange> {
-    return events
-        .ofType<FacilityChangeUserLocationUpdated>()
-        .map { { ui: Ui -> ui.showToolbarWithSearchField() } }
-        .startWith(Observable.just({ ui: Ui -> ui.showToolbarWithoutSearchField() }))
+    return confirmFacilityChange(replayedEvents)
   }
 
   private fun confirmFacilityChange(events: Observable<UiEvent>): Observable<UiChange> {
--- app/src/main/java/org/simple/clinic/facility/change/confirm/FacilityChangeComponent.kt
+++ app/src/main/java/org/simple/clinic/facility/change/confirm/FacilityChangeComponent.kt
@@ -2,10 +2,14 @@ package org.simple.clinic.facility.change.confirm
 
 import dagger.Subcomponent
 import org.simple.clinic.activity.BindsActivity
+import org.simple.clinic.di.AssistedInjectModule
 import org.simple.clinic.facility.change.FacilityChangeActivity
+import org.simple.clinic.facilitypicker.FacilityPickerView
 
-@Subcomponent
-interface FacilityChangeComponent {
+@Subcomponent(
+    modules = [AssistedInjectModule::class]
+)
+interface FacilityChangeComponent: FacilityPickerView.Injector {
 
   fun inject(activity: FacilityChangeActivity)
 

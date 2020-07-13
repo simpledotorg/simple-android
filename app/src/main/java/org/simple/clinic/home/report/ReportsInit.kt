c9350d7321c196a6f237f9d7579cf89707cc8eb6
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-07-13 09:57:13 +0530

    Add no-op Mobius classes to `ReportsScreen` package

---
+++ app/src/main/java/org/simple/clinic/home/report/ReportsInit.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.home.report
+
+import com.spotify.mobius.First
+import com.spotify.mobius.Init
+import org.simple.clinic.mobius.first
+
+class ReportsInit : Init<ReportsModel, ReportsEffect> {
+  override fun init(model: ReportsModel): First<ReportsModel, ReportsEffect> {
+    return first(model)
+  }
+}
---
+++ app/src/main/java/org/simple/clinic/home/report/ReportsModel.kt
@@ -0,0 +1,7 @@
+package org.simple.clinic.home.report
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class ReportsModel : Parcelable

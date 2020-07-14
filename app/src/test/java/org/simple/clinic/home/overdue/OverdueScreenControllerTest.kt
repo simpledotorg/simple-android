50ca3a76aeea1465d8dd334696429f314941d256
Vinay Shenoy <vinay@obvious.in>
2020-07-14 16:10:30 +0530

    Fix UUID instances in `OverdueScreenControllerTest`

--- app/src/test/java/org/simple/clinic/home/overdue/OverdueScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/home/overdue/OverdueScreenControllerTest.kt
@@ -30,8 +30,8 @@ class OverdueScreenControllerTest {
   private val repository = mock<AppointmentRepository>()
   private val facilityRepository = mock<FacilityRepository>()
   private val userSession = mock<UserSession>()
-  private val facility = TestData.facility()
-  private val user = TestData.loggedInUser()
+  private val facility = TestData.facility(uuid = UUID.fromString("f4430584-eeaf-4352-b1f5-c21cc96faa6c"))
+  private val user = TestData.loggedInUser(uuid = UUID.fromString("977c9f22-c333-477f-826b-00fa601f16ab"))
   private val dateOnClock = LocalDate.parse("2018-01-01")
   private val userClock = TestUserClock(dateOnClock)
 
@@ -64,7 +64,7 @@ class OverdueScreenControllerTest {
 
   @Test
   fun `when showPhoneMaskBottomSheet config is true and call patient is clicked then open phone mask bottom sheet`() {
-    val patientUuid = UUID.randomUUID()
+    val patientUuid = UUID.fromString("55daf914-82df-4c41-ba1b-131216fed30c")
 
     uiEvents.onNext(CallPatientClicked(patientUuid))
 
@@ -75,8 +75,20 @@ class OverdueScreenControllerTest {
   fun `when screen is created then the overdue appointments must be displayed`() {
     // given
     val overdueAppointments = listOf(
-        TestData.overdueAppointment(isHighRisk = true),
-        TestData.overdueAppointment(isHighRisk = false)
+        TestData.overdueAppointment(
+            patientUuid = UUID.fromString("d79923ea-bedd-484a-ae28-465be3051f45"),
+            appointmentUuid = UUID.fromString("5ee43e06-44fd-413e-a222-23c79f0b0f3a"),
+            phoneNumberUuid = UUID.fromString("1f8c184c-09b2-408c-a87e-96a144b7cf22"),
+            facilityUuid = facility.uuid,
+            isHighRisk = true
+        ),
+        TestData.overdueAppointment(
+            patientUuid = UUID.fromString("d89a1040-e732-4dd9-b975-eea7b9c53414"),
+            appointmentUuid = UUID.fromString("3e4a7da3-1921-4260-93bd-ff9f1dc98476"),
+            phoneNumberUuid = UUID.fromString("50c1fb5c-0406-4e98-8150-696accdab113"),
+            facilityUuid = facility.uuid,
+            isHighRisk = false
+        )
     )
     whenever(repository.overdueAppointments(dateOnClock, facility))
         .thenReturn(Observable.just(overdueAppointments))

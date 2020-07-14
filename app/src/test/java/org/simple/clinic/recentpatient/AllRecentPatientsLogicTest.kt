e6937a2003959633af7973d92a17988c010f7502
Vinay Shenoy <vinay@obvious.in>
2020-07-14 12:01:29 +0530

    Inject current facility directly into `AllRecentPatientsEffectHandler`

--- app/src/test/java/org/simple/clinic/recentpatient/AllRecentPatientsLogicTest.kt
+++ app/src/test/java/org/simple/clinic/recentpatient/AllRecentPatientsLogicTest.kt
@@ -4,6 +4,7 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
 import com.nhaarman.mockitokotlin2.whenever
+import dagger.Lazy
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.subjects.PublishSubject
@@ -12,16 +13,13 @@ import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.simple.clinic.TestData
-import org.simple.clinic.facility.FacilityRepository
 import org.simple.clinic.patient.Age
 import org.simple.clinic.patient.Gender
 import org.simple.clinic.patient.PatientRepository
 import org.simple.clinic.recentpatientsview.RecentPatientItem
-import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.RxErrorsRule
 import org.simple.clinic.util.TestUserClock
 import org.simple.clinic.util.scheduler.TestSchedulersProvider
-import org.simple.clinic.util.toOptional
 import org.simple.clinic.widgets.UiEvent
 import org.simple.mobius.migration.MobiusTestFixture
 import java.time.Duration
@@ -37,12 +35,9 @@ class AllRecentPatientsLogicTest {
 
   private val ui: AllRecentPatientsUi = mock()
   private val uiActions: AllRecentPatientsUiActions = mock()
-  private val userSession: UserSession = mock()
   private val patientRepository: PatientRepository = mock()
-  private val facilityRepository: FacilityRepository = mock()
 
   private val uiEvents: Subject<UiEvent> = PublishSubject.create()
-  private val loggedInUser = TestData.loggedInUser(uuid = UUID.fromString("d192d336-1831-439d-ab6c-2b9228e0a06a"))
   private val facility = TestData.facility(uuid = UUID.fromString("b8e0fe64-6c31-43df-837a-80ce05b80cea"))
   private val dateFormatter = DateTimeFormatter.ISO_INSTANT
   private val userClock = TestUserClock(LocalDate.parse("2020-02-14"))
@@ -167,14 +162,10 @@ class AllRecentPatientsLogicTest {
   }
 
   private fun setupController() {
-    whenever(userSession.loggedInUser()).thenReturn(Observable.just(loggedInUser.toOptional()))
-    whenever(facilityRepository.currentFacility(loggedInUser)).thenReturn(Observable.just(facility))
-
     val effectHandler = AllRecentPatientsEffectHandler(
         schedulersProvider = TestSchedulersProvider.trampoline(),
-        userSession = userSession,
-        facilityRepository = facilityRepository,
         patientRepository = patientRepository,
+        currentFacility = Lazy { facility },
         uiActions = uiActions
     )
 

a1925fd12b02dca2f455ad4c68d587ba24e05fb9
Vinay Shenoy <vinay@obvious.in>
2020-07-14 11:48:15 +0530

    Rename `RecentPatientsScreenControllerTest`

--- app/src/test/java/org/simple/clinic/recentpatient/AllRecentPatientsLogicTest.kt
+++ app/src/test/java/org/simple/clinic/recentpatient/AllRecentPatientsLogicTest.kt
@@ -32,7 +32,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.UUID
 
-class RecentPatientsScreenControllerTest {
+class AllRecentPatientsLogicTest {
 
   @get:Rule
   val rxErrorsRule = RxErrorsRule()

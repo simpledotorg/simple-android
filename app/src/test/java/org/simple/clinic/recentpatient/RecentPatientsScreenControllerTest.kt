376d5e6fb1b6affc6dfdcd601b84e0d5c21f54da
Vinay Shenoy <code@vinaysshenoy.com>
2020-07-21 11:16:25 +0530

    Migrate recent patients screen to Mobius: Stabilize tests (#1518)
    * Remove unnecessary parameterized test runner
    
    * Remove unnecessary overriding of IO scheduler
    
    * Fix the time in the test clock for all tests
    
    * Fix UUID instances in tests
    
    * Setup controller separately in each test
    
    * Trigger screen created event in each test
    
    * Tighten assertions in tests
    
    * Add "Given-When-Then" assertions to all tests
    
    * Update CHANGELOG

--- app/src/test/java/org/simple/clinic/recentpatient/RecentPatientsScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/recentpatient/RecentPatientsScreenControllerTest.kt
@@ -2,21 +2,19 @@ package org.simple.clinic.recentpatient
 
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
 import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Observable
-import io.reactivex.plugins.RxJavaPlugins
-import io.reactivex.schedulers.Schedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
-import junitparams.JUnitParamsRunner
-import org.junit.Before
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.simple.clinic.TestData
 import org.simple.clinic.facility.FacilityRepository
 import org.simple.clinic.patient.Age
 import org.simple.clinic.patient.Gender
-import org.simple.clinic.TestData
 import org.simple.clinic.patient.PatientRepository
 import org.simple.clinic.user.UserSession
 import org.simple.clinic.util.RxErrorsRule
@@ -30,7 +28,6 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.UUID
 
-@RunWith(JUnitParamsRunner::class)
 class RecentPatientsScreenControllerTest {
 
   @get:Rule
@@ -42,40 +39,25 @@ class RecentPatientsScreenControllerTest {
   private val facilityRepository: FacilityRepository = mock()
 
   private val uiEvents: Subject<UiEvent> = PublishSubject.create()
-  private val loggedInUser = TestData.loggedInUser()
-  private val facility = TestData.facility()
+  private val loggedInUser = TestData.loggedInUser(uuid = UUID.fromString("d192d336-1831-439d-ab6c-2b9228e0a06a"))
+  private val facility = TestData.facility(uuid = UUID.fromString("b8e0fe64-6c31-43df-837a-80ce05b80cea"))
   private val dateFormatter = DateTimeFormatter.ISO_INSTANT
-  private val userClock = TestUserClock()
+  private val userClock = TestUserClock(LocalDate.parse("2020-02-14"))
 
-  private val controller = RecentPatientsScreenController(
-      userSession = userSession,
-      patientRepository = patientRepository,
-      facilityRepository = facilityRepository,
-      userClock = userClock,
-      dateFormatter = dateFormatter
-  )
+  private lateinit var controllerSubscription: Disposable
 
-  @Before
-  fun setUp() {
-    // This is needed because we manually subscribe to the refresh user status
-    // operation on the IO thread, which was causing flakiness in this test.
-    RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
-
-    whenever(userSession.loggedInUser()).thenReturn(Observable.just(loggedInUser.toOptional()))
-    whenever(facilityRepository.currentFacility(loggedInUser)).thenReturn(Observable.just(facility))
-
-    uiEvents
-        .compose(controller)
-        .subscribe { uiChange -> uiChange(screen) }
+  @After
+  fun tearDown() {
+    controllerSubscription.dispose()
   }
 
   @Test
   fun `when screen opens then fetch and set recent patients`() {
-    val patientUuid1 = UUID.randomUUID()
-    val patientUuid2 = UUID.randomUUID()
-    val patientUuid3 = UUID.randomUUID()
+    // given
+    val patientUuid1 = UUID.fromString("b5cfb2e6-0d73-4189-8131-5b91d8c45780")
+    val patientUuid2 = UUID.fromString("47f4281e-d571-4c36-a7fc-039d0a289e2f")
+    val patientUuid3 = UUID.fromString("073c9099-5f60-4bc3-b40e-2c67f5ac74e4")
 
-    userClock.setDate(LocalDate.parse("2020-02-14"))
     val today = Instant.now(userClock)
     val yesterday = today.minus(Duration.ofDays(1))
     val twoDaysAgo = today.minus(Duration.ofDays(2))
@@ -106,8 +88,10 @@ class RecentPatientsScreenControllerTest {
         )
     )))
 
-    uiEvents.onNext(ScreenCreated())
+    // when
+    setupController()
 
+    // then
     verify(screen).updateRecentPatients(listOf(
         RecentPatientItem(
             uuid = patientUuid1,
@@ -140,14 +124,62 @@ class RecentPatientsScreenControllerTest {
             isNewRegistration = false
         )
     ))
+    verifyNoMoreInteractions(screen)
   }
 
   @Test
   fun `when any recent patient item is clicked, then open patient summary`() {
-    val patientUuid = UUID.randomUUID()
+    // given
+    val patientUuid = UUID.fromString("c5070a89-d848-4822-80c2-d7c306e437b1")
+    val today = Instant.now(userClock)
+
+    whenever(patientRepository.recentPatients(facility.uuid)).thenReturn(Observable.just(listOf(
+        TestData.recentPatient(
+            uuid = patientUuid,
+            fullName = "Ajay Kumar",
+            age = Age(42, Instant.now(userClock)),
+            gender = Gender.Transgender,
+            updatedAt = today,
+            patientRecordedAt = today
+        )
+    )))
+
+    // when
+    setupController()
     uiEvents.onNext(RecentPatientItemClicked(patientUuid = patientUuid))
 
+    // then
+    verify(screen).updateRecentPatients(listOf(RecentPatientItem(
+        uuid = patientUuid,
+        name = "Ajay Kumar",
+        age = 42,
+        gender = Gender.Transgender,
+        lastSeen = Instant.parse("2020-02-14T00:00:00Z"),
+        dateFormatter = dateFormatter,
+        clock = userClock,
+        isNewRegistration = true
+    )))
     verify(screen).openPatientSummary(patientUuid)
+    verifyNoMoreInteractions(screen)
+  }
+
+  private fun setupController() {
+    whenever(userSession.loggedInUser()).thenReturn(Observable.just(loggedInUser.toOptional()))
+    whenever(facilityRepository.currentFacility(loggedInUser)).thenReturn(Observable.just(facility))
+
+    val controller = RecentPatientsScreenController(
+        userSession = userSession,
+        patientRepository = patientRepository,
+        facilityRepository = facilityRepository,
+        userClock = userClock,
+        dateFormatter = dateFormatter
+    )
+
+    controllerSubscription = uiEvents
+        .compose(controller)
+        .subscribe { uiChange -> uiChange(screen) }
+
+    uiEvents.onNext(ScreenCreated())
   }
 }
 

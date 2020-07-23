0198d91f861f021b13832e68f6228b9f9726b4e0
Vinay Shenoy <vinay@obvious.in>
2020-07-23 10:20:56 +0530

    Migrate registration loading screen to Mobius: Sterilize screen (#1525)
    * Remove controller from `RegistrationLoadingScreenControllerTest`
    
    * Rename `RegistrationLoadingScreenControllerTest` -> `RegistrationLoadingLogicTest`
    
    * Remove `RegistrationLoadingScreenController`
    
    * Implement UI actions on screen directly
    
    * Inject current user and facility into `RegistrationLoadingEffectHandler`
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingEffectHandler.kt
@@ -3,19 +3,21 @@ package org.simple.clinic.registration.register
 import com.spotify.mobius.rx2.RxMobius
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import dagger.Lazy
+import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
-import io.reactivex.rxkotlin.Observables
-import org.simple.clinic.facility.FacilityRepository
+import org.simple.clinic.facility.Facility
+import org.simple.clinic.user.User
 import org.simple.clinic.user.UserSession
 import org.simple.clinic.user.registeruser.RegisterUser
-import org.simple.clinic.util.filterAndUnwrapJust
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class RegistrationLoadingEffectHandler @AssistedInject constructor(
     private val schedulers: SchedulersProvider,
     private val userSession: UserSession,
-    private val facilityRepository: FacilityRepository,
     private val registerUser: RegisterUser,
+    private val currentUser: Lazy<User>,
+    private val currentFacility: Lazy<Facility>,
     @Assisted private val uiActions: RegistrationLoadingUiActions
 ) {
 
@@ -37,19 +39,14 @@ class RegistrationLoadingEffectHandler @AssistedInject constructor(
   private fun loadRegistrationDetails(): ObservableTransformer<LoadRegistrationDetails, RegistrationLoadingEvent> {
     return ObservableTransformer { effects ->
       effects
+          .observeOn(schedulers.io())
           .switchMap {
-            val userStream = userSession
-                .loggedInUser()
-                .subscribeOn(schedulers.io())
-                .filterAndUnwrapJust()
-                .take(1)
+            val details = RegistrationDetailsLoaded(
+                user = currentUser.get(),
+                facility = currentFacility.get()
+            )
 
-            val facilityStream = userStream
-                .switchMap(facilityRepository::currentFacility)
-                .subscribeOn(schedulers.io())
-                .take(1)
-
-            Observables.zip(userStream, facilityStream) { user, facility -> RegistrationDetailsLoaded(user, facility) }
+            Observable.just(details)
           }
     }
   }

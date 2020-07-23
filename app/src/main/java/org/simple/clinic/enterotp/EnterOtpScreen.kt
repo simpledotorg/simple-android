bd134b6c8ad6c2cf5d1b0a7b1fc60a5c016b722e
Vinay Shenoy <vinay@obvious.in>
2020-07-23 15:36:46 +0530

    Wire mobius loop into the screen

--- app/src/main/java/org/simple/clinic/enterotp/EnterOtpScreen.kt
+++ app/src/main/java/org/simple/clinic/enterotp/EnterOtpScreen.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.enterotp
 
 import android.content.Context
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
 import android.view.inputmethod.EditorInfo
@@ -10,6 +11,7 @@ import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.view.detaches
 import com.jakewharton.rxbinding3.widget.editorActions
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.ofType
 import kotlinx.android.synthetic.main.screen_enterotp.view.*
 import org.simple.clinic.LOGIN_OTP_LENGTH
 import org.simple.clinic.R
@@ -17,6 +19,7 @@ import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.appconfig.Country
 import org.simple.clinic.bindUiToController
 import org.simple.clinic.di.injector
+import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ScreenCreated
@@ -40,6 +43,9 @@ class EnterOtpScreen(
   @Inject
   lateinit var country: Country
 
+  @Inject
+  lateinit var effectHandlerFactory: EnterOtpEffectHandler.Factory
+
   private val events by unsafeLazy {
     Observable
         .mergeArray(
@@ -49,6 +55,19 @@ class EnterOtpScreen(
         )
         .compose(ReportAnalyticsEvents())
         .share()
+  }
+
+  private val delegate by unsafeLazy {
+    val uiRenderer = EnterOtpUiRenderer(this)
+
+    MobiusDelegate.forView(
+        events = events.ofType(),
+        defaultModel = EnterOtpModel.create(),
+        update = EnterOtpUpdate(),
+        effectHandler = effectHandlerFactory.create(this).build(),
+        init = EnterOtpInit(),
+        modelUpdateListener = uiRenderer::render
+    )
   }
 
   override fun onFinishInflate() {
@@ -67,6 +86,24 @@ class EnterOtpScreen(
 
     otpEntryEditText.showKeyboard()
     backButton.setOnClickListener { goBack() }
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    delegate.start()
+  }
+
+  override fun onDetachedFromWindow() {
+    delegate.stop()
+    super.onDetachedFromWindow()
+  }
+
+  override fun onSaveInstanceState(): Parcelable? {
+    return delegate.onSaveInstanceState(super.onSaveInstanceState())
+  }
+
+  override fun onRestoreInstanceState(state: Parcelable?) {
+    super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
 
   private fun screenCreates() = Observable.just(ScreenCreated())

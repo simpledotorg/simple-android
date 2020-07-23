bbbde4ab2a554b8cb332eaece6750e74acdee366
Vinay Shenoy <vinay@obvious.in>
2020-07-23 16:58:13 +0530

    Render user phone number only when it changes

--- app/src/main/java/org/simple/clinic/enterotp/EnterOtpUiRenderer.kt
+++ app/src/main/java/org/simple/clinic/enterotp/EnterOtpUiRenderer.kt
@@ -4,14 +4,17 @@ import org.simple.clinic.enterotp.ValidationResult.IsNotRequiredLength
 import org.simple.clinic.enterotp.ValidationResult.NotValidated
 import org.simple.clinic.enterotp.ValidationResult.Valid
 import org.simple.clinic.mobius.ViewRenderer
+import org.simple.clinic.util.ValueChangedCallback
 
 class EnterOtpUiRenderer(
     private val ui: EnterOtpUi
 ) : ViewRenderer<EnterOtpModel> {
 
+  private val phoneNumberChangedCallback = ValueChangedCallback<String>()
+
   override fun render(model: EnterOtpModel) {
     if (model.hasLoadedUser) {
-      ui.showUserPhoneNumber(model.user!!.phoneNumber)
+      phoneNumberChangedCallback.pass(model.user!!.phoneNumber, ui::showUserPhoneNumber)
     }
 
     when(model.otpValidationResult) {

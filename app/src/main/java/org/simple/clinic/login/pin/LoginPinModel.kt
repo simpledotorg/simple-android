71642d555ba0521ce94bf5e9da64c0810999ddb2
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-07-24 14:57:41 +0530

    Add `OngoingLoginEntry` to `LoginPinModel`

--- app/src/main/java/org/simple/clinic/login/pin/LoginPinModel.kt
+++ app/src/main/java/org/simple/clinic/login/pin/LoginPinModel.kt
@@ -2,11 +2,12 @@ package org.simple.clinic.login.pin
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.user.OngoingLoginEntry
 
 @Parcelize
-class LoginPinModel : Parcelable {
+data class LoginPinModel(val ongoingLoginEntry: OngoingLoginEntry?) : Parcelable {
 
   companion object {
-    fun create() = LoginPinModel()
+    fun create() = LoginPinModel(null)
   }
 }

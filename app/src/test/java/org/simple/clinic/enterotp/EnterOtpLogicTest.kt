de0910f3ab36d7fb680d41ff8edbda82f8dc8fb3
Vinay Shenoy <vinay@obvious.in>
2020-07-27 15:14:25 +0530

    Rename `EnterOtpScreenControllerTest` -> `EnterOtpLogicTest`

--- app/src/test/java/org/simple/clinic/enterotp/EnterOtpLogicTest.kt
+++ app/src/test/java/org/simple/clinic/enterotp/EnterOtpLogicTest.kt
@@ -41,7 +41,7 @@ import org.simple.clinic.widgets.UiEvent
 import org.simple.mobius.migration.MobiusTestFixture
 import java.util.UUID
 
-class EnterOtpScreenControllerTest {
+class EnterOtpLogicTest {
 
   @get:Rule
   val rxErrorsRule = RxErrorsRule()

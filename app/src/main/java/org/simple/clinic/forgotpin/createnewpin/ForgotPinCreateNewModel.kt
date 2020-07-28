d7f8c3bece7998e35f8ae2013120a2bf43435ba9
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-07-28 10:40:32 +0530

    Track `Facility` in `ForgotPinCreateNewModel`

--- app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewModel.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewModel.kt
@@ -2,15 +2,17 @@ package org.simple.clinic.forgotpin.createnewpin
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.facility.Facility
 import org.simple.clinic.user.User
 
 @Parcelize
 data class ForgotPinCreateNewModel(
-    val user: User?
+    val user: User?,
+    val facility: Facility?
 ) : Parcelable {
 
   companion object {
-    fun create() = ForgotPinCreateNewModel(user = null)
+    fun create() = ForgotPinCreateNewModel(user = null, facility = null)
   }
 
   val hasUser: Boolean

360360238f4c97392a6db3d2c457921219c2a3b5
Vinay Shenoy <vinay@obvious.in>
2020-07-28 10:40:13 +0530

    Add additional `Identifier` to `PatientSearchCriteria`

--- app/src/main/java/org/simple/clinic/patient/PatientSearchCriteria.kt
+++ app/src/main/java/org/simple/clinic/patient/PatientSearchCriteria.kt
@@ -2,12 +2,19 @@ package org.simple.clinic.patient
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.patient.businessid.Identifier
 
-sealed class PatientSearchCriteria : Parcelable {
-
-  @Parcelize
-  data class Name(val patientName: String) : PatientSearchCriteria()
+sealed class PatientSearchCriteria(val additionalIdentifier: Identifier?) : Parcelable {
 
   @Parcelize
-  data class PhoneNumber(val phoneNumber: String) : PatientSearchCriteria()
+  data class Name(
+      val patientName: String,
+      private val _additionalIdentifier: Identifier? = null
+  ) : PatientSearchCriteria(_additionalIdentifier)
+
+  @Parcelize
+  data class PhoneNumber(
+      val phoneNumber: String,
+      private val _additionalIdentifier: Identifier? = null
+  ) : PatientSearchCriteria(_additionalIdentifier)
 }

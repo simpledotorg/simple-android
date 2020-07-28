cb01fd82c4b4ab038dbe1585fafb9e471bdcbecc
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-07-28 14:19:19 +0530

    Remove `ForgotPinCreateNewPinScreenController`

--- app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
@@ -6,20 +6,17 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.RelativeLayout
-import com.jakewharton.rxbinding3.view.detaches
 import com.jakewharton.rxbinding3.widget.editorActions
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
 import kotlinx.android.synthetic.main.screen_forgotpin_createpin.view.*
 import org.simple.clinic.ReportAnalyticsEvents
-import org.simple.clinic.bindUiToController
 import org.simple.clinic.di.injector
 import org.simple.clinic.forgotpin.confirmpin.ForgotPinConfirmPinScreenKey
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ScreenCreated
-import org.simple.clinic.widgets.ScreenDestroyed
 import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.hideKeyboard
 import org.simple.clinic.widgets.showKeyboard
@@ -29,9 +26,6 @@ class ForgotPinCreateNewPinScreen(
     context: Context,
     attributeSet: AttributeSet?
 ) : RelativeLayout(context, attributeSet), ForgotPinCreateNewPinUi, UiActions {
-
-  @Inject
-  lateinit var controller: ForgotPinCreateNewPinScreenController
 
   @Inject
   lateinit var screenRouter: ScreenRouter
@@ -47,7 +41,6 @@ class ForgotPinCreateNewPinScreen(
             pinSubmitClicked()
         )
         .compose(ReportAnalyticsEvents())
-        .share()
   }
 
   private val delegate by unsafeLazy {
@@ -70,13 +63,6 @@ class ForgotPinCreateNewPinScreen(
     }
 
     context.injector<Injector>().inject(this)
-
-    bindUiToController(
-        ui = this,
-        events = events,
-        controller = controller,
-        screenDestroys = detaches().map { ScreenDestroyed() }
-    )
 
     createPinEditText.showKeyboard()
   }

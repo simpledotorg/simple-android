78a207e4e16162e126f63b874e911da27eda17bd
Vinay Shenoy <code@vinaysshenoy.com>
2020-07-30 16:33:16 +0530

    Migrate facility selection screen to Mobius: Stabilize tests (#1551)
    * Rename `screen_patient_facility_change` -> `activity_select_facility`
    
    * Setup controller in each test
    
    * Use static UUIDs in `FacilitySelectionActivityControllerTest`
    
    * Tighten assertions in `FacilitySelectionScreenControllerTest`
    
    * Add "Given-When-Then" assertions to all tests
    
    * Update CHANGELOG

--- app/src/test/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionActivityControllerTest.kt
+++ app/src/test/java/org/simple/clinic/scheduleappointment/facilityselection/FacilitySelectionActivityControllerTest.kt
@@ -2,39 +2,51 @@ package org.simple.clinic.scheduleappointment.facilityselection
 
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.PublishSubject
-import org.junit.Before
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.simple.clinic.TestData
 import org.simple.clinic.util.RxErrorsRule
 import org.simple.clinic.widgets.UiEvent
+import java.util.UUID
 
 class FacilitySelectionActivityControllerTest {
 
   @get:Rule
   val rxErrorsRule = RxErrorsRule()
 
-  private val uiEvents = PublishSubject.create<UiEvent>()!!
+  private val uiEvents = PublishSubject.create<UiEvent>()
   private val screen = mock<FacilitySelectionActivity>()
 
-  private lateinit var controller: FacilitySelectionActivityController
+  private lateinit var controllerSubscription: Disposable
 
-  @Before
-  fun setUp() {
-    controller = FacilitySelectionActivityController()
-
-    uiEvents
-        .compose(controller)
-        .subscribe { uiChange -> uiChange(screen) }
+  @After
+  fun tearDown() {
+    controllerSubscription.dispose()
   }
 
   @Test
   fun `when facility is selected then it should be passed back as result`() {
-    val newFacility = TestData.facility()
+    // given
+    val newFacility = TestData.facility(uuid = UUID.fromString("758f6c2e-2bd9-415f-9aaa-665923034e92"))
 
+    // when
+    setupController()
     uiEvents.onNext(FacilitySelected(newFacility))
 
+    // then
     verify(screen).sendSelectedFacility(newFacility)
+    verifyNoMoreInteractions(screen)
+  }
+
+  private fun setupController() {
+    val controller = FacilitySelectionActivityController()
+
+    controllerSubscription = uiEvents
+        .compose(controller)
+        .subscribe { uiChange -> uiChange(screen) }
   }
 }

0d5a1a8ed916d017e1ab3713b4e8feb3445f7611
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-07-30 17:06:16 +0530

    Migrate forgot pin screen to Mobius - Cleanup & Sterilise (#1550)
    * Remove controller from the `ForgotPinCreateNewPinScreenControllerTest`
    
    * Stop extending UiActions in `ForgotPinCreateNewPinUi`
    
    * Remove `ForgotPinCreateNewPinScreenController`
    
    * Remove `ScreenCreated` event from `ForgotPinCreateNewPinScreen`
    
    * Rename `ForgotPinCreateNewPinScreenControllerTest` to `ForgotPinCreateNewPinScreenLogicTest`
    
    * Inject current facility in `ForgotPinCreateNewEffectHandler`
    
    * Inject current user in `ForgotPinCreateNewEffectHandler`
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
@@ -6,29 +6,24 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.RelativeLayout
-import com.jakewharton.rxbinding3.view.detaches
 import com.jakewharton.rxbinding3.widget.editorActions
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
 import kotlinx.android.synthetic.main.screen_forgotpin_createpin.view.*
 import org.simple.clinic.ReportAnalyticsEvents
-import org.simple.clinic.bindUiToController
 import org.simple.clinic.di.injector
 import org.simple.clinic.forgotpin.confirmpin.ForgotPinConfirmPinScreenKey
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.util.unsafeLazy
-import org.simple.clinic.widgets.ScreenCreated
-import org.simple.clinic.widgets.ScreenDestroyed
-import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.hideKeyboard
 import org.simple.clinic.widgets.showKeyboard
 import javax.inject.Inject
 
-class ForgotPinCreateNewPinScreen(context: Context, attributeSet: AttributeSet?) : RelativeLayout(context, attributeSet), ForgotPinCreateNewPinUi {
-
-  @Inject
-  lateinit var controller: ForgotPinCreateNewPinScreenController
+class ForgotPinCreateNewPinScreen(
+    context: Context,
+    attributeSet: AttributeSet?
+) : RelativeLayout(context, attributeSet), ForgotPinCreateNewPinUi, UiActions {
 
   @Inject
   lateinit var screenRouter: ScreenRouter
@@ -39,12 +34,10 @@ class ForgotPinCreateNewPinScreen(context: Context, attributeSet: AttributeSet?)
   private val events by unsafeLazy {
     Observable
         .merge(
-            screenCreates(),
             pinTextChanges(),
             pinSubmitClicked()
         )
         .compose(ReportAnalyticsEvents())
-        .share()
   }
 
   private val delegate by unsafeLazy {
@@ -68,13 +61,6 @@ class ForgotPinCreateNewPinScreen(context: Context, attributeSet: AttributeSet?)
 
     context.injector<Injector>().inject(this)
 
-    bindUiToController(
-        ui = this,
-        events = events,
-        controller = controller,
-        screenDestroys = detaches().map { ScreenDestroyed() }
-    )
-
     createPinEditText.showKeyboard()
   }
 
@@ -95,8 +81,6 @@ class ForgotPinCreateNewPinScreen(context: Context, attributeSet: AttributeSet?)
   override fun onRestoreInstanceState(state: Parcelable?) {
     super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
-
-  private fun screenCreates(): Observable<UiEvent> = Observable.just(ScreenCreated())
 
   private fun pinTextChanges() =
       createPinEditText

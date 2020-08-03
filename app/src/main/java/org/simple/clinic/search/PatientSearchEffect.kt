22af667a927e29c7222086192a676d475e5541af
Vinay Shenoy <vinay@obvious.in>
2020-08-03 11:20:44 +0530

    Add effect to report input validation errors to Analytics

--- app/src/main/java/org/simple/clinic/search/PatientSearchEffect.kt
+++ app/src/main/java/org/simple/clinic/search/PatientSearchEffect.kt
@@ -1,3 +1,5 @@
 package org.simple.clinic.search
 
 sealed class PatientSearchEffect
+
+data class ReportValidationErrorsToAnalytics(val errors: Set<PatientSearchValidationError>) : PatientSearchEffect()

2d52e7bbe2e4dc3eea5aac9400e0c4ef9d2ad592
Vinay Shenoy <vinay@obvious.in>
2020-08-03 11:39:47 +0530

    Add regression test for sending input validation error

--- app/src/test/java/org/simple/clinic/search/PatientSearchScreenControllerTest.kt
+++ app/src/test/java/org/simple/clinic/search/PatientSearchScreenControllerTest.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.search
 
+import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.clearInvocations
 import com.nhaarman.mockitokotlin2.mock
@@ -12,13 +13,17 @@ import io.reactivex.subjects.PublishSubject
 import junitparams.JUnitParamsRunner
 import junitparams.Parameters
 import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.simple.clinic.TestData
+import org.simple.clinic.analytics.MockAnalyticsReporter
 import org.simple.clinic.patient.PatientSearchCriteria.Name
 import org.simple.clinic.patient.PatientSearchCriteria.PhoneNumber
 import org.simple.clinic.patient.businessid.Identifier
+import org.simple.clinic.platform.analytics.Analytics
+import org.simple.clinic.search.PatientSearchValidationError.INPUT_EMPTY
 import org.simple.clinic.util.RxErrorsRule
 import org.simple.clinic.util.scheduler.TestSchedulersProvider
 import org.simple.clinic.widgets.UiEvent
@@ -39,10 +44,18 @@ class PatientSearchScreenControllerTest {
   private lateinit var testFixture: MobiusTestFixture<PatientSearchModel, PatientSearchEvent, PatientSearchEffect>
   private val uiEvents = PublishSubject.create<UiEvent>()
 
+  private val analyticsReporter = MockAnalyticsReporter()
+
+  @Before
+  fun setUp() {
+    Analytics.addReporter(analyticsReporter)
+  }
+
   @After
   fun tearDown() {
     controllerSubscription.dispose()
     testFixture.dispose()
+    Analytics.clearReporters()
   }
 
   @Test
@@ -228,6 +241,24 @@ class PatientSearchScreenControllerTest {
     verify(ui).hideAllPatientsInFacility()
     verify(ui).showSearchButton()
     verifyNoMoreInteractions(ui)
+  }
+
+  @Test
+  fun `when the search query is blank, the input validation error must be reported to analytics`() {
+    // when
+    setupController()
+    uiEvents.onNext(SearchQueryTextChanged(""))
+    uiEvents.onNext(SearchClicked())
+
+    // then
+    verify(ui).hideSearchButton()
+    verify(ui).showAllPatientsInFacility()
+    verify(ui).setEmptyTextFieldErrorVisible(false)
+    verify(ui).setEmptyTextFieldErrorVisible(true)
+    verifyNoMoreInteractions(ui)
+
+    val expectedEvent = MockAnalyticsReporter.Event("InputValidationError", mapOf("name" to INPUT_EMPTY.analyticsName))
+    assertThat(analyticsReporter.receivedEvents).containsExactly(expectedEvent)
   }
 
   private fun setupController() {

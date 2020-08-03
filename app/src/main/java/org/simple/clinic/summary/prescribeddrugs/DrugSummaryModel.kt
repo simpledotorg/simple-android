c184c7a836d936352ea9c6ef6b4bf5e6030abcd0
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-03 14:43:10 +0530

    Track prescribed drugs in model

--- app/src/main/java/org/simple/clinic/summary/prescribeddrugs/DrugSummaryModel.kt
+++ app/src/main/java/org/simple/clinic/summary/prescribeddrugs/DrugSummaryModel.kt
@@ -2,14 +2,23 @@ package org.simple.clinic.summary.prescribeddrugs
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.drugs.PrescribedDrug
 import java.util.UUID
 
 @Parcelize
 data class DrugSummaryModel(
-    val patientUuid: UUID
+    val patientUuid: UUID,
+    val prescribedDrugs: List<PrescribedDrug>?
 ) : Parcelable {
 
   companion object {
-    fun create(patientUuid: UUID) = DrugSummaryModel(patientUuid)
+    fun create(patientUuid: UUID) = DrugSummaryModel(
+        patientUuid = patientUuid,
+        prescribedDrugs = null
+    )
+  }
+
+  fun prescribedDrugsLoaded(prescribedDrugs: List<PrescribedDrug>): DrugSummaryModel {
+    return copy(prescribedDrugs = prescribedDrugs)
   }
 }

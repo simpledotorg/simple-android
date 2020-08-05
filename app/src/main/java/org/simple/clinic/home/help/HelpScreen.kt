6e68e75a2bd91d7f5a6e2621516e2e1c66c7b488
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-05 12:05:03 +0530

    Wire Mobius loop in `HelpScreen`

--- app/src/main/java/org/simple/clinic/home/help/HelpScreen.kt
+++ app/src/main/java/org/simple/clinic/home/help/HelpScreen.kt
@@ -2,17 +2,20 @@ package org.simple.clinic.home.help
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.view.detaches
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.ofType
 import kotlinx.android.synthetic.main.screen_help.view.*
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.bindUiToController
 import org.simple.clinic.di.injector
 import org.simple.clinic.help.HelpScreenTryAgainClicked
+import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ScreenCreated
@@ -28,11 +31,45 @@ class HelpScreen(context: Context, attrs: AttributeSet) : LinearLayout(context, 
   @Inject
   lateinit var screenRouter: ScreenRouter
 
+  @Inject
+  lateinit var effectHandlerFactory: HelpScreenEffectHandler.Factory
+
   private val events by unsafeLazy {
     Observable
         .merge(screenCreates(), tryAgainClicks())
         .compose(ReportAnalyticsEvents())
         .share()
+  }
+
+  private val delegate by unsafeLazy {
+    val uiRenderer = HelpScreenUiRenderer(this)
+
+    MobiusDelegate.forView(
+        events = events.ofType(),
+        defaultModel = HelpScreenModel.create(),
+        init = HelpScreenInit(),
+        update = HelpScreenUpdate(),
+        effectHandler = effectHandlerFactory.create(this).build(),
+        modelUpdateListener = uiRenderer::render
+    )
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    delegate.start()
+  }
+
+  override fun onDetachedFromWindow() {
+    delegate.stop()
+    super.onDetachedFromWindow()
+  }
+
+  override fun onSaveInstanceState(): Parcelable? {
+    return delegate.onSaveInstanceState(super.onSaveInstanceState())
+  }
+
+  override fun onRestoreInstanceState(state: Parcelable?) {
+    super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
 
   @SuppressLint("SetJavaScriptEnabled")

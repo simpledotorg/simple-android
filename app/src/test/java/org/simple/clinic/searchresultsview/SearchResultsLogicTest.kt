ac4646625e9800cff9cf1a06ad6e5bb113f02e7c
Vinay Shenoy <vinay@obvious.in>
2020-08-05 16:37:43 +0530

    Rename tests class to `SearchResultsLogicTest`

--- app/src/test/java/org/simple/clinic/searchresultsview/SearchResultsLogicTest.kt
+++ app/src/test/java/org/simple/clinic/searchresultsview/SearchResultsLogicTest.kt
@@ -27,7 +27,7 @@ import org.simple.clinic.widgets.UiEvent
 import org.simple.mobius.migration.MobiusTestFixture
 import java.util.UUID
 
-class PatientSearchViewControllerTest {
+class SearchResultsLogicTest {
 
   @get:Rule
   val rxErrorsRule = RxErrorsRule()

1a2b5aa1a1cb6de058d5658e388b8ac685225cc1
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-05 14:15:27 +0530

    Add effect to load help content

--- app/src/main/java/org/simple/clinic/home/help/HelpScreenEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/home/help/HelpScreenEffectHandler.kt
@@ -4,9 +4,11 @@ import com.spotify.mobius.rx2.RxMobius
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.help.HelpRepository
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class HelpScreenEffectHandler @AssistedInject constructor(
+    private val helpRepository: HelpRepository,
     private val schedulersProvider: SchedulersProvider,
     @Assisted private val uiActions: HelpScreenUiActions
 ) {
@@ -19,5 +21,15 @@ class HelpScreenEffectHandler @AssistedInject constructor(
   fun build(): ObservableTransformer<HelpScreenEffect, HelpScreenEvent> = RxMobius
       .subtypeEffectHandler<HelpScreenEffect, HelpScreenEvent>()
       .addAction(ShowLoadingView::class.java, uiActions::showLoadingView, schedulersProvider.ui())
+      .addTransformer(LoadHelpContent::class.java, loadHelpContent())
       .build()
+
+  private fun loadHelpContent(): ObservableTransformer<LoadHelpContent, HelpScreenEvent> {
+    return ObservableTransformer { effects ->
+      effects
+          .observeOn(schedulersProvider.io())
+          .flatMap { helpRepository.helpContentText() }
+          .map(::HelpContentLoaded)
+    }
+  }
 }

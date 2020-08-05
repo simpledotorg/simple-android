c60c4e9986c6ac041b916ee12f5ffd6bfad8f124
Vinay Shenoy <vinay@obvious.in>
2020-08-05 10:36:56 +0530

    Remove parameterized tests

--- app/src/test/java/org/simple/clinic/searchresultsview/PatientSearchViewControllerTest.kt
+++ app/src/test/java/org/simple/clinic/searchresultsview/PatientSearchViewControllerTest.kt
@@ -9,18 +9,14 @@ import io.reactivex.Observable
 import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
-import junitparams.JUnitParamsRunner
-import junitparams.Parameters
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.simple.clinic.TestData
 import org.simple.clinic.bp.BloodPressureMeasurement
 import org.simple.clinic.bp.PatientToFacilityId
 import org.simple.clinic.facility.FacilityRepository
-import org.simple.clinic.TestData
 import org.simple.clinic.patient.PatientRepository
-import org.simple.clinic.patient.PatientSearchCriteria
 import org.simple.clinic.patient.PatientSearchCriteria.Name
 import org.simple.clinic.patient.PatientSearchCriteria.PhoneNumber
 import org.simple.clinic.user.UserSession
@@ -28,7 +24,6 @@ import org.simple.clinic.util.RxErrorsRule
 import org.simple.clinic.widgets.UiEvent
 import java.util.UUID
 
-@RunWith(JUnitParamsRunner::class)
 class PatientSearchViewControllerTest {
 
   @get:Rule
@@ -76,8 +71,9 @@ class PatientSearchViewControllerTest {
   }
 
   @Test
-  @Parameters(method = "params for search criteria")
-  fun `when register new patient clicked then RegisterNewPatient event should be emitted`(criteria: PatientSearchCriteria) {
+  fun `when register new patient clicked while searching by name, then RegisterNewPatient event should be emitted`() {
+    val criteria = Name(patientName = patientName)
+
     screenCreated()
     uiEvents.onNext(SearchPatientWithCriteria(criteria))
     uiEvents.onNext(RegisterNewPatientClicked)
@@ -86,9 +82,20 @@ class PatientSearchViewControllerTest {
   }
 
   @Test
-  @Parameters(method = "params for search criteria")
-  fun `when searching patients returns results, the results should be displayed`(searchCriteria: PatientSearchCriteria) {
+  fun `when register new patient clicked while searching by phone, then RegisterNewPatient event should be emitted`() {
+    val criteria = PhoneNumber(phoneNumber = phoneNumber)
+
+    screenCreated()
+    uiEvents.onNext(SearchPatientWithCriteria(criteria))
+    uiEvents.onNext(RegisterNewPatientClicked)
+
+    verify(ui).registerNewPatient(RegisterNewPatient(criteria))
+  }
+
+  @Test
+  fun `when searching patients by name returns results, the results should be displayed`() {
     // given
+    val searchCriteria = Name(patientName = patientName)
     val searchResult1InCurrentFacility = TestData.patientSearchResult(
         uuid = UUID.fromString("1d5f18d9-43f7-4e7f-92d3-a4f641709470"),
         fullName = "Anish Acharya in CHC Bagta"
@@ -144,11 +151,64 @@ class PatientSearchViewControllerTest {
     verify(ui).updateSearchResults(expectedSearchResults)
   }
 
-  @Suppress("Unused")
-  private fun `params for search criteria`(): List<PatientSearchCriteria> = listOf(
-      Name(patientName = patientName),
-      PhoneNumber(phoneNumber = phoneNumber)
-  )
+  @Test
+  fun `when searching patients by phone number returns results, the results should be displayed`() {
+    // given
+    val searchCriteria = PhoneNumber(phoneNumber = phoneNumber)
+    val searchResult1InCurrentFacility = TestData.patientSearchResult(
+        uuid = UUID.fromString("1d5f18d9-43f7-4e7f-92d3-a4f641709470"),
+        fullName = "Anish Acharya in CHC Bagta"
+    )
+    val searchResult2InCurrentFacility = TestData.patientSearchResult(
+        uuid = UUID.fromString("6aba53eb-c8dd-452c-b640-c11c6df94f8a"),
+        fullName = "Deepa in CHC Bagta"
+    )
+    val searchResult3InCurrentFacility = TestData.patientSearchResult(
+        uuid = UUID.fromString("65965892-7218-4ca1-bfca-e7b037751802"),
+        fullName = "Vinay in CHC Bagta"
+    )
+    val searchResult1InOtherFacility = TestData.patientSearchResult(
+        uuid = UUID.fromString("139bfac5-1adc-43fa-9406-d1000fb67a88"),
+        fullName = "Sanchita in CHC Ballianwali"
+    )
+    val searchResult2InOtherFacility = TestData.patientSearchResult(
+        uuid = UUID.fromString("760c7342-b0f2-4130-9d4c-c5f18336d4b8"),
+        fullName = "Daniel in CHC Ballianwali"
+    )
+
+    val allSearchResults = listOf(
+        searchResult1InCurrentFacility,
+        searchResult2InOtherFacility,
+        searchResult3InCurrentFacility,
+        searchResult2InCurrentFacility,
+        searchResult1InOtherFacility
+    )
+    val patientUuids = allSearchResults.map { it.uuid }
+    val patientAndFacilityIdsToReturn = listOf(
+        PatientToFacilityId(patientUuid = searchResult1InCurrentFacility.uuid, facilityUuid = currentFacility.uuid),
+        PatientToFacilityId(patientUuid = searchResult2InCurrentFacility.uuid, facilityUuid = currentFacility.uuid),
+        PatientToFacilityId(patientUuid = searchResult3InCurrentFacility.uuid, facilityUuid = currentFacility.uuid),
+        PatientToFacilityId(patientUuid = searchResult1InOtherFacility.uuid, facilityUuid = otherFacility.uuid),
+        PatientToFacilityId(patientUuid = searchResult2InOtherFacility.uuid, facilityUuid = otherFacility.uuid)
+    )
+
+    whenever(bloodPressureDao.patientToFacilityIds(patientUuids))
+        .doReturn(Flowable.just(patientAndFacilityIdsToReturn))
+    whenever(patientRepository.search(searchCriteria))
+        .doReturn(Observable.just(allSearchResults))
+
+    // when
+    screenCreated()
+    uiEvents.onNext(SearchPatientWithCriteria(searchCriteria))
+
+    // then
+    val expectedSearchResults = PatientSearchResults(
+        visitedCurrentFacility = listOf(searchResult1InCurrentFacility, searchResult3InCurrentFacility, searchResult2InCurrentFacility),
+        notVisitedCurrentFacility = listOf(searchResult2InOtherFacility, searchResult1InOtherFacility),
+        currentFacility = currentFacility
+    )
+    verify(ui).updateSearchResults(expectedSearchResults)
+  }
 
   private fun screenCreated() {
     uiEvents.onNext(SearchResultsViewCreated)

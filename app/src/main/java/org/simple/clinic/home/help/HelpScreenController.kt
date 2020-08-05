7d986b75ccd8fb79ef82f5d37b46594bc6309e83
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-05 14:45:48 +0530

    Migrate sync help to Mobius

--- app/src/main/java/org/simple/clinic/home/help/HelpScreenController.kt
+++ app/src/main/java/org/simple/clinic/home/help/HelpScreenController.kt
@@ -3,9 +3,7 @@ package org.simple.clinic.home.help
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
-import io.reactivex.rxkotlin.ofType
 import org.simple.clinic.ReplayUntilScreenIsDestroyed
-import org.simple.clinic.help.HelpPullResult
 import org.simple.clinic.help.HelpRepository
 import org.simple.clinic.help.HelpSync
 import org.simple.clinic.widgets.UiEvent
@@ -23,36 +21,6 @@ class HelpScreenController @Inject constructor(
     val replayedEvents = ReplayUntilScreenIsDestroyed(events)
         .replay()
 
-    return Observable.mergeArray(
-        syncHelp(replayedEvents)
-    )
-  }
-
-  private fun syncHelp(events: Observable<UiEvent>): Observable<UiChange> {
-    val helpSyncResultsStream = events
-        .ofType<HelpScreenTryAgainClicked>()
-        .flatMapSingle { sync.pullWithResult() }
-        .replay()
-        .refCount()
-
-    val showNetworkError = helpSyncResultsStream
-        .ofType<HelpPullResult.NetworkError>()
-        .map {
-          { ui: Ui ->
-            ui.showNoHelpAvailable()
-            ui.showNetworkErrorMessage()
-          }
-        }
-
-    val showUnexpectedError = helpSyncResultsStream
-        .ofType<HelpPullResult.OtherError>()
-        .map {
-          { ui: Ui ->
-            ui.showNoHelpAvailable()
-            ui.showUnexpectedErrorMessage()
-          }
-        }
-
-    return Observable.merge(showNetworkError, showUnexpectedError)
+    return Observable.never()
   }
 }

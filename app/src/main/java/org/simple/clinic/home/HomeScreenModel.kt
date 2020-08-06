826adbe26a331b29401f7358e4a9647f390de3d5
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-06 10:11:37 +0530

    Track `Facility` in `HomeScreenModel`

--- app/src/main/java/org/simple/clinic/home/HomeScreenModel.kt
+++ app/src/main/java/org/simple/clinic/home/HomeScreenModel.kt
@@ -2,11 +2,23 @@ package org.simple.clinic.home
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.facility.Facility
 
 @Parcelize
-class HomeScreenModel : Parcelable {
+data class HomeScreenModel(
+    val facility: Facility?
+) : Parcelable {
 
   companion object {
-    fun create() = HomeScreenModel()
+    fun create() = HomeScreenModel(
+        facility = null
+    )
+  }
+
+  val hasFacility: Boolean
+    get() = facility != null
+
+  fun facilityLoaded(facility: Facility): HomeScreenModel {
+    return copy(facility = facility)
   }
 }

feac8696c9bff6eadd7c87008612d5964b7c7197
Vinay Shenoy <code@vinaysshenoy.com>
2020-08-06 17:56:44 +0530

    Migrate patient search screen to Mobius: Strangulate and remove controller (#1592)
    * Add effect to report input validation errors to Analytics
    
    * Add regression test for sending input validation error
    
    * Keep entered query updated in the model
    
    * Validate the saved input in the Update function
    
    * Render the error message in the UI renderer
    
    * Remove unused methods from the controller
    
    * Track additional identifier in the model
    
    * Move opening of search results
    
    * Remove unused events and `InputValidator` transformer
    
    * Move opening of patient summary to Mobius
    
    * Move toggling of "All patients in facility" view to Mobius
    
    * Move toggling of search button to Mobius
    
    * Remove the controller
    
    * Implement UI actions separately on the screen
    
    * Update CHANGELOG
    
    * Extract handling of searh clicks to a different method

--- app/src/main/java/org/simple/clinic/search/PatientSearchEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/search/PatientSearchEffectHandler.kt
@@ -4,6 +4,7 @@ import com.spotify.mobius.rx2.RxMobius
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.platform.analytics.Analytics
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class PatientSearchEffectHandler @AssistedInject constructor(
@@ -19,6 +20,13 @@ class PatientSearchEffectHandler @AssistedInject constructor(
   fun build(): ObservableTransformer<PatientSearchEffect, PatientSearchEvent> {
     return RxMobius
         .subtypeEffectHandler<PatientSearchEffect, PatientSearchEvent>()
+        .addConsumer(ReportValidationErrorsToAnalytics::class.java, { reportValidationErrorsToAnalytics(it.errors) }, schedulers.io())
+        .addConsumer(OpenSearchResults::class.java, { uiActions.openSearchResultsScreen(it.searchCriteria) }, schedulers.ui())
+        .addConsumer(OpenPatientSummary::class.java, { uiActions.openPatientSummary(it.patientId) }, schedulers.ui())
         .build()
+  }
+
+  private fun reportValidationErrorsToAnalytics(errors: Set<PatientSearchValidationError>) {
+    errors.forEach { Analytics.reportInputValidationError(it.analyticsName) }
   }
 }

7060c071e8f90942ee4f721e11695cecff95e37d
Honey Sonwani <honey@obvious.in>
2020-08-10 11:56:47 +0530

    Add `SignatureActivity` UI with translucent background

---
+++ app/src/main/java/org/simple/clinic/signature/SignatureActivity.kt
@@ -0,0 +1,13 @@
+package org.simple.clinic.signature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.simple.clinic.R
+
+class SignatureActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_signature)
+  }
+}
+

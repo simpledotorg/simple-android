f5f359a7bd371f7f00dc47961da0eb3381ded2c3
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-10 11:22:08 +0530

    Wire Mobius loop in `LoggedOutOfDeviceDialog`

--- app/src/main/java/org/simple/clinic/registration/phone/loggedout/LoggedOutOfDeviceDialog.kt
+++ app/src/main/java/org/simple/clinic/registration/phone/loggedout/LoggedOutOfDeviceDialog.kt
@@ -9,11 +9,13 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.FragmentManager
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.ofType
 import io.reactivex.subjects.PublishSubject
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.bindUiToController
 import org.simple.clinic.main.TheActivity
+import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.ScreenDestroyed
@@ -49,6 +51,9 @@ class LoggedOutOfDeviceDialog : AppCompatDialogFragment(), LoggedOutOfDeviceDial
   @Inject
   lateinit var controller: LoggedOutOfDeviceDialogController
 
+  @Inject
+  lateinit var effectHandler: LoggedOutOfDeviceEffectHandler
+
   private val okayButton: Button by unsafeLazy {
     (dialog as AlertDialog).getButton(DialogInterface.BUTTON_POSITIVE)
   }
@@ -62,9 +67,28 @@ class LoggedOutOfDeviceDialog : AppCompatDialogFragment(), LoggedOutOfDeviceDial
         .share()
   }
 
+  private val delegate by unsafeLazy {
+    val uiRenderer = LoggedOutOfDeviceUiRenderer(this)
+
+    MobiusDelegate.forActivity(
+        events = events.ofType(),
+        defaultModel = LoggedOutOfDeviceModel.create(),
+        init = LoggedOutOfDeviceInit(),
+        update = LoggedOutOfDeviceUpdate(),
+        effectHandler = effectHandler.build(),
+        modelUpdateListener = uiRenderer::render
+    )
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    delegate.onRestoreInstanceState(savedInstanceState)
     TheActivity.component.inject(this)
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    delegate.onSaveInstanceState(outState)
+    super.onSaveInstanceState(outState)
   }
 
   @SuppressLint("CheckResult")
@@ -96,6 +120,12 @@ class LoggedOutOfDeviceDialog : AppCompatDialogFragment(), LoggedOutOfDeviceDial
   override fun onStart() {
     super.onStart()
     onStarts.onNext(Any())
+    delegate.start()
+  }
+
+  override fun onStop() {
+    delegate.stop()
+    super.onStop()
   }
 
   override fun onDestroyView() {

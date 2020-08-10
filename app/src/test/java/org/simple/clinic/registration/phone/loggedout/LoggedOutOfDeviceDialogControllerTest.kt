b15399d53f61cb895f9184058b283c4d91cd71c4
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-10 12:46:18 +0530

    Remove controller from tests

--- app/src/test/java/org/simple/clinic/registration/phone/loggedout/LoggedOutOfDeviceDialogControllerTest.kt
+++ app/src/test/java/org/simple/clinic/registration/phone/loggedout/LoggedOutOfDeviceDialogControllerTest.kt
@@ -7,7 +7,6 @@ import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
 import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Single
-import io.reactivex.disposables.Disposable
 import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.subjects.PublishSubject
@@ -20,7 +19,6 @@ import org.simple.clinic.user.UserSession.LogoutResult.Failure
 import org.simple.clinic.user.UserSession.LogoutResult.Success
 import org.simple.clinic.util.RxErrorsRule
 import org.simple.clinic.util.scheduler.TestSchedulersProvider
-import org.simple.clinic.widgets.ScreenCreated
 import org.simple.clinic.widgets.UiEvent
 import org.simple.mobius.migration.MobiusTestFixture
 
@@ -33,12 +31,10 @@ class LoggedOutOfDeviceDialogControllerTest {
   private val userSession = mock<UserSession>()
   private val uiEvents = PublishSubject.create<UiEvent>()
 
-  private lateinit var controllerSubscription: Disposable
   private lateinit var testFixture: MobiusTestFixture<LoggedOutOfDeviceModel, LoggedOutOfDeviceEvent, LoggedOutOfDeviceEffect>
 
   @After
   fun tearDown() {
-    controllerSubscription.dispose()
     testFixture.dispose()
   }
 
@@ -106,14 +102,6 @@ class LoggedOutOfDeviceDialogControllerTest {
   }
 
   private fun setupController() {
-    val controller = LoggedOutOfDeviceDialogController(userSession)
-
-    controllerSubscription = uiEvents
-        .compose(controller)
-        .subscribe({ uiChange -> uiChange(ui) }, { throw it })
-
-    uiEvents.onNext(ScreenCreated())
-
     val effectHandler = LoggedOutOfDeviceEffectHandler(
         userSession = userSession,
         schedulersProvider = TestSchedulersProvider.trampoline()

85f0e76ad7b3d0f421e7f0d1eae897de47e5adbb
Vinay Shenoy <vinay@obvious.in>
2020-08-11 15:09:23 +0530

    Change all concrete types of `InputField` to data classes
    This is being done in preparation for a future commit where we need to
    test for a list of input fields being generated.

--- app/src/main/java/org/simple/clinic/newentry/form/DistrictField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/DistrictField.kt
@@ -2,7 +2,9 @@ package org.simple.clinic.newentry.form
 
 import org.simple.clinic.newentry.form.ValidationError.MissingValue
 
-class DistrictField(labelResId: Int) : InputField<String>(labelResId) {
+data class DistrictField(
+    private val _labelResId: Int
+) : InputField<String>(_labelResId) {
   override fun validate(value: String): Set<ValidationError> {
     return if (value.isBlank()) setOf(MissingValue) else emptySet()
   }
--- app/src/main/java/org/simple/clinic/newentry/form/PatientNameField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/PatientNameField.kt
@@ -2,7 +2,9 @@ package org.simple.clinic.newentry.form
 
 import org.simple.clinic.newentry.form.ValidationError.MissingValue
 
-class PatientNameField(labelResId: Int) : InputField<String>(labelResId) {
+data class PatientNameField(
+    private val _labelResId: Int
+) : InputField<String>(_labelResId) {
   override fun validate(value: String): Set<ValidationError> {
     return if (value.isBlank()) setOf(MissingValue) else emptySet()
   }

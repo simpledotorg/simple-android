f367b91a615f54aea762eb086d13b9a2d8c2ab97
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-13 09:21:54 +0530

    Track facility in `AppLockModel`

--- app/src/main/java/org/simple/clinic/login/applock/AppLockModel.kt
+++ app/src/main/java/org/simple/clinic/login/applock/AppLockModel.kt
@@ -2,23 +2,33 @@ package org.simple.clinic.login.applock
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.facility.Facility
 import org.simple.clinic.user.User
 
 @Parcelize
 data class AppLockModel(
-    val user: User?
+    val user: User?,
+    val facility: Facility?
 ) : Parcelable {
 
   companion object {
     fun create() = AppLockModel(
-        user = null
+        user = null,
+        facility = null
     )
   }
 
   val hasUser: Boolean
     get() = user != null
 
+  val hasFacility: Boolean
+    get() = facility != null
+
   fun userLoaded(user: User): AppLockModel {
     return copy(user = user)
+  }
+
+  fun facilityLoaded(facility: Facility): AppLockModel {
+    return copy(facility = facility)
   }
 }

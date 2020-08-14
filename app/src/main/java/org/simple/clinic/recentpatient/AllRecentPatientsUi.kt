5ad4ac60ab2b605f844427c6f262fc9c2d5d4163
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-08-14 18:20:42 +0530

    Fix cannot open patient summary from recent patient's screen (#1632)
    * Implement `RecentPatientItem` adapter for all recent patients screen
    
    Because `RecentPatientItemType` adapter was sending out a different click event, the click events in all recent patients screen were not being recognised. So in order to fix that, I have implemented a separate adapter for all recent patient screen. This is basically a copy of `RecentPatientItemType` adapter but with just `RecentPatientItem`, since this is a priority bug we can go ahead with this approach, but later on we can figure out a better solution to handle these scenarios to avoid duplicate code.
    
    * Update CHANGELOG
    
    (cherry picked from commit 094be1a2d3420f6d6a4585af01e67c7b1c1c2875)

--- app/src/main/java/org/simple/clinic/recentpatient/AllRecentPatientsUi.kt
+++ app/src/main/java/org/simple/clinic/recentpatient/AllRecentPatientsUi.kt
@@ -1,7 +1,5 @@
 package org.simple.clinic.recentpatient
 
-import org.simple.clinic.recentpatientsview.RecentPatientItemType
-
 interface AllRecentPatientsUi {
-  fun updateRecentPatients(allItemTypes: List<RecentPatientItemType>)
+  fun updateRecentPatients(allItemTypes: List<RecentPatientItem>)
 }

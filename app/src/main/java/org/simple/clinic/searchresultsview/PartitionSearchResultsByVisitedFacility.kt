fcd49f91e904eb2cae66e2352b53cc75d859eee0
Vinay Shenoy <vinay@obvious.in>
2020-08-18 16:14:22 +0530

    Migrate patient search view to Mobius: Strangulate controller and sterilize the screen (#1642)
    * Migrate showing of patient search results to Mobius
    
    * Remove controller from tests
    
    * Rename tests class to `SearchResultsLogicTest`
    
    * Remove `PatientSearchViewController`
    
    * Trigger search patient using a deferred event source
    
    Since the trigger was originally being emitted when the parent view
    was attached to the window, the event was dropped because the Mobius
    delegate was not yet started.
    
    Changing this to a deferred event source ensures that the event is
    retained and triggered after the view is attached to the screen.
    
    * Change fetching of patient facility IDs to be a synchronous call
    
    * Move partioning of patient search results to a factory method
    
    This is being done in order support to generate this without needing
    an Observable transformer in the search results screen.
    
    * Inject current facility into the effect handler
    
    * Partition search results in the effect handler
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/searchresultsview/PartitionSearchResultsByVisitedFacility.kt
+++ app/src/main/java/org/simple/clinic/searchresultsview/PartitionSearchResultsByVisitedFacility.kt
@@ -5,11 +5,8 @@ import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
 import io.reactivex.rxkotlin.Observables
 import org.simple.clinic.bp.BloodPressureMeasurement
-import org.simple.clinic.bp.PatientToFacilityId
 import org.simple.clinic.facility.Facility
-import org.simple.clinic.patient.FacilityUuid
 import org.simple.clinic.patient.PatientSearchResult
-import org.simple.clinic.patient.PatientUuid
 
 class PartitionSearchResultsByVisitedFacility(
     private val bloodPressureDao: BloodPressureMeasurement.RoomDao,
@@ -21,49 +18,9 @@ class PartitionSearchResultsByVisitedFacility(
 
     val patientToFacilityUuidStream = searchResults
         .map { patients -> patients.map { it.uuid } }
-        .switchMap {
-          bloodPressureDao
-              .patientToFacilityIds(it)
-              .toObservable()
-        }
+        .map(bloodPressureDao::patientToFacilityIds)
 
     return Observables.combineLatest(searchResults, patientToFacilityUuidStream, facilityStream)
-        .map { (patients, patientToFacilities, facility) ->
-          val patientsToVisitedFacilities = mapPatientsToVisitedFacilities(patientToFacilities)
-
-          val (patientsInCurrentFacility, patientsInOtherFacility) = patients.partition { patientSearchResult ->
-            hasPatientVisitedFacility(
-                patientsToVisitedFacilities = patientsToVisitedFacilities,
-                facilityUuid = facility.uuid,
-                patientUuid = patientSearchResult.uuid
-            )
-          }
-
-          PatientSearchResults(
-              visitedCurrentFacility = patientsInCurrentFacility,
-              notVisitedCurrentFacility = patientsInOtherFacility,
-              currentFacility = facility
-          )
-        }
-  }
-
-  private fun hasPatientVisitedFacility(
-      patientsToVisitedFacilities: Map<PatientUuid, Set<FacilityUuid>>,
-      facilityUuid: FacilityUuid,
-      patientUuid: PatientUuid
-  ): Boolean {
-    return patientsToVisitedFacilities[patientUuid]?.contains(facilityUuid) ?: false
-  }
-
-  private fun mapPatientsToVisitedFacilities(patientToFacilities: List<PatientToFacilityId>): Map<PatientUuid, Set<FacilityUuid>> {
-    return patientToFacilities
-        .fold(mutableMapOf<PatientUuid, MutableSet<FacilityUuid>>()) { facilityUuids, (patientUuid, facilityUuid) ->
-          if (patientUuid !in facilityUuids) {
-            facilityUuids[patientUuid] = mutableSetOf()
-          }
-
-          facilityUuids[patientUuid]?.add(facilityUuid)
-          facilityUuids
-        }
+        .map { (patients, patientToFacilities, facility) -> PatientSearchResults.from(patients, patientToFacilities, facility) }
   }
 }

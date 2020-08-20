57f577fb0ee688ed3b497100365d4737e97b66c5
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-20 09:12:52 +0530

    Implement `TeleconsultationFacilityInfoPayload`

---
+++ app/src/main/java/org/simple/clinic/summary/teleconsultation/sync/TeleconsultationFacilityInfoPayload.kt
@@ -0,0 +1,28 @@
+package org.simple.clinic.summary.teleconsultation.sync
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import java.time.Instant
+import java.util.UUID
+
+@JsonClass(generateAdapter = true)
+data class TeleconsultationFacilityInfoPayload(
+
+    @Json(name = "id")
+    val id: UUID,
+
+    @Json(name = "facility_id")
+    val facilityId: UUID,
+
+    @Json(name = "medical_officers")
+    val medicalOfficers: List<MedicalOfficerPayload>,
+
+    @Json(name = "created_at")
+    val createdAt: Instant,
+
+    @Json(name = "updated_at")
+    val updatedAt: Instant,
+
+    @Json(name = "deleted_at")
+    val deletedAt: Instant?
+)

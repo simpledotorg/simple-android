83487f61d6b549f7644d6f1f5df47beaefd49716
Vinay Shenoy <vinay@obvious.in>
2020-08-26 15:30:53 +0530

    Change `SyncCoordinator#push` to be a fully synchronous operation

--- app/src/main/java/org/simple/clinic/sync/SyncCoordinator.kt
+++ app/src/main/java/org/simple/clinic/sync/SyncCoordinator.kt
@@ -4,7 +4,6 @@ import com.f2prateek.rx.preferences2.Preference
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
-import io.reactivex.functions.Consumer
 import org.simple.clinic.patient.SyncStatus
 import org.simple.clinic.util.Just
 import org.simple.clinic.util.Optional
@@ -16,27 +15,32 @@ class SyncCoordinator @Inject constructor() {
   fun <T : Any, P> push(
       repository: SynceableRepository<T, P>,
       pushNetworkCall: (List<T>) -> Single<DataPushResponse>
-  ): Completable = repository
-      .recordsWithSyncStatus(SyncStatus.PENDING)
-      .filter { it.isNotEmpty() }
-      .flatMapSingleElement { pushNetworkCall(it).doOnSuccess(logValidationErrorsIfAny(it)) }
-      .map { it.validationErrors }
-      .map { errors -> errors.map { it.uuid } }
-      .flatMapCompletable { recordIdsWithErrors ->
-        repository
-            .setSyncStatus(from = SyncStatus.PENDING, to = SyncStatus.DONE)
-            .andThen(when {
-              recordIdsWithErrors.isEmpty() -> Completable.complete()
-              else -> repository.setSyncStatus(recordIdsWithErrors, SyncStatus.INVALID)
-            })
-      }
+  ): Completable {
+    return Completable
+        .fromAction {
+          val pendingSyncRecords = repository.recordsWithSyncStatus(SyncStatus.PENDING).blockingGet()
 
-  private fun <T : Any> logValidationErrorsIfAny(records: List<T>): Consumer<in DataPushResponse> {
-    return Consumer { response ->
-      if (response.validationErrors.isNotEmpty()) {
-        val recordType = records.first().javaClass.simpleName
-        Timber.e("Server sent validation errors when syncing $recordType : ${response.validationErrors}")
-      }
+          if (pendingSyncRecords.isNotEmpty()) {
+            val response = pushNetworkCall(pendingSyncRecords).blockingGet()
+            repository.setSyncStatus(SyncStatus.PENDING, SyncStatus.DONE).blockingAwait()
+
+            val validationErrors = response.validationErrors
+            val recordIdsWithErrors = validationErrors.map { it.uuid }
+            if (recordIdsWithErrors.isNotEmpty()) {
+              logValidationErrorsIfAny(pendingSyncRecords, validationErrors)
+              repository.setSyncStatus(recordIdsWithErrors, SyncStatus.INVALID)
+            }
+          }
+        }
+  }
+
+  private fun <T : Any> logValidationErrorsIfAny(
+      records: List<T>,
+      validationErrors: List<ValidationErrors>
+  ) {
+    if (validationErrors.isNotEmpty()) {
+      val recordType = records.first().javaClass.simpleName
+      Timber.e("Server sent validation errors when syncing $recordType : ${validationErrors}")
     }
   }
 

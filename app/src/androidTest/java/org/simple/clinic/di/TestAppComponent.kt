5f50a5a1297ba274322acaf458af20cb7dcfdf3a
Vinay Shenoy <vinay@obvious.in>
2020-08-26 14:10:45 +0530

    Remove redundant sync instrumented tests

--- app/src/androidTest/java/org/simple/clinic/di/TestAppComponent.kt
+++ app/src/androidTest/java/org/simple/clinic/di/TestAppComponent.kt
@@ -8,21 +8,15 @@ import org.simple.clinic.TestDataModule
 import org.simple.clinic.appconfig.SelectedCountryPersistenceAndroidTest
 import org.simple.clinic.bloodsugar.BloodSugarHistoryListItemDataSourceTest
 import org.simple.clinic.bloodsugar.BloodSugarRepositoryAndroidTest
-import org.simple.clinic.bloodsugar.sync.BloodSugarSyncAndroidTest
 import org.simple.clinic.bp.BloodPressureHistoryListItemDataSourceTest
 import org.simple.clinic.bp.BloodPressureRepositoryAndroidTest
-import org.simple.clinic.bp.sync.BloodPressureSyncAndroidTest
 import org.simple.clinic.drugs.PrescriptionRepositoryAndroidTest
-import org.simple.clinic.drugs.sync.PrescriptionSyncAndroidTest
 import org.simple.clinic.facility.FacilityRepositoryAndroidTest
 import org.simple.clinic.home.overdue.OverdueAppointmentRowDataSourceTest
 import org.simple.clinic.login.LoginUserWithOtpServerIntegrationTest
 import org.simple.clinic.medicalhistory.MedicalHistoryRepositoryAndroidTest
-import org.simple.clinic.medicalhistory.MedicalHistorySyncAndroidTest
 import org.simple.clinic.overdue.AppointmentRepositoryAndroidTest
-import org.simple.clinic.overdue.AppointmentSyncAndroidTest
 import org.simple.clinic.patient.PatientRepositoryAndroidTest
-import org.simple.clinic.patient.PatientSyncAndroidTest
 import org.simple.clinic.patient.businessid.BusinessIdMetaDataAdapterAndroidTest
 import org.simple.clinic.protocolv2.ProtocolRepositoryAndroidTest
 import org.simple.clinic.protocolv2.sync.ProtocolSyncAndroidTest
@@ -66,20 +60,14 @@ interface TestAppComponent {
 
   fun inject(target: TestClinicApp)
   fun inject(target: UserSessionAndroidTest)
-  fun inject(target: PrescriptionSyncAndroidTest)
-  fun inject(target: BloodPressureSyncAndroidTest)
-  fun inject(target: BloodSugarSyncAndroidTest)
   fun inject(target: ProtocolSyncAndroidTest)
   fun inject(target: PatientRepositoryAndroidTest)
   fun inject(target: PrescriptionRepositoryAndroidTest)
   fun inject(target: FacilityRepositoryAndroidTest)
   fun inject(target: UserDaoAndroidTest)
-  fun inject(target: AppointmentSyncAndroidTest)
   fun inject(target: AppointmentRepositoryAndroidTest)
   fun inject(target: LocalAuthenticationRule)
-  fun inject(target: MedicalHistorySyncAndroidTest)
   fun inject(target: MedicalHistoryRepositoryAndroidTest)
-  fun inject(target: PatientSyncAndroidTest)
   fun inject(target: BloodPressureRepositoryAndroidTest)
   fun inject(target: OngoingLoginEntryRepositoryTest)
   fun inject(target: BruteForceProtectionAndroidTest)

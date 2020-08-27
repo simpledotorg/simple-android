ccbca31ea27ea497c2bfde668ed5329992d6ab1f
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-27 10:21:33 +0530

    Add new contact doctor sheet UI

---
+++ app/src/main/java/org/simple/clinic/summary/teleconsultation/contactdoctor/ContactDoctorSheet_New.kt
@@ -0,0 +1,13 @@
+package org.simple.clinic.summary.teleconsultation.contactdoctor
+
+import android.os.Bundle
+import org.simple.clinic.R
+import org.simple.clinic.widgets.BottomSheetActivity
+
+class ContactDoctorSheet_New : BottomSheetActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.sheet_contact_doctor_new)
+  }
+}

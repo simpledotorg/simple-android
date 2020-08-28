70de4a13a442b9ea0ee38718faab028c86dccf52
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-08-28 13:53:48 +0530

    Change `MedicineFrequency` to sealed class

--- app/src/main/java/org/simple/clinic/teleconsultlog/medicinefrequency/MedicineFrequency.kt
+++ app/src/main/java/org/simple/clinic/teleconsultlog/medicinefrequency/MedicineFrequency.kt
@@ -1,8 +1,53 @@
 package org.simple.clinic.teleconsultlog.medicinefrequency
 
-enum class MedicineFrequency {
-  OD,
-  BD,
-  TDS,
-  QDS
+import android.os.Parcelable
+import androidx.room.TypeConverter
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.util.room.SafeEnumTypeAdapter
+
+sealed class MedicineFrequency : Parcelable {
+
+  @Parcelize
+  object OD : MedicineFrequency()
+
+  @Parcelize
+  object BD : MedicineFrequency()
+
+  @Parcelize
+  object TDS : MedicineFrequency()
+
+  @Parcelize
+  object QDS : MedicineFrequency()
+
+  @Parcelize
+  data class Unknown(val actualValue: String) : MedicineFrequency()
+
+  object TypeAdapter : SafeEnumTypeAdapter<MedicineFrequency>(
+      knownMappings = mapOf(
+          OD to "od",
+          BD to "bd",
+          TDS to "tds",
+          QDS to "qds"
+      ),
+      unknownStringToEnumConverter = { Unknown(it) },
+      unknownEnumToStringConverter = { (it as Unknown).actualValue }
+  )
+
+  class RoomTypeConverter {
+    @TypeConverter
+    fun toEnum(value: String?): MedicineFrequency? = TypeAdapter.toEnum(value)
+
+    @TypeConverter
+    fun fromEnum(medicineFrequency: MedicineFrequency?): String? = TypeAdapter.fromEnum(medicineFrequency)
+  }
+
+  class MoshiTypeConverter {
+    @FromJson
+    fun toEnum(value: String?): MedicineFrequency? = TypeAdapter.toEnum(value)
+
+    @ToJson
+    fun fromEnum(medicineFrequency: MedicineFrequency?): String? = TypeAdapter.fromEnum(medicineFrequency)
+  }
 }

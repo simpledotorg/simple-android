cf1038612f8036648659e1d7b54261122d55c3c4
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-09-01 14:34:07 +0530

    Remove `process_token` usage from `TeleconsultationSync`

--- app/src/main/java/org/simple/clinic/summary/teleconsultation/sync/TeleconsultFacilityInfoApi.kt
+++ app/src/main/java/org/simple/clinic/summary/teleconsultation/sync/TeleconsultFacilityInfoApi.kt
@@ -3,13 +3,9 @@ package org.simple.clinic.summary.teleconsultation.sync
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
-import retrofit2.http.Query
 
 interface TeleconsultFacilityInfoApi {
   @Headers(value = ["X-RESYNC-TOKEN: 1"])
   @GET("v4/teleconsultation_medical_officers/sync")
-  fun pull(
-      @Query("limit") recordsToPull: Int,
-      @Query("process_token") lastPullToken: String? = null
-  ): Call<TeleconsultFacilityInfoPullResponse>
+  fun pull(): Call<TeleconsultFacilityInfoPullResponse>
 }

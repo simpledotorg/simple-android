2213002711bfce5e196b6466d589dc51d189392c
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-09-01 17:06:16 +0530

    Remove `process_token` usage from `TeleconsultationSync` (#1671)

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryModule.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryModule.kt
@@ -1,20 +1,13 @@
 package org.simple.clinic.summary
 
-import com.f2prateek.rx.preferences2.Preference
-import com.f2prateek.rx.preferences2.RxSharedPreferences
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.AppDatabase
-import org.simple.clinic.main.TypedPreference
-import org.simple.clinic.main.TypedPreference.Type.LastTeleconsultationFacilityPullToken
 import org.simple.clinic.remoteconfig.ConfigReader
 import org.simple.clinic.summary.bloodpressures.BloodPressureSummaryViewConfig
 import org.simple.clinic.summary.bloodsugar.BloodSugarSummaryConfigModule
 import org.simple.clinic.summary.teleconsultation.api.TeleconsultationApi
 import org.simple.clinic.summary.teleconsultation.sync.TeleconsultFacilityInfoApi
-import org.simple.clinic.util.Optional
-import org.simple.clinic.util.preference.OptionalRxPreferencesConverter
-import org.simple.clinic.util.preference.StringPreferenceConverter
 import retrofit2.Retrofit
 import retrofit2.create
 import javax.inject.Named
@@ -48,10 +41,4 @@ class PatientSummaryModule {
 
   @Provides
   fun teleconsultationFacilityWithMedicalOfficersDao(appDatabase: AppDatabase) = appDatabase.teleconsultFacilityWithMedicalOfficersDao()
-
-  @Provides
-  @TypedPreference(LastTeleconsultationFacilityPullToken)
-  fun lastPullToken(rxSharedPrefs: RxSharedPreferences): Preference<Optional<String>> {
-    return rxSharedPrefs.getObject("last_teleconsultation_facility_pull_token", Optional.empty(), OptionalRxPreferencesConverter(StringPreferenceConverter()))
-  }
 }

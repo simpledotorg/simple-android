d7c063a67c9f30befe58f0524e104262ff2c4b82
Vinay Shenoy <vinay@obvious.in>
2020-09-04 15:17:34 +0530

    Refactor Dagger setup
    - Create a `AppLockModule` which includes `AppLockConfigModule`
    - Move providing of `AppLockTimestamp` to the `AppLockModule`
    
    This was mostly done because the `AppLockConfigModule` has different
    implementations in the source sets and I didn't to duplicate the app
    lock timestamp providers there as well.

--- app/src/debug/java/org/simple/clinic/login/AppLockConfigModule.kt
+++ app/src/debug/java/org/simple/clinic/login/AppLockConfigModule.kt
@@ -2,11 +2,7 @@ package org.simple.clinic.login
 
 import dagger.Module
 import dagger.Provides
-import org.simple.clinic.di.AppScope
 import org.simple.clinic.login.applock.AppLockConfig
-import org.simple.clinic.storage.MemoryValue
-import org.simple.clinic.util.Optional
-import java.time.Instant
 import java.util.concurrent.TimeUnit
 
 @Module
@@ -15,11 +11,5 @@ class AppLockConfigModule {
   @Provides
   fun appLockConfig(): AppLockConfig {
     return AppLockConfig(lockAfterTimeMillis = TimeUnit.SECONDS.toMillis(30))
-  }
-
-  @Provides
-  @AppScope
-  fun provideAppLockAfterTimestamp(): MemoryValue<Optional<Instant>> {
-    return MemoryValue(defaultValue = Optional.empty())
   }
 }

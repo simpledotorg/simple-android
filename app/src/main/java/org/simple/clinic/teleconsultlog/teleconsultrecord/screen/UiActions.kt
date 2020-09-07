3dc1d41b0706e650db099ff682c4b75e031f3e72
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-09-07 17:25:17 +0530

    Add support for recording a teleconsultation log for the patient #2 (#1682)
    * Rename `createTeleconsultRecordForMO` to `createTeleconsultRecordForMedicalOfficer`
    
    * Use `Transaction` for `TeleconsultRecordWithPrescribedDrugs#getPrescribedUuidForTeleconsultRecordUuid`
    
    * Handle `LoadTeleconsultRecordWithPrescribedDrugs` effect
    
    * Handle create teleconsult record effect
    
    * Stop passing teleconuslt record id to `UiActions#navigateToTeleconsultSuccessScreen`
    
    We can get the teleconsult record id from the screen args. So there is no need for redirection from effect handler to update and then back to effect handler.
    
    * When teleconsult record is created then navigate to teleconsult success screen
    
    * Update model when teleconsult record info exists
    
    * Load teleconsult record with prescribed drugs on screen create
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/teleconsultlog/teleconsultrecord/screen/UiActions.kt
+++ app/src/main/java/org/simple/clinic/teleconsultlog/teleconsultrecord/screen/UiActions.kt
@@ -1,8 +1,6 @@
 package org.simple.clinic.teleconsultlog.teleconsultrecord.screen
 
-import java.util.UUID
-
 interface UiActions {
   fun goBackToPreviousScreen()
-  fun navigateToTeleconsultSuccessScreen(teleconsultRecordId: UUID)
+  fun navigateToTeleconsultSuccessScreen()
 }
--- app/src/test/java/org/simple/clinic/teleconsultlog/teleconsultrecord/screen/TeleconsultRecordEffectHandlerTest.kt
+++ app/src/test/java/org/simple/clinic/teleconsultlog/teleconsultrecord/screen/TeleconsultRecordEffectHandlerTest.kt
@@ -1,19 +1,39 @@
 package org.simple.clinic.teleconsultlog.teleconsultrecord.screen
 
+import com.nhaarman.mockitokotlin2.doNothing
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
+import com.nhaarman.mockitokotlin2.whenever
 import org.junit.After
 import org.junit.Test
+import org.simple.clinic.TestData
 import org.simple.clinic.mobius.EffectHandlerTestCase
+import org.simple.clinic.teleconsultlog.teleconsultrecord.Answer.Yes
+import org.simple.clinic.teleconsultlog.teleconsultrecord.TeleconsultRecordRepository
+import org.simple.clinic.teleconsultlog.teleconsultrecord.TeleconsultationType.Audio
+import org.simple.clinic.util.TestUtcClock
 import org.simple.clinic.util.scheduler.TestSchedulersProvider
+import java.time.Instant
 import java.util.UUID
 
 class TeleconsultRecordEffectHandlerTest {
 
+  private val teleconsultRecordId = UUID.fromString("7e9111e5-7707-434a-af36-dad313f406ee")
+  private val user = TestData.loggedInUser(
+      uuid = UUID.fromString("b5a3aabe-ec33-4e0b-a7a9-b2e4bd4c4303")
+  )
+  private val utcClock = TestUtcClock(instant = Instant.parse("2018-01-01T00:00:00Z"))
+  private val teleconsultRecordRepository = mock<TeleconsultRecordRepository>()
   private val uiActions = mock<UiActions>()
+
   private val effectHandler = TeleconsultRecordEffectHandler(
+      user = { user },
+      teleconsultRecordRepository = teleconsultRecordRepository,
       schedulersProvider = TestSchedulersProvider.trampoline(),
+      utcClock = utcClock,
       uiActions = uiActions
   ).build()
   private val effectHandlerTestCase = EffectHandlerTestCase(effectHandler)
@@ -37,16 +57,77 @@ class TeleconsultRecordEffectHandlerTest {
 
   @Test
   fun `when navigate to teleconsult success screen effect is received, then navigate to teleconsult success screen`() {
-    // given
-    val teleconsultRecordId = UUID.fromString("4d30c778-dec0-48f0-90a0-acf4d568eb6e")
-
     // when
-    effectHandlerTestCase.dispatch(NavigateToTeleconsultSuccess(teleconsultRecordId))
+    effectHandlerTestCase.dispatch(NavigateToTeleconsultSuccess)
 
     // then
     effectHandlerTestCase.assertNoOutgoingEvents()
 
-    verify(uiActions).navigateToTeleconsultSuccessScreen(teleconsultRecordId)
+    verify(uiActions).navigateToTeleconsultSuccessScreen()
     verifyNoMoreInteractions(uiActions)
+  }
+
+  @Test
+  fun `when load teleconsult record with prescribed drugs effect is received, then load the teleconsult record with prescribed drugs`() {
+    // given
+    val teleconsultRecordWithPrescribedDrugs = TestData.teleconsultRecordWithPrescribedDrugs(
+        teleconsultRecord = TestData.teleconsultRecord(
+            id = teleconsultRecordId
+        ),
+        prescribedDrugs = emptyList()
+    )
+
+    whenever(teleconsultRecordRepository.getTeleconsultRecordWithPrescribedDrugs(teleconsultRecordId)) doReturn teleconsultRecordWithPrescribedDrugs
+
+    // when
+    effectHandlerTestCase.dispatch(LoadTeleconsultRecordWithPrescribedDrugs(teleconsultRecordId))
+
+    // then
+    effectHandlerTestCase.assertOutgoingEvents(TeleconsultRecordWithPrescribedDrugsLoaded(teleconsultRecordWithPrescribedDrugs))
+
+    verifyZeroInteractions(uiActions)
+  }
+
+  @Test
+  fun `when create teleconsult record effect is received, then create the teleconsult record`() {
+    // given
+    val patientUuid = UUID.fromString("cd8c77c4-67de-482a-bfd0-2b035291b45d")
+    val teleconsultRecord = TestData.teleconsultRecord(
+        id = teleconsultRecordId,
+        patientId = patientUuid,
+        medicalOfficerId = user.uuid,
+        teleconsultRecordInfo = TestData.teleconsultRecordInfo(
+            recordedAt = Instant.parse("2018-01-01T00:00:00Z"),
+            teleconsultationType = Audio,
+            patientTookMedicines = Yes,
+            patientConsented = Yes,
+            medicalOfficerNumber = null
+        ),
+        teleconsultRequestInfo = null,
+        createdAt = Instant.now(utcClock),
+        updatedAt = Instant.now(utcClock)
+    )
+
+    // when
+    effectHandlerTestCase.dispatch(CreateTeleconsultRecord(
+        teleconsultRecordId = teleconsultRecordId,
+        patientUuid = patientUuid,
+        teleconsultationType = Audio,
+        patientTookMedicine = Yes,
+        patientConsented = Yes
+    ))
+
+    // then
+    effectHandlerTestCase.assertOutgoingEvents(TeleconsultRecordCreated)
+
+    verify(teleconsultRecordRepository).createTeleconsultRecordForMedicalOfficer(
+        teleconsultRecordId = teleconsultRecord.id,
+        patientUuid = teleconsultRecord.patientId,
+        medicalOfficerId = teleconsultRecord.medicalOfficerId,
+        teleconsultRecordInfo = teleconsultRecord.teleconsultRecordInfo!!
+    )
+    verifyNoMoreInteractions(teleconsultRecordRepository)
+
+    verifyZeroInteractions(uiActions)
   }
 }

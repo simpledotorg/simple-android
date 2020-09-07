1fd6114dac73211aa1aee9e1f1b0abe667fc35a2
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-09-07 09:34:30 +0530

    Handle `LoadTeleconsultRecordWithPrescribedDrugs` effect

--- app/src/main/java/org/simple/clinic/teleconsultlog/teleconsultrecord/screen/TeleconsultRecordUpdate.kt
+++ app/src/main/java/org/simple/clinic/teleconsultlog/teleconsultrecord/screen/TeleconsultRecordUpdate.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.teleconsultlog.teleconsultrecord.screen
 
 import com.spotify.mobius.Next
+import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
 import org.simple.clinic.mobius.dispatch
 
@@ -9,6 +10,7 @@ class TeleconsultRecordUpdate : Update<TeleconsultRecordModel, TeleconsultRecord
   override fun update(model: TeleconsultRecordModel, event: TeleconsultRecordEvent): Next<TeleconsultRecordModel, TeleconsultRecordEffect> {
     return when (event) {
       BackClicked -> dispatch(GoBack)
+      is TeleconsultRecordWithPrescribedDrugsLoaded -> noChange()
     }
   }
 }

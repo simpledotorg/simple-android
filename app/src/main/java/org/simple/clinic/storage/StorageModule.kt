c890ce8f1503f13a0f0ab772772494d602b074f2
Vinay Shenoy <vinay@obvious.in>
2020-09-07 11:39:12 +0530

    Fix the query executor used to run Room database queries

--- app/src/main/java/org/simple/clinic/storage/StorageModule.kt
+++ app/src/main/java/org/simple/clinic/storage/StorageModule.kt
@@ -6,6 +6,7 @@ import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteOpenHelper
 import dagger.Module
 import dagger.Provides
+import io.requery.android.database.sqlite.SQLiteGlobal
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.di.AppScope
 import org.simple.clinic.di.AppSqliteOpenHelperFactory
@@ -13,6 +14,7 @@ import org.simple.clinic.storage.migrations.RoomMigrationsModule
 import org.simple.clinic.storage.text.TextRecord
 import org.simple.clinic.storage.text.TextStoreModule
 import org.simple.clinic.user.User
+import org.simple.clinic.util.ThreadPools
 
 @Module(includes = [
   RoomMigrationsModule::class,
@@ -28,9 +30,20 @@ class StorageModule {
       factory: SupportSQLiteOpenHelper.Factory,
       migrations: List<@JvmSuppressWildcards Migration>
   ): AppDatabase {
+    // Don't occupy all connections with Room threads since there are
+    // non-Room accesses of the database which SQLite itself might do
+    // internally.
+    val sqliteThreadPoolCount = SQLiteGlobal.getWALConnectionPoolSize() / 2
+    val queryExecutor = ThreadPools.create(
+        corePoolSize = 1,
+        maxPoolSize = sqliteThreadPoolCount,
+        threadPrefix = "room-query-"
+    )
+
     return Room.databaseBuilder(appContext, AppDatabase::class.java, "red-db")
         .openHelperFactory(factory)
         .addMigrations(*migrations.toTypedArray())
+        .setQueryExecutor(queryExecutor)
         .build()
   }
 

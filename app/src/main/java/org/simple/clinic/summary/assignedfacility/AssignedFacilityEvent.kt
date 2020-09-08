d5eea428fb52d53c614cf324bbfb48bf7686840d
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-09-08 14:05:49 +0530

    Override `analyticsName` for `AssignedFacilityEvent`'s (#1685)

--- app/src/main/java/org/simple/clinic/summary/assignedfacility/AssignedFacilityEvent.kt
+++ app/src/main/java/org/simple/clinic/summary/assignedfacility/AssignedFacilityEvent.kt
@@ -6,8 +6,14 @@ import org.simple.clinic.widgets.UiEvent
 
 sealed class AssignedFacilityEvent : UiEvent
 
-data class AssignedFacilityLoaded(val facility: Optional<Facility>) : AssignedFacilityEvent()
+data class AssignedFacilityLoaded(val facility: Optional<Facility>) : AssignedFacilityEvent() {
+  override val analyticsName: String = "Assigned Facility:Facility Loaded"
+}
 
-object ChangeAssignedFacilityButtonClicked : AssignedFacilityEvent()
+object ChangeAssignedFacilityButtonClicked : AssignedFacilityEvent() {
+  override val analyticsName: String = "Assigned Facility:Change Facility"
+}
 
-data class AssignedFacilitySelected(val facility: Facility) : AssignedFacilityEvent()
+data class AssignedFacilitySelected(val facility: Facility) : AssignedFacilityEvent() {
+  override val analyticsName: String = "Assigned Facility:Facility Selected"
+}

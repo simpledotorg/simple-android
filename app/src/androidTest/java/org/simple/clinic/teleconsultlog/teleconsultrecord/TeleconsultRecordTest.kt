043ba6246b769db6fa98d837715b89fc38b5509f
Honey Sonwani <honey@obvious.in>
2020-09-22 18:13:20 +0530

    Fix qa-android-test failing
    `teleconsultation_records_should_be_fetched_correctly()` was failing because of unexpected teleconsultRecord was received in the list from `TeleconsultRecordRepositoryTest` which is added in the list now.

--- app/src/androidTest/java/org/simple/clinic/teleconsultlog/teleconsultrecord/TeleconsultRecordTest.kt
+++ app/src/androidTest/java/org/simple/clinic/teleconsultlog/teleconsultrecord/TeleconsultRecordTest.kt
@@ -35,10 +35,11 @@ class TeleconsultRecordTest {
   fun teleconsultation_records_should_be_fetched_correctly() {
 
     // given
-    val teleconsultRecordId1 = UUID.fromString("9ee0bc81-26e6-4ced-9b33-fa76a0a995e3")
+    val teleconsultRecordId1 = UUID.fromString("700ee55d-7f49-4bda-9a4a-c5ce903ce485")
     val teleconsultRecordId2 = UUID.fromString("f1ad859f-d076-4bae-b0f2-3fb23c60d880")
-    val patientId = UUID.fromString("a706dfbf-315a-4201-be42-10b784c6d0b4")
-    val medicalOffcerId = UUID.fromString("97c558bb-df4e-4c7a-a5ac-df2520ccd4b1")
+    val patientUuid1 = UUID.fromString("3c00cdf9-4304-4dc7-8d32-6fbd5cd8f14d")
+    val patientUuid2 = UUID.fromString("a706dfbf-315a-4201-be42-10b784c6d0b4")
+    val medicalOfficerUuid = UUID.fromString("7142092e-24b1-4757-b7b6-a00fbd60332b")
     val facilityId = UUID.fromString("f12c2316-85e4-466e-9c92-efa52605c6b9")
     val requesterId = UUID.fromString("7a131902-c214-4aa9-800f-201513d77fd7")
     val date = LocalDate.parse("2020-09-03")
@@ -59,8 +60,8 @@ class TeleconsultRecordTest {
 
     val teleconsultRecord1 = TestData.teleconsultRecord(
         id = teleconsultRecordId1,
-        patientId = patientId,
-        medicalOfficerId = medicalOffcerId,
+        patientId = patientUuid1,
+        medicalOfficerId = medicalOfficerUuid,
         teleconsultRecordInfo = teleconsultRecordInfo,
         teleconsultRequestInfo = teleconsultRequestInfo,
         timestamps = Timestamps(
@@ -72,8 +73,8 @@ class TeleconsultRecordTest {
 
     val teleconsultRecord2 = TestData.teleconsultRecord(
         id = teleconsultRecordId2,
-        patientId = patientId,
-        medicalOfficerId = medicalOffcerId,
+        patientId = patientUuid2,
+        medicalOfficerId = medicalOfficerUuid,
         teleconsultRecordInfo = teleconsultRecordInfo,
         teleconsultRequestInfo = teleconsultRequestInfo,
         timestamps = Timestamps(
@@ -83,19 +84,19 @@ class TeleconsultRecordTest {
         )
     )
 
-    val teleconsultRecordList = listOf(
+    val expectedTeleconsultRecordList = listOf(
         teleconsultRecord1,
         teleconsultRecord2
     )
 
     teleconsultRecordDao.save(
-        teleconsultRecordList
+        expectedTeleconsultRecordList
     )
 
     // when
-    val teleconsultRecordListReceived =  teleconsultRecordDao.getAll()
+    val teleconsultRecordReceived =  teleconsultRecordDao.getAll()
 
     // then
-    assertThat(teleconsultRecordListReceived).isEqualTo(teleconsultRecordList)
+    assertThat(teleconsultRecordReceived).isEqualTo(expectedTeleconsultRecordList)
   }
 }

18658f50ef7e254b3959788382be86d6817987b6
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-09-23 11:17:40 +0530

    Setup DI in `ContactDoctorSheet`

--- app/src/main/java/org/simple/clinic/summary/teleconsultation/contactdoctor/ContactDoctorSheet.kt
+++ app/src/main/java/org/simple/clinic/summary/teleconsultation/contactdoctor/ContactDoctorSheet.kt
@@ -3,16 +3,22 @@ package org.simple.clinic.summary.teleconsultation.contactdoctor
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.sheet_contact_doctor_new.*
+import org.simple.clinic.ClinicApp
 import org.simple.clinic.R
+import org.simple.clinic.di.InjectorProviderContextWrapper
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.summary.teleconsultation.sync.MedicalOfficer
+import org.simple.clinic.util.LocaleOverrideContextWrapper
 import org.simple.clinic.util.unsafeLazy
+import org.simple.clinic.util.wrap
 import org.simple.clinic.widgets.BottomSheetActivity
 import org.simple.clinic.widgets.DividerItemDecorator
 import org.simple.clinic.widgets.ItemAdapter
 import org.simple.clinic.widgets.dp
+import java.util.Locale
 import javax.inject.Inject
 
 class ContactDoctorSheet : BottomSheetActivity(), ContactDoctorUi {
@@ -25,6 +31,9 @@ class ContactDoctorSheet : BottomSheetActivity(), ContactDoctorUi {
 
   @Inject
   lateinit var effectHandler: ContactDoctorEffectHandler
+
+  @Inject
+  lateinit var locale: Locale
 
   private val itemAdapter = ItemAdapter(DoctorListItem.DiffCallback())
 
@@ -40,6 +49,8 @@ class ContactDoctorSheet : BottomSheetActivity(), ContactDoctorUi {
         modelUpdateListener = uiRenderer::render
     )
   }
+
+  private lateinit var component: ContactDoctorComponent
 
   override fun onStart() {
     super.onStart()
@@ -71,5 +82,25 @@ class ContactDoctorSheet : BottomSheetActivity(), ContactDoctorUi {
 
   override fun showMedicalOfficers(medicalOfficers: List<MedicalOfficer>) {
     itemAdapter.submitList(DoctorListItem.from(medicalOfficers))
+  }
+
+  override fun attachBaseContext(baseContext: Context) {
+    setupDiGraph()
+
+    val wrappedContext = baseContext
+        .wrap { LocaleOverrideContextWrapper.wrap(it, locale) }
+        .wrap { InjectorProviderContextWrapper.wrap(it, component) }
+        .wrap { ViewPumpContextWrapper.wrap(it) }
+
+    super.attachBaseContext(wrappedContext)
+  }
+
+  private fun setupDiGraph() {
+    component = ClinicApp.appComponent
+        .contactDoctorComponent()
+        .activity(this)
+        .build()
+
+    component.inject(this)
   }
 }

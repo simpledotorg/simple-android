b4bda8279e17b1fe79a3961168e88d9450ad0e3d
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-09-29 14:52:53 +0530

    When teleconsult request is created, then load the patient teleconsult info

--- app/src/main/java/org/simple/clinic/summary/teleconsultation/contactdoctor/ContactDoctorUpdate.kt
+++ app/src/main/java/org/simple/clinic/summary/teleconsultation/contactdoctor/ContactDoctorUpdate.kt
@@ -3,6 +3,7 @@ package org.simple.clinic.summary.teleconsultation.contactdoctor
 import com.spotify.mobius.Next
 import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
+import org.simple.clinic.mobius.dispatch
 import org.simple.clinic.mobius.next
 
 class ContactDoctorUpdate : Update<ContactDoctorModel, ContactDoctorEvent, ContactDoctorEffect> {
@@ -10,7 +11,11 @@ class ContactDoctorUpdate : Update<ContactDoctorModel, ContactDoctorEvent, Conta
   override fun update(model: ContactDoctorModel, event: ContactDoctorEvent): Next<ContactDoctorModel, ContactDoctorEffect> {
     return when (event) {
       is MedicalOfficersLoaded -> next(model.medicalOfficersLoaded(event.medicalOfficers))
-      is TeleconsultRequestCreated -> noChange()
+      is TeleconsultRequestCreated -> dispatch(LoadPatientTeleconsultInfo(
+          patientUuid = model.patientUuid,
+          teleconsultRecordId = event.teleconsultRecordId,
+          doctorPhoneNumber = event.doctorPhoneNumber
+      ))
       is PatientTeleconsultInfoLoaded -> noChange()
     }
   }
--- app/src/test/java/org/simple/clinic/summary/teleconsultation/contactdoctor/ContactDoctorUpdateTest.kt
+++ app/src/test/java/org/simple/clinic/summary/teleconsultation/contactdoctor/ContactDoctorUpdateTest.kt
@@ -1,7 +1,9 @@
 package org.simple.clinic.summary.teleconsultation.contactdoctor
 
+import com.spotify.mobius.test.NextMatchers.hasEffects
 import com.spotify.mobius.test.NextMatchers.hasModel
 import com.spotify.mobius.test.NextMatchers.hasNoEffects
+import com.spotify.mobius.test.NextMatchers.hasNoModel
 import com.spotify.mobius.test.UpdateSpec
 import com.spotify.mobius.test.UpdateSpec.assertThatNext
 import org.junit.Test
@@ -10,9 +12,12 @@ import java.util.UUID
 
 class ContactDoctorUpdateTest {
 
+  private val patientUuid = UUID.fromString("ba46b46a-058a-4694-bb02-69aad1694bc7")
+  private val model = ContactDoctorModel.create(patientUuid)
+  private val updateSpec = UpdateSpec(ContactDoctorUpdate())
+
   @Test
   fun `when medical officers are loaded, then update the model`() {
-    val patientUuid = UUID.fromString("ba46b46a-058a-4694-bb02-69aad1694bc7")
     val medicalOfficers = listOf(
         TestData.medicalOfficer(
             id = UUID.fromString("a07e53e8-959b-4d08-bea7-161932ecc0a5"),
@@ -21,15 +26,33 @@ class ContactDoctorUpdateTest {
         )
     )
 
-    val model = ContactDoctorModel.create(patientUuid)
-    val updateSpec = UpdateSpec(ContactDoctorUpdate())
-
     updateSpec
         .given(model)
         .whenEvent(MedicalOfficersLoaded(medicalOfficers))
         .then(assertThatNext(
             hasModel(model.medicalOfficersLoaded(medicalOfficers)),
             hasNoEffects()
+        ))
+  }
+
+  @Test
+  fun `when teleconsult request is created, then load patient teleconsult info`() {
+    val teleconsultRecordId = UUID.fromString("64ee25e0-8d32-42f5-b3e9-1a34f80e8e56")
+    val doctorPhoneNumber = "+911111111111"
+
+    updateSpec
+        .given(model)
+        .whenEvent(TeleconsultRequestCreated(
+            teleconsultRecordId = teleconsultRecordId,
+            doctorPhoneNumber = doctorPhoneNumber
+        ))
+        .then(assertThatNext(
+            hasNoModel(),
+            hasEffects(LoadPatientTeleconsultInfo(
+                patientUuid = patientUuid,
+                teleconsultRecordId = teleconsultRecordId,
+                doctorPhoneNumber = doctorPhoneNumber
+            ))
         ))
   }
 }

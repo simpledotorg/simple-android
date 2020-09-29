e5cfd7615c486d311e64b9630301a59ee4e31a00
Vinay Shenoy <vinay@obvious.in>
2020-09-29 11:31:40 +0530

    Change fetching of remote config to be a synchronous call

--- app/src/sharedTest/java/org/simple/clinic/remoteconfig/NoOpRemoteConfigService.kt
+++ app/src/sharedTest/java/org/simple/clinic/remoteconfig/NoOpRemoteConfigService.kt
@@ -1,14 +1,12 @@
 package org.simple.clinic.remoteconfig
 
-import io.reactivex.Completable
-
 class NoOpRemoteConfigService(private val configReader: ConfigReader) : RemoteConfigService {
 
   override fun reader(): ConfigReader {
     return configReader
   }
 
-  override fun update(): Completable {
-    return Completable.complete()
+  override fun update() {
+    /* Nothing to do here */
   }
 }

63b8e133964342d47049e6d7e1a1dcdf515bc6f5
Sasikanth Miriyampalli <hello@sasikanth.dev>
2020-10-12 12:31:43 +0530

    Request storage permission when downloading or sharing prescription (#1894)
    * Request storage permissions when downloading or sharing prescription
    
    * Update CHANGELOG
    
    * Add storage permission to AndroidManifest
    
    We require storage permission for Android version >10 to access external storage. Without that permission we cannot request the storage permission at run time or manually enable/disable the permission from Android settings

--- app/src/main/java/org/simple/clinic/teleconsultlog/shareprescription/TeleconsultSharePrescriptionEvent.kt
+++ app/src/main/java/org/simple/clinic/teleconsultlog/shareprescription/TeleconsultSharePrescriptionEvent.kt
@@ -1,9 +1,13 @@
 package org.simple.clinic.teleconsultlog.shareprescription
 
+import android.Manifest
 import android.graphics.Bitmap
 import android.net.Uri
 import org.simple.clinic.drugs.PrescribedDrug
 import org.simple.clinic.patient.PatientProfile
+import org.simple.clinic.platform.util.RuntimePermissionResult
+import org.simple.clinic.util.Optional
+import org.simple.clinic.util.RequiresPermission
 import org.simple.clinic.widgets.UiEvent
 
 sealed class TeleconsultSharePrescriptionEvent : UiEvent
@@ -14,7 +18,12 @@ data class SignatureLoaded(val bitmap: Bitmap) : TeleconsultSharePrescriptionEve
 
 data class MedicalRegistrationIdLoaded(val medicalRegistrationId: String) : TeleconsultSharePrescriptionEvent()
 
-data class DownloadClicked(val bitmap: Bitmap) : TeleconsultSharePrescriptionEvent() {
+data class DownloadClicked(
+    val bitmap: Bitmap,
+    override var permission: Optional<RuntimePermissionResult> = Optional.empty(),
+    override val permissionString: String = Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    override val permissionRequestCode: Int = 1
+) : TeleconsultSharePrescriptionEvent(), RequiresPermission {
   override val analyticsName: String = "Share Prescription Screen:Download Clicked"
 }
 
@@ -26,7 +35,12 @@ object DoneClicked : TeleconsultSharePrescriptionEvent() {
 
 data class PatientProfileLoaded(val patientProfile: PatientProfile) : TeleconsultSharePrescriptionEvent()
 
-data class ShareClicked(val bitmap: Bitmap) : TeleconsultSharePrescriptionEvent() {
+data class ShareClicked(
+    val bitmap: Bitmap,
+    override var permission: Optional<RuntimePermissionResult> = Optional.empty(),
+    override val permissionString: String = Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    override val permissionRequestCode: Int = 2
+) : TeleconsultSharePrescriptionEvent(), RequiresPermission {
   override val analyticsName: String = "Share Prescription Screen:Share Clicked"
 }
 

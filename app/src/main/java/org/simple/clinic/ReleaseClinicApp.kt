0d0705f715812fb193a95160b7db62b8e869999f
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-10-15 12:24:26 +0530

    Turn off `Logger` for `com.spotify.mobius.ControllerStateBase`

--- app/src/main/java/org/simple/clinic/ReleaseClinicApp.kt
+++ app/src/main/java/org/simple/clinic/ReleaseClinicApp.kt
@@ -1,18 +1,31 @@
 package org.simple.clinic
 
 import android.annotation.SuppressLint
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
 import org.simple.clinic.analytics.MixpanelAnalyticsReporter
 import org.simple.clinic.analytics.swallowErrors
 import org.simple.clinic.di.AppComponent
 import org.simple.clinic.di.AppModule
 import org.simple.clinic.di.DaggerAppComponent
 import org.simple.clinic.util.unsafeLazy
+import org.slf4j.LoggerFactory
 
 @SuppressLint("Registered")
 class ReleaseClinicApp : ClinicApp() {
 
   override val analyticsReporters by unsafeLazy {
     listOf(MixpanelAnalyticsReporter(this).swallowErrors())
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+
+    // Since it's possible to log sensitive information
+    // present in Mobius model & events. We are turning off the logs
+    // for `ControllerStateBase` where we log those.
+    val logger = LoggerFactory.getLogger("com.spotify.mobius.ControllerStateBase") as Logger
+    logger.level = Level.OFF
   }
 
   override fun buildDaggerGraph(): AppComponent {

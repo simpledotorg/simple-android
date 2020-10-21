b1b27457a02aaac973c3ba0c2e72eec60730b21b
Vinay Shenoy <vinay@obvious.in>
2020-10-21 18:03:49 +0530

    Track the current facility in the model

--- app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeUpdate.kt
+++ app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeUpdate.kt
@@ -1,9 +1,9 @@
 package org.simple.clinic.facility.change.confirm
 
 import com.spotify.mobius.Next
-import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
 import org.simple.clinic.mobius.dispatch
+import org.simple.clinic.mobius.next
 
 class ConfirmFacilityChangeUpdate : Update<ConfirmFacilityChangeModel, ConfirmFacilityChangeEvent, ConfirmFacilityChangeEffect> {
   override fun update(
@@ -13,7 +13,7 @@ class ConfirmFacilityChangeUpdate : Update<ConfirmFacilityChangeModel, ConfirmFa
     return when (event) {
       is FacilityChangeConfirmed -> dispatch(ChangeFacilityEffect(event.selectedFacility))
       FacilityChanged -> dispatch(CloseSheet)
-      is CurrentFacilityLoaded -> noChange()
+      is CurrentFacilityLoaded -> next(model.currentFacilityLoaded(event.currentFacility))
     }
   }
 }
--- app/src/test/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeUpdateTest.kt
+++ app/src/test/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeUpdateTest.kt
@@ -1,6 +1,8 @@
 package org.simple.clinic.facility.change.confirm
 
 import com.spotify.mobius.test.NextMatchers.hasEffects
+import com.spotify.mobius.test.NextMatchers.hasModel
+import com.spotify.mobius.test.NextMatchers.hasNoEffects
 import com.spotify.mobius.test.NextMatchers.hasNoModel
 import com.spotify.mobius.test.UpdateSpec
 import com.spotify.mobius.test.UpdateSpec.assertThatNext
@@ -12,11 +14,16 @@ class ConfirmFacilityChangeUpdateTest {
 
   private val updateSpec = UpdateSpec(ConfirmFacilityChangeUpdate())
 
+  private val currentFacility = TestData.facility(UUID.fromString("3d362e26-c8ca-4bd6-8910-4fbcbcb98678"))
+
+  private val defaultModel = ConfirmFacilityChangeModel.create()
+
   @Test
   fun `when facility change is confirmed then user's facility should be changed`() {
     val selectedFacility = TestData.facility(UUID.fromString("ef47531f-9b8c-4045-8578-eda31f0952c4"))
+
     updateSpec
-        .given(ConfirmFacilityChangeModel.create())
+        .given(defaultModel)
         .whenEvent(FacilityChangeConfirmed(selectedFacility))
         .then(
             assertThatNext(
@@ -29,12 +36,25 @@ class ConfirmFacilityChangeUpdateTest {
   @Test
   fun `when user's facility is changed then the sheet should be closed`() {
     updateSpec
-        .given(ConfirmFacilityChangeModel.create())
+        .given(defaultModel)
         .whenEvent(FacilityChanged)
         .then(
             assertThatNext(
                 hasNoModel(),
                 hasEffects(CloseSheet as ConfirmFacilityChangeEffect)
+            )
+        )
+  }
+
+  @Test
+  fun `when the current facility is loaded, then update the ui`() {
+    updateSpec
+        .given(defaultModel)
+        .whenEvent(CurrentFacilityLoaded(currentFacility))
+        .then(
+            assertThatNext(
+                hasModel(defaultModel.currentFacilityLoaded(currentFacility)),
+                hasNoEffects()
             )
         )
   }

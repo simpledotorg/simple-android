0bcb6f19a74dbaa737660f09c0e1ee1c179b0cff
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-11-03 10:41:59 +0530

    Use ViewBinding in `SplashScreen`

--- app/src/main/java/org/simple/clinic/splash/SplashScreen.kt
+++ app/src/main/java/org/simple/clinic/splash/SplashScreen.kt
@@ -5,13 +5,22 @@ import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.findNavController
 import com.airbnb.lottie.LottieDrawable
-import kotlinx.android.synthetic.main.screen_splash.view.*
 import org.simple.clinic.R
+import org.simple.clinic.databinding.ScreenSplashBinding
 
 class SplashScreen(context: Context, attributeSet: AttributeSet) : ConstraintLayout(context, attributeSet) {
 
+  private var binding: ScreenSplashBinding? = null
+
+  private val splashLottieView
+    get() = binding!!.splashLottieView
+
+  private val nextButton
+    get() = binding!!.nextButton
+
   override fun onFinishInflate() {
     super.onFinishInflate()
+    binding = ScreenSplashBinding.bind(this)
     if (isInEditMode) {
       return
     }
@@ -25,5 +34,10 @@ class SplashScreen(context: Context, attributeSet: AttributeSet) : ConstraintLay
     nextButton.setOnClickListener {
       findNavController().navigate(R.id.action_splashScreen_to_onboardingScreen)
     }
+  }
+
+  override fun onDetachedFromWindow() {
+    binding = null
+    super.onDetachedFromWindow()
   }
 }

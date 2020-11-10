b839045200b26dfc7737a53283d5027e90c13314
Vinay Shenoy <vinay@obvious.in>
2020-11-10 11:45:29 +0530

    Send scan results to the parent activity from `ScanSimpleIdScreen`

--- app/src/main/java/org/simple/clinic/scanid/ScanSimpleIdUpdate.kt
+++ app/src/main/java/org/simple/clinic/scanid/ScanSimpleIdUpdate.kt
@@ -5,6 +5,8 @@ import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
 import org.simple.clinic.mobius.dispatch
 import org.simple.clinic.mobius.next
+import org.simple.clinic.patient.businessid.Identifier
+import org.simple.clinic.patient.businessid.Identifier.IdentifierType.BpPassport
 import org.simple.clinic.scanid.ShortCodeValidationResult.Failure
 import org.simple.clinic.scanid.ShortCodeValidationResult.Success
 import java.util.UUID
@@ -24,7 +26,8 @@ class ScanSimpleIdUpdate : Update<ScanSimpleIdModel, ScanSimpleIdEvent, ScanSimp
   private fun simpleIdQrScanned(event: ScanSimpleIdScreenQrCodeScanned): Next<ScanSimpleIdModel, ScanSimpleIdEffect> {
     return try {
       val bpPassportCode = UUID.fromString(event.text)
-      noChange()
+      val identifier = Identifier(bpPassportCode.toString(), BpPassport)
+      dispatch(SendScannedIdentifierResult(ScannedId(identifier)))
     } catch (e: IllegalArgumentException) {
       noChange()
     }
@@ -32,10 +35,10 @@ class ScanSimpleIdUpdate : Update<ScanSimpleIdModel, ScanSimpleIdEvent, ScanSimp
 
   private fun shortCodeValidated(model: ScanSimpleIdModel, event: ShortCodeValidated): Next<ScanSimpleIdModel, ScanSimpleIdEffect> {
     val effect = when (event.result) {
-      Success -> null
+      Success -> SendScannedIdentifierResult(EnteredShortCode(model.shortCode!!.shortCodeText))
       is Failure -> ShowShortCodeValidationError(event.result)
     }
 
-    return if (effect != null) dispatch(effect) else noChange()
+    return dispatch(effect)
   }
 }

2175f27a78bf6c67cd7bb5e3976c2019e428bfbd
Vinay <vinay@obvious.in>
2020-11-30 13:10:49 +0530

    Add debug activity for verifying the WebView launch

---
+++ app/src/debug/java/org/simple/clinic/WebviewTestActivity.kt
@@ -0,0 +1,30 @@
+package org.simple.clinic
+
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import org.simple.clinic.util.withLocale
+import java.util.Locale
+
+class WebviewTestActivity: AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(WebView(this).apply {
+      layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+    })
+  }
+
+  override fun attachBaseContext(baseContext: Context) {
+    super.attachBaseContext(baseContext)
+    applyOverrideConfiguration(Configuration())
+  }
+
+  override fun applyOverrideConfiguration(overrideConfiguration: Configuration) {
+    super.applyOverrideConfiguration(overrideConfiguration.withLocale(Locale.UK))
+  }
+}

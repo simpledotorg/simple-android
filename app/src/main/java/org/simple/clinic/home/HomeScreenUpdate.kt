789f3d86af5a2ac1608526bef24d11489ea3878f
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-12-01 12:36:42 +0530

    Open patient summary if there is patient for scanned BP passport

--- app/src/main/java/org/simple/clinic/home/HomeScreenUpdate.kt
+++ app/src/main/java/org/simple/clinic/home/HomeScreenUpdate.kt
@@ -1,18 +1,22 @@
 package org.simple.clinic.home
 
 import com.spotify.mobius.Next
-import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
 import org.simple.clinic.mobius.dispatch
 import org.simple.clinic.mobius.next
 
 class HomeScreenUpdate : Update<HomeScreenModel, HomeScreenEvent, HomeScreenEffect> {
+
   override fun update(model: HomeScreenModel, event: HomeScreenEvent): Next<HomeScreenModel, HomeScreenEffect> {
     return when (event) {
       HomeFacilitySelectionClicked -> dispatch(OpenFacilitySelection)
       is CurrentFacilityLoaded -> next(model.facilityLoaded(event.facility))
       is OverdueAppointmentCountLoaded -> next(model.overdueAppointmentCountLoaded(event.overdueAppointmentCount))
-      is PatientSearchByIdentifierCompleted -> noChange()
+      is PatientSearchByIdentifierCompleted -> scannedPatientBusinessId(event)
     }
+  }
+
+  private fun scannedPatientBusinessId(event: PatientSearchByIdentifierCompleted): Next<HomeScreenModel, HomeScreenEffect> {
+    return dispatch(OpenPatientSummary(event.patient.get().uuid))
   }
 }
---
+++ app/src/test/java/org/simple/clinic/home/HomeScreenUpdateTest.kt
@@ -0,0 +1,34 @@
+package org.simple.clinic.home
+
+import com.spotify.mobius.test.NextMatchers
+import com.spotify.mobius.test.UpdateSpec
+import org.junit.Test
+import org.simple.clinic.TestData
+import org.simple.clinic.patient.businessid.Identifier
+import org.simple.clinic.util.Optional
+import java.util.UUID
+
+class HomeScreenUpdateTest {
+
+  private val defaultModel = HomeScreenModel.create()
+  private val updateSpec = UpdateSpec(HomeScreenUpdate())
+
+  @Test
+  fun `when the scanned identifier has a corresponding patient, the patient summary screen must be opened`() {
+    val identifier = TestData.identifier("88d12415-b10d-4ebb-bf48-482ece022139", Identifier.IdentifierType.BpPassport)
+    val patient = TestData.patient(uuid = UUID.fromString("614a3a62-92be-4551-92d0-beca649cfd7c"))
+
+    val event = PatientSearchByIdentifierCompleted(
+        patient = Optional.of(patient),
+        identifier = identifier
+    )
+
+    updateSpec
+        .given(defaultModel)
+        .whenEvent(event)
+        .then(UpdateSpec.assertThatNext(
+            NextMatchers.hasNoModel(),
+            NextMatchers.hasEffects(OpenPatientSummary(patient.uuid))
+        ))
+  }
+}

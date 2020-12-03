7d1635446c9af7d94b9c017a7016e22e5d93a58d
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-12-03 16:56:32 +0530

    Set target aspect ration for the image analyzer
    In some scenarios it might be possible certain devices send a large image to image analyser which will cause the analyser to fail. In order to resolve this we calculate the aspect ratio from the view finder/preview dimensions and set the target aspect ratio. Just to be safe we are also setting the target orientation to avoid any orientation related scanning issues.

--- app/src/main/java/org/simple/clinic/widgets/qrcodescanner/QrCodeScannerView.kt
+++ app/src/main/java/org/simple/clinic/widgets/qrcodescanner/QrCodeScannerView.kt
@@ -2,9 +2,11 @@ package org.simple.clinic.widgets.qrcodescanner
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.view.View
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
@@ -17,6 +19,9 @@ import org.simple.clinic.R
 import org.simple.clinic.di.injector
 import java.util.concurrent.Executors
 import javax.inject.Inject
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class QrCodeScannerView
 constructor(
@@ -24,6 +29,11 @@ constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr), IQrCodeScannerView {
+
+  companion object {
+    private const val RATIO_4_3_VALUE = 4.0 / 3.0
+    private const val RATIO_16_9_VALUE = 16.0 / 9.0
+  }
 
   @Inject
   lateinit var activity: AppCompatActivity
@@ -55,6 +65,13 @@ constructor(
   }
 
   private fun startCamera(cameraProvider: ProcessCameraProvider) {
+    // Get screen metrics used to setup camera for full screen resolution
+    val metrics = DisplayMetrics().also { previewView.display.getRealMetrics(it) }
+
+    val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+
+    val rotation = previewView.display.rotation
+
     val cameraSelector = CameraSelector.Builder()
         .requireLensFacing(CameraSelector.LENS_FACING_BACK)
         .build()
@@ -62,7 +79,8 @@ constructor(
     val preview = Preview.Builder().build()
 
     val analyzer = ImageAnalysis.Builder()
-        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+        .setTargetAspectRatio(screenAspectRatio)
+        .setTargetRotation(rotation)
         .build()
         .also {
           it.setAnalyzer(cameraExecutor, ZxingQrCodeAnalyzer(scans::onNext))
@@ -72,6 +90,27 @@ constructor(
 
     cameraProvider.bindToLifecycle(activity, cameraSelector, preview, analyzer)
     preview.setSurfaceProvider(previewView.surfaceProvider)
+  }
+
+  /**
+   *  [androidx.camera.core.ImageAnalysisConfig] requires enum value of
+   *  [androidx.camera.core.AspectRatio]. Currently it has values of 4:3 & 16:9.
+   *
+   *  Detecting the most suitable ratio for dimensions provided in @params by counting absolute
+   *  of preview ratio to one of the provided values.
+   *
+   *  @param width - preview width
+   *  @param height - preview height
+   *  @return suitable aspect ratio
+   *
+   *  source: https://github.com/android/camera-samples/blob/bfc46b68ef51f337104d3487c2394b204cf0d453/CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/CameraFragment.kt#L350
+   */
+  private fun aspectRatio(width: Int, height: Int): Int {
+    val previewRatio = max(width, height).toDouble() / min(width, height)
+    if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+      return AspectRatio.RATIO_4_3
+    }
+    return AspectRatio.RATIO_16_9
   }
 
   override fun onDetachedFromWindow() {

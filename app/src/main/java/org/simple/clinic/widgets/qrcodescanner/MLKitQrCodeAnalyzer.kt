9375dc5d48ce56a9d04d2cfacd724011ac09fc52
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-12-08 12:17:56 +0530

    Add `MLKitQrCodeAnalyzer`

---
+++ app/src/main/java/org/simple/clinic/widgets/qrcodescanner/MLKitQrCodeAnalyzer.kt
@@ -0,0 +1,43 @@
+package org.simple.clinic.widgets.qrcodescanner
+
+import android.annotation.SuppressLint
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+
+class MLKitQrCodeAnalyzer(
+    private val onQrCodeDetected: OnQrCodeDetected
+) : ImageAnalysis.Analyzer {
+
+  private val options = BarcodeScannerOptions.Builder()
+      .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+      .build()
+  private val scanner = BarcodeScanning.getClient(options)
+
+
+  @SuppressLint("UnsafeExperimentalUsageError")
+  override fun analyze(imageProxy: ImageProxy) {
+    val mediaImage = imageProxy.image
+    if (mediaImage != null) {
+      val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+      scanner.process(image)
+          .addOnSuccessListener { barcodes ->
+            barcodes.forEach { barcode ->
+              val qrCodeResult = barcode.rawValue
+              if (qrCodeResult != null) {
+                onQrCodeDetected(qrCodeResult)
+              }
+            }
+          }
+          .addOnFailureListener {
+            // Do nothing
+          }
+          .addOnCompleteListener {
+            imageProxy.close()
+          }
+    }
+  }
+}

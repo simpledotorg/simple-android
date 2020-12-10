7f402e6301a935d02b6a586d7d8e379de586124c
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-12-10 07:49:01 +0530

    Track `ScanSearchState` in `ScanSimpleIdModel`

--- app/src/main/java/org/simple/clinic/scanid/ScanSimpleIdModel.kt
+++ app/src/main/java/org/simple/clinic/scanid/ScanSimpleIdModel.kt
@@ -5,11 +5,12 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ScanSimpleIdModel(
-    val shortCode: ShortCodeInput?
+    val shortCode: ShortCodeInput?,
+    val scanSearchState: ScanSearchState
 ) : Parcelable {
 
   companion object {
-    fun create() = ScanSimpleIdModel(shortCode = null)
+    fun create() = ScanSimpleIdModel(shortCode = null, scanSearchState = ScanSearchState.NotSearching)
   }
 
   fun shortCodeChanged(shortCode: ShortCodeInput): ScanSimpleIdModel {

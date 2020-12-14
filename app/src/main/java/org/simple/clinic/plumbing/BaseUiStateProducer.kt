d946dab9ae1bad596d95cb1ee6e9e2dcd526563b
Vinay <vinay@obvious.in>
2020-12-14 12:06:06 +0530

    Remove unused classes related to the v2 screen architecture

--- app/src/main/java/org/simple/clinic/plumbing/BaseUiStateProducer.kt
+++
@@ -1,8 +0,0 @@
-package org.simple.clinic.plumbing
-
-import io.reactivex.ObservableTransformer
-import io.reactivex.subjects.BehaviorSubject
-
-abstract class BaseUiStateProducer<E, S> : ObservableTransformer<E, S> {
-  val states: BehaviorSubject<S> = BehaviorSubject.create()
-}

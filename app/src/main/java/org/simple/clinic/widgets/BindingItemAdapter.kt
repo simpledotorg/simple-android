931cbfbf68c50f7543391aceb8c9979b5145bc3b
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-12-16 10:58:58 +0530

    Implement `BindingItemAdapter`

---
+++ app/src/main/java/org/simple/clinic/widgets/BindingItemAdapter.kt
@@ -0,0 +1,51 @@
+package org.simple.clinic.widgets
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.viewbinding.ViewBinding
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
+import org.simple.clinic.widgets.recyclerview.BindingViewHolder
+
+// TODO (SM): Rename to `ItemAdapter` once view binding migration for adapters is finished.
+open class BindingItemAdapter<I : BindingItemAdapter.Item<E>, E>(
+    diffCallback: DiffUtil.ItemCallback<I>,
+    private val bindings: Map<Int, (layoutInflater: LayoutInflater, parent: ViewGroup) -> ViewBinding>
+) : ListAdapter<I, BindingViewHolder>(diffCallback) {
+
+  private val eventSubject: Subject<E> = PublishSubject.create<E>()
+
+  val itemEvents: Observable<E> = eventSubject.hide()
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder {
+    val layoutInflater = LayoutInflater.from(parent.context)
+
+    val binding = bindings
+        .filter { it.key == viewType }
+        .values
+        .first()
+
+    return BindingViewHolder(binding = binding.invoke(layoutInflater, parent))
+  }
+
+  override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
+    getItem(position).render(holder, eventSubject)
+  }
+
+  override fun getItemViewType(position: Int): Int {
+    return getItem(position).layoutResId()
+  }
+
+  interface Item<E> {
+
+    @LayoutRes
+    fun layoutResId(): Int
+
+    fun render(holder: BindingViewHolder, subject: Subject<E>)
+  }
+}
+
---
+++ app/src/main/java/org/simple/clinic/widgets/recyclerview/BindingViewHolder.kt
@@ -0,0 +1,6 @@
+package org.simple.clinic.widgets.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+class BindingViewHolder(val binding: ViewBinding) : RecyclerView.ViewHolder(binding.root)

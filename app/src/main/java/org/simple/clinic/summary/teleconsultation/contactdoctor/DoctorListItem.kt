aa1d630567f5a239dfa4998a1e890083eeb2f7e4
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-12-17 09:56:56 +0530

    Migrate `DoctorListItem` to use ViewBinding

--- app/src/main/java/org/simple/clinic/summary/teleconsultation/contactdoctor/DoctorListItem.kt
+++ app/src/main/java/org/simple/clinic/summary/teleconsultation/contactdoctor/DoctorListItem.kt
@@ -2,20 +2,20 @@ package org.simple.clinic.summary.teleconsultation.contactdoctor
 
 import androidx.recyclerview.widget.DiffUtil
 import io.reactivex.subjects.Subject
-import kotlinx.android.synthetic.main.list_contact_doctor.*
 import org.simple.clinic.R
+import org.simple.clinic.databinding.ListContactDoctorBinding
 import org.simple.clinic.summary.teleconsultation.contactdoctor.DoctorListItem.Event.SmsClicked
 import org.simple.clinic.summary.teleconsultation.contactdoctor.DoctorListItem.Event.WhatsAppClicked
 import org.simple.clinic.summary.teleconsultation.sync.MedicalOfficer
-import org.simple.clinic.widgets.ItemAdapter
-import org.simple.clinic.widgets.recyclerview.ViewHolderX
+import org.simple.clinic.widgets.BindingItemAdapter
+import org.simple.clinic.widgets.recyclerview.BindingViewHolder
 import java.util.UUID
 
 data class DoctorListItem(
     private val doctorId: UUID,
     private val name: String,
     private val number: String
-) : ItemAdapter.Item<DoctorListItem.Event> {
+) : BindingItemAdapter.Item<DoctorListItem.Event> {
 
   companion object {
     fun from(medicalOfficers: List<MedicalOfficer>): List<DoctorListItem> {
@@ -42,17 +42,19 @@ data class DoctorListItem(
 
   override fun layoutResId(): Int = R.layout.list_contact_doctor
 
-  override fun render(holder: ViewHolderX, subject: Subject<Event>) {
-    holder.doctorNameTextView.text = name
-    holder.doctorNumberTextView.text = number
+  override fun render(holder: BindingViewHolder, subject: Subject<Event>) {
+    val binding = holder.binding as ListContactDoctorBinding
 
-    holder.whatsAppDoctorImageView.setOnClickListener {
+    binding.doctorNameTextView.text = name
+    binding.doctorNumberTextView.text = number
+
+    binding.whatsAppDoctorImageView.setOnClickListener {
       subject.onNext(WhatsAppClicked(
           doctorId = doctorId,
           phoneNumber = number
       ))
     }
-    holder.messageDoctorImageView.setOnClickListener {
+    binding.messageDoctorImageView.setOnClickListener {
       subject.onNext(SmsClicked(
           doctorId = doctorId,
           phoneNumber = number

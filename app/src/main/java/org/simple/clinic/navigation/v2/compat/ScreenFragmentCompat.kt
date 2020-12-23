a25c631d1ec635259882f3311ad541d41d590de5
Vinay <vinay@obvious.in>
2020-12-23 12:22:19 +0530

    Add support for back press overriding in `ScreenFragmentCompat`

--- app/src/main/java/org/simple/clinic/navigation/v2/compat/ScreenFragmentCompat.kt
+++ app/src/main/java/org/simple/clinic/navigation/v2/compat/ScreenFragmentCompat.kt
@@ -5,9 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.simple.clinic.navigation.v2.HandlesBack
 import org.simple.clinic.navigation.v2.ScreenKey
 
-class ScreenFragmentCompat : Fragment() {
+class ScreenFragmentCompat: Fragment(), HandlesBack {
 
   companion object {
     fun create() = ScreenFragmentCompat()
@@ -23,5 +24,11 @@ class ScreenFragmentCompat : Fragment() {
       savedInstanceState: Bundle?
   ): View {
     return inflater.inflate(key.layoutRes(), container, false)
+  }
+
+  override fun onBackPressed(): Boolean {
+    val screen = requireView()
+
+    return (screen as? HandlesBack)?.onBackPressed() ?: false
   }
 }

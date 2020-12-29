c18a5257ab02429091f892183f8c39123466fc42
Vinay <vinay@obvious.in>
2020-12-29 11:48:53 +0530

    Remove explicit back press handling in `AccessDeniedScreen`
    This is being done because the access denied screen is being used in
    two different activities (`AuthenticationActivity` and `TheActivity`).
    
    Since we are moving the activities to the new navigation framework in
    pieces, we will not be able to have two different methods of back
    press handling in the same screen since the `ScreenRouter` and the
    `Router` cannot be injected into the class simultaneously.
    
    Since we already clear the history whenever we push the access denied
    screen into the stack in both the activities, this additional back press
    handling is redundant and can be removed.

--- app/src/main/java/org/simple/clinic/deniedaccess/AccessDeniedScreen.kt
+++ app/src/main/java/org/simple/clinic/deniedaccess/AccessDeniedScreen.kt
@@ -6,8 +6,6 @@ import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import org.simple.clinic.databinding.ScreenAccessDeniedBinding
 import org.simple.clinic.di.injector
-import org.simple.clinic.router.screen.BackPressInterceptCallback
-import org.simple.clinic.router.screen.BackPressInterceptor
 import org.simple.clinic.router.screen.ScreenRouter
 import org.simple.clinic.util.unsafeLazy
 import javax.inject.Inject
@@ -40,17 +38,6 @@ class AccessDeniedScreen(context: Context, attrs: AttributeSet) : RelativeLayout
     context.injector<AccessDeniedScreenInjector>().inject(this)
 
     userFullNameText.text = screenKey.fullName
-    handleBackClicks()
-  }
-
-  private fun handleBackClicks() {
-    val interceptor = object : BackPressInterceptor {
-      override fun onInterceptBackPress(callback: BackPressInterceptCallback) {
-        activity.finish()
-        callback.markBackPressIntercepted()
-      }
-    }
-    screenRouter.registerBackPressInterceptor(interceptor)
   }
 
   override fun onDetachedFromWindow() {

45ef54d214402392e9bf2f32ab267c32ce6b4a03
Vinay <vinay@obvious.in>
2020-12-30 15:56:36 +0530

    Add `BaseDialog` as a base class for all dialogs supporting the new navigation framework

---
+++ app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseDialog.kt
@@ -0,0 +1,129 @@
+package org.simple.clinic.navigation.v2.fragments
+
+import android.app.Dialog
+import android.os.Bundle
+import android.os.Parcelable
+import android.view.KeyEvent.ACTION_UP
+import android.view.KeyEvent.KEYCODE_BACK
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import com.spotify.mobius.Connection
+import com.spotify.mobius.EventSource
+import com.spotify.mobius.Init
+import com.spotify.mobius.MobiusLoop
+import com.spotify.mobius.Next.noChange
+import com.spotify.mobius.Update
+import com.spotify.mobius.android.MobiusAndroid
+import com.spotify.mobius.extras.Connectables
+import com.spotify.mobius.functions.Consumer
+import com.spotify.mobius.rx2.RxMobius
+import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
+import org.simple.clinic.mobius.eventSources
+import org.simple.clinic.mobius.first
+import org.simple.clinic.navigation.v2.ScreenKey
+import org.simple.clinic.util.unsafeLazy
+
+abstract class BaseDialog<K : ScreenKey, M : Parcelable, E, F> : DialogFragment() {
+
+  companion object {
+    private const val KEY_MODEL = "org.simple.clinic.navigation.v2.fragments.BaseScreen.KEY_MODEL"
+  }
+
+  private val loop: MobiusLoop.Builder<M, E, F> by unsafeLazy {
+    RxMobius
+        .loop(createUpdate()::update, createEffectHandler())
+        .init(createInit())
+        .eventSources(additionalEventSources())
+  }
+
+  private val controller: MobiusLoop.Controller<M, E> by unsafeLazy {
+    MobiusAndroid.controller(loop, defaultModel())
+  }
+
+  protected val screenKey by unsafeLazy { ScreenKey.key<K>(this) }
+
+  abstract fun defaultModel(): M
+
+  abstract fun onModelUpdate(model: M)
+
+  abstract fun createDialog(savedInstanceState: Bundle?): Dialog
+
+  open fun events(): Observable<E> = Observable.never()
+
+  open fun createUpdate(): Update<M, E, F> = Update { _, _ -> noChange() }
+
+  open fun createInit(): Init<M, F> = Init { model -> first(model) }
+
+  open fun createEffectHandler(): ObservableTransformer<F, E> = ObservableTransformer { Observable.never() }
+
+  open fun additionalEventSources(): List<EventSource<E>> = emptyList()
+
+  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    val dialog = createDialog(savedInstanceState)
+
+    // This is needed because the router is not aware of the changes
+    // in the history when the bottom sheet dialog is dismissed in the
+    // normal fashion.
+    dialog.setOnKeyListener { _, keyCode, event ->
+      if (event.action == ACTION_UP && keyCode == KEYCODE_BACK) {
+        backPressed()
+        true
+      } else {
+        false
+      }
+    }
+
+    return dialog
+  }
+
+  private fun backPressed() {
+    requireActivity().onBackPressed()
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    controller.connect(Connectables.contramap({ it }, ::connectViews))
+
+    if (savedInstanceState != null) {
+      val savedModel = savedInstanceState.getParcelable<M>(KEY_MODEL)!!
+      controller.replaceModel(savedModel)
+    }
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    controller.disconnect()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    controller.start()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    controller.stop()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    outState.putParcelable(KEY_MODEL, controller.model)
+  }
+
+  private fun connectViews(output: Consumer<E>): Connection<M> {
+    val eventsDisposable = events().subscribe(output::accept)
+
+    return object : Connection<M> {
+
+      override fun dispose() {
+        eventsDisposable.dispose()
+      }
+
+      override fun accept(model: M) {
+        onModelUpdate(model)
+      }
+    }
+  }
+}

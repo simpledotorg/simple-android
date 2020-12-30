e4ef59d0b47f88e9676030cd4e61b466e70b621b
Janhavi <janhavi@obvious.in>
2020-12-30 17:52:36 +0530

    Remove `renderIdentifierText` from `LinkIdWithPatientView`

--- app/src/main/java/org/simple/clinic/summary/linkId/LinkIdWithPatientViewUi.kt
+++ app/src/main/java/org/simple/clinic/summary/linkId/LinkIdWithPatientViewUi.kt
@@ -3,6 +3,5 @@ package org.simple.clinic.summary.linkId
 import org.simple.clinic.patient.businessid.Identifier
 
 interface LinkIdWithPatientViewUi {
-  fun renderIdentifierText(identifier: Identifier)
   fun renderPatientName(patientName: String)
 }

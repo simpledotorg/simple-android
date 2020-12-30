93ce0e38fe9bb97b43f1b68d20b4db1c9ed9c355
Sasikanth Miriyampalli <sasikanth@obvious.in>
2020-12-30 12:45:39 +0530

    Handle `OpenLinkIdWithPatientScreen` effect

--- app/src/main/java/org/simple/clinic/instantsearch/InstantSearchUiActions.kt
+++ app/src/main/java/org/simple/clinic/instantsearch/InstantSearchUiActions.kt
@@ -2,9 +2,11 @@ package org.simple.clinic.instantsearch
 
 import org.simple.clinic.facility.Facility
 import org.simple.clinic.patient.PatientSearchResult
+import org.simple.clinic.patient.businessid.Identifier
 import java.util.UUID
 
 interface InstantSearchUiActions {
   fun showPatientsSearchResults(patients: List<PatientSearchResult>, facility: Facility)
   fun openPatientSummary(patientId: UUID)
+  fun openLinkIdWithPatientScreen(patientId: UUID, identifier: Identifier)
 }

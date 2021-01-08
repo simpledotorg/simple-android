fd65054ccb80847f98c0b610ac5362ed7d462e9e
Vinay <vinay@obvious.in>
2021-01-08 14:43:43 +0530

    Remove `otp` property from `LoginPinScreenKey`

--- app/src/main/java/org/simple/clinic/login/pin/LoginPinScreenKey.kt
+++ app/src/main/java/org/simple/clinic/login/pin/LoginPinScreenKey.kt
@@ -6,10 +6,8 @@ import kotlinx.android.parcel.Parcelize
 import org.simple.clinic.R
 import org.simple.clinic.router.screen.FullScreenKey
 
-// TODO: Remove added otp property
-// Temporarily added to stop the current login flow from breaking
 @Parcelize
-data class LoginPinScreenKey(val otp: String = "") : FullScreenKey, Parcelable {
+class LoginPinScreenKey : FullScreenKey, Parcelable {
 
   @IgnoredOnParcel
   override val analyticsName = "Login PIN Entry"

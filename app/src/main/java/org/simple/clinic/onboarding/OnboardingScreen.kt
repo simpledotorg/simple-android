47823d0c7f0d7ff1ff465039442270063dd93230
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-01-20 11:16:16 +0530

    Use `buildSpannedString` instead of `Truss` in `OnboardingScreen`

--- app/src/main/java/org/simple/clinic/onboarding/OnboardingScreen.kt
+++ app/src/main/java/org/simple/clinic/onboarding/OnboardingScreen.kt
@@ -1,10 +1,13 @@
 package org.simple.clinic.onboarding
 
 import android.content.Context
+import android.text.style.ForegroundColorSpan
 import android.text.style.TextAppearanceSpan
 import android.util.AttributeSet
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.text.buildSpannedString
+import androidx.core.text.inSpans
 import com.jakewharton.rxbinding3.view.clicks
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.cast
@@ -14,7 +17,7 @@ import org.simple.clinic.databinding.ScreenOnboardingBinding
 import org.simple.clinic.di.injector
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.registerorlogin.AuthenticationActivity
-import org.simple.clinic.util.Truss
+import org.simple.clinic.router.util.resolveColor
 import org.simple.clinic.util.disableAnimations
 import org.simple.clinic.util.finishWithoutAnimations
 import org.simple.clinic.util.unsafeLazy
@@ -100,39 +103,57 @@ class OnboardingScreen(context: Context, attributeSet: AttributeSet) : Constrain
   }
 
   private fun setIntroOneTextView() {
-    val introOneFormattedString = Truss()
-        .append(context.getString(R.string.screenonboarding_intro_1))
-        .pushSpan(TextAppearanceSpan(context, R.style.TextAppearance_Simple_Subtitle1_Medium))
-        .append(context.getString(R.string.screenonboarding_intro_1_hypertension))
-        .popSpan()
-        .build()
+    val onboardingEmphasisTextSpan = TextAppearanceSpan(context, R.style.TextAppearance_Simple_Subtitle1_Medium)
+    val onboardingEmphasisColorSpan = ForegroundColorSpan(context.resolveColor(attrRes = R.attr.colorOnBackground))
+
+    val introOneFormattedString = buildSpannedString {
+      append(context.getString(R.string.screenonboarding_intro_1))
+
+      inSpans(onboardingEmphasisTextSpan, onboardingEmphasisColorSpan) {
+        append(context.getString(R.string.screenonboarding_intro_1_hypertension))
+      }
+    }
 
     introOneTextView.text = introOneFormattedString
   }
 
   private fun setIntroTwoTextView() {
-    val introTwoFormattedString = Truss()
-        .append(context.getString(R.string.screenonboarding_intro_2))
-        .pushSpan(TextAppearanceSpan(context, R.style.TextAppearance_Simple_Subtitle1_Medium))
-        .append(context.getString(R.string.screenonboarding_intro_2_bp))
-        .popSpan()
-        .append(context.getString(R.string.screenonboarding_intro_2_and))
-        .pushSpan(TextAppearanceSpan(context, R.style.TextAppearance_Simple_Subtitle1_Medium))
-        .append(context.getString(R.string.screenonboarding_intro_2_medicines))
-        .popSpan()
-        .build()
+    val introTwoFormattedString = buildSpannedString {
+      append(context.getString(R.string.screenonboarding_intro_2))
+
+      inSpans(
+          TextAppearanceSpan(context, R.style.TextAppearance_Simple_Subtitle1_Medium),
+          ForegroundColorSpan(context.resolveColor(attrRes = R.attr.colorOnBackground))
+      ) {
+        append(context.getString(R.string.screenonboarding_intro_2_bp))
+      }
+
+      append(context.getString(R.string.screenonboarding_intro_2_and))
+
+      inSpans(
+          TextAppearanceSpan(context, R.style.TextAppearance_Simple_Subtitle1_Medium),
+          ForegroundColorSpan(context.resolveColor(attrRes = R.attr.colorOnBackground))
+      ) {
+        append(context.getString(R.string.screenonboarding_intro_2_medicines))
+      }
+    }
 
     introTwoTextView.text = introTwoFormattedString
   }
 
   private fun setIntroThreeTextView() {
-    val introThreeFormattedString = Truss()
-        .append(context.getString(R.string.screenonboarding_intro_3))
-        .pushSpan(TextAppearanceSpan(context, R.style.TextAppearance_Simple_Subtitle1_Medium))
-        .append(context.getString(R.string.screenonboarding_intro_3_reminder))
-        .popSpan()
-        .append(context.getString(R.string.screenonboarding_intro_3_visits))
-        .build()
+    val onboardingEmphasisTextSpan = TextAppearanceSpan(context, R.style.TextAppearance_Simple_Subtitle1_Medium)
+    val onboardingEmphasisColorSpan = ForegroundColorSpan(context.resolveColor(attrRes = R.attr.colorOnBackground))
+
+    val introThreeFormattedString = buildSpannedString {
+      append(context.getString(R.string.screenonboarding_intro_3))
+
+      inSpans(onboardingEmphasisTextSpan, onboardingEmphasisColorSpan) {
+        append(context.getString(R.string.screenonboarding_intro_3_reminder))
+      }
+
+      append(context.getString(R.string.screenonboarding_intro_3_visits))
+    }
 
     introThreeTextView.text = introThreeFormattedString
   }

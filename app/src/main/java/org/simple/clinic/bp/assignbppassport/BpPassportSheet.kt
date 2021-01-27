b7d49865a0342c50968cf1283825acbb1f2c455e
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-01-27 09:19:40 +0530

    Move setting of `bpPassportNumberTextview` text to `onViewCreated`

--- app/src/main/java/org/simple/clinic/bp/assignbppassport/BpPassportSheet.kt
+++ app/src/main/java/org/simple/clinic/bp/assignbppassport/BpPassportSheet.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jakewharton.rxbinding3.view.clicks
@@ -81,8 +82,8 @@ class BpPassportSheet :
 
   override fun createEffectHandler() = effectHandlerFactory.create(this).build()
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
     bpPassportNumberTextview.text = getString(R.string.sheet_bp_passport_number, bpPassportIdentifier.displayValue())
   }
 

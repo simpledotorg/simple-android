4d2799fa7bf68ddde8a7b5f6df3bb01505b7f1e5
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-01-28 09:33:08 +0530

    Use `MobiusLoopViewModel` in `BaseScreen`

--- app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseScreen.kt
+++ app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseScreen.kt
@@ -6,16 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
 import com.spotify.mobius.EventSource
 import com.spotify.mobius.Init
 import com.spotify.mobius.MobiusLoop
 import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
-import com.spotify.mobius.android.MobiusAndroid
+import com.spotify.mobius.android.MobiusLoopViewModel
+import com.spotify.mobius.functions.Consumer
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
+import io.reactivex.disposables.CompositeDisposable
 import org.simple.clinic.mobius.ViewRenderer
 import org.simple.clinic.mobius.eventSources
 import org.simple.clinic.mobius.first
@@ -31,13 +35,10 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F> 
   private val loop: MobiusLoop.Builder<M, E, F> by unsafeLazy {
     RxMobius
         .loop(createUpdate()::update, createEffectHandler())
-        .init(createInit())
         .eventSources(additionalEventSources())
   }
 
-  private val controller: MobiusLoop.Controller<M, E> by unsafeLazy {
-    MobiusAndroid.controller(loop, defaultModel())
-  }
+  private val disposable = CompositeDisposable()
 
   protected val screenKey by unsafeLazy { ScreenKey.key<K>(this) }
 
@@ -45,6 +46,8 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F> 
 
   protected val binding: B
     get() = _binding!!
+
+  private lateinit var viewModel: MobiusLoopViewModel<M, E, F, Nothing>
 
   abstract fun defaultModel(): M
 
@@ -62,6 +65,13 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F> 
 
   open fun additionalEventSources(): List<EventSource<E>> = emptyList()
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val modelToStartFrom = savedInstanceState?.getParcelable<M>(KEY_MODEL) ?: defaultModel()
+
+    viewModel = getViewModel(BaseScreenViewModelFactory(modelToStartFrom))
+  }
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     _binding = bindView(inflater, container)
 
@@ -71,33 +81,35 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F> 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    val rxBridge = RxMobiusBridge(events(), uiRenderer())
-    controller.connect(rxBridge)
+    viewModel.models.observe(viewLifecycleOwner, uiRenderer()::render)
 
-    if (savedInstanceState != null) {
-      val savedModel = savedInstanceState.getParcelable<M>(KEY_MODEL)!!
-      controller.replaceModel(savedModel)
-    }
+    disposable.add(
+        events().subscribe { viewModel.dispatchEvent(it!!) }
+    )
   }
 
   override fun onDestroyView() {
     super.onDestroyView()
-    controller.disconnect()
+    disposable.clear()
     _binding = null
-  }
-
-  override fun onResume() {
-    super.onResume()
-    controller.start()
-  }
-
-  override fun onPause() {
-    super.onPause()
-    controller.stop()
   }
 
   override fun onSaveInstanceState(outState: Bundle) {
     super.onSaveInstanceState(outState)
-    outState.putParcelable(KEY_MODEL, controller.model)
+    outState.putParcelable(KEY_MODEL, viewModel.model)
+  }
+
+  private inline fun <reified T : ViewModel> Fragment.getViewModel(factory: ViewModelProvider.Factory) =
+      ViewModelProvider(this, factory).get(T::class.java)
+
+  private inner class BaseScreenViewModelFactory(private val modelToStartFrom: M) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+      return MobiusLoopViewModel.create(
+          { _: Consumer<Nothing> -> loop },
+          modelToStartFrom,
+          createInit()
+      ) as T
+    }
   }
 }

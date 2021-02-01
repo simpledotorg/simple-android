b8fc4c0dac157538572c443afd28cddaadc31c8f
Vinay <vinay@obvious.in>
2021-02-01 13:06:02 +0530

    Remove Mobius delegate and related methods from `ConfirmFacilityChangeSheet`

--- app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeSheet.kt
+++ app/src/main/java/org/simple/clinic/facility/change/confirm/ConfirmFacilityChangeSheet.kt
@@ -3,33 +3,25 @@ package org.simple.clinic.facility.change.confirm
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jakewharton.rxbinding3.view.clicks
-import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.cast
-import io.reactivex.rxkotlin.ofType
 import kotlinx.android.parcel.Parcelize
-import org.simple.clinic.ClinicApp
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.databinding.SheetConfirmFacilityChangeBinding
-import org.simple.clinic.di.InjectorProviderContextWrapper
 import org.simple.clinic.di.injector
 import org.simple.clinic.facility.Facility
 import org.simple.clinic.facility.change.confirm.di.ConfirmFacilityChangeComponent
 import org.simple.clinic.feature.Features
-import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.navigation.v2.ScreenKey
 import org.simple.clinic.navigation.v2.fragments.BaseBottomSheet
 import org.simple.clinic.util.unsafeLazy
-import org.simple.clinic.util.withLocale
-import org.simple.clinic.util.wrap
 import org.simple.clinic.widgets.UiEvent
 import java.util.Locale
 import javax.inject.Inject
@@ -87,20 +79,6 @@ class ConfirmFacilityChangeSheet :
 
   private val selectedFacility: Facility by unsafeLazy { screenKey.facility }
 
-  private val events: Observable<UiEvent> by unsafeLazy {
-    positiveButtonClicks()
-  }
-
-  private val delegate by unsafeLazy {
-    MobiusDelegate.forActivity(
-        events.ofType(),
-        ConfirmFacilityChangeModel.create(),
-        ConfirmFacilityChangeUpdate(),
-        effectHandlerFactory.create(this).build(),
-        ConfirmFacilityChangeInit()
-    )
-  }
-
   private val facilityName
     get() = binding.facilityName
 
@@ -123,62 +101,10 @@ class ConfirmFacilityChangeSheet :
     }
   }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-
-    binding = SheetConfirmFacilityChangeBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-
-    facilityName.text = getString(R.string.confirmfacilitychange_facility_name, selectedFacility.name)
-    cancelButton.setOnClickListener {
-      closeSheetAfterCancel()
-    }
-
-    delegate.onRestoreInstanceState(savedInstanceState)
-  }
-
   private fun closeSheetAfterCancel() {
     val intent = Intent()
     setResult(Activity.RESULT_CANCELED, intent)
     finish()
-  }
-
-  override fun attachBaseContext(baseContext: Context) {
-    setupDi()
-
-    val wrappedContext = baseContext
-        .wrap { InjectorProviderContextWrapper.wrap(it, component) }
-        .wrap { ViewPumpContextWrapper.wrap(it) }
-
-    super.attachBaseContext(wrappedContext)
-    applyOverrideConfiguration(Configuration())
-  }
-
-  override fun applyOverrideConfiguration(overrideConfiguration: Configuration) {
-    super.applyOverrideConfiguration(overrideConfiguration.withLocale(locale, features))
-  }
-
-  override fun onStart() {
-    super.onStart()
-    delegate.start()
-  }
-
-  override fun onStop() {
-    super.onStop()
-    delegate.stop()
-  }
-
-  override fun onSaveInstanceState(outState: Bundle) {
-    delegate.onSaveInstanceState(outState)
-    super.onSaveInstanceState(outState)
-  }
-
-  private fun setupDi() {
-    component = ClinicApp.appComponent
-        .confirmFacilityChangeComponent()
-        .create(activity = this)
-
-    component.inject(this)
   }
 
   private fun positiveButtonClicks(): Observable<UiEvent> =

82600af1a1a7c7141fa695b59a4a0b3adeb087f6
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-03-17 09:06:32 +0530

    Handle back clicks in reports webview

--- app/src/main/java/org/simple/clinic/home/report/ReportsWebViewClient.kt
+++ app/src/main/java/org/simple/clinic/home/report/ReportsWebViewClient.kt
@@ -4,11 +4,16 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
-class ReportsWebViewClient : WebViewClient() {
+class ReportsWebViewClient(
+    private val backClicked: () -> Unit
+) : WebViewClient() {
 
   override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
     val url = request?.url?.toString() ?: return super.shouldOverrideUrlLoading(view, request)
 
+    if (url == "simple://progress-tab") {
+      backClicked.invoke()
+    }
     view?.loadUrl(url)
     return false
   }

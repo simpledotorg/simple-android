f30b083ef3958468547cec36fb4f3064b5284dde
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-03-17 07:28:06 +0530

    Use different `Chronology` for `DateTimeFormatter`s depending on the `Country`

--- app/src/main/java/org/simple/clinic/di/DateFormatterModule.kt
+++ app/src/main/java/org/simple/clinic/di/DateFormatterModule.kt
@@ -2,7 +2,11 @@ package org.simple.clinic.di
 
 import dagger.Module
 import dagger.Provides
+import org.simple.clinic.appconfig.Country
 import org.simple.clinic.di.DateFormatter.Type.FileDateTime
+import org.threeten.extra.chrono.EthiopicChronology
+import java.time.chrono.Chronology
+import java.time.chrono.IsoChronology
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 import javax.inject.Named
@@ -11,26 +15,52 @@ import javax.inject.Named
 class DateFormatterModule {
 
   @Provides
+  fun providesChronology(country: Country): Chronology {
+    return when (country.isoCountryCode) {
+      Country.ETHIOPIA -> EthiopicChronology.INSTANCE
+      else -> IsoChronology.INSTANCE
+    }
+  }
+
+  @Provides
   @Named("full_date")
-  fun provideDateFormatterForFullDate(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("d-MMM-yyyy", locale)
+  fun provideDateFormatterForFullDate(locale: Locale, chronology: Chronology): DateTimeFormatter {
+    return DateTimeFormatter.ofPattern("d-MMM-yyyy", locale)
+        .withChronology(chronology)
+  }
 
   @Provides
   @Named("date_for_user_input")
-  fun provideDateFormatterForUserInput(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", locale)
+  fun provideDateFormatterForUserInput(locale: Locale, chronology: Chronology): DateTimeFormatter {
+    return DateTimeFormatter.ofPattern("dd/MM/yyyy", locale)
+        .withChronology(chronology)
+  }
 
   @Provides
   @Named("time_for_bps_recorded")
-  fun providesTimeFormatterForBPRecorded(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("h:mm a", locale)
+  fun providesTimeFormatterForBPRecorded(locale: Locale, chronology: Chronology): DateTimeFormatter {
+    return DateTimeFormatter.ofPattern("h:mm a", locale)
+        .withChronology(chronology)
+  }
 
   @Provides
   @Named("exact_date")
-  fun providesFormatterForFullReadableDate(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM, yyyy", locale)
+  fun providesFormatterForFullReadableDate(locale: Locale, chronology: Chronology): DateTimeFormatter {
+    return DateTimeFormatter.ofPattern("d MMMM, yyyy", locale)
+        .withChronology(chronology)
+  }
 
   @Provides
   @Named("time_for_measurement_history")
-  fun providesTimeFormatterForMeasurementHistory(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("h:mm a", locale)
+  fun providesTimeFormatterForMeasurementHistory(locale: Locale, chronology: Chronology): DateTimeFormatter {
+    return DateTimeFormatter.ofPattern("h:mm a", locale)
+        .withChronology(chronology)
+  }
 
   @Provides
   @DateFormatter(FileDateTime)
-  fun provideFormatterForFileDateTime(locale: Locale): DateTimeFormatter = DateTimeFormatter.ofPattern("d MMM yyyy h.mm.ss a", locale)
+  fun provideFormatterForFileDateTime(locale: Locale, chronology: Chronology): DateTimeFormatter {
+    return DateTimeFormatter.ofPattern("d MMM yyyy h.mm.ss a", locale)
+        .withChronology(chronology)
+  }
 }

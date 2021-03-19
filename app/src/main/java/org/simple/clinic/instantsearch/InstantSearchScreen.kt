934971caa1ce6ad5c00573ae52bd2bbddc057e88
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-03-19 07:13:05 +0530

    Move opening of instant search keyboard to Mobius loop

--- app/src/main/java/org/simple/clinic/instantsearch/InstantSearchScreen.kt
+++ app/src/main/java/org/simple/clinic/instantsearch/InstantSearchScreen.kt
@@ -162,14 +162,6 @@ class InstantSearchScreen :
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    // When a scanned BP Passport does not result in a match, we bring up a bottom sheet which asks
-    // whether this is a new registration or an existing patient. If we show the keyboard in these
-    // cases, the UI is janky since the keyboard pops up and immediately another bottom sheet pops up.
-    // This improves the experience by showing the keyboard only if we have arrived here by searching
-    // for a patient by the name
-    if (screenKey.additionalIdentifier == null) {
-      showKeyboard()
-    }
     instantSearchToolbar.setNavigationOnClickListener {
       router.pop()
     }

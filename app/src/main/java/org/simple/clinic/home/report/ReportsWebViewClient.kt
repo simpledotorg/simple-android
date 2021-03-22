34903cf395491aa68ce0cb362e18a94a2e0f978b
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-03-22 15:32:15 +0530

    Show custom error screen for drug stock web view when there is no internet connection (#2495)
    * Add `drug_stock_no_connection.html`
    
    * Load custom error view when opening drug stock page fails because of network issues
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/home/report/ReportsWebViewClient.kt
+++ app/src/main/java/org/simple/clinic/home/report/ReportsWebViewClient.kt
@@ -1,8 +1,12 @@
 package org.simple.clinic.home.report
 
+import android.os.Build
+import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import timber.log.Timber
 
 class ReportsWebViewClient(
     private val backClicked: () -> Unit
@@ -16,5 +20,23 @@ class ReportsWebViewClient(
     }
     view?.loadUrl(url)
     return false
+  }
+
+  @RequiresApi(Build.VERSION_CODES.M)
+  override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+    super.onReceivedError(view, request, error)
+    handleError(webView = view, errorCode = error?.errorCode)
+  }
+
+  @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+  override fun onReceivedError(view: WebView?, errorCode: Int, description: String?, failingUrl: String?) {
+    super.onReceivedError(view, errorCode, description, failingUrl)
+    handleError(webView = view, errorCode = errorCode)
+  }
+
+  private fun handleError(webView: WebView?, errorCode: Int?) {
+    if (errorCode == ERROR_HOST_LOOKUP || errorCode == ERROR_CONNECT) {
+      webView?.loadUrl("file:///android_asset/drug_stock_no_connection.html")
+    }
   }
 }

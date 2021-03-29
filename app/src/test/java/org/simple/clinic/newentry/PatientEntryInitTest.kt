085e7c16b2a63bbc502383d24661ef30cdc2b484
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-03-29 16:28:27 +0530

    Add test for village type ahead feature flag in `PatientEntryScreen`

--- app/src/test/java/org/simple/clinic/newentry/PatientEntryInitTest.kt
+++ app/src/test/java/org/simple/clinic/newentry/PatientEntryInitTest.kt
@@ -5,10 +5,16 @@ import com.spotify.mobius.test.FirstMatchers.hasModel
 import com.spotify.mobius.test.InitSpec
 import com.spotify.mobius.test.InitSpec.assertThatFirst
 import org.junit.Test
+import org.simple.clinic.editpatient.EditPatientInit
+import org.simple.clinic.editpatient.EditPatientModel
+import org.simple.clinic.editpatient.EditPatientState
+import org.simple.clinic.editpatient.FetchBpPassportsEffect
+import org.simple.clinic.editpatient.PrefillFormEffect
 import org.simple.clinic.feature.Feature
 import org.simple.clinic.feature.Features
 import org.simple.clinic.remoteconfig.DefaultValueConfigReader
 import org.simple.clinic.remoteconfig.NoOpRemoteConfigService
+import java.util.UUID
 
 class PatientEntryInitTest {
   private val initSpec = InitSpec(PatientEntryInit(isVillageTypeAheadEnabled = true))
@@ -20,6 +26,18 @@ class PatientEntryInitTest {
         hasModel(defaultModel),
         hasEffects(
             FetchPatientEntry, LoadInputFields, FetchColonyOrVillagesEffect
+        )
+    ))
+  }
+
+  @Test
+  fun `when screen is created and village type ahead is not enabled, then do not fetch colony or villages`() {
+    val initSpec = InitSpec(PatientEntryInit(isVillageTypeAheadEnabled = false))
+
+    initSpec.whenInit(defaultModel).then(assertThatFirst(
+        hasModel(defaultModel),
+        hasEffects(
+            FetchPatientEntry, LoadInputFields
         )
     ))
   }

15c92349975a36ab21dfca9e32668549e9b09dda
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-04-01 09:40:37 +0530

    Move `changeFacilityButton` click listener to `onViewCreated`

--- app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentSheet.kt
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentSheet.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
@@ -155,8 +156,8 @@ class ScheduleAppointmentSheet : BaseBottomSheet<
     context.injector<Injector>().inject(this)
   }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
     changeFacilityButton.setOnClickListener {
       openFacilitySelection()
     }

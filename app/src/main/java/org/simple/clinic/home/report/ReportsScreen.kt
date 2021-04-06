24d8b838490c4d38af89d0a759c35bc79379c61c
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-04-06 17:23:48 +0530

    Migrate `ReportsScreen` to a fragment (#2535)
    * Change `ReportsScreen` to extend `BaseScreen`
    
    * Override Mobius methods from `BaseScreen`
    
    * Use ViewBinding in `ReportsScreen`
    
    * Inject DI dependencies in `onAttach`
    
    * Move UI setup calls to `onViewCreated`
    
    * Remove view lifecycle methods
    
    * Remove `MobiusDelegate` from the screen
    
    * Use `ReportsScreen.Key` when creating `HomeTab`
    
    * Remove `ReportsScreenKey`
    
    * Update CHANGELOG
    
    * Report analytics events in `ReportsScreen`

--- app/src/main/java/org/simple/clinic/home/report/ReportsScreen.kt
+++ app/src/main/java/org/simple/clinic/home/report/ReportsScreen.kt
@@ -2,77 +2,71 @@ package org.simple.clinic.home.report
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Parcelable
-import android.util.AttributeSet
-import android.widget.FrameLayout
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.cast
 import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.screen_report.view.*
+import kotlinx.android.parcel.Parcelize
+import org.simple.clinic.ReportAnalyticsEvents
+import org.simple.clinic.databinding.ScreenReportBinding
 import org.simple.clinic.di.injector
-import org.simple.clinic.mobius.MobiusDelegate
-import org.simple.clinic.util.unsafeLazy
+import org.simple.clinic.navigation.v2.ScreenKey
+import org.simple.clinic.navigation.v2.fragments.BaseScreen
+import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.visibleOrGone
 import javax.inject.Inject
 
-class ReportsScreen(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs), ReportsUi {
+class ReportsScreen : BaseScreen<
+    ReportsScreen.Key,
+    ScreenReportBinding,
+    ReportsModel,
+    ReportsEvent,
+    ReportsEffect>(), ReportsUi {
 
   @Inject
   lateinit var effectHandler: ReportsEffectHandler
 
-  private val webViewBackClicks = PublishSubject.create<ReportsEvent>()
+  private val webViewBackClicks = PublishSubject.create<UiEvent>()
 
-  private val events: Observable<ReportsEvent> by unsafeLazy {
-    Observable
-        .mergeArray(webViewBackClicks)
-        .cast()
-  }
+  private val webView
+    get() = binding.webView
 
-  private val delegate by unsafeLazy {
-    val uiRenderer = ReportsUiRenderer(this)
+  private val noReportView
+    get() = binding.noReportView
 
-    MobiusDelegate.forView(
-        events = events,
-        defaultModel = ReportsModel.create(),
-        init = ReportsInit(),
-        update = ReportsUpdate(),
-        effectHandler = effectHandler.build(),
-        modelUpdateListener = uiRenderer::render
-    )
+  override fun defaultModel() = ReportsModel.create()
+
+  override fun bindView(layoutInflater: LayoutInflater, container: ViewGroup?) =
+      ScreenReportBinding.inflate(layoutInflater, container, false)
+
+  override fun uiRenderer() = ReportsUiRenderer(this)
+
+  override fun events() = Observable
+      .mergeArray(webViewBackClicks)
+      .compose(ReportAnalyticsEvents())
+      .cast<ReportsEvent>()
+
+  override fun createUpdate() = ReportsUpdate()
+
+  override fun createInit() = ReportsInit()
+
+  override fun createEffectHandler() = effectHandler.build()
+
+  override fun onAttach(context: Context) {
+    super.onAttach(context)
+    context.injector<Injector>().inject(this)
   }
 
   @SuppressLint("SetJavaScriptEnabled")
-  override fun onFinishInflate() {
-    super.onFinishInflate()
-
-    if (isInEditMode) {
-      return
-    }
-
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
     webView.settings.javaScriptEnabled = true
     webView.webViewClient = ReportsWebViewClient(
         backClicked = { webViewBackClicks.onNext(WebBackClicked) }
     )
-
-    context.injector<Injector>().inject(this)
-  }
-
-  override fun onAttachedToWindow() {
-    super.onAttachedToWindow()
-    delegate.start()
-  }
-
-  override fun onDetachedFromWindow() {
-    delegate.stop()
-    super.onDetachedFromWindow()
-  }
-
-  override fun onSaveInstanceState(): Parcelable {
-    return delegate.onSaveInstanceState(super.onSaveInstanceState())
-  }
-
-  override fun onRestoreInstanceState(state: Parcelable?) {
-    super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
 
   override fun showReport(html: String) {
@@ -92,5 +86,13 @@ class ReportsScreen(context: Context, attrs: AttributeSet) : FrameLayout(context
 
   interface Injector {
     fun inject(target: ReportsScreen)
+  }
+
+  @Parcelize
+  class Key : ScreenKey() {
+
+    override val analyticsName = "Reports"
+
+    override fun instantiateFragment() = ReportsScreen()
   }
 }

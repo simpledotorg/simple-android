f408573f27afc54d57d6b537e8f97156e375c6f6
Vinay <vinay@obvious.in>
2021-04-08 12:36:07 +0530

    Add alternate constructor to `DbMethodLookup` to use for dependency injection

--- app/src/main/java/org/simple/clinic/storage/monitoring/DbMethodLookup.kt
+++ app/src/main/java/org/simple/clinic/storage/monitoring/DbMethodLookup.kt
@@ -1,11 +1,26 @@
 package org.simple.clinic.storage.monitoring
 
+import android.app.Application
 import androidx.annotation.VisibleForTesting
 import androidx.annotation.VisibleForTesting.PRIVATE
+import javax.inject.Inject
 
 class DbMethodLookup @VisibleForTesting(otherwise = PRIVATE) constructor(
     metadataCsv: String
 ) {
+
+  /*
+  * This constructor does load the DB metadata on the main thread when initialised. However, in
+  * practice, the impact is relatively light (~5ms on a low end device, Samsung Galaxy M01 Core).
+  *
+  * Given that the size of this file will not increase by a lot, and the fact that is only done once
+  * per launch, I decided to leave it as is because the effort involved in making it asynchronous
+  * for something that does not happen more than once per cold start seemed too much.
+  **/
+  @Inject
+  constructor(application: Application): this(
+      metadataCsv = application.assets.open("db_metadata.csv").reader().readText()
+  )
 
   private val allDaoMethods: List<DaoMetadata> = DaoMetadata.parse(metadataCsv)
 

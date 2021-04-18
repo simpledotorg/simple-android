0ec80994f82419c097b49c80204064f29dba7597
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-04-18 12:18:52 +0530

    Migrate router `build.gradle` to `build.gradle.kts`

---
+++ router/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+  id("com.android.library")
+  kotlin("android")
+  kotlin("kapt")
+}
+
+android {
+  compileSdkVersion(versions.compileSdk)
+
+  defaultConfig {
+    minSdkVersion(versions.minSdk)
+    targetSdkVersion(versions.compileSdk)
+    versionCode = 1
+    versionName = "1.0"
+  }
+
+  buildTypes {
+    getByName("release") {
+      minifyEnabled(false)
+    }
+  }
+
+  compileOptions {
+    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+  }
+}
+
+dependencies {
+  implementation("androidx.appcompat:appcompat:${versions.supportLib}")
+  implementation("com.squareup.flow:flow:${versions.flow}")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${versions.kotlin}")
+  implementation("io.reactivex.rxjava2:rxjava:${versions.rxJava}")
+  implementation("com.jakewharton.timber:timber:${versions.timber}")
+
+  testImplementation("junit:junit:${versions.junit}")
+  testImplementation("pl.pragmatists:JUnitParams:${versions.junitParams}")
+  testImplementation("com.google.truth:truth:${versions.truth}")
+}

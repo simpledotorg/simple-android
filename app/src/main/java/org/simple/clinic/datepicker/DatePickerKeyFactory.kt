3de0d19d9611d48c84b61ba928c32b7aa6dd13f9
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-04-19 14:23:56 +0530

    Move `TextInputDatePickerSheet` behind a feature flag (#2550)
    * Move `TextInputDatePickerSheet` behind a feature flag
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/datepicker/DatePickerKeyFactory.kt
+++ app/src/main/java/org/simple/clinic/datepicker/DatePickerKeyFactory.kt
@@ -4,21 +4,30 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import org.simple.clinic.appconfig.Country
 import org.simple.clinic.datepicker.calendar.CalendarDatePicker
+import org.simple.clinic.feature.Feature
+import org.simple.clinic.feature.Features
 import org.simple.clinic.navigation.v2.ScreenKey
 import org.simple.clinic.textInputdatepicker.TextInputDatePickerSheet
 import java.time.LocalDate
 import javax.inject.Inject
 
 class DatePickerKeyFactory @Inject constructor(
-    private val country: Country
+    private val country: Country,
+    private val features: Features
 ) {
 
   fun key(
       preselectedDate: LocalDate,
       allowedDateRange: ClosedRange<LocalDate>
   ): ScreenKey {
+    if (features.isDisabled(Feature.EthiopianCalendar)) {
+      return showCalendarDatePicker(
+          preselectedDate = preselectedDate,
+          allowedDateRange = allowedDateRange
+      )
+    }
+
     return when (country.isoCountryCode) {
-      Country.BANGLADESH, Country.INDIA -> showCalendarDatePicker(preselectedDate, allowedDateRange)
       Country.ETHIOPIA -> showTextInputDatePicker(preselectedDate, allowedDateRange)
       else -> showCalendarDatePicker(preselectedDate, allowedDateRange)
     }

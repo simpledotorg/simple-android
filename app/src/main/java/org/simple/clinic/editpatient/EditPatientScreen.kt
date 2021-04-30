33be641e4043a093c1524483de26d22e82715943
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-04-30 08:57:22 +0530

    Render alternate id view when alternate id is `IndiaNationalHealthId`

--- app/src/main/java/org/simple/clinic/editpatient/EditPatientScreen.kt
+++ app/src/main/java/org/simple/clinic/editpatient/EditPatientScreen.kt
@@ -23,6 +23,7 @@ import io.reactivex.rxkotlin.cast
 import io.reactivex.subjects.PublishSubject
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
+import org.simple.clinic.databinding.PatientEditAlternateIdViewBinding
 import org.simple.clinic.databinding.PatientEditBpPassportViewBinding
 import org.simple.clinic.databinding.ScreenEditPatientBinding
 import org.simple.clinic.di.injector
@@ -210,6 +211,12 @@ class EditPatientScreen(context: Context, attributeSet: AttributeSet) : Relative
 
   private val bpPassportsLabel
     get() = binding!!.bpPassportsLabel
+
+  private val alternateIdLabel
+    get() = binding!!.alternateIdLabel
+
+  private val alternateIdContainer
+    get() = binding!!.alternateIdContainer
 
   private val formScrollView
     get() = binding!!.formScrollView
@@ -705,7 +712,32 @@ class EditPatientScreen(context: Context, attributeSet: AttributeSet) : Relative
   }
 
   override fun setAlternateId(alternateId: Identifier) {
+    when (alternateId.type) {
+      Identifier.IdentifierType.BangladeshNationalId -> setAlternateIdTextField(alternateId)
+      Identifier.IdentifierType.EthiopiaMedicalRecordNumber -> setAlternateIdTextField(alternateId)
+      Identifier.IdentifierType.IndiaNationalHealthId -> setAlternateIdContainer(alternateId)
+      else -> throw IllegalArgumentException("Unknown alternate id: $alternateId")
+    }
+  }
+
+  private fun setAlternateIdTextField(alternateId: Identifier) {
     alternativeIdInputEditText.setTextAndCursor(alternateId.displayValue())
+  }
+
+  private fun setAlternateIdContainer(alternateId: Identifier) {
+    alternateIdLabel.visibility = View.VISIBLE
+    alternateIdLabel.text = alternateId.displayType(resources)
+
+    alternateIdContainer.visibility = View.VISIBLE
+
+    inflateAlternateIdView(alternateId.displayValue())
+  }
+
+  private fun inflateAlternateIdView(identifier: String) {
+    val layoutInflater = LayoutInflater.from(context)
+    val alternateIdView = PatientEditAlternateIdViewBinding.inflate(layoutInflater, this, false)
+    alternateIdView.alternateIdentifier.text = identifier
+    alternateIdContainer.addView(alternateIdView.root)
   }
 
   override fun onBackPressed(): Boolean {

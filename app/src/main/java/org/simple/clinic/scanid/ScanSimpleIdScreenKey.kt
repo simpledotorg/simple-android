84e091c9f7cc3f78344744c66fcf56541bd92373
Honey Sonwani <honey@obvious.in>
2021-05-04 03:44:50 +0530

    Add `IgnoredOnParcel` on the analytics name

--- app/src/main/java/org/simple/clinic/scanid/ScanSimpleIdScreenKey.kt
+++ app/src/main/java/org/simple/clinic/scanid/ScanSimpleIdScreenKey.kt
@@ -1,11 +1,13 @@
 package org.simple.clinic.scanid
 
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 import org.simple.clinic.navigation.v2.ScreenKey
 
 @Parcelize
 class ScanSimpleIdScreenKey : ScreenKey() {
 
+  @IgnoredOnParcel
   override val analyticsName: String = "ScanSimpleId"
 
   override fun instantiateFragment() = ScanSimpleIdScreen()

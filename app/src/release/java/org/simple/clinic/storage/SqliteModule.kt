d43224a35d3a272192fc24bd13a200500d0edb28
Vinay <vinay@obvious.in>
2021-05-05 16:32:12 +0530

    Use the `AnalyticsReportingSqlDelegate` to report performance events

--- app/src/release/java/org/simple/clinic/storage/SqliteModule.kt
+++ app/src/release/java/org/simple/clinic/storage/SqliteModule.kt
@@ -4,15 +4,15 @@ import androidx.sqlite.db.SupportSQLiteOpenHelper
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.di.AppSqliteOpenHelperFactory
+import org.simple.clinic.storage.monitoring.AnalyticsReportingSqlDelegate
 import org.simple.clinic.storage.monitoring.DelegateableSQLiteOpenHelper
-import org.simple.clinic.storage.monitoring.FirebasePerfReportingSqlDelegate
 
 @Module
 class SqliteModule {
 
   @Provides
   fun sqliteOpenHelperFactory(
-      sqlMonitoringDelegate: FirebasePerfReportingSqlDelegate
+      sqlMonitoringDelegate: AnalyticsReportingSqlDelegate
   ): SupportSQLiteOpenHelper.Factory {
     val factory = AppSqliteOpenHelperFactory()
 

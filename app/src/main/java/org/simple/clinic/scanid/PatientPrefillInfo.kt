4ffac5bc09f0810077daec1e3e1bd673cba21717
Janhavi Singh <janhavi@obvious.in>
2021-05-11 17:36:37 +0530

    Make `PatientPrefillInfo` Parcelable

--- app/src/main/java/org/simple/clinic/scanid/PatientPrefillInfo.kt
+++ app/src/main/java/org/simple/clinic/scanid/PatientPrefillInfo.kt
@@ -1,8 +1,12 @@
 package org.simple.clinic.scanid
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class PatientPrefillInfo(
     val fullName: String,
     val gender: String,
     val dateOfBirth: String,
     val address: String
-)
+) : Parcelable

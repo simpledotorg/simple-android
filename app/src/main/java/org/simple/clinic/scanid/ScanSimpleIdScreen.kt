c85946da7697a71904da35719aed70d95baa560a
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-05-19 15:29:43 +0530

    Use `LifecycleCameraController` for setting preview and analyzer

--- app/src/main/java/org/simple/clinic/scanid/ScanSimpleIdScreen.kt
+++ app/src/main/java/org/simple/clinic/scanid/ScanSimpleIdScreen.kt
@@ -1,24 +1,17 @@
 package org.simple.clinic.scanid
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Rect
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.view.inputmethod.EditorInfo
-import androidx.camera.core.AspectRatio
-import androidx.camera.core.CameraControl
 import androidx.camera.core.CameraSelector
-import androidx.camera.core.FocusMeteringAction
 import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.content.ContextCompat
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.jakewharton.rxbinding3.widget.editorActionEvents
@@ -53,9 +46,6 @@ import java.time.Instant
 import java.util.UUID
 import java.util.concurrent.Executors
 import javax.inject.Inject
-import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
 
 class ScanSimpleIdScreen : BaseScreen<
     ScanSimpleIdScreenKey,
@@ -63,11 +53,6 @@ class ScanSimpleIdScreen : BaseScreen<
     ScanSimpleIdModel,
     ScanSimpleIdEvent,
     ScanSimpleIdEffect>(), ScanSimpleIdUi, ScanSimpleIdUiActions {
-
-  companion object {
-    private const val RATIO_4_3_VALUE = 4.0 / 3.0
-    private const val RATIO_16_9_VALUE = 16.0 / 9.0
-  }
 
   @Inject
   lateinit var utcClock: UtcClock
@@ -111,11 +96,14 @@ class ScanSimpleIdScreen : BaseScreen<
   private val scanErrorTextView
     get() = binding.scanErrorTextView
 
+  private val cameraController by unsafeLazy {
+    LifecycleCameraController(requireContext()).apply {
+      isTapToFocusEnabled = true
+    }
+  }
+
   private val keyboardVisibilityDetector = KeyboardVisibilityDetector()
   private val cameraExecutor = Executors.newSingleThreadExecutor()
-  private val cameraProviderFuture by unsafeLazy {
-    ProcessCameraProvider.getInstance(requireContext())
-  }
 
   private val qrScans = PublishSubject.create<ScanSimpleIdEvent>()
 
@@ -148,30 +136,21 @@ class ScanSimpleIdScreen : BaseScreen<
     binding.root.hideKeyboard()
     toolBar.setNavigationOnClickListener { router.pop() }
 
-    cameraProviderFuture.addListener({
-      val cameraProvider = cameraProviderFuture.get()
-      startCamera(cameraProvider)
-    }, ContextCompat.getMainExecutor(requireContext()))
+    cameraController.bindToLifecycle(this)
+    previewView.scaleType = PreviewView.ScaleType.FILL_CENTER
+    previewView.controller = cameraController
+
+    configCameraController()
   }
 
-  @SuppressLint("ClickableViewAccessibility")
-  private fun startCamera(cameraProvider: ProcessCameraProvider) {
-    // Get screen metrics used to setup camera for full screen resolution
-    val metrics = DisplayMetrics().also { previewView.display.getRealMetrics(it) }
+  override fun onDestroyView() {
+    super.onDestroyView()
+    cameraController.unbind()
+  }
 
-    val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
-
-    val rotation = previewView.display.rotation
-
-    val cameraSelector = CameraSelector.Builder()
+  private fun configCameraController() {
+    cameraController.cameraSelector = CameraSelector.Builder()
         .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-        .build()
-
-    val preview = Preview.Builder().build()
-
-    val analyzer = ImageAnalysis.Builder()
-        .setTargetAspectRatio(screenAspectRatio)
-        .setTargetRotation(rotation)
         .build()
 
     val googlePlayServicesAvailability = googleApiAvailability.isGooglePlayServicesAvailable(requireContext())
@@ -180,63 +159,18 @@ class ScanSimpleIdScreen : BaseScreen<
     val isMLKitQrCodeScannerEnabled = features.isEnabled(Feature.MLKitQrCodeScanner)
     val qrCodeAnalyzer = if (isMLKitQrCodeScannerEnabled && isGooglePlayServicesAvailable) {
       MLKitQrCodeAnalyzer(bitmapUtils, ::qrCodeScanned, mlKitUnavailable = {
-        setQrCodeAnalyzer(analyzer, ZxingQrCodeAnalyzer(::qrCodeScanned))
+        setQrCodeAnalyzer(ZxingQrCodeAnalyzer(::qrCodeScanned))
       })
     } else {
       ZxingQrCodeAnalyzer(::qrCodeScanned)
     }
 
-    setQrCodeAnalyzer(analyzer, qrCodeAnalyzer)
-
-    cameraProvider.unbindAll()
-
-    val camera = cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview, analyzer)
-    preview.setSurfaceProvider(previewView.surfaceProvider)
-
-    previewView.setOnTouchListener { _, motionEvent ->
-      when (motionEvent.action) {
-        MotionEvent.ACTION_DOWN -> true
-        MotionEvent.ACTION_UP -> {
-          processTapToFocus(motionEvent, camera.cameraControl)
-          true
-        }
-        else -> false
-      }
-    }
+    setQrCodeAnalyzer(qrCodeAnalyzer)
   }
 
-  private fun setQrCodeAnalyzer(analyzer: ImageAnalysis, qrCodeAnalyzer: ImageAnalysis.Analyzer) {
-    analyzer.clearAnalyzer()
-    analyzer.setAnalyzer(cameraExecutor, qrCodeAnalyzer)
-  }
-
-  private fun processTapToFocus(motionEvent: MotionEvent, cameraControl: CameraControl) {
-    val meteringPointFactory = previewView.meteringPointFactory
-    val point = meteringPointFactory.createPoint(motionEvent.x, motionEvent.y)
-    val action = FocusMeteringAction.Builder(point).build()
-
-    cameraControl.startFocusAndMetering(action)
-  }
-
-  /**
-   *  [androidx.camera.core.ImageAnalysis.Builder.setTargetAspectRatio] requires enum value of
-   *  [androidx.camera.core.AspectRatio]. Currently it has values of 4:3 & 16:9.
-   *
-   *  Detecting the most suitable ratio for dimensions provided in @params by counting absolute
-   *  of preview ratio to one of the provided values.
-   *
-   *  @param width - preview width
-   *  @param height - preview height
-   *  @return suitable aspect ratio
-   *
-   *  source: https://github.com/android/camera-samples/blob/bfc46b68ef51f337104d3487c2394b204cf0d453/CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/CameraFragment.kt#L350
-   */
-  private fun aspectRatio(width: Int, height: Int): Int {
-    val previewRatio = max(width, height).toDouble() / min(width, height)
-    if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
-      return AspectRatio.RATIO_4_3
-    }
-    return AspectRatio.RATIO_16_9
+  private fun setQrCodeAnalyzer(qrCodeAnalyzer: ImageAnalysis.Analyzer) {
+    cameraController.clearImageAnalysisAnalyzer()
+    cameraController.setImageAnalysisAnalyzer(cameraExecutor, qrCodeAnalyzer)
   }
 
   private fun qrCodeScanned(qrCode: String) {

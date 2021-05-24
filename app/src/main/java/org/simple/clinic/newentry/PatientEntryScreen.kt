aa59d39b9f4abe2679b82fa356acae91e7760a51
Honey Sonwani <honey@obvious.in>
2021-05-24 20:18:53 +0530

    Prefill NHID in `PatientEntryScreen` (#2652)
    * Add `identifierTypeTextView` ID
    
    * Prefill identifier type and value depending on the Identifier received
    
    * Update CHANGELOG
    
    * Remove `patiententry_identifier` translations
    
    because the original key is not more the same as these.

--- app/src/main/java/org/simple/clinic/newentry/PatientEntryScreen.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryScreen.kt
@@ -209,6 +209,9 @@ class PatientEntryScreen(context: Context, attrs: AttributeSet) : RelativeLayout
   private val saveButton
     get() = binding!!.saveButton
 
+  private val identifierTypeTextView
+    get() = binding!!.identifierTypeTextView
+
   private val villageTypeAheadAdapter by unsafeLazy {
     ArrayAdapter<String>(
         context,
@@ -478,6 +481,20 @@ class PatientEntryScreen(context: Context, attrs: AttributeSet) : RelativeLayout
   }
 
   private fun prefillIdentifier(identifier: Identifier) {
+    when (identifier.type) {
+      Identifier.IdentifierType.IndiaNationalHealthId -> prefillIndiaNationalHealthID(identifier)
+      Identifier.IdentifierType.BpPassport -> prefillBpPassport(identifier)
+      else -> throw IllegalArgumentException("Unknown alternate id: $identifier")
+    }
+  }
+
+  private fun prefillIndiaNationalHealthID(identifier: Identifier) {
+    identifierTypeTextView.text = resources.getString(R.string.patiententry_identifier_national_health_id)
+    identifierTextView.text = identifier.displayValue()
+  }
+
+  private fun prefillBpPassport(identifier: Identifier) {
+    identifierTypeTextView.text = resources.getString(R.string.patiententry_identifier_bp_passport)
     identifierTextView.text = identifier.displayValue()
   }
 

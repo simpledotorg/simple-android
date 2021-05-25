326bfd35c9369c94393cdd09688f0ac2cc4e2ccf
Janhavi Singh <janhavi@obvious.in>
2021-05-25 18:34:30 +0530

    Create `IndiaNHIDDateOfBirthMoshiAdapter` with `IndiaNHIDDateOFBirth` Json qualifier

---
+++ app/src/main/java/org/simple/clinic/scanid/IndiaNHIDDateOfBirthMoshiAdapter.kt
@@ -0,0 +1,26 @@
+package org.simple.clinic.scanid
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+class IndiaNHIDDateOfBirthMoshiAdapter {
+  companion object {
+    private val dateTimeFormatter = DateTimeFormatter.ofPattern("d/M/yyyy", Locale.ENGLISH)
+  }
+
+  @FromJson
+  @IndiaNHIDDateOfBirth
+  fun toLocalDate(value: String?): LocalDate? {
+    return value?.let {
+      dateTimeFormatter.parse(value, LocalDate::from)
+    }
+  }
+
+  @ToJson
+  fun fromLocalDate(@IndiaNHIDDateOfBirth date: LocalDate?): String? {
+    return date?.format(dateTimeFormatter)
+  }
+}

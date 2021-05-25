b998740eba90584c71b616ae9ffba3e268f9f8f5
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-05-25 10:19:53 +0530

    Redact sensitive information from logs (#2650)
    * Add redacted unicode string
    
    We will eventually move this inside a factory or property builder that will redact sensitive data. For now we can keep it as global and use it in `toString` override.
    
    * Redact `User` information for logs
    
    * Redact `PatientSummaryProfile` information for logs
    
    * Redact `MedicalOfficer`s information for logs
    
    * Update CHANGELOG
    
    (cherry picked from commit 5044945e173bbe33c04698f906ca4e9d5353fa65)

--- app/src/main/java/org/simple/clinic/summary/PatientSummaryProfile.kt
+++ app/src/main/java/org/simple/clinic/summary/PatientSummaryProfile.kt
@@ -8,6 +8,7 @@ import org.simple.clinic.patient.PatientAddress
 import org.simple.clinic.patient.PatientPhoneNumber
 import org.simple.clinic.patient.businessid.BusinessId
 import org.simple.clinic.patient.businessid.Identifier
+import org.simple.clinic.util.Unicode
 
 @Parcelize
 data class PatientSummaryProfile(
@@ -24,5 +25,9 @@ data class PatientSummaryProfile(
 
   fun hasIdentifier(identifier: Identifier): Boolean {
     return bpPassport?.identifier == identifier || alternativeId?.identifier == identifier
+  }
+
+  override fun toString(): String {
+    return "PatientSummaryProfile(${Unicode.redacted})"
   }
 }

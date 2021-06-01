378064027708c81703b3d050ce64bd6b4cdc98f9
Honey Sonwani <honeysonwani88@gmail.com>
2021-06-01 16:15:24 +0530

    Load data when newUser is not waiting for sms verification as well

--- app/src/main/java/org/simple/clinic/home/patients/PatientsTabUpdate.kt
+++ app/src/main/java/org/simple/clinic/home/patients/PatientsTabUpdate.kt
@@ -28,7 +28,6 @@ class PatientsTabUpdate : Update<PatientsTabModel, PatientsTabEvent, PatientsTab
     }
   }
 
-  // TODO (vs) 26/05/20: This should actually be rendered and not be as effects. Move later.
   private fun showAccountNotifications(
       model: PatientsTabModel,
       event: UserDetailsLoaded
@@ -39,20 +38,16 @@ class PatientsTabUpdate : Update<PatientsTabModel, PatientsTabEvent, PatientsTab
 
     val effects = mutableSetOf<PatientsTabEffect>()
 
-    when {
-      newUser.isWaitingForApproval -> {
-        // User is waiting for approval (new registration or login on a new device before being approved).
-        clearDismissedApprovalStatusIfNeeded(previousUser, effects)
-      }
-
-      newUser.isApprovedForSyncing && (previousUser == null || previousUser.isWaitingForApproval) -> {
-        // User was just approved
-        effects.add(LoadInfoForShowingApprovalStatus)
-      }
-
-      else -> effects.add(HideUserAccountStatus)
+    if (newUser.isWaitingForApproval) {
+      // User is waiting for approval (new registration or login on a new device before being approved).
+      clearDismissedApprovalStatusIfNeeded(previousUser, effects)
     }
 
+    if (newUser.isApprovedForSyncing && (previousUser == null || previousUser.isWaitingForApproval) && !newUser.isPendingSmsVerification) {
+      // User was just approved
+      effects.add(LoadInfoForShowingApprovalStatus)
+    }
+    
     return next(updatedModel, effects)
   }
 

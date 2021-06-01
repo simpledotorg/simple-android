a73688a616d4caf86be5a37663b7f6fe1939908b
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-06-01 10:23:02 +0530

    Remove unused code

--- app/src/main/java/org/simple/clinic/searchresultsview/SearchResultsEffect.kt
+++
@@ -1,7 +0,0 @@
-package org.simple.clinic.searchresultsview
-
-import org.simple.clinic.patient.PatientSearchCriteria
-
-sealed class SearchResultsEffect
-
-data class SearchWithCriteria(val searchCriteria: PatientSearchCriteria) : SearchResultsEffect()

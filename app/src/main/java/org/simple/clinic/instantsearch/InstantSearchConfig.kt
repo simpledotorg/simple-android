6957e6a799623d6b8917385451764740640c8090
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-06-03 13:41:16 +0530

    Add `pagingConfig` to `InstantSearchConfig`

--- app/src/main/java/org/simple/clinic/instantsearch/InstantSearchConfig.kt
+++ app/src/main/java/org/simple/clinic/instantsearch/InstantSearchConfig.kt
@@ -1,17 +1,21 @@
 package org.simple.clinic.instantsearch
 
+import androidx.paging.PagingConfig
 import org.simple.clinic.remoteconfig.ConfigReader
 
 data class InstantSearchConfig(
-    val minLengthOfSearchQuery: Int
+    val minLengthOfSearchQuery: Int,
+    val pagingConfig: PagingConfig
 ) {
 
   companion object {
     fun read(configReader: ConfigReader): InstantSearchConfig {
       val minLengthOfSearchQuery = configReader.long("min_length_of_search_query", 2)
+      val instantSearchPageSize = configReader.long("instant_search_page_size", 20).toInt()
 
       return InstantSearchConfig(
           minLengthOfSearchQuery = minLengthOfSearchQuery.toInt(),
+          pagingConfig = PagingConfig(pageSize = instantSearchPageSize)
       )
     }
   }

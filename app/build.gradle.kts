32e3b018de8e4cc6f43406e84107b23089c18366
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-06-09 14:13:39 +0530

    Opt into experimental Coroutines API usage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -231,6 +231,7 @@ android {
 
   kotlinOptions {
     jvmTarget = "1.8"
+    freeCompilerArgs = freeCompilerArgs + listOf("-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi")
   }
 
   sourceSets {

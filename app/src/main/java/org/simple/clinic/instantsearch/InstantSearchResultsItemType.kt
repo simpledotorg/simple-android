cc66f9a796b2260b5ec6ff61ced16891de30728b
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-06-10 06:18:11 +0530

    Add `InstantSearchResultsItemType` extending `PagingItemAdapter`

---
+++ app/src/main/java/org/simple/clinic/instantsearch/InstantSearchResultsItemType.kt
@@ -0,0 +1,165 @@
+package org.simple.clinic.instantsearch
+
+import androidx.annotation.VisibleForTesting
+import androidx.core.content.ContextCompat
+import androidx.paging.PagingData
+import androidx.paging.TerminalSeparatorType.SOURCE_COMPLETE
+import androidx.paging.insertSeparators
+import androidx.paging.map
+import androidx.recyclerview.widget.DiffUtil
+import io.reactivex.subjects.Subject
+import org.simple.clinic.R
+import org.simple.clinic.databinding.ListPatientSearchBinding
+import org.simple.clinic.databinding.ListPatientSearchHeaderBinding
+import org.simple.clinic.facility.Facility
+import org.simple.clinic.patient.PatientSearchResult
+import org.simple.clinic.widgets.PagingItemAdapter
+import org.simple.clinic.widgets.PatientSearchResultItemView.PatientSearchResultViewModel
+import org.simple.clinic.widgets.recyclerview.BindingViewHolder
+import java.util.UUID
+
+sealed class InstantSearchResultsItemType : PagingItemAdapter.Item<InstantSearchResultsItemType.Event> {
+
+  companion object {
+
+    fun from(
+        patientSearchResults: PagingData<PatientSearchResult>,
+        currentFacility: Facility,
+        searchQuery: String?
+    ): PagingData<InstantSearchResultsItemType> {
+      return patientSearchResults
+          .map { SearchResult.forSearchResult(it, currentFacility.uuid, searchQuery) }
+          .insertSeparators(SOURCE_COMPLETE) { before, after -> insertHeaders(before, after, currentFacility) }
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    fun insertHeaders(
+        beforeSearchResult: SearchResult?,
+        afterSearchResult: SearchResult?,
+        currentFacility: Facility
+    ): InstantSearchResultsItemType? {
+      return when {
+        shouldAddAssignedFacility(beforeSearchResult, afterSearchResult) -> AssignedFacilityHeader(facilityName = currentFacility.name)
+        shouldAddNearbyFacilityHeader(beforeSearchResult, afterSearchResult) -> NearbyFacilitiesHeader
+        else -> null
+      }
+    }
+
+    private fun shouldAddAssignedFacility(beforeSearchResult: SearchResult?, afterSearchResult: SearchResult?) =
+        beforeSearchResult == null && afterSearchResult != null && afterSearchResult.isAtCurrentFacility
+
+    private fun shouldAddNearbyFacilityHeader(beforeSearchResult: SearchResult?, afterSearchResult: SearchResult?) =
+        isFirstSearchResultNotAtCurrentFacility(beforeSearchResult, afterSearchResult) || shouldAddNearbyFacilityHeaderInBetweenSearchResults(beforeSearchResult, afterSearchResult)
+
+    private fun isFirstSearchResultNotAtCurrentFacility(beforeSearchResult: SearchResult?, afterSearchResult: SearchResult?) =
+        beforeSearchResult == null && afterSearchResult != null && !afterSearchResult.isAtCurrentFacility
+
+    private fun shouldAddNearbyFacilityHeaderInBetweenSearchResults(beforeSearchResult: SearchResult?, afterSearchResult: SearchResult?) =
+        beforeSearchResult != null && afterSearchResult != null && beforeSearchResult.isAtCurrentFacility && !afterSearchResult.isAtCurrentFacility
+  }
+
+  sealed class Event {
+    data class ResultClicked(val patientUuid: UUID) : Event()
+  }
+
+  data class AssignedFacilityHeader(val facilityName: String) : InstantSearchResultsItemType() {
+
+    override fun layoutResId(): Int = R.layout.list_patient_search_header
+
+    override fun render(holder: BindingViewHolder, subject: Subject<Event>) {
+      val binding = holder.binding as ListPatientSearchHeaderBinding
+      val context = holder.itemView.context
+      val headerColor = ContextCompat.getColor(context, R.color.color_on_surface_67)
+
+      binding.patientsearchHeader.text = context.getString(R.string.patientinstantsearch_assigned_facility_header, facilityName)
+      binding.patientsearchHeader.setTextColor(headerColor)
+    }
+  }
+
+  object NearbyFacilitiesHeader : InstantSearchResultsItemType() {
+
+    override fun layoutResId(): Int = R.layout.list_patient_search_header
+
+    override fun render(holder: BindingViewHolder, subject: Subject<Event>) {
+      val binding = holder.binding as ListPatientSearchHeaderBinding
+      val context = holder.itemView.context
+      val headerColor = ContextCompat.getColor(context, R.color.simple_red_500)
+
+      binding.patientsearchHeader.text = context.getString(R.string.patientinstantsearch_nearby_facilities_header)
+      binding.patientsearchHeader.setTextColor(headerColor)
+    }
+  }
+
+  data class SearchResult(
+      val searchResultViewModel: PatientSearchResultViewModel,
+      val currentFacilityId: UUID,
+      val assignedFacilityId: UUID?,
+      val searchQuery: String?
+  ) : InstantSearchResultsItemType() {
+
+    companion object {
+      fun forSearchResult(
+          searchResult: PatientSearchResult,
+          currentFacilityId: UUID,
+          searchQuery: String?
+      ): SearchResult {
+        return SearchResult(
+            searchResultViewModel = mapPatientSearchResultToViewModel(searchResult),
+            currentFacilityId = currentFacilityId,
+            assignedFacilityId = searchResult.assignedFacilityId,
+            searchQuery = searchQuery
+        )
+      }
+
+      private fun mapPatientSearchResultToViewModel(searchResult: PatientSearchResult): PatientSearchResultViewModel {
+        return PatientSearchResultViewModel(
+            uuid = searchResult.uuid,
+            fullName = searchResult.fullName,
+            gender = searchResult.gender,
+            age = searchResult.age,
+            dateOfBirth = searchResult.dateOfBirth,
+            address = searchResult.address,
+            phoneNumber = searchResult.phoneNumber,
+            lastSeen = searchResult.lastSeen,
+            identifier = searchResult.identifier
+        )
+      }
+    }
+
+    val isAtCurrentFacility
+      get() = assignedFacilityId == currentFacilityId
+
+    override fun layoutResId(): Int = R.layout.list_patient_search
+
+    override fun render(holder: BindingViewHolder, subject: Subject<Event>) {
+      val binding = holder.binding as ListPatientSearchBinding
+
+      binding.patientSearchResultView.setOnClickListener {
+        subject.onNext(Event.ResultClicked(searchResultViewModel.uuid))
+      }
+      binding.patientSearchResultView.render(searchResultViewModel, currentFacilityId, searchQuery)
+    }
+  }
+
+  class DiffCallback : DiffUtil.ItemCallback<InstantSearchResultsItemType>() {
+
+    override fun areItemsTheSame(
+        oldItem: InstantSearchResultsItemType,
+        newItem: InstantSearchResultsItemType
+    ): Boolean {
+      return when {
+        oldItem is AssignedFacilityHeader && newItem is AssignedFacilityHeader -> oldItem.facilityName == newItem.facilityName
+        oldItem is NearbyFacilitiesHeader && newItem is NearbyFacilitiesHeader -> true
+        oldItem is SearchResult && newItem is SearchResult -> oldItem.searchResultViewModel.uuid == newItem.searchResultViewModel.uuid
+        else -> false
+      }
+    }
+
+    override fun areContentsTheSame(
+        oldItem: InstantSearchResultsItemType,
+        newItem: InstantSearchResultsItemType
+    ): Boolean {
+      return oldItem == newItem
+    }
+  }
+}
---
+++ app/src/test/java/org/simple/clinic/instantsearch/InstantSearchResultsItemTypeTest.kt
@@ -0,0 +1,161 @@
+package org.simple.clinic.instantsearch
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.simple.clinic.TestData
+import org.simple.clinic.instantsearch.InstantSearchResultsItemType.AssignedFacilityHeader
+import org.simple.clinic.instantsearch.InstantSearchResultsItemType.NearbyFacilitiesHeader
+import org.simple.clinic.instantsearch.InstantSearchResultsItemType.SearchResult
+import java.util.UUID
+
+class InstantSearchResultsItemTypeTest {
+
+  private val currentFacility = TestData.facility(
+      uuid = UUID.fromString("7174598a-434f-4043-9140-8c23046d4ff6")
+  )
+  private val otherFacility = TestData.facility(
+      uuid = UUID.fromString("c7328023-bcd4-4862-b16f-5404df3fd051")
+  )
+
+  @Test
+  fun assigned_facility_header_must_be_shown_when_the_first_search_result_is_at_current_facility() {
+    // given
+    val patientInCurrentFacility = SearchResult.forSearchResult(
+        searchResult = TestData.patientSearchResult(
+            uuid = UUID.fromString("2ea03550-896d-4a3d-a977-5ec9c3f91002"),
+            assignedFacilityId = currentFacility.uuid
+        ),
+        currentFacilityId = currentFacility.uuid,
+        searchQuery = null
+    )
+
+    // when
+    val header = InstantSearchResultsItemType.insertHeaders(
+        beforeSearchResult = null,
+        afterSearchResult = patientInCurrentFacility,
+        currentFacility = currentFacility
+    )
+
+    // then
+    assertThat(header).isEqualTo(AssignedFacilityHeader(currentFacility.name))
+  }
+
+  @Test
+  fun nearby_facility_header_must_be_shown_when_the_first_search_results_is_not_at_current_facility() {
+    // given
+    val patientInOtherFacility = SearchResult.forSearchResult(
+        searchResult = TestData.patientSearchResult(
+            uuid = UUID.fromString("2ea03550-896d-4a3d-a977-5ec9c3f91002"),
+            assignedFacilityId = otherFacility.uuid
+        ),
+        currentFacilityId = currentFacility.uuid,
+        searchQuery = null
+    )
+
+    // when
+    val header = InstantSearchResultsItemType.insertHeaders(
+        beforeSearchResult = null,
+        afterSearchResult = patientInOtherFacility,
+        currentFacility = currentFacility
+    )
+
+    // then
+    assertThat(header).isEqualTo(NearbyFacilitiesHeader)
+  }
+
+  @Test
+  fun nearby_facility_header_must_be_shown_between_search_results_at_current_facility_and_not_at_current_facility() {
+    // given
+    val patientInCurrentFacility = SearchResult.forSearchResult(
+        searchResult = TestData.patientSearchResult(
+            uuid = UUID.fromString("2ea03550-896d-4a3d-a977-5ec9c3f91002"),
+            assignedFacilityId = currentFacility.uuid
+        ),
+        currentFacilityId = currentFacility.uuid,
+        searchQuery = null
+    )
+
+    val patientInOtherFacility = SearchResult.forSearchResult(
+        searchResult = TestData.patientSearchResult(
+            uuid = UUID.fromString("39d44632-3183-4755-ae10-966651447197"),
+            assignedFacilityId = otherFacility.uuid
+        ),
+        currentFacilityId = currentFacility.uuid,
+        searchQuery = null
+    )
+
+    // when
+    val header = InstantSearchResultsItemType.insertHeaders(
+        beforeSearchResult = patientInCurrentFacility,
+        afterSearchResult = patientInOtherFacility,
+        currentFacility = currentFacility
+    )
+
+    // then
+    assertThat(header).isEqualTo(NearbyFacilitiesHeader)
+  }
+
+  @Test
+  fun no_header_should_be_shown_when_both_search_results_are_at_current_facility() {
+    // given
+    val patient1InCurrentFacility = SearchResult.forSearchResult(
+        searchResult = TestData.patientSearchResult(
+            uuid = UUID.fromString("2ea03550-896d-4a3d-a977-5ec9c3f91002"),
+            assignedFacilityId = currentFacility.uuid
+        ),
+        currentFacilityId = currentFacility.uuid,
+        searchQuery = null
+    )
+
+    val patient2InCurrentFacility = SearchResult.forSearchResult(
+        searchResult = TestData.patientSearchResult(
+            uuid = UUID.fromString("39d44632-3183-4755-ae10-966651447197"),
+            assignedFacilityId = currentFacility.uuid
+        ),
+        currentFacilityId = currentFacility.uuid,
+        searchQuery = null
+    )
+
+    // when
+    val header = InstantSearchResultsItemType.insertHeaders(
+        beforeSearchResult = patient1InCurrentFacility,
+        afterSearchResult = patient2InCurrentFacility,
+        currentFacility = currentFacility
+    )
+
+    // then
+    assertThat(header).isNull()
+  }
+
+  @Test
+  fun no_header_should_be_shown_when_both_search_results_are_not_at_current_facility() {
+    // given
+    val patient1InOtherFacility = SearchResult.forSearchResult(
+        searchResult = TestData.patientSearchResult(
+            uuid = UUID.fromString("2ea03550-896d-4a3d-a977-5ec9c3f91002"),
+            assignedFacilityId = otherFacility.uuid
+        ),
+        currentFacilityId = currentFacility.uuid,
+        searchQuery = null
+    )
+
+    val patient2InOtherFacility = SearchResult.forSearchResult(
+        searchResult = TestData.patientSearchResult(
+            uuid = UUID.fromString("39d44632-3183-4755-ae10-966651447197"),
+            assignedFacilityId = otherFacility.uuid
+        ),
+        currentFacilityId = currentFacility.uuid,
+        searchQuery = null
+    )
+
+    // when
+    val header = InstantSearchResultsItemType.insertHeaders(
+        beforeSearchResult = patient1InOtherFacility,
+        afterSearchResult = patient2InOtherFacility,
+        currentFacility = currentFacility
+    )
+
+    // then
+    assertThat(header).isNull()
+  }
+}

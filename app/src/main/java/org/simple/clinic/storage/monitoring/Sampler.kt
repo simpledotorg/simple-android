ffebcd73a61a272ee566834a4f7ac07695d8b182
Vinay Shenoy <code@vinaysshenoy.com>
2021-06-11 12:24:08 +0530

    Transform generated Room DAOs instead of using reflection to instrument performance (#2703)
    * Add new version of Room metadata generator tool
    
    * Add new Gradle task for transforming the generated Room DAO
    
    * Use the new task to transform generated Room DAOs
    
    * Include debug builds for transforming Room DAOs
    
    * Remove older task for generating Room metadata
    
    * Add no-op `SqlPerformanceReporter` class to report SQL profiling information
    
    * Add `ReportSink` interface to represent a destination to pipe profiling results
    
    * Add support to send SQL profiling events to all sinks in `SqlPerformanceReporter`
    
    * Add reporting sink to push SQL profiling data to Analytics
    
    * Add the analytics reporting sink to the SQL performance reporter
    
    * Remove alternate SupportSQLiteOpenHelper injection for release builds
    
    This is no longer necessary since the profiling code is handled at a different layer.
    
    * Remove code related to older method of SQL performance profiling
    
    * Ensure sample rate validation will not result in a fatal crash
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/storage/monitoring/Sampler.kt
+++ app/src/main/java/org/simple/clinic/storage/monitoring/Sampler.kt
@@ -6,15 +6,11 @@ import kotlin.random.Random
  * Class used to encapsulate random sampling of any kind
  * */
 class Sampler(
-    private val sampleRate: Float
+    sampleRate: Float
 ) {
 
-  init {
-    if (sampleRate < 0.0 || sampleRate > 1.0) {
-      throw IllegalArgumentException("Sample rate must be between 0.0 and 1.0! Found $sampleRate")
-    }
-  }
+  private val verifiedSampleRate = if (sampleRate < 0.0 || sampleRate > 1.0) 0F else sampleRate
 
   val sample: Boolean
-    get() = Random.nextFloat() < sampleRate
+    get() = Random.nextFloat() < verifiedSampleRate
 }

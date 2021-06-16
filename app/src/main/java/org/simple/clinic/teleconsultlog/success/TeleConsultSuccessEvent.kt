0fc57120f3d3062d7ac23862ba0a197627371150
Honey Sonwani <honey@obvious.in>
2021-06-16 22:23:22 +0530

    Add missing analytics events in teleconsult log package (#2712)
    * Extend UIEvent to create analytics
    
    * Add analytics when prescription yes or no is clicked
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/teleconsultlog/success/TeleConsultSuccessEvent.kt
+++ app/src/main/java/org/simple/clinic/teleconsultlog/success/TeleConsultSuccessEvent.kt
@@ -1,11 +1,16 @@
 package org.simple.clinic.teleconsultlog.success
 
 import org.simple.clinic.patient.Patient
+import org.simple.clinic.widgets.UiEvent
 
-sealed class TeleConsultSuccessEvent
+sealed class TeleConsultSuccessEvent : UiEvent
 
 data class PatientDetailsLoaded(val patient: Patient) : TeleConsultSuccessEvent()
 
-object NoPrescriptionClicked : TeleConsultSuccessEvent()
+object NoPrescriptionClicked : TeleConsultSuccessEvent() {
+  override val analyticsName: String = "Teleconsult Log:No Prescription Clicked"
+}
 
-object YesPrescriptionClicked : TeleConsultSuccessEvent()
+object YesPrescriptionClicked : TeleConsultSuccessEvent() {
+  override val analyticsName: String = "Teleconsult Log:Yes Prescription Clicked"
+}

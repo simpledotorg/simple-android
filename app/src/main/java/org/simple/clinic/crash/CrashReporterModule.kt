fe4dea3da54a5e8397f9891a9b9fad5f2a7f38e9
Vinay <vinay@obvious.in>
2021-06-18 12:09:42 +0530

    Use `CrashReporter` in place of `CrashReporter_Old` in DI
    This is done in order to let the code continue to function as we move
    functionality into `CrashReporter`. This will eventually be removed.

--- app/src/main/java/org/simple/clinic/crash/CrashReporterModule.kt
+++ app/src/main/java/org/simple/clinic/crash/CrashReporterModule.kt
@@ -2,14 +2,14 @@ package org.simple.clinic.crash
 
 import dagger.Module
 import dagger.Provides
+import org.simple.clinic.platform.crash.CrashReporter
 import org.simple.clinic.platform.crash.CrashReporter_Old
-import org.simple.clinic.platform.crash.NoOpCrashReporter
 
 @Module
 class CrashReporterModule {
 
   @Provides
   fun crashReporter(): CrashReporter_Old {
-    return NoOpCrashReporter()
+    return CrashReporter
   }
 }

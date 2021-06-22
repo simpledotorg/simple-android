1b242740d9272f85a5c8f2480f6555f3e4dbac40
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-06-22 09:08:52 +0530

    Setup screen UI in `onViewCreated`

--- app/src/main/java/org/simple/clinic/recentpatient/RecentPatientsScreen.kt
+++ app/src/main/java/org/simple/clinic/recentpatient/RecentPatientsScreen.kt
@@ -1,7 +1,9 @@
 package org.simple.clinic.recentpatient
 
+import android.os.Bundle
 import android.os.Parcelable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.reactivex.Observable
@@ -92,6 +94,12 @@ class RecentPatientsScreen : BaseScreen<
         init = AllRecentPatientsInit(),
         modelUpdateListener = uiRenderer::render
     )
+  }
+
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    setupScreen()
   }
 
   override fun onFinishInflate() {

6f2dcb3aa01d525fa112e3681231aa4db4bc89c8
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-06-22 12:20:15 +0530

    When recent patient item is clicked, then open patient summary

--- app/src/test/java/org/simple/clinic/recentpatient/AllRecentPatientsUpdateTest.kt
+++ app/src/test/java/org/simple/clinic/recentpatient/AllRecentPatientsUpdateTest.kt
@@ -11,20 +11,35 @@ import java.util.UUID
 
 class AllRecentPatientsUpdateTest {
 
+  private val defaultModel = AllRecentPatientsModel.create()
+  private val updateSpec = UpdateSpec(AllRecentPatientsUpdate())
+
   @Test
   fun `when recent patients are loaded, then show recent patients`() {
-    val defaultModel = AllRecentPatientsModel.create()
     val recentPatients = PagingData.from(listOf(
         TestData.recentPatient(uuid = UUID.fromString("68bb2fba-512f-4419-bdda-9b661e90b805")),
         TestData.recentPatient(uuid = UUID.fromString("1fb172bc-b705-4ffb-a5bd-425811c430ab"))
     ))
 
-    UpdateSpec(AllRecentPatientsUpdate())
+    updateSpec
         .given(defaultModel)
         .whenEvent(RecentPatientsLoaded(recentPatients))
         .then(assertThatNext(
             hasNoModel(),
             hasEffects(ShowRecentPatients(recentPatients))
+        ))
+  }
+
+  @Test
+  fun `when recent patient is clicked, then open patient summary`() {
+    val patientUuid = UUID.fromString("61891a99-b9f1-48bc-99d2-0495f16c6de5")
+
+    updateSpec
+        .given(defaultModel)
+        .whenEvent(RecentPatientItemClicked(patientUuid = patientUuid))
+        .then(assertThatNext(
+            hasNoModel(),
+            hasEffects(OpenPatientSummary(patientUuid))
         ))
   }
 }

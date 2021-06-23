b7038e5d82eae6a51489df63da4b7a727715535b
Janhavi Singh <janhavi@obvious.in>
2021-06-23 10:49:55 +0530

    Add `INDIA_NHID_LENGTH` as a global variable

--- app/src/main/java/org/simple/clinic/Globals.kt
+++ app/src/main/java/org/simple/clinic/Globals.kt
@@ -4,3 +4,4 @@ const val MAX_ALLOWED_PATIENT_AGE: Int = 120
 const val MIN_ALLOWED_PATIENT_AGE: Int = 1
 const val SECURITY_PIN_LENGTH: Int = 4
 const val LOGIN_OTP_LENGTH = 6
+const val INDIA_NHID_LENGTH = 14

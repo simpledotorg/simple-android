f4727ec7c96a106dbaf0297099b8f1506a0a59ef
Vinay <vinay@obvious.in>
2021-06-29 10:47:01 +0530

    Add `CompleteMedicalRecord` to represent the full patient record

---
+++ app/src/main/java/org/simple/clinic/patient/CompleteMedicalRecord.kt
@@ -0,0 +1,16 @@
+package org.simple.clinic.patient
+
+import org.simple.clinic.bloodsugar.BloodSugarMeasurement
+import org.simple.clinic.bp.BloodPressureMeasurement
+import org.simple.clinic.drugs.PrescribedDrug
+import org.simple.clinic.medicalhistory.MedicalHistory
+import org.simple.clinic.overdue.Appointment
+
+data class CompleteMedicalRecord(
+    val patient: PatientProfile,
+    val medicalHistory: MedicalHistory,
+    val appointments: List<Appointment>,
+    val bloodPressures: List<BloodPressureMeasurement>,
+    val bloodSugars: List<BloodSugarMeasurement>,
+    val prescribedDrugs: List<PrescribedDrug>
+)

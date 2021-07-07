d60ceb8dc71c2414e48ae860b104e7fa5f89b6fb
Janhavi Singh <janhavi@obvious.in>
2021-07-07 17:00:54 +0530

    Add `PatientDetails` data class

---
+++ app/src/main/java/org/simple/clinic/contactpatient/PatientDetails.kt
@@ -0,0 +1,17 @@
+package org.simple.clinic.contactpatient
+
+import org.simple.clinic.medicalhistory.Answer
+import org.simple.clinic.patient.Gender
+import java.time.Instant
+
+data class PatientDetails(
+    val name: String,
+    val gender: Gender,
+    val age: Int,
+    val phoneNumber: String?,
+    val patientAddress: String,
+    val registeredFacility: String,
+    val diagnosedWithDiabetes: Answer?,
+    val diagnosedWithHypertension: Answer?,
+    val lastVisited: Instant
+)

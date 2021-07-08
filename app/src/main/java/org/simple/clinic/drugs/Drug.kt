6470e4f42641be9a144dd6548f315a6cb926c54a
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-07-08 12:33:46 +0530

    Add `Drug` database model (#2812)
    * Add `Drug` Room table
    
    * Add migration for adding `Drug`s table
    
    * Update CHANGELOG

---
+++ app/src/main/java/org/simple/clinic/drugs/Drug.kt
@@ -0,0 +1,33 @@
+package org.simple.clinic.drugs
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.simple.clinic.storage.Timestamps
+import org.simple.clinic.teleconsultlog.medicinefrequency.MedicineFrequency
+import java.util.UUID
+
+@Entity
+data class Drug(
+    @PrimaryKey
+    val id: UUID,
+
+    val name: String,
+
+    val category: DrugCategory?,
+
+    val frequency: MedicineFrequency?,
+
+    val composition: String?,
+
+    val dosage: String?,
+
+    val rxNormCode: String?,
+
+    val protocol: Answer,
+
+    val common: Answer,
+
+    @Embedded
+    val timestamps: Timestamps
+)
---
+++ app/src/main/java/org/simple/clinic/drugs/DrugCategory.kt
@@ -0,0 +1,60 @@
+package org.simple.clinic.drugs
+
+import androidx.annotation.VisibleForTesting
+import androidx.room.TypeConverter
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import org.simple.clinic.util.room.SafeEnumTypeAdapter
+
+sealed class DrugCategory {
+
+  sealed class Hypertension : DrugCategory() {
+
+    object CCB : Hypertension()
+
+    object ARB : Hypertension()
+
+    object ACE : Hypertension()
+
+    object Diuretic : Hypertension()
+
+    object Other : Hypertension()
+  }
+
+  object Diabetes : DrugCategory()
+
+  object Other : DrugCategory()
+
+  data class Unknown(val actualValue: String) : DrugCategory()
+
+  @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+  object TypeAdapter : SafeEnumTypeAdapter<DrugCategory>(
+      knownMappings = mapOf(
+          Hypertension.CCB to "hypertension_ccb",
+          Hypertension.ARB to "hypertension_arb",
+          Hypertension.ACE to "hypertension_ace",
+          Hypertension.Diuretic to "hypertension_diuretic",
+          Hypertension.Other to "hypertension_other",
+          Diabetes to "diabetes",
+          Other to "other"
+      ),
+      unknownStringToEnumConverter = { Unknown(it) },
+      unknownEnumToStringConverter = { (it as Unknown).actualValue }
+  )
+
+  class RoomTypeConverter {
+    @TypeConverter
+    fun toEnum(value: String?): DrugCategory? = TypeAdapter.toEnum(value)
+
+    @TypeConverter
+    fun fromEnum(drugCategory: DrugCategory?): String? = TypeAdapter.fromEnum(drugCategory)
+  }
+
+  class MoshiTypeConverter {
+    @FromJson
+    fun toEnum(value: String?): DrugCategory? = TypeAdapter.toEnum(value)
+
+    @ToJson
+    fun fromEnum(drugCategory: DrugCategory?): String? = TypeAdapter.fromEnum(drugCategory)
+  }
+}

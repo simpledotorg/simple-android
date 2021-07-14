35654277b948e7979d8cb519c1301981904752b8
Vinay <vinay@obvious.in>
2021-07-14 14:20:17 +0530

    Change `TeleconsultationSync` to use the frequent sync config

--- app/src/main/java/org/simple/clinic/summary/teleconsultation/sync/TeleconsultationSync.kt
+++ app/src/main/java/org/simple/clinic/summary/teleconsultation/sync/TeleconsultationSync.kt
@@ -4,14 +4,14 @@ import io.reactivex.Completable
 import org.simple.clinic.sync.ModelSync
 import org.simple.clinic.sync.SyncConfig
 import org.simple.clinic.sync.SyncConfigType
-import org.simple.clinic.sync.SyncConfigType.Type.Daily
+import org.simple.clinic.sync.SyncConfigType.Type.Frequent
 import org.simple.clinic.util.read
 import javax.inject.Inject
 
 class TeleconsultationSync @Inject constructor(
     private val repository: TeleconsultationFacilityRepository,
     private val api: TeleconsultFacilityInfoApi,
-    @SyncConfigType(Daily) private val config: SyncConfig
+    @SyncConfigType(Frequent) private val config: SyncConfig
 ) : ModelSync {
 
   override val name: String = "TeleconsultationFacilityInfo"

76ed2596b78ded7509bcad5287f056f262536af2
Vinay <vinay@obvious.in>
2021-07-14 16:30:47 +0530

    Remove `SyncInterval#mostFrequent`

--- app/src/main/java/org/simple/clinic/sync/SyncInterval.kt
+++ app/src/main/java/org/simple/clinic/sync/SyncInterval.kt
@@ -5,12 +5,4 @@ import java.time.Duration
 enum class SyncInterval(val frequency: Duration, val backOffDelay: Duration) {
   FREQUENT(frequency = Duration.ofMinutes(16L), backOffDelay = Duration.ofMinutes(5L)),
   DAILY(frequency = Duration.ofDays(1L), backOffDelay = Duration.ofMinutes(5L));
-
-  companion object {
-    fun mostFrequent(): Duration {
-      return enumValues<SyncInterval>()
-          .map { it.frequency }
-          .minOrNull()!!
-    }
-  }
 }

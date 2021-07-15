3b876d7e18742109d149f1167fcb860c38fa9fdd
Janhavi Singh <58420800+janhavisinghh@users.noreply.github.com>
2021-07-15 07:50:43 +0530

    Update `ContactPatientBottomSheet` Behaviour - Final (#2818)
    * Add all `_Old` suffix to all `ContactPatientUI` methods which render `callPatientView_Old`
    
    * Switch to call patient view based on overdue list changes feature flag
    
    * Add `patientProfileHasPhoneNumber` in `ContactPatientModel`
    
    * Render `showPatientWithNoPhoneNumberUi` of patient with no phone number
    
    * Render `showPatientWithPhoneNumberUi` of patient with phone number
    
    * Render patient profile in `ContactPatientBottomSheet`
    
    * Render set appointment reminder view new or old based on overdue list changes feature flag
    
    * Render `showSecureCallUi` based on secure calling feature flag
    
    * Render `hideSecureCallUi` based on secure calling feature flag
    
    * Add `_Old` suffice to all `ContactBottomSheet` event methods which use `callPatientView_Old`
    
    * Add new `ContactBottomSheet` event methods which use `callPatientView`
    
    * Set result of call label for patient with and without phone number
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/contactpatient/ContactPatientUi.kt
+++ app/src/main/java/org/simple/clinic/contactpatient/ContactPatientUi.kt
@@ -5,14 +5,31 @@ import org.simple.clinic.patient.Gender
 import java.time.LocalDate
 
 interface ContactPatientUi {
+  fun switchToCallPatientView_Old()
   fun switchToCallPatientView()
+
   fun switchToSetAppointmentReminderView()
 
-  fun renderPatientDetails(name: String, gender: Gender, age: Int, phoneNumber: String)
-  fun showCallResultSection()
-  fun hideCallResultSection()
+  fun renderPatientDetails_Old(name: String, gender: Gender, age: Int, phoneNumber: String)
+  fun renderPatientDetails(patientDetails: PatientDetails)
+
+  fun showCallResultSection_Old()
+  fun hideCallResultSection_Old()
+  
+  fun showSecureCallUi_Old()
   fun showSecureCallUi()
+
+  fun hideSecureCallUi_Old()
   fun hideSecureCallUi()
+
+  fun showPatientWithNoPhoneNumberUi()
+  fun hidePatientWithNoPhoneNumberUi()
+
+  fun showPatientWithPhoneNumberUi()
+  fun hidePatientWithPhoneNumberUi()
+
+  fun setResultOfCallLabelText()
+  fun setResultLabelText()
 
   fun renderSelectedAppointmentDate(
       selectedAppointmentReminderPeriod: TimeToAppointment,

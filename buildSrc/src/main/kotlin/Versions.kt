14aca2d18295fa9a84f62d132e40705802bfb4f4
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-07-20 09:13:33 +0530

    Bump Fragment to v1.3.6

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -77,7 +77,7 @@ object Versions {
   const val lint = "27.1.1"
   const val rootbeer = "0.0.9"
   const val mlKitBarcode = "16.1.4"
-  const val fragment = "1.2.5"
+  const val fragment = "1.3.6"
   const val androidXCoreKtx = "1.3.2"
   const val threetenExtra = "1.6.0"
   const val spotless = "5.6.1"

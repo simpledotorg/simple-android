818cd01baa86efbde4c0b00d9319007bc11e962a
Vinay <vinay@obvious.in>
2021-07-20 09:41:39 +0530

    [WIP] Set up CI workflow for temporary Heroku environments

---
+++ .github/scripts/compute_node_dependency_hash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+nodeVersion=$(node -v)
+npmVersion=$(npm -v)
+herokuCliVersion=$(npm view heroku version)
+
+echo "Generating cache key from dependencies [node, npm, heroku]"
+
+stringToHash="${nodeVersion} ${npmVersion} ${herokuCliVersion}"
+
+hashed=$(shasum <<< stringToHash | cut -f 1 -d ' ')
+
+echo "Hashed \"${stringToHash}\" -> \"${hashed}\""
+
+echo "::set-output name=node_dep_hash::${hashed}"

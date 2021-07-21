94319bfcd2b5c5f8152d424782fd1d36ab156a02
Janhavi Singh <janhavi@obvious.in>
2021-07-21 13:54:49 +0530

    Handle `ShowEditFrequencyDialog` effect

--- app/src/main/java/org/simple/clinic/drugs/selection/custom/CustomDrugEntryEffect.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/CustomDrugEntryEffect.kt
@@ -1,3 +1,7 @@
 package org.simple.clinic.drugs.selection.custom
 
+import org.simple.clinic.drugs.search.DrugFrequency
+
 sealed class CustomDrugEntryEffect
+
+data class ShowEditFrequencyDialog(val frequency: DrugFrequency) : CustomDrugEntryEffect()

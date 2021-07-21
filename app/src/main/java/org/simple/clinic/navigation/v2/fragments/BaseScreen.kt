f4dcec59f825699f44a27e8d7a2ed48f44cc4eb1
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-07-21 09:13:14 +0530

    Add `MobiusLoopViewModel` in `BaseScreen`

--- app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseScreen.kt
+++ app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseScreen.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.viewbinding.ViewBinding
 import com.spotify.mobius.EventSource
 import com.spotify.mobius.Init
@@ -13,6 +16,8 @@ import com.spotify.mobius.MobiusLoop
 import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
 import com.spotify.mobius.android.MobiusAndroid
+import com.spotify.mobius.android.MobiusLoopViewModel
+import com.spotify.mobius.functions.Consumer
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
@@ -27,6 +32,8 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F, 
   companion object {
     private const val KEY_MODEL = "org.simple.clinic.navigation.v2.fragments.BaseScreen.KEY_MODEL"
   }
+
+  private lateinit var _viewModel: MobiusLoopViewModel<M, E, F, V>
 
   private val loop: MobiusLoop.Builder<M, E, F> by unsafeLazy {
     RxMobius
@@ -77,6 +84,23 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F, 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
+    val startModel = savedInstanceState?.getParcelable(KEY_MODEL) ?: defaultModel()
+
+    _viewModel = ViewModelProvider(viewModelStore, object : ViewModelProvider.Factory {
+      private fun loop(viewEffectsConsumer: Consumer<V>) = RxMobius
+          .loop(createUpdate(), createEffectHandler())
+          .eventSources(additionalEventSources())
+
+      @Suppress("UNCHECKED_CAST")
+      override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MobiusLoopViewModel.create<M, E, F, V>(
+            ::loop,
+            startModel,
+            createInit()
+        ) as T
+      }
+    }).get()
+
     val rxBridge = RxMobiusBridge(events(), uiRenderer())
     controller.connect(rxBridge)
 
@@ -105,5 +129,6 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F, 
   override fun onSaveInstanceState(outState: Bundle) {
     super.onSaveInstanceState(outState)
     outState.putParcelable(KEY_MODEL, controller.model)
+    outState.putParcelable(KEY_MODEL, _viewModel.model)
   }
 }

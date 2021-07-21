32f0161d08323c4440bdc486ecc3c30b483670d1
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-07-21 09:15:40 +0530

    Remove Mobius controller usage from `BaseScreen`

--- app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseScreen.kt
+++ app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseScreen.kt
@@ -12,10 +12,8 @@ import androidx.lifecycle.get
 import androidx.viewbinding.ViewBinding
 import com.spotify.mobius.EventSource
 import com.spotify.mobius.Init
-import com.spotify.mobius.MobiusLoop
 import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
-import com.spotify.mobius.android.MobiusAndroid
 import com.spotify.mobius.android.MobiusLoopViewModel
 import com.spotify.mobius.functions.Consumer
 import com.spotify.mobius.rx2.RxMobius
@@ -35,25 +33,12 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F, 
 
   private lateinit var _viewModel: MobiusLoopViewModel<M, E, F, V>
 
-  private val loop: MobiusLoop.Builder<M, E, F> by unsafeLazy {
-    RxMobius
-        .loop(createUpdate()::update, createEffectHandler())
-        .eventSources(additionalEventSources())
-  }
-
-  private val controller: MobiusLoop.Controller<M, E> by unsafeLazy {
-    MobiusAndroid.controller(loop, defaultModel(), createInit())
-  }
-
   protected val screenKey by unsafeLazy { ScreenKey.key<K>(this) }
 
   private var _binding: B? = null
 
   protected val binding: B
     get() = _binding!!
-
-  val currentModel
-    get() = controller.model
 
   abstract fun defaultModel(): M
 
@@ -100,35 +85,15 @@ abstract class BaseScreen<K : ScreenKey, B : ViewBinding, M : Parcelable, E, F, 
         ) as T
       }
     }).get()
-
-    val rxBridge = RxMobiusBridge(events(), uiRenderer())
-    controller.connect(rxBridge)
-
-    if (savedInstanceState != null) {
-      val savedModel = savedInstanceState.getParcelable<M>(KEY_MODEL)!!
-      controller.replaceModel(savedModel)
-    }
   }
 
   override fun onDestroyView() {
     super.onDestroyView()
-    controller.disconnect()
     _binding = null
-  }
-
-  override fun onResume() {
-    super.onResume()
-    controller.start()
-  }
-
-  override fun onPause() {
-    super.onPause()
-    controller.stop()
   }
 
   override fun onSaveInstanceState(outState: Bundle) {
     super.onSaveInstanceState(outState)
-    outState.putParcelable(KEY_MODEL, controller.model)
     outState.putParcelable(KEY_MODEL, _viewModel.model)
   }
 }

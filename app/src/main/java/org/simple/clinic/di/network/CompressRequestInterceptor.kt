b511234118c657e85c30390208401b48a3ceacab
Vinay <vinay@obvious.in>
2021-07-26 14:34:06 +0530

    Toggle HTTP request body compression based on remote config

--- app/src/main/java/org/simple/clinic/di/network/CompressRequestInterceptor.kt
+++ app/src/main/java/org/simple/clinic/di/network/CompressRequestInterceptor.kt
@@ -3,6 +3,8 @@ package org.simple.clinic.di.network
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import org.simple.clinic.feature.Feature.HttpRequestBodyCompression
+import org.simple.clinic.feature.Features
 import javax.inject.Inject
 
 /**
@@ -10,14 +12,24 @@ import javax.inject.Inject
  *
  * Copied from https://github.com/square/okhttp/blob/8bb58332dbacdd2bb9beb8e5c813f22508ebeb27/samples/guide/src/main/java/okhttp3/recipes/RequestBodyCompression.java
  **/
-class CompressRequestInterceptor @Inject constructor(
-    private val requestCompression: RequestCompression
+class CompressRequestInterceptor(
+    private val requestCompression: RequestCompression,
+    private val requestBodyCompressionEnabled: Boolean
 ) : Interceptor {
+
+  @Inject
+  constructor(
+      requestCompression: RequestCompression,
+      features: Features
+  ) : this(
+      requestCompression = requestCompression,
+      requestBodyCompressionEnabled = features.isEnabled(HttpRequestBodyCompression)
+  )
 
   override fun intercept(chain: Interceptor.Chain): Response {
     val originalRequest = chain.request()
 
-    val transformedRequest = if (originalRequest.eligibleForCompression)
+    val transformedRequest = if (requestBodyCompressionEnabled && originalRequest.eligibleForCompression)
       requestCompression.compress(originalRequest)
     else
       originalRequest

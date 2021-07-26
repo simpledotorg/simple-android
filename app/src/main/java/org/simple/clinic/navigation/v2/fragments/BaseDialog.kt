1d14b65a7fcd10019a58f7ac920bd606773688a7
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-07-26 11:07:37 +0530

    Add `MobiusLoopViewModel` to `BaseDialog` (#2865)
    * Remove `ViewRenderer` generic property from `BaseDialog`
    
    * Set `NoopViewRenderer` as default UI renderer in `BaseDialog`
    
    * Add view effects generic type to `BaseDialog`
    
    * Add `MobiusLoopViewModel` in `BaseDialog`
    
    * Remove Mobius controller usage from `BaseDialog`
    
    * When viewmodel model updates, then render UI
    
    * Change `BaseDialog#createEffectHandler` signature to accept view effects consumer
    
    * Handle view effects in `BaseDialog`
    
    * Forward UI events to viewmodel in `BaseDialog`

--- app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseDialog.kt
+++ app/src/main/java/org/simple/clinic/navigation/v2/fragments/BaseDialog.kt
@@ -6,44 +6,44 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.view.View
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.spotify.mobius.EventSource
 import com.spotify.mobius.Init
-import com.spotify.mobius.MobiusLoop
 import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
-import com.spotify.mobius.android.MobiusAndroid
+import com.spotify.mobius.android.MobiusLoopViewModel
+import com.spotify.mobius.functions.Consumer
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
+import io.reactivex.disposables.Disposable
+import org.simple.clinic.mobius.ViewEffectsHandler
 import org.simple.clinic.mobius.ViewRenderer
 import org.simple.clinic.mobius.eventSources
 import org.simple.clinic.mobius.first
 import org.simple.clinic.navigation.v2.ScreenKey
 import org.simple.clinic.util.unsafeLazy
 
-abstract class BaseDialog<K : ScreenKey, M : Parcelable, E, F, R : ViewRenderer<M>> : DialogFragment() {
+abstract class BaseDialog<K : ScreenKey, M : Parcelable, E, F, V> : DialogFragment() {
 
   companion object {
     private const val KEY_MODEL = "org.simple.clinic.navigation.v2.fragments.BaseScreen.KEY_MODEL"
   }
 
-  private val loop: MobiusLoop.Builder<M, E, F> by unsafeLazy {
-    RxMobius
-        .loop(createUpdate()::update, createEffectHandler())
-        .eventSources(additionalEventSources())
-  }
-
-  private val controller: MobiusLoop.Controller<M, E> by unsafeLazy {
-    MobiusAndroid.controller(loop, defaultModel(), createInit())
-  }
+  private lateinit var viewModel: MobiusLoopViewModel<M, E, F, V>
+  private lateinit var eventsDisposable: Disposable
 
   protected val screenKey by unsafeLazy { ScreenKey.key<K>(this) }
 
   abstract fun defaultModel(): M
 
-  abstract fun uiRenderer(): R
-
   abstract fun createDialog(savedInstanceState: Bundle?): Dialog
+
+  open fun uiRenderer(): ViewRenderer<M> = NoopViewRenderer()
+
+  open fun viewEffectHandler(): ViewEffectsHandler<V> = NoopViewEffectsHandler()
 
   open fun events(): Observable<E> = Observable.never()
 
@@ -51,7 +51,7 @@ abstract class BaseDialog<K : ScreenKey, M : Parcelable, E, F, R : ViewRenderer<
 
   open fun createInit(): Init<M, F> = Init { model -> first(model) }
 
-  open fun createEffectHandler(): ObservableTransformer<F, E> = ObservableTransformer { Observable.never() }
+  open fun createEffectHandler(viewEffectsConsumer: Consumer<V>): ObservableTransformer<F, E> = ObservableTransformer { Observable.never() }
 
   open fun additionalEventSources(): List<EventSource<E>> = emptyList()
 
@@ -66,33 +66,44 @@ abstract class BaseDialog<K : ScreenKey, M : Parcelable, E, F, R : ViewRenderer<
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    val rxBridge = RxMobiusBridge(events(), uiRenderer())
-    controller.connect(rxBridge)
+    val startModel = savedInstanceState?.getParcelable(KEY_MODEL) ?: defaultModel()
 
-    if (savedInstanceState != null) {
-      val savedModel = savedInstanceState.getParcelable<M>(KEY_MODEL)!!
-      controller.replaceModel(savedModel)
-    }
+    viewModel = ViewModelProvider(viewModelStore, object : ViewModelProvider.Factory {
+      private fun loop(viewEffectsConsumer: Consumer<V>) = RxMobius
+          .loop(createUpdate(), createEffectHandler(viewEffectsConsumer))
+          .eventSources(additionalEventSources())
+
+      @Suppress("UNCHECKED_CAST")
+      override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MobiusLoopViewModel.create<M, E, F, V>(
+            ::loop,
+            startModel,
+            createInit()
+        ) as T
+      }
+    }).get()
+
+    eventsDisposable = events().subscribe { viewModel.dispatchEvent(it!!) }
+
+    val uiRenderer = uiRenderer()
+    viewModel.models.observe(viewLifecycleOwner, uiRenderer::render)
+
+    val viewEffectHandler = viewEffectHandler()
+    viewModel.viewEffects.setObserver(
+        viewLifecycleOwner,
+        { liveViewEffect -> viewEffectHandler.handle(liveViewEffect) },
+        { pausedViewEffects -> pausedViewEffects.forEach(viewEffectHandler::handle) }
+    )
   }
 
   override fun onDestroyView() {
     super.onDestroyView()
-    controller.disconnect()
-  }
-
-  override fun onResume() {
-    super.onResume()
-    controller.start()
-  }
-
-  override fun onPause() {
-    super.onPause()
-    controller.stop()
+    eventsDisposable.dispose()
   }
 
   override fun onSaveInstanceState(outState: Bundle) {
     super.onSaveInstanceState(outState)
-    outState.putParcelable(KEY_MODEL, controller.model)
+    outState.putParcelable(KEY_MODEL, viewModel.model)
   }
 
   override fun onCancel(dialog: DialogInterface) {

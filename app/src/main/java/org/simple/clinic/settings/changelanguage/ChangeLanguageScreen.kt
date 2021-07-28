c1cd4264ec71741347b982c230a6c6a362f89401
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-07-28 09:56:06 +0530

    Migrate `ChangeLanguageScreen` to a fragment (#2885)
    * Use `ConstraintLayout` as parent in `screen_change_language`
    
    * Add `ScreenKey` in `ChangeLanguageScreen`
    
    * Use `ChangeLanguageScreen.Key` to open change language screen
    
    * Extend `BaseScreen` in `ChangeLanguageScreen`
    
    * Override fragment lifecycle methods in `ChangeLanguageScreen`
    
    * Set layout manager in the layout file for languages list
    
    * Use `requireActivity` instead of injected activity
    
    * Remove unused code
    
    * Set null adapter for languages list when fragment view is destroyed
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageScreen.kt
+++ app/src/main/java/org/simple/clinic/settings/changelanguage/ChangeLanguageScreen.kt
@@ -1,53 +1,51 @@
 package org.simple.clinic.settings.changelanguage
 
 import android.content.Context
-import android.os.Parcelable
-import android.util.AttributeSet
-import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.jakewharton.rxbinding3.view.clicks
+import com.spotify.mobius.functions.Consumer
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.cast
 import io.reactivex.rxkotlin.ofType
+import kotlinx.parcelize.Parcelize
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.databinding.ListChangeLanguageViewBinding
 import org.simple.clinic.databinding.ScreenChangeLanguageBinding
 import org.simple.clinic.di.injector
-import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.navigation.v2.Router
+import org.simple.clinic.navigation.v2.ScreenKey
+import org.simple.clinic.navigation.v2.fragments.BaseScreen
 import org.simple.clinic.settings.Language
 import org.simple.clinic.settings.changelanguage.ChangeLanguageListItem.Event.ListItemClicked
-import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ItemAdapter
 import javax.inject.Inject
 
-class ChangeLanguageScreen(
-    context: Context,
-    attributeSet: AttributeSet
-) : ConstraintLayout(context, attributeSet), ChangeLanguageUi, UiActions {
+class ChangeLanguageScreen : BaseScreen<
+    ChangeLanguageScreen.Key,
+    ScreenChangeLanguageBinding,
+    ChangeLanguageModel,
+    ChangeLanguageEvent,
+    ChangeLanguageEffect,
+    Unit>(), ChangeLanguageUi, UiActions {
 
   @Inject
   lateinit var router: Router
 
   @Inject
-  lateinit var activity: AppCompatActivity
-
-  @Inject
-  lateinit var effectHandler: ChangeLanguageEffectHandler.Factory
-
-  private var binding: ScreenChangeLanguageBinding? = null
+  lateinit var effectHandlerFactory: ChangeLanguageEffectHandler.Factory
 
   private val toolbar
-    get() = binding!!.toolbar
+    get() = binding.toolbar
 
   private val languagesList
-    get() = binding!!.languagesList
+    get() = binding.languagesList
 
   private val doneButton
-    get() = binding!!.doneButton
+    get() = binding.doneButton
 
   private val languagesAdapter = ItemAdapter(
       diffCallback = ChangeLanguageListItem.DiffCallback(),
@@ -58,47 +56,50 @@ class ChangeLanguageScreen(
       )
   )
 
-  private val events: Observable<ChangeLanguageEvent> by unsafeLazy {
-    Observable
-        .merge(
-            doneButtonClicks(),
-            languageSelections()
-        )
-        .compose(ReportAnalyticsEvents())
-        .cast<ChangeLanguageEvent>()
-  }
+  override fun defaultModel() = ChangeLanguageModel.FETCHING_LANGUAGES
 
-  private val uiRenderer = ChangeLanguageUiRenderer(this)
+  override fun createInit() = ChangeLanguageInit()
 
-  private val delegate: MobiusDelegate<ChangeLanguageModel, ChangeLanguageEvent, ChangeLanguageEffect> by unsafeLazy {
-    MobiusDelegate.forView(
-        events = events,
-        defaultModel = ChangeLanguageModel.FETCHING_LANGUAGES,
-        init = ChangeLanguageInit(),
-        update = ChangeLanguageUpdate(),
-        effectHandler = effectHandler.create(uiActions = this).build(),
-        modelUpdateListener = uiRenderer::render
-    )
-  }
+  override fun createUpdate() = ChangeLanguageUpdate()
 
-  override fun onFinishInflate() {
-    super.onFinishInflate()
-    if (isInEditMode) {
-      return
-    }
+  override fun createEffectHandler(viewEffectsConsumer: Consumer<Unit>) =
+      effectHandlerFactory.create(this).build()
 
-    binding = ScreenChangeLanguageBinding.bind(this)
+  override fun uiRenderer() = ChangeLanguageUiRenderer(this)
 
+  override fun events() = Observable
+      .merge(
+          doneButtonClicks(),
+          languageSelections()
+      )
+      .compose(ReportAnalyticsEvents())
+      .cast<ChangeLanguageEvent>()
+
+  override fun bindView(
+      layoutInflater: LayoutInflater,
+      container: ViewGroup?
+  ) = ScreenChangeLanguageBinding.inflate(layoutInflater, container, false)
+
+  override fun onAttach(context: Context) {
+    super.onAttach(context)
     context.injector<Injector>().inject(this)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
     setupLanguagesList()
     toolbar.setNavigationOnClickListener { router.pop() }
   }
 
+  override fun onDestroyView() {
+    languagesList.adapter = null
+    super.onDestroyView()
+  }
+
   private fun setupLanguagesList() {
     languagesList.apply {
       setHasFixedSize(true)
-      layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
       adapter = languagesAdapter
     }
   }
@@ -115,25 +116,6 @@ class ChangeLanguageScreen(
     return doneButton
         .clicks()
         .map { SaveCurrentLanguageEvent }
-  }
-
-  override fun onAttachedToWindow() {
-    super.onAttachedToWindow()
-    delegate.start()
-  }
-
-  override fun onDetachedFromWindow() {
-    delegate.stop()
-    binding = null
-    super.onDetachedFromWindow()
-  }
-
-  override fun onSaveInstanceState(): Parcelable {
-    return delegate.onSaveInstanceState(super.onSaveInstanceState())
-  }
-
-  override fun onRestoreInstanceState(state: Parcelable?) {
-    super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
 
   override fun displayLanguages(supportedLanguages: List<Language>, selectedLanguage: Language?) {
@@ -153,10 +135,18 @@ class ChangeLanguageScreen(
   }
 
   override fun restartActivity() {
-    activity.recreate()
+    requireActivity().recreate()
   }
 
   interface Injector {
     fun inject(target: ChangeLanguageScreen)
+  }
+
+  @Parcelize
+  data class Key(
+      override val analyticsName: String = "Language Selection"
+  ) : ScreenKey() {
+
+    override fun instantiateFragment() = ChangeLanguageScreen()
   }
 }

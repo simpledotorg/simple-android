27f79c4ea733360c120268b0bd26ea206abc0933
Vinay <vinay@obvious.in>
2021-08-05 13:28:55 +0530

    Change `AppLockScreenKey` to a data class

--- app/src/main/java/org/simple/clinic/login/applock/AppLockScreenKey.kt
+++ app/src/main/java/org/simple/clinic/login/applock/AppLockScreenKey.kt
@@ -1,17 +1,14 @@
 package org.simple.clinic.login.applock
 
 import android.os.Parcelable
-import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 import org.simple.clinic.navigation.v2.ScreenKey
 
 @Parcelize
 data class AppLockScreenKey(
-    val nextScreen: ScreenKey
+    val nextScreen: ScreenKey,
+    override val analyticsName: String = "App Lock"
 ) : ScreenKey(), Parcelable {
-
-  @IgnoredOnParcel
-  override val analyticsName = "App Lock"
 
   override fun instantiateFragment() = AppLockScreen()
 }

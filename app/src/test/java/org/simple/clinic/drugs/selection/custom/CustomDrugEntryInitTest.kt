d7f445fcb4b1296425bee235a8cf2e0637555d45
Janhavi Singh <janhavi@obvious.in>
2021-08-10 21:51:58 +0530

    when sheet is created in update mode, then fetch prescription

--- app/src/test/java/org/simple/clinic/drugs/selection/custom/CustomDrugEntryInitTest.kt
+++ app/src/test/java/org/simple/clinic/drugs/selection/custom/CustomDrugEntryInitTest.kt
@@ -42,4 +42,20 @@ class CustomDrugEntryInitTest {
             )
         )
   }
+
+  @Test
+  fun `when sheet is created in update mode, then fetch prescription`() {
+    val prescribedDrugUuid = UUID.fromString("e046a649-dfc0-45b5-89d4-7a4b0af1c282")
+    val model = CustomDrugEntryModel.default(openAs = OpenAs.Update(
+        patientUuid = UUID.fromString("13008153-beda-475a-909c-793d03e654fb"),
+        prescribedDrugUuid = prescribedDrugUuid))
+
+    initSpec
+        .whenInit(model)
+        .then(
+            assertThatFirst(
+                hasEffects(FetchPrescription(prescribedDrugUuid))
+            )
+        )
+  }
 }

0f7e491b2a62c3baafa00c9b8034aa8ddd98132b
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-08-10 08:46:29 +0530

    Override lifecycle methods of the fragment

--- app/src/main/java/org/simple/clinic/editpatient/EditPatientScreen.kt
+++ app/src/main/java/org/simple/clinic/editpatient/EditPatientScreen.kt
@@ -1,5 +1,7 @@
 package org.simple.clinic.editpatient
 
+import android.content.Context
+import android.os.Bundle
 import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
@@ -336,6 +338,18 @@ class EditPatientScreen : BaseScreen<
       layoutInflater: LayoutInflater,
       container: ViewGroup?
   ) = ScreenEditPatientBinding.inflate(layoutInflater, container, false)
+
+  override fun onAttach(context: Context) {
+    super.onAttach(context)
+    context.injector<Injector>().inject(this)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    if (features.isEnabled(VillageTypeAhead)) {
+      colonyOrVillageEditText.setAdapter(villageTypeAheadAdapter)
+    }
+  }
 
   override fun onFinishInflate() {
     super.onFinishInflate()

d3b1ee8c1cae54ea02ba3523a5c859a55389125f
Vinay <vinay@obvious.in>
2021-08-12 10:18:02 +0530

    Add script to generate a Heroku app name from the pull request reference

---
+++ .github/scripts/generate_heroku_app_name.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Extract the PR number from the GitHub ref ("refs/pull/<pr number>/merge")
+gitRef=$(cut -d"/" -f3 <<< ${1})
+echo "GiHub ref: ${gitRef}"
+
+herokuAppName="simple-android-review-app-$gitRef"
+echo "Heroku app name: ${herokuAppName}"
+
+echo "::set-output name=heroku_app_name::${herokuAppName}"

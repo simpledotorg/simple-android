45c576e9143633db0a588f8bbf45410da2e24532
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-08-17 10:43:54 +0530

    Create `RegistrationPhoneViewEffectHandler` to handle UI actions from view effects

---
+++ app/src/main/java/org/simple/clinic/registration/phone/RegistrationPhoneViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.registration.phone
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class RegistrationPhoneViewEffectHandler(
+    private val uiActions: RegistrationPhoneUiActions
+) : ViewEffectsHandler<RegistrationPhoneViewEffect> {
+
+  override fun handle(viewEffect: RegistrationPhoneViewEffect) {
+
+  }
+}

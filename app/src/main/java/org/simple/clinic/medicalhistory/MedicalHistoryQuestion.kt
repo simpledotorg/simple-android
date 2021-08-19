9fbcd8d3e682da70c7f4a53d04487e19f87e8652
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-08-19 11:34:58 +0530

    Rename `medicalhistory_question_diagnosed_with_hypertension` to `medicalhistory_diagnosis_hypertension`

--- app/src/main/java/org/simple/clinic/medicalhistory/MedicalHistoryQuestion.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/MedicalHistoryQuestion.kt
@@ -4,7 +4,7 @@ import androidx.annotation.StringRes
 import org.simple.clinic.R
 
 enum class MedicalHistoryQuestion(@StringRes val questionRes: Int) {
-  DIAGNOSED_WITH_HYPERTENSION(R.string.medicalhistory_question_diagnosed_with_hypertension),
+  DIAGNOSED_WITH_HYPERTENSION(R.string.medicalhistory_diagnosis_hypertension),
   HAS_HAD_A_HEART_ATTACK(R.string.medicalhistory_question_heartattack),
   HAS_HAD_A_STROKE(R.string.medicalhistory_question_stroke),
   HAS_HAD_A_KIDNEY_DISEASE(R.string.medicalhistory_question_kidney),

6a1254362a81f0cb3f0ef7544fd3a7a80218924f
Vinay <vinay@obvious.in>
2021-08-20 13:18:38 +0530

    Generate Heroku app name for release branches

--- .github/scripts/generate_heroku_app_name.sh
+++ .github/scripts/generate_heroku_app_name.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Extract the PR number from the GitHub ref ("refs/pull/<pr number>/merge")
-gitRef=$(cut -d"/" -f3 <<< ${1})
-echo "GiHub ref: ${gitRef}"
+# Extract the PR number from the GitHub ref ("refs/pull/<pr number>/merge"|"refs/heads/release/2021-08-20")
+echo "GitHub reference: ${1}"
 
-herokuAppName="simple-android-review-app-$gitRef"
+if [[ ${1} == *"/release/"* ]]; then
+  herokuAppName="simple-android-review-app-$(date +"%H%M")"
+else
+  herokuAppName="simple-android-review-app-$(cut -d"/" -f3 <<< ${1})"
+fi
+
 echo "Heroku app name: ${herokuAppName}"
 
 echo "::set-output name=heroku_app_name::${herokuAppName}"

0bfe8bbe004adf78243602f9450c91702bd04a1a
Honey Sonwani <honeysonwani88@gmail.com>
2021-08-23 17:25:21 +0530

    Add `ScreenKey` in `EnterOtpScreen`

--- app/src/main/java/org/simple/clinic/enterotp/EnterOtpScreen.kt
+++ app/src/main/java/org/simple/clinic/enterotp/EnterOtpScreen.kt
@@ -6,11 +6,14 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
 import androidx.transition.TransitionManager
 import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.widget.editorActions
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.simple.clinic.LOGIN_OTP_LENGTH
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
@@ -19,6 +22,8 @@ import org.simple.clinic.databinding.ScreenEnterotpBinding
 import org.simple.clinic.di.injector
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.navigation.v2.Router
+import org.simple.clinic.navigation.v2.ScreenKey
+import org.simple.clinic.router.screen.FullScreenKey
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.hideKeyboard
@@ -201,5 +206,12 @@ class EnterOtpScreen(
 
   interface Injector {
     fun inject(target: EnterOtpScreen)
+  }
+
+  @Parcelize
+  data class Key(
+      override val analyticsName = "Enter Login OTP Manually"
+  ) : ScreenKey() {
+    override fun instantiateFragment() = EnterOtpScreen()
   }
 }

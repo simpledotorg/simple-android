ef0a243c194d7ed09b43fca01d1022116805d127
Vinay <vinay@obvious.in>
2021-08-24 14:50:50 +0530

    Add Moshi type adapter for the `Outcome` type

--- app/src/main/java/org/simple/clinic/overdue/callresult/Outcome.kt
+++ app/src/main/java/org/simple/clinic/overdue/callresult/Outcome.kt
@@ -1,6 +1,8 @@
 package org.simple.clinic.overdue.callresult
 
 import androidx.room.TypeConverter
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
 import org.simple.clinic.util.room.SafeEnumTypeAdapter
 
 sealed class Outcome {
@@ -30,5 +32,14 @@ sealed class Outcome {
 
     @TypeConverter
     fun fromEnum(outcome: Outcome?): String? = TypeAdapter.fromEnum(outcome)
+  }
+  
+  class MoshiTypeAdapter {
+
+    @FromJson
+    fun fromJson(value: String?): Outcome? = TypeAdapter.toEnum(value)
+
+    @ToJson
+    fun toJson(outcome: Outcome?): String? = TypeAdapter.fromEnum(outcome)
   }
 }

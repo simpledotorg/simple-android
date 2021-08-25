4bbfde166355f03a02d4daa040b41dee82be83e6
Janhavi Singh <janhavi@obvious.in>
2021-08-25 20:03:27 +0530

    Add `DrugFrequencyProvider` interface

---
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyChoiceItem.kt
@@ -0,0 +1,5 @@
+package org.simple.clinic.drugs.selection.custom.drugfrequency.country
+
+import org.simple.clinic.drugs.search.DrugFrequency
+
+data class DrugFrequencyChoiceItem(val drugFrequency: DrugFrequency?, val labelResId: Int)

a01024e873b31086db1b178125078d02350ebbb7
Janhavi Singh <janhavi@obvious.in>
2021-08-26 17:14:36 +0530

    Add `EthiopiaDrugFrequencyProvider` which returns a list of Ethiopian acronyms for drug frequencies

---
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/EthiopiaDrugFrequencyProvider.kt
@@ -0,0 +1,19 @@
+package org.simple.clinic.drugs.selection.custom.drugfrequency.country
+
+import org.simple.clinic.R
+import org.simple.clinic.drugs.search.DrugFrequency.BD
+import org.simple.clinic.drugs.search.DrugFrequency.OD
+import org.simple.clinic.drugs.search.DrugFrequency.QDS
+import org.simple.clinic.drugs.search.DrugFrequency.TDS
+
+class EthiopiaDrugFrequencyProvider : DrugFrequencyProvider {
+  override fun provide(): List<DrugFrequencyChoiceItem> {
+    return listOf(
+        DrugFrequencyChoiceItem(drugFrequency = null, labelResId = R.string.custom_drug_entry_sheet_frequency_none),
+        DrugFrequencyChoiceItem(drugFrequency = OD, labelResId = R.string.custom_drug_entry_sheet_frequency_ethiopia_PD),
+        DrugFrequencyChoiceItem(drugFrequency = BD, labelResId = R.string.custom_drug_entry_sheet_frequency_ethiopia_BID),
+        DrugFrequencyChoiceItem(drugFrequency = TDS, labelResId = R.string.custom_drug_entry_sheet_frequency_ethiopia_TID),
+        DrugFrequencyChoiceItem(drugFrequency = QDS, labelResId = R.string.custom_drug_entry_sheet_frequency_ethiopia_QID)
+    )
+  }
+}

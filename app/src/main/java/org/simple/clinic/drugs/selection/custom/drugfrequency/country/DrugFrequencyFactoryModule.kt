d021fe9adf2cde0c6e47dc65f3dc404d25ffa099
Janhavi Singh <janhavi@obvious.in>
2021-08-27 10:33:48 +0530

    Return `CommonDrugFrequencyProvider` when the country is not `Ethiopia`
    `CommonDrugFrequencyProvider` will be the case for almost all countries, so we don't need to throw an error in case of it being a different country.

--- app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyFactoryModule.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyFactoryModule.kt
@@ -3,20 +3,16 @@ package org.simple.clinic.drugs.selection.custom.drugfrequency.country
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.appconfig.Country
-import org.simple.clinic.appconfig.Country.Companion.BANGLADESH
 import org.simple.clinic.appconfig.Country.Companion.ETHIOPIA
-import org.simple.clinic.appconfig.Country.Companion.INDIA
-import org.simple.clinic.appconfig.Country.Companion.SRI_LANKA
 
 @Module
 class DrugFrequencyFactoryModule {
 
   @Provides
   fun provideDrugFrequencyProvider(country: Country): DrugFrequencyProvider {
-    return when(val isoCountryCode = country.isoCountryCode){
+    return when (country.isoCountryCode) {
       ETHIOPIA -> EthiopiaDrugFrequencyProvider()
-      INDIA, BANGLADESH, SRI_LANKA -> CommonDrugFrequencyProvider()
-      else -> throw IllegalArgumentException("Unknown country code: $isoCountryCode")
+      else -> CommonDrugFrequencyProvider()
     }
   }
 }

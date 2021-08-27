71f8f4ee5ac661d673fa80fd118a220dc03638f3
Janhavi Singh <janhavi@obvious.in>
2021-08-27 10:28:07 +0530

    Add the `@StringRes` annotation to the `labelResId` property

--- app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyChoiceItem.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyChoiceItem.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.drugs.selection.custom.drugfrequency.country
 
+import androidx.annotation.StringRes
 import org.simple.clinic.drugs.search.DrugFrequency
 
-data class DrugFrequencyChoiceItem(val drugFrequency: DrugFrequency?, val labelResId: Int)
+data class DrugFrequencyChoiceItem(val drugFrequency: DrugFrequency?, @StringRes val labelResId: Int)

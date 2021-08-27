d829a7b4472f676e34d76743430ec62a961e4f7a
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-08-27 12:47:18 +0530

    Implement `ScheduleAppointmentViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/scheduleappointment/ScheduleAppointmentViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.scheduleappointment
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class ScheduleAppointmentViewEffectHandler(
+    private val uiActions: ScheduleAppointmentUiActions
+) : ViewEffectsHandler<ScheduleAppointmentViewEffect> {
+
+  override fun handle(viewEffect: ScheduleAppointmentViewEffect) {
+
+  }
+}

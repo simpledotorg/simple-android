f832b306e4f60d29c771a6aa6c4fdd31b500869e
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-08-30 08:43:06 +0530

    Provide retrofit instance for deployment

--- app/src/main/java/org/simple/clinic/di/network/RetrofitModule.kt
+++ app/src/main/java/org/simple/clinic/di/network/RetrofitModule.kt
@@ -7,6 +7,7 @@ import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import org.simple.clinic.BuildConfig
 import org.simple.clinic.appconfig.Country
+import org.simple.clinic.appconfig.Deployment
 import org.simple.clinic.di.AppScope
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -48,6 +49,24 @@ class RetrofitModule {
     // this is a safety check that will generate the right endpoint regardless of whether the
     // endpoint defined in the manifest has a trailing slash or not.
     val baseUrl = country.endpoint.toString().removeSuffix("/")
+    val endpoint = "$baseUrl/".toHttpUrl()
+
+    return commonRetrofitBuilder
+        .baseUrl(endpoint)
+        .build()
+  }
+
+  @Provides
+  @AppScope
+  @Named("for_deployment")
+  fun retrofitForDeployment(
+      deployment: Deployment,
+      commonRetrofitBuilder: Retrofit.Builder
+  ): Retrofit {
+    // Since the endpoint is not under our control, and is defined at the server level,
+    // this is a safety check that will generate the right endpoint regardless of whether the
+    // endpoint defined in the manifest has a trailing slash or not.
+    val baseUrl = deployment.endPoint.toString().removeSuffix("/")
     val endpoint = "$baseUrl/".toHttpUrl()
 
     return commonRetrofitBuilder

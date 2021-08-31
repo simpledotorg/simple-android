089f5cce290ee1c6ec3bb6be66cda5721aa96abf
Vinay <vinay@obvious.in>
2021-08-31 11:00:26 +0530

    Add DI provider for the call result sync API

--- app/src/main/java/org/simple/clinic/overdue/callresult/di/CallResultModule.kt
+++ app/src/main/java/org/simple/clinic/overdue/callresult/di/CallResultModule.kt
@@ -3,10 +3,18 @@ package org.simple.clinic.overdue.callresult.di
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.AppDatabase
+import org.simple.clinic.overdue.callresult.CallResultSyncApi
+import retrofit2.Retrofit
+import javax.inject.Named
 
 @Module
 class CallResultModule {
 
   @Provides
   fun provideRoomDao(appDatabase: AppDatabase) = appDatabase.callResultDao()
+
+  @Provides
+  fun provideApi(@Named("for_deployment") retrofit: Retrofit): CallResultSyncApi {
+    return retrofit.create(CallResultSyncApi::class.java)
+  }
 }

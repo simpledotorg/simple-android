30bcd14588eefec9a017be3b670a1f78a589db41
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-09-02 10:46:16 +0530

    Track selected state in `SelectStateModel`

--- app/src/main/java/org/simple/clinic/selectstate/SelectStateModel.kt
+++ app/src/main/java/org/simple/clinic/selectstate/SelectStateModel.kt
@@ -7,7 +7,8 @@ import org.simple.clinic.appconfig.State
 @Parcelize
 data class SelectStateModel(
     val states: List<State>?,
-    val statesFetchError: StatesFetchError?
+    val statesFetchError: StatesFetchError?,
+    val selectedState: State?
 ) : Parcelable {
 
   val hasStates
@@ -17,11 +18,14 @@ data class SelectStateModel(
 
     fun create() = SelectStateModel(
         states = null,
-        statesFetchError = null
+        statesFetchError = null,
+        selectedState = null
     )
   }
 
   fun statesLoaded(states: List<State>) = copy(states = states, statesFetchError = null)
 
   fun failedToLoadStates(statesFetchError: StatesFetchError) = copy(statesFetchError = statesFetchError, states = null)
+
+  fun stateChanged(state: State) = copy(selectedState = state)
 }

58fe865de81acc71abaecb954f6615e1cc3364d4
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-09-06 10:02:51 +0530

    Create `EditPatientViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/editpatient/EditPatientViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.editpatient
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class EditPatientViewEffectHandler(
+    private val ui: EditPatientUi
+) : ViewEffectsHandler<EditPatientViewEffect> {
+
+  override fun handle(viewEffect: EditPatientViewEffect) {
+    // no-op
+  }
+}

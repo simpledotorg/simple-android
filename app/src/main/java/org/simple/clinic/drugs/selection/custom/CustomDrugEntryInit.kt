f42762b149a94d6c5279835b39c2e730394feec2
Janhavi Singh <janhavi@obvious.in>
2021-09-06 15:52:55 +0530

    Refactor `CustomDrugEntryInit`

--- app/src/main/java/org/simple/clinic/drugs/selection/custom/CustomDrugEntryInit.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/CustomDrugEntryInit.kt
@@ -1,15 +1,27 @@
 package org.simple.clinic.drugs.selection.custom
 
 import com.spotify.mobius.First
+import com.spotify.mobius.First.first
 import com.spotify.mobius.Init
-import org.simple.clinic.mobius.first
+import org.simple.clinic.drugs.selection.custom.OpenAs.New.FromDrugList
+import org.simple.clinic.drugs.selection.custom.OpenAs.New.FromDrugName
+import org.simple.clinic.drugs.selection.custom.OpenAs.Update
 
 class CustomDrugEntryInit : Init<CustomDrugEntryModel, CustomDrugEntryEffect> {
   override fun init(model: CustomDrugEntryModel): First<CustomDrugEntryModel, CustomDrugEntryEffect> {
-    return when (model.openAs) {
-      is OpenAs.New.FromDrugList -> first(model.drugInfoProgressStateLoading(), FetchDrug(model.openAs.drugUuid), LoadDrugFrequencyChoiceItems)
-      is OpenAs.New.FromDrugName -> first(model.drugNameLoaded(model.openAs.drugName).drugInfoProgressStateLoaded(), LoadDrugFrequencyChoiceItems)
-      is OpenAs.Update -> first(model.drugInfoProgressStateLoading(), FetchPrescription(model.openAs.prescribedDrugUuid), LoadDrugFrequencyChoiceItems)
+    val updatedModel = when (model.openAs) {
+      is FromDrugList, is Update -> model.drugInfoProgressStateLoading()
+      is FromDrugName -> model.drugNameLoaded(model.openAs.drugName).drugInfoProgressStateLoaded()
     }
+
+    val effect = when (updatedModel.openAs) {
+      is FromDrugList -> FetchDrug(updatedModel.openAs.drugUuid)
+      is Update -> FetchPrescription(updatedModel.openAs.prescribedDrugUuid)
+      is FromDrugName -> null
+    }
+
+    val effects = mutableSetOf(LoadDrugFrequencyChoiceItems, effect)
+
+    return (first(updatedModel, effects))
   }
 }

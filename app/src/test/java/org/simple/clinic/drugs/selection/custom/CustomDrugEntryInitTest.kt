d6a1ebb5fa029f78cd3ba701b8b3244ce6458def
Janhavi Singh <58420800+janhavisinghh@users.noreply.github.com>
2021-09-06 16:08:57 +0530

    Mobius loop implementations to add a progress state in `CustomDrugEntrySheet` - 1 (#3028)
    * Add `CustomDrugEntryProgressState` enum
    
    * Add `customDrugEntryProgressState` to `CustomDrugEntryModel`
    
    * Set the screen to progress state when when sheet is created in create mode from the drug list
    
    * Set the screen to progress state when sheet is created in update mode
    
    * Set the screen to done state when sheet is created in create mode from the drug name
    
    * Update CHANGELOG
    
    * Rename `CustomDrugEntryProgressState` to `DrugInfoProgressState`
    
    * Rename the `customDrugEntryProgressState` methods to `drugInfoProgressState`

--- app/src/test/java/org/simple/clinic/drugs/selection/custom/CustomDrugEntryInitTest.kt
+++ app/src/test/java/org/simple/clinic/drugs/selection/custom/CustomDrugEntryInitTest.kt
@@ -13,7 +13,7 @@ class CustomDrugEntryInitTest {
   private val dosagePlaceholder = "mg"
 
   @Test
-  fun `when sheet is created in create mode from the drug list, fetch drug from the drugUuid and load drug frequency choice items`() {
+  fun `when sheet is created in create mode from the drug list, fetch drug from the drugUuid, load drug frequency choice items and set the screen in progress state`() {
     val drugUuid = UUID.fromString("6106544f-2b18-410d-992b-81860a08f02a")
     val model = CustomDrugEntryModel.default(openAs = OpenAs.New.FromDrugList(drugUuid), dosagePlaceholder)
 
@@ -21,28 +21,28 @@ class CustomDrugEntryInitTest {
         .whenInit(model)
         .then(
             assertThatFirst(
-                hasModel(model),
+                hasModel(model.drugInfoProgressStateLoading()),
                 hasEffects(FetchDrug(drugUuid), LoadDrugFrequencyChoiceItems)
             )
         )
   }
 
   @Test
-  fun `when sheet is created in create mode from a drug name, then update the drug name in the model and load drug frequency choice items`() {
+  fun `when sheet is created in create mode from a drug name, then update the drug name in the model, load drug frequency choice items and set the progress state to done`() {
     val model = CustomDrugEntryModel.default(openAs = OpenAs.New.FromDrugName(drugName), dosagePlaceholder)
 
     initSpec
         .whenInit(model)
         .then(
             assertThatFirst(
-                hasModel(model.drugNameLoaded(drugName)),
+                hasModel(model.drugNameLoaded(drugName).drugInfoProgressStateLoaded()),
                 hasEffects(LoadDrugFrequencyChoiceItems)
             )
         )
   }
 
   @Test
-  fun `when sheet is created in update mode, then fetch prescription and load drug frequency choice items`() {
+  fun `when sheet is created in update mode, then fetch prescription, load drug frequency choice items and set the screen to progress state`() {
     val prescribedDrugUuid = UUID.fromString("e046a649-dfc0-45b5-89d4-7a4b0af1c282")
     val model = CustomDrugEntryModel.default(openAs = OpenAs.Update(prescribedDrugUuid = prescribedDrugUuid), dosagePlaceholder)
 
@@ -50,6 +50,7 @@ class CustomDrugEntryInitTest {
         .whenInit(model)
         .then(
             assertThatFirst(
+                hasModel(model.drugInfoProgressStateLoading()),
                 hasEffects(FetchPrescription(prescribedDrugUuid), LoadDrugFrequencyChoiceItems)
             )
         )

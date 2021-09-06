199ebb67820cab97e23670fa31c8a0b40dc41fa3
Janhavi Singh <janhavi@obvious.in>
2021-09-06 18:41:57 +0530

    Instead of storing a frequency label string ID reference in `CommonDrugFrequencyProvider`, store the strings directly

--- app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/CommonDrugFrequencyProvider.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/CommonDrugFrequencyProvider.kt
@@ -2,16 +2,19 @@ package org.simple.clinic.drugs.selection.custom.drugfrequency.country
 
 import android.content.res.Resources
 import org.simple.clinic.R
-import org.simple.clinic.drugs.search.DrugFrequency
+import org.simple.clinic.drugs.search.DrugFrequency.BD
+import org.simple.clinic.drugs.search.DrugFrequency.OD
+import org.simple.clinic.drugs.search.DrugFrequency.QDS
+import org.simple.clinic.drugs.search.DrugFrequency.TDS
 
 class CommonDrugFrequencyProvider : DrugFrequencyProvider {
   override fun provide(resources: Resources): List<DrugFrequencyChoiceItem> {
     return listOf(
-        DrugFrequencyChoiceItem(drugFrequency = null, labelResId = R.string.custom_drug_entry_sheet_frequency_none),
-        DrugFrequencyChoiceItem(drugFrequency = DrugFrequency.OD, labelResId = R.string.custom_drug_entry_sheet_frequency_OD),
-        DrugFrequencyChoiceItem(drugFrequency = DrugFrequency.BD, labelResId = R.string.custom_drug_entry_sheet_frequency_BD),
-        DrugFrequencyChoiceItem(drugFrequency = DrugFrequency.TDS, labelResId = R.string.custom_drug_entry_sheet_frequency_TDS),
-        DrugFrequencyChoiceItem(drugFrequency = DrugFrequency.QDS, labelResId = R.string.custom_drug_entry_sheet_frequency_QDS)
+        DrugFrequencyChoiceItem(drugFrequency = null, label = resources.getString(R.string.custom_drug_entry_sheet_frequency_none)),
+        DrugFrequencyChoiceItem(drugFrequency = OD, label = resources.getString(R.string.custom_drug_entry_sheet_frequency_OD)),
+        DrugFrequencyChoiceItem(drugFrequency = BD, label = resources.getString(R.string.custom_drug_entry_sheet_frequency_BD)),
+        DrugFrequencyChoiceItem(drugFrequency = TDS, label = resources.getString(R.string.custom_drug_entry_sheet_frequency_TDS)),
+        DrugFrequencyChoiceItem(drugFrequency = QDS, label = resources.getString(R.string.custom_drug_entry_sheet_frequency_QDS))
     )
   }
 }

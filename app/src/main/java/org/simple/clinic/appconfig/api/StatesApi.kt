45f49cf51d8a240035378d0b806a779b62fb5295
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-09-08 09:35:50 +0530

    Change return type of states API to `Call<StatesPayload>`
    This is the new format for states API, you can look at sample JSON here: https://api-qa.simple.org/api/v4/states

--- app/src/main/java/org/simple/clinic/appconfig/api/StatesApi.kt
+++ app/src/main/java/org/simple/clinic/appconfig/api/StatesApi.kt
@@ -1,9 +1,11 @@
 package org.simple.clinic.appconfig.api
 
+import org.simple.clinic.appconfig.StatesPayload
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface StatesApi {
 
   @GET("v4/states")
-  fun fetchStates(): List<String>
+  fun fetchStates(): Call<StatesPayload>
 }

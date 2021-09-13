eafe6d960b907f99ba64dfa7d6aa5f3766ac2f27
Honey Sonwani <honeysonwani88@gmail.com>
2021-09-13 18:31:45 +0530

    Format `EnterOtpUiRenderer`

--- app/src/main/java/org/simple/clinic/enterotp/EnterOtpUiRenderer.kt
+++ app/src/main/java/org/simple/clinic/enterotp/EnterOtpUiRenderer.kt
@@ -41,7 +41,10 @@ class EnterOtpUiRenderer(
     when (model.protectedState) {
       is Allowed -> {
         ui.showOtpEntryMode(OtpEntry)
-        generateUiForAllowingOtpEntry(model.hasNoIncorrectPinEntries, model.hasReachedMinPinRetries, model.protectedState.attemptsRemaining)
+        generateUiForAllowingOtpEntry(
+            model.hasNoIncorrectPinEntries,
+            model.hasReachedMinPinRetries,
+            model.protectedState.attemptsRemaining)
       }
       is Blocked -> {
         ui.showOtpEntryMode(BruteForceOtpEntryLocked(model.protectedState.blockedTill))
@@ -50,7 +53,11 @@ class EnterOtpUiRenderer(
     }
   }
 
-  private fun generateUiForAllowingOtpEntry(hasNoIncorrectPinEntries: Boolean, hasReachedMinPinRetries: Boolean, attemptsRemaining: Int) {
+  private fun generateUiForAllowingOtpEntry(
+      hasNoIncorrectPinEntries: Boolean,
+      hasReachedMinPinRetries: Boolean,
+      attemptsRemaining: Int
+  ) {
     if (hasNoIncorrectPinEntries) {
       ui.hideError()
     } else {
@@ -58,7 +65,10 @@ class EnterOtpUiRenderer(
     }
   }
 
-  private fun showErrorWithFailedAttempts(hasReachedMinPinRetries: Boolean, attemptsRemaining: Int) {
+  private fun showErrorWithFailedAttempts(
+      hasReachedMinPinRetries: Boolean,
+      attemptsRemaining: Int
+  ) {
     if (hasReachedMinPinRetries) {
       ui.showFailedAttemptOtpError(attemptsRemaining = attemptsRemaining)
     } else {

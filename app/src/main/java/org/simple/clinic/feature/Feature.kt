297038377d167dba5d5d7dbdc27c580007db8fb3
Vinay Shenoy <vinay@obvious.in>
2021-09-13 13:27:52 +0530

    Add `CallResultSync` to the list of syncs based on a local feature flag (#3142)
    * Add local feature flag to track whether call result sync should be enabled
    
    * Conditionally add `CallResultSync` to the list of syncs
    
    * Conditionally add `CallResultRepository` to the list of frequently syncing repositories
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/feature/Feature.kt
+++ app/src/main/java/org/simple/clinic/feature/Feature.kt
@@ -32,5 +32,6 @@ enum class Feature(
   OverdueListDownloadAndShare(false, "overdue_list_download_and_share"),
   CustomDrugSearchScreen(false, "custom_drug_search_screen"),
   OnlinePatientLookup(true, "online_patient_lookup"),
-  HttpRequestBodyCompression(false, "http_request_body_compression_enabled")
+  HttpRequestBodyCompression(false, "http_request_body_compression_enabled"),
+  CallResultSyncEnabled(false)
 }

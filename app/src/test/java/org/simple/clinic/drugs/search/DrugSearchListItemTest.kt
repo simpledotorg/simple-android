1dfde1aed56bf0164aa086f579abfe6e57fc6eff
Janhavi Singh <58420800+janhavisinghh@users.noreply.github.com>
2021-09-21 18:50:06 +0530

    Implement providing drug frequencies label depending on the country in `DrugSearchScreen` (#3159)
    * Handle `LoadDrugFrequencyChoiceItems`
    
    * Add `drugFrequencyToFrequencyChoiceItemMap` in `DrugSearchModel`
    
    * when drug frequency choice items are loaded, then update the model with a map of frequency to frequency choice items
    
    * Add `drugFrequencyToFrequencyChoiceItemMap` parameter to `SetDrugsSearchResults` effect and ui action
    
    * when the screen is created, then load drug frequency choice items
    
    * Set drug frequency label using the `drugFrequencyToFrequencyChoiceItemMap` in drug search list items
    
    This will set the drug frequency label based on the country in drug search result items
    
    * Update CHANGELOG

--- app/src/test/java/org/simple/clinic/drugs/search/DrugSearchListItemTest.kt
+++ app/src/test/java/org/simple/clinic/drugs/search/DrugSearchListItemTest.kt
@@ -8,9 +8,18 @@ import org.simple.clinic.drugs.search.DrugSearchListItem.Divider
 import org.simple.clinic.drugs.search.DrugSearchListItem.DrugSearchResult
 import org.simple.clinic.drugs.search.DrugSearchListItem.NewCustomDrug
 import org.simple.clinic.drugs.search.DrugSearchListItem.TopCornerCapItem
+import org.simple.clinic.drugs.selection.custom.drugfrequency.country.DrugFrequencyChoiceItem
 import java.util.UUID
 
 class DrugSearchListItemTest {
+
+  private val drugFrequencyToFrequencyChoiceItemMap = mapOf(
+      null to DrugFrequencyChoiceItem(drugFrequency = null, label = "None"),
+      DrugFrequency.OD to DrugFrequencyChoiceItem(drugFrequency = DrugFrequency.OD, label = "OD"),
+      DrugFrequency.BD to DrugFrequencyChoiceItem(drugFrequency = DrugFrequency.BD, label = "BD"),
+      DrugFrequency.TDS to DrugFrequencyChoiceItem(drugFrequency = DrugFrequency.TDS, label = "TDS"),
+      DrugFrequency.QDS to DrugFrequencyChoiceItem(drugFrequency = DrugFrequency.QDS, label = "QDS")
+  )
 
   @Test
   fun `divider must be added in between drug search results`() {
@@ -19,13 +28,13 @@ class DrugSearchListItemTest {
         id = UUID.fromString("6fecac0f-fe87-4283-b6d9-7a69be1de5af"),
         name = "Amlodipine",
         dosage = "10 mg"
-    ))
+    ), drugFrequencyToFrequencyChoiceItemMap)
 
     val amlodipine20 = DrugSearchResult(TestData.drug(
         id = UUID.fromString("494e0237-0f42-4c61-9d62-77aaa68916af"),
         name = "Amlodipine",
         dosage = "20 mg"
-    ))
+    ), drugFrequencyToFrequencyChoiceItemMap)
 
     // when
     val divider = DrugSearchListItem.insertSeparators(
@@ -58,7 +67,7 @@ class DrugSearchListItemTest {
         id = UUID.fromString("4c998e87-0bb2-4875-9cfe-c1c3efb625c6"),
         name = "Amlodipine",
         dosage = "10 mg"
-    ))
+    ), drugFrequencyToFrequencyChoiceItemMap)
 
     // when
     val topCornerCapItem = DrugSearchListItem.insertSeparators(
@@ -78,7 +87,7 @@ class DrugSearchListItemTest {
         id = UUID.fromString("0871a787-049c-4f79-9726-61a30adbaeb4"),
         name = "Amlodipine",
         dosage = "10 mg"
-    ))
+    ), drugFrequencyToFrequencyChoiceItemMap)
 
     // when
     val bottomCornerCapItem = DrugSearchListItem.insertSeparators(

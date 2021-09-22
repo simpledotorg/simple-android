d80f80ee836486305839ee6ae2bbb3d7a6461fad
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-09-22 13:25:09 +0530

    Provide drug frequency to label map as part of dagger graph
    We are doing this to remove the `DrugFrequencyFactory` in later commits

--- app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyModule.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyModule.kt
@@ -1,9 +1,11 @@
 package org.simple.clinic.drugs.selection.custom.drugfrequency.country
 
+import android.content.res.Resources
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.appconfig.Country
 import org.simple.clinic.appconfig.Country.Companion.ETHIOPIA
+import org.simple.clinic.drugs.search.DrugFrequency
 
 @Module
 class DrugFrequencyModule {
@@ -14,5 +16,14 @@ class DrugFrequencyModule {
       ETHIOPIA -> EthiopiaDrugFrequencyProvider()
       else -> CommonDrugFrequencyProvider()
     }
+  }
+
+  @Provides
+  @JvmSuppressWildcards
+  fun provideDrugFrequencyToLabelMap(
+      provider: DrugFrequencyProvider,
+      resources: Resources
+  ): Map<DrugFrequency?, DrugFrequencyLabel> {
+    return provider.provide(resources)
   }
 }

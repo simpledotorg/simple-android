57220975e409606f6846e94d681194a92453e40f
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-09-22 12:20:17 +0530

    Change return type of `DrugFrequencyProvider#provide` to map of drug frequency and label

--- app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyProvider.kt
+++ app/src/main/java/org/simple/clinic/drugs/selection/custom/drugfrequency/country/DrugFrequencyProvider.kt
@@ -1,7 +1,8 @@
 package org.simple.clinic.drugs.selection.custom.drugfrequency.country
 
 import android.content.res.Resources
+import org.simple.clinic.drugs.search.DrugFrequency
 
 interface DrugFrequencyProvider {
-  fun provide(resources: Resources): List<DrugFrequencyChoiceItem>
+  fun provide(resources: Resources): Map<DrugFrequency?, DrugFrequencyLabel>
 }

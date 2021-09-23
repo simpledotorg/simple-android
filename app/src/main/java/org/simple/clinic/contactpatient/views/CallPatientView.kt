939ea15c5493da71afac883b208e52e93a97de85
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-09-23 15:17:20 +0530

    Fix contact patient bottom sheet not showing correct results (#3184)
    * Add `secureCallingGroup` to `patientWithPhoneNumberGroup` reference ids
    
    * Render patient with phone number results in `CallPatientView`
    
    We will remove the constraint layout group in later commits. Since, both phone number and no phone number call result group share views, there is a weird behaviour with state restoration. Please, refer [this](https://app.shortcut.com/simpledotorg/story/5156/fix-contactpatientbottomsheet-not-behaving-as-expected-when-navigated-back-from-remove-from-overdue-list-sheet) for more info.
    
    * Render patient with no phone number results in `CallPatientView`
    
    * Remove patient with phone number and without phone number groups from `contactpatient_callpatient`
    
    Since, both phone number and no phone number call result group share views, there is a weird behaviour with state restoration. Please, refer [this](https://app.shortcut.com/simpledotorg/story/5156/fix-contactpatientbottomsheet-not-behaving-as-expected-when-navigated-back-from-remove-from-overdue-list-sheet) for more info.
    
    * Format `contactpatient_callpatient`
    
    Updated visibilities so that resutls are shown properly in preview
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/contactpatient/views/CallPatientView.kt
+++ app/src/main/java/org/simple/clinic/contactpatient/views/CallPatientView.kt
@@ -35,8 +35,14 @@ class CallPatientView(
   private val agreedToVisitTextView
     get() = binding!!.agreedToVisitTextView
 
+  private val agreedToVisitSeparator
+    get() = binding!!.agreedToVisitSeparator
+
   private val remindToCallLaterTextView
     get() = binding!!.remindToCallLaterTextView
+
+  private val remindToCallSeparator
+    get() = binding!!.remindToCallLaterSeparator
 
   private val removeFromOverdueListTextView
     get() = binding!!.removeFromOverdueListTextView
@@ -80,12 +86,6 @@ class CallPatientView(
   private val lastVisitedLabel
     get() = binding!!.lastVisitedLabel
 
-  private val patientWithPhoneNumberResultsGroup
-    get() = binding!!.patientWithPhoneNumberResultsGroup
-
-  private val patientWithNoPhoneNumberResultsGroup
-    get() = binding!!.patientWithNoPhoneNumberResultGroup
-
   private val patientDiedStatusView
     get() = binding!!.patientDiedStatusView
 
@@ -122,13 +122,13 @@ class CallPatientView(
   var showPatientWithCallResultLayout: Boolean = false
     set(value) {
       field = value
-      patientWithPhoneNumberResultsGroup.visibility = if (field) View.VISIBLE else View.GONE
+      renderPatientWithPhoneNumberResults(field)
     }
 
   var showPatientWithNoPhoneNumberResults: Boolean = false
     set(value) {
       field = value
-      patientWithNoPhoneNumberResultsGroup.visibility = if (field) View.VISIBLE else View.GONE
+      renderPatientWithNoPhoneNumberResults(field)
     }
 
   var showPatientDiedStatus: Boolean = false
@@ -211,5 +211,23 @@ class CallPatientView(
         .map { (_, diagnosisTitle) -> diagnosisTitle }
         .ifEmpty { listOf(resources.getString(R.string.contactpatient_diagnosis_none)) }
         .joinToString()
+  }
+
+  private fun renderPatientWithPhoneNumberResults(isVisible: Boolean) {
+    resultOfCallLabelTextView.visibleOrGone(isVisible)
+    agreedToVisitTextView.visibleOrGone(isVisible)
+    agreedToVisitSeparator.visibleOrGone(isVisible)
+    remindToCallLaterTextView.visibleOrGone(isVisible)
+    remindToCallSeparator.visibleOrGone(isVisible)
+    removeFromOverdueListTextView.visibleOrGone(isVisible)
+  }
+
+  private fun renderPatientWithNoPhoneNumberResults(isVisible: Boolean) {
+    resultOfCallLabelTextView.visibleOrGone(isVisible)
+    agreedToVisitTextView.visibleOrGone(isVisible)
+    agreedToVisitSeparator.visibleOrGone(isVisible)
+    remindToCallLaterTextView.visibility = View.GONE
+    remindToCallSeparator.visibility = View.GONE
+    removeFromOverdueListTextView.visibleOrGone(isVisible)
   }
 }

0fb2d6e3c707e5930aeb8981ef92c2e4cb282aa0
honeysonwani <honey@obvious.in>
2021-10-13 15:38:17 +0530

    Create `EnterOtpViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/enterotp/EnterOtpViewEffectHandler.kt
@@ -0,0 +1,9 @@
+package org.simple.clinic.enterotp
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class EnterOtpViewEffectHandler : ViewEffectsHandler<EnterOtpViewEffect> {
+  override fun handle(viewEffect: EnterOtpViewEffect) {
+    // no-op
+  }
+}

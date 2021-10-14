a1454ea582d1fad67274f3f6523b67e6463dea98
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-10-14 10:04:35 +0530

    Implement `SelectCountryViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/selectcountry/SelectCountryViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.selectcountry
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class SelectCountryViewEffectHandler(
+    private val uiActions: UiActions
+) : ViewEffectsHandler<SelectCountryViewEffect> {
+
+  override fun handle(viewEffect: SelectCountryViewEffect) {
+    // no-op
+  }
+}

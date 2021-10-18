85539a3af1758d816f4e1ccd8066879643cb8199
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-10-18 08:48:08 +0530

    Migrate `RegistrationLoadingScreen` to a fragment (#3257)
    * Use `ViewSwitcher` as parent in `screen_registration_loading`
    
    * Add `ScreenKey` in `RegistrationLoadingScreen`
    
    * Use `RegistrationLoadingScreen.Key` to navigate to loading screen
    
    * Extend `BaseScreen` in `RegistrationLoadingScreen`
    
    * Use binding from base screen
    
    * Use screen key from base screen
    
    * Inject into `RegistrationLoadingScreen` in `onAttach`
    
    * Handle back clicks in `onViewCreated`
    
    * Override Mobius functions from base screen
    
    * Remove unused code
    
    * Use `requireActivity` instead of injected `Activity`
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoScreen.kt
+++ app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoScreen.kt
@@ -19,10 +19,9 @@ import org.simple.clinic.databinding.ScreenIntroVideoBinding
 import org.simple.clinic.di.injector
 import org.simple.clinic.navigation.v2.Router
 import org.simple.clinic.navigation.v2.ScreenKey
-import org.simple.clinic.navigation.v2.compat.wrap
 import org.simple.clinic.navigation.v2.fragments.BaseScreen
 import org.simple.clinic.platform.crash.CrashReporter
-import org.simple.clinic.registration.register.RegistrationLoadingScreenKey
+import org.simple.clinic.registration.register.RegistrationLoadingScreen
 import org.simple.clinic.simplevideo.SimpleVideo
 import org.simple.clinic.simplevideo.SimpleVideoConfig
 import org.simple.clinic.simplevideo.SimpleVideoConfig.Type.TrainingVideo
@@ -74,7 +73,7 @@ class IntroVideoScreen : BaseScreen<
   }
 
   override fun openHome() {
-    router.push(RegistrationLoadingScreenKey(screenKey.registrationEntry).wrap())
+    router.push(RegistrationLoadingScreen.Key(screenKey.registrationEntry))
   }
 
   private fun videoClicks(): Observable<IntroVideoEvent> {

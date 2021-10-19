9d299a5018d2ca12b12d09a87ebf6da38772b889
Vinay <vinay@obvious.in>
2021-10-19 10:15:30 +0530

    Remove tests for verifying capturing of flaky tests

--- app/src/androidTest/java/org/simple/clinic/CanaryInstrumentationTest.kt
+++ app/src/androidTest/java/org/simple/clinic/CanaryInstrumentationTest.kt
@@ -2,12 +2,10 @@ package org.simple.clinic
 
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.common.truth.Truth.assertThat
-import com.vinaysshenoy.quarantine.Quarantine
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.simple.clinic.util.Rules
-import kotlin.random.Random
 
 
 class CanaryInstrumentationTest {
@@ -20,19 +18,5 @@ class CanaryInstrumentationTest {
     val context = InstrumentationRegistry.getInstrumentation().targetContext
 
     assertThat(context).isNotNull()
-  }
-
-  @Test
-  fun test_that_30p_flaky_test_is_captured() {
-    if (Quarantine.isEnabled) {
-      assertThat(Random.nextFloat()).isGreaterThan(0.3F)
-    }
-  }
-
-  @Test
-  fun test_that_70p_flaky_test_is_captured() {
-    if (Quarantine.isEnabled) {
-      assertThat(Random.nextFloat()).isGreaterThan(0.7F)
-    }
   }
 }

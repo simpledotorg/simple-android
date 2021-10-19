130a03fdf508eb25ad3a8bbb8d84c5a12c94db0b
Vinay <vinay@obvious.in>
2021-10-19 10:26:48 +0530

    Disable capturing flaky tests in the unit test suite

--- app/src/sharedTest/java/org/simple/clinic/util/Rules.kt
+++ app/src/sharedTest/java/org/simple/clinic/util/Rules.kt
@@ -1,12 +1,10 @@
 package org.simple.clinic.util
 
-import com.vinaysshenoy.quarantine.QuarantineTestRule
 import org.junit.rules.RuleChain
 
 object Rules {
 
   fun global(): RuleChain = RuleChain
       .emptyRuleChain()
-      .around(QuarantineTestRule())
       .around(RxErrorsRule())
 }

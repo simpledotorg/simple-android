6ba203adf8cbe34a5cfb566c48c88b8c809ed7ca
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-10-28 09:14:33 +0530

    Remove unused code

--- app/src/main/java/org/simple/clinic/splash/SplashScreen.kt
+++ app/src/main/java/org/simple/clinic/splash/SplashScreen.kt
@@ -8,12 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.airbnb.lottie.LottieDrawable
 import kotlinx.parcelize.Parcelize
-import org.simple.clinic.R
 import org.simple.clinic.databinding.ScreenSplashBinding
 import org.simple.clinic.di.injector
 import org.simple.clinic.navigation.v2.Router
 import org.simple.clinic.navigation.v2.ScreenKey
-import org.simple.clinic.navigation.v2.compat.FullScreenKey
 import org.simple.clinic.navigation.v2.compat.wrap
 import org.simple.clinic.onboarding.OnboardingScreen.OnboardingScreenKey
 import javax.inject.Inject
@@ -59,28 +57,8 @@ class SplashScreen : Fragment() {
     binding = null
   }
 
-  override fun onFinishInflate() {
-    super.onFinishInflate()
-    if (isInEditMode) {
-      return
-    }
-  }
-
-  override fun onDetachedFromWindow() {
-    binding = null
-    super.onDetachedFromWindow()
-  }
-
   interface Injector {
     fun inject(target: SplashScreen)
-  }
-
-  @Parcelize
-  object SplashScreenKey : FullScreenKey {
-
-    override val analyticsName = "Splash Screen"
-
-    override fun layoutRes() = R.layout.screen_splash
   }
 
   @Parcelize

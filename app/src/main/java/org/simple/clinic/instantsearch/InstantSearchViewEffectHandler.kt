77fcb93898256fe46586918f5921e6cf1dd25780
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-10-28 10:40:25 +0530

    Implement `InstantSearchViewEffectHandler` to handle UI actions

---
+++ app/src/main/java/org/simple/clinic/instantsearch/InstantSearchViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.instantsearch
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class InstantSearchViewEffectHandler(
+    private val uiActions: InstantSearchUiActions
+) : ViewEffectsHandler<InstantSearchViewEffect> {
+
+  override fun handle(viewEffect: InstantSearchViewEffect) {
+    // nothing to do here yet
+  }
+}

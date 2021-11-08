8bf1cdb67ed7e4ab18cee3e5dc830309e71de41a
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-11-08 15:18:00 +0530

    Return early if the entered phone number for the user matches demo phone number

--- app/src/main/java/org/simple/clinic/Globals.kt
+++ app/src/main/java/org/simple/clinic/Globals.kt
@@ -1,7 +1,11 @@
 package org.simple.clinic
 
+import java.util.UUID
+
 const val MAX_ALLOWED_PATIENT_AGE: Int = 120
 const val MIN_ALLOWED_PATIENT_AGE: Int = 1
 const val SECURITY_PIN_LENGTH: Int = 4
 const val LOGIN_OTP_LENGTH = 6
 const val INDIA_NHID_LENGTH = 14
+const val DEMO_USER_PHONE_NUMBER = "0000000000"
+val DEMO_USER_ID = UUID.fromString("82646d4c-c7ac-4402-b765-2af0575f3ef3")

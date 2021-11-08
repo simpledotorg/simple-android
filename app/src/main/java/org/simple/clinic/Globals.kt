0a9799aa3c0fb0cdf0b66023c92c15ed9271ca69
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-11-08 15:46:45 +0530

    Create demo user data in `LoginPinServerVerificationMethod`

--- app/src/main/java/org/simple/clinic/Globals.kt
+++ app/src/main/java/org/simple/clinic/Globals.kt
@@ -8,4 +8,6 @@ const val SECURITY_PIN_LENGTH: Int = 4
 const val LOGIN_OTP_LENGTH = 6
 const val INDIA_NHID_LENGTH = 14
 const val DEMO_USER_PHONE_NUMBER = "0000000000"
+const val DEMO_USER_PIN = "0000"
 val DEMO_USER_ID = UUID.fromString("82646d4c-c7ac-4402-b765-2af0575f3ef3")
+val DEMO_FACILITY_ID = UUID.fromString("6efec9bf-3fdc-4893-8170-4d6699df2150")
--- app/src/main/java/org/simple/clinic/security/pin/verification/LoginPinServerVerificationMethod.kt
+++ app/src/main/java/org/simple/clinic/security/pin/verification/LoginPinServerVerificationMethod.kt
@@ -1,5 +1,9 @@
 package org.simple.clinic.security.pin.verification
 
+import org.simple.clinic.DEMO_FACILITY_ID
+import org.simple.clinic.DEMO_USER_ID
+import org.simple.clinic.DEMO_USER_PHONE_NUMBER
+import org.simple.clinic.DEMO_USER_PIN
 import org.simple.clinic.login.activateuser.ActivateUser
 import org.simple.clinic.login.activateuser.ActivateUser.Result
 import org.simple.clinic.security.pin.verification.PinVerificationMethod.VerificationResult
@@ -21,6 +25,15 @@ class LoginPinServerVerificationMethod @Inject constructor(
   override fun verify(pin: String): VerificationResult {
     val entry = repository.entryImmediate()
 
+    if (entry.phoneNumber == DEMO_USER_PHONE_NUMBER && pin == DEMO_USER_PIN) {
+      /**
+       * IMPORTANT: We are using this just for Google Play review team,
+       * this ensures we are allowing them to test locally without linking
+       * to server which has actual PHI.
+       */
+      return Correct(demoUserData(pin))
+    }
+
     return when (val result = activateUser.activate(entry.uuid, pin)) {
       is Result.Success -> Correct(UserData.create(pin, result.userPayload))
       Result.IncorrectPin -> Incorrect(pin)
@@ -29,6 +42,22 @@ class LoginPinServerVerificationMethod @Inject constructor(
       is Result.OtherError -> VerificationResult.OtherError(result.cause)
     }
   }
+
+  private fun demoUserData(pin: String) = UserData.create(
+      pin = pin,
+      payload = LoggedInUserPayload(
+          uuid = DEMO_USER_ID,
+          fullName = "Demo User",
+          phoneNumber = DEMO_USER_PHONE_NUMBER,
+          pinDigest = "pin-digest",
+          registrationFacilityId = DEMO_FACILITY_ID,
+          status = UserStatus.WaitingForApproval,
+          createdAt = Instant.parse("2018-01-01T00:00:00Z"),
+          updatedAt = Instant.parse("2018-01-01T00:00:00Z"),
+          teleconsultPhoneNumber = null,
+          capabilities = User.Capabilities(canTeleconsult = User.CapabilityStatus.No)
+      )
+  )
 
   data class UserData(
       val pin: String,

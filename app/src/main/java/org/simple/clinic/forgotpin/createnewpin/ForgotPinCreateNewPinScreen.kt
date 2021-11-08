6f0b7839e7dd6c68df78ae8b52ddb689a36daba4
Honey <honey@obvious.in>
2021-11-08 13:19:32 +0530

    Use screenKey to `ForgotPinCreateNewPinScreen`

--- app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
@@ -6,15 +6,18 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
 import com.jakewharton.rxbinding3.widget.editorActions
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.ofType
+import kotlinx.parcelize.Parcelize
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.databinding.ScreenForgotpinCreatepinBinding
 import org.simple.clinic.di.injector
 import org.simple.clinic.forgotpin.confirmpin.ForgotPinConfirmPinScreen
 import org.simple.clinic.mobius.MobiusDelegate
 import org.simple.clinic.navigation.v2.Router
+import org.simple.clinic.navigation.v2.ScreenKey
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.hideKeyboard
 import org.simple.clinic.widgets.showKeyboard
@@ -132,5 +135,12 @@ class ForgotPinCreateNewPinScreen(
 
   interface Injector {
     fun inject(target: ForgotPinCreateNewPinScreen)
+  }
+
+  @Parcelize
+  data class Key(
+      override val analyticsName: String = "Forgot PIN Create New PIN"
+  ) : ScreenKey() {
+    override fun instantiateFragment(): Fragment = ForgotPinCreateNewPinScreen()
   }
 }

46d74d49f800b8af6eed7522877d5511cf884619
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-11-08 10:24:31 +0530

    Extract UI actions into `PatientEntryUiActions` from `PatientEntryUi`

--- app/src/main/java/org/simple/clinic/newentry/PatientEntryUiActions.kt
+++ app/src/main/java/org/simple/clinic/newentry/PatientEntryUiActions.kt
@@ -1,3 +1,13 @@
 package org.simple.clinic.newentry
 
-interface PatientEntryUiActions
+import org.simple.clinic.newentry.country.InputFields
+import org.simple.clinic.patient.OngoingNewPatientEntry
+
+interface PatientEntryUiActions {
+  fun prefillFields(entry: OngoingNewPatientEntry)
+  fun scrollToFirstFieldWithError()
+  fun scrollFormOnGenderSelection()
+  fun setShowDatePatternInDateOfBirthLabel(showPattern: Boolean)
+  fun openMedicalHistoryEntryScreen()
+  fun setupUi(inputFields: InputFields)
+}

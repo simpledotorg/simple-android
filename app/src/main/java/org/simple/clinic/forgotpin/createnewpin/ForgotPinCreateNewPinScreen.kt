160eac1c6ea743242f37e6de0dd351f6ee50b2ca
Honey <honey@obvious.in>
2021-11-08 14:49:15 +0530

    Remove unused code

--- app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
+++ app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewPinScreen.kt
@@ -2,33 +2,23 @@ package org.simple.clinic.forgotpin.createnewpin
 
 import android.content.Context
 import android.os.Bundle
-import android.os.Parcelable
-import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
 import com.jakewharton.rxbinding3.widget.editorActions
-import com.spotify.mobius.Init
-import com.spotify.mobius.Update
 import com.spotify.mobius.functions.Consumer
 import io.reactivex.Observable
-import io.reactivex.ObservableTransformer
 import io.reactivex.rxkotlin.cast
-import io.reactivex.rxkotlin.ofType
 import kotlinx.parcelize.Parcelize
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.databinding.ScreenForgotpinCreatepinBinding
 import org.simple.clinic.di.injector
 import org.simple.clinic.forgotpin.confirmpin.ForgotPinConfirmPinScreen
-import org.simple.clinic.mobius.MobiusDelegate
-import org.simple.clinic.mobius.ViewRenderer
 import org.simple.clinic.navigation.v2.Router
 import org.simple.clinic.navigation.v2.ScreenKey
 import org.simple.clinic.navigation.v2.fragments.BaseScreen
-import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.hideKeyboard
 import org.simple.clinic.widgets.showKeyboard
 import javax.inject.Inject
@@ -62,28 +52,6 @@ class ForgotPinCreateNewPinScreen : BaseScreen<
   private val appLockRoot
     get() = binding.applockRoot
 
-  private val events by unsafeLazy {
-    Observable
-        .merge(
-            pinTextChanges(),
-            pinSubmitClicked()
-        )
-        .compose(ReportAnalyticsEvents())
-  }
-
-  private val delegate by unsafeLazy {
-    val uiRenderer = ForgotPinCreateNewUiRenderer(this)
-
-    MobiusDelegate.forView(
-        events = events.ofType(),
-        defaultModel = ForgotPinCreateNewModel.create(),
-        update = ForgotPinCreateNewUpdate(),
-        effectHandler = effectHandlerFactory.create(this).build(),
-        init = ForgotPinCreateNewInit(),
-        modelUpdateListener = uiRenderer::render
-    )
-  }
-  
   override fun defaultModel() = ForgotPinCreateNewModel.create()
 
   override fun bindView(layoutInflater: LayoutInflater, container: ViewGroup?) =
@@ -115,38 +83,6 @@ class ForgotPinCreateNewPinScreen : BaseScreen<
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     createPinEditText.showKeyboard()
-  }
-
-  override fun onFinishInflate() {
-    super.onFinishInflate()
-    if (isInEditMode) {
-      return
-    }
-
-    binding = ScreenForgotpinCreatepinBinding.bind(this)
-
-    context.injector<Injector>().inject(this)
-
-    createPinEditText.showKeyboard()
-  }
-
-  override fun onAttachedToWindow() {
-    super.onAttachedToWindow()
-    delegate.start()
-  }
-
-  override fun onDetachedFromWindow() {
-    delegate.stop()
-    binding = null
-    super.onDetachedFromWindow()
-  }
-
-  override fun onSaveInstanceState(): Parcelable {
-    return delegate.onSaveInstanceState(super.onSaveInstanceState())
-  }
-
-  override fun onRestoreInstanceState(state: Parcelable?) {
-    super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
 
   private fun pinTextChanges() =

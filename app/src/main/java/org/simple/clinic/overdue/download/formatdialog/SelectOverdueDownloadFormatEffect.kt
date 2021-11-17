c77b78c6bc4889fd3be704a81a43ea0a024ed605
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-11-17 14:39:28 +0530

    Add `SelectOverdueDownloadFormatViewEffect` as subtype of `SelectOverdueDownloadFormatEffect`

--- app/src/main/java/org/simple/clinic/overdue/download/formatdialog/SelectOverdueDownloadFormatEffect.kt
+++ app/src/main/java/org/simple/clinic/overdue/download/formatdialog/SelectOverdueDownloadFormatEffect.kt
@@ -5,3 +5,5 @@ import org.simple.clinic.overdue.download.OverdueListDownloadFormat
 sealed class SelectOverdueDownloadFormatEffect
 
 data class DownloadForShare(val downloadFormat: OverdueListDownloadFormat) : SelectOverdueDownloadFormatEffect()
+
+sealed class SelectOverdueDownloadFormatViewEffect : SelectOverdueDownloadFormatEffect()

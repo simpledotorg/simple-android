4b4d53b02cf92d617c22ebe4537183c27bd09e32
Vinay <vinay@obvious.in>
2021-11-24 10:25:45 +0530

    Add `SentryInfrastructure` to add user metadata to Sentry reports

---
+++ app/src/main/java/org/simple/clinic/plumbing/infrastructure/SentryInfrastructure.kt
@@ -0,0 +1,16 @@
+package org.simple.clinic.plumbing.infrastructure
+
+import io.sentry.Sentry
+import org.simple.clinic.appconfig.Country
+import org.simple.clinic.appconfig.Deployment
+import org.simple.clinic.user.User
+
+class SentryInfrastructure: Infrastructure {
+
+  override fun addDetails(user: User, country: Country, deployment: Deployment) {
+    Sentry.setTag("userUuid", user.uuid.toString())
+    Sentry.setTag("facilityUuid", user.currentFacilityUuid.toString())
+    Sentry.setTag("countryCode", country.isoCountryCode)
+    Sentry.setTag("deployment", deployment.endPoint.toString())
+  }
+}

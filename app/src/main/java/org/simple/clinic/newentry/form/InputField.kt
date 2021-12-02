bf89609bf78ce92b4c240d7cb0327fba7ecdfc77
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-12-02 13:12:39 +0530

    Change `InputField` as `sealed class`
    This allow us to annotate the child classes with `Parcelize` and place the list of `InputField`s in a UI model.

--- app/src/main/java/org/simple/clinic/newentry/form/InputField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/InputField.kt
@@ -2,6 +2,6 @@ package org.simple.clinic.newentry.form
 
 import androidx.annotation.StringRes
 
-abstract class InputField<T>(@StringRes val labelResId: Int) {
+sealed class InputField<T>(@StringRes val labelResId: Int) {
   abstract fun validate(value: T): Set<ValidationError>
 }

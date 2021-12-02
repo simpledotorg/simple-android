5c0dffe5ab22300b55bf192cab298d25b445bba9
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-12-02 13:24:08 +0530

    Make `InputField`s parcelable

--- app/src/main/java/org/simple/clinic/newentry/form/DateOfBirthField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/DateOfBirthField.kt
@@ -1,11 +1,13 @@
 package org.simple.clinic.newentry.form
 
+import kotlinx.parcelize.Parcelize
 import org.simple.clinic.newentry.form.ValidationError.DateIsInFuture
 import org.simple.clinic.newentry.form.ValidationError.InvalidDateFormat
 import org.simple.clinic.newentry.form.ValidationError.MissingValue
 import java.time.LocalDate
 import java.time.format.DateTimeParseException
 
+@Parcelize
 data class DateOfBirthField(
     private val parseDate: (String) -> LocalDate,
     private val today: LocalDate,
--- app/src/main/java/org/simple/clinic/newentry/form/PatientNameField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/PatientNameField.kt
@@ -1,7 +1,9 @@
 package org.simple.clinic.newentry.form
 
+import kotlinx.parcelize.Parcelize
 import org.simple.clinic.newentry.form.ValidationError.MissingValue
 
+@Parcelize
 data class PatientNameField(
     private val _labelResId: Int
 ) : InputField<String>(_labelResId) {

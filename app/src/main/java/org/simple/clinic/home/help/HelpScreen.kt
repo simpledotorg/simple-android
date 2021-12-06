a48ad1f990bc2ada634cd65004b84f6adb1d67d5
Honey Sonwani <honey@obvious.in>
2021-12-06 14:50:29 +0530

    Migrate `HelpScreen` to a Fragment (#3334)
    * Add screenKey in the `HelpScreen`
    
    * Update root layout to `LinearLayout`
    
    * Extend `BaseScreen` in `HelpScreen`
    
    * Override Mobius functions in `HelpScreen`
    
    * Use binding from `BaseScreen`
    
    * Use lifecycle method OnAttach to inject the screen
    
    * Use lifecycle method OnViewCreated to handle listeners
    
    * Open `HelpScreen#Key` from `HomeScreen`
    
    * Remove unused code
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/home/help/HelpScreen.kt
+++ app/src/main/java/org/simple/clinic/home/help/HelpScreen.kt
@@ -2,25 +2,33 @@ package org.simple.clinic.home.help
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Parcelable
-import android.util.AttributeSet
-import android.widget.LinearLayout
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.jakewharton.rxbinding3.view.clicks
-import io.reactivex.rxkotlin.ofType
+import com.spotify.mobius.functions.Consumer
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.cast
+import kotlinx.parcelize.Parcelize
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.databinding.ScreenHelpBinding
 import org.simple.clinic.di.injector
-import org.simple.clinic.mobius.MobiusDelegate
+import org.simple.clinic.home.HomeScreen
 import org.simple.clinic.navigation.v2.Router
-import org.simple.clinic.util.unsafeLazy
+import org.simple.clinic.navigation.v2.ScreenKey
+import org.simple.clinic.navigation.v2.fragments.BaseScreen
 import org.simple.clinic.widgets.visibleOrGone
 import javax.inject.Inject
 
-class HelpScreen(
-    context: Context,
-    attrs: AttributeSet
-) : LinearLayout(context, attrs), HelpScreenUi, HelpScreenUiActions {
+class HelpScreen : BaseScreen<
+    HelpScreen.Key,
+    ScreenHelpBinding,
+    HelpScreenModel,
+    HelpScreenEvent,
+    HelpScreenEffect,
+    Unit>(), HelpScreenUi, HelpScreenUiActions {
 
   @Inject
   lateinit var router: Router
@@ -28,79 +36,56 @@ class HelpScreen(
   @Inject
   lateinit var effectHandlerFactory: HelpScreenEffectHandler.Factory
 
-  private var binding: ScreenHelpBinding? = null
-
   private val toolbar
-    get() = binding!!.toolbar
+    get() = binding.toolbar
 
   private val webView
-    get() = binding!!.webView
+    get() = binding.webView
 
   private val tryAgainButton
-    get() = binding!!.tryAgainButton
+    get() = binding.tryAgainButton
 
   private val progressBar
-    get() = binding!!.progressBar
+    get() = binding.progressBar
 
   private val noContentView
-    get() = binding!!.noContentView
+    get() = binding.noContentView
 
   private val errorMessageTextView
-    get() = binding!!.errorMessageTextView
+    get() = binding.errorMessageTextView
 
-  private val events by unsafeLazy {
-    tryAgainClicks()
-        .compose(ReportAnalyticsEvents())
-  }
-
-  private val delegate by unsafeLazy {
-    val uiRenderer = HelpScreenUiRenderer(this)
-
-    MobiusDelegate.forView(
-        events = events.ofType(),
-        defaultModel = HelpScreenModel.create(),
-        init = HelpScreenInit(),
-        update = HelpScreenUpdate(),
-        effectHandler = effectHandlerFactory.create(this).build(),
-        modelUpdateListener = uiRenderer::render
-    )
-  }
-
-  override fun onAttachedToWindow() {
-    super.onAttachedToWindow()
-    delegate.start()
-  }
-
-  override fun onDetachedFromWindow() {
-    delegate.stop()
-    binding = null
-    super.onDetachedFromWindow()
-  }
-
-  override fun onSaveInstanceState(): Parcelable? {
-    return delegate.onSaveInstanceState(super.onSaveInstanceState())
-  }
-
-  override fun onRestoreInstanceState(state: Parcelable?) {
-    super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
+  override fun onAttach(context: Context) {
+    super.onAttach(context)
+    context.injector<Injector>().inject(this)
   }
 
   @SuppressLint("SetJavaScriptEnabled")
-  override fun onFinishInflate() {
-    super.onFinishInflate()
-
-    if (isInEditMode) {
-      return
-    }
-
-    binding = ScreenHelpBinding.bind(this)
-
-    context.injector<Injector>().inject(this)
-
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
     toolbar.setNavigationOnClickListener { router.pop() }
 
     webView.settings.javaScriptEnabled = true
   }
+
+  override fun defaultModel() = HelpScreenModel.create()
+
+  override fun bindView(layoutInflater: LayoutInflater, container: ViewGroup?) = ScreenHelpBinding
+      .inflate(layoutInflater, container, false)
+
+  override fun createEffectHandler(viewEffectsConsumer: Consumer<Unit>) =
+      effectHandlerFactory.create(this).build()
+
+  override fun createInit() = HelpScreenInit()
+
+  override fun createUpdate() = HelpScreenUpdate()
+
+  override fun events(): Observable<HelpScreenEvent> {
+    return tryAgainClicks()
+        .compose(ReportAnalyticsEvents())
+        .cast<HelpScreenEvent>()
+  }
+
+  override fun uiRenderer() = HelpScreenUiRenderer(this)
 
   private fun tryAgainClicks() = tryAgainButton
       .clicks()
@@ -135,5 +120,13 @@ class HelpScreen(
 
   interface Injector {
     fun inject(target: HelpScreen)
+  }
+
+  @Parcelize
+  data class Key(
+      override val analyticsName: String = "Help"
+  ) : ScreenKey() {
+
+    override fun instantiateFragment() = HomeScreen()
   }
 }

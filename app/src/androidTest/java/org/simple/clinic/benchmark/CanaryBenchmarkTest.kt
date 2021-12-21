1db062a60699e0481a603342b5190bfb16a0a7d4
Vinay <vinay@obvious.in>
2021-12-21 12:29:56 +0530

    Add base class for representing a benchmark test

--- app/src/androidTest/java/org/simple/clinic/benchmark/CanaryBenchmarkTest.kt
+++ app/src/androidTest/java/org/simple/clinic/benchmark/CanaryBenchmarkTest.kt
@@ -1,13 +1,8 @@
 package org.simple.clinic.benchmark
 
-import org.junit.Rule
 import org.junit.Test
 
-@BenchmarkTest
-class CanaryBenchmarkTest {
-
-  @get:Rule
-  val rule = BenchmarkTestRule()
+class CanaryBenchmarkTest : BaseBenchmarkTest() {
 
   @Test
   fun benchmark_test_timings_should_be_tracked() {

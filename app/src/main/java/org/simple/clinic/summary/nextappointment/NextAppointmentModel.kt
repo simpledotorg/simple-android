a0d623cf64a2152720be62a97073387086e626e0
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-12-30 17:34:37 +0530

    When next appointment card is created, load `NextAppointmentPatientProfile`

---
+++ app/src/main/java/org/simple/clinic/summary/nextappointment/NextAppointmentModel.kt
@@ -0,0 +1,24 @@
+package org.simple.clinic.summary.nextappointment
+
+import kotlinx.parcelize.Parcelize
+import java.util.UUID
+
+data class NextAppointmentModel(
+    val patientUuid: UUID,
+    val nextAppointmentPatientProfile: NextAppointmentPatientProfile?
+) {
+
+  companion object {
+    fun default(patientUuid: UUID) = NextAppointmentModel(
+        patientUuid = patientUuid,
+        nextAppointmentPatientProfile = null
+    )
+  }
+
+  val hasNextAppointmentPatientProfile
+    get() = nextAppointmentPatientProfile != null
+
+  fun nextAppointmentPatientProfileLoaded(nextAppointmentPatientProfile: NextAppointmentPatientProfile?): NextAppointmentModel {
+    return copy(nextAppointmentPatientProfile = nextAppointmentPatientProfile)
+  }
+}

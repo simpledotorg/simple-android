8334f42db6bf9b3a2cf1d07933fe9c9e70ed866a
Sasikanth Miriyampalli <sasikanth@obvious.in>
2021-12-30 17:09:59 +0530

    When there is no next appointment patient profile, then show add button and no appointment text

---
+++ app/src/main/java/org/simple/clinic/summary/nextappointment/NextAppointmentUi.kt
@@ -0,0 +1,6 @@
+package org.simple.clinic.summary.nextappointment
+
+interface NextAppointmentUi {
+  fun showNoAppointment()
+  fun showAddAppointmentButton()
+}
---
+++ app/src/test/java/org/simple/clinic/summary/nextappointment/NextAppointmentUiRendererTest.kt
@@ -0,0 +1,28 @@
+package org.simple.clinic.summary.nextappointment
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.junit.Test
+import java.util.UUID
+
+class NextAppointmentUiRendererTest {
+
+  @Test
+  fun `when next appointment patient profile is not present, then render no appointment view`() {
+    // given
+    val ui = mock<NextAppointmentUi>()
+    val uiRenderer = NextAppointmentUiRenderer(ui)
+    val defaultModel = NextAppointmentModel.default(
+        patientUuid = UUID.fromString("305c6c33-90b5-4895-9f82-e6d071d05955")
+    )
+
+    // when
+    uiRenderer.render(defaultModel)
+
+    // then
+    verify(ui).showNoAppointment()
+    verify(ui).showAddAppointmentButton()
+    verifyNoMoreInteractions(ui)
+  }
+}

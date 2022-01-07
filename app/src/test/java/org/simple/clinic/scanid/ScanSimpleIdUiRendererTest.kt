eb5773f53b2aafcf870f6fef3a3ed4c74177b925
Honey Sonwani <honey@obvious.in>
2022-01-07 10:28:50 +0530

    Adding support for scanning Identifier for `EditPatientScreen` (#3373)
    * Add `OpenedFrom.EditPatientScreen.ToAddNHID` in `OpenedFrom` sealed class
    
    * Add `openedFrom` to `ScanSimpleIdModel`
    
    * Add if `ScanSimpleIdScreen` is opened from `EditPatientScreen` in the model
    
    * When `ScanSimpleIdScreen` is opened from `EditPatientScreen`, then hide entered code container view
    
    * When `ScanSimpleIdScreenQrCodeScanned` and screen is opened from `EditPatientScreen` to add NHID, then parse the json into object
    
    * Handle `GoBackToEditPatientScreen` effect
    
    * When patient is not found, screen is opened from edit patient and online patient lookup is disabled, then go back to edit patient screen
    
    * When online patient lookup is completed, screen is opened from edit patient and patient is not found, then go back to edit patient screen
    
    * Add `OpenedFrom.EditPatientScreen.ToAddBpPassport` in the `OpenedFrom` sealed class
    
    * When `ScanSimpleIdScreenQrCodeScanned` and screen is opened from `EditPatientScreen` to add Bp passport, then search patient by identifier
    
    * Refactor and extract `searchPatientByBpPassport`
    
    * Check if opened from Bp passport button from `EditPatientScreen`, go back to `EditPatientScreen`
    
    * Add tests for when online lookup is completed and adding bp passport from `EditPatientScreen`, then go back to `EditPatientScreen`
    
    * Add `IdentifierAlreadyExists` in `ScanErrorState`
    
    * Handle `ShowScannedQrCodeError` effect
    
    * When screen is opened from edit patient and patient with identifier already exists, show scanned qr code error
    
    * When screen is opened from edit patient and online patient lookup is completed and had found medical records, show patient with identifier exists error
    
    * When scanned qr code is invalid and screen is opened from edit patient screen, then show scanned qr code error
    
    * Implement `showInvalidQrCodeError` snackbar
    
    * Implement `showPatientWithIdentifierExistsError` snackbar
    
    * Simplify `ScanSimpleIdUpdate#scannedQrCode` method and handle showing of invalid qr code error properly
    
    * Add `ScanSimpleIdUpdate#patientIsFoundWhileSearchingFromOnlineLookup` to fix deeply nested control statement
    
    * Refactor `ScanSimpleIdUpdate` to use `isOpenedFromEditPatientScreenToAddBpPassport` and `isOpenedFromEditPatientScreenToAddNhid` from `ScanSimpleIdModel`
    
    Co-authored-by: Janhavi Singh <janhavi@obvious.in>

--- app/src/test/java/org/simple/clinic/scanid/ScanSimpleIdUiRendererTest.kt
+++ app/src/test/java/org/simple/clinic/scanid/ScanSimpleIdUiRendererTest.kt
@@ -13,7 +13,7 @@ class ScanSimpleIdUiRendererTest {
   @Test
   fun `when searching for a patient, then show search progress`() {
     // given
-    val searchingModel = ScanSimpleIdModel.create()
+    val searchingModel = ScanSimpleIdModel.create(OpenedFrom.PatientsTabScreen)
         .searching()
 
     // when
@@ -28,7 +28,7 @@ class ScanSimpleIdUiRendererTest {
   @Test
   fun `when searching for patient is completed, then hide search progress`() {
     // given
-    val notSearchingModel = ScanSimpleIdModel.create()
+    val notSearchingModel = ScanSimpleIdModel.create(OpenedFrom.PatientsTabScreen)
         .notSearching()
 
     // when
@@ -43,7 +43,7 @@ class ScanSimpleIdUiRendererTest {
   @Test
   fun `when there is no scan error, then hide scan error`() {
     // given
-    val defaultModel = ScanSimpleIdModel.create()
+    val defaultModel = ScanSimpleIdModel.create(OpenedFrom.PatientsTabScreen)
 
     // when
     uiRenderer.render(defaultModel)
@@ -57,7 +57,7 @@ class ScanSimpleIdUiRendererTest {
   @Test
   fun `when there is scan error, then show scan error`() {
     // given
-    val defaultModel = ScanSimpleIdModel.create()
+    val defaultModel = ScanSimpleIdModel.create(OpenedFrom.PatientsTabScreen)
         .invalidQrCode()
 
     // when
@@ -66,6 +66,21 @@ class ScanSimpleIdUiRendererTest {
     // then
     verify(ui).hideSearchingForPatient()
     verify(ui).showScanError()
+    verifyNoMoreInteractions(ui)
+  }
+
+  @Test
+  fun `when scan simple id screen is opened from edit patient screen, then hide entered code container view`() {
+    // given
+    val defaultModel = ScanSimpleIdModel.create(openedFrom = OpenedFrom.EditPatientScreen.ToAddNHID)
+
+    // when
+    uiRenderer.render(defaultModel)
+
+    // then
+    verify(ui).hideEnteredCodeContainerView()
+    verify(ui).hideSearchingForPatient()
+    verify(ui).hideScanError()
     verifyNoMoreInteractions(ui)
   }
 }

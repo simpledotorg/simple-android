5dd3690a2daafbc10af64e49ef6016f1cf298739
Janhavi Singh <janhavi@obvious.in>
2022-01-28 11:43:39 +0530

    Implement `RemoveOverdueViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/removeoverdueappointment/RemoveOverdueViewEffectHandler.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.removeoverdueappointment
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class RemoveOverdueViewEffectHandler(
+    private val uiActions: RemoveOverdueUiActions
+) : ViewEffectsHandler<RemoveOverdueViewEffect> {
+  override fun handle(viewEffect: RemoveOverdueViewEffect) {
+    // nothing to see here, yet
+  }
+}

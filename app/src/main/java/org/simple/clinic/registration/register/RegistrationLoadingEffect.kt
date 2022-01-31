c2f7f415d9ef9dbf6572de08cbeee52c93795f1a
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-01-31 11:15:40 +0530

    Add `RegistrationLoadingViewEffect` as subtype of `RegistrationLoadingEffect`

--- app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingEffect.kt
+++ app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingEffect.kt
@@ -10,3 +10,5 @@ data class RegisterUserAtFacility(val user: User) : RegistrationLoadingEffect()
 object GoToHomeScreen : RegistrationLoadingEffect()
 
 data class ConvertRegistrationEntryToUserDetails(val registrationEntry: OngoingRegistrationEntry) : RegistrationLoadingEffect()
+
+sealed class RegistrationLoadingViewEffect : RegistrationLoadingEffect()

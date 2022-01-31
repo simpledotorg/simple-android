71359012fc5b019986bf85c8c0637391a57049f7
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-01-31 11:17:02 +0530

    Implement `RegistrationLoadingViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.registration.register
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class RegistrationLoadingViewEffectHandler(
+    private val uiActions: RegistrationLoadingUiActions
+) : ViewEffectsHandler<RegistrationLoadingViewEffect> {
+
+  override fun handle(viewEffect: RegistrationLoadingViewEffect) {
+    // nothing to do here yet (～￣▽￣)～
+  }
+}

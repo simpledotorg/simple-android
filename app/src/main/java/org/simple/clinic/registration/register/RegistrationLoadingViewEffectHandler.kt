fdcedafbd8441f55ffcf3dbbd8d27cb92056545e
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-01-31 11:21:22 +0530

    Move `GoToHomeScreen` effect to view effect

--- app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingViewEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/registration/register/RegistrationLoadingViewEffectHandler.kt
@@ -1,12 +1,15 @@
 package org.simple.clinic.registration.register
 
 import org.simple.clinic.mobius.ViewEffectsHandler
+import org.simple.clinic.util.exhaustive
 
 class RegistrationLoadingViewEffectHandler(
     private val uiActions: RegistrationLoadingUiActions
 ) : ViewEffectsHandler<RegistrationLoadingViewEffect> {
 
   override fun handle(viewEffect: RegistrationLoadingViewEffect) {
-    // nothing to do here yet (～￣▽￣)～
+    when (viewEffect) {
+      GoToHomeScreen -> uiActions.openHomeScreen()
+    }.exhaustive()
   }
 }

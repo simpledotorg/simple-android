4ece4e4654b231a16e86275bc61b0bab363c8103
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-01-31 11:39:17 +0530

    Implement `RegistrationPinViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/registration/pin/RegistrationPinViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.registration.pin
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class RegistrationPinViewEffectHandler(
+    private val uiActions: RegistrationPinUiActions
+) : ViewEffectsHandler<RegistrationPinViewEffect> {
+
+  override fun handle(viewEffect: RegistrationPinViewEffect) {
+    // we will have something here soon
+  }
+}

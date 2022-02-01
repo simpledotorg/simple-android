92b9806f0f62a862a8983266f18757b95f4353e5
Janhavi Singh <janhavi@obvious.in>
2022-02-01 14:53:37 +0530

    Implement `RegistrationNameViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/registration/name/RegistrationNameViewEffectHandler.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.registration.name
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class RegistrationNameViewEffectHandler(
+    private val uiActions: RegistrationNameUiActions
+) : ViewEffectsHandler<RegistrationNameViewEffect> {
+  override fun handle(viewEffect: RegistrationNameViewEffect) {
+    // Nothing to do here, yet. ٩(- ̮̮̃-̃)۶
+  }
+}

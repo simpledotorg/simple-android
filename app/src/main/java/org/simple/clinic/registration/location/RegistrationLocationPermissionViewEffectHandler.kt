7a1a9b65d9cf55bb1d6ded04c70e9eed01a9c83b
Janhavi Singh <janhavi@obvious.in>
2022-02-01 16:05:41 +0530

    Implement `RegistrationLocationPermissionViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/registration/location/RegistrationLocationPermissionViewEffectHandler.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.registration.location
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class RegistrationLocationPermissionViewEffectHandler(
+    private val uiActions: RegistrationLocationPermissionUiActions
+) : ViewEffectsHandler<RegistrationLocationPermissionViewEffect> {
+  override fun handle(viewEffect: RegistrationLocationPermissionViewEffect) {
+    // Nothing to look at here, yet. 龴ↀ◡ↀ龴
+  }
+}

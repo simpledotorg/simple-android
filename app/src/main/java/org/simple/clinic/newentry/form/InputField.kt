dfd3f7bebe652a93807ffeb834f3969491a0baa2
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-02-01 10:59:46 +0530

    Remove `validate` function from `InputField`
    We are currently not using this function anywhere

--- app/src/main/java/org/simple/clinic/newentry/form/InputField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/InputField.kt
@@ -3,6 +3,4 @@ package org.simple.clinic.newentry.form
 import android.os.Parcelable
 import androidx.annotation.StringRes
 
-sealed class InputField<T>(@StringRes val labelResId: Int) : Parcelable {
-  abstract fun validate(value: T): Set<ValidationError>
-}
+sealed class InputField<T>(@StringRes val labelResId: Int) : Parcelable
--- app/src/main/java/org/simple/clinic/newentry/form/VillageOrColonyField.kt
+++ app/src/main/java/org/simple/clinic/newentry/form/VillageOrColonyField.kt
@@ -1,13 +1,8 @@
 package org.simple.clinic.newentry.form
 
 import kotlinx.parcelize.Parcelize
-import org.simple.clinic.newentry.form.ValidationError.MissingValue
 
 @Parcelize
 data class VillageOrColonyField(
     private val _labelResId: Int
-) : InputField<String>(_labelResId) {
-  override fun validate(value: String): Set<ValidationError> {
-    return if (value.isBlank()) setOf(MissingValue) else emptySet()
-  }
-}
+) : InputField<String>(_labelResId)

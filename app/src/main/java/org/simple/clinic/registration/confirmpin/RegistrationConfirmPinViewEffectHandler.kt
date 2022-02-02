ac0ad88455f93b60667471b0afdfdcdb45324137
Janhavi Singh <janhavi@obvious.in>
2022-02-02 14:21:23 +0530

    Implement `RegistrationConfirmPinViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/registration/confirmpin/RegistrationConfirmPinViewEffectHandler.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.registration.confirmpin
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class RegistrationConfirmPinViewEffectHandler(
+    private val uiActions: RegistrationConfirmPinUiActions
+) : ViewEffectsHandler<RegistrationConfirmPinViewEffect> {
+  override fun handle(viewEffect: RegistrationConfirmPinViewEffect) {
+    // Nothing to see here, yet. ᕙ(⇀‸↼‶)ᕗ
+  }
+}

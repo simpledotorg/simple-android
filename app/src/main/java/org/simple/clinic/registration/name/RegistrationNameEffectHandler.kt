8ce0d77cb6ceed37e433fb0a228550c3789de790
Janhavi Singh <58420800+janhavisinghh@users.noreply.github.com>
2022-02-02 08:25:14 +0530

    Migrate `RegistrationFullNameScreen` to use view effects (#3472)
    * Add `RegistrationNameViewEffect` as subtype of `RegistrationNameEffect`
    
    * Implement `RegistrationNameViewEffectHandler`
    
    * Update `RegistrationFullNameScreen` view effect type to `RegistrationNameViewEffect`
    
    * Override view effect handler in `RegistrationFullNameScreen`
    
    * Add `Consumer<RegistrationNameViewEffect>` as dependency for `RegistrationNameEffectHandler`
    
    * Pass consumed view effects to view effects consumer
    
    * Move `PrefillFields` effect to view effect
    
    * Move `ProceedToPinEntry` effect to view effect
    
    * Remove unused dependencies from the effect handler
    
    * Add effect handler tests for `ValidateEnteredName` effect
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/registration/name/RegistrationNameEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/registration/name/RegistrationNameEffectHandler.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.registration.name
 
+import com.spotify.mobius.functions.Consumer
 import com.spotify.mobius.rx2.RxMobius
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
@@ -7,24 +8,23 @@ import dagger.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
 import org.simple.clinic.registration.name.RegistrationNameValidationResult.Blank
 import org.simple.clinic.registration.name.RegistrationNameValidationResult.Valid
-import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class RegistrationNameEffectHandler @AssistedInject constructor(
-    private val schedulers: SchedulersProvider,
-    @Assisted private val uiActions: RegistrationNameUiActions
+    @Assisted private val viewEffectConsumer: Consumer<RegistrationNameViewEffect>
 ) {
 
   @AssistedFactory
   interface Factory {
-    fun create(uiActions: RegistrationNameUiActions): RegistrationNameEffectHandler
+    fun create(
+        viewEffectConsumer: Consumer<RegistrationNameViewEffect>
+    ): RegistrationNameEffectHandler
   }
 
   fun build(): ObservableTransformer<RegistrationNameEffect, RegistrationNameEvent> {
     return RxMobius
         .subtypeEffectHandler<RegistrationNameEffect, RegistrationNameEvent>()
-        .addConsumer(PrefillFields::class.java, { uiActions.preFillUserDetails(it.entry) }, schedulers.ui())
         .addTransformer(ValidateEnteredName::class.java, validateNameEntry())
-        .addConsumer(ProceedToPinEntry::class.java, { uiActions.openRegistrationPinEntryScreen(it.entry) }, schedulers.ui())
+        .addConsumer(RegistrationNameViewEffect::class.java, viewEffectConsumer::accept)
         .build()
   }
 

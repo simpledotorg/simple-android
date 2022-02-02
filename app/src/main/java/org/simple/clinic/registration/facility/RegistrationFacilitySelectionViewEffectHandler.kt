c9cb3c4307bdf9450eccfd4c5e76c5b3f598c809
Janhavi Singh <janhavi@obvious.in>
2022-02-02 13:38:42 +0530

    Implement `RegistrationFacilitySelectionViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionViewEffectHandler.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.registration.facility
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class RegistrationFacilitySelectionViewEffectHandler(
+    private val uiActions: RegistrationFacilitySelectionUiActions
+) : ViewEffectsHandler<RegistrationFacilitySelectionViewEffect>{
+  override fun handle(viewEffect: RegistrationFacilitySelectionViewEffect) {
+    // Nothing to do here, yet. (◕ヮ◕) (^o^) (✿◠‿◠)
+  }
+}

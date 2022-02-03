aa84b756499b4900c25719d9deeb146215f575f1
Janhavi Singh <janhavi@obvious.in>
2022-02-03 10:03:54 +0530

    Add `exhaustive` extension to this `when` block

--- app/src/main/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionViewEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionViewEffectHandler.kt
@@ -1,14 +1,15 @@
 package org.simple.clinic.registration.facility
 
 import org.simple.clinic.mobius.ViewEffectsHandler
+import org.simple.clinic.util.exhaustive
 
 class RegistrationFacilitySelectionViewEffectHandler(
     private val uiActions: RegistrationFacilitySelectionUiActions
 ) : ViewEffectsHandler<RegistrationFacilitySelectionViewEffect>{
   override fun handle(viewEffect: RegistrationFacilitySelectionViewEffect) {
-    when(viewEffect){
+    when (viewEffect) {
       is OpenConfirmFacilitySheet -> uiActions.showConfirmFacilitySheet(viewEffect.facility.uuid, viewEffect.facility.name)
       is MoveToIntroVideoScreen -> uiActions.openIntroVideoScreen(viewEffect.registrationEntry)
-    }
+    }.exhaustive()
   }
 }

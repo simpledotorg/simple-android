4c28e02818d2a48c68db0522c77e2a8d1a0cc128
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-02-07 07:26:30 +0530

    Stop sending username in Datadog user info

--- app/src/main/java/org/simple/clinic/plumbing/infrastructure/DatadogInfrastructure.kt
+++ app/src/main/java/org/simple/clinic/plumbing/infrastructure/DatadogInfrastructure.kt
@@ -6,12 +6,11 @@ import org.simple.clinic.appconfig.Deployment
 import org.simple.clinic.user.User
 import javax.inject.Inject
 
-class DatadogInfrastructure @Inject constructor(): Infrastructure {
+class DatadogInfrastructure @Inject constructor() : Infrastructure {
 
   override fun addDetails(user: User, country: Country, deployment: Deployment) {
     Datadog.setUserInfo(
         id = user.uuid.toString(),
-        name = user.fullName,
         extraInfo = mapOf(
             "facilityId" to user.currentFacilityUuid.toString(),
             "countryCode" to country.isoCountryCode,

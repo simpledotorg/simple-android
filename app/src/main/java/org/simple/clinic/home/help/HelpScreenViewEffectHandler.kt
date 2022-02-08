b8899955fbb64cb5cd55a968327680e324ff4938
Honey <honey@obvious.in>
2022-02-08 19:17:36 +0530

    Create `HelpScreenViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/home/help/HelpScreenViewEffectHandler.kt
@@ -0,0 +1,9 @@
+package org.simple.clinic.home.help
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class HelpScreenViewEffectHandler : ViewEffectsHandler<HelpScreenViewEffect> {
+  override fun handle(viewEffect: HelpScreenViewEffect) {
+    // no-op
+  }
+}

72e7b068678790d7ec22ea42ecf2327e8363b46e
Janhavi Singh <janhavi@obvious.in>
2022-02-08 14:11:51 +0530

    Implement `IntroVideoViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.introvideoscreen
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class IntroVideoViewEffectHandler(
+    private val uiActions: UiActions
+) : ViewEffectsHandler<IntroVideoViewEffect> {
+  
+  override fun handle(viewEffect: IntroVideoViewEffect) {
+    // nothing to see here, yet. -\(".")/-
+  }
+}

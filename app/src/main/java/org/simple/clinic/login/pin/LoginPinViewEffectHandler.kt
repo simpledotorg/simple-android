10e26a0c3e8cbc8b36e0e1db2cafe881de8040e3
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-02-08 12:33:15 +0530

    Implement `LoginPinViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/login/pin/LoginPinViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.login.pin
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class LoginPinViewEffectHandler(
+    private val uiActions: UiActions
+) : ViewEffectsHandler<LoginPinViewEffect> {
+
+  override fun handle(viewEffect: LoginPinViewEffect) {
+    // nothing to see here
+  }
+}

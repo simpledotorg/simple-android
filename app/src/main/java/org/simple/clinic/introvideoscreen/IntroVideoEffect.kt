649748362970fd7728303c240412b68d806b5403
Janhavi Singh <janhavi@obvious.in>
2022-02-08 14:13:19 +0530

    Move `OpenVideo` to view effects

--- app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoEffect.kt
+++ app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoEffect.kt
@@ -2,8 +2,8 @@ package org.simple.clinic.introvideoscreen
 
 sealed class IntroVideoEffect
 
-object OpenVideo : IntroVideoEffect()
-
 object OpenHome : IntroVideoEffect()
 
 sealed class IntroVideoViewEffect : IntroVideoEffect()
+
+object OpenVideo : IntroVideoViewEffect()
--- app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoViewEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/introvideoscreen/IntroVideoViewEffectHandler.kt
@@ -1,12 +1,15 @@
 package org.simple.clinic.introvideoscreen
 
 import org.simple.clinic.mobius.ViewEffectsHandler
+import org.simple.clinic.util.exhaustive
 
 class IntroVideoViewEffectHandler(
     private val uiActions: UiActions
 ) : ViewEffectsHandler<IntroVideoViewEffect> {
   
   override fun handle(viewEffect: IntroVideoViewEffect) {
-    // nothing to see here, yet. -\(".")/-
+    when(viewEffect){
+      OpenVideo -> uiActions.openVideo()
+    }.exhaustive()
   }
 }

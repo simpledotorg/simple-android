067fc0deaadd886f0cf91cea1539b3c14d3369b7
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-02-28 12:09:19 +0530

    Remove assigned facility id from `SearchResult`

--- app/src/main/java/org/simple/clinic/instantsearch/InstantSearchResultsItemType.kt
+++ app/src/main/java/org/simple/clinic/instantsearch/InstantSearchResultsItemType.kt
@@ -93,7 +93,6 @@ sealed class InstantSearchResultsItemType : PagingItemAdapter.Item<InstantSearch
   data class SearchResult(
       val searchResultViewModel: PatientSearchResultViewModel,
       val currentFacilityId: UUID,
-      val assignedFacilityId: UUID?,
       val searchQuery: String?
   ) : InstantSearchResultsItemType() {
 
@@ -106,7 +105,6 @@ sealed class InstantSearchResultsItemType : PagingItemAdapter.Item<InstantSearch
         return SearchResult(
             searchResultViewModel = mapPatientSearchResultToViewModel(searchResult),
             currentFacilityId = currentFacilityId,
-            assignedFacilityId = searchResult.assignedFacilityId,
             searchQuery = searchQuery
         )
       }
@@ -128,7 +126,7 @@ sealed class InstantSearchResultsItemType : PagingItemAdapter.Item<InstantSearch
     }
 
     val isAtCurrentFacility
-      get() = assignedFacilityId == currentFacilityId
+      get() = searchResultViewModel.assignedFacilityId == currentFacilityId
 
     override fun layoutResId(): Int = R.layout.list_patient_search
 

355768cd0302b620d033cfadea68a895f2649196
Honey <honey@obvious.in>
2022-03-10 12:37:21 +0530

    Add date as a @Query parameter instead of @Body

--- app/src/main/java/org/simple/clinic/drugstockreminders/DrugStockReminderApi.kt
+++ app/src/main/java/org/simple/clinic/drugstockreminders/DrugStockReminderApi.kt
@@ -1,13 +1,13 @@
 package org.simple.clinic.drugstockreminders
 
 import retrofit2.Call
-import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface DrugStockReminderApi {
 
   @GET("v4/drug_stock")
   fun drugStockReminder(
-      @Body body: DrugStockReminderRequest
+      @Query("date") previousMonth: String
   ): Call<DrugStockReminderResponsePayload>
 }

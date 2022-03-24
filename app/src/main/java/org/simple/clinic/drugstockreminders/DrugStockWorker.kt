31a9b806cbdb0d07f7e8ed4d503db275d046f904
Honey <honey@obvious.in>
2022-03-24 12:42:39 +0530

    Add notification channel for the drug stock reminder update

--- app/src/main/java/org/simple/clinic/drugstockreminders/DrugStockWorker.kt
+++ app/src/main/java/org/simple/clinic/drugstockreminders/DrugStockWorker.kt
@@ -1,6 +1,9 @@
 package org.simple.clinic.drugstockreminders
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.work.RxWorker
 import androidx.work.WorkerParameters
 import io.reactivex.Single
@@ -10,10 +13,28 @@ class DrugStockWorker(
     workerParams: WorkerParameters
 ) : RxWorker(context, workerParams) {
 
+  companion object {
+    private const val NOTIFICATION_CHANNEL_ID = "org.simple.clinic.drugstockreminders"
+    private const val NOTIFICATION_CHANNEL_NAME = "Drug Stock Reminders"
+  }
+
+  private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
   init {
     ClinicApp.appComponent.inject(this)
   }
   override fun createWork(): Single<Result> {
-    // todo add the work here next
+    createNotificationChannel()
+  }
+
+  private fun createNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      notificationManager.createNotificationChannel(
+          NotificationChannel(
+              NOTIFICATION_CHANNEL_ID,
+              NOTIFICATION_CHANNEL_NAME,
+              NotificationManager.IMPORTANCE_HIGH
+          ))
+    }
   }
 }

a4e3b85930449c93b13427c1d54e39e352045f0a
Janhavi Singh <janhavi@obvious.in>
2022-03-28 11:25:48 +0530

    Make `CriticalAppUpdateModel` parcelable

--- app/src/main/java/org/simple/clinic/ContactType.kt
+++ app/src/main/java/org/simple/clinic/ContactType.kt
@@ -1,15 +1,20 @@
 package org.simple.clinic
 
+import android.os.Parcelable
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
+import kotlinx.parcelize.Parcelize
 import org.simple.clinic.util.room.SafeEnumTypeAdapter
 
-sealed class ContactType {
+sealed class ContactType : Parcelable {
 
+  @Parcelize
   object WhatsApp : ContactType()
 
+  @Parcelize
   object Telegram : ContactType()
 
+  @Parcelize
   data class Unknown(val actualValue: String) : ContactType()
 
   object TypeAdapter : SafeEnumTypeAdapter<ContactType>(

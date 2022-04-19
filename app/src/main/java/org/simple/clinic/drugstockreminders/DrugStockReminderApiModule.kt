505c0087766fa8d689d30d6a43d05418b3620009
Sasikanth Miriymapalli <sasikanth@obvious.in>
2022-04-19 12:03:36 +0530

    Add preference for "is drug stock report filled"

--- app/src/main/java/org/simple/clinic/drugstockreminders/DrugStockReminderApiModule.kt
+++ app/src/main/java/org/simple/clinic/drugstockreminders/DrugStockReminderApiModule.kt
@@ -6,7 +6,9 @@ import dagger.Module
 import dagger.Provides
 import org.simple.clinic.main.TypedPreference
 import org.simple.clinic.main.TypedPreference.Type.DrugStockReportLastCheckedAt
+import org.simple.clinic.main.TypedPreference.Type.IsDrugStockReportFilled
 import org.simple.clinic.main.TypedPreference.Type.UpdateDrugStockReportsMonth
+import org.simple.clinic.util.preference.BooleanPreferenceConverter
 import org.simple.clinic.util.preference.InstantRxPreferencesConverter
 import org.simple.clinic.util.preference.StringPreferenceConverter
 import org.simple.clinic.util.preference.getOptional
@@ -35,5 +37,11 @@ object DrugStockReminderApiModule {
   @Provides
   fun drugStockReportLastCheckAtPreference(rxSharedPreferences: RxSharedPreferences): Preference<Instant> {
     return rxSharedPreferences.getObject("drug_stock_report_last_checked_at", Instant.EPOCH, InstantRxPreferencesConverter())
+  }
+
+  @TypedPreference(IsDrugStockReportFilled)
+  @Provides
+  fun isDrugStockReportFilledPreference(rxSharedPreferences: RxSharedPreferences): Preference<Optional<Boolean>> {
+    return rxSharedPreferences.getOptional("is_drug_stock_report_filled", BooleanPreferenceConverter(), Optional.empty())
   }
 }
--- app/src/main/java/org/simple/clinic/main/TypedPreference.kt
+++ app/src/main/java/org/simple/clinic/main/TypedPreference.kt
@@ -16,6 +16,7 @@ annotation class TypedPreference(val value: Type) {
     IsLightAppUpdateNotificationShown,
     IsMediumAppUpdateNotificationShown,
     UpdateDrugStockReportsMonth,
-    DrugStockReportLastCheckedAt
+    DrugStockReportLastCheckedAt,
+    IsDrugStockReportFilled
   }
 }

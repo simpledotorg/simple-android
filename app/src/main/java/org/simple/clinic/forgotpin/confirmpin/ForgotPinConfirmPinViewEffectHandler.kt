d92277584a7255806b192b09a2261ea49402ead6
Sasikanth Miriymapalli <sasikanth@obvious.in>
2022-04-21 08:57:29 +0530

    Implement `ForgotPinConfirmPinViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/forgotpin/confirmpin/ForgotPinConfirmPinViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.forgotpin.confirmpin
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class ForgotPinConfirmPinViewEffectHandler(
+    private val uiActions: ForgotPinConfirmPinUiActions
+) : ViewEffectsHandler<ForgotPinConfirmPinViewEffect> {
+
+  override fun handle(viewEffect: ForgotPinConfirmPinViewEffect) {
+    // no-op
+  }
+}

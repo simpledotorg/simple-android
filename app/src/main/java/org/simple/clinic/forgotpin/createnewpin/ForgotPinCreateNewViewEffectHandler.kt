925cae8aec2cb85ead86f0c19c1be682c9bc2ac9
Sasikanth Miriymapalli <sasikanth@obvious.in>
2022-04-21 08:23:31 +0530

    Implement `ForgotPinCreateNewViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/forgotpin/createnewpin/ForgotPinCreateNewViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.forgotpin.createnewpin
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class ForgotPinCreateNewViewEffectHandler(
+    private val uiActions: UiActions
+) : ViewEffectsHandler<ForgotPinCreateNewViewEffect> {
+
+  override fun handle(viewEffect: ForgotPinCreateNewViewEffect) {
+    // no-op
+  }
+}

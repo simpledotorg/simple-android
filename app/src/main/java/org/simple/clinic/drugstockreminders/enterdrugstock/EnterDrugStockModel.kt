7e0693f02ab2f18e01eb0ab9517ed2250367e2dd
Honey <honey@obvious.in>
2022-04-22 07:29:01 +0530

    Add required Mobius no-op implementation of `EnterDrugStockScreen`

---
+++ app/src/main/java/org/simple/clinic/drugstockreminders/enterdrugstock/EnterDrugStockModel.kt
@@ -0,0 +1,7 @@
+package org.simple.clinic.drugstockreminders.enterdrugstock
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+object EnterDrugStockModel : Parcelable
---
+++ app/src/main/java/org/simple/clinic/drugstockreminders/enterdrugstock/EnterDrugStockScreenUiRenderer.kt
@@ -0,0 +1,10 @@
+package org.simple.clinic.drugstockreminders.enterdrugstock
+
+import org.simple.clinic.mobius.ViewRenderer
+
+class EnterDrugStockScreenUiRenderer() : ViewRenderer<EnterDrugStockModel> {
+
+  override fun render(model: EnterDrugStockModel) {
+    // no-op
+  }
+}

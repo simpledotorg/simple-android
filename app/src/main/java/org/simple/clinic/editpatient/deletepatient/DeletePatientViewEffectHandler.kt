8567f4e19806366e42899cd310ffbeb9fd73bb81
Sasikanth Miriymapalli <sasikanth@obvious.in>
2022-04-25 12:45:33 +0530

    Implement `DeletePatientViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/editpatient/deletepatient/DeletePatientViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.editpatient.deletepatient
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class DeletePatientViewEffectHandler(
+    private val uiActions: UiActions
+) : ViewEffectsHandler<DeletePatientViewEffect> {
+
+  override fun handle(viewEffect: DeletePatientViewEffect) {
+    // no-op
+  }
+}

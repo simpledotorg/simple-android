8b324bad970c308b05cf9d45e2fc576a8867ea26
Sasikanth Miriymapalli <sasikanth@obvious.in>
2022-04-27 08:40:29 +0530

    Migrate `OpenBloodPressureEntrySheet` effect to view effect

--- app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenEffect.kt
+++ app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryScreenEffect.kt
@@ -7,10 +7,10 @@ sealed class BloodPressureHistoryScreenEffect
 
 data class LoadPatient(val patientUuid: UUID) : BloodPressureHistoryScreenEffect()
 
-data class OpenBloodPressureEntrySheet(val patientUuid: UUID) : BloodPressureHistoryScreenEffect()
-
 data class OpenBloodPressureUpdateSheet(val bloodPressureMeasurement: BloodPressureMeasurement) : BloodPressureHistoryScreenEffect()
 
 data class ShowBloodPressures(val patientUuid: UUID) : BloodPressureHistoryScreenEffect()
 
 sealed class BloodPressureHistoryViewEffect : BloodPressureHistoryScreenEffect()
+
+data class OpenBloodPressureEntrySheet(val patientUuid: UUID) : BloodPressureHistoryViewEffect()
--- app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryViewEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/bp/history/BloodPressureHistoryViewEffectHandler.kt
@@ -1,12 +1,15 @@
 package org.simple.clinic.bp.history
 
 import org.simple.clinic.mobius.ViewEffectsHandler
+import org.simple.clinic.util.exhaustive
 
 class BloodPressureHistoryViewEffectHandler(
     private val uiActions: BloodPressureHistoryScreenUiActions
 ) : ViewEffectsHandler<BloodPressureHistoryViewEffect> {
 
   override fun handle(viewEffect: BloodPressureHistoryViewEffect) {
-    // no-op
+    when (viewEffect) {
+      is OpenBloodPressureEntrySheet -> uiActions.openBloodPressureEntrySheet(viewEffect.patientUuid)
+    }.exhaustive()
   }
 }

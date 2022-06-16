0ec79dda350a41fb2fb721f3507d5bfcb1ac6665
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-06-16 16:01:43 +0530

    Validate search query when overdue search query is changed (#3956)

--- app/src/test/java/org/simple/clinic/home/overdue/search/OverdueSearchUpdateTest.kt
+++ app/src/test/java/org/simple/clinic/home/overdue/search/OverdueSearchUpdateTest.kt
@@ -1,5 +1,6 @@
 package org.simple.clinic.home.overdue.search
 
+import com.spotify.mobius.test.NextMatchers.hasEffects
 import com.spotify.mobius.test.NextMatchers.hasModel
 import com.spotify.mobius.test.NextMatchers.hasNoEffects
 import com.spotify.mobius.test.UpdateSpec
@@ -8,20 +9,34 @@ import org.junit.Test
 
 class OverdueSearchUpdateTest {
 
+  private val updateSpec = UpdateSpec(OverdueSearchUpdate())
+  private val defaultModel = OverdueSearchModel.create()
+
   @Test
   fun `when overdue search history is loaded, then update the model`() {
-    val defaultModel = OverdueSearchModel.create()
     val searchHistory = setOf(
         "Babri",
         "Ramesh"
     )
 
-    UpdateSpec(OverdueSearchUpdate())
+    updateSpec
         .given(defaultModel)
         .whenEvent(OverdueSearchHistoryLoaded(searchHistory))
         .then(assertThatNext(
             hasModel(defaultModel.overdueSearchHistoryLoaded(searchHistory)),
             hasNoEffects()
+        ))
+  }
+
+  @Test
+  fun `when overdue search query is changed, then validate the search query`() {
+    val searchQuery = "Babri"
+    updateSpec
+        .given(defaultModel)
+        .whenEvent(OverdueSearchQueryChanged(searchQuery))
+        .then(assertThatNext(
+            hasModel(defaultModel.overdueSearchQueryChanged(searchQuery)),
+            hasEffects(ValidateOverdueSearchQuery(searchQuery))
         ))
   }
 }

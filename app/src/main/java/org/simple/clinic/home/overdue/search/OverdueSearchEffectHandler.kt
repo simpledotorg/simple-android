16dc21af9b6f79c007eb3097d8099d3750f4fa3a
Janhavi Singh <58420800+janhavisinghh@users.noreply.github.com>
2022-06-21 17:28:43 +0530

    Implement searching of overdue patients when search query is valid (#3970)

--- app/src/main/java/org/simple/clinic/home/overdue/search/OverdueSearchEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/search/OverdueSearchEffectHandler.kt
@@ -2,16 +2,24 @@ package org.simple.clinic.home.overdue.search
 
 import com.spotify.mobius.functions.Consumer
 import com.spotify.mobius.rx2.RxMobius
+import dagger.Lazy
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import io.reactivex.ObservableTransformer
+import org.simple.clinic.facility.Facility
+import org.simple.clinic.overdue.AppointmentRepository
+import org.simple.clinic.util.PagerFactory
 import org.simple.clinic.util.scheduler.SchedulersProvider
 
 class OverdueSearchEffectHandler @AssistedInject constructor(
     private val overdueSearchHistory: OverdueSearchHistory,
     private val overdueSearchQueryValidator: OverdueSearchQueryValidator,
     private val schedulersProvider: SchedulersProvider,
+    private val appointmentRepository: AppointmentRepository,
+    private val pagerFactory: PagerFactory,
+    private val overdueSearchConfig: OverdueSearchConfig,
+    private val currentFacility: Lazy<Facility>,
     @Assisted private val viewEffectsConsumer: Consumer<OverdueSearchViewEffect>
 ) {
 
@@ -26,6 +34,7 @@ class OverdueSearchEffectHandler @AssistedInject constructor(
         .addTransformer(LoadOverdueSearchHistory::class.java, loadOverdueSearchHistory())
         .addTransformer(ValidateOverdueSearchQuery::class.java, validateOverdueSearchQuery())
         .addConsumer(AddQueryToOverdueSearchHistory::class.java, ::addQueryToSearchHistory)
+        .addTransformer(SearchOverduePatients::class.java, searchOverduePatients())
         .addConsumer(OverdueSearchViewEffect::class.java, viewEffectsConsumer::accept)
         .build()
   }
@@ -49,6 +58,27 @@ class OverdueSearchEffectHandler @AssistedInject constructor(
           .observeOn(schedulersProvider.io())
           .switchMap { overdueSearchHistory.fetch() }
           .map(::OverdueSearchHistoryLoaded)
+    }
+  }
+
+  private fun searchOverduePatients(): ObservableTransformer<SearchOverduePatients, OverdueSearchEvent> {
+    return ObservableTransformer { effects ->
+      effects
+          .observeOn(schedulersProvider.io())
+          .switchMap {
+            pagerFactory.createPager(
+                sourceFactory = {
+                  appointmentRepository.searchOverduePatient(
+                      it.searchQuery,
+                      it.since,
+                      currentFacility.get().uuid
+                  )
+                },
+                pageSize = overdueSearchConfig.pagingLoadSize,
+                enablePlaceholders = false
+            )
+          }
+          .map(::OverdueSearchResultsLoaded)
     }
   }
 }
--- app/src/main/java/org/simple/clinic/home/overdue/search/OverdueSearchEvent.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/search/OverdueSearchEvent.kt
@@ -1,5 +1,7 @@
 package org.simple.clinic.home.overdue.search
 
+import androidx.paging.PagingData
+import org.simple.clinic.home.overdue.OverdueAppointment
 import org.simple.clinic.home.overdue.search.OverdueSearchQueryValidator.Result
 
 sealed class OverdueSearchEvent
@@ -9,3 +11,5 @@ data class OverdueSearchHistoryLoaded(val searchHistory: Set<String>) : OverdueS
 data class OverdueSearchQueryChanged(val searchQuery: String) : OverdueSearchEvent()
 
 data class OverdueSearchQueryValidated(val result: Result) : OverdueSearchEvent()
+
+data class OverdueSearchResultsLoaded(val overdueAppointments: PagingData<OverdueAppointment>) : OverdueSearchEvent()

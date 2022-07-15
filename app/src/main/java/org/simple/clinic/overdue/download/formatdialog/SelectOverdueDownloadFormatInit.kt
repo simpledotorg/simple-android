71c11cd59616c0a9a296597cbb9effdf0ce0f3b6
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-07-15 12:20:25 +0530

    Use overdue appointment selector to fetch selected ids in overdue download format selection dialog (#4095)
    https://app.shortcut.com/simpledotorg/story/8717/use-overdue-appointment-selector-to-fetch-selected-ids-in-overdue-download-format-selection-dialog

--- app/src/main/java/org/simple/clinic/overdue/download/formatdialog/SelectOverdueDownloadFormatInit.kt
+++ app/src/main/java/org/simple/clinic/overdue/download/formatdialog/SelectOverdueDownloadFormatInit.kt
@@ -4,12 +4,11 @@ import com.spotify.mobius.First
 import com.spotify.mobius.First.first
 import com.spotify.mobius.Init
 import org.simple.clinic.mobius.first
-import org.simple.clinic.overdue.download.OverdueListFileFormat.CSV
 
 class SelectOverdueDownloadFormatInit : Init<SelectOverdueDownloadFormatModel, SelectOverdueDownloadFormatEffect> {
   override fun init(model: SelectOverdueDownloadFormatModel): First<SelectOverdueDownloadFormatModel, SelectOverdueDownloadFormatEffect> {
     return if (model.openAs is SharingInProgress) {
-      first(model.overdueDownloadInProgress(), DownloadForShare(CSV, model.openAs.selectedAppointmentIds))
+      first(model.overdueDownloadInProgress(), LoadSelectedOverdueAppointmentIds)
     } else {
       first(model)
     }

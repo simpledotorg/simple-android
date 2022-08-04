3b1bd64c2fc765e6d4be127392475294172a53ad
Janhavi Singh <janhavi@obvious.in>
2022-08-04 11:52:53 +0530

    Implement `BloodSugarHistoryScreenViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/bloodsugar/history/BloodSugarHistoryScreenViewEffectHandler.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.bloodsugar.history
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class BloodSugarHistoryScreenViewEffectHandler(
+    private val uiActions: BloodSugarHistoryScreenUiActions
+) : ViewEffectsHandler<BloodSugarHistoryScreenViewEffect> {
+  override fun handle(viewEffect: BloodSugarHistoryScreenViewEffect) {
+    // Nothing to do here, yet.
+  }
+}

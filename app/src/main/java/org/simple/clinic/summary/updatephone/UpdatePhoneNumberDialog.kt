adff62c0b068fd4482d1ff6a57c5c7bed6935c72
Sasikanth Miriymapalli <sasikanth@obvious.in>
2022-08-08 10:37:17 +0530

    Remove unused code

--- app/src/main/java/org/simple/clinic/summary/updatephone/UpdatePhoneNumberDialog.kt
+++ app/src/main/java/org/simple/clinic/summary/updatephone/UpdatePhoneNumberDialog.kt
@@ -1,6 +1,5 @@
 package org.simple.clinic.summary.updatephone
 
-import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
@@ -10,13 +9,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.jakewharton.rxbinding3.view.clicks
 import com.spotify.mobius.functions.Consumer
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.cast
-import io.reactivex.subjects.PublishSubject
 import kotlinx.parcelize.Parcelize
 import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
@@ -25,10 +22,6 @@ import org.simple.clinic.di.injector
 import org.simple.clinic.navigation.v2.Router
 import org.simple.clinic.navigation.v2.ScreenKey
 import org.simple.clinic.navigation.v2.fragments.BaseDialog
-import org.simple.clinic.patient.PatientUuid
-import org.simple.clinic.widgets.ScreenCreated
-import org.simple.clinic.widgets.ScreenDestroyed
-import org.simple.clinic.widgets.UiEvent
 import org.simple.clinic.widgets.setTextAndCursor
 import java.util.UUID
 import javax.inject.Inject
@@ -41,49 +34,17 @@ class UpdatePhoneNumberDialog : BaseDialog<
     UpdatePhoneNumberEffect,
     Nothing>(), UpdatePhoneNumberDialogUi, UpdatePhoneNumberUiActions {
 
-  private var binding: DialogPatientsummaryUpdatephoneBinding? = null
-
   private val numberEditText
-    get() = binding!!.numberEditText
+    get() = binding.numberEditText
 
   private val phoneInputLayout
-    get() = binding!!.phoneInputLayout
-
-  companion object {
-    private const val FRAGMENT_TAG = "UpdatePhoneNumberDialog"
-    private const val KEY_PATIENT_UUID = "patientUuid"
-
-    fun show(patientUuid: PatientUuid, fragmentManager: FragmentManager) {
-      val existingFragment = fragmentManager.findFragmentByTag(FRAGMENT_TAG)
-
-      if (existingFragment != null) {
-        fragmentManager
-            .beginTransaction()
-            .remove(existingFragment)
-            .commitNowAllowingStateLoss()
-      }
-
-      val fragment = UpdatePhoneNumberDialog().apply {
-        isCancelable = false
-        arguments = Bundle(1).apply {
-          putSerializable(KEY_PATIENT_UUID, patientUuid)
-        }
-      }
-
-      fragmentManager
-          .beginTransaction()
-          .add(fragment, FRAGMENT_TAG)
-          .commitNowAllowingStateLoss()
-    }
-  }
+    get() = binding.phoneInputLayout
 
   @Inject
   lateinit var effectHandlerFactory: UpdatePhoneNumberEffectHandler.Factory
 
   @Inject
   lateinit var router: Router
-
-  private val screenDestroys = PublishSubject.create<ScreenDestroyed>()
 
   override fun defaultModel() = UpdatePhoneNumberModel.create(
       patientUuid = screenKey.patientUuid
@@ -100,7 +61,6 @@ class UpdatePhoneNumberDialog : BaseDialog<
 
     return Observable
         .merge(
-            dialogCreates(),
             cancelClicks(cancelButton),
             saveClicks(saveButton)
         )
@@ -121,27 +81,14 @@ class UpdatePhoneNumberDialog : BaseDialog<
     context.injector<Injector>().inject(this)
   }
 
-  @SuppressLint("CheckResult", "InflateParams")
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-    val layoutInflater = LayoutInflater.from(requireContext())
-    binding = DialogPatientsummaryUpdatephoneBinding.inflate(layoutInflater, null, false)
-
     return MaterialAlertDialogBuilder(requireContext())
         .setTitle(R.string.patientsummary_updatephone_dialog_title)
         .setMessage(R.string.patientsummary_updatephone_dialog_message)
-        .setView(binding!!.root)
+        .setView(binding.root)
         .setPositiveButton(R.string.patientsummary_updatephone_save, null)
         .setNegativeButton(R.string.patientsummary_updatephone_cancel, null)
         .create()
-  }
-
-  override fun onDestroyView() {
-    super.onDestroyView()
-    screenDestroys.onNext(ScreenDestroyed())
-  }
-
-  private fun dialogCreates(): Observable<UiEvent> {
-    return Observable.just(ScreenCreated())
   }
 
   private fun cancelClicks(cancelButton: Button) =

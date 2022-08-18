2db44e8c8f008a26fba0ae3e34b884b7439f28ec
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-08-18 14:43:40 +0530

    Migrate `ConfirmRemoveBloodSugarDialog` to `BaseDialog` (#4182)
    * Add screen key in `ConfirmRemoveBloodSugarDialog`
    
    * Extend base dialog in `ConfirmRemoveBloodSugarDialog`
    
    * Remove Mobius delegate from `ConfirmRemoveBloodSugarDialog`
    
    * Use screen key to open `ConfirmRemoveBloodSugarDialog`
    
    * Remove unused code
    
    * Use Router for dismissing dialog
    
    * Handle dialog actions in `onResume`
    
    Since we don't want the dialog to be dismissed before events are handled by Mobius loop. We are manually handling the action button clicks.
    
    By default alert dialog dismisses dialog after action button is clicked. That's why we are handling them manually.
    
    * Update CHANGELOG

--- app/src/main/java/org/simple/clinic/bloodsugar/entry/confirmremovebloodsugar/ConfirmRemoveBloodSugarDialog.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/confirmremovebloodsugar/ConfirmRemoveBloodSugarDialog.kt
@@ -3,116 +3,98 @@ package org.simple.clinic.bloodsugar.entry.confirmremovebloodsugar
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDialogFragment
-import androidx.fragment.app.FragmentManager
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AlertDialog.BUTTON_NEGATIVE
+import androidx.appcompat.app.AlertDialog.BUTTON_POSITIVE
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import io.reactivex.rxkotlin.ofType
+import com.spotify.mobius.functions.Consumer
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.cast
 import io.reactivex.subjects.PublishSubject
+import kotlinx.parcelize.Parcelize
 import org.simple.clinic.R
 import org.simple.clinic.di.injector
-import org.simple.clinic.mobius.MobiusDelegate
-import org.simple.clinic.util.unsafeLazy
+import org.simple.clinic.navigation.v2.Router
+import org.simple.clinic.navigation.v2.ScreenKey
+import org.simple.clinic.navigation.v2.Succeeded
+import org.simple.clinic.navigation.v2.fragments.BaseDialog
 import java.util.UUID
 import javax.inject.Inject
 
-class ConfirmRemoveBloodSugarDialog : AppCompatDialogFragment(), ConfirmRemoveBloodSugarUiActions {
-  companion object {
-    private const val KEY_BLOOD_SUGAR_UUID = "bloodSugarMeasurementUuid"
-
-    fun show(bloodSugarMeasurementUuid: UUID, fragmentManager: FragmentManager) {
-      val fragmentTag = "fragment_confirm_remove_blood_sugar"
-
-      val existingFragment = fragmentManager.findFragmentByTag(fragmentTag)
-
-      if (existingFragment != null) {
-        fragmentManager
-            .beginTransaction()
-            .remove(existingFragment)
-            .commitNowAllowingStateLoss()
-      }
-
-      val args = Bundle()
-      args.putSerializable(KEY_BLOOD_SUGAR_UUID, bloodSugarMeasurementUuid)
-
-      val fragment = ConfirmRemoveBloodSugarDialog()
-      fragment.arguments = args
-
-      fragmentManager
-          .beginTransaction()
-          .add(fragment, fragmentTag)
-          .commitNowAllowingStateLoss()
-    }
-  }
+class ConfirmRemoveBloodSugarDialog : BaseDialog<
+    ConfirmRemoveBloodSugarDialog.Key,
+    Nothing,
+    ConfirmRemoveBloodSugarModel,
+    ConfirmRemoveBloodSugarEvent,
+    ConfirmRemoveBloodSugarEffect,
+    Nothing>(), ConfirmRemoveBloodSugarUiActions {
 
   @Inject
   lateinit var effectHandler: ConfirmRemoveBloodSugarEffectHandler.Factory
 
+  @Inject
+  lateinit var router: Router
+
   private val events = PublishSubject.create<ConfirmRemoveBloodSugarEvent>()
 
-  private val delegate by unsafeLazy {
-    val bloodSugarMeasurementUuid = requireArguments().getSerializable(KEY_BLOOD_SUGAR_UUID) as UUID
-
-    MobiusDelegate.forActivity(
-        events = events.ofType(),
-        defaultModel = ConfirmRemoveBloodSugarModel.create(bloodSugarMeasurementUuid),
-        update = ConfirmRemoveBloodSugarUpdate(),
-        effectHandler = effectHandler.create(this).build()
-    )
+  override fun bindView(layoutInflater: LayoutInflater, container: ViewGroup?): Nothing? {
+    return null
   }
 
-  private var removeBloodSugarListener: RemoveBloodSugarListener? = null
+  override fun defaultModel() = ConfirmRemoveBloodSugarModel.create(screenKey.bloodSugarUuid)
+
+  override fun events(): Observable<ConfirmRemoveBloodSugarEvent> = events.cast()
+
+  override fun createUpdate() = ConfirmRemoveBloodSugarUpdate()
+
+  override fun createEffectHandler(viewEffectsConsumer: Consumer<Nothing>) = effectHandler
+      .create(this)
+      .build()
 
   override fun onAttach(context: Context) {
     super.onAttach(context)
     context.injector<ConfirmRemoveBloodSugarDialogInjector>().inject(this)
-    removeBloodSugarListener = parentFragment as? RemoveBloodSugarListener
-    if (removeBloodSugarListener == null) {
-      throw ClassCastException("$context must implement RemoveBloodSugarListener")
-    }
-  }
-
-  override fun onDetach() {
-    removeBloodSugarListener = null
-    super.onDetach()
-  }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    delegate.onRestoreInstanceState(savedInstanceState)
   }
 
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
     return MaterialAlertDialogBuilder(requireContext(), R.style.ThemeOverlay_Simple_MaterialAlertDialog_Destructive)
         .setTitle(R.string.bloodsugarentry_remove_blood_sugar_title)
         .setMessage(R.string.bloodsugarentry_remove_blood_sugar_message)
-        .setPositiveButton(R.string.bloodsugarentry_remove_blood_sugar_confirm) { _, _ ->
-          events.onNext(RemoveBloodSugarClicked)
-        }
+        .setPositiveButton(R.string.bloodsugarentry_remove_blood_sugar_confirm, null)
         .setNegativeButton(R.string.bloodsugarentry_remove_blood_sugar_cancel, null)
         .create()
   }
 
-  override fun onStart() {
-    super.onStart()
-    delegate.start()
-  }
+  override fun onResume() {
+    super.onResume()
+    val alertDialog = (dialog as AlertDialog)
 
-  override fun onStop() {
-    delegate.stop()
-    super.onStop()
-  }
+    alertDialog.getButton(BUTTON_POSITIVE).setOnClickListener {
+      events.onNext(RemoveBloodSugarClicked)
+    }
 
-  override fun onSaveInstanceState(outState: Bundle) {
-    delegate.onSaveInstanceState(outState)
-    super.onSaveInstanceState(outState)
+    alertDialog.getButton(BUTTON_NEGATIVE).setOnClickListener {
+      router.pop()
+    }
   }
 
   override fun closeDialog() {
-    // TODO: Use pop with result once migrated to navigation v2
-    removeBloodSugarListener?.onBloodSugarRemoved()
+    router.popWithResult(Succeeded(BloodSugarRemoved))
   }
 
-  interface RemoveBloodSugarListener {
-    fun onBloodSugarRemoved()
+  @Parcelize
+  data class Key(
+      val bloodSugarUuid: UUID,
+      override val analyticsName: String = "Confirm Remove Blood Sugar Dialog",
+      override val type: ScreenType = ScreenType.Modal
+  ) : ScreenKey() {
+
+    override fun instantiateFragment() = ConfirmRemoveBloodSugarDialog()
   }
+
+  @Parcelize
+  object BloodSugarRemoved : Parcelable
 }

2a9050d226723dcb1ff55a00fdb3f8f1311b1669
Sasikanth Miriyampalli <sasikanth@obvious.in>
2022-11-11 13:02:22 +0530

    Extract function to handle RegistrationFacilityConfirmed event

--- app/src/main/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionUpdate.kt
+++ app/src/main/java/org/simple/clinic/registration/facility/RegistrationFacilitySelectionUpdate.kt
@@ -14,16 +14,21 @@ class RegistrationFacilitySelectionUpdate(
   ): Next<RegistrationFacilitySelectionModel, RegistrationFacilitySelectionEffect> {
     return when (event) {
       is RegistrationFacilityClicked -> dispatch(OpenConfirmFacilitySheet(event.facility))
-      is RegistrationFacilityConfirmed -> {
-        val updatedEntry = model.ongoingEntry.withFacilityUuid(event.facilityUuid)
-        val effect = if (showIntroVideoScreen) {
-          MoveToIntroVideoScreen(updatedEntry)
-        } else {
-          MoveToRegistrationLoadingScreen(updatedEntry)
-        }
-
-        dispatch(effect)
-      }
+      is RegistrationFacilityConfirmed -> registrationFacilityConfirmed(model, event)
     }
+  }
+  
+  private fun registrationFacilityConfirmed(
+      model: RegistrationFacilitySelectionModel,
+      event: RegistrationFacilityConfirmed
+  ): Next<RegistrationFacilitySelectionModel, RegistrationFacilitySelectionEffect> {
+    val updatedEntry = model.ongoingEntry.withFacilityUuid(event.facilityUuid)
+    val effect = if (showIntroVideoScreen) {
+      MoveToIntroVideoScreen(updatedEntry)
+    } else {
+      MoveToRegistrationLoadingScreen(updatedEntry)
+    }
+
+    return dispatch(effect)
   }
 }

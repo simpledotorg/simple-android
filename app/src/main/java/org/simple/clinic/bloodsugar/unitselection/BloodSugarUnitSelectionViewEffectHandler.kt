c532f7bca92153d371bc32a365c9032511662106
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-12-30 11:41:28 +0530

    Add `BloodSugarUnitSelectionViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/bloodsugar/unitselection/BloodSugarUnitSelectionViewEffectHandler.kt
@@ -0,0 +1,12 @@
+package org.simple.clinic.bloodsugar.unitselection
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class BloodSugarUnitSelectionViewEffectHandler(
+    private val uiActions: BloodSugarUnitSelectionUiActions
+) : ViewEffectsHandler<BloodSugarUnitSelectionViewEffect> {
+
+  override fun handle(viewEffect: BloodSugarUnitSelectionViewEffect) {
+    // no-op
+  }
+}

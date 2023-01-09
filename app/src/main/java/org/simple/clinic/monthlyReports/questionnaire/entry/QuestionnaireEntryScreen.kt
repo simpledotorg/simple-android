bee1cfa4e028679c2d75651079f5fc2d9647a326
Siddharth Agarwal <sagarwal@rtsl.org>
2023-01-09 22:59:45 +0530

    Questionnaire form screen mobius arch setup

--- app/src/main/java/org/simple/clinic/monthlyReports/questionnaire/entry/QuestionnaireEntryScreen.kt
+++ app/src/main/java/org/simple/clinic/monthlyReports/questionnaire/entry/QuestionnaireEntryScreen.kt
@@ -117,6 +117,12 @@ class QuestionnaireEntryScreen : BaseScreen<
   override fun goBack() {
   }
 
+  override fun showProgress() {
+  }
+
+  override fun hideProgress() {
+  }
+
   interface Injector {
     fun inject(target: QuestionnaireEntryScreen)
   }

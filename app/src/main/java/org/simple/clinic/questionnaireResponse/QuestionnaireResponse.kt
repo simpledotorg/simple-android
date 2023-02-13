8f185ef0e84e77da9810c0d936bdabf47dd32a6e
Siddharth Agarwal <siddhagarwal10@gmail.com>
2023-02-13 16:43:27 +0530

    Questionnaire response repository added to the code (#4552)
    * Questionnaire form screen mobius arch setup
    
    * Questionnaire form screen mobius arch test setup
    
    * questionnaire form screen toolbar setup with facility name
    
    * Questionnaire response table added to the room database
    
    * sync status added to questionnaire response table
    
    * package fixed for questionnaire response
    
    * Questionnaire form screen mobius arch setup
    
    * repository class and dao created for questionnaire response
    
    * Build fix
    
    * Questionnaire response repository added
    
    ---------
    
    Co-authored-by: Siddharth Agarwal <sagarwal@rtsl.org>

--- app/src/main/java/org/simple/clinic/questionnaireResponse/QuestionnaireResponse.kt
+++ app/src/main/java/org/simple/clinic/questionnaireResponse/QuestionnaireResponse.kt
@@ -1,9 +1,15 @@
 package org.simple.clinic.questionnaireResponse
 
 import android.os.Parcelable
+import androidx.room.Dao
 import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Update
+import io.reactivex.Flowable
 import kotlinx.parcelize.Parcelize
 import org.simple.clinic.monthlyReports.questionnaire.QuestionnaireType
 import org.simple.clinic.patient.SyncStatus
@@ -30,4 +36,61 @@ data class QuestionnaireResponse(
     val timestamps: Timestamps,
 
     val syncStatus: SyncStatus
-) : Parcelable
+) : Parcelable {
+  @Dao
+  interface RoomDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun save(questionnaireResponses: List<QuestionnaireResponse>)
+
+    @Query("SELECT * FROM QuestionnaireResponse WHERE uuid = :uuid LIMIT 1")
+    fun getOne(uuid: UUID): QuestionnaireResponse?
+
+    @Query("SELECT * FROM QuestionnaireResponse WHERE questionnaireType = :type")
+    fun getByQuestionnaireType(type: QuestionnaireType): List<QuestionnaireResponse>
+
+    @Update
+    fun updateQuestionnaireResponse(questionnaireResponse: QuestionnaireResponse)
+
+    @Query("SELECT * FROM QuestionnaireResponse WHERE syncStatus = :status")
+    fun withSyncStatus(status: SyncStatus): List<QuestionnaireResponse>
+
+    @Query("""
+      SELECT * FROM QuestionnaireResponse
+      WHERE syncStatus = :syncStatus
+      LIMIT :limit OFFSET :offset
+    """)
+    fun recordsWithSyncStatusBatched(
+        syncStatus: SyncStatus,
+        limit: Int,
+        offset: Int
+    ): List<QuestionnaireResponse>
+
+    @Query("UPDATE QuestionnaireResponse SET syncStatus = :newStatus WHERE syncStatus = :oldStatus")
+    fun updateSyncStatus(oldStatus: SyncStatus, newStatus: SyncStatus)
+
+    @Query("UPDATE QuestionnaireResponse SET syncStatus = :newStatus WHERE uuid IN (:uuids)")
+    fun updateSyncStatusForIds(uuids: List<UUID>, newStatus: SyncStatus)
+
+    @Query("SELECT uuid FROM QuestionnaireResponse WHERE syncStatus = :syncStatus")
+    fun recordIdsWithSyncStatus(syncStatus: SyncStatus): List<UUID>
+
+    @Query("SELECT COUNT(uuid) FROM QuestionnaireResponse")
+    fun count(): Flowable<Int>
+
+    @Query("SELECT COUNT(uuid) FROM QuestionnaireResponse WHERE syncStatus = :syncStatus")
+    fun countWithStatus(syncStatus: SyncStatus): Flowable<Int>
+
+    @Query("DELETE FROM QuestionnaireResponse")
+    fun clear()
+
+    @Query("""
+      DELETE FROM QuestionnaireResponse
+      WHERE deletedAt IS NOT NULL AND syncStatus == 'DONE'
+    """)
+    fun purgeDeleted()
+
+    @Query(""" SELECT * FROM QuestionnaireResponse """)
+    fun getAllQuestionnaireResponse(): List<QuestionnaireResponse>
+  }
+}

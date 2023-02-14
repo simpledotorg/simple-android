b7e99b9146c47275492c717c8f3fbaa0e651d1ef
Siddharth Agarwal <sagarwal@rtsl.org>
2023-02-14 14:15:25 +0530

    Questionnaire response sync api added

--- app/src/androidTest/java/org/simple/clinic/sync/QuestionnaireResponseSyncIntegrationTest.kt
+++ app/src/androidTest/java/org/simple/clinic/sync/QuestionnaireResponseSyncIntegrationTest.kt
@@ -10,13 +10,8 @@ import org.junit.rules.RuleChain
 import org.simple.clinic.AppDatabase
 import org.simple.clinic.TestClinicApp
 import org.simple.clinic.main.TypedPreference
-import org.simple.clinic.main.TypedPreference.Type.LastQuestionnairePullToken
 import org.simple.clinic.main.TypedPreference.Type.LastQuestionnaireResponsePullToken
 import org.simple.clinic.patient.SyncStatus
-import org.simple.clinic.questionnaire.MonthlyScreeningReports
-import org.simple.clinic.questionnaire.QuestionnaireRepository
-import org.simple.clinic.questionnaire.sync.QuestionnaireSync
-import org.simple.clinic.questionnaire.sync.QuestionnaireSyncApi
 import org.simple.clinic.questionnaireresponse.QuestionnaireResponse
 import org.simple.clinic.questionnaireresponse.QuestionnaireResponseRepository
 import org.simple.clinic.questionnaireresponse.sync.QuestionnaireResponseSync
@@ -24,7 +19,6 @@ import org.simple.clinic.questionnaireresponse.sync.QuestionnaireResponseSyncApi
 import org.simple.clinic.rules.SaveDatabaseRule
 import org.simple.clinic.rules.ServerAuthenticationRule
 import org.simple.clinic.user.UserSession
-import org.simple.clinic.util.unsafeLazy
 import org.simple.sharedTestCode.TestData
 import org.simple.sharedTestCode.util.Rules
 import java.util.Optional
@@ -38,24 +32,14 @@ class QuestionnaireResponseSyncIntegrationTest {
   lateinit var appDatabase: AppDatabase
 
   @Inject
-  lateinit var questionnaireRepository: QuestionnaireRepository
-
-  @Inject
-  lateinit var questionnaireResponseRepository: QuestionnaireResponseRepository
-
-  @Inject
-  @TypedPreference(LastQuestionnairePullToken)
-  lateinit var questionnaireLastPullToken: Preference<Optional<String>>
+  lateinit var repository: QuestionnaireResponseRepository
 
   @Inject
   @TypedPreference(LastQuestionnaireResponsePullToken)
   lateinit var lastPullToken: Preference<Optional<String>>
 
   @Inject
-  lateinit var questionnaireSyncApi: QuestionnaireSyncApi
-
-  @Inject
-  lateinit var questionnaireResponseSyncApi: QuestionnaireResponseSyncApi
+  lateinit var syncApi: QuestionnaireResponseSyncApi
 
   @Inject
   lateinit var userSession: UserSession
@@ -69,14 +53,10 @@ class QuestionnaireResponseSyncIntegrationTest {
       .around(ServerAuthenticationRule())
       .around(SaveDatabaseRule())
 
-  private lateinit var questionnaireSync: QuestionnaireSync
-
-  private lateinit var questionnaireResponseSync: QuestionnaireResponseSync
+  private lateinit var sync: QuestionnaireResponseSync
 
   private val batchSize = 3
   private lateinit var config: SyncConfig
-
-  private val currentFacilityUuid: UUID by unsafeLazy { userSession.loggedInUserImmediate()!!.currentFacilityUuid }
 
   @Before
   fun setUp() {
@@ -91,32 +71,18 @@ class QuestionnaireResponseSyncIntegrationTest {
         name = ""
     )
 
-    questionnaireSync = QuestionnaireSync(
+    sync = QuestionnaireResponseSync(
         syncCoordinator = SyncCoordinator(),
-        api = questionnaireSyncApi,
-        repository = questionnaireRepository,
-        lastPullToken = questionnaireLastPullToken,
-        config = config
-    )
-
-    questionnaireResponseSync = QuestionnaireResponseSync(
-        syncCoordinator = SyncCoordinator(),
-        api = questionnaireResponseSyncApi,
-        repository = questionnaireResponseRepository,
+        api = syncApi,
+        repository = repository,
         lastPullToken = lastPullToken,
         config = config
     )
   }
 
   private fun resetLocalData() {
-    clearQuestionnaireData()
     clearQuestionnaireResponseData()
     lastPullToken.delete()
-    questionnaireLastPullToken.delete()
-  }
-
-  private fun clearQuestionnaireData() {
-    appDatabase.questionnaireDao().clearData()
   }
 
   private fun clearQuestionnaireResponseData() {
@@ -127,35 +93,28 @@ class QuestionnaireResponseSyncIntegrationTest {
   fun syncing_records_should_work_as_expected() {
     // given
     val totalNumberOfRecords = batchSize * 2 + 1
-
-    questionnaireSync.pull()
-    val monthlyScreeningReportQuestionnaire = questionnaireRepository.questionnairesByType(MonthlyScreeningReports)
-
-    Truth.assertThat(monthlyScreeningReportQuestionnaire).isNotNull()
-
     val records = (1..totalNumberOfRecords).map {
       val uuid = UUID.randomUUID()
+      val questionnaireUuid = UUID.randomUUID()
       TestData.questionnaireResponse(
           uuid = uuid,
-          questionnaireId = monthlyScreeningReportQuestionnaire.uuid,
-          questionnaireType = monthlyScreeningReportQuestionnaire.questionnaire_type,
-          facilityId = currentFacilityUuid,
+          questionnaireId = questionnaireUuid,
           syncStatus = SyncStatus.PENDING
       )
     }
     Truth.assertThat(records).containsNoDuplicates()
 
-    questionnaireResponseRepository.save(records)
-    Truth.assertThat(questionnaireResponseRepository.pendingSyncRecordCount().blockingFirst()).isEqualTo(totalNumberOfRecords)
+    repository.save(records)
+    Truth.assertThat(repository.pendingSyncRecordCount().blockingFirst()).isEqualTo(totalNumberOfRecords)
 
     // when
-    questionnaireResponseSync.push()
+    sync.push()
     clearQuestionnaireResponseData()
-    questionnaireResponseSync.pull()
+    sync.pull()
 
     // then
     val expectedPulledRecords = records.map { it.syncCompleted() }
-    val pulledRecords = questionnaireResponseRepository.recordsWithSyncStatus(SyncStatus.DONE)
+    val pulledRecords = repository.recordsWithSyncStatus(SyncStatus.DONE)
 
     Truth.assertThat(pulledRecords).containsAtLeastElementsIn(expectedPulledRecords)
   }

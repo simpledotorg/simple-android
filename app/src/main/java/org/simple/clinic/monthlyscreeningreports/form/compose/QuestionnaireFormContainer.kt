ab53c781dc274035a2f957a2f91b7d4a18cced1f
Siddharth Agarwal <sagarwal@rtsl.org>
2023-03-07 18:01:50 +0530

    Change state of form submitted view to scrollable

--- app/src/main/java/org/simple/clinic/monthlyscreeningreports/form/compose/QuestionnaireFormContainer.kt
+++ app/src/main/java/org/simple/clinic/monthlyscreeningreports/form/compose/QuestionnaireFormContainer.kt
@@ -1,9 +1,7 @@
 package org.simple.clinic.monthlyscreeningreports.form.compose
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -22,8 +20,11 @@ fun QuestionnaireFormContainer(
 ) {
   Column(
       modifier = Modifier
-          .verticalScroll(state = ScrollState(0))
-          .padding(horizontal = dimensionResource(id = R.dimen.spacing_16))
+          .padding(
+              start = dimensionResource(id = R.dimen.spacing_16),
+              end = dimensionResource(id = R.dimen.spacing_16),
+              bottom = dimensionResource(id = R.dimen.spacing_24)
+          )
   ) {
     viewGroupComponentData.children?.forEach {
       when (it) {

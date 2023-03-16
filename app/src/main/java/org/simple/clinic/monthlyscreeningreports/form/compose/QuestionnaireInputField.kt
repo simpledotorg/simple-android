bca2fa0a5cd2807910d93a0b66ef4337eade94ec
Siddharth Agarwal <sagarwal@rtsl.org>
2023-03-16 12:04:16 +0530

    Only allow digits as input for screening form

--- app/src/main/java/org/simple/clinic/monthlyscreeningreports/form/compose/QuestionnaireInputField.kt
+++ app/src/main/java/org/simple/clinic/monthlyscreeningreports/form/compose/QuestionnaireInputField.kt
@@ -1,25 +1,21 @@
 package org.simple.clinic.monthlyscreeningreports.form.compose
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import org.simple.clinic.R
+import org.simple.clinic.monthlyscreeningreports.form.compose.util.getKeyBoardType
+import org.simple.clinic.monthlyscreeningreports.form.compose.util.getTextFieldColors
 import org.simple.clinic.questionnaire.component.InputFieldComponentData
 import org.simple.clinic.questionnaire.component.properties.Integer
 
@@ -36,28 +32,20 @@ fun InputField(
         selection = TextRange(initValue.length)
     ))
   }
+  val pattern = remember { Regex("^\\d+\$") }
   TextField(
       value = text,
       onValueChange = {
-        text = it
-        setContentValue(it.text, inputFieldComponentData, content)
+        if (it.text.isEmpty() || it.text.matches(pattern)) {
+          text = it
+          setContentValue(it.text, inputFieldComponentData, content)
+        }
       },
       label = { Text(text = inputFieldComponentData.text) },
       singleLine = true,
       shape = RoundedCornerShape(0.dp),
-      colors = TextFieldDefaults.textFieldColors(
-          containerColor = colorResource(id = R.color.white),
-          focusedLabelColor = colorResource(id = R.color.color_on_surface_67),
-          unfocusedLabelColor = colorResource(id = R.color.color_on_surface_67),
-          focusedIndicatorColor = colorResource(id = R.color.simple_light_blue_500),
-          unfocusedIndicatorColor = colorResource(id = R.color.color_on_surface_67),
-          cursorColor = colorResource(id = R.color.simple_light_blue_500)
-      ),
-      keyboardOptions = KeyboardOptions(
-          keyboardType = if (inputFieldComponentData.type == Integer)
-            KeyboardType.Number else KeyboardType.Text,
-          imeAction = ImeAction.Next
-      ),
+      colors = getTextFieldColors(),
+      keyboardOptions = getKeyBoardType(inputType = inputFieldComponentData.type),
       textStyle = TextStyle(
           fontFamily = FontFamily.SansSerif
       ),

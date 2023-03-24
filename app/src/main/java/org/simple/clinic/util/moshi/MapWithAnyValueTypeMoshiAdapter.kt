952468e07529a47f51173cdc4490a3d4836aefcc
Siddharth Agarwal <sagarwal@rtsl.org>
2023-03-24 14:13:54 +0530

    Private fun created to follow DRY approach and comments added for moshi adapter

--- app/src/main/java/org/simple/clinic/util/moshi/MapWithAnyValueTypeMoshiAdapter.kt
+++ app/src/main/java/org/simple/clinic/util/moshi/MapWithAnyValueTypeMoshiAdapter.kt
@@ -10,6 +10,9 @@ class MapWithAnyValueTypeMoshiAdapter {
     value?.forEach {
       map[it.key] = try {
         if (it.value is Double && (it.value as Double).rem(1).equals(0.0)) {
+          // This condition will convert 100.0 to 100 which is not intended for float/double values.
+          // Currently, `QuestionnaireResponsePayload.kt_content` only has integer values.
+          // Update this logic when float/double values get introduced.
           (it.value as Double).toInt()
         } else {
           it.value
--- app/src/main/java/org/simple/clinic/util/room/MapRoomTypeConverter.kt
+++ app/src/main/java/org/simple/clinic/util/room/MapRoomTypeConverter.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.util.room
 
 import androidx.room.TypeConverter
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.ToNumberPolicy
 import com.google.gson.reflect.TypeToken
@@ -10,19 +11,19 @@ import javax.inject.Inject
 class MapRoomTypeConverter @Inject constructor() {
   @TypeConverter
   fun fromMap(value: Map<String, Any?>): String {
-    val gson = GsonBuilder()
-        .serializeNulls()
-        .setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
-        .create()
-    return gson.toJson(value)
+    return getGson().toJson(value)
   }
 
   @TypeConverter
   fun toMap(value: String): Map<String, Any?> {
     val mapType: Type = object : TypeToken<Map<String?, Any?>?>() {}.type
+    return getGson().fromJson(value, mapType)
+  }
+
+  private fun getGson(): Gson {
     return GsonBuilder()
         .serializeNulls()
         .setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
-        .create().fromJson(value, mapType)
+        .create()
   }
 }

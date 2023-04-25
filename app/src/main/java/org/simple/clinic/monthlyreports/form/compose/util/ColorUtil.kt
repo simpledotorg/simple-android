694bb6209deaee51d0e4c60b07f6ecdc56a6023b
Siddharth Agarwal <sagarwal@rtsl.org>
2023-04-25 18:43:47 +0530

    Icon rendered from material icon using string name

---
+++ app/src/main/java/org/simple/clinic/monthlyreports/form/compose/util/ColorUtil.kt
@@ -0,0 +1,15 @@
+package org.simple.clinic.monthlyreports.form.compose.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import org.simple.clinic.R
+
+@Composable
+fun getColor(colorString: String): Color {
+  return try {
+    Color(android.graphics.Color.parseColor("#$colorString"))
+  } catch (_: Throwable) {
+    colorResource(id = R.color.simple_dark_grey)
+  }
+}

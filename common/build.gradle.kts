12ab307090f8cb138d7b3e6b5466c36fbc710798
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-06-06 03:53:55 +0530

    Move theme related XML files to common module

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.appcompat)
   implementation(libs.material)
+  implementation(libs.edittext.pinentry)
 
   val composeBom = platform(libs.androidx.compose.bom)
   implementation(composeBom)

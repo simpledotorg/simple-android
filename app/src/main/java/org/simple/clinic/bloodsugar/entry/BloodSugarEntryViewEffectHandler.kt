e3ccc824940a5f3a2e197d1f04f1cc73d1b69744
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-06-09 14:51:00 +0530

    Add `BloodSugarEntryUiActions` as dependency to `BloodSugarEntryViewEffectHandler`

--- app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntryViewEffectHandler.kt
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntryViewEffectHandler.kt
@@ -2,7 +2,9 @@ package org.simple.clinic.bloodsugar.entry
 
 import org.simple.clinic.mobius.ViewEffectsHandler
 
-class BloodSugarEntryViewEffectHandler : ViewEffectsHandler<BloodSugarEntryViewEffect> {
+class BloodSugarEntryViewEffectHandler(
+    private val uiActions: BloodSugarEntryUiActions
+) : ViewEffectsHandler<BloodSugarEntryViewEffect> {
 
   override fun handle(viewEffect: BloodSugarEntryViewEffect) {
     // no-op

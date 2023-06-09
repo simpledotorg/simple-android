868708fae1ae2548530c2947f6083e42ed25bdfd
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-06-09 14:41:16 +0530

    Implement `BloodSugarEntryViewEffectHandler`

---
+++ app/src/main/java/org/simple/clinic/bloodsugar/entry/BloodSugarEntryViewEffectHandler.kt
@@ -0,0 +1,10 @@
+package org.simple.clinic.bloodsugar.entry
+
+import org.simple.clinic.mobius.ViewEffectsHandler
+
+class BloodSugarEntryViewEffectHandler : ViewEffectsHandler<BloodSugarEntryViewEffect> {
+
+  override fun handle(viewEffect: BloodSugarEntryViewEffect) {
+    // no-op
+  }
+}

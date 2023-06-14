755711bdabd6d222c50859e0432ec6414b7365ae
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-06-14 08:28:02 +0530

    Remove old test cases that are no longer needed with new overdue query

--- app/src/androidTest/java/org/simple/clinic/overdue/AppointmentRepositoryAndroidTest.kt
+++ app/src/androidTest/java/org/simple/clinic/overdue/AppointmentRepositoryAndroidTest.kt
@@ -289,60 +289,6 @@ class AppointmentRepositoryAndroidTest {
   }
 
   @Test
-  fun when_fetching_overdue_appointments_it_should_exclude_appointments_more_than_a_year_overdue() {
-    fun createOverdueAppointment(
-        patientUuid: UUID,
-        scheduledDate: LocalDate,
-        facilityUuid: UUID
-    ) {
-      val patientProfile = testData.patientProfile(
-          patientUuid = patientUuid,
-          generatePhoneNumber = true
-      )
-      patientRepository.save(listOf(patientProfile))
-
-      val bp = testData.bloodPressureMeasurement(
-          patientUuid = patientUuid,
-          facilityUuid = facilityUuid
-      )
-      bpRepository.save(listOf(bp))
-
-      val appointment = testData.appointment(
-          patientUuid = patientUuid,
-          facilityUuid = facilityUuid,
-          scheduledDate = scheduledDate,
-          status = Scheduled,
-          cancelReason = null
-      )
-      appointmentRepository.save(listOf(appointment))
-    }
-
-    //given
-    val patientWithOneDayOverdue = UUID.fromString("9b794e72-6ebb-48c3-a8d7-69751ffeecc2")
-    val patientWithTenDaysOverdue = UUID.fromString("0fc57e45-7018-4c03-9218-f90f6fc0f268")
-    val patientWithOverAnYearDaysOverdue = UUID.fromString("51467803-f588-4a65-8def-7a15f41bdd13")
-
-    val now = LocalDate.now(clock)
-    val facilityUuid = UUID.fromString("ccc66ec1-5029-455b-bf92-caa6d90a9a79")
-
-    createOverdueAppointment(patientWithOneDayOverdue, now.minusDays(1), facilityUuid)
-    createOverdueAppointment(patientWithTenDaysOverdue, now.minusDays(10), facilityUuid)
-    createOverdueAppointment(patientWithOverAnYearDaysOverdue, now.minusDays(370), facilityUuid)
-
-    //when
-    val overduePatientUuids = PagingTestCase(pagingSource = appointmentRepository.overdueAppointmentsInFacility(since = now,
-        facilityId = facilityUuid),
-        loadSize = 10)
-        .loadPage()
-        .data
-        .map { it.appointment.patientUuid }
-
-    //then
-    assertThat(overduePatientUuids).containsExactly(patientWithOneDayOverdue, patientWithTenDaysOverdue)
-    assertThat(overduePatientUuids).doesNotContain(patientWithOverAnYearDaysOverdue)
-  }
-
-  @Test
   fun when_fetching_appointment_for_patient_it_should_return_the_last_created_appointment() {
     // given
     val scheduledDateForFirstAppointment = LocalDate.parse("2018-02-01")

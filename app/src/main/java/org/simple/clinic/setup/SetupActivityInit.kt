94cbc07a7bcec391611790276704c7d6f312d841
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-06-26 10:17:08 +0530

    Execute database encryption when setup activity is created

--- app/src/main/java/org/simple/clinic/setup/SetupActivityInit.kt
+++ app/src/main/java/org/simple/clinic/setup/SetupActivityInit.kt
@@ -7,6 +7,6 @@ import org.simple.clinic.mobius.first
 class SetupActivityInit : Init<SetupActivityModel, SetupActivityEffect> {
 
   override fun init(model: SetupActivityModel): First<SetupActivityModel, SetupActivityEffect> {
-    return first(model, CheckIfAppCanRun)
+    return first(model, ExecuteDatabaseEncryption)
   }
 }
--- app/src/test/java/org/simple/clinic/setup/SetupActivityInitTest.kt
+++ app/src/test/java/org/simple/clinic/setup/SetupActivityInitTest.kt
@@ -11,7 +11,7 @@ import java.time.Instant
 class SetupActivityInitTest {
 
   @Test
-  fun `when the screen is created, check whether the app is allowed to run`() {
+  fun `when the screen is created, then execute database encryption`() {
     val spec = InitSpec(SetupActivityInit())
     val model = SetupActivityModel.create(TestUtcClock(Instant.parse("2018-01-01T00:00:00Z")))
 
@@ -19,7 +19,7 @@ class SetupActivityInitTest {
         .whenInit(model)
         .then(assertThatFirst(
             hasModel(model),
-            hasEffects(CheckIfAppCanRun)
+            hasEffects(ExecuteDatabaseEncryption)
         ))
   }
 }

3bf4e20481952049b2c5f4272bd6cb3eb18814c9
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-07-31 09:00:39 +0530

    Remove `MixpanelAnalyticsReporter`

--- app/src/main/java/org/simple/clinic/ReleaseClinicApp.kt
+++ app/src/main/java/org/simple/clinic/ReleaseClinicApp.kt
@@ -4,12 +4,10 @@ import android.annotation.SuppressLint
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.Logger
 import org.simple.clinic.analytics.FirebaseAnalyticsReporter
-import org.simple.clinic.analytics.MixpanelAnalyticsReporter
 import org.simple.clinic.analytics.swallowErrors
 import org.simple.clinic.di.AppComponent
 import org.simple.clinic.di.AppModule
 import org.simple.clinic.di.DaggerAppComponent
-import org.simple.clinic.storage.monitoring.Sampler
 import org.simple.clinic.util.unsafeLazy
 import org.slf4j.LoggerFactory
 
@@ -17,14 +15,7 @@ import org.slf4j.LoggerFactory
 class ReleaseClinicApp : ClinicApp() {
 
   override val analyticsReporters by unsafeLazy {
-    val mixpanelSamplingRate = remoteConfig.double("mixpanel_sampling_rate", 0.0)
-    val sampler = Sampler(mixpanelSamplingRate.toFloat())
-
     listOf(
-        MixpanelAnalyticsReporter(
-            app = this,
-            sampler = sampler
-        ).swallowErrors(),
         FirebaseAnalyticsReporter(this).swallowErrors()
     )
   }

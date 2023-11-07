852eb8febfd45ca1191a3bf8e1558cfbf4923ce4
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-11-07 11:27:33 +0530

    Add Compose Livedata dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -437,6 +437,7 @@ dependencies {
   implementation(composeBom)
   androidTestImplementation(composeBom)
   implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.compose.livedata)
   implementation(libs.androidx.compose.material.iconsExtended)
   implementation(libs.androidx.compose.ui.tooling.preview)
   debugImplementation(libs.androidx.compose.ui.tooling)

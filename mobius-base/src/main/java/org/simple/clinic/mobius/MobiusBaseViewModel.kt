8ced0ae4ce6a2659e015739b73b04bab33f405b1
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-11-08 10:00:54 +0530

    Add `MobiusBaseViewModel`
    This is added so that we can support saved state handle in ViewModel for saving and restoring state after process death
    
    https://developer.android.com/topic/libraries/architecture/viewmodel/viewmodel-savedstate

---
+++ mobius-base/src/main/java/org/simple/clinic/mobius/MobiusBaseViewModel.kt
@@ -0,0 +1,51 @@
+package org.simple.clinic.mobius
+
+import android.os.Parcelable
+import androidx.lifecycle.Observer
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.distinctUntilChanged
+import com.spotify.mobius.Init
+import com.spotify.mobius.MobiusLoop
+import com.spotify.mobius.android.MobiusLoopViewModel
+import com.spotify.mobius.android.runners.MainThreadWorkRunner
+import com.spotify.mobius.functions.Consumer
+import timber.log.Timber
+
+private const val MAX_EFFECTS_QUEUE_SIZE = 100
+
+class MobiusBaseViewModel<M : Parcelable, E, F, V>(
+    private val modelKey: String,
+    private val savedStateHandle: SavedStateHandle,
+    loopFactoryProvider: (Consumer<V>) -> MobiusLoop.Builder<M, E, F>,
+    defaultModel: M,
+    init: Init<M, F>,
+) : MobiusLoopViewModel<M, E, F, V>(
+    loopFactoryProvider,
+    savedStateHandle.get<M>(modelKey) ?: defaultModel,
+    init,
+    MainThreadWorkRunner.create(),
+    MAX_EFFECTS_QUEUE_SIZE
+) {
+
+  @get:JvmName("distinctSavedStateModels")
+  val models = savedStateHandle.getLiveData<M>(modelKey).distinctUntilChanged()
+
+  // At the time of writing, `MobiusLoopViewModel` function doesn't provide a public function
+  // to hook into model changes in the ViewModel. `onModelChanged` function is private.
+  // Instead we are observing the `getModels` livedata to update the saved state handle.
+  private val modelsObserver = Observer<M> {
+    val currentSavedModel = savedStateHandle.get<M>(modelKey)
+    if (currentSavedModel != it) {
+      Timber.d("Updating state: $it")
+      savedStateHandle[modelKey] = it
+    }
+  }
+
+  init {
+    getModels().distinctUntilChanged().observeForever(modelsObserver)
+  }
+
+  override fun onClearedInternal() {
+    getModels().removeObserver(modelsObserver)
+  }
+}

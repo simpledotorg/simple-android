6405d19ea14a8848e24567bf5dc7f629f2c50267
Siddharth Agarwal <sagarwal@rtsl.org>
2024-05-08 15:20:32 +0530

    Hide patient reassignment view in recent item list if feature flag is disabled

--- app/src/main/java/org/simple/clinic/recentpatientsview/RecentPatientItem.kt
+++ app/src/main/java/org/simple/clinic/recentpatientsview/RecentPatientItem.kt
@@ -31,14 +31,23 @@ sealed class RecentPatientItemType : ItemAdapter.Item<UiEvent> {
     ): List<RecentPatientItemType> {
       val today = LocalDate.now(userClock)
 
-      return recentPatients.map { recentPatientItem(it, today, userClock, dateFormatter) }
+      return recentPatients.map {
+        recentPatientItem(
+            it,
+            today,
+            userClock,
+            dateFormatter,
+            isPatientReassignmentFeatureEnabled
+        )
+      }
     }
 
     private fun recentPatientItem(
         recentPatient: RecentPatient,
         today: LocalDate,
         userClock: UserClock,
-        dateFormatter: DateTimeFormatter
+        dateFormatter: DateTimeFormatter,
+        isPatientReassignmentFeatureEnabled: Boolean,
     ): RecentPatientItem {
       val patientRegisteredOnDate = recentPatient.patientRecordedAt.toLocalDateAtZone(userClock.zone)
       val isNewRegistration = today == patientRegisteredOnDate
@@ -52,7 +61,8 @@ sealed class RecentPatientItemType : ItemAdapter.Item<UiEvent> {
           dateFormatter = dateFormatter,
           clock = userClock,
           isNewRegistration = isNewRegistration,
-          isEligibleForReassignment = recentPatient.isEligibleForReassignment
+          isEligibleForReassignment = isPatientReassignmentFeatureEnabled &&
+              recentPatient.isEligibleForReassignment
       )
     }
   }

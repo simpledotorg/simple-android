6a8e8d3f11a04c37a87a3c8f9c7d49f753831931
Sasikanth Miriyampalli <hello@sasikanth.dev>
2024-07-22 11:06:52 +0530

    Add function to get blood pressure measurements paging source in `BloodPressureRepository`

--- app/src/main/java/org/simple/clinic/bp/BloodPressureRepository.kt
+++ app/src/main/java/org/simple/clinic/bp/BloodPressureRepository.kt
@@ -1,6 +1,7 @@
 package org.simple.clinic.bp
 
 import androidx.paging.DataSource
+import androidx.paging.PagingSource
 import io.reactivex.Completable
 import io.reactivex.Observable
 import org.simple.clinic.bp.sync.BloodPressureMeasurementPayload
@@ -142,6 +143,10 @@ class BloodPressureRepository @Inject constructor(
 
   fun allBloodPressuresDataSource(patientUuid: UUID): DataSource.Factory<Int, BloodPressureMeasurement> {
     return dao.allBloodPressuresDataSource(patientUuid)
+  }
+
+  fun allBloodPressuresPagingSource(patientUuid: UUID): PagingSource<Int, BloodPressureMeasurement> {
+    return dao.allBloodPressuresPagingSource(patientUuid)
   }
 
   override fun pendingSyncRecordCount(): Observable<Int> {

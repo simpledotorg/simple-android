953343b694bad9512677826e99aac656388c465f
Siddharth Agarwal <sagarwal@rtsl.org>
2024-10-28 18:00:20 +0530

    Refactor code

--- app/src/main/java/org/simple/clinic/summary/compose/StatinNudgeView.kt
+++ app/src/main/java/org/simple/clinic/summary/compose/StatinNudgeView.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +16,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -32,118 +33,114 @@ fun StatinNudge(
     isVisible: Boolean,
     modifier: Modifier = Modifier
 ) {
-    AnimatedVisibility(
-        visible = isVisible,
+  AnimatedVisibility(
+      visible = isVisible,
+  ) {
+    Card(
+        modifier = modifier
     ) {
-        Card(
-            modifier = modifier
-        ) {
-            Column(
+      Column(
+          modifier = Modifier
+              .padding(16.dp),
+          horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+        Row {
+          Box(
+              modifier = Modifier
+                  .weight(2f)
+          )
+          Column(
+              modifier = Modifier
+                  .weight(3f),
+              horizontalAlignment = Alignment.CenterHorizontally
+          ) {
+            Text(
                 modifier = Modifier
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Row {
-                    Box(
-                        modifier = Modifier
-                            .weight(2f)
-                    )
-                    Column(
-                        modifier = Modifier
-                            .weight(3f),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .background(SimpleTheme.colors.material.error, shape = RoundedCornerShape(50))
-                                .padding(horizontal = 8.dp, vertical = 4.dp),
-                            style = SimpleTheme.typography.material.button,
-                            color = SimpleTheme.colors.onToolbarPrimary,
-                            text = stringResource(R.string.statin_alert_at_risk_patient)
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(4.dp))
-                RiskProgressBar()
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = buildAnnotatedString {
-                        append(stringResource(R.string.statin_alert_refer_to_doctor_for))
-                        append(" ")
-                        withStyle(style = SimpleTheme.typography.body2Bold.toSpanStyle()) {
-                            append(stringResource(R.string.statin_alert_statin_medicine))
-                        }
-                    },
-                    color = SimpleTheme.colors.material.error,
-                    style = SimpleTheme.typography.material.body2,
-                )
-            }
+                    .background(SimpleTheme.colors.material.error, shape = RoundedCornerShape(50))
+                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                style = SimpleTheme.typography.material.button,
+                color = SimpleTheme.colors.onToolbarPrimary,
+                text = stringResource(R.string.statin_alert_at_risk_patient)
+            )
+          }
         }
+        Spacer(modifier = Modifier.height(4.dp))
+        RiskProgressBar()
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = buildAnnotatedString {
+              append(stringResource(R.string.statin_alert_refer_to_doctor_for))
+              append(" ")
+              withStyle(style = SimpleTheme.typography.body2Bold.toSpanStyle()) {
+                append(stringResource(R.string.statin_alert_statin_medicine))
+              }
+            },
+            color = SimpleTheme.colors.material.error,
+            style = SimpleTheme.typography.material.body2,
+        )
+      }
     }
+  }
 }
 
 @Composable
 fun RiskProgressBar() {
-    val riskColors = listOf(
-        Color(0xFF00B849), // Very Low
-        Color(0xFFFFC800), // Low
-        SimpleTheme.colors.material.error, // Medium
-        Color(0xFFB81631), // High
-        Color(0xFF731814)  // Very High
-    )
+  val riskColors = listOf(
+      Color(0xFF00B849), // Very Low
+      Color(0xFFFFC800), // Low
+      SimpleTheme.colors.material.error, // Medium
+      Color(0xFFB81631), // High
+      Color(0xFF731814)  // Very High
+  )
 
-    Box(
+  val indicatorColor = Color(0xFF2F363D)
+
+  Box(
+      modifier = Modifier
+          .fillMaxWidth()
+          .height(14.dp)
+          .drawWithContent {
+              drawContent()
+
+              val widthPerSegment = size.width / riskColors.size
+
+              drawLine(
+                  color = indicatorColor,
+                  start = Offset(2 * widthPerSegment, 0f),
+                  end = Offset(2 * widthPerSegment, size.height),
+                  strokeWidth = 2.dp.toPx()
+              )
+              drawLine(
+                  color = indicatorColor,
+                  start = Offset(size.width, 0f),
+                  end = Offset(size.width, size.height),
+                  strokeWidth = 2.dp.toPx()
+              )
+          },
+      contentAlignment = Alignment.Center,
+  ) {
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(14.dp)
-            .drawWithContent {
-                drawContent()
-
-                val widthPerSegment = size.width / riskColors.size
-
-                drawLine(
-                    color = Color(0xFF2F363D),
-                    start = Offset(2 * widthPerSegment, 0f),
-                    end = Offset(2 * widthPerSegment, size.height),
-                    strokeWidth = 2.dp.toPx()
-                )
-                drawLine(
-                    color = Color(0xFF2F363D),
-                    start = Offset(size.width, 0f),
-                    end = Offset(size.width, size.height),
-                    strokeWidth = 2.dp.toPx()
-                )
-            }
+            .height(4.dp)
+            .clip(RoundedCornerShape(50))
     ) {
-        Row(
+      riskColors.forEach { color ->
+        Box(
             modifier = Modifier
-                .fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            riskColors.forEachIndexed { index, color ->
-                Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .height(4.dp)
-                        .background(
-                            color,
-                            shape = when (index) {
-                                0 -> RoundedCornerShape(topStartPercent = 50, bottomStartPercent = 50)
-                                riskColors.size - 1 -> RoundedCornerShape(topEndPercent = 50, bottomEndPercent = 50)
-                                else -> RoundedCornerShape(0)
-                            }
-                        )
-
-                )
-            }
-        }
+                .weight(1f)
+                .fillMaxHeight()
+                .background(color),
+        )
+      }
     }
+  }
 }
 
 @Preview
 @Composable
 fun StatinNudgePreview() {
-    SimpleTheme {
-        StatinNudge(true)
-    }
+  SimpleTheme {
+    StatinNudge(true)
+  }
 }

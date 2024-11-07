47c0605d99074911bdc77bab80782639145a0a24
Siddharth Agarwal <sagarwal@rtsl.org>
2024-11-07 12:31:54 +0530

    Remove duplicate file

--- app/src/main/java/org/simple/clinic/summary/compose/StatinNudgeView.kt
+++ app/src/main/java/org/simple/clinic/summary/compose/StatinNudgeView.kt
@@ -28,7 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.simple.clinic.R
 import org.simple.clinic.common.ui.theme.SimpleTheme
-import org.simple.clinic.common.ui.util.toAnnotatedString
+import org.simple.clinic.util.toAnnotatedString
 
 
 @Composable
@@ -102,22 +102,22 @@ fun RiskProgressBar() {
           .fillMaxWidth()
           .height(14.dp)
           .drawWithContent {
-            drawContent()
+              drawContent()
 
-            val widthPerSegment = size.width / riskColors.size
+              val widthPerSegment = size.width / riskColors.size
 
-            drawLine(
-                color = indicatorColor,
-                start = Offset(2 * widthPerSegment, 0f),
-                end = Offset(2 * widthPerSegment, size.height),
-                strokeWidth = 2.dp.toPx()
-            )
-            drawLine(
-                color = indicatorColor,
-                start = Offset(size.width, 0f),
-                end = Offset(size.width, size.height),
-                strokeWidth = 2.dp.toPx()
-            )
+              drawLine(
+                  color = indicatorColor,
+                  start = Offset(2 * widthPerSegment, 0f),
+                  end = Offset(2 * widthPerSegment, size.height),
+                  strokeWidth = 2.dp.toPx()
+              )
+              drawLine(
+                  color = indicatorColor,
+                  start = Offset(size.width, 0f),
+                  end = Offset(size.width, size.height),
+                  strokeWidth = 2.dp.toPx()
+              )
           },
       contentAlignment = Alignment.Center,
   ) {
--- app/src/main/java/org/simple/clinic/util/Strings.kt
+++ app/src/main/java/org/simple/clinic/util/Strings.kt
@@ -1,5 +1,14 @@
 package org.simple.clinic.util
 
+import android.graphics.Typeface
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import android.text.style.StyleSpan
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import java.util.UUID
 
 fun String?.nullIfBlank(): String? {
@@ -16,4 +25,26 @@ fun String?.asUuid(): UUID? = try {
   UUID.fromString(this)
 } catch (e: IllegalArgumentException) {
   null
+}
+
+fun String.toAnnotatedString() = getSpannedText(this).toAnnotatedString()
+
+private fun Spanned.toAnnotatedString(): AnnotatedString = buildAnnotatedString {
+  val spanned = this@toAnnotatedString
+  append(spanned.toString())
+  getSpans(0, spanned.length, Any::class.java).forEach { span ->
+    val start = getSpanStart(span)
+    val end = getSpanEnd(span)
+    if (span is StyleSpan && span.style == Typeface.BOLD) {
+      addStyle(SpanStyle(fontWeight = FontWeight.Bold), start, end)
+    }
+  }
+}
+
+private fun getSpannedText(text: String): Spanned {
+  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+    Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT)
+  } else {
+    Html.fromHtml(text)
+  }
 }

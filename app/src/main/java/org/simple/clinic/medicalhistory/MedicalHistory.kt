5f77e5402cbf3ce51e986fc3a560f99e255bb2ca
Siddharth Agarwal <sagarwal@rtsl.org>
2024-12-04 16:08:17 +0530

    Add `Current Smoker ?` to `NewMedicalHistoryScreen`

--- app/src/main/java/org/simple/clinic/medicalhistory/MedicalHistory.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/MedicalHistory.kt
@@ -22,6 +22,7 @@ import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HasHadAKidneyDise
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HasHadAStroke
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IsOnDiabetesTreatment
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IsOnHypertensionTreatment
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IsSmoker
 import org.simple.clinic.patient.PatientUuid
 import org.simple.clinic.patient.SyncStatus
 import java.time.Instant
@@ -80,6 +81,7 @@ data class MedicalHistory(
       DiagnosedWithDiabetes -> copy(diagnosedWithDiabetes = answer)
       is IsOnHypertensionTreatment -> copy(isOnHypertensionTreatment = answer)
       IsOnDiabetesTreatment -> copy(isOnDiabetesTreatment = answer)
+      IsSmoker -> copy(isSmoker = answer)
     }
   }
 
--- app/src/main/java/org/simple/clinic/medicalhistory/OngoingMedicalHistoryEntry.kt
+++ app/src/main/java/org/simple/clinic/medicalhistory/OngoingMedicalHistoryEntry.kt
@@ -10,6 +10,7 @@ import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HasHadAKidneyDise
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.HasHadAStroke
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IsOnDiabetesTreatment
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IsOnHypertensionTreatment
+import org.simple.clinic.medicalhistory.MedicalHistoryQuestion.IsSmoker
 
 @Parcelize
 data class OngoingMedicalHistoryEntry(
@@ -32,6 +33,7 @@ data class OngoingMedicalHistoryEntry(
       DiagnosedWithDiabetes -> copy(hasDiabetes = answer)
       is IsOnHypertensionTreatment -> copy(isOnHypertensionTreatment = answer)
       IsOnDiabetesTreatment -> copy(isOnDiabetesTreatment = answer)
+      IsSmoker -> copy(isSmoker = answer)
     }
   }
 }

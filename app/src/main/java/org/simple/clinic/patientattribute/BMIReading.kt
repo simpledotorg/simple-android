a2767fb1d2299bc0144caf7b770fac036860f492
Siddharth Agarwal <sagarwal@rtsl.org>
2024-12-12 18:24:50 +0530

    Add method `calculateBMI`

--- app/src/main/java/org/simple/clinic/patientattribute/BMIReading.kt
+++ app/src/main/java/org/simple/clinic/patientattribute/BMIReading.kt
@@ -4,4 +4,20 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class BMIReading(val height: String, val weight: String) : Parcelable
+data class BMIReading(val height: String, val weight: String) : Parcelable {
+
+  fun calculateBMI(): Double? {
+    if (height.isBlank() || weight.isBlank()) return null
+
+    try {
+      val heightInMeters = height.toDouble() / 100
+      val weightInKg = weight.toDouble()
+
+      if (heightInMeters <= 0 || weightInKg <= 0) return null
+
+      return weightInKg / (heightInMeters * heightInMeters)
+    } catch (e: NumberFormatException) {
+      return null
+    }
+  }
+}

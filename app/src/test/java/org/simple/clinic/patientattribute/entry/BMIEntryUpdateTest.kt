8fe2bcb52a0c9fece8680a78605dd051edc0581a
Siddharth Agarwal <sagarwal@rtsl.org>
2024-12-20 00:07:58 +0530

    Handle Height and weight entry changes

--- app/src/test/java/org/simple/clinic/patientattribute/entry/BMIEntryUpdateTest.kt
+++ app/src/test/java/org/simple/clinic/patientattribute/entry/BMIEntryUpdateTest.kt
@@ -1,6 +1,8 @@
 package org.simple.clinic.patientattribute.entry
 
 import com.spotify.mobius.test.NextMatchers.hasEffects
+import com.spotify.mobius.test.NextMatchers.hasModel
+import com.spotify.mobius.test.NextMatchers.hasNoEffects
 import com.spotify.mobius.test.NextMatchers.hasNoModel
 import com.spotify.mobius.test.UpdateSpec
 import com.spotify.mobius.test.UpdateSpec.assertThatNext
@@ -37,6 +39,65 @@ class BMIEntryUpdateTest {
         .then(assertThatNext(
             hasNoModel(),
             hasEffects(CloseSheet)
+        ))
+  }
+
+  @Test
+  fun `when height is changed and length is not 3, then update the model`() {
+    val height = "17"
+    spec
+        .given(defaultModel)
+        .whenEvent(HeightChanged(height))
+        .then(assertThatNext(
+            hasModel(defaultModel.heightChanged(height)),
+            hasNoEffects()
+        ))
+  }
+
+  @Test
+  fun `when height is changed and length is 3, then update the model and change the focus to weight`() {
+    val height = "177"
+    spec
+        .given(defaultModel)
+        .whenEvent(HeightChanged(height))
+        .then(assertThatNext(
+            hasModel(defaultModel.heightChanged(height)),
+            hasEffects(ChangeFocusToWeight)
+        ))
+  }
+
+  @Test
+  fun `when weight is changed, then update the model`() {
+    val weight = "68"
+    spec
+        .given(defaultModel)
+        .whenEvent(WeightChanged(weight))
+        .then(assertThatNext(
+            hasModel(defaultModel.weightChanged(weight)),
+            hasNoEffects()
+        ))
+  }
+
+  @Test
+  fun `when weight backspace is clicked and is not empty, then update the model`() {
+    val model = defaultModel.weightChanged("68")
+    spec
+        .given(model)
+        .whenEvent(WeightBackspaceClicked)
+        .then(assertThatNext(
+            hasModel(model.deleteWeightLastDigit()),
+            hasNoEffects()
+        ))
+  }
+
+  @Test
+  fun `when weight backspace is clicked and is empty, then update the model and change the focus to height`() {
+    spec
+        .given(defaultModel)
+        .whenEvent(WeightBackspaceClicked)
+        .then(assertThatNext(
+            hasModel(defaultModel.deleteWeightLastDigit()),
+            hasEffects(ChangeFocusToHeight)
         ))
   }
 }

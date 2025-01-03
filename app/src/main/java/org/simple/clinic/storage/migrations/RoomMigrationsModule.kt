54c41b4b7dd9bb8187e74f3d781590c0cf2a78eb
Siddharth Agarwal <sagarwal@rtsl.org>
2025-01-03 13:18:02 +0530

    Refactor code

--- app/src/main/java/org/simple/clinic/storage/migrations/RoomMigrationsModule.kt
+++ app/src/main/java/org/simple/clinic/storage/migrations/RoomMigrationsModule.kt
@@ -123,7 +123,7 @@ class RoomMigrationsModule {
       migration115: Migration_115,
       migration116: Migration_116,
       migration117: Migration_117,
-      migration118: Migration_118
+      migration118: Migration_118,
   ): List<Migration> {
     return listOf(
         migration_3_4,
@@ -240,7 +240,7 @@ class RoomMigrationsModule {
         migration115,
         migration116,
         migration117,
-        migration118
+        migration118,
     )
   }
 }

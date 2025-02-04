530bfe9024aff9fe9b91424f421b62ea5f10e174
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-02-04 10:53:01 +0530

    Add benchmark plugin to `mobius-base` module
    We can only add the benchmark plugin to library modules and we currently don't have a benchmark module. So, as hack, we are applying it for `mobius-base` module to get `lockClocks` task in the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.android.lint) apply false
+  alias(libs.plugins.androidx.benchmark) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.kotlin.parcelize) apply false

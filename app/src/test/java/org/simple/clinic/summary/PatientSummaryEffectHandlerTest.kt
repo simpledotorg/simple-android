1392cec9122516a7299f8a043527042459557f95
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-02-27 15:01:22 +0530

    Add view effect to open cholesterol entry sheet

--- app/src/test/java/org/simple/clinic/summary/PatientSummaryEffectHandlerTest.kt
+++ app/src/test/java/org/simple/clinic/summary/PatientSummaryEffectHandlerTest.kt
@@ -1043,4 +1043,15 @@ class PatientSummaryEffectHandlerTest {
     verify(uiActions).openBMIEntrySheet(patientUuid)
     verifyNoMoreInteractions(uiActions)
   }
+
+  @Test
+  fun `when open cholesterol entry sheet effect is received, then open cholesterol entry sheet`() {
+    // when
+    testCase.dispatch(OpenCholesterolEntrySheet(patientUuid = patientUuid))
+
+    // then
+    testCase.assertNoOutgoingEvents()
+    verify(uiActions).openCholesterolEntrySheet(patientUuid = patientUuid)
+    verifyNoMoreInteractions(uiActions)
+  }
 }

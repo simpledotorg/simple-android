80fecb4585130afbc80726791465362e04275b6b
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-05-06 12:52:17 +0530

    Add extension function to apply status bar padding to `AppBarLayout`

---
+++ app/src/main/java/org/simple/clinic/util/WindowInsetsExt.kt
@@ -0,0 +1,59 @@
+package org.simple.clinic.util
+
+import android.view.View
+import androidx.core.graphics.Insets
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsCompat.Type
+import androidx.core.view.WindowInsetsCompat.Type.InsetsType
+import androidx.core.view.updatePadding
+import com.google.android.material.appbar.AppBarLayout
+
+data class InsetsInitialPadding(
+    val start: Int,
+    val top: Int,
+    val end: Int,
+    val bottom: Int,
+)
+
+fun AppBarLayout.applyStatusBarPadding() {
+  updateInsetPadding(typeMask = Type.statusBars()) { view, insets, initialPadding ->
+    view.updatePadding(top = initialPadding.top + insets.top)
+  }
+}
+
+fun View.updateInsetPadding(
+    @InsetsType typeMask: Int,
+    block: (View, Insets, InsetsInitialPadding) -> Unit
+) {
+  val initialPadding = InsetsInitialPadding(
+      start = paddingStart,
+      top = paddingTop,
+      end = paddingEnd,
+      bottom = paddingBottom,
+  )
+
+  setOnApplyWindowInsetsListener { view, systemInsets ->
+    val insetsCompat = WindowInsetsCompat.toWindowInsetsCompat(systemInsets)
+    val insets = insetsCompat.getInsets(typeMask)
+    block(view, insets, initialPadding)
+
+    systemInsets
+  }
+
+  requestApplyInsetsWhenAttached()
+}
+
+fun View.requestApplyInsetsWhenAttached() {
+  if (isAttachedToWindow) {
+    requestApplyInsets()
+  } else {
+    addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+      override fun onViewAttachedToWindow(v: View) {
+        v.removeOnAttachStateChangeListener(this)
+        v.requestApplyInsets()
+      }
+
+      override fun onViewDetachedFromWindow(v: View) = Unit
+    })
+  }
+}

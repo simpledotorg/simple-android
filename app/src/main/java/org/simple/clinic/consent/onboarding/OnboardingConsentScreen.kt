6930ba11166cf004b16e3df31443834ab554438c
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-05-13 10:01:11 +0530

    Apply status bar padding to `OnboardingConsentScreen`

--- app/src/main/java/org/simple/clinic/consent/onboarding/OnboardingConsentScreen.kt
+++ app/src/main/java/org/simple/clinic/consent/onboarding/OnboardingConsentScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
@@ -136,6 +137,7 @@ private fun OnboardingConsentScreen(
                 drawContent()
               }
             }
+            .statusBarsPadding()
     ) {
       Image(
           modifier = Modifier

0e1758fc2772f37fba7d9b1c0a08c3760c4bdf68
Siddharth Agarwal <sagarwal@rtsl.org>
2025-05-16 13:03:09 +0530

    Add `OverduePendingListFooter`

---
+++ app/src/main/java/org/simple/clinic/home/overdue/compose/OverduePendingListFooter.kt
@@ -0,0 +1,33 @@
+package org.simple.clinic.home.overdue.compose
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.simple.clinic.R
+import org.simple.clinic.home.overdue.PendingListState
+
+@Composable
+fun OverduePendingListFooter(
+    modifier: Modifier = Modifier,
+    pendingListState: PendingListState,
+    onClick: () -> Unit,
+) {
+  val buttonTextResource = when (pendingListState) {
+    PendingListState.SEE_ALL -> R.string.overdue_pending_list_button_see_less
+    PendingListState.SEE_LESS -> R.string.overdue_pending_list_button_see_all
+  }
+
+  TextButton(
+      onClick = onClick,
+      modifier = modifier
+          .fillMaxWidth()
+          .heightIn(min = 48.dp)
+  ) {
+    Text(text = stringResource(id = buttonTextResource).uppercase())
+  }
+}

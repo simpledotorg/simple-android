d73e23f57e8a8029010fc46382f23d8dbe8312d0
Siddharth Agarwal <sagarwal@rtsl.org>
2025-06-10 13:00:35 +0530

    Add `OverdueUiModelMapper`

---
+++ app/src/main/java/org/simple/clinic/home/overdue/compose/OverdueUiModelMapper.kt
@@ -0,0 +1,442 @@
+package org.simple.clinic.home.overdue.compose
+
+import org.simple.clinic.R
+import org.simple.clinic.home.overdue.OverdueAppointment
+import org.simple.clinic.home.overdue.OverdueAppointmentSectionTitle
+import org.simple.clinic.home.overdue.OverdueAppointmentSectionTitle.AGREED_TO_VISIT
+import org.simple.clinic.home.overdue.OverdueAppointmentSectionTitle.MORE_THAN_A_YEAR_OVERDUE
+import org.simple.clinic.home.overdue.OverdueAppointmentSectionTitle.PENDING_TO_CALL
+import org.simple.clinic.home.overdue.OverdueAppointmentSectionTitle.REMIND_TO_CALL
+import org.simple.clinic.home.overdue.OverdueAppointmentSectionTitle.REMOVED_FROM_OVERDUE
+import org.simple.clinic.home.overdue.OverdueAppointmentSections
+import org.simple.clinic.home.overdue.OverdueListSectionStates
+import org.simple.clinic.home.overdue.PendingListState.SEE_ALL
+import org.simple.clinic.home.overdue.PendingListState.SEE_LESS
+import org.simple.clinic.patient.Answer
+import org.simple.clinic.util.UserClock
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+import java.util.Locale
+import java.util.UUID
+
+class OverdueUiModelMapper {
+
+  companion object {
+
+    fun from(
+        overdueAppointmentSections: OverdueAppointmentSections,
+        clock: UserClock,
+        pendingListDefaultStateSize: Int,
+        overdueListSectionStates: OverdueListSectionStates,
+        isOverdueInstantSearchEnabled: Boolean,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        selectedOverdueAppointments: Set<UUID>,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        locale: Locale,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+        onToggleSection: (OverdueAppointmentSectionTitle) -> Unit,
+        onPendingFooterClicked: () -> Unit,
+        onSearchButtonClicked: () -> Unit,
+    ): List<OverdueUiModel> {
+      val searchOverduePatientsButtonListItem = searchOverduePatientItem(
+          isOverdueInstantSearchEnabled,
+          onSearchButtonClicked
+      )
+
+      val pendingToCallListItem = pendingToCallItem(
+          overdueAppointmentSections,
+          clock,
+          pendingListDefaultStateSize,
+          overdueListSectionStates,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          locale,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection,
+          onToggleSection,
+          onPendingFooterClicked
+      )
+
+      val agreedToVisitListItem = agreedToVisitItem(
+          overdueAppointmentSections,
+          clock,
+          overdueListSectionStates,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          locale,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection,
+          onToggleSection
+      )
+
+      val remindToCallListItem = remindToCallItem(
+          overdueAppointmentSections,
+          clock,
+          overdueListSectionStates,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          locale,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection,
+          onToggleSection
+      )
+
+      val removedFromOverdueListItem = removedFromOverdueItem(
+          overdueAppointmentSections,
+          clock,
+          overdueListSectionStates,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          locale,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection,
+          onToggleSection
+      )
+
+      val moreThanAnOneYearOverdueListItem = moreThanAnOneYearOverdueItem(
+          overdueAppointmentSections,
+          clock,
+          overdueListSectionStates,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          locale,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection,
+          onToggleSection
+      )
+
+      val dividerListItem = listOf(OverdueUiModel.Divider)
+
+      return searchOverduePatientsButtonListItem +
+          pendingToCallListItem + dividerListItem +
+          agreedToVisitListItem + dividerListItem +
+          remindToCallListItem + dividerListItem +
+          removedFromOverdueListItem + dividerListItem +
+          moreThanAnOneYearOverdueListItem
+    }
+
+
+    private fun searchOverduePatientItem(
+        isOverdueInstantSearchEnabled: Boolean,
+        onSearchButtonClicked: () -> Unit,
+    ): List<OverdueUiModel> =
+        if (isOverdueInstantSearchEnabled) {
+          listOf(OverdueUiModel.SearchButton(onSearchClicked = onSearchButtonClicked))
+        } else {
+          emptyList()
+        }
+
+    private fun moreThanAnOneYearOverdueItem(
+        overdueAppointmentSections: OverdueAppointmentSections,
+        clock: UserClock,
+        overdueListSectionStates: OverdueListSectionStates,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        selectedOverdueAppointments: Set<UUID>,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        locale: Locale,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+        onToggleSection: (OverdueAppointmentSectionTitle) -> Unit,
+    ): List<OverdueUiModel> {
+      val moreThanAnOneYearOverdueHeader = listOf(
+          OverdueUiModel.Header(R.string.overdue_no_visit_in_one_year_call_header,
+              overdueAppointmentSections.moreThanAnYearOverdueAppointments.size,
+              overdueListSectionStates.isMoreThanAnOneYearOverdueHeader,
+              MORE_THAN_A_YEAR_OVERDUE,
+              locale,
+              onToggleSection
+          ))
+
+      val moreThanAnOneYearOverdueListItems = expandedOverdueAppointmentList(
+          overdueListSectionStates.isMoreThanAnOneYearOverdueHeader,
+          overdueAppointmentSections.moreThanAnYearOverdueAppointments,
+          clock,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection
+      )
+
+      return moreThanAnOneYearOverdueHeader + moreThanAnOneYearOverdueListItems
+    }
+
+    private fun removedFromOverdueItem(
+        overdueAppointmentSections: OverdueAppointmentSections,
+        clock: UserClock,
+        overdueListSectionStates: OverdueListSectionStates,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        selectedOverdueAppointments: Set<UUID>,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        locale: Locale,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+        onToggleSection: (OverdueAppointmentSectionTitle) -> Unit,
+    ): List<OverdueUiModel> {
+      val removedFromOverdueListHeader = listOf(
+          OverdueUiModel.Header(R.string.overdue_removed_from_list_call_header,
+              overdueAppointmentSections.removedFromOverdueAppointments.size,
+              overdueListSectionStates.isRemovedFromOverdueListHeaderExpanded,
+              REMOVED_FROM_OVERDUE,
+              locale,
+              onToggleSection
+          ))
+
+      val removedFromOverdueListItems = expandedOverdueAppointmentList(
+          overdueListSectionStates.isRemovedFromOverdueListHeaderExpanded,
+          overdueAppointmentSections.removedFromOverdueAppointments,
+          clock,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection
+      )
+
+      return removedFromOverdueListHeader + removedFromOverdueListItems
+    }
+
+    private fun remindToCallItem(
+        overdueAppointmentSections: OverdueAppointmentSections,
+        clock: UserClock,
+        overdueListSectionStates: OverdueListSectionStates,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        selectedOverdueAppointments: Set<UUID>,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        locale: Locale,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+        onToggleSection: (OverdueAppointmentSectionTitle) -> Unit,
+    ): List<OverdueUiModel> {
+      val remindToCallHeader = listOf(
+          OverdueUiModel.Header(R.string.overdue_remind_to_call_header,
+              overdueAppointmentSections.remindToCallLaterAppointments.size,
+              overdueListSectionStates.isRemindToCallLaterHeaderExpanded,
+              REMIND_TO_CALL,
+              locale,
+              onToggleSection
+          ))
+
+      val remindToCallListItems = expandedOverdueAppointmentList(
+          overdueListSectionStates.isRemindToCallLaterHeaderExpanded,
+          overdueAppointmentSections.remindToCallLaterAppointments,
+          clock,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection
+      )
+
+      return remindToCallHeader + remindToCallListItems
+    }
+
+    private fun agreedToVisitItem(
+        overdueAppointmentSections: OverdueAppointmentSections,
+        clock: UserClock,
+        overdueListSectionStates: OverdueListSectionStates,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        selectedOverdueAppointments: Set<UUID>,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        locale: Locale,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+        onToggleSection: (OverdueAppointmentSectionTitle) -> Unit
+    ): List<OverdueUiModel> {
+      val agreedToVisitHeader = listOf(
+          OverdueUiModel.Header(R.string.overdue_agreed_to_visit_call_header,
+              overdueAppointmentSections.agreedToVisitAppointments.size,
+              overdueListSectionStates.isAgreedToVisitHeaderExpanded,
+              AGREED_TO_VISIT,
+              locale,
+              onToggleSection
+          ))
+
+      val agreedToVisitListItems = expandedOverdueAppointmentList(
+          overdueListSectionStates.isAgreedToVisitHeaderExpanded,
+          overdueAppointmentSections.agreedToVisitAppointments,
+          clock,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection
+      )
+
+      return agreedToVisitHeader + agreedToVisitListItems
+    }
+
+    private fun pendingToCallItem(
+        overdueAppointmentSections: OverdueAppointmentSections,
+        clock: UserClock,
+        pendingListDefaultStateSize: Int,
+        overdueListSectionStates: OverdueListSectionStates,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        selectedOverdueAppointments: Set<UUID>,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        locale: Locale,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+        onToggleSection: (OverdueAppointmentSectionTitle) -> Unit,
+        onPendingFooterClicked: () -> Unit,
+    ): List<OverdueUiModel> {
+      val pendingAppointments = overdueAppointmentSections.pendingAppointments
+      val pendingToCallHeader = listOf(
+          OverdueUiModel.Header(R.string.overdue_pending_to_call_header,
+              overdueAppointmentSections.pendingAppointments.size,
+              overdueListSectionStates.isPendingHeaderExpanded,
+              PENDING_TO_CALL,
+              locale,
+              onToggleSection
+          ))
+
+      val pendingAppointmentsContent = generatePendingAppointmentsContent(
+          overdueAppointmentSections,
+          clock,
+          pendingListDefaultStateSize,
+          overdueListSectionStates,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection
+      )
+
+      val showPendingListFooter = pendingAppointments.size > pendingListDefaultStateSize && overdueListSectionStates.isPendingHeaderExpanded
+      val pendingListFooterItem = if (showPendingListFooter) {
+        listOf(OverdueUiModel.Footer(
+            pendingListState = overdueListSectionStates.pendingListState,
+            onClick = onPendingFooterClicked
+        ))
+      } else {
+        emptyList()
+      }
+
+      return pendingToCallHeader + pendingAppointmentsContent + pendingListFooterItem
+    }
+
+    private fun generatePendingAppointmentsContent(
+        overdueAppointmentSections: OverdueAppointmentSections,
+        clock: UserClock,
+        pendingListDefaultStateSize: Int,
+        overdueListSectionStates: OverdueListSectionStates,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        selectedOverdueAppointments: Set<UUID>,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+    ): List<OverdueUiModel> {
+      val pendingAppointmentsList = when (overdueListSectionStates.pendingListState) {
+        SEE_LESS -> overdueAppointmentSections.pendingAppointments.take(pendingListDefaultStateSize)
+        SEE_ALL -> overdueAppointmentSections.pendingAppointments
+      }
+
+      val expandedPendingAppointmentList = expandedOverdueAppointmentList(
+          overdueListSectionStates.isPendingHeaderExpanded,
+          pendingAppointmentsList,
+          clock,
+          isOverdueSelectAndDownloadEnabled,
+          selectedOverdueAppointments,
+          isPatientReassignmentFeatureEnabled,
+          onCallPatient,
+          onOpenPatientDetails,
+          onToggleAppointmentSelection
+      )
+
+      return if (pendingAppointmentsList.isEmpty() && overdueListSectionStates.isPendingHeaderExpanded) {
+        listOf(OverdueUiModel.NoPendingPatients)
+      } else {
+        expandedPendingAppointmentList
+      }
+    }
+
+    private fun expandedOverdueAppointmentList(
+        isListExpanded: Boolean,
+        overdueAppointment: List<OverdueAppointment>,
+        clock: UserClock,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        selectedOverdueAppointments: Set<UUID>,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+    ): List<OverdueUiModel> {
+      return if (isListExpanded) {
+        overdueAppointment.map {
+          val isAppointmentSelected = selectedOverdueAppointments.contains(it.appointment.uuid)
+          from(
+              it,
+              clock,
+              isOverdueSelectAndDownloadEnabled,
+              isAppointmentSelected,
+              isPatientReassignmentFeatureEnabled,
+              onCallPatient,
+              onOpenPatientDetails,
+              onToggleAppointmentSelection
+          )
+        }
+      } else {
+        emptyList()
+      }
+    }
+
+    private fun from(
+        overdueAppointment: OverdueAppointment,
+        clock: UserClock,
+        isOverdueSelectAndDownloadEnabled: Boolean,
+        isAppointmentSelected: Boolean,
+        isPatientReassignmentFeatureEnabled: Boolean,
+        onCallPatient: (UUID) -> Unit,
+        onOpenPatientDetails: (UUID) -> Unit,
+        onToggleAppointmentSelection: (UUID) -> Unit,
+    ): OverdueUiModel {
+      return OverdueUiModel.Patient(
+          appointmentUuid = overdueAppointment.appointment.uuid,
+          patientUuid = overdueAppointment.appointment.patientUuid,
+          name = overdueAppointment.fullName,
+          gender = overdueAppointment.gender,
+          age = overdueAppointment.ageDetails.estimateAge(clock),
+          phoneNumber = overdueAppointment.phoneNumber?.number,
+          overdueDays = daysBetweenNowAndDate(overdueAppointment.appointment.scheduledDate, clock),
+          villageName = overdueAppointment.patientAddress.colonyOrVillage,
+          isOverdueSelectAndDownloadEnabled = isOverdueSelectAndDownloadEnabled,
+          isAppointmentSelected = isAppointmentSelected,
+          isEligibleForReassignment = (overdueAppointment.eligibleForReassignment == Answer.Yes) && isPatientReassignmentFeatureEnabled,
+          onCallClicked = { onCallPatient(overdueAppointment.appointment.patientUuid) },
+          onRowClicked = { onOpenPatientDetails(overdueAppointment.appointment.patientUuid) },
+          onCheckboxClicked = { onToggleAppointmentSelection(overdueAppointment.appointment.uuid) }
+      )
+    }
+
+    private fun daysBetweenNowAndDate(
+        date: LocalDate,
+        clock: UserClock
+    ): Int {
+      return ChronoUnit.DAYS.between(date, LocalDate.now(clock)).toInt()
+    }
+  }
+}
+

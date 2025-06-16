478db826d8cd59e03b1fbe41240ef8431bbf691b
Sasikanth Miriyampalli <hello@sasikanth.dev>
2025-06-16 09:41:15 +0530

    Use `BloodPressureSummary` composable in `BloodSugarSummaryView`

--- app/src/main/java/org/simple/clinic/summary/bloodsugar/ui/BloodSugarSummaryView.kt
+++ app/src/main/java/org/simple/clinic/summary/bloodsugar/ui/BloodSugarSummaryView.kt
@@ -5,18 +5,19 @@ import android.content.Context
 import android.content.Intent
 import android.os.Parcelable
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.f2prateek.rx.preferences2.Preference
 import com.google.android.material.card.MaterialCardView
-import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.view.detaches
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.cast
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.subjects.PublishSubject
-import org.simple.clinic.R
 import org.simple.clinic.ReportAnalyticsEvents
 import org.simple.clinic.bloodsugar.BloodSugarMeasurement
 import org.simple.clinic.bloodsugar.BloodSugarMeasurementType
@@ -25,7 +26,7 @@ import org.simple.clinic.bloodsugar.Unknown
 import org.simple.clinic.bloodsugar.entry.BloodSugarEntrySheet
 import org.simple.clinic.bloodsugar.history.BloodSugarHistoryScreen
 import org.simple.clinic.bloodsugar.selection.type.BloodSugarTypePickerSheet
-import org.simple.clinic.databinding.PatientsummaryBloodsugarsummaryContentBinding
+import org.simple.clinic.common.ui.theme.SimpleTheme
 import org.simple.clinic.di.injector
 import org.simple.clinic.facility.Facility
 import org.simple.clinic.facility.alertchange.AlertFacilityChangeSheet
@@ -60,7 +61,6 @@ import org.simple.clinic.util.UtcClock
 import org.simple.clinic.util.toLocalDateAtZone
 import org.simple.clinic.util.unsafeLazy
 import org.simple.clinic.widgets.ScreenDestroyed
-import org.simple.clinic.widgets.setPaddingBottom
 import java.time.Duration
 import java.time.Instant
 import java.time.ZoneId
@@ -73,20 +73,6 @@ class BloodSugarSummaryView(
     context: Context,
     attributes: AttributeSet
 ) : MaterialCardView(context, attributes), BloodSugarSummaryViewUi, UiActions, PatientSummaryChildView {
-
-  private var binding: PatientsummaryBloodsugarsummaryContentBinding? = null
-
-  private val addNewBloodSugar
-    get() = binding!!.addNewBloodSugar
-
-  private val bloodSugarSeeAll
-    get() = binding!!.bloodSugarSeeAll
-
-  private val bloodSugarItemContainer
-    get() = binding!!.bloodSugarItemContainer
-
-  private val noBloodSugarTextView
-    get() = binding!!.noBloodSugarTextView
 
   @Inject
   lateinit var activity: AppCompatActivity
@@ -142,15 +128,13 @@ class BloodSugarSummaryView(
 
   private val viewEvents = PublishSubject.create<BloodSugarSummaryViewEvent>()
 
+  private var summaryItems by mutableStateOf(emptyList<BloodSugarSummaryItem>())
+  private var canShowSeeAllButton by mutableStateOf(false)
+
   private var modelUpdateCallback: PatientSummaryModelUpdateCallback? = null
 
   private val events: Observable<BloodSugarSummaryViewEvent> by unsafeLazy {
-    Observable
-        .merge(
-            addNewBloodSugarClicks(),
-            seeAllClicks(),
-            viewEvents
-        )
+    viewEvents
         .compose(ReportAnalyticsEvents())
         .cast<BloodSugarSummaryViewEvent>()
   }
@@ -169,11 +153,6 @@ class BloodSugarSummaryView(
     )
   }
 
-  init {
-    val layoutInflater = LayoutInflater.from(context)
-    binding = PatientsummaryBloodsugarsummaryContentBinding.inflate(layoutInflater, this, true)
-  }
-
   override fun onFinishInflate() {
     super.onFinishInflate()
     if (isInEditMode) {
@@ -183,6 +162,28 @@ class BloodSugarSummaryView(
 
     val screenDestroys: Observable<ScreenDestroyed> = detaches().map { ScreenDestroyed }
     openEntrySheetAfterTypeIsSelected(screenDestroys)
+
+    addView(ComposeView(context).apply {
+      setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+      setContent {
+        SimpleTheme {
+          BloodSugarSummary(
+              summaryItems = summaryItems,
+              canShowSeeAllButton = canShowSeeAllButton,
+              onSeeAllClick = {
+                viewEvents.onNext(SeeAllClicked)
+              },
+              onAddBPClick = {
+                viewEvents.onNext(NewBloodSugarClicked)
+              },
+              onEditBPClick = { id, type ->
+                viewEvents.onNext(BloodSugarClicked(id = id, measurementType = type))
+              }
+          )
+        }
+      }
+    })
   }
 
   override fun onAttachedToWindow() {
@@ -203,21 +204,12 @@ class BloodSugarSummaryView(
     super.onRestoreInstanceState(delegate.onRestoreInstanceState(state))
   }
 
-  private fun addNewBloodSugarClicks(): Observable<BloodSugarSummaryViewEvent> {
-    return addNewBloodSugar.clicks().map { NewBloodSugarClicked }
-  }
-
-  private fun seeAllClicks(): Observable<BloodSugarSummaryViewEvent> {
-    return bloodSugarSeeAll.clicks().map { SeeAllClicked }
-  }
-
   override fun showBloodSugarSummary(bloodSugars: List<BloodSugarMeasurement>) {
-    render(bloodSugars)
+    summaryItems = generateBloodSugarSummaryItems(bloodSugars)
   }
 
   override fun showNoBloodSugarsView() {
-    bloodSugarItemContainer.visibility = View.GONE
-    noBloodSugarTextView.visibility = View.VISIBLE
+    summaryItems = emptyList()
   }
 
   override fun showBloodSugarTypeSelector(currentFacility: Facility) {
@@ -229,11 +221,11 @@ class BloodSugarSummaryView(
   }
 
   override fun showSeeAllButton() {
-    bloodSugarSeeAll.visibility = View.VISIBLE
+    canShowSeeAllButton = true
   }
 
   override fun hideSeeAllButton() {
-    bloodSugarSeeAll.visibility = View.GONE
+    canShowSeeAllButton = false
   }
 
   override fun showBloodSugarHistoryScreen(patientUuid: UUID) {
@@ -273,24 +265,7 @@ class BloodSugarSummaryView(
     ))
   }
 
-  private fun render(bloodSugarMeasurements: List<BloodSugarMeasurement>) {
-    val listItemViews = generateBloodSugarRows(bloodSugarMeasurements)
-
-    bloodSugarItemContainer.removeAllViews()
-    listItemViews.forEach(bloodSugarItemContainer::addView)
-
-    val itemContainerBottomPadding = if (listItemViews.size > 1) {
-      R.dimen.patientsummary_blood_sugar_summary_item_container_bottom_padding_8
-    } else {
-      R.dimen.patientsummary_blood_sugar_summary_item_container_bottom_padding_24
-    }
-    bloodSugarItemContainer.setPaddingBottom(itemContainerBottomPadding)
-
-    bloodSugarItemContainer.visibility = View.VISIBLE
-    noBloodSugarTextView.visibility = View.GONE
-  }
-
-  private fun generateBloodSugarRows(bloodSugarMeasurements: List<BloodSugarMeasurement>): List<View> {
+  private fun generateBloodSugarSummaryItems(bloodSugarMeasurements: List<BloodSugarMeasurement>): List<BloodSugarSummaryItem> {
     val measurementsByDate = bloodSugarMeasurements.groupBy { item -> item.recordedAt.atZone(utcClock.zone).toLocalDate() }
 
     return measurementsByDate.mapValues { (_, measurementList) ->
@@ -304,21 +279,14 @@ class BloodSugarSummaryView(
           null
         }
 
-        val bloodSugarItemView = LayoutInflater.from(context).inflate(R.layout.list_patientsummary_bloodsugar_measurement, this, false) as BloodSugarItemView
-        bloodSugarItemView.render(
-            measurement = measurement,
-            bloodSugarDate = dateFormatter.format(recordedAt),
-            bloodSugarTime = bloodSugarTime,
-            isBloodSugarEditable = isBloodSugarEditable,
-            bloodSugarUnitPreference = bloodSugarUnitPreference.get(),
-        ) { clickedMeasurement ->
-          viewEvents.onNext(BloodSugarClicked(
-              id = clickedMeasurement.uuid,
-              measurementType = clickedMeasurement.reading.type
-          ))
-        }
-
-        bloodSugarItemView
+        BloodSugarSummaryItem(
+            id = measurement.uuid,
+            reading = measurement.reading,
+            measurementUnit = bloodSugarUnitPreference.get(),
+            date = dateFormatter.format(recordedAt),
+            time = bloodSugarTime,
+            canEdit = isBloodSugarEditable
+        )
       }
     }.values.flatten()
   }

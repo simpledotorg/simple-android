5ee7bdedcf66e653844ddada264f5e2dfe9cf0cd
sagarwal <sagarwal@rtsl.org>
2025-08-26 13:45:55 +0530

    Use `TobaccoQuestion` in medical history summary

--- app/src/main/java/org/simple/clinic/summary/medicalhistory/ui/MedicalHistorySummary.kt
+++ app/src/main/java/org/simple/clinic/summary/medicalhistory/ui/MedicalHistorySummary.kt
@@ -19,6 +19,7 @@ import org.simple.clinic.common.ui.theme.SimpleTheme
 import org.simple.clinic.medicalhistory.Answer
 import org.simple.clinic.medicalhistory.MedicalHistoryQuestion
 import org.simple.clinic.medicalhistory.ui.MedicalHistoryQuestionItem
+import org.simple.clinic.medicalhistory.ui.TobaccoQuestion
 
 @Composable
 fun MedicalHistorySummary(
@@ -59,81 +60,13 @@ fun MedicalHistorySummary(
         onAnswerChange = onAnswerChange
     )
 
-    if (showSmokerQuestion && showSmokelessTobaccoQuestion) {
-      TobaccoUseContainer(
+    if (showSmokerQuestion) {
+      TobaccoQuestion(
           isSmokingAnswer = isSmokingAnswer,
           isUsingSmokelessTobaccoAnswer = isUsingSmokelessTobaccoAnswer,
+          showSmokelessTobaccoQuestion = showSmokelessTobaccoQuestion,
           onAnswerChange = onAnswerChange
       )
-    } else if (showSmokerQuestion) {
-      SmokerContainer(
-          smokerAnswer = isSmokingAnswer,
-          onAnswerChange = onAnswerChange
-      )
-    }
-  }
-}
-
-@Composable
-fun SmokerContainer(
-    smokerAnswer: Answer?,
-    modifier: Modifier = Modifier,
-    onAnswerChange: (MedicalHistoryQuestion, Answer) -> Unit,
-) {
-  Card(modifier = modifier) {
-    MedicalHistoryQuestionItem(
-        modifier = Modifier
-            .padding(horizontal = dimensionResource(R.dimen.spacing_16))
-            .padding(vertical = dimensionResource(R.dimen.spacing_8)),
-        question = MedicalHistoryQuestion.IsSmoking,
-        selectedAnswer = smokerAnswer,
-        showDivider = false,
-        onSelectionChange = {
-          onAnswerChange(MedicalHistoryQuestion.IsSmoking, it)
-        }
-    )
-  }
-}
-
-@Composable
-fun TobaccoUseContainer(
-    isSmokingAnswer: Answer?,
-    isUsingSmokelessTobaccoAnswer: Answer?,
-    modifier: Modifier = Modifier,
-    onAnswerChange: (MedicalHistoryQuestion, Answer) -> Unit,
-) {
-  Card(modifier = modifier) {
-    Column(
-        modifier = Modifier
-            .padding(horizontal = dimensionResource(R.dimen.spacing_16))
-            .padding(
-                top = dimensionResource(R.dimen.spacing_16),
-                bottom = dimensionResource(R.dimen.spacing_4)
-            )
-    ) {
-      Text(
-          text = stringResource(R.string.medicalhistorysummaryview_tobacco_use),
-          style = SimpleTheme.typography.subtitle1Medium,
-          color = MaterialTheme.colors.onSurface,
-      )
-
-      Spacer(Modifier.requiredHeight(dimensionResource(R.dimen.spacing_4)))
-
-      MedicalHistoryQuestionItem(
-          question = MedicalHistoryQuestion.IsSmoking,
-          selectedAnswer = isSmokingAnswer,
-          showDivider = true,
-      ) {
-        onAnswerChange(MedicalHistoryQuestion.IsSmoking, it)
-      }
-
-      MedicalHistoryQuestionItem(
-          question = MedicalHistoryQuestion.IsUsingSmokelessTobacco,
-          selectedAnswer = isUsingSmokelessTobaccoAnswer,
-          showDivider = false,
-      ) {
-        onAnswerChange(MedicalHistoryQuestion.IsUsingSmokelessTobacco, it)
-      }
     }
   }
 }
